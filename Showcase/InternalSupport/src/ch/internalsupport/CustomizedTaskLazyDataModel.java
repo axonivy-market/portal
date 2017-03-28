07d98c343b06ca43a1a43801ceabd2cea8bcbea2
anh.nguyen <anh.nguyen>
2017-03-28 09:01:11 +0000

    IVYPORTAL-2670 Custom task columns - Format code.
    Former-commit-id: 92aca876d6ae3d421581d3ac4396ec25abbaf450

--- Showcase/InternalSupport/src/ch/internalsupport/CustomizedTaskLazyDataModel.java
+++ Showcase/InternalSupport/src/ch/internalsupport/CustomizedTaskLazyDataModel.java
@@ -5,16 +5,16 @@ import ch.ivy.addon.portalkit.datamodel.TaskLazyDataModel;
 import ch.ivyteam.ivy.workflow.query.TaskQuery;
 
 public class CustomizedTaskLazyDataModel extends TaskLazyDataModel {
-	
-	private static final long serialVersionUID = 7996851327481047161L;
-	
-	public CustomizedTaskLazyDataModel() {
+
+  private static final long serialVersionUID = 7996851327481047161L;
+
+  public CustomizedTaskLazyDataModel() {
     super();
   }
-	
-	@Override
-	public TaskQuery buildTaskQuery() {
-	  TaskQuery taskQuery = TaskQuery.create();
+
+  @Override
+  public TaskQuery buildTaskQuery() {
+    TaskQuery taskQuery = TaskQuery.create();
     if ("customVarcharField5".equalsIgnoreCase(queryCriteria.getSortField())) {
       if (queryCriteria.isSortDescending()) {
         taskQuery.orderBy().customVarCharField5().descending();
@@ -23,13 +23,13 @@ public class CustomizedTaskLazyDataModel extends TaskLazyDataModel {
       }
     }
     return taskQuery;
-	}
-	
-	@Override
-	public void extendSortTasksInNotDisplayedTaskMap() {
-		if("customVarcharField5".equalsIgnoreCase(queryCriteria.getSortField())) {
-		  comparator = comparatorString(RemoteTask::getCustomVarCharField5);
-		}
-	}
+  }
+
+  @Override
+  public void extendSortTasksInNotDisplayedTaskMap() {
+    if ("customVarcharField5".equalsIgnoreCase(queryCriteria.getSortField())) {
+      comparator = comparatorString(RemoteTask::getCustomVarCharField5);
+    }
+  }
 
 }
