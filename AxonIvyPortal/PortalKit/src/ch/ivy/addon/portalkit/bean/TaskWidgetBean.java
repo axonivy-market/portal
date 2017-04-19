871e643a034d7e423547213e358c27aedcb344af
tung.le <tung.le>
2017-04-19 06:59:02 +0000

    IVYPORTAL-3642 Use HTML in task descriptions III. - Displayed task description when in html or unavailable

--- AxonIvyPortal/PortalKit/src/ch/ivy/addon/portalkit/bean/TaskWidgetBean.java
+++ AxonIvyPortal/PortalKit/src/ch/ivy/addon/portalkit/bean/TaskWidgetBean.java
@@ -8,11 +8,15 @@ import static org.owasp.html.Sanitizers.STYLES;
 import static org.owasp.html.Sanitizers.TABLES;
 
 import java.io.Serializable;
+import java.util.Optional;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 
+import org.apache.commons.lang3.StringUtils;
+
 import ch.ivy.addon.portalkit.datamodel.TaskLazyDataModel;
+import ch.ivyteam.ivy.environment.Ivy;
 
 @ManagedBean
 @ViewScoped
@@ -48,7 +52,27 @@ public class TaskWidgetBean implements Serializable {
     this.dataModel = dataModel;
   }
 
-  public String sanitizeHTML(String html) {
-    return BLOCKS.and(FORMATTING).and(LINKS).and(STYLES).and(IMAGES).and(TABLES).sanitize(html);
+  public String sanitizeHTML(String text) {
+    String sanitizedText =
+        Optional.ofNullable(text)
+            .map(t -> BLOCKS.and(FORMATTING).and(LINKS).and(STYLES).and(IMAGES).and(TABLES).sanitize(t)).orElse("");
+    if (StringUtils.isEmpty(sanitizedText.trim())) {
+      return Ivy.cms().co("/ch.ivy.addon.portalkit.ui.jsf/components/taskStart/taskDescriptionNotAvailable");
+    }
+    return sanitizedText;
+  }
+
+  public String createTaskDescriptionInTaskStart(String text) {
+    if (containsHTML(text)) {
+      return Ivy.cms().co("/ch.ivy.addon.portalkit.ui.jsf/components/taskStart/descriptionInHTML");
+    }
+    if (Optional.ofNullable(text).map(t -> t.trim().equals("")).orElse(true)) {
+      return Ivy.cms().co("/ch.ivy.addon.portalkit.ui.jsf/components/taskStart/taskDescriptionNotAvailable");
+    }
+    return text;
+  }
+
+  private boolean containsHTML(String text) {
+    return Optional.ofNullable(text).map(t -> t.matches(".*<.*>.*")).orElse(false);
   }
 }
