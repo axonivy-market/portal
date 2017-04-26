f3ef18b3824e3ce5f9cb9285290f028b33e9a959
long.do <long.do>
2017-04-26 11:01:10 +0000

    IVYPORTAL-2212 - Sticky task list: Set default end page to Portal automatically

--- AxonIvyPortal/PortalKit/src/ch/ivy/addon/portalkit/persistence/variable/GlobalVariable.java
+++ AxonIvyPortal/PortalKit/src/ch/ivy/addon/portalkit/persistence/variable/GlobalVariable.java
@@ -5,4 +5,5 @@ public class GlobalVariable {
   public static final String PROVIDER_URL_KEY = "LDAP_PROVIDER_URL";
   public static final String HIDE_LOGOUT_BUTTON = "HIDE_LOGOUT_BUTTON";
   public static final String SHOW_ENVIRONMENT_INFO = "SHOW_ENVIRONMENT_INFO";
+  public static final String DEFAULT_END_PAGE_TO_PORTAL = "DEFAULT_END_PAGE_TO_PORTAL";
 }
