37c63df3966a80451eb5eba979e3beb44e67e6cc
anh.nguyen <anh.nguyen>
2017-05-15 06:38:16 +0000

    IVYPORTAL-3718 Integrate Express process starts - Added express workflow to process list + favorite process.

--- AxonIvyPortal/PortalKit/src/ch/ivy/addon/portalkit/persistence/domain/UserProcess.java
+++ AxonIvyPortal/PortalKit/src/ch/ivy/addon/portalkit/persistence/domain/UserProcess.java
@@ -1,12 +1,28 @@
 package ch.ivy.addon.portalkit.persistence.domain;
 
+import java.util.Objects;
+
+import ch.ivy.addon.portalkit.enums.UserProcessType;
+
 
 public class UserProcess extends BusinessEntity {
   private String userName;
   private String processName;
   private String icon;
   private String link;
+  private UserProcessType type;
   private boolean defaultProcess;
+
+  public UserProcess() {
+
+  }
+
+  public UserProcess(String processName, UserProcessType type, String userName, String link) {
+    this.processName = processName;
+    this.type = type;
+    this.userName = userName;
+    this.link = link;
+  }
 
   public String getUserName() {
     return userName;
@@ -48,9 +64,22 @@ public class UserProcess extends BusinessEntity {
     this.defaultProcess = defaultProcess;
   }
 
+  public UserProcessType getType() {
+    return type;
+  }
+
+  public void setType(UserProcessType type) {
+    this.type = type;
+  }
+
   @Override
   public String toString() {
     return "UserProcess {userName=" + userName + ", processName=" + processName + ", icon=" + icon + ", link=" + link
         + ", isDefaultProcess=" + defaultProcess + ", id=" + getId() + "}";
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(defaultProcess, icon, link, processName, type, userName);
   }
 }
