f8efab3823ddd146127cdd06205a418ff32382ce
nguyen.to <nguyen.to>
2017-06-21 04:43:21 +0000

    IVYPORTAL-3932 Various or urgent requests in S140: - IVYPORTAL-3988 Fix bug: Empty folder created on disk when starting a Workflow start (Express)

--- AxonIvyPortal/AxonIvyExpress/src/ch/ivy/addon/portalkit/util/DocumentFileUtils.java
+++ AxonIvyPortal/AxonIvyExpress/src/ch/ivy/addon/portalkit/util/DocumentFileUtils.java
@@ -8,18 +8,20 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import javax.faces.context.FacesContext;
 
-import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.FilenameUtils;
 import org.primefaces.model.DefaultStreamedContent;
 import org.primefaces.model.StreamedContent;
 
 import ch.ivy.addon.portalkit.vo.DocumentFile;
 import ch.ivyteam.ivy.environment.Ivy;
+import ch.ivyteam.ivy.workflow.ICase;
+import ch.ivyteam.ivy.workflow.document.IDocument;
+import ch.ivyteam.ivy.workflow.document.IDocumentService;
 
 /**
  * @author lptchi
@@ -28,90 +30,73 @@ import ch.ivyteam.ivy.environment.Ivy;
 public class DocumentFileUtils {
 	
 	/**
-	 * @param caseId root folder 
+	 * @param currentCase root folder 
 	 * @param inputStream file to check exist
 	 * @param fileName file name to check exist
 	 * @return boolean check file is exist or not
 	 */
-	public static boolean checkFileExist(String caseId, InputStream inputStream, String fileName){
+	public static boolean checkFileExist(ICase currentCase, InputStream inputStream, String fileName){
 		String originalFileName = FilenameUtils.getName(fileName);
+		IDocumentService documentService = currentCase.documents();
 		String home = Ivy.var().get("de_portalkit_upload_folder");
-		File newFile = new File(home + "\\" + caseId + "\\" + originalFileName);
-		
-		return newFile.exists();
+		List<IDocument> documents = documentService.getAll();
+		documents = documents.stream().filter(document -> document.getPath().asString().contains(home) && document.getPath().asString().contains(originalFileName)).collect(Collectors.toList());
+		return !documents.isEmpty();
 	}
 	/**
 	 * @param filePath file Path to delete
+	 * @param currentCase current case
 	 * @return boolean delete success or not
 	 */
-	public static boolean deleteFile(String filePath){
+	public static boolean deleteFile(String filePath, ICase currentCase){
 		boolean kq = false;
-		File fileDelete = new File(filePath);
-		if (fileDelete.exists()){
-			kq = fileDelete.delete();
+		IDocumentService documentService = currentCase.documents();
+		List<IDocument> documents = documentService.getAll().stream().filter(document -> document.getPath().asString().contains(filePath)).collect(Collectors.toList());
+		if (documents != null && !documents.isEmpty()) {
+			documentService.delete(documents.get(0));
+			kq = true;
 		}
 		return kq;
 	}
 	
 	/**
-	 * @param caseId root folder by case Id
-	 */
-	public static void makeDirectory(String caseId){
-		
-		String home = Ivy.var().get("de_portalkit_upload_folder");
-		File caseFolder = new File(home + "\\" + caseId);
-		if (!caseFolder.exists()){
-			try {
-				FileUtils.forceMkdir(caseFolder);
-			} catch (IOException e) {
-				Ivy.log().debug(e);
-			}
-		}
-	}
-	
-	/**
-	 * @param caseId list all file in the folder with case Id
+	 * @param currentCase list all file in the folder with current case
 	 * @return List<DocumentFile> list of {@link DocumentFile}
 	 */
-	public static List<DocumentFile> listFileInDirectory(String caseId){
+	public static List<DocumentFile> listFileInDirectory(ICase currentCase){
+		IDocumentService documentService = currentCase.documents();
 		String home = Ivy.var().get("de_portalkit_upload_folder");
-		File caseFolder = new File(home + "\\" + caseId);
+		List<IDocument> documents = documentService.getAll();
+		documents = documents.stream().filter(document -> document.getPath().asString().contains(home)).collect(Collectors.toList());
 		List<DocumentFile> listFile = new ArrayList<DocumentFile>();
-		if (caseFolder.exists()){
-			File[] allFiles = caseFolder.listFiles();
-			if (allFiles != null && allFiles.length > 0){
-				for (File file : allFiles){
-					DocumentFile documentFile = new DocumentFile();
-					documentFile.setFileName(file.getName());
-					documentFile.setFileSize(file.length() < 1024 ? "1KB" : file.length()/1024 + "KB");
-					documentFile.setModifiedDate(new Date(file.lastModified()));
-					documentFile.setDownloadPath(file.getAbsolutePath());
-					listFile.add(documentFile);
-				}
+		if (documents != null){
+			for (IDocument document : documents){
+				DocumentFile documentFile = new DocumentFile();
+				documentFile.setFileName(document.getName());
+				documentFile.setFileSize(document.getSize() < 1024 ? "1KB" : document.getSize()/1024 + "KB");
+				documentFile.setModifiedDate(document.getLastModification().getTimestamp().toJavaDate());
+				documentFile.setDownloadPath(document.getPath().asString());
+				listFile.add(documentFile);
 			}
-		}else{
-			makeDirectory(caseId);
 		}
 		return listFile;
 	}
 	
 	/**
 	 * Upload file and override the old one
-	 * @param caseId root folder by case Id
+	 * @param currentCase root folder by current case
 	 * @param inputStream file to upload
 	 * @param fileName file name of file upload
 	 * @return upload success or not
 	 */
-	public static boolean uploadToDirectory(String caseId, InputStream inputStream, String fileName){
+	public static boolean uploadToDirectory(ICase currentCase, InputStream inputStream, String fileName){
 		String home = Ivy.var().get("de_portalkit_upload_folder");
+		IDocumentService documentService = currentCase.documents();
 		String originalFileName = FilenameUtils.getName(fileName);
-		File newFile = new File(home + "\\" + caseId + "\\" + originalFileName);
 		try {
-			FileUtils.copyInputStreamToFile(inputStream, newFile);
+			documentService.add((home.isEmpty() ? "" : home + "/") + currentCase.getId() + "/" + originalFileName).write().withContentFrom(inputStream);
 			return true;
-		} catch (IOException e) {
-			
-			Ivy.log().debug(e);
+		} catch (Exception e) {
 			return false;
 		}
 	}
@@ -119,16 +104,23 @@ public class DocumentFileUtils {
 	 /**
 	 * @param path file path to download
 	 * @param fileName file name to return when download
+	 * @param currentCase current case
 	 * @return {@link StreamedContent}
 	 */
-	public static StreamedContent downloadFile(String path, String fileName) {
+	public static StreamedContent downloadFile(String path, String fileName, ICase currentCase) {
 		 StreamedContent output = null;
 		 String contentType = FacesContext.getCurrentInstance().getExternalContext().getMimeType(path);
-		 try{
-			 output = new DefaultStreamedContent(new FileInputStream(path), contentType, fileName);
-		 }catch(IOException e){
-			 Ivy.log().debug(e);
-		 }
+		 IDocumentService documentService = currentCase.documents();
+		List<IDocument> documents = documentService.getAll().stream().filter(document -> document.getPath().asString().contains(path)).collect(Collectors.toList());
+		if (documents != null && !documents.isEmpty()) {
+			File fileInputStream = documents.get(0).read().asJavaFile();
+			try{
+				 output = new DefaultStreamedContent(new FileInputStream(fileInputStream), contentType, fileName);
+			 }catch(IOException e){
+				 Ivy.log().debug(e);
+			 }
+		}
+		 
 		return output;
 	 }
 }
