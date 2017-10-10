ca09589e6edfa15cd9eb8a84092354b140444b54
nguyen.to <nguyen.to>
2017-10-10 02:26:12 +0000

    IVYPORTAL-2103: Remove parent (part of) pom.xml - Merge PortalGUITest to project

---
+++ AxonIvyPortal/PortalTest/src_test/portal/guitest/test/SearchTaskTest.java
@@ -0,0 +1,47 @@
+package portal.guitest.test;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import portal.guitest.common.BaseTest;
+import portal.guitest.common.TestAccount;
+import portal.guitest.page.HomePage;
+import portal.guitest.page.LoginPage;
+import portal.guitest.page.SearchResultPage;
+import portal.guitest.page.TemplatePage.GlobalSearch;
+
+public class SearchTaskTest extends BaseTest {
+  private HomePage homePage;
+
+  @Before
+  public void setup() {
+    super.setup();
+    navigateToUrl(createTestingTasksUrl);
+    redirectToRelativeLink(HomePage.PORTAL_HOME_PAGE_URL);
+
+    LoginPage loginPage = new LoginPage(TestAccount.DEMO_USER);
+    loginPage.login();
+    homePage = new HomePage();
+  }
+
+  @Test
+  public void testFindTaskByNameAndOpenTaskList() {
+    GlobalSearch globalSearch = homePage.getGlobalSearch();
+    assertTrue(globalSearch.isDisplayed());
+    
+    globalSearch.clickOnGlobalSearchIcon();
+    homePage.waitForElementDisplayed(globalSearch.getSearchContainer(), true);
+    
+    String taskName = "Annual Leave Request";
+    globalSearch.inputSearchKeyword(taskName);
+    
+    homePage.waitAjaxIndicatorDisappear();
+
+    assertEquals(1, globalSearch.countFoundTasks());
+
+    int numberOfTasks = globalSearch.countFoundTasks();
+    SearchResultPage resultPage = globalSearch.startTaskOnGlobalSearch(taskName);
+    assertEquals(numberOfTasks, resultPage.countTasks());
+    assertTrue(resultPage.isTaskItemSelected(0));
+  }
+}
---
+++ AxonIvyPortal/PortalTest/src_test/portal/guitest/test/TaskDescriptionChangeTest.java
@@ -0,0 +1,63 @@
+package portal.guitest.test;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import portal.guitest.common.BaseTest;
+import portal.guitest.common.TestAccount;
+import portal.guitest.page.HomePage;
+import portal.guitest.page.LoginPage;
+import portal.guitest.page.TaskWidgetPage;
+
+public class TaskDescriptionChangeTest extends BaseTest {
+
+  @Before
+  public void setup() {
+    super.setup();
+    createTestingTasks();
+    redirectToRelativeLink(HomePage.PORTAL_HOME_PAGE_URL);
+  }
+
+  private void login(TestAccount testAccount) {
+    LoginPage loginPage = new LoginPage(testAccount);
+    loginPage.login();
+  }
+
+  @Test
+  public void testChangeTaskDescription() {
+    login(TestAccount.ADMIN_USER);
+    int firstTask = 0;
+    TaskWidgetPage taskWidgetPage = new TaskWidgetPage();
+    taskWidgetPage.expand();
+    taskWidgetPage.openTaskDetails(firstTask);
+    testChangeTaskDescription(firstTask, "Hello World!", "Hello World!", "Hello World!", taskWidgetPage);
+    testChangeTaskDescription(
+        firstTask,
+        "<b>HTML</b> description could contain malicious script <script>alert('Attacking')</script> but it will be sanitized.",
+        "HTML description could contain malicious script but it will be sanitized.",
+        "HTML description could contain malicious script but it will be sanitized.", taskWidgetPage);
+    testChangeTaskDescription(firstTask, "", "[no description is available]", "[no description is available]",
+        taskWidgetPage);
+    testChangeTaskDescription(firstTask, "And you can change description if it is empty",
+        "And you can change description if it is empty", "And you can change description if it is empty",
+        taskWidgetPage);
+  }
+
+  private void testChangeTaskDescription(int firstTask, String newDescription, String shownDescriptionInDetails,
+      String shownDescriptionInHeader, TaskWidgetPage taskWidgetPage) {
+    taskWidgetPage.changeDescriptionOfTask(firstTask, newDescription);
+    assertEquals(taskWidgetPage.getDescriptionOfTaskAt(firstTask), shownDescriptionInDetails);
+    assertEquals(taskWidgetPage.getDescriptionInHeaderOfTaskAt(firstTask), shownDescriptionInHeader);
+  }
+
+  @Test
+  public void testUserWithoutPermissionCannotChangeTaskName() {
+    login(TestAccount.DEMO_USER);
+    int firstTask = 0;
+    TaskWidgetPage taskWidgetPage = new TaskWidgetPage();
+    taskWidgetPage.expand();
+    taskWidgetPage.openTaskDetails(firstTask);
+    assertFalse(taskWidgetPage.isTaskNameChangeComponentPresented(firstTask));
+  }
+
+}
