ad54ca69d93706d10cc3bbef038026c47d45276d
nguyen.to <nguyen.to>
2017-10-10 06:26:04 +0000

    IVYPORTAL-2103: Remove parent (part of) pom.xml - It's obsoleted now

--- AxonIvyPortal/PortalGUITest/src/test/java/portal/common/DateTimePattern.java
+++
@@ -1,7 +0,0 @@
-package portal.common;
-
-public class DateTimePattern {
-
-  public static final String DATE_PATTERN = "dd.MM.yyyy";
-  public static final String DATE_TIME_PATTERN = "dd.MM.yyyy HH:mm";
-}
