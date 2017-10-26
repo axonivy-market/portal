cd9e7c1218078108cba237fd46a94b28051adef3
tung.le <tung.le>
2017-10-26 09:03:59 +0000

    ECDEV-1398 Various requests & changes in S146 - Remove obsolete code after ivyTeam fixed XIVY-2042
    Former-commit-id: 49004234d52ce4b152436ead8ed4611b3bab3d07

--- AxonIvyPortal/PortalKit/src/ch/ivy/addon/portalkit/service/ExpressProcessService.java
+++ AxonIvyPortal/PortalKit/src/ch/ivy/addon/portalkit/service/ExpressProcessService.java
@@ -1,7 +1,6 @@
 package ch.ivy.addon.portalkit.service;
 
 
-import java.util.Collections;
 import java.util.List;
 
 import ch.ivy.addon.portalkit.bo.ExpressProcess;
@@ -9,10 +8,7 @@ import ch.ivy.addon.portalkit.bo.ExpressProcess;
 public class ExpressProcessService extends AbstractExpressService<ExpressProcess> {
 
   public List<ExpressProcess> findAllOrderByName() {
-    if (repo().search(getType()).execute().totalCount() > 0) {
-      return repo().search(getType()).orderBy().textField("processName").ascending().execute().getAll();
-    }
-    return Collections.emptyList();
+    return repo().search(getType()).orderBy().textField("processName").ascending().execute().getAll();
   }
 
   @Override
