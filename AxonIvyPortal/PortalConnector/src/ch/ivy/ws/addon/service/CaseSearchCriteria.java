9aff3ed6652a42f98b1c2960f2160a4d386e4080
nguyen.to <nguyen.to>
2017-11-07 04:42:45 +0000

    IVYPORTAL-2769: Support case query II - Implement

--- AxonIvyPortal/PortalConnector/src/ch/ivy/ws/addon/service/CaseSearchCriteria.java
+++ AxonIvyPortal/PortalConnector/src/ch/ivy/ws/addon/service/CaseSearchCriteria.java
@@ -3,28 +3,20 @@ package ch.ivy.ws.addon.service;
 import java.util.List;
 import java.util.Objects;
 
-import ch.ivy.ws.addon.enums.CaseSortedField;
-import ch.ivyteam.ivy.workflow.CaseState;
+import org.apache.commons.lang.StringUtils;
 
 public class CaseSearchCriteria {
 
+  private String jsonQuery;
   private String involvedUsername;
   private List<String> involvedApplications;
-  private List<CaseState> includedStates;
-  private List<CaseState> excludedStates;
-  private String keyword;
-  private Long taskId;
-  private Long caseId;
   private boolean ignoreInvolvedUser;
-  private CaseSortedField sortedField;
-  private boolean isSortingDescending;
   private boolean isBusinessCase;
   private boolean isTechnicalCase;
   private Long businessCaseId;
 
   public boolean isEmpty() {
-    return !hasInvolvedUsername() && !hasInvolvedApplications() && !hasKeyword() && !hasExcludedStates()
-        && !hasIncludedStates() && !hasTaskId() && !hasCaseId();
+    return !hasInvolvedUsername() && !hasInvolvedApplications() && !hasJsonQuery();
   }
 
   public String getInvolvedUsername() {
@@ -43,44 +35,8 @@ public class CaseSearchCriteria {
     this.involvedApplications = involvedApplications;
   }
 
-  public List<CaseState> getIncludedStates() {
-    return includedStates;
-  }
-
-  public void setIncludedStates(List<CaseState> includedStates) {
-    this.includedStates = includedStates;
-  }
-
-  public List<CaseState> getExcludedStates() {
-    return excludedStates;
-  }
-
-  public void setExcludedStates(List<CaseState> excludedStates) {
-    this.excludedStates = excludedStates;
-  }
-
-  public String getKeyword() {
-    return keyword;
-  }
-
-  public void setKeyword(String keyword) {
-    this.keyword = keyword;
-  }
-
-  public boolean hasIncludedStates() {
-    return includedStates != null && !includedStates.isEmpty();
-  }
-
-  public boolean hasExcludedStates() {
-    return excludedStates != null && !excludedStates.isEmpty();
-  }
-
   public boolean hasInvolvedApplications() {
     return involvedApplications != null && !involvedApplications.isEmpty();
-  }
-
-  public boolean hasKeyword() {
-    return !(Objects.toString(keyword, "").isEmpty());
   }
 
   public boolean hasInvolvedUsername() {
@@ -93,46 +49,6 @@ public class CaseSearchCriteria {
 
   public void setIgnoreInvolvedUser(boolean ignoreInvolvedUser) {
     this.ignoreInvolvedUser = ignoreInvolvedUser;
-  }
-
-  public Long getTaskId() {
-    return taskId;
-  }
-
-  public void setTaskId(Long taskId) {
-    this.taskId = taskId;
-  }
-
-  public boolean hasTaskId() {
-    return taskId != null && taskId != 0;
-  }
-
-  public Long getCaseId() {
-    return caseId;
-  }
-
-  public void setCaseId(Long caseId) {
-    this.caseId = caseId;
-  }
-
-  public boolean hasCaseId() {
-    return caseId != null && caseId != 0;
-  }
-
-  public CaseSortedField getSortedField() {
-    return sortedField;
-  }
-
-  public void setSortedField(CaseSortedField sortedField) {
-    this.sortedField = sortedField;
-  }
-
-  public boolean isSortingDescending() {
-    return isSortingDescending;
-  }
-
-  public void setSortingDescending(boolean isSortingDescending) {
-    this.isSortingDescending = isSortingDescending;
   }
 
   public boolean isBusinessCase() {
@@ -157,6 +73,18 @@ public class CaseSearchCriteria {
 
   public void setBusinessCaseId(Long businessCaseId) {
 	this.businessCaseId = businessCaseId;
+  }
+  
+  public String getJsonQuery() {
+	    return jsonQuery;
+  }
+
+  public void setJsonQuery(String jsonQuery) {
+    this.jsonQuery = jsonQuery;
+  }
+
+  public boolean hasJsonQuery() {
+    return StringUtils.isNotBlank(jsonQuery);
   }
   
 }
