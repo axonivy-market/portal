8dca63fb5cf8fb9d6f51ab4f1b87243282a5dbc7
nguyen.to <nguyen.to>
2017-11-08 09:32:14 +0000

    IVYPORTAL-4433: Fix performance bug in user/role selection - Add sorting for case
    Former-commit-id: 280f86f7708f164d4ed770ed5dcda50642db625b

--- AxonIvyPortal/PortalKit/src/ch/ivy/addon/portalkit/service/CaseQueryService.java
+++ AxonIvyPortal/PortalKit/src/ch/ivy/addon/portalkit/service/CaseQueryService.java
@@ -2,10 +2,12 @@ package ch.ivy.addon.portalkit.service;
 
 import java.util.List;
 
+import ch.ivy.addon.portalkit.enums.CaseSortField;
 import ch.ivy.addon.portalkit.support.CaseQueryCriteria;
 import ch.ivyteam.ivy.workflow.CaseState;
 import ch.ivyteam.ivy.workflow.query.CaseQuery;
 import ch.ivyteam.ivy.workflow.query.CaseQuery.IFilterQuery;
+import ch.ivyteam.ivy.workflow.query.CaseQuery.OrderByColumnQuery;
 import ch.ivyteam.ivy.workflow.query.TaskQuery;
 
 public class CaseQueryService {
@@ -44,6 +46,9 @@ public class CaseQueryService {
         if (criteria.hasKeyword()) {
             finalQuery.where().and(queryForKeyword(criteria.getKeyword()));
         }
+        
+        CaseSortingQueryAppender appender = new CaseSortingQueryAppender(finalQuery);
+        finalQuery = appender.appendSorting(criteria).toQuery();
         return finalQuery;
     }
 
@@ -78,5 +83,92 @@ public class CaseQueryService {
         }
         return filterByKeywordQuery;
     }
+    
+    private static final class CaseSortingQueryAppender {
+
+        private CaseQuery query;
+
+        public CaseSortingQueryAppender(CaseQuery query) {
+          this.query = query;
+        }
+
+        public CaseQuery toQuery() {
+          return query;
+        }
+
+        public CaseSortingQueryAppender appendSorting(CaseQueryCriteria criteria) {
+          appendSortByNameIfSet(criteria);
+          appendSortByIdIfSet(criteria);
+          appendSortByStartTimeIfSet(criteria);
+          appendSortByEndTimeIfSet(criteria);
+          appendSortByCreatorIfSet(criteria);
+          appendSortByStateIfSet(criteria);
+          return this;
+        }
+
+        private void appendSortByNameIfSet(CaseQueryCriteria criteria) {
+          if (!CaseSortField.NAME.toString().equalsIgnoreCase(criteria.getSortField())) {
+            return;
+          }
+          OrderByColumnQuery orderByName = query.orderBy().name();
+          if (criteria.isSortDescending()) {
+            orderByName.descending();
+          }
+        }
+
+        private void appendSortByIdIfSet(CaseQueryCriteria criteria) {
+          if (criteria.getSortField() == null) {
+            query.orderBy().caseId().descending();
+            return;
+          }
+          if (!CaseSortField.ID.toString().equalsIgnoreCase(criteria.getSortField())) {
+            return;
+          }
+          OrderByColumnQuery orderByName = query.orderBy().caseId();
+          if (criteria.isSortDescending()) {
+            orderByName.descending();
+          }
+        }
+
+        private void appendSortByStartTimeIfSet(CaseQueryCriteria criteria) {
+          if (!CaseSortField.START_TIME.toString().equalsIgnoreCase(criteria.getSortField())) {
+            return;
+          }
+          OrderByColumnQuery orderByName = query.orderBy().startTimestamp();
+          if (criteria.isSortDescending()) {
+            orderByName.descending();
+          }
+        }
+
+        private void appendSortByEndTimeIfSet(CaseQueryCriteria criteria) {
+          if (!CaseSortField.END_TIME.toString().equalsIgnoreCase(criteria.getSortField())) {
+            return;
+          }
+          OrderByColumnQuery orderByName = query.orderBy().endTimestamp();
+          if (criteria.isSortDescending()) {
+            orderByName.descending();
+          }
+        }
+
+        private void appendSortByCreatorIfSet(CaseQueryCriteria criteria) {
+          if (!CaseSortField.CREATOR.toString().equalsIgnoreCase(criteria.getSortField())) {
+            return;
+          }
+          OrderByColumnQuery orderByName = query.orderBy().creatorUserDisplayName();
+          if (criteria.isSortDescending()) {
+            orderByName.descending();
+          }
+        }
+
+        private void appendSortByStateIfSet(CaseQueryCriteria criteria) {
+          if (!CaseSortField.STATE.toString().equalsIgnoreCase(criteria.getSortField())) {
+            return;
+          }
+          OrderByColumnQuery orderByName = query.orderBy().state();
+          if (criteria.isSortDescending()) {
+            orderByName.descending();
+          }
+        }
+   }
 
 }
