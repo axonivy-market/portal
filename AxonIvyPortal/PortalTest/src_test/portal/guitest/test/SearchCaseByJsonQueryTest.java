bdf7c5a4f949dc3a2d6cf3447624cf7c7d2d645d
nguyen.to <nguyen.to>
2017-11-10 10:22:21 +0000

    IVYPORTAL-2769: Support case query II - Update test
    Former-commit-id: e5d629f940ee0b505a6e552696956115d2e8906f

--- AxonIvyPortal/PortalTest/src_test/portal/guitest/test/SearchCaseByJsonQueryTest.java
+++ AxonIvyPortal/PortalTest/src_test/portal/guitest/test/SearchCaseByJsonQueryTest.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 import portal.guitest.common.BaseTest;
 import portal.guitest.common.TestAccount;
 import portal.guitest.page.CasePage;
+import portal.guitest.page.HomePage;
 import portal.guitest.page.LoginPage;
 
 public class SearchCaseByJsonQueryTest extends BaseTest {
@@ -15,13 +16,14 @@ public class SearchCaseByJsonQueryTest extends BaseTest {
     public void setup() {
       super.setup();
       navigateToUrl(createTestingTasksUrl);
+      navigateToUrl(HomePage.PORTAL_HOME_PAGE_URL);
+      
+      LoginPage loginPage = new LoginPage(TestAccount.ADMIN_USER);
+      loginPage.login();
     }
     
     @Test
     public void testSearchCaseByJsonQuery() {
-        LoginPage loginPage = new LoginPage(TestAccount.ADMIN_USER);
-        loginPage.login();
-        
         navigateToUrl("internalSupport/15FA421D253C6746/searchByCaseJsonQuery.ivp");
         CasePage casePage = new CasePage();
         assertEquals(1, casePage.getNumberOfCases());
