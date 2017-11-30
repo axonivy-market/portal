c551be0b93559dd5ee31ff230ab07690245903b1
tung.le <tung.le>
2017-11-30 10:36:06 +0000

    IVYPORTAL-4687 Save+load+remove user defined filters for cases - Added tests

--- AxonIvyPortal/PortalTest/src_test/portal/guitest/test/CaseFilterTest.java
+++ AxonIvyPortal/PortalTest/src_test/portal/guitest/test/CaseFilterTest.java
@@ -2,6 +2,7 @@ package portal.guitest.test;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.server.browserlaunchers.Sleeper;
 
 import portal.guitest.common.BaseTest;
 import portal.guitest.common.TestAccount;
@@ -26,12 +27,58 @@ public class CaseFilterTest extends BaseTest {
     HomePage homePage = new HomePage();
     MainMenuPage mainMenuPage = homePage.openMainMenu();
     CasePage casePage = mainMenuPage.selectCaseMenu();
-    
+
     casePage.openAdvancedFilter("Description", "description");
     casePage.filterByDescription("random text");
     assertEquals(0, casePage.getNumberOfCases());
-    
+
     casePage.filterByDescription("Leave Request Description");
     assertEquals(1, casePage.getNumberOfCases());
+  }
+
+  @Test
+  public void testSaveFilter() {
+    LoginPage loginPage = new LoginPage(TestAccount.ADMIN_USER);
+    loginPage.login();
+
+    HomePage homePage = new HomePage();
+    MainMenuPage mainMenuPage = homePage.openMainMenu();
+    CasePage casePage = mainMenuPage.selectCaseMenu();
+
+    casePage.openAdvancedFilter("Description", "description");
+    casePage.filterByDescription("Leave");
+    String filterName = "Leave";
+    casePage.saveFilter(filterName);
+
+    casePage = mainMenuPage.selectCaseMenu();
+    Sleeper.sleepTight(1000);
+    assertEquals(filterName, casePage.getFilterName());
+  }
+
+  @Test
+  public void testSaveTaskFilterOnDifferentTaskList() {
+    LoginPage loginPage = new LoginPage(TestAccount.DEMO_USER);
+    loginPage.login();
+    MainMenuPage mainMenuPage = new MainMenuPage();
+    CasePage casePage = mainMenuPage.selectCaseMenu();
+
+    String filterName = "MyFilter";
+
+    casePage.openAdvancedFilter("Description", "description");
+    casePage.filterByDescription("Sick");
+    casePage.saveFilter(filterName);
+
+    redirectToRelativeLink(HomePage.INTERNAL_SUPPORT_HOME_PAGE_URL);
+    casePage = mainMenuPage.selectCaseMenu();
+
+    assertFalse(casePage.isFilterSelectionVisible());
+
+    casePage.openAdvancedFilter("Description", "description");
+    casePage.filterByDescription("Leave");
+    casePage.saveFilter(filterName);
+
+    casePage = mainMenuPage.selectCaseMenu();
+    Sleeper.sleepTight(1000);
+    assertEquals(filterName, casePage.getFilterName());
   }
 }
