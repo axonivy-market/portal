a3f1b9c9dc6c01ed4e004df79d40aef86d9e1693
Nam Le Dinh <nam.ledinh@axonactive.com>
2017-12-22 14:53:25 +0700

    IVYPORTAL-4779: Improve Portal performance - Implementation

---
+++ AxonIvyPortal/PortalConnector/src/ch/ivy/ws/addon/enums/CategoryDataType.java
@@ -0,0 +1,9 @@
+package ch.ivy.ws.addon.enums;
+
+public enum CategoryDataType {
+  CURRENT_USER_CATEGORY,
+  ALL_USERS_CATEGORY,
+  UNASSIGNED_CATEGORY,
+  MY_TASK_CATEGORY,
+  GROUP_TASK_CATEGORY
+}
