28008af10c957aa46f2fa932e42c2947ddd97b1f
AAVN\anhnguyenhoang <anh.nguyenhoang@axonactive.com>
2018-02-27 09:11:00 +0700

    IVYPORTAL-2674 Process history component - Added UI tests.
    Former-commit-id: 040bd016a9295a2e37813e25753045b52aad92bf

---
+++ AxonIvyPortal/PortalTest/src_test/portal/guitest/page/ProcessHistoryPage.java
@@ -0,0 +1,16 @@
+package portal.guitest.page;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+public class ProcessHistoryPage extends TemplatePage {
+
+  public int countCases() {
+    WebElement caseList = findElementByClassName("ui-datascroller-list");
+    return caseList.findElements(By.className("ui-datascroller-item")).size();
+  }
+  
+  public boolean isEmptyMessageDisplay() {
+    return isElementDisplayed(By.className("process-history-empty-message"));
+  }
+}
