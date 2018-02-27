1d14e6bc6aadb084e9ce8737bbbeaaf9734022af
nguyen.to <nguyen.to@axonactive.com>
2018-02-27 18:23:19 +0700

    IVYPORTAL-5030: Implement Portal statistic customization (Drilldown) II - Improve UI of expiry chart. - Improve related task list name of expiry chart.
    Former-commit-id: 5423b6e03acc1246aeb2c8b04fdf39892007355f

--- AxonIvyPortal/PortalKit/src/ch/ivy/addon/portalkit/bean/StatisticDashboardBean.java
+++ AxonIvyPortal/PortalKit/src/ch/ivy/addon/portalkit/bean/StatisticDashboardBean.java
@@ -1,6 +1,7 @@
 package ch.ivy.addon.portalkit.bean;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
@@ -24,9 +25,57 @@ import ch.ivyteam.ivy.environment.Ivy;
 @ViewScoped
 public class StatisticDashboardBean implements Serializable {
   private static final long serialVersionUID = 1L;
+  private static final int YEAR_CHART_WIDTH = 750;
+  private static final int MONTH_CHART_WIDTH = 500;
+  private static final int WEEK_CHART_WIDTH = 600;
+  private static final int DAY_CHART_WIDTH = 500;
 
   private StatisticService statisticService = new StatisticService();
   private List<StatisticChart> statisticChartList;
+  
+  public String getChartWidthStyle(List<StatisticChart> chartList) {
+    List<String> chartIdSuffixes = new ArrayList<>(); 
+    for (StatisticChart chart : chartList) {
+      String chartId = chart.getId();
+      if (chartId.contains("_")) {
+        //chart with format: id + _ + suffix is lower level (month/week/day/hour) chart when drill down
+        chartIdSuffixes.add(chartId.substring(chartId.indexOf("_") + 1));
+      }
+    }
+    int maxWidth = 0;
+    for (String suffix : chartIdSuffixes) {
+      if (statisticService.selectThisYear(suffix)) {
+        if (maxWidth < YEAR_CHART_WIDTH) {
+          maxWidth = YEAR_CHART_WIDTH;
+        }
+      } else if (statisticService.selectWeekOfMonth(suffix)) {
+        if (maxWidth < WEEK_CHART_WIDTH) {
+          maxWidth = WEEK_CHART_WIDTH;
+        }
+      } else if (statisticService.selectMonthOfYear(suffix)) {
+        if (maxWidth < MONTH_CHART_WIDTH) {
+          maxWidth = MONTH_CHART_WIDTH;
+        }
+      } else if (statisticService.selectDayOfWeek(suffix)) {
+        if (maxWidth < DAY_CHART_WIDTH) {
+          maxWidth = DAY_CHART_WIDTH;
+        }
+      } 
+    }
+    if (maxWidth > 0) {
+      return "width: " + maxWidth + "px";
+    }
+    return "";
+  }
+
+  public boolean shouldRenderArrows(List<StatisticChart> chartList) {
+    for (StatisticChart chart : chartList) {
+      if (chart.getId().contains("_")) {
+        return false;
+      }
+    }
+    return true;
+  }
 
   @PostConstruct
   public void initialize() {
