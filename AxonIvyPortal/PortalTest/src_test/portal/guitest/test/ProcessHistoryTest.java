040bd016a9295a2e37813e25753045b52aad92bf
AAVN\anhnguyenhoang <anh.nguyenhoang@axonactive.com>
2018-02-27 09:11:00 +0700

    IVYPORTAL-2674 Process history component - Added UI tests.

---
+++ AxonIvyPortal/PortalTest/src_test/portal/guitest/test/ProcessHistoryTest.java
@@ -0,0 +1,49 @@
+package portal.guitest.test;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import portal.guitest.common.BaseTest;
+import portal.guitest.common.TestAccount;
+import portal.guitest.page.HomePage;
+import portal.guitest.page.LoginPage;
+import portal.guitest.page.ProcessHistoryPage;
+
+public class ProcessHistoryTest extends BaseTest {
+  private static final String CREATE_CASE_LINK = "InternalSupport/16193718E2B3D6C4/InspectResource.ivp";
+  private static final String DISPLAY_PROCESS_HISTORY_PAGE =
+      "InternalSupport/16193718E2B3D6C4/viewProcessHistoryOfResource.ivp";
+
+  @Before
+  public void setup() {
+    super.setup();
+    redirectToRelativeLink(HomePage.PORTAL_HOME_PAGE_URL);
+
+    LoginPage loginPage = new LoginPage(TestAccount.DEMO_USER);
+    loginPage.login();
+  }
+
+  @Test
+  public void testDisplayProcessHistory() {
+    redirectToRelativeLink(CREATE_CASE_LINK);
+    redirectToRelativeLink(CREATE_CASE_LINK);
+    redirectToRelativeLink(CREATE_CASE_LINK);
+
+    redirectToRelativeLink(DISPLAY_PROCESS_HISTORY_PAGE);
+
+    ProcessHistoryPage processHistoryPage = new ProcessHistoryPage();
+    processHistoryPage.waitForPageLoaded();
+
+    assertEquals(3, processHistoryPage.countCases());
+  }
+
+  @Test
+  public void testDisplayEmptyMessage() {
+    redirectToRelativeLink(DISPLAY_PROCESS_HISTORY_PAGE);
+
+    ProcessHistoryPage processHistoryPage = new ProcessHistoryPage();
+    processHistoryPage.waitForPageLoaded();
+    
+    assertTrue(processHistoryPage.isEmptyMessageDisplay());
+  }
+}
