86d8b78381523e58ad02fd05afdae610db7c2964
nguyen.to <nguyen.to@axonactive.com>
2018-03-12 13:50:38 +0700

    Revert "IVYPORTAL-5030: Implement Portal statistic customization (Drilldown) II"
    This reverts commit 647981dba01a7fbf96a88920ffb2fc47135ccaaa.

--- AxonIvyPortal/PortalKit/src/ch/ivy/addon/portalkit/bean/StatisticDashboardBean.java
+++ AxonIvyPortal/PortalKit/src/ch/ivy/addon/portalkit/bean/StatisticDashboardBean.java
@@ -1,11 +1,14 @@
 package ch.ivy.addon.portalkit.bean;
 
+import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 
+import org.apache.commons.lang3.StringUtils;
 import org.primefaces.model.chart.BarChartModel;
 import org.primefaces.model.chart.DonutChartModel;
 import org.primefaces.model.chart.PieChartModel;
@@ -20,9 +23,59 @@ import ch.ivyteam.ivy.environment.Ivy;
 
 @ManagedBean
 @ViewScoped
-public class StatisticDashboardBean {
+public class StatisticDashboardBean implements Serializable {
+  private static final long serialVersionUID = 1L;
+  private static final int YEAR_CHART_WIDTH = 750;
+  private static final int MONTH_CHART_WIDTH = 500;
+  private static final int WEEK_CHART_WIDTH = 600;
+  private static final int DAY_CHART_WIDTH = 500;
+
   private StatisticService statisticService = new StatisticService();
   private List<StatisticChart> statisticChartList;
+  
+  public String getChartWidthStyle(List<StatisticChart> chartList) {
+    List<String> chartIdSuffixes = new ArrayList<>(); 
+    for (StatisticChart chart : chartList) {
+      String chartId = chart.getId();
+      if (chartId.contains("_")) {
+        //chart with format: id + _ + suffix is lower level (month/week/day/hour) chart when drill down
+        chartIdSuffixes.add(chartId.substring(chartId.indexOf("_") + 1));
+      }
+    }
+    int maxWidth = 0;
+    for (String suffix : chartIdSuffixes) {
+      if (statisticService.selectThisYear(suffix)) {
+        if (maxWidth < YEAR_CHART_WIDTH) {
+          maxWidth = YEAR_CHART_WIDTH;
+        }
+      } else if (statisticService.selectWeekOfMonth(suffix)) {
+        if (maxWidth < WEEK_CHART_WIDTH) {
+          maxWidth = WEEK_CHART_WIDTH;
+        }
+      } else if (statisticService.selectMonthOfYear(suffix)) {
+        if (maxWidth < MONTH_CHART_WIDTH) {
+          maxWidth = MONTH_CHART_WIDTH;
+        }
+      } else if (statisticService.selectDayOfWeek(suffix)) {
+        if (maxWidth < DAY_CHART_WIDTH) {
+          maxWidth = DAY_CHART_WIDTH;
+        }
+      } 
+    }
+    if (maxWidth > 0) {
+      return "width: " + maxWidth + "px";
+    }
+    return "";
+  }
+
+  public boolean shouldRenderArrows(List<StatisticChart> chartList) {
+    for (StatisticChart chart : chartList) {
+      if (chart.getId().contains("_")) {
+        return false;
+      }
+    }
+    return true;
+  }
 
   @PostConstruct
   public void initialize() {
@@ -56,7 +109,7 @@ public class StatisticDashboardBean {
    */
   public BarChartModel generateTaskByExpiryModel(StatisticChart statisticChart) {
     List<ExpiryStatistic> statisticData = statisticService.getExpiryStatisticData(statisticChart.getJsonQuery());
-    return statisticService.generateTaskByExpiryModel(statisticData, true);
+    return statisticService.generateTaskByExpiryModel(statisticData, true, StringUtils.EMPTY, StringUtils.EMPTY, StringUtils.EMPTY);
   }
 
   /**
