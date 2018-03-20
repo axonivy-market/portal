20928eb3318026c216e4863de38afa9d1d069736
nguyen.to <nguyen.to@axonactive.com>
2018-03-20 11:30:38 +0700

    IVYPORTAL-5123: Implement Portal statistic customization (Drilldown) III - Move constants to another separate class

---
+++ AxonIvyPortal/PortalKit/src/ch/ivy/addon/portalkit/statistics/StatisticChartConstants.java
@@ -0,0 +1,122 @@
+package ch.ivy.addon.portalkit.statistics;
+
+import ch.ivyteam.ivy.environment.Ivy;
+
+public class StatisticChartConstants {
+
+  public static final String TODAY_EXPIRY_KEY = "/ch.ivy.addon.portalkit.ui.jsf/statistic/chart/barchart/today";
+  public static final String THIS_WEEK_EXPIRY_KEY =
+      "/ch.ivy.addon.portalkit.ui.jsf/statistic/chart/taskByExpiry/thisWeek";
+  public static final String THIS_MONTH_EXPIRY_KEY =
+      "/ch.ivy.addon.portalkit.ui.jsf/statistic/chart/taskByExpiry/thisMonth";
+  public static final String THIS_YEAR_EXPIRY_KEY =
+      "/ch.ivy.addon.portalkit.ui.jsf/statistic/chart/taskByExpiry/thisYear";
+  
+  public static final String JANUARY_CMS = Ivy.cms().co(
+          "/ch.ivy.addon.portalkit.ui.jsf/statistic/chart/barchart/monthsOfYear/January");
+  public static final String FEBRUARY_CMS = Ivy.cms().co(
+          "/ch.ivy.addon.portalkit.ui.jsf/statistic/chart/barchart/monthsOfYear/February");
+  public static final String MARCH_CMS = Ivy.cms().co(
+          "/ch.ivy.addon.portalkit.ui.jsf/statistic/chart/barchart/monthsOfYear/March");
+  public static final String APRIL_CMS = Ivy.cms().co(
+          "/ch.ivy.addon.portalkit.ui.jsf/statistic/chart/barchart/monthsOfYear/April");
+  public static final String MAY_CMS = Ivy.cms().co(
+          "/ch.ivy.addon.portalkit.ui.jsf/statistic/chart/barchart/monthsOfYear/May");
+  public static final String JUNE_CMS = Ivy.cms().co(
+          "/ch.ivy.addon.portalkit.ui.jsf/statistic/chart/barchart/monthsOfYear/June");
+  public static final String JULY_CMS = Ivy.cms().co(
+          "/ch.ivy.addon.portalkit.ui.jsf/statistic/chart/barchart/monthsOfYear/July");
+  public static final String AUGUST_CMS = Ivy.cms().co(
+          "/ch.ivy.addon.portalkit.ui.jsf/statistic/chart/barchart/monthsOfYear/August");
+  public static final String SEPTEMBER_CMS = Ivy.cms().co(
+          "/ch.ivy.addon.portalkit.ui.jsf/statistic/chart/barchart/monthsOfYear/September");
+  public static final String OCTOBER_CMS = Ivy.cms().co(
+          "/ch.ivy.addon.portalkit.ui.jsf/statistic/chart/barchart/monthsOfYear/October");
+  public static final String NOVEMBER_CMS = Ivy.cms().co(
+          "/ch.ivy.addon.portalkit.ui.jsf/statistic/chart/barchart/monthsOfYear/November");
+  public static final String DECEMBER_CMS = Ivy.cms().co(
+          "/ch.ivy.addon.portalkit.ui.jsf/statistic/chart/barchart/monthsOfYear/December");
+
+  public static final String FIRSTWEEK_CMS = Ivy.cms().co(
+          "/ch.ivy.addon.portalkit.ui.jsf/statistic/chart/barchart/weeksOfMonth/firstWeek");
+  public static final String SECONDWEEK_CMS = Ivy.cms().co(
+          "/ch.ivy.addon.portalkit.ui.jsf/statistic/chart/barchart/weeksOfMonth/secondWeek");
+  public static final String THIRDWEEK_CMS = Ivy.cms().co(
+          "/ch.ivy.addon.portalkit.ui.jsf/statistic/chart/barchart/weeksOfMonth/thirdWeek");
+  public static final String FOURTHWEEK_CMS = Ivy.cms().co(
+          "/ch.ivy.addon.portalkit.ui.jsf/statistic/chart/barchart/weeksOfMonth/fourthWeek");
+  public static final String FIFTHWEEK_CMS = Ivy.cms().co(
+              "/ch.ivy.addon.portalkit.ui.jsf/statistic/chart/barchart/weeksOfMonth/fifthWeek");
+  public static final String SIXTHWEEK_CMS = Ivy.cms().co(
+              "/ch.ivy.addon.portalkit.ui.jsf/statistic/chart/barchart/weeksOfMonth/sixthWeek");
+
+  public static final String MONDAY_CMS = Ivy.cms().co(
+          "/ch.ivy.addon.portalkit.ui.jsf/statistic/chart/barchart/daysOfWeek/monday");
+  public static final String TUESDAY_CMS = Ivy.cms().co(
+          "/ch.ivy.addon.portalkit.ui.jsf/statistic/chart/barchart/daysOfWeek/tuesday");
+  public static final String WEDNESDAY_CMS = Ivy.cms().co(
+          "/ch.ivy.addon.portalkit.ui.jsf/statistic/chart/barchart/daysOfWeek/wednesday");
+  public static final String THURSDAY_CMS = Ivy.cms().co(
+          "/ch.ivy.addon.portalkit.ui.jsf/statistic/chart/barchart/daysOfWeek/thursday");
+  public static final String FRIDAY_CMS = Ivy.cms().co(
+          "/ch.ivy.addon.portalkit.ui.jsf/statistic/chart/barchart/daysOfWeek/friday");
+  public static final String SATURDAY_CMS = Ivy.cms().co(
+          "/ch.ivy.addon.portalkit.ui.jsf/statistic/chart/barchart/daysOfWeek/saturday");
+  public static final String SUNDAY_CMS = Ivy.cms().co(
+          "/ch.ivy.addon.portalkit.ui.jsf/statistic/chart/barchart/daysOfWeek/sunday");
+
+  public static final String BEFORE_8 = "<8";
+  public static final String IN_8 = "8";
+  public static final String IN_9 = "9";
+  public static final String IN_10 = "10";
+  public static final String IN_11 = "11";
+  public static final String IN_12 = "12";
+  public static final String IN_13 = "13";
+  public static final String IN_14 = "14";
+  public static final String IN_15 = "15";
+  public static final String IN_16 = "16";
+  public static final String IN_17 = "17";
+  public static final String AFTER_18 = ">18";
+
+  public static final String[] MONTHSOFYEAR = {JANUARY_CMS, FEBRUARY_CMS, MARCH_CMS, APRIL_CMS, MAY_CMS, JUNE_CMS,
+          JULY_CMS, AUGUST_CMS, SEPTEMBER_CMS, OCTOBER_CMS, NOVEMBER_CMS, DECEMBER_CMS};
+  public static final String[] WEEKSOFMONTH = {FIRSTWEEK_CMS, SECONDWEEK_CMS, THIRDWEEK_CMS, FOURTHWEEK_CMS, FIFTHWEEK_CMS, SIXTHWEEK_CMS};
+  public static final String[] DAYSOFWEEK = {MONDAY_CMS, TUESDAY_CMS, WEDNESDAY_CMS, THURSDAY_CMS, FRIDAY_CMS,
+          SATURDAY_CMS, SUNDAY_CMS};
+  public static final String[] HOURSOFDAY = {BEFORE_8, IN_8, IN_9, IN_10, IN_11, IN_12, IN_13, IN_14, IN_15, IN_16, IN_17, AFTER_18};
+
+
+  public static final String DRILLDOWN_LEVEL_YEAR = "YEAR";
+  public static final String DRILLDOWN_LEVEL_MONTH = "MONTH";
+  public static final String DRILLDOWN_LEVEL_WEEK = "WEEK";
+  public static final String DRILLDOWN_LEVEL_DAY = "DAY";
+  public static final String DRILLDOWN_LEVEL_HOUR = "HOUR";
+
+  public static final String[] DRILLDOWN_LEVELS = {DRILLDOWN_LEVEL_YEAR, DRILLDOWN_LEVEL_MONTH, DRILLDOWN_LEVEL_WEEK, DRILLDOWN_LEVEL_DAY, DRILLDOWN_LEVEL_HOUR};
+
+  public static final String EXCEPTION_PRIORITY_KEY =
+          "/ch.ivy.addon.portalkit.ui.jsf/statistic/chart/taskByPriority/exception";
+  public static final String HIGH_PRIORITY_KEY = "/ch.ivy.addon.portalkit.ui.jsf/statistic/chart/taskByPriority/high";
+  public static final String NORMAL_PRIORITY_KEY =
+          "/ch.ivy.addon.portalkit.ui.jsf/statistic/chart/taskByPriority/normal";
+  public static final String LOW_PRIORITY_KEY = "/ch.ivy.addon.portalkit.ui.jsf/statistic/chart/taskByPriority/low";
+
+  public static final String CREATED_CASE_KEY = "/ch.ivy.addon.portalkit.ui.jsf/caseState/CREATED";
+  public static final String RUNNING_CASE_KEY = "/ch.ivy.addon.portalkit.ui.jsf/caseState/RUNNING";
+  public static final String DONE_CASE_KEY = "/ch.ivy.addon.portalkit.ui.jsf/caseState/DONE";
+  public static final String FAILED_CASE_KEY = "/ch.ivy.addon.portalkit.ui.jsf/caseState/FAILED";
+
+  public static final String EXPIRY_PERIOD_CMS = "/ch.ivy.addon.portalkit.ui.jsf/statistic/chart/expiryPeriod";
+  public static final String TASK_CMS = "/ch.ivy.addon.portalkit.ui.jsf/common/tasks";
+  public static final String TASK_DATATIP_CMS = "/ch.ivy.addon.portalkit.ui.jsf/common/tasks";
+  public static final String SECOND_DATATIP_CMS = "/ch.ivy.addon.portalkit.ui.jsf/statistic/chart/second";
+
+  public static final String NO_CATEGORY_CMS = Ivy.cms().co(
+          "/ch.ivy.addon.portalkit.ui.jsf/statistic/filter/noCategory");
+
+  public static final String USER_ID = "userId";
+  public static final String NAME = "name";
+
+  public static final String JSON_QUERY = "jsonQuery";
+  public static final String RESULT = "result";
+}
