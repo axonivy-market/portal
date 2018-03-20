856d4752811f5deb549660a2b3e616bf6a1d9c58
nguyen.to <nguyen.to@axonactive.com>
2018-03-20 18:37:57 +0700

    IVYPORTAL-5123: Implement Portal statistic customization (Drilldown) III - Change time unit of elapsed time chart from seconds to days/hours/minutes/seconds

--- AxonIvyPortal/PortalKit/webContent/resources/js/statistic-widget.js
+++ AxonIvyPortal/PortalKit/webContent/resources/js/statistic-widget.js
@@ -32,5 +32,40 @@ function chartExtender() {
   };
   this.cfg.seriesDefaults.rendererOptions.dataLabelFormatString = '%.4s%%';
   this.cfg.seriesDefaults.rendererOptions.dataLabelThreshold = 0;
-
 }
+
+function elapsedTimeChartExtender() {
+	  this.cfg.grid = {
+	    background : 'rgba(255,255,255, 0)',
+	    drawGridlines: false,
+	    drawBorder : false,
+	    shadow: false
+	  };
+	  this.cfg.seriesDefaults.rendererOptions.dataLabelFormatString = '%.4s%%';
+	  this.cfg.seriesDefaults.rendererOptions.dataLabelThreshold = 0;
+	  this.cfg.highlighter = {
+	    show: true,
+	    useAxesFormatters: false,
+	    tooltipContentEditor: function(ev, seriesIndex, pointIndex, plot) {
+	        var seconds = plot.series[seriesIndex].data[pointIndex][1];
+
+	        var days = Math.floor(seconds / (3600*24));
+	        seconds -= days*3600*24;
+	        var hours = Math.floor(seconds / 3600);
+	        seconds -= hours*3600;
+	        var minutes = Math.floor(seconds / 60);
+	        seconds -= minutes*60;
+	        var result = seconds + " seconds";
+	        if (minutes > 0) {
+	        	result = minutes + " minutes - " + result;
+	        }
+	        if (hours > 0) {
+	        	result = hours + " hours - " + result;
+	        }
+	        if (days > 0) {
+	        	result = days + " days - " + result;
+	        }
+	        return result;
+	    }
+	  }
+	}
