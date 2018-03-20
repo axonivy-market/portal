67f65d71b8dd2f926cdfd2d73e5cb62bad9bf2ce
Nam Le Dinh <nam.ledinh@axonactive.com>
2018-03-20 09:27:11 +0700

    IVYPORTAL-5085: Reporting - Cases - Implement

--- AxonIvyPortal/PortalKit/src/ch/ivy/addon/portalkit/enums/StatisticChartType.java
+++ AxonIvyPortal/PortalKit/src/ch/ivy/addon/portalkit/enums/StatisticChartType.java
@@ -3,6 +3,8 @@ package ch.ivy.addon.portalkit.enums;
 public enum StatisticChartType {
   TASK_BY_PRIORITY("/ch.ivy.addon.portalkit.ui.jsf/statistic/chartType/taskByPriority"),
   CASES_BY_STATE("/ch.ivy.addon.portalkit.ui.jsf/statistic/chartType/caseByState"),
+  CASES_BY_FINISHED_TASK("/ch.ivy.addon.portalkit.ui.jsf/statistic/chartType/caseByFinishedTask"),
+  CASES_BY_FINISHED_TIME("/ch.ivy.addon.portalkit.ui.jsf/statistic/chartType/caseByFinishedTime"),
   TASK_BY_EXPIRY("/ch.ivy.addon.portalkit.ui.jsf/statistic/chartType/taskByExpiry"),
   ELAPSED_TIME_BY_CASE_CATEGORY("/ch.ivy.addon.portalkit.ui.jsf/statistic/chartType/elapsedTimeByCaseCategory");
 
