26a357123ec0c136a99255cb7b6c8bf247b425b4
AAVN\anhnguyenhoang <anh.nguyenhoang@axonactive.com>
2018-03-23 17:03:51 +0700

    IVYPORTAL-5120 Various or urgent requests in S153 - Handle some sonar warnings.
    Former-commit-id: 94d9dd24e8aa99e420ac9207488f747241f6591d

--- AxonIvyPortal/PortalKit/src/ch/ivy/addon/portalkit/comparator/UserProcessIndexComparator.java
+++ AxonIvyPortal/PortalKit/src/ch/ivy/addon/portalkit/comparator/UserProcessIndexComparator.java
@@ -7,6 +7,8 @@ import ch.ivy.addon.portalkit.persistence.domain.UserProcess;
 
 public class UserProcessIndexComparator {
 
+  private UserProcessIndexComparator() {}
+
   public static <U extends Comparable<? super U>> Comparator<? super UserProcess> comparatorNullsLast(
       Function<UserProcess, U> function) {
     return Comparator.comparing(function, Comparator.nullsLast(Comparator.naturalOrder()));
