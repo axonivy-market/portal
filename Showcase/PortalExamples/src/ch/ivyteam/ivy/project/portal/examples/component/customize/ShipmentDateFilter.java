7e8b77cd33c8b197eaf30d609b7690e9f61ccc7a
tntanh_wawa <anh.tran@axonactive.com>
2018-03-23 16:32:40 +0700

    IVYPORTAL-5119: PortalCustomization Best Practice Examples - Customize task filters

---
+++ Showcase/PortalExamples/src/ch/ivyteam/ivy/project/portal/examples/component/customize/ShipmentDateFilter.java
@@ -0,0 +1,101 @@
+package ch.ivyteam.ivy.project.portal.examples.component.customize;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+
+import ch.ivy.addon.portalkit.service.DateTimeGlobalSettingService;
+import ch.ivy.addon.portalkit.taskfilter.TaskFilter;
+import ch.ivyteam.ivy.environment.Ivy;
+import ch.ivyteam.ivy.workflow.query.TaskQuery;
+
+public class ShipmentDateFilter extends TaskFilter {
+
+  private Date fromShipmentDate;
+  private Date toShipmentDate;
+
+  @Override
+  public String label() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("Shipment Date");
+    sb.append(" (");
+    sb.append("from");
+    sb.append("/");
+    sb.append("to");
+    sb.append(")");
+    return sb.toString();
+  }
+
+  @Override
+  public String value() {
+    DateTimeGlobalSettingService service = new DateTimeGlobalSettingService();
+    DateFormat formatter = new SimpleDateFormat(service.getDateTimePattern());
+    if (fromShipmentDate != null && toShipmentDate != null) {
+      return String.format(DASH, formatter.format(fromShipmentDate), formatter.format(toShipmentDate));
+    }
+
+    if (fromShipmentDate != null) {
+      return String.format(GREATER_EQUAL, formatter.format(fromShipmentDate));
+    }
+
+    if (toShipmentDate != null) {
+      return String.format(LESS_EQUAL, formatter.format(toShipmentDate));
+    }
+    return ALL;
+  }
+
+  @Override
+  public TaskQuery buildQuery() {
+    if (fromShipmentDate == null && toShipmentDate == null) {
+      return null;
+    }
+
+    TaskQuery query = TaskQuery.create();
+    if (fromShipmentDate != null) {
+      query.where().customTimestampField1().isGreaterOrEqualThan(fromShipmentDate);
+    }
+
+    if (toShipmentDate != null) {
+      query.where().customTimestampField1().isLowerOrEqualThan(toShipmentDate);
+    }
+
+    return query;
+  }
+
+  @Override
+  public void resetValues() {
+    fromShipmentDate = null;
+    toShipmentDate = null;
+  }
+  
+  @Override
+  public void validate() {
+    if (fromShipmentDate != null && toShipmentDate != null && (fromShipmentDate.compareTo(toShipmentDate) > 0)) {
+      FacesContext.getCurrentInstance().validationFailed();
+      FacesContext.getCurrentInstance().addMessage(
+          null,
+          new FacesMessage(FacesMessage.SEVERITY_ERROR, Ivy.cms().co(
+              "/ch.ivy.addon.portalkit.ui.jsf/common/dateFromBiggerThanTo"), null));
+    }
+  }
+
+  public Date getFromShipmentDate() {
+    return fromShipmentDate;
+  }
+
+  public void setFromShipmentDate(Date fromShipmentDate) {
+    this.fromShipmentDate = fromShipmentDate;
+  }
+
+  public Date getToShipmentDate() {
+    return toShipmentDate;
+  }
+
+  public void setToShipmentDate(Date toShipmentDate) {
+    this.toShipmentDate = toShipmentDate;
+  }
+
+}
