31f2cd5f0127d2d1bd8368778a9df596aa5580b7
tntanh_wawa <anh.tran@axonactive.com>
2018-03-23 16:32:40 +0700

    IVYPORTAL-5119: PortalCustomization Best Practice Examples - Customize task filters

---
+++ Showcase/PortalExamples/src/ch/ivyteam/ivy/project/portal/examples/component/customize/CustomizedTaskFilterContainer.java
@@ -0,0 +1,84 @@
+package ch.ivyteam.ivy.project.portal.examples.component.customize;
+
+import java.util.Collections;
+
+import ch.ivy.addon.portalkit.comparator.TaskFilterComparator;
+import ch.ivy.addon.portalkit.taskfilter.TaskCreationDateFilter;
+import ch.ivy.addon.portalkit.taskfilter.TaskDescriptionFilter;
+import ch.ivy.addon.portalkit.taskfilter.TaskExpiredDateFilter;
+import ch.ivy.addon.portalkit.taskfilter.TaskFilterContainer;
+import ch.ivy.addon.portalkit.taskfilter.TaskResponsibleFilter;
+
+/**
+ * @author tntanh
+ *
+ */
+public class CustomizedTaskFilterContainer extends TaskFilterContainer {
+
+  private TaskDescriptionFilter descriptionFilter = new TaskDescriptionFilter();
+  private TaskCreationDateFilter creationDateFilter = new TaskCreationDateFilter();
+  private TaskExpiredDateFilter expiredDateFilter = new TaskExpiredDateFilter();
+  private TaskResponsibleFilter responsibleFilter = new TaskResponsibleFilter();
+  private CustomerNameFilter customerNameFilter = new CustomerNameFilter();
+  private ShipmentDateFilter shipmentDateFilter = new ShipmentDateFilter();
+
+  public CustomizedTaskFilterContainer() {
+    filters.remove(stateFilter);
+    filters.add(descriptionFilter);
+    filters.add(creationDateFilter);
+    filters.add(expiredDateFilter);
+    filters.add(responsibleFilter);
+    filters.add(customerNameFilter);
+    filters.add(shipmentDateFilter);
+    Collections.sort(filters, new TaskFilterComparator());
+  }
+  
+  public TaskDescriptionFilter getDescriptionFilter() {
+    return descriptionFilter;
+  }
+
+  public void setDescriptionFilter(TaskDescriptionFilter descriptionFilter) {
+    this.descriptionFilter = descriptionFilter;
+  }
+
+  public TaskCreationDateFilter getCreationDateFilter() {
+    return creationDateFilter;
+  }
+
+  public void setCreationDateFilter(TaskCreationDateFilter creationDateFilter) {
+    this.creationDateFilter = creationDateFilter;
+  }
+
+  public TaskExpiredDateFilter getExpiredDateFilter() {
+    return expiredDateFilter;
+  }
+
+  public void setExpiredDateFilter(TaskExpiredDateFilter expiredDateFilter) {
+    this.expiredDateFilter = expiredDateFilter;
+  }
+
+  public TaskResponsibleFilter getResponsibleFilter() {
+    return responsibleFilter;
+  }
+
+  public void setResponsibleFilter(TaskResponsibleFilter responsibleFilter) {
+    this.responsibleFilter = responsibleFilter;
+  }
+
+  public CustomerNameFilter getCustomerNameFilter() {
+    return customerNameFilter;
+  }
+
+  public void setCustomerNameFilter(CustomerNameFilter customerNameFilter) {
+    this.customerNameFilter = customerNameFilter;
+  }
+
+  public ShipmentDateFilter getShipmentDateFilter() {
+    return shipmentDateFilter;
+  }
+
+  public void setShipmentDateFilter(ShipmentDateFilter shipmentDateFilter) {
+    this.shipmentDateFilter = shipmentDateFilter;
+  }
+
+}
