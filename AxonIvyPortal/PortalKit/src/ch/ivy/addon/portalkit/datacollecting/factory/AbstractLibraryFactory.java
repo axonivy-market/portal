4070cdf76975485c5bc01439154d565f1ad3bcc9
Long Do <long.do@axonactive.com>
2018-03-28 15:04:00 +0700

    IVYPORTAL-4815 Add release information to portal: + Refactored

--- AxonIvyPortal/PortalKit/src/ch/ivy/addon/portalkit/datacollecting/factory/AbstractLibraryFactory.java
+++
@@ -1,9 +0,0 @@
-package ch.ivy.addon.portalkit.datacollecting.factory;
-
-import ch.ivy.addon.portalkit.datacollecting.service.AbstractLibraryService;
-
-
-public abstract class AbstractLibraryFactory {
-
-  public abstract AbstractLibraryService createService();
-}
