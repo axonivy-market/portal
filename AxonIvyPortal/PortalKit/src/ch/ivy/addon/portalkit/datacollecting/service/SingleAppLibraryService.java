2a47807bd3bc14a439da87bb8228d73787cbcb6b
Long Do <long.do@axonactive.com>
2018-03-28 17:49:36 +0700

    IVYPORTAL-4815 Add release information to portal: + Fixed NPE in SingleAppLibraryService.java

--- AxonIvyPortal/PortalKit/src/ch/ivy/addon/portalkit/datacollecting/service/SingleAppLibraryService.java
+++ AxonIvyPortal/PortalKit/src/ch/ivy/addon/portalkit/datacollecting/service/SingleAppLibraryService.java
@@ -1,5 +1,6 @@
 package ch.ivy.addon.portalkit.datacollecting.service;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -12,9 +13,11 @@ public class SingleAppLibraryService extends AbstractLibraryService {
 
   @Override
   public Map<String, List<RemoteLibrary>> collectLibraries() {
-    return Ivy.request().getApplication().getLibraries().stream()
-        .filter(lib -> !portalLibraryStrings.contains(lib.getId())).map(RemoteLibraryMapper::mapLibrary)
-        .collect(Collectors.groupingBy(RemoteLibrary::getApplication));
+    List<RemoteLibrary> libraries = Ivy.request().getApplication().getLibraries().stream()
+        .filter(lib -> !portalLibraryStrings.contains(lib.getId())).map(RemoteLibraryMapper::mapLibrary).collect(Collectors.toList());
+    Map<String, List<RemoteLibrary>> librariesMap = new HashMap<>();
+    librariesMap.put(Ivy.cms().co("/ch.ivy.addon.portalkit.ui.jsf/projectInfo/applicationProjectVersion"), libraries);
+    return librariesMap;
   }
 
 }
