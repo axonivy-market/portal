980051528fec684632e2328780cadbb06afa1140
AAVN\anhnguyenhoang <anh.nguyenhoang@axonactive.com>
2018-03-29 08:53:07 +0700

    IVYPORTAL-2514 Detect PortalConnector compatibility - Implemented

--- AxonIvyPortal/PortalKit/src/ch/ivy/addon/portalkit/enums/PortalLibrary.java
+++ AxonIvyPortal/PortalKit/src/ch/ivy/addon/portalkit/enums/PortalLibrary.java
@@ -3,7 +3,8 @@ package ch.ivy.addon.portalkit.enums;
 public enum PortalLibrary {
   PORTAL_KIT("ch.ivyteam.ivy.project.portal:portalKit"), 
   PORTAL_TEMPLATE("ch.ivyteam.ivy.project.portal:portalTemplate"),
-  AXON_EXPRESS("ch.ivyteam.ivy.project.portal:axonIvyExpress");
+  AXON_EXPRESS("ch.ivyteam.ivy.project.portal:axonIvyExpress"),
+  PORTAL_CONNECTOR("ch.ivyteam.ivy.project.system:portalConnector");
 
   private String value;
 
