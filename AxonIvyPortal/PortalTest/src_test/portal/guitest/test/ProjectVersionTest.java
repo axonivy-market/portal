a7514c61a599532aa5ced7c8c7578a8149aeb83f
Long Do <long.do@axonactive.com>
2018-04-10 11:44:43 +0700

    IVYPORTAL-5293 Add release information to portal II: + Introduced auto test + Added vertical scrollbar if content is too high

---
+++ AxonIvyPortal/PortalTest/src_test/portal/guitest/test/ProjectVersionTest.java
@@ -0,0 +1,25 @@
+package portal.guitest.test;
+
+import org.junit.Test;
+
+import portal.guitest.common.BaseTest;
+import portal.guitest.common.TestAccount;
+import portal.guitest.page.HomePage;
+import portal.guitest.page.LoginPage;
+import portal.guitest.page.ProjectVersionPage;
+
+public class ProjectVersionTest extends BaseTest {
+
+  @Test
+  public void shouldShowProjectVersion() {
+    navigateToUrl(HomePage.PORTAL_HOME_PAGE_URL);
+    LoginPage loginPage = new LoginPage(TestAccount.DEMO_USER);
+    loginPage.login();
+    HomePage homePage = new HomePage();
+    ProjectVersionPage projectVersionPage = homePage.openProjectVersionPage();
+    assertTrue("Engine version not displayed", projectVersionPage.isEngineVersionDisplayed());
+    assertTrue("Portal version not displayed", projectVersionPage.isPortalVersionDisplayed());
+    assertTrue("First project version not displayed", projectVersionPage.isFirstProjectDisplayed());
+  }
+
+}
