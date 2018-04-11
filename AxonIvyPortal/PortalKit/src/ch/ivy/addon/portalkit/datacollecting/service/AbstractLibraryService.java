2cea6452b0aab1320bc0309457ce36e79bfb95d8
Long Do <long.do@axonactive.com>
2018-04-11 10:59:41 +0700

    IVYPORTAL-5293 Add release information to portal II: + Moved constants in AbstractLibraryService.java to enums/PortalLibrary.java + Check condition if ivyApplication is null + ViewBean implements Serializable

--- AxonIvyPortal/PortalKit/src/ch/ivy/addon/portalkit/datacollecting/service/AbstractLibraryService.java
+++ AxonIvyPortal/PortalKit/src/ch/ivy/addon/portalkit/datacollecting/service/AbstractLibraryService.java
@@ -1,23 +1,17 @@
 package ch.ivy.addon.portalkit.datacollecting.service;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import ch.ivy.addon.portalkit.bo.RemoteLibrary;
+import ch.ivy.addon.portalkit.enums.PortalLibrary;
 
 public abstract class AbstractLibraryService {
 
-  protected final static String PORTAL_STYLE_LIBRARY = "ch.ivyteam.ivy.project.portal:portalStyle";
-  protected final static String PORTAL_KIT_LIBRARY = "ch.ivyteam.ivy.project.portal:portalKit";
-  protected final static String PORTAL_TEMPLATE_LIBRARY = "ch.ivyteam.ivy.project.portal:portalTemplate";
-  protected final static String PORTAL_CONNECTOR_LIBRARY = "ch.ivyteam.ivy.project.system:portalConnector";
-  protected final static String SELF_SERVICE_LIBRARY = "ch.ivyteam.ivy.project.portal:selfService";
-  protected final static String EXPRESS_LIBRARY = "ch.ivyteam.ivy.project.portal:axonIvyExpress";
-  
-  protected final List<String> portalLibraryStrings =
-      Arrays.asList(PORTAL_STYLE_LIBRARY, PORTAL_KIT_LIBRARY, PORTAL_TEMPLATE_LIBRARY, PORTAL_CONNECTOR_LIBRARY,
-          SELF_SERVICE_LIBRARY, EXPRESS_LIBRARY);
+  protected final List<String> portalLibraryStrings = Stream.of(PortalLibrary.values()).map(PortalLibrary::getValue)
+      .collect(Collectors.toList());
   
   public abstract Map<String, List<RemoteLibrary>> collectLibraries();
 }
