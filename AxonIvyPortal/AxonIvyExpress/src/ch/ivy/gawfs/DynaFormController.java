e653a8e76d9cebf950c2a4e8f9aeddbae48cf19a
nttai_wawa <tai.nguyen@axonactive.com>
2018-04-12 13:43:59 +0700

    IVYPORTAL-3856: Improve Express layout - Field positioning, Revert code for main process

--- AxonIvyPortal/AxonIvyExpress/src/ch/ivy/gawfs/DynaFormController.java
+++ AxonIvyPortal/AxonIvyExpress/src/ch/ivy/gawfs/DynaFormController.java
@@ -23,10 +23,10 @@ public class DynaFormController implements Serializable {
 
   private DynaFormModel model;
   private DynaFormRow tempRow;
-  private CommonDragAndDropController commonDragAndDropController;
+  private DragAndDropController dragAndDropController;
 
   public DynaFormController(DragAndDropController dragAndDropController) {
-    this.commonDragAndDropController = dragAndDropController;
+    this.dragAndDropController = dragAndDropController;
     model = new DynaFormModel();
 
   }
@@ -34,13 +34,95 @@ public class DynaFormController implements Serializable {
   /**
    * Create form model and elements
    */
-  public void createForm() {
-    if (commonDragAndDropController instanceof FourPanelDragAndDropController) {
-      createForm((FourPanelDragAndDropController) commonDragAndDropController);
-    } else if (commonDragAndDropController instanceof SixPanelDragAndDropController) {
-      createForm((SixPanelDragAndDropController) commonDragAndDropController);
-    } else {
-      createForm((DragAndDropController) commonDragAndDropController);
+  public void createForm(){
+      model = new DynaFormModel();
+      dragAndDropController.getFinalUsedFormelements().clear();
+
+      initializeHeaderSection();
+      initializeMainSections();
+      initializeFooterSection();
+  }
+
+  /**
+   * Loop to create all Elements places in the Header Section
+   * 
+   */
+  private void initializeHeaderSection() {
+    for (Formelement element : dragAndDropController.getSelectedFormelementsHeader()) {
+      dragAndDropController.addFinalUsedFormelements(element);
+      tempRow = model.createRegularRow();
+      String elementType = element.getType().getValue();
+
+      if(!element.getType().equals(FormElementType.FILE_UPLOAD)){
+        DynaFormLabel label = tempRow.addLabel(element.getLabel());
+        DynaFormControl control = tempRow.addControl(element, elementType, 3, 1);
+        label.setForControl(control);
+        
+      }else{
+        DynaFormLabel label = tempRow.addLabel(element.getLabel(),4,1);
+        tempRow = model.createRegularRow();
+        DynaFormControl control = tempRow.addControl(element, elementType, 4, 1);
+        label.setForControl(control);
+      }
+    }
+  }
+
+  /**
+   * Loop to create all Elements from the Left and Right Section
+   */
+  private void initializeMainSections() {
+    for (int i = 0; i < getMax(dragAndDropController.getSelectedFormelementsLeftPanel().size(), dragAndDropController.getSelectedFormelementsRightPanel().size()); i++) {
+      tempRow = model.createRegularRow();
+
+      //Elements from the Left Section
+      if (dragAndDropController.getSelectedFormelementsLeftPanel().size()> i) {
+        Formelement element = dragAndDropController.getSelectedFormelementsLeftPanel().get(i);
+        dragAndDropController.addFinalUsedFormelements(element);
+        
+        DynaFormLabel label = tempRow.addLabel(element.getLabel());
+        DynaFormControl control = tempRow.addControl(element, element.getType().getValue());
+        label.setForControl(control);        
+      } else {
+        tempRow.addLabel(StringUtils.EMPTY);
+        tempRow.addControl(null, CONTROL_SPACER);
+      }
+
+      //Elements from the Right Section
+      if (dragAndDropController.getSelectedFormelementsRightPanel().size()> i) {  
+        Formelement element = dragAndDropController.getSelectedFormelementsRightPanel().get(i);
+        dragAndDropController.addFinalUsedFormelements(element);
+        
+        DynaFormLabel label = tempRow.addLabel(element.getLabel());
+        DynaFormControl control = tempRow.addControl(element, element.getType().getValue());
+        label.setForControl(control);
+        
+      } else {
+        tempRow.addLabel(StringUtils.EMPTY);
+        tempRow.addControl(null, CONTROL_SPACER);
+        
+      }
+    }
+  }
+
+  /**
+   * Loop to create all Elements places in the Footer Section
+   */
+  private void initializeFooterSection() {
+    for (Formelement element : dragAndDropController.getSelectedFormelementsFooter()) {
+      dragAndDropController.addFinalUsedFormelements(element);
+      tempRow = model.createRegularRow();
+      String elementType = element.getType().getValue();
+
+      if(!element.getType().equals(FormElementType.FILE_UPLOAD)){
+        DynaFormLabel label = tempRow.addLabel(element.getLabel());
+        DynaFormControl control = tempRow.addControl(element, elementType,3,1);
+        label.setForControl(control);
+      }else{
+        DynaFormLabel label = tempRow.addLabel(element.getLabel(),4,1);
+        tempRow = model.createRegularRow();
+        DynaFormControl control = tempRow.addControl(element, elementType,4,1);
+        label.setForControl(control);
+      }
     }
   }
 
@@ -63,8 +145,10 @@ public class DynaFormController implements Serializable {
   }
 
   public String submitForm() {
-    FacesMessage.Severity sev = FacesContext.getCurrentInstance().getMaximumSeverity();
-    boolean hasErrors = (sev != null && (FacesMessage.SEVERITY_ERROR.compareTo(sev) >= 0));
+    FacesMessage.Severity sev = FacesContext.getCurrentInstance()
+        .getMaximumSeverity();
+    boolean hasErrors = (sev != null && (FacesMessage.SEVERITY_ERROR
+        .compareTo(sev) >= 0));
 
     RequestContext requestContext = RequestContext.getCurrentInstance();
     requestContext.addCallbackParam("isValid", !hasErrors);
@@ -76,210 +160,12 @@ public class DynaFormController implements Serializable {
     return model;
   }
 
-  private void createForm(FourPanelDragAndDropController dragAndDropController) {
-    model = new DynaFormModel();
-    dragAndDropController.getFinalUsedFormelements().clear();
-  }
-
-  private void createForm(SixPanelDragAndDropController dragAndDropController) {
-    model = new DynaFormModel();
-    dragAndDropController.getFinalUsedFormelements().clear();
-    initializeHeaderSection(dragAndDropController);
-    initializeLeftAndRightSection(dragAndDropController);
-    initializeFooterSection(dragAndDropController);
-  }
-
-
-  private void createForm(DragAndDropController dragAndDropController) {
-    model = new DynaFormModel();
-    dragAndDropController.getFinalUsedFormelements().clear();
-
-    initializeHeaderSection(dragAndDropController);
-    initializeMainSections(dragAndDropController);
-    initializeFooterSection(dragAndDropController);
-  }
-
-  /**
-   * Loop to create all Elements places in the Header Section
-   * 
-   */
-  private void initializeHeaderSection(DragAndDropController dragAndDropController) {
-    for (Formelement element : dragAndDropController.getSelectedFormelementsHeader()) {
-      dragAndDropController.addFinalUsedFormelements(element);
-      tempRow = model.createRegularRow();
-      String elementType = element.getType().getValue();
-      if (!element.getType().equals(FormElementType.FILE_UPLOAD)) {
-        DynaFormLabel label = tempRow.addLabel(element.getLabel());
-        DynaFormControl control = tempRow.addControl(element, elementType, 3, 1);
-        label.setForControl(control);
-      } else {
-        DynaFormLabel label = tempRow.addLabel(element.getLabel(), 4, 1);
-        tempRow = model.createRegularRow();
-        DynaFormControl control = tempRow.addControl(element, elementType, 4, 1);
-        label.setForControl(control);
-      }
+  private int getMax(int a, int b ){
+    if (a > b) {
+      return a;
+    }else{
+      return b;
     }
-  }
-
-  /**
-   * Loop to create all Elements from the Left and Right Section
-   */
-  private void initializeMainSections(DragAndDropController dragAndDropController) {
-    for (int i = 0; i < getMax(dragAndDropController.getSelectedFormelementsLeftPanel().size(), dragAndDropController.getSelectedFormelementsRightPanel().size()); i++) {
-      tempRow = model.createRegularRow();
-
-      // Elements from the Left Section
-      if (dragAndDropController.getSelectedFormelementsLeftPanel().size() > i) {
-        Formelement element = dragAndDropController.getSelectedFormelementsLeftPanel().get(i);
-        dragAndDropController.addFinalUsedFormelements(element);
-
-        DynaFormLabel label = tempRow.addLabel(element.getLabel());
-        DynaFormControl control = tempRow.addControl(element, element.getType().getValue());
-        label.setForControl(control);
-      } else {
-        tempRow.addLabel(StringUtils.EMPTY);
-        tempRow.addControl(null, CONTROL_SPACER);
-      }
-
-      // Elements from the Right Section
-      if (dragAndDropController.getSelectedFormelementsRightPanel().size() > i) {
-        Formelement element = dragAndDropController.getSelectedFormelementsRightPanel().get(i);
-        dragAndDropController.addFinalUsedFormelements(element);
-
-        DynaFormLabel label = tempRow.addLabel(element.getLabel());
-        DynaFormControl control = tempRow.addControl(element, element.getType().getValue());
-        label.setForControl(control);
-
-      } else {
-        tempRow.addLabel(StringUtils.EMPTY);
-        tempRow.addControl(null, CONTROL_SPACER);
-
-      }
-    }
-  }
-
-  /**
-   * Loop to create all Elements places in the Footer Section
-   */
-  private void initializeFooterSection(DragAndDropController dragAndDropController) {
-    for (Formelement element : dragAndDropController.getSelectedFormelementsFooter()) {
-      dragAndDropController.addFinalUsedFormelements(element);
-      tempRow = model.createRegularRow();
-      String elementType = element.getType().getValue();
-
-      if (!element.getType().equals(FormElementType.FILE_UPLOAD)) {
-        DynaFormLabel label = tempRow.addLabel(element.getLabel());
-        DynaFormControl control = tempRow.addControl(element, elementType, 3, 1);
-        label.setForControl(control);
-      } else {
-        DynaFormLabel label = tempRow.addLabel(element.getLabel(), 4, 1);
-        tempRow = model.createRegularRow();
-        DynaFormControl control = tempRow.addControl(element, elementType, 4, 1);
-        label.setForControl(control);
-      }
-    }
-  }
-
-  private void initializeFooterSection(SixPanelDragAndDropController dragAndDropController) {
-    // Loop to create all Elements places in the Footer Section
-    for (Formelement element : dragAndDropController.getSelectedFormelementsFooter()) {
-      dragAndDropController.addFinalUsedFormelements(element);
-      tempRow = model.createRegularRow();
-      if (!FormElementType.FILE_UPLOAD.equals(element.getType())) {
-        DynaFormLabel label = tempRow.addLabel(element.getLabel());
-        DynaFormControl control = tempRow.addControl(element, element.getType().getValue(), 5, 1);
-        label.setForControl(control);
-      } else {
-        DynaFormLabel label = tempRow.addLabel(element.getLabel(), 6, 1);
-        tempRow = model.createRegularRow();
-        DynaFormControl control = tempRow.addControl(element, element.getType().getValue(), 6, 1);
-        label.setForControl(control);
-      }
-    }
-  }
-
-  private void initializeLeftAndRightSection(SixPanelDragAndDropController dragAndDropController) {
-    // Loop to create all Elements from the Left and Right Section
-    for (int i = 0; i < getMax(dragAndDropController.getSelectedFormelementsLeftPanel().size(), dragAndDropController.getSelectedFormelementsRightPanel().size()); i++) {
-      tempRow = model.createRegularRow();
-      // Elements from the Left Section
-      if (dragAndDropController.getSelectedFormelementsLeftPanel().size() > i) {
-        Formelement element = dragAndDropController.getSelectedFormelementsLeftPanel().get(i);
-        dragAndDropController.addFinalUsedFormelements(element);
-        DynaFormLabel label = tempRow.addLabel(element.getLabel());
-        DynaFormControl control = tempRow.addControl(element, element.getType().getValue(), 3, 1);
-        label.setForControl(control);
-      } else {
-        tempRow.addLabel(StringUtils.EMPTY);
-        tempRow.addControl(null, CONTROL_SPACER, 3, 1);
-      }
-
-      // Elements from the Right Section
-      if (dragAndDropController.getSelectedFormelementsRightPanel().size() > i) {
-        Formelement element = dragAndDropController.getSelectedFormelementsRightPanel().get(i);
-        dragAndDropController.addFinalUsedFormelements(element);
-        DynaFormLabel label = tempRow.addLabel(element.getLabel());
-        DynaFormControl control = tempRow.addControl(element, element.getType().getValue());
-        label.setForControl(control);
-      } else {
-        tempRow.addLabel(StringUtils.EMPTY);
-        tempRow.addControl(null, CONTROL_SPACER);
-      }
-    }
-  }
-
-  private void initializeHeaderSection(SixPanelDragAndDropController dragAndDropController) {
-    // Loop to create all Elements places in the Header Section
-    for (int i = 0; i < getMax(dragAndDropController.getSelectedFormelementsHeader1().size(), dragAndDropController.getSelectedFormelementsHeader2().size(), dragAndDropController
-        .getSelectedFormelementsHeader3().size()); i++) {
-      tempRow = model.createRegularRow();
-      // Elements from the Header Section 1
-      if (dragAndDropController.getSelectedFormelementsHeader1().size() > i) {
-        Formelement element = dragAndDropController.getSelectedFormelementsHeader1().get(i);
-        dragAndDropController.addFinalUsedFormelements(element);
-
-        DynaFormLabel label = tempRow.addLabel(element.getLabel());
-        DynaFormControl control = tempRow.addControl(element, element.getType().getValue());
-        label.setForControl(control);
-      } else {
-        tempRow.addLabel(StringUtils.EMPTY);
-        tempRow.addControl(null, CONTROL_SPACER);
-      }
-      // Elements from the Header Section 2
-      if (dragAndDropController.getSelectedFormelementsHeader2().size() > i) {
-        Formelement element = dragAndDropController.getSelectedFormelementsHeader2().get(i);
-        dragAndDropController.addFinalUsedFormelements(element);
-
-        DynaFormLabel label = tempRow.addLabel(element.getLabel());
-        DynaFormControl control = tempRow.addControl(element, element.getType().getValue());
-        label.setForControl(control);
-      } else {
-        tempRow.addLabel(StringUtils.EMPTY);
-        tempRow.addControl(null, CONTROL_SPACER);
-      }
-      // Elements from the Header Section 3
-      if (dragAndDropController.getSelectedFormelementsHeader3().size() > i) {
-        Formelement element = dragAndDropController.getSelectedFormelementsHeader3().get(i);
-        dragAndDropController.addFinalUsedFormelements(element);
-
-        DynaFormLabel label = tempRow.addLabel(element.getLabel());
-        DynaFormControl control = tempRow.addControl(element, element.getType().getValue());
-        label.setForControl(control);
-      } else {
-        tempRow.addLabel(StringUtils.EMPTY);
-        tempRow.addControl(null, CONTROL_SPACER);
-      }
-    }
-  }
-
-  private int getMax(int... a) {
-    int max = a[0];
-    for (int i = 1; i < a.length; i++) {
-      if (a[i] > max) {
-        max = a[i];
-      }
-    }
-    return max;
   }
 
 }
