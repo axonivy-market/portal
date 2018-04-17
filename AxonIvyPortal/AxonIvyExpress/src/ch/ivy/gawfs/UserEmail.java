7be723baeb14f3aef0d427755878aee18c88b91c
Long Do <long.do@axonactive.com>
2018-04-17 09:17:03 +0700

    IVYPORTAL-5208 Axon.Ivy Express - Additional Task Types: + Introduced service to send email

--- AxonIvyPortal/AxonIvyExpress/src/ch/ivy/gawfs/UserEmail.java
+++ AxonIvyPortal/AxonIvyExpress/src/ch/ivy/gawfs/UserEmail.java
@@ -10,37 +10,44 @@ public class UserEmail {
   private String subject;
   private String content;
   private List<AttachmentDTO> attachments;
-  
+
   public String getRecipients() {
     return recipients;
   }
+
   public void setRecipients(String recipients) {
     this.recipients = recipients;
   }
+
   public String getResponseTo() {
     return responseTo;
   }
+
   public void setResponseTo(String responseTo) {
     this.responseTo = responseTo;
   }
+
   public String getSubject() {
     return subject;
   }
+
   public void setSubject(String subject) {
     this.subject = subject;
   }
+
   public String getContent() {
     return content;
   }
+
   public void setContent(String content) {
     this.content = content;
   }
+
   public List<AttachmentDTO> getAttachments() {
     return attachments;
   }
+
   public void setAttachments(List<AttachmentDTO> attachments) {
     this.attachments = attachments;
   }
-  
-  
 }
