7bd86155747a350cb6c97d6285aa561770a35bb9
nam.mai <nam.mai@axonactive.com>
2018-04-18 10:37:28 +0700

    IVYPORTAL-5207: Axon.Ivy Express - Enhance Create Process - Reverted code

--- AxonIvyPortal/AxonIvyExpress/src/ch/ivy/gawfs/DynaFormController.java
+++ AxonIvyPortal/AxonIvyExpress/src/ch/ivy/gawfs/DynaFormController.java
@@ -12,160 +12,128 @@ import org.primefaces.extensions.model.dynaform.DynaFormLabel;
 import org.primefaces.extensions.model.dynaform.DynaFormModel;
 import org.primefaces.extensions.model.dynaform.DynaFormRow;
 
-import ch.ivy.gawfs.enums.FormElementType;
-import ch.ivy.gawfs.enums.UploadFileFormat;
-
 public class DynaFormController implements Serializable {
 
-  private static final String CONTROL_SPACER = "Spacer";
+	private static final String CONTROL_SPACER = "Spacer";
 
-  private static final long serialVersionUID = -1486400417160014320L;
+	private static final String FORM_ELEMENT_FILE_UPLOAD = "FileUpload";
 
-  private DynaFormModel model;
-  private DynaFormRow tempRow;
-  private DragAndDropController dragAndDropController;
+	private static final long serialVersionUID = -1486400417160014320L;
 
-  public DynaFormController(DragAndDropController dragAndDropController) {
-    this.dragAndDropController = dragAndDropController;
-    model = new DynaFormModel();
+	private DynaFormModel model;
+	private DynaFormRow tempRow;
+	private DragAndDropController dragAndDropController;
 
-  }
+	public DynaFormController(DragAndDropController dragAndDropController) {
+		this.dragAndDropController = dragAndDropController;
+		model = new DynaFormModel();
 
-  /**
-   * Create form model and elements
-   */
-  public void createForm(){
-      model = new DynaFormModel();
-      dragAndDropController.getFinalUsedFormelements().clear();
+	}
 
-      initializeHeaderSection();
-      initializeMainSections();
-      initializeFooterSection();
-  }
+	public void createForm(){
+			model = new DynaFormModel();
+			dragAndDropController.getFinalUsedFormelements().clear();
+			
+		//Loop to create all Elements places in the Header Section	
+		for (Formelement element : dragAndDropController.getSelectedFormelementsHeader()) {				
+			dragAndDropController.addFinalUsedFormelements(element);
+			
+			tempRow = model.createRegularRow();
+			
+			if(!element.getType().equals(FORM_ELEMENT_FILE_UPLOAD)){
+				
+				DynaFormLabel label = tempRow.addLabel(element.getLabel());
+				DynaFormControl control = tempRow.addControl(element, element.getType(),3,1);
+				label.setForControl(control);
+				
+			}else{
+				DynaFormLabel label = tempRow.addLabel(element.getLabel(),4,1);
+				tempRow = model.createRegularRow();
+				DynaFormControl control = tempRow.addControl(element, element.getType(),4,1);
+				label.setForControl(control);
+			}
+		}
+		
+		
+		//Loop to create all Elements from the Left and Right Section	
+		for (int i = 0; i < getMax(dragAndDropController.getSelectedFormelementsLeftPanel().size(), dragAndDropController.getSelectedFormelementsRightPanel().size()); i++) {
+			
+			tempRow = model.createRegularRow();
+			
+			//Elements from the Left Section	
+			if (dragAndDropController.getSelectedFormelementsLeftPanel().size()> i) {
+				Formelement element = dragAndDropController.getSelectedFormelementsLeftPanel().get(i);
+				dragAndDropController.addFinalUsedFormelements(element);
+				
+				DynaFormLabel label = tempRow.addLabel(element.getLabel());
+				DynaFormControl control = tempRow.addControl(element, element.getType());
+				label.setForControl(control);				
+			} else {
+				tempRow.addLabel(StringUtils.EMPTY);
+				tempRow.addControl(null, CONTROL_SPACER);
+			}
+			
+			//Elements from the Right Section	
+			if (dragAndDropController.getSelectedFormelementsRightPanel().size()> i) {	
+				
+				Formelement element = dragAndDropController.getSelectedFormelementsRightPanel().get(i);
+				dragAndDropController.addFinalUsedFormelements(element);
+				
+				DynaFormLabel label = tempRow.addLabel(element.getLabel());
+				DynaFormControl control = tempRow.addControl(element, element.getType());
+				label.setForControl(control);
+				
+			} else {
+				tempRow.addLabel(StringUtils.EMPTY);
+				tempRow.addControl(null, CONTROL_SPACER);
+				
+			}	
+	
+		}
+		
+		//Loop to create all Elements places in the Footer Section	
+		for (Formelement element : dragAndDropController.getSelectedFormelementsFooter()) {
+			
+			dragAndDropController.addFinalUsedFormelements(element);
+			tempRow = model.createRegularRow();
+			
+			if(!element.getType().equals(FORM_ELEMENT_FILE_UPLOAD)){
+				DynaFormLabel label = tempRow.addLabel(element.getLabel());
+				DynaFormControl control = tempRow.addControl(element, element.getType(),3,1);
+				label.setForControl(control);
+			}else{
+				DynaFormLabel label = tempRow.addLabel(element.getLabel(),4,1);
+				tempRow = model.createRegularRow();
+				DynaFormControl control = tempRow.addControl(element, element.getType(),4,1);
+				label.setForControl(control);
+			}
+		}
+	}
 
-  /**
-   * Loop to create all Elements places in the Header Section
-   * 
-   */
-  private void initializeHeaderSection() {
-    for (Formelement element : dragAndDropController.getSelectedFormelementsHeader()) {
-      dragAndDropController.addFinalUsedFormelements(element);
-      tempRow = model.createRegularRow();
-      String elementType = element.getType().getValue();
 
-      if(!element.getType().equals(FormElementType.FILE_UPLOAD)){
-        DynaFormLabel label = tempRow.addLabel(element.getLabel());
-        DynaFormControl control = tempRow.addControl(element, elementType, 3, 1);
-        label.setForControl(control);
-        
-      }else{
-        DynaFormLabel label = tempRow.addLabel(element.getLabel(),4,1);
-        tempRow = model.createRegularRow();
-        DynaFormControl control = tempRow.addControl(element, elementType, 4, 1);
-        label.setForControl(control);
-      }
+	public String submitForm() {
+		FacesMessage.Severity sev = FacesContext.getCurrentInstance()
+				.getMaximumSeverity();
+		boolean hasErrors = (sev != null && (FacesMessage.SEVERITY_ERROR
+				.compareTo(sev) >= 0));
+
+		RequestContext requestContext = RequestContext.getCurrentInstance();
+		requestContext.addCallbackParam("isValid", !hasErrors);
+
+		return null;
+	}
+
+	public DynaFormModel getModel() {
+		return model;
+	}
+	
+	
+    private int getMax(int a, int b ){
+    	if (a > b) {
+    		return a;
+    	}else{
+    		return b;
+    	}
     }
-  }
-
-  /**
-   * Loop to create all Elements from the Left and Right Section
-   */
-  private void initializeMainSections() {
-    for (int i = 0; i < getMax(dragAndDropController.getSelectedFormelementsLeftPanel().size(), dragAndDropController.getSelectedFormelementsRightPanel().size()); i++) {
-      tempRow = model.createRegularRow();
-
-      //Elements from the Left Section
-      if (dragAndDropController.getSelectedFormelementsLeftPanel().size()> i) {
-        Formelement element = dragAndDropController.getSelectedFormelementsLeftPanel().get(i);
-        dragAndDropController.addFinalUsedFormelements(element);
-        
-        DynaFormLabel label = tempRow.addLabel(element.getLabel());
-        DynaFormControl control = tempRow.addControl(element, element.getType().getValue());
-        label.setForControl(control);        
-      } else {
-        tempRow.addLabel(StringUtils.EMPTY);
-        tempRow.addControl(null, CONTROL_SPACER);
-      }
-
-      //Elements from the Right Section
-      if (dragAndDropController.getSelectedFormelementsRightPanel().size()> i) {  
-        Formelement element = dragAndDropController.getSelectedFormelementsRightPanel().get(i);
-        dragAndDropController.addFinalUsedFormelements(element);
-        
-        DynaFormLabel label = tempRow.addLabel(element.getLabel());
-        DynaFormControl control = tempRow.addControl(element, element.getType().getValue());
-        label.setForControl(control);
-        
-      } else {
-        tempRow.addLabel(StringUtils.EMPTY);
-        tempRow.addControl(null, CONTROL_SPACER);
-        
-      }
-    }
-  }
-
-  /**
-   * Loop to create all Elements places in the Footer Section
-   */
-  private void initializeFooterSection() {
-    for (Formelement element : dragAndDropController.getSelectedFormelementsFooter()) {
-      dragAndDropController.addFinalUsedFormelements(element);
-      tempRow = model.createRegularRow();
-      String elementType = element.getType().getValue();
-
-      if(!element.getType().equals(FormElementType.FILE_UPLOAD)){
-        DynaFormLabel label = tempRow.addLabel(element.getLabel());
-        DynaFormControl control = tempRow.addControl(element, elementType,3,1);
-        label.setForControl(control);
-      }else{
-        DynaFormLabel label = tempRow.addLabel(element.getLabel(),4,1);
-        tempRow = model.createRegularRow();
-        DynaFormControl control = tempRow.addControl(element, elementType,4,1);
-        label.setForControl(control);
-      }
-    }
-  }
-
-  /**
-   * Initialize "File Upload" element
-   * 
-   * @return initialized "File Upload" element
-   */
-  public static Formelement initializeFileUploadElement() {
-    Formelement fileUpload = new Formelement();
-    fileUpload.setType(FormElementType.FILE_UPLOAD);
-
-    // Initialize value for number of files allowed
-    fileUpload.setIntSetting(0);
-
-    for (UploadFileFormat format : UploadFileFormat.values()) {
-      fileUpload.addOption(new FormelementOption(format.getLabel()));
-    }
-    return fileUpload;
-  }
-
-  public String submitForm() {
-    FacesMessage.Severity sev = FacesContext.getCurrentInstance()
-        .getMaximumSeverity();
-    boolean hasErrors = (sev != null && (FacesMessage.SEVERITY_ERROR
-        .compareTo(sev) >= 0));
-
-    RequestContext requestContext = RequestContext.getCurrentInstance();
-    requestContext.addCallbackParam("isValid", !hasErrors);
-
-    return null;
-  }
-
-  public DynaFormModel getModel() {
-    return model;
-  }
-
-  private int getMax(int a, int b ){
-    if (a > b) {
-      return a;
-    }else{
-      return b;
-    }
-  }
 
 }
--- AxonIvyPortal/PortalKit/src/ch/ivy/addon/portalkit/util/PermissionUtils.java
+++ AxonIvyPortal/PortalKit/src/ch/ivy/addon/portalkit/util/PermissionUtils.java
@@ -1,14 +1,8 @@
 package ch.ivy.addon.portalkit.util;
 
-import ch.ivy.addon.portalkit.bo.ExpressProcess;
 import ch.ivyteam.ivy.environment.Ivy;
-import ch.ivyteam.ivy.security.IRole;
-import ch.ivyteam.ivy.security.ISecurityMember;
-import ch.ivyteam.ivy.security.IUser;
 
 public class PermissionUtils {
-  private static final String ADMIN_ROLE = "AXONIVY_PORTAL_ADMIN";
-
   /**
    * Check if current user has read all tasks permission
    * 
@@ -37,30 +31,5 @@ public class PermissionUtils {
   public static boolean checkTaskReadOwnCaseTasksPermission() {
     return Ivy.session().hasPermission(Ivy.request().getApplication().getSecurityDescriptor(),
         ch.ivyteam.ivy.security.IPermission.TASK_READ_OWN_CASE_TASKS);
-  }
-
-  /**
-   * Check if user can start an Express workflow
-   * 
-   * @param workflow
-   * @return True: has permission to start Express workflow, False: Do not have permission to start Express workflow
-   */
-  public static boolean canStartExpressWorkflow(ExpressProcess workflow) {
-    boolean isWorkflowOwner = Ivy.session().canActAsUser(Ivy.request().getApplication().getSecurityContext().findUser(workflow.getProcessOwner().substring(1)));
-    boolean hasAdminRole = Ivy.session().hasRole(Ivy.request().getApplication().getSecurityContext().findRole(ADMIN_ROLE), false);
-
-    if (isWorkflowOwner || hasAdminRole) {
-      return true;
-    }
-
-    for (ISecurityMember member : workflow.getProcessPermissions()) {
-      boolean isAssignedUser = member.isUser() && Ivy.session().canActAsUser((IUser) member);
-      boolean hasAssignedRole = !member.isUser() && Ivy.session().hasRole((IRole) member, false);
-      if (isAssignedUser || hasAssignedRole) {
-        return true;
-      }
-    }
-
-    return false;
   }
 }
