28d865b93e519ef6be918478605932f4850886a1
AAVN\anhnguyenhoang <anh.nguyenhoang@axonactive.com>
2018-05-11 17:15:11 +0700

    IVYPORTAL-5429 Clean Sonar critical and major violations - Clean sonar issues.

--- AxonIvyPortal/PortalConnector/src/ch/ivy/ws/addon/service/IsAliveServiceImpl.java
+++ AxonIvyPortal/PortalConnector/src/ch/ivy/ws/addon/service/IsAliveServiceImpl.java
@@ -1,60 +1,63 @@
 package ch.ivy.ws.addon.service;
 
+import static ch.ivyteam.ivy.server.ServerFactory.getServer;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.Callable;
+import java.util.stream.Collectors;
 
 import ch.ivy.ws.addon.WSException;
 import ch.ivy.ws.addon.bo.IsAliveServiceResult;
 import ch.ivy.ws.addon.types.IvyApplication;
 import ch.ivyteam.ivy.application.IApplication;
-import ch.ivyteam.ivy.server.IServer;
 import ch.ivyteam.ivy.server.ServerFactory;
 
 /**
  * Default implementation for the isAlive service
+ * 
  * @deprecated This class is deprecated
  *
  */
 @Deprecated
 public class IsAliveServiceImpl extends AbstractService implements IIsAliveService {
 
-	private static String SYSTEM_APP_NAME = "System";
-	
-	@Override
-	public IsAliveServiceResult isActive(final List<String> apps) throws WSException {
-		try{
-			return ServerFactory.getServer().getSecurityManager().executeAsSystem(new Callable<IsAliveServiceResult>() {
-				@Override
-				public IsAliveServiceResult call() throws Exception {
-					IsAliveServiceResult result = new IsAliveServiceResult();
-					
-					if (apps == null || apps.isEmpty()) {
-					  IServer server = ch.ivyteam.ivy.server.ServerFactory.getServer();
-						
-					  for(IApplication i : server.getApplicationConfigurationManager().getApplications()){
-						if (i.getName().equals(SYSTEM_APP_NAME) == false && apps != null) {
-					      apps.add(i.getName());
-						}
-					  }				
-					}				
-					
-					List<IvyApplication> ivyApps = new ArrayList<>();
-          if (apps != null) {
-            for (String name : apps) {
-              IvyApplication app = isApplicationActive(name);
-              ivyApps.add(app);
-            }
-          }
-					result.setApps(ivyApps);
-					
-					return result;
-				}
-			});
-		} catch (Exception e){
-			throw new WSException(10006, e);
-		}
-		
-	}
+  private static final String SYSTEM_APP_NAME = "System";
 
+  @Override
+  public IsAliveServiceResult isActive(final List<String> apps) throws WSException {
+    try {
+      return ServerFactory.getServer().getSecurityManager().executeAsSystem(new Callable<IsAliveServiceResult>() {
+        @Override
+        public IsAliveServiceResult call() throws Exception {
+          IsAliveServiceResult result = isAliveServiceResult(new ArrayList<>());
+          if (apps == null) {
+            return result;
+          }
+          if (apps.isEmpty()) {
+            getServerAppNames(apps);
+          }
+          for (String name : apps) {
+            IvyApplication app = isApplicationActive(name);
+            result.getApps().add(app);
+          }
+          return result;
+        }
+      });
+    } catch (Exception e) {
+      throw new WSException(10006, e);
+    }
+  }
+
+  private void getServerAppNames(final List<String> apps) {
+    List<IApplication> serverApp = getServer().getApplicationConfigurationManager().getApplications();
+    apps.addAll(serverApp.stream().filter(app -> !app.getName().equals(SYSTEM_APP_NAME)).map(IApplication::getName)
+        .collect(Collectors.toList()));
+  }
+
+  private IsAliveServiceResult isAliveServiceResult(List<IvyApplication> apps) {
+    IsAliveServiceResult result = new IsAliveServiceResult();
+    result.setApps(apps);
+    return result;
+  }
 }
--- AxonIvyPortal/PortalConnector/src/ch/ivy/ws/addon/service/WebStartableSearchCriteria.java
+++ AxonIvyPortal/PortalConnector/src/ch/ivy/ws/addon/service/WebStartableSearchCriteria.java
@@ -1,67 +1,6 @@
 package ch.ivy.ws.addon.service;
 
-import java.util.List;
-import java.util.Objects;
 
-public class WebStartableSearchCriteria {
+public class WebStartableSearchCriteria extends AbstractSearchCriteria {
 
-  private String involvedUsername;
-  private List<String> involvedApplications;
-  private Long serverId;
-  private String keyword;
-
-  public boolean isEmpty() {
-    return !hasInvolvedUsername()
-        && !hasInvolvedApplications()
-        && !hasKeyword()
-        && !hasServerId();
-  }
-  
-  public String getInvolvedUsername() {
-    return involvedUsername;
-  }
-
-  public void setInvolvedUsername(String involvedUsername) {
-    this.involvedUsername = involvedUsername;
-  }
-
-  public List<String> getInvolvedApplications() {
-    return involvedApplications;
-  }
-
-  public void setInvolvedApplications(List<String> involvedApplications) {
-    this.involvedApplications = involvedApplications;
-  }
-
-  public String getKeyword() {
-    return keyword;
-  }
-
-  public void setKeyword(String keyword) {
-    this.keyword = keyword;
-  }
-
-  public boolean hasInvolvedApplications() {
-    return involvedApplications != null && !involvedApplications.isEmpty();
-  }
-
-  public boolean hasKeyword() {
-    return !(Objects.toString(keyword, "").isEmpty());
-  }
-
-  public boolean hasInvolvedUsername() {
-    return !(Objects.toString(involvedUsername, "").isEmpty());
-  }
-  
-  public Long getServerId() {
-    return serverId;
-  }
-
-  public void setServerId(Long serverId) {
-    this.serverId = serverId;
-  }
-  
-  public boolean hasServerId() {
-    return serverId != null && serverId != 0;
-  }
 }
