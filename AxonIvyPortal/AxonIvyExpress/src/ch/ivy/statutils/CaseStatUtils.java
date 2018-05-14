23ffa0bb49aa76cf01767e23246622c21a8bb4e2
AAVN\anhnguyenhoang <anh.nguyenhoang@axonactive.com>
2018-05-14 15:42:14 +0700

    IVYPORTAL-5429 Clean Sonar critical and major violations - Clean sonar issues in AxonIvyExpress
    Former-commit-id: 0c9035001bd9436ade0e8ddece697772e5d9128b

---
+++ AxonIvyPortal/AxonIvyExpress/src/ch/ivy/statutils/CaseStatUtils.java
@@ -0,0 +1,48 @@
+package ch.ivy.statutils;
+
+import static ch.ivyteam.ivy.workflow.CaseProperty.START_TIMESTAMP;
+import static ch.ivyteam.logicalexpression.RelationalOperator.EQUAL_OR_LARGER;
+
+import java.util.Date;
+import java.util.List;
+
+import ch.ivy.addon.portalkit.util.CaseUtils;
+import ch.ivyteam.ivy.environment.Ivy;
+import ch.ivyteam.ivy.workflow.CaseProperty;
+import ch.ivyteam.ivy.workflow.CaseState;
+import ch.ivyteam.ivy.workflow.ICase;
+import ch.ivyteam.ivy.workflow.IPropertyFilter;
+import ch.ivyteam.logicalexpression.RelationalOperator;
+
+public class CaseStatUtils {
+
+  private CaseStatUtils() {
+
+  }
+
+  public static int getFinishedProcessesCount(Date from) {
+    // finished cases
+    IPropertyFilter<CaseProperty> filter =
+        Ivy.wf().createCasePropertyFilter(CaseProperty.STATE, RelationalOperator.EQUAL, CaseState.DONE.intValue());
+    // date filter
+    filter = filter.and(Ivy.wf().createCasePropertyFilter(START_TIMESTAMP, EQUAL_OR_LARGER, from));
+
+    List<ICase> caseList = CaseUtils.findICases(filter);
+
+    return caseList.size();
+  }
+
+  public static int getRunningProcessesCount(Date from) {
+    // running cases
+    IPropertyFilter<CaseProperty> filter =
+        Ivy.wf().createCasePropertyFilter(CaseProperty.STATE, RelationalOperator.EQUAL, CaseState.RUNNING.intValue());
+    filter = filter.or(CaseProperty.STATE, RelationalOperator.EQUAL, CaseState.CREATED.intValue());
+    // date filter
+    filter = filter.and(Ivy.wf().createCasePropertyFilter(START_TIMESTAMP, EQUAL_OR_LARGER, from));
+
+    List<ICase> caseList = CaseUtils.findICases(filter);
+
+    return caseList.size();
+  }
+
+}
