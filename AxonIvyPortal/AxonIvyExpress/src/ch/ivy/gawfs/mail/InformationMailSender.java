a505ded88143d8304417757c899ac9d86982972a
nguyen.to <nguyen.to@axonactive.com>
2018-05-15 09:51:47 +0700

    IVYPORTAL-5435: Axon.Ivy Express - Additional Task Types II - Send Information Email

--- AxonIvyPortal/AxonIvyExpress/src/ch/ivy/gawfs/mail/InformationMailSender.java
+++ AxonIvyPortal/AxonIvyExpress/src/ch/ivy/gawfs/mail/InformationMailSender.java
@@ -1,35 +1,33 @@
 package ch.ivy.gawfs.mail;
 
-import java.io.File;
 import java.io.IOException;
-import java.util.HashMap;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 import ch.ivy.addon.portalkit.bo.ExpressUserEmail;
 import ch.ivy.addon.portalkit.dto.ExpressAttachment;
 import ch.ivy.gawfs.mail.Mail.MailBuilder;
 import ch.ivyteam.ivy.environment.Ivy;
+import ch.ivyteam.ivy.scripting.objects.File;
 
 public class InformationMailSender {
   public void send(ExpressUserEmail mail) {
     MailBuilder mailBuilder = new MailBuilder();
-    //TODO z1 send email
+    //send email
     mailBuilder.to(mail.getRecipients()).replyTo(mail.getResponseTo()).subject(mail.getSubject())
     .message(mail.getContent()).attachments(getAttachments(mail.getAttachments())).build().send();
   }
 
-  private Map<String, File> getAttachments(List<ExpressAttachment> attachments) {
-    Map<String, File> attachmentMap = new HashMap<>();
+  private List<File> getAttachments(List<ExpressAttachment> attachments) {
+    List<File> attachmentFiles = new ArrayList<>();
     for (ExpressAttachment attachment : attachments) {
       try {
-        attachmentMap.put(attachment.getName(),
-            new ch.ivyteam.ivy.scripting.objects.File(attachment.getPath(), false).getJavaFile());
+        attachmentFiles.add(new ch.ivyteam.ivy.scripting.objects.File(attachment.getPath(), false));
       } catch (IOException ex) {
         Ivy.log().warn("Attachment is not existed", ex);
       }
     }
-    return attachmentMap;
+    return attachmentFiles;
 
   }
 
