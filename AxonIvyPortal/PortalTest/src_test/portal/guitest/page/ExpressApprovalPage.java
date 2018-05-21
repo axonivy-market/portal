8c5d0d29ae9b76f8e2dca217edc0a6b6a6711396
lttung_wawa <tung.le@axonactive.com>
2018-05-21 17:28:11 +0700

    IVYPORTAL-5568 Axon.Ivy Express - Additional Task Types III - Handled rejection

--- AxonIvyPortal/PortalTest/src_test/portal/guitest/page/ExpressApprovalPage.java
+++ AxonIvyPortal/PortalTest/src_test/portal/guitest/page/ExpressApprovalPage.java
@@ -3,10 +3,14 @@ package portal.guitest.page;
 import org.openqa.selenium.By;
 
 public class ExpressApprovalPage extends TaskTemplatePage {
-  public void finish() {
+  public void approve() {
     click(By.id("form:approve-btn"));
   }
 
+  public void reject() {
+    click(By.id("form:refuse-btn"));
+  }
+  
   public void comment(String comment) {
     findElementByCssSelector("textarea[id*='comment']").sendKeys(comment);
   }
