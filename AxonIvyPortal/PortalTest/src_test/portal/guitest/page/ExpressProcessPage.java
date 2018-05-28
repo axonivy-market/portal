9ac2beac781608bd31cc9dafbf5d1b62c2df120d
nam.mai <nam.mai@axonactive.com>
2018-05-28 08:46:38 +0700

    IVYPORTAL-5570: Axon.Ivy Express - Create Forms III - Handled feedback: Wrong case note - Handled feedback: Ugly label of file upload component - Handled feedback: Incorrect applicant - Handled feedback: Approval task type disappear when first task is email - Handled feedback: Ugly UI of Preview Form - Handled feedback: Error when save a created Express process - Fixed GUI test

--- AxonIvyPortal/PortalTest/src_test/portal/guitest/page/ExpressProcessPage.java
+++ AxonIvyPortal/PortalTest/src_test/portal/guitest/page/ExpressProcessPage.java
@@ -43,6 +43,7 @@ public class ExpressProcessPage extends TemplatePage{
   
   public void createTask(int taskIndex, int typeIndex, String taskName, String taskDescription, List<ExpressResponsible> responsibles){
     chooseTaskType(taskIndex, typeIndex);
+    waitAjaxIndicatorDisappear();
     type(By.id(String.format("form:defined-tasks-list:%d:task-name", taskIndex)), taskName);
     type(By.id(String.format("form:defined-tasks-list:%d:task-description", taskIndex)), taskDescription);
     click(By.id(String.format("form:defined-tasks-list:%d:task-responsible", taskIndex)));
--- AxonIvyPortal/PortalTest/src_test/portal/guitest/page/ExpressReviewPage.java
+++ AxonIvyPortal/PortalTest/src_test/portal/guitest/page/ExpressReviewPage.java
@@ -1,9 +1,11 @@
 package portal.guitest.page;
 
+import java.util.List;
 import java.util.stream.Collectors;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.server.browserlaunchers.Sleeper;
 
 public class ExpressReviewPage extends TaskTemplatePage {
   public void finish() {
@@ -11,6 +13,11 @@ public class ExpressReviewPage extends TaskTemplatePage {
   }
 
   public String getApprovalResult() {
+    List<WebElement> openFieldSetButtons = findListElementsByCssSelector("fieldset[id *= ':finished-task-fieldset'] .ui-icon-plusthick");
+    for (WebElement elem : openFieldSetButtons) {
+      elem.click();
+    }
+    Sleeper.sleepTight(2000);
     return findListElementsByCssSelector("div[id*='approval-result-table'] td").stream().map(WebElement::getText)
         .collect(Collectors.joining(","));
   }
