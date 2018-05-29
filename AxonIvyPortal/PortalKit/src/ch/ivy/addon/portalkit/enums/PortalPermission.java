d09f7528b05b07bad4c6962a524313328de58d0c
AAVN\anhnguyenhoang <anh.nguyenhoang@axonactive.com>
2018-05-29 15:32:39 +0700

    IVYPORTAL-5256 Enhance Permission Handling in Portal - Implemented.
    Former-commit-id: a3698b9261c37b6b57e2a70f1577138c0658dbe1

--- AxonIvyPortal/PortalKit/src/ch/ivy/addon/portalkit/enums/PortalPermission.java
+++ AxonIvyPortal/PortalKit/src/ch/ivy/addon/portalkit/enums/PortalPermission.java
@@ -1,15 +1,22 @@
 package ch.ivy.addon.portalkit.enums;
 
 public enum PortalPermission {
-  TASK_WRITE_ACTIVATOR_OWN_TASKS("TaskWriteActivatorOwnTasks");
+  TASK_WRITE_ACTIVATOR_OWN_TASKS("TaskWriteActivatorOwnTasks", PortalPermissionGroup.TASK_PERMISSIONS_GROUP);
 
   private String value;
+  private PortalPermissionGroup group;
 
-  private PortalPermission(String value) {
+  private PortalPermission(String value, PortalPermissionGroup group) {
     this.value = value;
+    this.group = group;
   }
 
   public String getValue() {
     return value;
   }
+
+  public PortalPermissionGroup getGroup() {
+    return group;
+  }
+
 }
