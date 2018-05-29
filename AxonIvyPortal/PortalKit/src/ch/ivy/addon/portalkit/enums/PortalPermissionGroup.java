a3698b9261c37b6b57e2a70f1577138c0658dbe1
AAVN\anhnguyenhoang <anh.nguyenhoang@axonactive.com>
2018-05-29 15:32:39 +0700

    IVYPORTAL-5256 Enhance Permission Handling in Portal - Implemented.

--- AxonIvyPortal/PortalKit/src/ch/ivy/addon/portalkit/enums/PortalPermissionGroup.java
+++ AxonIvyPortal/PortalKit/src/ch/ivy/addon/portalkit/enums/PortalPermissionGroup.java
@@ -1,7 +1,11 @@
 package ch.ivy.addon.portalkit.enums;
 
 public enum PortalPermissionGroup {
-  PORTAL_PERMISSION_GROUP("PortalPermissions");
+  PORTAL_PERMISSION_GROUP("PortalPermissions"),
+  TASK_PERMISSIONS_GROUP("PortalTaskPermissions"),
+  CASE_PERMISSIONS_GROUP("PortalCasePermissions"),
+  GENERAL_PERMISSIONS_GROUP("PortalGeneralPortalPermissions"),
+  ABSENCE_AND_SUBSTITUTE_GROUP("PortalAbsenceAndSubstitutePermissions");
 
   private String value;
 
