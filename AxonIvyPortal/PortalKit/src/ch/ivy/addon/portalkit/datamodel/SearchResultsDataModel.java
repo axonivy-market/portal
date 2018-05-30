171e678d594aba430f4fbe9483667382f6144111
Long Do <long.do@axonactive.com>
2018-05-30 09:16:32 +0700

    IVYPORTAL-5231 Enhance Search Function - Global search: + Implemented standard search: process, task, case

---
+++ AxonIvyPortal/PortalKit/src/ch/ivy/addon/portalkit/datamodel/SearchResultsDataModel.java
@@ -0,0 +1,39 @@
+package ch.ivy.addon.portalkit.datamodel;
+
+import java.io.Serializable;
+
+public class SearchResultsDataModel implements Serializable {
+
+  protected String keyword;
+  protected TaskLazyDataModel taskDataModel;
+  protected CaseLazyDataModel caseDataModel;
+  
+  public SearchResultsDataModel() {
+    taskDataModel = new TaskLazyDataModel("search-results-tabview:task-results");
+    caseDataModel = new CaseLazyDataModel("search-results-tabview:case-results");
+  }
+
+  public String getKeyword() {
+    return keyword;
+  }
+
+  public void setKeyword(String keyword) {
+    this.keyword = keyword;
+  }
+
+  public TaskLazyDataModel getTaskDataModel() {
+    return taskDataModel;
+  }
+
+  public void setTaskDataModel(TaskLazyDataModel taskDataModel) {
+    this.taskDataModel = taskDataModel;
+  }
+
+  public void setCaseDataModel(CaseLazyDataModel caseDataModel) {
+    this.caseDataModel = caseDataModel;
+  }
+  
+  public CaseLazyDataModel getCaseDataModel() {
+    return caseDataModel;
+  }
+}
