42181838d6702e884641cbb7df839f11080794fd
Nam Le Dinh <nam.ledinh@axonactive.com>
2018-06-06 11:24:01 +0700

    IVYPORTAL-5688: Improvements for Statistics - Enhance Create Chart filters II - Revert redundant code
    Former-commit-id: c7c824357b0d23d7e76084c9d2b62ea0896ff135

--- AxonIvyPortal/PortalConnector/src/ch/ivy/ws/addon/service/TaskServiceImpl.java
+++ AxonIvyPortal/PortalConnector/src/ch/ivy/ws/addon/service/TaskServiceImpl.java
@@ -27,7 +27,6 @@ import ch.ivy.ws.addon.bo.AvailableAppsResult;
 import ch.ivy.ws.addon.bo.NoteServiceResult;
 import ch.ivy.ws.addon.bo.TaskServiceResult;
 import ch.ivy.ws.addon.enums.PortalPermission;
-import ch.ivy.ws.addon.persistence.PortalCaseDAO;
 import ch.ivy.ws.addon.transformer.IvyNoteTransformer;
 import ch.ivy.ws.addon.transformer.IvyTaskTransformer;
 import ch.ivy.ws.addon.types.ElapsedTimeStatistic;
@@ -350,10 +349,6 @@ public class TaskServiceImpl extends AbstractService implements ITaskService {
 
   @Override
   public TaskServiceResult countTasksByCriteria(TaskSearchCriteria taskSearchCriteria) throws WSException {
-    
-    PortalCaseDAO portalCaseDAO = new PortalCaseDAO();
-    List<String> result = portalCaseDAO.findCustomVarChar1Fields("char", 10);
-    Ivy.log().error(result);
     List<WSException> errors = Collections.emptyList();
     try {
       return securityManager().executeAsSystem(() -> {
