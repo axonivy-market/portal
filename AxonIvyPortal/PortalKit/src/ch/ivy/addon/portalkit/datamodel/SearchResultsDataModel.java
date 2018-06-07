610bb6bb4577c9efa60d102a4333dd3b09491116
Long Do <long.do@axonactive.com>
2018-06-07 15:51:30 +0700

    IVYPORTAL-5691 Enhance Search Function - Global search II: + Added CMS

--- AxonIvyPortal/PortalKit/src/ch/ivy/addon/portalkit/datamodel/SearchResultsDataModel.java
+++ AxonIvyPortal/PortalKit/src/ch/ivy/addon/portalkit/datamodel/SearchResultsDataModel.java
@@ -2,19 +2,49 @@ package ch.ivy.addon.portalkit.datamodel;
 
 import java.io.Serializable;
 
+import org.apache.commons.lang3.StringUtils;
+
+import ch.ivy.addon.portalkit.enums.TaskAssigneeType;
+import ch.ivy.addon.portalkit.util.PermissionUtils;
+import ch.ivy.addon.portalkit.util.SecurityServiceUtils;
+
 public class SearchResultsDataModel implements Serializable {
 
+  protected static final String CASE_WIDGET_COMPONENT_ID = "search-results-tabview:case-results";
+  protected static final String TASK_WIDGET_COMPONENT_ID = "search-results-tabview:task-results";
   protected String keyword;
   protected TaskLazyDataModel taskDataModel;
   protected CaseLazyDataModel caseDataModel;
   
   public SearchResultsDataModel() {
-    taskDataModel = new TaskLazyDataModel("search-results-tabview:task-results");
-    caseDataModel = new CaseLazyDataModel("search-results-tabview:case-results");
+    boolean hasReadAllTasksPermisson = PermissionUtils.checkReadAllTasksPermission();
+    boolean hasReadAllCasesPermission = PermissionUtils.checkReadAllCasesPermission();
+    taskDataModel = initTaskDataModel();
+    taskDataModel.setIgnoreInvolvedUser(hasReadAllTasksPermisson);
+    taskDataModel.setTaskAssigneeType(TaskAssigneeType.ALL);
+    
+    caseDataModel = initCaseDataModel();
+    caseDataModel.setIgnoreInvolvedUser(hasReadAllCasesPermission);
+    Long serverId = SecurityServiceUtils.getServerIdFromSession();
+    if (serverId != null) {
+      caseDataModel.setServerId(serverId);
+    }
+    String applicationName = SecurityServiceUtils.getApplicationNameFromSession();
+    if (StringUtils.isNotBlank(applicationName)) {
+      caseDataModel.setInvolvedApplications(applicationName);
+    }
   }
   
   public void search() {
     // Placeholder for customization
+  }
+  
+  protected TaskLazyDataModel initTaskDataModel() {
+    return new TaskLazyDataModel(TASK_WIDGET_COMPONENT_ID); 
+  }
+  
+  protected CaseLazyDataModel initCaseDataModel() {
+    return new CaseLazyDataModel(CASE_WIDGET_COMPONENT_ID);
   }
 
   public String getKeyword() {
