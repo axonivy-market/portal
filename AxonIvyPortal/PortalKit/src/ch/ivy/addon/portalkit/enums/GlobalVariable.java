29346bb8e842146f239810b7a4e80399329b827a
nguyen.to <nguyen.to@axonactive.com>
2018-06-08 15:29:09 +0700

    IVYPORTAL-5664: Portal administration - Configuration/Properties - Add dropdown selection for edit mode.
    Former-commit-id: eb2872453f5c7b119e3494726f0916be6af176ba

--- AxonIvyPortal/PortalKit/src/ch/ivy/addon/portalkit/enums/GlobalVariable.java
+++ AxonIvyPortal/PortalKit/src/ch/ivy/addon/portalkit/enums/GlobalVariable.java
@@ -1,5 +1,8 @@
 package ch.ivy.addon.portalkit.enums;
 
+import java.util.Arrays;
+import java.util.List;
+
 import ch.ivy.addon.portalkit.bean.TaskWidgetBean;
 import ch.ivy.addon.portalkit.document.DocumentExtensionConstants;
 import ch.ivy.addon.portalkit.statistics.StatisticChartConstants;
@@ -33,4 +36,15 @@ public enum GlobalVariable {
     return defaultValue;
   }
 
+  public static List<GlobalVariable> getBooleanType() {
+    return Arrays.asList(HIDE_LOGOUT_BUTTON, SHOW_ENVIRONMENT_INFO, SHOW_TASK_BUTTON_LABELS, HIDE_CHANGE_PASSWORD_BUTTON, HIDE_UPLOAD_DOCUMENT_FOR_DONE_CASE, ENABLE_SCRIPT_CHECKING_FOR_UPLOADED_DOCUMENT);
+  }
+
+  public static List<GlobalVariable> getNumberType() {
+    return Arrays.asList(REFRESH_TASK_LIST_INTERVAL, CLIENT_SIDE_TIMEOUT);
+  }
+
+  public static List<GlobalVariable> getEnumType() {
+    return Arrays.asList(EXPIRY_CHART_LAST_DRILLDOWN_LEVEL);
+  }
 }
