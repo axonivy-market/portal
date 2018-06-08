80c886231b1e61413158be176d2c7e0c4147a345
nam.chu <nam.chu@axonactive.com>
2018-06-08 10:04:37 +0700

    IVYPORTAL-5665 Case history/notes - show more page and export Enhancement - Modify test

--- AxonIvyPortal/PortalTest/src_test/portal/guitest/page/NoteHistoryPage.java
+++ AxonIvyPortal/PortalTest/src_test/portal/guitest/page/NoteHistoryPage.java
@@ -4,17 +4,20 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
 public class NoteHistoryPage extends TemplatePage {
-    
-    private static final String TABLE_ROWS_PATH = "div[id*='notes-table'] table>tbody>tr";
-    
-    public int countNotes() {
-        waitForElementDisplayed(By.cssSelector("div[id*='notes-table']"), true);
-        return driver.findElements(By.cssSelector(TABLE_ROWS_PATH)).size();
-    }
-    
-    public String getNoteContentOfRow(int index) {
-        WebElement firstRow = driver.findElements(By.cssSelector(TABLE_ROWS_PATH)).get(index);
-        return firstRow.findElements(By.xpath("td")).get(2).getText();
-    }
 
+  private static final String TABLE_ROWS_PATH = "div[id*='notes-table'] table>tbody>tr";
+
+  public int countNotes() {
+    waitForElementDisplayed(By.cssSelector("div[id*='notes-table']"), true);
+    return driver.findElements(By.cssSelector(TABLE_ROWS_PATH)).size();
+  }
+
+  public String getNoteContentOfRow(int index) {
+    WebElement firstRow = driver.findElements(By.cssSelector(TABLE_ROWS_PATH)).get(index);
+    return firstRow.findElements(By.xpath("td")).get(2).getText();
+  }
+
+  public String getCaseName(){
+      return findElementById("form:case-name").getText(); 
+   }
 }
