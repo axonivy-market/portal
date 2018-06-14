34b6b1b378369e283192513e3a816da081d60032
lttung_wawa <tung.le@axonactive.com>
2018-06-14 09:55:30 +0700

    IVYPORTAL-5291 Case and Task Query created by user in portal - for statistics - Added analysis page

--- AxonIvyPortal/PortalKit/src/ch/ivy/addon/portalkit/service/TaskFilterService.java
+++ AxonIvyPortal/PortalKit/src/ch/ivy/addon/portalkit/service/TaskFilterService.java
@@ -4,6 +4,7 @@ package ch.ivy.addon.portalkit.service;
 import java.util.ArrayList;
 import java.util.List;
 
+import ch.ivy.addon.portalkit.datamodel.TaskAnalysisLazyDataModel;
 import ch.ivy.addon.portalkit.datamodel.TaskLazyDataModel;
 import ch.ivy.addon.portalkit.taskfilter.TaskFilter;
 import ch.ivy.addon.portalkit.taskfilter.TaskFilterData;
@@ -15,6 +16,22 @@ public class TaskFilterService extends AbstractFilterService<TaskFilterData> {
   }
 
   public void applyFilter(TaskLazyDataModel dataModel, TaskFilterData savedFilterData) throws ReflectiveOperationException {
+    List<TaskFilter> filters = dataModel.getFilterContainer().getFilters();
+    dataModel.setSelectedFilters(new ArrayList<>());
+    for (int i = 0; i < filters.size(); i++) {
+      TaskFilter taskFilter = dataModel.getFilterContainer().getFilters().get(i);
+      for (int j = 0; j < savedFilterData.getFilters().size(); j++) {
+        TaskFilter savedTaskFilter = savedFilterData.getFilters().get(j);
+        if (taskFilter.getClass().equals(savedTaskFilter.getClass())) {
+          copyFilterValues(taskFilter, savedTaskFilter);
+          dataModel.getSelectedFilters().add(taskFilter);
+          break;
+        }
+      }
+    }
+  }
+
+  public void applyFilter(TaskAnalysisLazyDataModel dataModel, TaskFilterData savedFilterData) throws ReflectiveOperationException {
     List<TaskFilter> filters = dataModel.getFilterContainer().getFilters();
     dataModel.setSelectedFilters(new ArrayList<>());
     for (int i = 0; i < filters.size(); i++) {
