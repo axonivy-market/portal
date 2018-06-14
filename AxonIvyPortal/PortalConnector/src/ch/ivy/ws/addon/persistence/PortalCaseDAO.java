82e6f9e8a9e5a4d8e0c23b2816f27c1ed88b3751
lttung_wawa <tung.le@axonactive.com>
2018-06-14 09:51:40 +0700

    IVYPORTAL-5291 Case and Task Query created by user in portal - for statistics - Added analysis page
    Former-commit-id: cd504997a4d63e285d85d2983af764e9ada2d400

--- AxonIvyPortal/PortalConnector/src/ch/ivy/ws/addon/persistence/PortalCaseDAO.java
+++ AxonIvyPortal/PortalConnector/src/ch/ivy/ws/addon/persistence/PortalCaseDAO.java
@@ -11,44 +11,44 @@ public class PortalCaseDAO extends AbstractDAO{
   
   @SuppressWarnings("unchecked")
   public List<String> findCustomVarChar1Fields(String keyword,int limit) {
-      Query query = generateQueryForCustomVarCharFields("customVarCharField1", keyword, limit);
+      Query query = generateQueryForField("customVarCharField1", keyword, limit);
       return query.getResultList();
   }
 
   @SuppressWarnings("unchecked")
   public List<String> findCustomVarChar2Fields(String keyword, int limit) {
-      Query query = generateQueryForCustomVarCharFields("customVarCharField2", keyword, limit);
+      Query query = generateQueryForField("customVarCharField2", keyword, limit);
       return query.getResultList();
   }
   
   @SuppressWarnings("unchecked")
   public List<String> findCustomVarChar3Fields(String keyword, int limit) {
-      Query query = generateQueryForCustomVarCharFields("customVarCharField3", keyword, limit);
+      Query query = generateQueryForField("customVarCharField3", keyword, limit);
       return query.getResultList();
   }
   
   @SuppressWarnings("unchecked")
   public List<String> findCustomVarChar4Fields(String keyword, int limit) {
-      Query query = generateQueryForCustomVarCharFields("customVarCharField4", keyword, limit);
+      Query query = generateQueryForField("customVarCharField4", keyword, limit);
       return query.getResultList();
   }
   
   @SuppressWarnings("unchecked")
   public List<String> findCustomVarChar5Fields(String keyword, int limit) {
-      Query query = generateQueryForCustomVarCharFields("customVarCharField5", keyword, limit);
+      Query query = generateQueryForField("customVarCharField5", keyword, limit);
       return query.getResultList();
   }
   
-  private Query generateQueryForCustomVarCharFields(String customVarCharColumn ,String keyword, int limit) {
+  private Query generateQueryForField(String fieldName ,String keyword, int limit) {
     Query query = null;
     String queryString = null;
     if(StringUtils.isNoneBlank(keyword)) {
-      queryString = String.format("Select distinct %1$s from PortalSystemIvyCase where lower(%1$s) like lower(concat('%%', :%1$s,'%%')) order by %1$s", customVarCharColumn);
+      queryString = String.format("Select distinct %1$s from PortalSystemIvyCase where lower(%1$s) like lower(concat('%%', :%1$s,'%%')) order by %1$s", fieldName);
       query = getEM().createQuery(queryString);
-      query.setParameter(customVarCharColumn, keyword);
+      query.setParameter(fieldName, keyword);
     }
     else {
-      queryString = String.format("Select distinct %1$s from PortalSystemIvyCase where %1$s is not null order by %1$s", customVarCharColumn);
+      queryString = String.format("Select distinct %1$s from PortalSystemIvyCase where %1$s is not null order by %1$s", fieldName);
       query = getEM().createQuery(queryString);
     }
     if(limit > 0) {
---
+++ AxonIvyPortal/PortalTemplate/src/ch/ivy/addon/portal/generic/view/TaskAnalysisView.java
@@ -0,0 +1,164 @@
+package ch.ivy.addon.portal.generic.view;
+
+import org.apache.commons.lang3.StringUtils;
+
+import ch.ivy.addon.portalkit.bo.MainMenuNode;
+import ch.ivy.addon.portalkit.datamodel.TaskAnalysisLazyDataModel;
+import ch.ivyteam.ivy.environment.Ivy;
+
+public class TaskAnalysisView {
+
+  public static class Builder {
+
+    private String pageTitle = StringUtils.EMPTY;
+    private TaskAnalysisLazyDataModel dataModel;
+    private String keyword = StringUtils.EMPTY;
+    private long remoteTaskId = -1L;
+    private boolean canLinkBackCaseDetail = Boolean.FALSE;
+    private String caseName = StringUtils.EMPTY;
+    private boolean hideTaskFilter = Boolean.FALSE;
+    private boolean showHeaderToolbar = Boolean.TRUE;
+    private String noTaskFoundMessage = Ivy.cms().co("/ch.ivy.addon.portalkit.ui.jsf/components/taskView/noTask");
+    private boolean compactMode = Boolean.TRUE;
+
+    private MainMenuNode category;
+
+    public Builder pageTitle(String pageTitle) {
+      this.pageTitle = pageTitle;
+      return this;
+    }
+
+    public Builder caseName(String caseName) {
+      this.caseName = caseName;
+      return this;
+    }
+
+    public Builder dataModel(TaskAnalysisLazyDataModel taskAnalysisLazyDataModel) {
+      this.dataModel = taskAnalysisLazyDataModel;
+      return this;
+    }
+
+    public Builder keyword(String keyword) {
+      this.keyword = keyword;
+      return this;
+    }
+
+    public Builder remoteTaskId(long taskId) {
+      this.remoteTaskId = taskId;
+      return this;
+    }
+
+    public Builder canLinkBackCaseDetail(boolean canLinkBackCaseDetail) {
+      this.canLinkBackCaseDetail = canLinkBackCaseDetail;
+      return this;
+    }
+
+    public Builder category(MainMenuNode category) {
+      this.category = category;
+      return this;
+    }
+
+    public Builder hideTaskFilter(boolean hideTaskFilter) {
+      this.hideTaskFilter = hideTaskFilter;
+      return this;
+    }
+
+    public Builder showHeaderToolbar(boolean showHeaderToolbar) {
+      this.showHeaderToolbar = showHeaderToolbar;
+      return this;
+    }
+
+    public Builder noTaskFoundMessage(String noTaskFoundMessage) {
+      this.noTaskFoundMessage = noTaskFoundMessage;
+      return this;
+    }
+    
+    public Builder compactMode(boolean compactMode) {
+      this.compactMode = compactMode;
+      return this;
+    }
+
+    public TaskAnalysisView createNewTaskView() {
+      return new TaskAnalysisView(this);
+    }
+  }
+
+  private final String pageTitle;
+  private TaskAnalysisLazyDataModel dataModel;
+  private final String keyword;
+  private final long remoteTaskId;
+  private final String caseName;
+  private final boolean canLinkBackCaseDetail;
+  private final MainMenuNode category;
+  private final boolean hideTaskFilter;
+  private final boolean showHeaderToolbar;
+  private final String noTaskFoundMessage;
+  private final boolean compactMode;
+
+  private TaskAnalysisView(Builder builder) {
+    pageTitle = builder.pageTitle;
+    dataModel = builder.dataModel;
+    keyword = builder.keyword;
+    remoteTaskId = builder.remoteTaskId;
+    category = builder.category;
+    canLinkBackCaseDetail = builder.canLinkBackCaseDetail;
+    caseName = builder.caseName;
+    hideTaskFilter = builder.hideTaskFilter;
+    showHeaderToolbar = builder.showHeaderToolbar;
+    noTaskFoundMessage = builder.noTaskFoundMessage;
+    compactMode = builder.compactMode;
+  }
+
+  public String getNoTaskFoundMessage() {
+    return noTaskFoundMessage;
+  }
+
+  public static Builder create() {
+    return new Builder();
+  }
+
+  public String getPageTitle() {
+    return pageTitle;
+  }
+
+  public TaskAnalysisLazyDataModel getDataModel() {
+    return dataModel;
+  }
+  
+  public void setDataModel(TaskAnalysisLazyDataModel dataModel) {
+    this.dataModel = dataModel;
+  }
+
+  public String getKeyword() {
+    return keyword;
+  }
+
+  public long getRemoteTaskId() {
+    return remoteTaskId;
+  }
+
+  public boolean canLinkBackCaseDetail() {
+    return canLinkBackCaseDetail;
+  }
+
+  public MainMenuNode getCategory() {
+    return category;
+  }
+
+  public String getCaseName() {
+    return caseName;
+  }
+
+  public boolean isHideTaskFilter() {
+    return hideTaskFilter;
+  }
+
+  public boolean isShowHeaderToolbar() {
+    return showHeaderToolbar;
+  }
+
+  public boolean isCompactMode() {
+    return compactMode;
+  }
+  
+}
