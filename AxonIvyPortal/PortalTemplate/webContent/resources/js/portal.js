a3c8d7d886a41b994813a5cc2efd617103fc50d4
nttai_wawa <tai.nguyen@axonactive.com>
2018-06-21 11:40:43 +0700

    IVYPORTAL-5291: Case and Task Query created by user in portal - highlight menu
    Former-commit-id: e5b6d31ca8cb130627c26b4599c3775a1370d1ab

--- AxonIvyPortal/PortalTemplate/webContent/resources/js/portal.js
+++ AxonIvyPortal/PortalTemplate/webContent/resources/js/portal.js
@@ -96,7 +96,7 @@ function getReversedState(state) {
 var MainMenu = {
   urlToFirstLevelMenu : [["Processes.xhtml", ["Processes", "Prozesse", "Processus"]],
       ["PortalTasks.xhtml", ["Tasks", "Aufgaben", "Tâches"]], ["PortalCases.xhtml", ["Cases", "Vorgänge", "Dossiers"]],
-      ["PortalDashBoard.xhtml", ["Statistics", "Statistiken", "Statistiques"]]],
+      ["PortalDashBoard.xhtml", ["Statistics", "Statistiken", "Statistiques"]], ["TaskAnalysis.xhtml", ["Statistics", "Statistiken", "Statistiques"]]],
 
   init : function(responsiveToolkit) {
     this.highlightFirstLevelMenu();
