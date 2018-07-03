49a8de65ff9fbc5d62b777242e0448888821140b
nam.mai <nam.mai@axonactive.com>
2018-07-03 09:42:27 +0700

    IVYPORTAL-5831: Case and Task Query created by user in portal - for statistics II - Fixed Sonar issues

--- AxonIvyPortal/PortalKit/src/ch/ivy/addon/portalkit/taskfilter/TaskWorkerFilter.java
+++ AxonIvyPortal/PortalKit/src/ch/ivy/addon/portalkit/taskfilter/TaskWorkerFilter.java
@@ -1,23 +1,12 @@
 package ch.ivy.addon.portalkit.taskfilter;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
-import java.util.TreeSet;
-import java.util.concurrent.Callable;
-import java.util.stream.Collectors;
 
 import org.apache.commons.lang.StringUtils;
 
 import ch.ivy.addon.portalkit.bo.RemoteSecurityMember;
-import ch.ivy.addon.portalkit.bo.RemoteUser;
-import ch.ivy.addon.portalkit.comparator.RemoteUserComparator;
-import ch.ivy.addon.portalkit.mapper.RemoteSecurityMemberMapper;
-import ch.ivyteam.ivy.application.IApplication;
+import ch.ivy.addon.portalkit.util.UserUtils;
 import ch.ivyteam.ivy.environment.Ivy;
-import ch.ivyteam.ivy.process.call.SubProcessCall;
-import ch.ivyteam.ivy.server.ServerFactory;
 import ch.ivyteam.ivy.workflow.query.TaskQuery;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
@@ -72,35 +61,8 @@ public class TaskWorkerFilter extends TaskFilter {
     return workers;
   }
 
-  @SuppressWarnings("unchecked")
   private void initWorkers() {
-    workers = new ArrayList<>();
-    try {
-      List<RemoteUser> users =
-          ServerFactory.getServer().getSecurityManager().executeAsSystem(new Callable<List<RemoteUser>>() {
-            @Override
-            public List<RemoteUser> call() throws Exception {
-              if (Ivy.request().getApplication().getName().equals(IApplication.PORTAL_APPLICATION_NAME)) {
-                return SubProcessCall.withPath(SECURITY_SERVICE_CALLABLE).withStartName("findAllUsers").call()
-                    .get("users", List.class);
-              }
-              return SubProcessCall.withPath(SECURITY_SERVICE_CALLABLE).withStartName("findAllUsersByApplication")
-                  .call(Ivy.request().getApplication().getName()).get("users", List.class);
-            }
-          });
-
-      List<RemoteUser> distinctUsers =
-          users.stream().collect(
-              Collectors.collectingAndThen(
-                  Collectors.toCollection(() -> new TreeSet<>(Comparator.comparing(RemoteUser::getUsername))),
-                  ArrayList::new));
-
-      Collections.sort(distinctUsers, new RemoteUserComparator());
-
-      workers.addAll(RemoteSecurityMemberMapper.mapFromRemoteUsers(distinctUsers));
-    } catch (Exception e) {
-      Ivy.log().error("Can't get list of users in worker filter", e);
-    }
+    workers = UserUtils.findAllUserByApplication("Can't get list of users in worker filter");
   }
 
   public void setWorkers(List<RemoteSecurityMember> workers) {
