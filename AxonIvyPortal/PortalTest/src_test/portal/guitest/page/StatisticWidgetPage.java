65623e5bfb61e6e7290ab078f2187789d6b7c737
AAVN\anhnguyenhoang <anh.nguyenhoang@axonactive.com>
2018-07-05 10:19:23 +0700

    IVYPORTAL-5690 Various or urgent requests in IVYPORTAL S158 - Fixed express gui test.

--- AxonIvyPortal/PortalTest/src_test/portal/guitest/page/StatisticWidgetPage.java
+++ AxonIvyPortal/PortalTest/src_test/portal/guitest/page/StatisticWidgetPage.java
@@ -2,7 +2,6 @@ package portal.guitest.page;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.server.browserlaunchers.Sleeper;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
