5d73880a7575cefc4a8cdb10eaf5d13b360b1073
Long Do <long.do@axonactive.com>
2018-07-16 09:40:42 +0700

    IVYPORTAL-5914 Implement Chat including IIS Support II: Implemented

--- AxonIvyPortal/PortalTemplate/src/ch/ivy/addon/portal/chat/ChatContactManager.java
+++ AxonIvyPortal/PortalTemplate/src/ch/ivy/addon/portal/chat/ChatContactManager.java
@@ -1,29 +1,17 @@
 package ch.ivy.addon.portal.chat;
 
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 
 import org.apache.commons.lang.StringUtils;
 
 import ch.ivyteam.ivy.environment.Ivy;
 import ch.ivyteam.ivy.security.ISecurityConstants;
+import ch.ivyteam.ivy.security.ISession;
 import ch.ivyteam.ivy.security.IUser;
 
 public final class ChatContactManager {
-
-  private static final String CONTACT_STATUS_CONTAINTER_PATH = System.getProperty("java.io.tmpdir")
-      + File.separator + "contact-status" + File.separator;
-  private static final String CONTACT_STATUS_FILE_PATH = CONTACT_STATUS_CONTAINTER_PATH + "%s";
-
-  private static final Map<String, Integer> ONLINE_INSTANCES = new HashMap<>();
 
   private ChatContactManager() {}
 
@@ -34,56 +22,9 @@ public final class ChatContactManager {
     return users;
   }
 
-  synchronized public static void setContactOnline(String contactName) throws IOException {
-    Path path = Paths.get(String.format(CONTACT_STATUS_FILE_PATH, contactName));
-    if (!Files.exists(path)) {
-      Files.createDirectories(path.getParent());
-      Files.createFile(path);
-    }
-    increaseNumberOfOnlineInstance(contactName);
-  }
-
-  synchronized public static void setContactOffline(String contactName) throws IOException {
-    if (ONLINE_INSTANCES.get(contactName) != null && ONLINE_INSTANCES.get(contactName) < 2) {
-      Path path = Paths.get(String.format(CONTACT_STATUS_FILE_PATH, contactName));
-      if (Files.exists(path)) {
-        Files.delete(path);
-      }
-    }
-    decreaseNumberOfOnlineUnstance(contactName);
-  }
-
-  public static List<String> getOnlineContacts() throws IOException {
-    Path start = Paths.get(CONTACT_STATUS_CONTAINTER_PATH);
-    List<String> onlineContacts = new ArrayList<>();
-    if (Files.exists(start)) {
-      onlineContacts =
-          Files.walk(start, 1).filter(path -> !Files.isDirectory(path)).map(path -> path.getFileName().toString())
-              .collect(Collectors.toList());
-    }
-    return onlineContacts;
-  }
-
-  public static int getNumberOfOnlineInstance(String contactName) {
-    return ONLINE_INSTANCES.get(contactName);
-  }
-
-  private static void increaseNumberOfOnlineInstance(String contactName) {
-    Integer current = ONLINE_INSTANCES.get(contactName);
-    if (current != null) {
-      ONLINE_INSTANCES.put(contactName, ++current);
-    } else {
-      ONLINE_INSTANCES.put(contactName, 1);
-    }
-  }
-
-  private static void decreaseNumberOfOnlineUnstance(String contactName) {
-    Integer current = ONLINE_INSTANCES.get(contactName);
-    if (current != null) {
-      ONLINE_INSTANCES.put(contactName, --current);
-    } else {
-      ONLINE_INSTANCES.put(contactName, 0);
-    }
+  public static List<String> getOnlineContacts() {
+    return Ivy.wf().getSecurityContext().getSessions().stream().map(ISession::getSessionUserName)
+        .filter(session -> !StringUtils.equals(session, Ivy.session().getSessionUserName())).collect(Collectors.toList());
   }
 
   private static List<ChatContact> loadContacts() {
@@ -107,12 +48,8 @@ public final class ChatContactManager {
   }
 
   private static void filterOnlineUser(List<ChatContact> users) {
-    try {
-      List<String> onlineUsers = getOnlineContacts();
-      users.stream().filter(user -> onlineUsers.contains(user.getName())).forEach(user -> user.setOnline(true));
-    } catch (IOException e) {
-      Ivy.log().warn("Could not get list of online users.", e);
-    }
+    List<String> onlineUsers = getOnlineContacts();
+    users.stream().filter(user -> onlineUsers.contains(user.getName())).forEach(user -> user.setOnline(true));
   }
 
   private static List<IUser> getContextUsers() {
