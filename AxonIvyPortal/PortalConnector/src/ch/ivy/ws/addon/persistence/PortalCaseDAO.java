f378fffef715e4ea17dbf2cc161e732b66ab8e43
Nam Le Dinh <nam.ledinh@axonactive.com>
2018-07-27 17:52:10 +0700

    IVYPORTAL-5865: Various or urgent requests in IVYPORTAL S159 - Hibernate warning in Portal

--- AxonIvyPortal/PortalConnector/src/ch/ivy/ws/addon/persistence/PortalCaseDAO.java
+++ AxonIvyPortal/PortalConnector/src/ch/ivy/ws/addon/persistence/PortalCaseDAO.java
@@ -2,60 +2,64 @@ package ch.ivy.ws.addon.persistence;
 
 import java.util.List;
 
+import javax.persistence.EntityManager;
 import javax.persistence.Query;
 
 import org.apache.commons.lang3.StringUtils;
 
-public class PortalCaseDAO extends AbstractDAO{
+import ch.ivyteam.ivy.environment.Ivy;
+import ch.ivyteam.ivy.process.data.persistence.IIvyEntityManager;
 
-  
-  @SuppressWarnings("unchecked")
-  public List<String> findCustomVarChar1Fields(String keyword,int limit, List<Long> applications) {
-      Query query = generateQueryForField("customVarCharField1", keyword, limit, applications);
-      return query.getResultList();
+public class PortalCaseDAO extends AbstractDAO {
+
+
+  public List<String> findCustomVarChar1Fields(String keyword, int limit, List<Long> applications) {
+    return queryForCustomVarChar("customVarCharField1", keyword, limit, applications);
   }
 
-  @SuppressWarnings("unchecked")
   public List<String> findCustomVarChar2Fields(String keyword, int limit, List<Long> applications) {
-      Query query = generateQueryForField("customVarCharField2", keyword, limit, applications);
-      return query.getResultList();
+    return queryForCustomVarChar("customVarCharField2", keyword, limit, applications);
   }
-  
-  @SuppressWarnings("unchecked")
+
   public List<String> findCustomVarChar3Fields(String keyword, int limit, List<Long> applications) {
-      Query query = generateQueryForField("customVarCharField3", keyword, limit, applications);
-      return query.getResultList();
+    return queryForCustomVarChar("customVarCharField3", keyword, limit, applications);
   }
-  
-  @SuppressWarnings("unchecked")
+
   public List<String> findCustomVarChar4Fields(String keyword, int limit, List<Long> applications) {
-      Query query = generateQueryForField("customVarCharField4", keyword, limit, applications);
-      return query.getResultList();
+    return queryForCustomVarChar("customVarCharField4", keyword, limit, applications);
   }
-  
-  @SuppressWarnings("unchecked")
+
   public List<String> findCustomVarChar5Fields(String keyword, int limit, List<Long> applications) {
-      Query query = generateQueryForField("customVarCharField5", keyword, limit, applications);
-      return query.getResultList();
+    return queryForCustomVarChar("customVarCharField5", keyword, limit, applications);
   }
-  
-  private Query generateQueryForField(String fieldName ,String keyword, int limit, List<Long> applicationIds) {
+
+  @SuppressWarnings("unchecked")
+  private List<String> queryForCustomVarChar(String fieldName, String keyword, int limit, List<Long> applicationIds) {
     Query query = null;
     String queryString = null;
-    if(StringUtils.isNoneBlank(keyword)) {
-      queryString = String.format("Select distinct %1$s from PortalSystemIvyCase where lower(%1$s) like lower(concat('%%', :%1$s,'%%')) and applicationId in (:applicationId) order by %1$s", fieldName);
-      query = getEM().createQuery(queryString);
-      query.setParameter(fieldName, keyword);
-      query.setParameter("applicationId", applicationIds);
+    IIvyEntityManager ivyEntityManager = Ivy.persistence().get(getPersistenceIdentifier());
+    EntityManager em = ivyEntityManager.createEntityManager();
+    try {
+
+      if (StringUtils.isNoneBlank(keyword)) {
+        queryString = String.format("Select distinct %1$s from PortalSystemIvyCase where lower(%1$s) like lower(concat('%%', :%1$s,'%%')) and applicationId in (:applicationId) order by %1$s", fieldName);
+        query = em.createQuery(queryString);
+        query.setParameter(fieldName, keyword);
+        query.setParameter("applicationId", applicationIds);
+      } else {
+        queryString = String.format("Select distinct %1$s from PortalSystemIvyCase where %1$s is not null and applicationId in (:applicationId) order by %1$s", fieldName);
+        query = em.createQuery(queryString);
+        query.setParameter("applicationId", applicationIds);
+      }
+      if (limit > 0) {
+        query.setMaxResults(limit);
+      }
+      em.getTransaction().begin();
+      List<String> result = query.getResultList();
+      em.getTransaction().commit();
+      return result;
+    } finally {
+      em.close();
     }
-    else {
-      queryString = String.format("Select distinct %1$s from PortalSystemIvyCase where %1$s is not null and applicationId in (:applicationId) order by %1$s", fieldName);
-      query = getEM().createQuery(queryString);
-      query.setParameter("applicationId", applicationIds);
-    }
-    if(limit > 0) {
-      query.setMaxResults(limit);
-    }
-    return query;
   }
 }
