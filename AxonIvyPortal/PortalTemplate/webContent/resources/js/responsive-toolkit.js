1abb619a5d10c9bb56db84cef869932550790df2
ttthai_wawa <hai.tran@axonacitve.com>
2018-11-02 13:34:05 +0700

    IVYPORTAL-6299: Improve Dashboard - Task Element - Improve task information in dashboard

--- AxonIvyPortal/PortalTemplate/webContent/resources/js/responsive-toolkit.js
+++ AxonIvyPortal/PortalTemplate/webContent/resources/js/responsive-toolkit.js
@@ -745,7 +745,13 @@ function DashboardMediumScreen() {
 		} else {
 			$dashboard.animate({marginLeft : firstCol.marginValWhenMainMenuClose}, animateDuration);
 		}
+		
+		hideTaskIdAndState();
 	}
+	
+	function hideTaskIdAndState() {
+      $(".js-task-start-info-task-state").remove();
+    }
 }
 
 function DashboardSmallScreen(){
@@ -765,6 +771,10 @@ function DashboardSmallScreen(){
 	  updateDashboard();
 	}
 	
+	function hideTaskIdAndState() {
+      $(".js-task-start-info-task-state").remove();
+    }
+	
 	function moveStatisticsToFirstCol() {
 		   $('.js-dashboard-main-content-1st-col').append($('.js-statistic-widget'));
 		   $('.js-dashboard-main-content-3rd-col').addClass('u-hidden');
@@ -775,6 +785,7 @@ function DashboardSmallScreen(){
 	    var $dashboardFirstCol = $('.js-dashboard-main-content-1st-col');
 	    var $dashboardSecondCol = $('.js-dashboard-main-content-2nd-col');
 	    moveStatisticsToFirstCol();
+	    hideTaskIdAndState();
 	    if ($mainMenu.hasClass('in')) {
 	      // Open main menu
 	      $dashboardFirstCol.animate({
