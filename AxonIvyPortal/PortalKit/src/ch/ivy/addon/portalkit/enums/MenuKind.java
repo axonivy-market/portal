1013de68160e7757de9316571658b69cc0706c37
Long Do <long.do@axonactive.com>
2018-11-02 15:46:35 +0700

    IVYPORTAL-6293 Implement mobile recognition, Login and Portal Header: + Highlight menu

--- AxonIvyPortal/PortalKit/src/ch/ivy/addon/portalkit/enums/MenuKind.java
+++ AxonIvyPortal/PortalKit/src/ch/ivy/addon/portalkit/enums/MenuKind.java
@@ -2,4 +2,9 @@ package ch.ivy.addon.portalkit.enums;
 
 public enum MenuKind {
   PROCESS, TASK, CASE, DASHBOARD, CUSTOM;
+  
+  @Override
+  public String toString() {
+    return super.toString().toLowerCase();
+  }
 }
