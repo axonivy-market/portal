05669c7bd09fc73dd70d55105e0670edae4d2b13
Long Do <long.do@axonactive.com>
2018-11-05 10:01:32 +0700

    IVYPORTAL-6293 Implement mobile recognition, Login and Portal Header: + Improved: detect mobile with resolution less than 992

--- AxonIvyPortal/PortalTemplate/src/ch/ivy/addon/portal/generic/common/DeviceDetector.java
+++ AxonIvyPortal/PortalTemplate/src/ch/ivy/addon/portal/generic/common/DeviceDetector.java
@@ -1,11 +1,15 @@
 package ch.ivy.addon.portal.generic.common;
 
+import java.awt.Toolkit;
+
 import org.apache.commons.lang3.StringUtils;
 
 import ch.ivyteam.ivy.environment.Ivy;
 import ch.ivyteam.ivy.request.IHttpRequest;
 
 public class DeviceDetector {
+
+  private static final int TABLET_WIDTH = 992;
 
   public static DeviceDetector newInstance() {
     return new DeviceDetector();
@@ -14,7 +18,9 @@ public class DeviceDetector {
   public boolean isMobile() {
     if (Ivy.request() instanceof IHttpRequest) {
         IHttpRequest httpRequest = (IHttpRequest) Ivy.request();
-        return StringUtils.containsIgnoreCase(httpRequest.getHttpServletRequest().getHeader("User-Agent"), "mobile");
+        boolean isMobileMode = StringUtils.containsIgnoreCase(httpRequest.getHttpServletRequest().getHeader("User-Agent"), "mobile");
+        boolean isLessThanTabletResolution = Toolkit.getDefaultToolkit().getScreenSize().getWidth() < TABLET_WIDTH;
+        return isMobileMode && isLessThanTabletResolution;
     }
     return false;
   }
