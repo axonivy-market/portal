a9546f29dbf62360814accf5fc52924b3022a1e0
Long Do <long.do@axonactive.com>
2018-11-13 14:51:27 +0700

    IVYPORTAL-6293 Implement mobile recognition, Login and Portal Header: + Introduce the switch button/link between mobile and desktop version

--- AxonIvyPortal/PortalTemplate/src/ch/ivy/addon/portal/generic/bean/DeviceBean.java
+++ AxonIvyPortal/PortalTemplate/src/ch/ivy/addon/portal/generic/bean/DeviceBean.java
@@ -1,15 +1,16 @@
 package ch.ivy.addon.portal.generic.bean;
 
-import javax.faces.bean.ApplicationScoped;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
 
 import ch.ivy.addon.portal.generic.common.DeviceDetector;
+import ch.ivyteam.ivy.environment.Ivy;
 
 @ManagedBean
-@ApplicationScoped
+@SessionScoped
 public class DeviceBean {
 
   public boolean isMobile() {
-    return DeviceDetector.newInstance().isMobile();
+    return DeviceDetector.instance().isMobile(Ivy.session().getIdentifier());
   }
 }
