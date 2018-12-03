8289666dcf097a301a4139d81bf3bf9e6e98039d
Nam Le Dinh <nam.ledinh@axonactive.com>
2018-12-03 13:51:54 +0700

    IVYPORTAL-6292: Implement mobile ProcessList - GUITest for process list
    Former-commit-id: 77051fed4edf1a0289500588f5bf20021cc205fe

--- AxonIvyPortal/PortalMobileTest/src_test/ch/ivyteam/ivy/project/portal/guitest/mobile/page/MobileTemplatePage.java
+++ AxonIvyPortal/PortalMobileTest/src_test/ch/ivyteam/ivy/project/portal/guitest/mobile/page/MobileTemplatePage.java
@@ -1,15 +1,26 @@
 package ch.ivyteam.ivy.project.portal.guitest.mobile.page;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
 import ch.ivyteam.ivy.project.portal.guitest.mobile.common.MobileAbstractPage;
 
 public class MobileTemplatePage extends MobileAbstractPage {
-  private static final String TEMPLATE_PAGE_LOCATOR = "id('logo')";
-
+  public static final String TEMPLATE_PAGE_LOCATOR = "id('logo')";
+  public static final String PROCESS_TAB_ID = "mobile-menu:mobile-menu-form:mobile-process-menu-item";
+  public static final String TASK_TAB_ID = "mobile-menu:mobile-menu-form:mobile-task-menu-item";
+  
   @Override
   protected String getLoadedLocator() {
     return TEMPLATE_PAGE_LOCATOR;
+  }
+  
+  public MobileTemplatePage() {
+    waitForLocatorDisplayed(getLoadedLocator());
+  }
+  
+  protected void waitForLocatorDisplayed(String locator) {
+    waitForElementDisplayed(locator, true, DEFAULT_TIMEOUT);
   }
   
   public void clickOnLogo() {
@@ -35,5 +46,23 @@ public class MobileTemplatePage extends MobileAbstractPage {
     if (displayed) {
       waitForElementDisplayed(ajaxIndicatorStartState, false, DEFAULT_TIMEOUT);
     }
+  }
+  
+  public MobileProcessPage openProcessTab() {
+    click(By.id(PROCESS_TAB_ID));
+    return new MobileProcessPage();
+  }
+  
+  public MobileTaskPage openTaskTab() {
+    click(By.id(TASK_TAB_ID));
+    return new MobileTaskPage();
+  }
+  
+  public WebElement getProcessTab() {
+    return findElementById(PROCESS_TAB_ID);
+  }
+  
+  public WebElement getTaskTab() {
+    return findElementById(TASK_TAB_ID);
   }
 }
