a46480b16d8b41ab6268e4614fe27f979cdef30d
ttthai_wawa <hai.tran@axonacitve.com>
2018-12-04 11:37:00 +0700

    IVYPORTAL-6387: Various or urgent requests in IVYPORTAL S165 - Handle email Remove warn log
    Former-commit-id: de93b39203680a1763bf3cb3b2e43c9d2972673a

--- AxonIvyPortal/PortalKit/src/ch/ivy/addon/portalkit/support/UrlDetector.java
+++ AxonIvyPortal/PortalKit/src/ch/ivy/addon/portalkit/support/UrlDetector.java
@@ -87,7 +87,6 @@ public class UrlDetector {
   
   public String getBaseURLWithoutContextPath(final HttpServletRequest request) throws MalformedURLException {
     URL url = new URL(request.getScheme(), request.getServerName(), request.getServerPort(), request.getContextPath());
-    Ivy.log().warn(url.getPath());
     return url.toString().substring(0, url.toString().lastIndexOf(url.getPath()));
   }
 
