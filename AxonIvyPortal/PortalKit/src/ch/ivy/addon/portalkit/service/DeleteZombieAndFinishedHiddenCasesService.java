8c1101b951a10f78b30e85a6331c966b02d334c6
Nguyen To <nguyen.to@axonactive.com>
2018-12-24 09:10:34 +0700

    IVYPORTAL-6536: Handle performance & fix for standard Portal II - Handle feedback: delete cases generated by Portal

--- AxonIvyPortal/PortalKit/src/ch/ivy/addon/portalkit/service/DeleteZombieAndFinishedHiddenCasesService.java
+++ AxonIvyPortal/PortalKit/src/ch/ivy/addon/portalkit/service/DeleteZombieAndFinishedHiddenCasesService.java
@@ -1,9 +1,10 @@
 package ch.ivy.addon.portalkit.service;
 
+import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
-import ch.ivy.addon.portalkit.constant.PortalConstants;
 import ch.ivy.addon.portalkit.enums.GlobalVariable;
 import ch.ivyteam.ivy.environment.Ivy;
 import ch.ivyteam.ivy.persistence.OrderDirection;
@@ -15,6 +16,12 @@ import ch.ivyteam.ivy.workflow.PropertyOrder;
 import ch.ivyteam.logicalexpression.RelationalOperator;
 
 public class DeleteZombieAndFinishedHiddenCasesService {
+  
+  private static final String SYNC_USER_QUATZ_JOB_CASE_NAME = "eventStarterbyQuatzScheduler";
+  private static final String PORTAL_CATEGORY = Ivy.cms().coLocale("/Processes/Cases/PortalCategory", Locale.ENGLISH);
+  private static final String PORTAL_SYNC_DATA_CASE_NAME = Ivy.cms().coLocale("/Processes/Cases/SynchronizeDataProcess/SynchronizeDataCaseName", Locale.ENGLISH);
+  private static final String PORTAL_SYNC_SERVER_CONFIG_CASE_NAME = Ivy.cms().coLocale("/Processes/Cases/SynchronizeServerProcess/SynchronizeServerConfigurationCaseName", Locale.ENGLISH);
+  private static final String PORTAL_SYNC_CASE_NAME_FORMAT = "%s: %s";
 
   public void deleteZombieAndFinishedHiddenCases() {
     Date currentDate = new java.util.Date();
@@ -33,13 +40,16 @@ public class DeleteZombieAndFinishedHiddenCasesService {
 
     int numOfDeletedCases = 0;
     GlobalSettingService globalSettingService = new GlobalSettingService();
-    boolean shouldDeleteCasesInAllApps = Boolean.parseBoolean(globalSettingService.findGlobalSettingValue(GlobalVariable.DELETE_ZOMBIE_AND_FINISHED_HIDDEN_CASES_IN_ALL_APPS.toString()));
+    boolean shouldDeleteAllCases = Boolean.parseBoolean(globalSettingService.findGlobalSettingValue(GlobalVariable.DELETE_ALL_ZOMBIE_AND_FINISHED_HIDDEN_CASES.toString()));
     for (int i = cases.size()-1; i >= 0; i--) {
       ICase iCase = (cases.get(i));
       boolean isHiddenCase = ("HIDE").equals(iCase.getAdditionalProperty("HIDE"));
       boolean isZombieCase = CaseState.ZOMBIE.equals(iCase.getState());
-      boolean isApplicationNamePortal = PortalConstants.PORTAL_APPLICATION_NAME.equals(iCase.getApplication().getName());
-      if ((isZombieCase || isHiddenCase) && (shouldDeleteCasesInAllApps ? true : isApplicationNamePortal)) {
+      String portalSyncDataCaseName = String.format(PORTAL_SYNC_CASE_NAME_FORMAT, PORTAL_CATEGORY, PORTAL_SYNC_DATA_CASE_NAME);
+      String portalSyncServerConfigCaseName = String.format(PORTAL_SYNC_CASE_NAME_FORMAT, PORTAL_CATEGORY, PORTAL_SYNC_SERVER_CONFIG_CASE_NAME);
+      List<String> portalSynchronizationCaseNames = Arrays.asList(SYNC_USER_QUATZ_JOB_CASE_NAME, portalSyncDataCaseName, portalSyncServerConfigCaseName);
+      boolean isCaseCreatedByPortal = portalSynchronizationCaseNames.contains(iCase.getName());
+      if ((isZombieCase || isHiddenCase) && (shouldDeleteAllCases ? true : isCaseCreatedByPortal)) {
         try {
           Ivy.wf().deleteCompletedCase(iCase);
           Ivy.log().info("Case was deleted with id: " + iCase.getId());
