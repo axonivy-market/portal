d7fedb1a3ceb3d08e8a8d2896432f4ddd6f173f5
nam.chu <nam.chu@axonactive.com>
2018-12-24 17:51:43 +0700

    IVYPORTAL-5692 Improve Portal performance - Case List - Apply cache to application level

--- AxonIvyPortal/PortalKit/src/ch/ivy/addon/portalkit/support/DataCache.java
+++ AxonIvyPortal/PortalKit/src/ch/ivy/addon/portalkit/support/DataCache.java
@@ -1,28 +1,52 @@
 package ch.ivy.addon.portalkit.support;
 
+import java.util.List;
+
+import ch.ivy.addon.portalkit.persistence.domain.Application;
+import ch.ivy.addon.portalkit.service.ApplicationService;
 import ch.ivy.addon.portalkit.service.GlobalSettingService;
+import ch.ivyteam.ivy.application.IApplication;
+import ch.ivyteam.ivy.data.cache.IDataCache;
 import ch.ivyteam.ivy.data.cache.IDataCacheEntry;
 import ch.ivyteam.ivy.data.cache.IDataCacheGroup;
 import ch.ivyteam.ivy.environment.Ivy;
+import ch.ivyteam.ivy.server.ServerFactory;
 
 public final class DataCache {
-  public static final String GLOBAL_SETTING_GROUP = "Global Settings";
+  public static final String GLOBAL_SETTING_GROUP = "Global Settings Group";
+  public static final String LOGOUT_PAGE_GROUP = "Logout Page Group";
+  public static final String LOGOUT_PAGE_INDENTIFIER = "Logout Page Id";
   
   public static Object getGlobalSettingFromCache(String attributeName){
-    final IDataCacheEntry entry = Ivy.datacache().getSessionCache().getEntry(DataCache.GLOBAL_SETTING_GROUP, attributeName);
+    IDataCacheEntry entry = Ivy.datacache().getAppCache().getEntry(GLOBAL_SETTING_GROUP, attributeName);
     return entry == null ? null : entry.getValue();
   }
   
   public static void cacheGlobalSetting(String name, Object value){
-    Ivy.datacache().getSessionCache().setEntry(GLOBAL_SETTING_GROUP, name, value);
+    Ivy.datacache().getAppCache().setEntry(GLOBAL_SETTING_GROUP, name, value);
   }
   
   public static void invalidateGlobalSettingCache(){
-    IDataCacheGroup wsGroupName = Ivy.datacache().getSessionCache().getGroup(GLOBAL_SETTING_GROUP);
-    if (wsGroupName != null){
-      Ivy.log().debug("CLEAR GLOBAL SETTING CACHE");
-      Ivy.datacache().getSessionCache().invalidateGroup(wsGroupName );
-    }
+    ApplicationService service = new ApplicationService();
+    final List<Application> findAllIvyApplications = service.findAllIvyApplications();
+    findAllIvyApplications.forEach(application -> {
+      try {
+        ServerFactory.getServer().getSecurityManager().executeAsSystem(() ->{
+          IApplication findApplication = ServerFactory.getServer().getApplicationConfigurationManager().findApplication(application.getName());
+          IDataCache cache = (IDataCache) findApplication .getAdapter(IDataCache.class);
+          IDataCacheGroup wsGroupName = cache.getGroup(GLOBAL_SETTING_GROUP);
+          if (wsGroupName != null){
+            Ivy.log().debug("CLEAR GLOBAL SETTING CACHE");
+            Ivy.datacache().getAppCache().invalidateGroup(wsGroupName );
+          }
+          return null;
+        });
+      } catch (Exception e) {
+        // TODO Auto-generated catch block
+        Ivy.log().error(e);
+      }
+      
+    });
   }
   
   public static Object getGlobalSettingValue(String attributeName){
