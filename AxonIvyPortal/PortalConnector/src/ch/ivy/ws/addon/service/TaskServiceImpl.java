3dfafc6bc6df160ed3c946299070dbdda83232c0
nam.chu <nam.chu@axonactive.com>
2019-01-04 11:56:12 +0700

    IVYPORTAL-6540 Hide technical Stuff - Optimization - Change query

--- AxonIvyPortal/PortalConnector/src/ch/ivy/ws/addon/service/TaskServiceImpl.java
+++ AxonIvyPortal/PortalConnector/src/ch/ivy/ws/addon/service/TaskServiceImpl.java
@@ -887,7 +887,7 @@ public class TaskServiceImpl extends AbstractService implements ITaskService {
   private void queryExcludeHiddenTasks(TaskQuery query, List<String> apps) {
     if (isHiddenTasksCasesExcluded(apps)){
       Ivy.log().error("EXTEND TASK QUERY WITH CUSTOM VARCHAR HIDE");
-      query.where().or().customVarCharField5().isNull().or().customVarCharField5().isNotEqual("HIDE");//additionalProperty("HIDE").isNull();
+      query.where().or().customVarCharField5().isNull();//additionalProperty("HIDE").isNull();
     }
   }
 
