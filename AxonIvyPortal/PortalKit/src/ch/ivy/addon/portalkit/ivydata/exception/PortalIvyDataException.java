b668e7b71846a22287e048a1fc9ec44e6dccbcea
Long Do <long.do@axonactive.com>
2019-01-14 16:42:00 +0700

    IVYPORTAL-6674 Remove portal connector III: + Added exception handle in service

--- AxonIvyPortal/PortalKit/src/ch/ivy/addon/portalkit/ivydata/exception/PortalIvyDataException.java
+++ AxonIvyPortal/PortalKit/src/ch/ivy/addon/portalkit/ivydata/exception/PortalIvyDataException.java
@@ -1,5 +1,13 @@
 package ch.ivy.addon.portalkit.ivydata.exception;
 
+import java.util.Optional;
+
+import org.apache.commons.lang3.StringUtils;
+
+import ch.ivyteam.ivy.application.IApplication;
+import ch.ivyteam.ivy.environment.Ivy;
+import ch.ivyteam.ivy.request.IProcessModelVersionRequest;
+
 public class PortalIvyDataException extends Exception {
 
   private static final long serialVersionUID = -8558165268719107465L;
@@ -7,12 +15,22 @@ public class PortalIvyDataException extends Exception {
   private String appName;
   private String userText;
   
+  public PortalIvyDataException(String userText) {
+    this.userText = userText;
+  }
+  
   public PortalIvyDataException(String appName, String userText) {
     this.appName = appName;
     this.userText = userText;
   }
   
   public String getAppName() {
+    if (StringUtils.isBlank(appName)) {
+      appName = Optional.ofNullable(Ivy.request())
+          .map(IProcessModelVersionRequest::getApplication)
+          .map(IApplication::getName)
+          .orElse(StringUtils.EMPTY);
+    }
     return appName;
   }
   
