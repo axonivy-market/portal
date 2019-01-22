4727b28c05679b51116da7e21a69a5b6554e8f68
Long Do <long.do@axonactive.com>
2019-01-22 14:29:57 +0700

    IVYPORTAL-6819 Remove portal connector IV: + Removed PortalConnector and unused showcase projects
    Former-commit-id: fb97edb9c9d35f4728a8d336612fb3e86775dc70

--- AxonIvyPortal/PortalConnector/src/ch/ivy/ws/addon/service/WebStartableSearchCriteria.java
+++
@@ -1,6 +0,0 @@
-package ch.ivy.ws.addon.service;
-
-
-public class WebStartableSearchCriteria extends AbstractSearchCriteria {
-
-}
--- AxonIvyPortal/PortalConnector/src/ch/ivy/ws/addon/util/IvyDates.java
+++
@@ -1,8 +0,0 @@
-package ch.ivy.ws.addon.util;
-
-
-public class IvyDates {
-	public ch.ivyteam.ivy.scripting.objects.Date getToday(){
-		return new ch.ivyteam.ivy.scripting.objects.Date();
-	}
-}
