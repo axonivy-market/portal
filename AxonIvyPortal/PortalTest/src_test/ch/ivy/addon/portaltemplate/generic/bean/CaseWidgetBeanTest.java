418acaf84cd82c624526e75525da0be8dda61367
nam.chu <nam.chu@axonactive.com>
2019-01-23 15:35:50 +0700

    IVYPORTAL-6819 Remove portal connector IV - Fix test

--- AxonIvyPortal/PortalTest/src_test/ch/ivy/addon/portaltemplate/generic/bean/CaseWidgetBeanTest.java
+++ AxonIvyPortal/PortalTest/src_test/ch/ivy/addon/portaltemplate/generic/bean/CaseWidgetBeanTest.java
@@ -1,12 +1,15 @@
 package ch.ivy.addon.portaltemplate.generic.bean;
 
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+
 import org.apache.commons.lang3.StringUtils;
-import org.junit.AfterClass;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Matchers;
+import org.mockito.Mock;
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
@@ -14,28 +17,30 @@ import org.powermock.modules.junit4.PowerMockRunner;
 import ch.ivy.addon.portalkit.bean.CaseWidgetBean;
 import ch.ivy.addon.portalkit.enums.AdditionalProperty;
 import ch.ivy.addon.portalkit.util.CaseUtils;
+import ch.ivy.addon.portalkit.util.PermissionUtils;
 import ch.ivyteam.ivy.workflow.ICase;
 
-@SuppressWarnings("unchecked")
 @RunWith(PowerMockRunner.class)
 public class CaseWidgetBeanTest {
   private static final String CUSTOMIZATION_ADDITIONAL_CASE_DETAILS_PAGE = "CustomizationAdditionalCaseDetailsPage";
   private static final String DEFAULT_ADDITIONAL_CASE_DETAILS_PAGE = "DefaultAdditionalCaseDetailsPage";
 
   CaseWidgetBean caseWidgetBean;
+  
+  @Mock
+  private FacesContext facesContext;
+  @Mock
+  private ExternalContext externalContext;
 
   @Before
+  @PrepareForTest(PermissionUtils.class)
   public void init() {
+    PowerMockito.mockStatic(PermissionUtils.class);
     caseWidgetBean = new CaseWidgetBean();
   }
   
-  @AfterClass
-  public static void cleanUp() {
-
-  }
-
   @Test
-  @PrepareForTest({CaseWidgetBean.class, CaseUtils.class})
+  @PrepareForTest({CaseWidgetBean.class, CaseUtils.class, PermissionUtils.class, FacesContext.class})
   public void testGetAdditionalCaseDetailsPageUri() throws Exception {
     PowerMockito.mockStatic(CaseUtils.class);
     
@@ -43,15 +48,28 @@ public class CaseWidgetBeanTest {
     PowerMockito.when(iCase.getAdditionalProperty(AdditionalProperty.CUSTOMIZATION_ADDITIONAL_CASE_DETAILS_PAGE.toString())).thenReturn(StringUtils.EMPTY);
     
     PowerMockito.when(CaseUtils.getProcessStartUriWithCaseParameters(Matchers.any(ICase.class), Matchers.anyString())).thenReturn(DEFAULT_ADDITIONAL_CASE_DETAILS_PAGE);
+    
+    PowerMockito.mockStatic(FacesContext.class);
+
+    PowerMockito.when(FacesContext.getCurrentInstance()).thenReturn(facesContext);
+    PowerMockito.when(facesContext.getExternalContext())
+        .thenReturn(externalContext);
+    PowerMockito.when(externalContext.getApplicationContextPath()).thenReturn("");
+    
     Assert.assertEquals(DEFAULT_ADDITIONAL_CASE_DETAILS_PAGE, caseWidgetBean.getAdditionalCaseDetailsPageUri(iCase)); 
   }
   
   @Test
-  @PrepareForTest({CaseWidgetBean.class})
+  @PrepareForTest({CaseWidgetBean.class, PermissionUtils.class, FacesContext.class})
   public void testGetAdditionalCaseDetailsPageUriWithCustomization() throws Exception {
     ICase iCase = PowerMockito.mock(ICase.class);
     PowerMockito.when(iCase.getAdditionalProperty(AdditionalProperty.CUSTOMIZATION_ADDITIONAL_CASE_DETAILS_PAGE.toString())).thenReturn(CUSTOMIZATION_ADDITIONAL_CASE_DETAILS_PAGE);
-    
+    PowerMockito.mockStatic(FacesContext.class);
+
+    PowerMockito.when(FacesContext.getCurrentInstance()).thenReturn(facesContext);
+    PowerMockito.when(facesContext.getExternalContext())
+        .thenReturn(externalContext);
+    PowerMockito.when(externalContext.getApplicationContextPath()).thenReturn("");
     Assert.assertEquals(CUSTOMIZATION_ADDITIONAL_CASE_DETAILS_PAGE, caseWidgetBean.getAdditionalCaseDetailsPageUri(iCase)); 
   }
   
