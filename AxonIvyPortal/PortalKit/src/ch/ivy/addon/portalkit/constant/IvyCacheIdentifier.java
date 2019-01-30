af923348fae2768c348e48c9011f5d3eefe39168
Long Do <long.do@axonactive.com>
2019-01-30 16:25:04 +0700

    IVYPORTAL-6819 Remove portal connector IV: + Fixed bugs

--- AxonIvyPortal/PortalKit/src/ch/ivy/addon/portalkit/constant/IvyCacheIdentifier.java
+++ AxonIvyPortal/PortalKit/src/ch/ivy/addon/portalkit/constant/IvyCacheIdentifier.java
@@ -4,7 +4,6 @@ public final class IvyCacheIdentifier {
   
   public static final String USERS_IN_APPLICATION = "USERS_IN_APPLICATION";
   public static final String ROLES_IN_APPLICATION = "ROLES_IN_APPLICATION";
-  public static final String ONLINE_APPLICATIONS = "ONLINE_APPLICATIONS";
   public static final String ONLINE_APPLICATIONS_BASED_ON_CONFIGURATION = "ONLINE_APPLICATIONS_BASED_ON_CONFIGURATION";
   public static final String ONLINE_APPLICATIONS_USER_CAN_WORK_ON = "ONLINE_APPLICATIONS_USER_CAN_WORK_ON";
   
