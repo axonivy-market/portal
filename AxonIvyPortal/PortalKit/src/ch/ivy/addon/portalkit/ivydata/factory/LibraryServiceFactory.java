18b62305d11507a2b0cecdb6ea7e04e48ad999da
Long Do <long.do@axonactive.com>
2019-01-31 09:27:27 +0700

    IVYPORTAL-6819 Remove portal connector IV: + Fixed bugs

--- AxonIvyPortal/PortalKit/src/ch/ivy/addon/portalkit/ivydata/factory/LibraryServiceFactory.java
+++ AxonIvyPortal/PortalKit/src/ch/ivy/addon/portalkit/ivydata/factory/LibraryServiceFactory.java
@@ -1,13 +1,27 @@
 package ch.ivy.addon.portalkit.ivydata.factory;
 
+import org.apache.commons.collections4.CollectionUtils;
+
+import ch.ivy.addon.portalkit.constant.PortalConstants;
 import ch.ivy.addon.portalkit.ivydata.service.ILibraryService;
+import ch.ivy.addon.portalkit.ivydata.service.impl.MultiAppLibraryService;
 import ch.ivy.addon.portalkit.ivydata.service.impl.NoRegisteredAppLibraryService;
+import ch.ivy.addon.portalkit.ivydata.service.impl.SingleAppLibraryService;
+import ch.ivy.addon.portalkit.service.ApplicationService;
+import ch.ivyteam.ivy.environment.Ivy;
 
 public class LibraryServiceFactory {
 
   private LibraryServiceFactory() {}
 
   public static ILibraryService getLibraryService() {
+    ApplicationService applicationService = new ApplicationService();
+    if (CollectionUtils.isNotEmpty(applicationService.findAllIvyApplications())) {
+      if (Ivy.request().getApplication().getName().equals(PortalConstants.PORTAL_APPLICATION_NAME)) {
+        return new MultiAppLibraryService();
+      }
+      return new SingleAppLibraryService();
+    }
     return new NoRegisteredAppLibraryService();
   }
 }
