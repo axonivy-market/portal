a60121ac0293f7d50ae91e87be54333010b1b96e
nam.chu <nam.chu@axonactive.com>
2019-02-21 18:45:03 +0700

    IVYPORTAL-6682 Merge relevant Performance issues to Trunk - Lazy load project info - Cache some application service
    Former-commit-id: 2cd9e9e23f196e27f865dab7312e06628b90259a

--- AxonIvyPortal/PortalKit/src/ch/ivy/addon/portalkit/constant/IvyCacheIdentifier.java
+++ AxonIvyPortal/PortalKit/src/ch/ivy/addon/portalkit/constant/IvyCacheIdentifier.java
@@ -1,22 +1,17 @@
 package ch.ivy.addon.portalkit.constant;
 
 public final class IvyCacheIdentifier {
+  private IvyCacheIdentifier() {}
   
   public static final String USERS_IN_APPLICATION = "USERS_IN_APPLICATION";
   public static final String ROLES_IN_APPLICATION = "ROLES_IN_APPLICATION";
   public static final String ONLINE_APPLICATIONS_BASED_ON_CONFIGURATION = "ONLINE_APPLICATIONS_BASED_ON_CONFIGURATION";
   public static final String ONLINE_APPLICATIONS_USER_CAN_WORK_ON = "ONLINE_APPLICATIONS_USER_CAN_WORK_ON";
-  
-  public static final String PORTAL_USERS_CACHE_GROUP_NAME = "PORTAL_CACHE";
-  public static final String USERS_LIST_CACHE_ENTRY_NAME = "USERS_LIST";
-  public static final String USERS_REPO_CACHE_ENTRY_NAME = "USERS_REPO";
-  
-  // cache for setting, logout page
   public static final String GLOBAL_SETTING_CACHE_GROUP_NAME = "GLOBAL_SETTINGS_GROUP";
-  
   public static final String LOGOUT_PAGE_CACHE_GROUP_NAME = "LOGOUT_PAGE_GROUP";
   public static final String LOGOUT_PAGE_CACHE_ENTRY_NAME = "LOGOUT_PAGE_ID";
+  public static final String ALL_IVY_AND_THIRD_PARTY_APPLICATIONS = "ALL_IVY_AND_THIRD_PARTY_APPLICATIONS";
+  public static final String ALL_IVY_APPLICATIONS = "ALL_IVY_APPLICATIONS";
+  public static final String ALL_THIRD_PARTY_APPLICATIONS = "ALL_IVY_APPLICATIONS";
   
-  private IvyCacheIdentifier() {
-  }
 }
