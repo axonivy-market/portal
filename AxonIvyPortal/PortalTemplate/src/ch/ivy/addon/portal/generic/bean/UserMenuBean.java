7c0e18749e6abc1d6242a07875938be0b1a56bdb
nam.chu <nam.chu@axonactive.com>
2019-02-25 16:19:06 +0700

    IVYPORTAL-6682 Merge relevant Performance issues to Trunk - Handle feedback

--- AxonIvyPortal/PortalTemplate/src/ch/ivy/addon/portal/generic/bean/UserMenuBean.java
+++ AxonIvyPortal/PortalTemplate/src/ch/ivy/addon/portal/generic/bean/UserMenuBean.java
@@ -7,12 +7,14 @@ import java.util.Arrays;
 import java.util.Map;
 import java.util.Optional;
 
+import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
 
 import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.lang3.BooleanUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.primefaces.context.RequestContext;
 
@@ -22,9 +24,10 @@ import ch.ivy.addon.portalkit.constant.PortalConstants;
 import ch.ivy.addon.portalkit.enums.GlobalVariable;
 import ch.ivy.addon.portalkit.enums.PortalLibrary;
 import ch.ivy.addon.portalkit.persistence.domain.Application;
-import ch.ivy.addon.portalkit.service.RegisteredApplicationService;
 import ch.ivy.addon.portalkit.service.GlobalSettingService;
 import ch.ivy.addon.portalkit.service.IvyAdapterService;
+import ch.ivy.addon.portalkit.service.IvyCacheService;
+import ch.ivy.addon.portalkit.service.RegisteredApplicationService;
 import ch.ivy.addon.portalkit.util.SecurityServiceUtils;
 import ch.ivy.addon.portalkit.util.TaskUtils;
 import ch.ivyteam.ivy.environment.Ivy;
@@ -36,69 +39,70 @@ import ch.ivyteam.ivy.system.ISystemProperty;
 public class UserMenuBean implements Serializable {
   private static final long serialVersionUID = 1L;
 
-  private String userName;
   public static final int MINUTE_TO_SECOND = 60;
   public static final int SECONND_TO_MILLISECOND = 1000;
   public static final int TIME_BEFORE_LOST_SESSION = 180000; // 3 minutes
+  
+  private String userName;
+  GlobalSettingService globalSettingService;
 
   public String getUserName() {
-    userName = Ivy.session().getSessionUserName();
     return userName;
+  }
+  
+  @PostConstruct
+  public void init() {
+    userName = Ivy.session().getSessionUserName();
+    globalSettingService = new GlobalSettingService();
   }
 
   public boolean isShowServerInformation() {
-    GlobalSettingService globalSettingService = new GlobalSettingService();
-    String isShowServerInformation = globalSettingService.findGlobalSettingValue(GlobalVariable.SHOW_ENVIRONMENT_INFO.toString());
-    return Boolean.parseBoolean(isShowServerInformation);
+    return Boolean.parseBoolean(globalSettingService.findGlobalSettingValue(GlobalVariable.SHOW_ENVIRONMENT_INFO.toString()));
   }
 
   public boolean isHiddenLogout() {
-    GlobalSettingService globalSettingService = new GlobalSettingService();
-    String isHiddenLogout = globalSettingService.findGlobalSettingValue(GlobalVariable.HIDE_LOGOUT_BUTTON.toString());
-    return Boolean.parseBoolean(isHiddenLogout);
+    return Boolean.parseBoolean(globalSettingService.findGlobalSettingValue(GlobalVariable.HIDE_LOGOUT_BUTTON.toString()));
   }
 
   public boolean isHiddenChangePassword() {
-    GlobalSettingService globalSettingService = new GlobalSettingService();
-    String isHiddenChangePassword =
-        globalSettingService.findGlobalSettingValue(GlobalVariable.HIDE_CHANGE_PASSWORD_BUTTON.toString());
-    return Boolean.parseBoolean(isHiddenChangePassword);
+    return Boolean.parseBoolean(globalSettingService.findGlobalSettingValue(GlobalVariable.HIDE_CHANGE_PASSWORD_BUTTON.toString()));
   }
 
   public boolean isHiddenStatisticWidget() {
-    GlobalSettingService globalSettingService = new GlobalSettingService();
-    String isHideStatisticWidget = globalSettingService.findGlobalSettingValue(GlobalVariable.HIDE_STATISTIC_WIDGET.toString());
-	return StringUtils.isNotBlank(isHideStatisticWidget) ? Boolean.parseBoolean(isHideStatisticWidget) : false;
+    return BooleanUtils.toBoolean(globalSettingService.findGlobalSettingValue(GlobalVariable.HIDE_STATISTIC_WIDGET.toString()));
   }
 
   public int getClientSideTimeout() {
-    GlobalSettingService globalSettingService = new GlobalSettingService();
     String clientSideTimeoutInMinute = globalSettingService.findGlobalSettingValue(GlobalVariable.CLIENT_SIDE_TIMEOUT.toString());
-    if (clientSideTimeoutInMinute != null && !clientSideTimeoutInMinute.isEmpty()) {
+    if (StringUtils.isNotBlank(clientSideTimeoutInMinute)) {
       return Integer.valueOf(clientSideTimeoutInMinute) * MINUTE_TO_SECOND * SECONND_TO_MILLISECOND;
     }
     return getDefaultClientSideTimeout();
   }
 
-  private int getDefaultClientSideTimeout() {
-    ExternalContext externalContext = FacesContext.getCurrentInstance().getExternalContext();
-    int serverSideTimeOutInMillisecond = externalContext.getSessionMaxInactiveInterval() * SECONND_TO_MILLISECOND;
-    return serverSideTimeOutInMillisecond - TIME_BEFORE_LOST_SESSION;
+  public void switchDesktopOrMobileVersion(boolean isMobile) throws IOException {
+    String url = String.format("%s?isMobile=%s", getHomePageURL(), isMobile);
+    getExternalContext().redirect(url);
+    DeviceDetector.instance().updateVersionState(Ivy.session().getHttpSessionIdentifier(), isMobile);
   }
 
   public String getLogoutPage() throws MalformedURLException {
-    Map<String, Object> response =
-        IvyAdapterService.startSubProcess("getLogoutPage()", null,
-            Arrays.asList(PortalLibrary.PORTAL_TEMPLATE.getValue()));
-    String logoutPage = (String) response.get("logoutPage");
-    return StringUtils.isNotBlank(logoutPage) ? logoutPage : getHomePageURL();
+    IvyCacheService cacheService = IvyCacheService.newInstance();
+    String logoutPageUrl = cacheService.getLogoutPageFromCache();
+    if (StringUtils.isNotBlank(logoutPageUrl)){
+      return logoutPageUrl;
+    }
+    String customizedlogoutPage = getCustomizedLogoutPage();
+    String logoutPage = StringUtils.isNotBlank(customizedlogoutPage) ? customizedlogoutPage : getHomePageURL();
+    cacheService.cacheLogoutPage(logoutPage);
+    return logoutPage;
   }
 
   public String getHomePageURL() throws MalformedURLException {
     RegisteredApplicationService applicationService = new RegisteredApplicationService();
-    String homePageURL = getHomePageFromSetting();
+    String homePageURL = globalSettingService.findGlobalSettingValue(GlobalVariable.HOMEPAGE_URL.toString());
     if (CollectionUtils.isEmpty(applicationService.findAllIvyApplications())) {
-      if (!StringUtils.isEmpty(homePageURL)) {
+      if (StringUtils.isNotBlank(homePageURL)) {
         return homePageURL;
       } else {
         return new PortalNavigator().getPortalStartUrl();
@@ -112,9 +116,10 @@ public class UserMenuBean implements Serializable {
       return SecurityServiceUtils.getDefaultPortalStartUrl();
     }
 
-    Application selectedApplication =
-        applicationService.findByDisplayNameAndName(selectedAppDisplayName, selectedApp);
-    return Optional.ofNullable(selectedApplication).map(Application::getLink).orElse(StringUtils.EMPTY);
+    Application selectedApplication = applicationService.findByDisplayNameAndName(selectedAppDisplayName, selectedApp);
+    return Optional.ofNullable(selectedApplication)
+        .map(Application::getLink)
+        .orElse(StringUtils.EMPTY);
   }
 
   public void navigateToHomePageOrDisplayWorkingTaskWarning(boolean isWorkingOnATask) throws IOException {
@@ -134,20 +139,7 @@ public class UserMenuBean implements Serializable {
     TaskUtils.parkTask(Ivy.wfTask());
     navigateToHomePage();
   }
-
-  private void navigateToHomePage() throws IOException {
-    FacesContext.getCurrentInstance().getExternalContext().redirect(getHomePageURL());
-  }
-
-  private String getHomePageFromSetting() {
-    GlobalSettingService globalSettingService = new GlobalSettingService();
-    return globalSettingService.findGlobalSettingValue(GlobalVariable.HOMEPAGE_URL.toString());
-  }
-
-  private boolean isDefaultPortalApp() {
-    return PortalConstants.PORTAL_APPLICATION_NAME.equals(Ivy.wf().getApplication().getName());
-  }
-
+  
   public boolean getErrorDetailToEndUser() {
     try {
       return ServerFactory.getServer().getSecurityManager().executeAsSystem(this::findShowErrorDetailSystemProperty);
@@ -157,16 +149,33 @@ public class UserMenuBean implements Serializable {
     return true;
   }
 
+  private void navigateToHomePage() throws IOException {
+    getExternalContext().redirect(getHomePageURL());
+  }
+
+  private boolean isDefaultPortalApp() {
+    return PortalConstants.PORTAL_APPLICATION_NAME.equals(Ivy.wf().getApplication().getName());
+  }
+
   private boolean findShowErrorDetailSystemProperty() {
     ISystemProperty systemProp =
         ServerFactory.getServer().getApplicationConfigurationManager().getSystemProp("Errors.ShowDetailsToEndUser");
     return systemProp.getBooleanValue();
   }
   
-  public void switchDesktopOrMobileVersion(boolean isMobile) throws IOException {
-    String url = String.format("%s?isMobile=%s", getHomePageURL(), isMobile);
-    FacesContext context = FacesContext.getCurrentInstance();
-    context.getExternalContext().redirect(url);
-    DeviceDetector.instance().updateVersionState(Ivy.session().getHttpSessionIdentifier(), isMobile);
+  private int getDefaultClientSideTimeout() {
+    ExternalContext externalContext = getExternalContext();
+    int serverSideTimeOutInMillisecond = externalContext.getSessionMaxInactiveInterval() * SECONND_TO_MILLISECOND;
+    return serverSideTimeOutInMillisecond - TIME_BEFORE_LOST_SESSION;
+  }
+
+  private ExternalContext getExternalContext() {
+    return FacesContext.getCurrentInstance().getExternalContext();
+  }
+  
+  private String getCustomizedLogoutPage() {
+    Map<String, Object> response = IvyAdapterService.startSubProcess("getLogoutPage()", null,
+            Arrays.asList(PortalLibrary.PORTAL_TEMPLATE.getValue()));
+    return (String) response.get("logoutPage");
   }
 }
