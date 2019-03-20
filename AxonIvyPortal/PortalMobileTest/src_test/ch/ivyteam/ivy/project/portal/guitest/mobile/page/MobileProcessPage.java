5f7c05a5201195cc71901a0bc0d6be840c6b03ce
Nam Le Dinh <nam.ledinh@axonactive.com>
2019-03-20 09:31:38 +0700

    IVYPORTAL-6985: Various or urgent requests in IVYPORTAL S170 - Fix PortalMobileGUITest unstable

--- AxonIvyPortal/PortalMobileTest/src_test/ch/ivyteam/ivy/project/portal/guitest/mobile/page/MobileProcessPage.java
+++ AxonIvyPortal/PortalMobileTest/src_test/ch/ivyteam/ivy/project/portal/guitest/mobile/page/MobileProcessPage.java
@@ -7,9 +7,9 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
 public class MobileProcessPage extends MobileTemplatePage{
-  private static final String DICTONARY_PROCESS_GROUP_SELECTION_PANEL_ID = "process-widget:dictonary-process-group-selection_items";
+  private static final String DICTONARY_PROCESS_GROUP_SELECTION_PANEL_ID = "process-widget:dictionary-process-group-selection_items";
   private static final String PROCESS_NAME_SEARCH_INPUT_ID = "process-widget:process-search:non-ajax-keyword-filter";
-  private static final String DICTIONARY_GROUP_ID = "process-widget:dictonary-process-group-selection";
+  private static final String DICTIONARY_GROUP_ID = "process-widget:dictionary-process-group-selection";
   private static final String PROCESS_ITEM_CSS_SELECTOR = 
       "[id$=':%d:none-express-processes:%d:process-item-form:process-item']"; //First input: Index of field set, Second input: Index of process item in that field set
   @Override
