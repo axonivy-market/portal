5c6fef17aac81d9408b447c83b42500d598df2a2
nam.mai <nam.mai@axonactive.com>
2019-04-01 09:45:37 +0700

    IVYPORTAL-6075: Showcase for standard components without customization - Implemented showcases

---
+++ Showcase/PortalExamples/src/ch/ivyteam/ivy/project/portal/examples/showcase/GdprRequest.java
@@ -0,0 +1,194 @@
+package ch.ivyteam.ivy.project.portal.examples.showcase;
+
+import java.util.Date;
+
+import ch.ivyteam.ivy.project.portal.examples.showcase.enums.Completeness;
+import ch.ivyteam.ivy.project.portal.examples.showcase.enums.ProcessState;
+
+//import ch.ivy.gdpr.enums.ProcessState;
+
+
+
+//	Change the name of table -> create class for it \\
+public class GdprRequest {
+	private ProcessState processState;
+	private String id;
+	private Long caseId;
+	private String appliacationUserName;
+	//private ProcessState processState;
+	private Date creationDate;
+	private java.util.List<GdprFile> files;
+	//private Request request;
+	//private Decision decision;
+	private String reviewedBy;
+	private Date dateOfReview;	
+	private String surname;
+	private String name;
+	private String address;
+	private Integer zip;
+	private String city;
+	private Integer phoneNumber;
+	private String emailAddress;
+	//private Identification identification;
+	private String requestReason;
+	private String identificationNumer;
+	private Completeness completeness;
+	
+	public ProcessState getProcessState() {
+		return processState;
+	}
+
+	public void setProcessState(ProcessState processState) {
+		this.processState = processState;
+	}
+	
+	public Completeness getCompleteness() {
+		return completeness;
+	}
+	public void setCompleteness(Completeness completeness) {
+		this.completeness = completeness;
+	}
+	public String getSurname() {
+		return surname;
+	}
+
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public Integer getZip() {
+		return zip;
+	}
+
+	public void setZip(Integer zip) {
+		this.zip = zip;
+	}
+	
+	public String getRequestReason() {
+		return requestReason;
+	}
+
+	public void setRequestReason(String requestReason) {
+		this.requestReason = requestReason;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public Integer getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(Integer phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public String getEmailAddress() {
+		return emailAddress;
+	}
+
+	public void setEmailAddress(String emailAddress) {
+		this.emailAddress = emailAddress;
+	}
+
+
+
+	public String getIdentificationNumer() {
+		return identificationNumer;
+	}
+
+	public void setIdentificationNumer(String identificationNumer) {
+		this.identificationNumer = identificationNumer;
+	}
+	
+
+
+	public Long getCaseId() {
+		return caseId;
+	}
+
+	public void setCaseId(Long caseId) {
+		this.caseId = caseId;
+	}
+
+	/*public ProcessState getProcessState() {
+		return processState;
+	}
+
+	public void setProcessState(ProcessState processState) {
+		this.processState = processState;
+	}*/
+
+	public Date getCreationDate() {
+		return creationDate;
+	}
+
+	public void setCreationDate(Date creationDate) {
+		this.creationDate = creationDate;
+	}
+
+	
+
+	public String getReviewedBy() {
+		return reviewedBy;
+	}
+
+	public void setReviewedBy(String reviewedBy) {
+		this.reviewedBy = reviewedBy;
+	}
+
+	public Date getDateOfReview() {
+		return dateOfReview;
+	}
+
+	public void setDateOfReview(Date dateOfReview) {
+		this.dateOfReview = dateOfReview;
+	}
+
+	public java.util.List<GdprFile> getFiles() {
+		return files;
+	}
+
+	public void setFiles(java.util.List<GdprFile> files) {
+		this.files = files;
+	}
+
+	
+
+	public String getAppliacationUserName() {
+		return appliacationUserName;
+	}
+
+	public void setAppliacationUserName(String appliacationUserName) {
+		this.appliacationUserName = appliacationUserName;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+}
