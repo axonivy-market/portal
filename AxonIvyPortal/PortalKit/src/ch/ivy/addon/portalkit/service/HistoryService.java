a9f0d7c08ebadd8f0ace54e1fb5f3939e70dc042
Nguyen To <nguyen.to@axonactive.com>
2019-04-09 15:22:02 +0700

    IVYPORTAL-5925: Include Selfservice BPM in Express - Show modal dialog and Include comments in the Case History - Show adhoc entries in dialog

--- AxonIvyPortal/PortalKit/src/ch/ivy/addon/portalkit/service/HistoryService.java
+++ AxonIvyPortal/PortalKit/src/ch/ivy/addon/portalkit/service/HistoryService.java
@@ -1,5 +1,6 @@
 package ch.ivy.addon.portalkit.service;
 
+import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -9,6 +10,7 @@ import org.apache.commons.lang.StringUtils;
 
 import ch.ivy.addon.portalkit.bo.History;
 import ch.ivy.addon.portalkit.bo.History.HistoryType;
+import ch.ivy.addon.portalkit.enums.AdditionalProperty;
 import ch.ivyteam.ivy.security.ISecurityConstants;
 import ch.ivyteam.ivy.security.IUser;
 import ch.ivyteam.ivy.workflow.INote;
@@ -42,12 +44,13 @@ public class HistoryService {
   }
 
   private List<History> createHistoriesFromITasks(List<ITask> tasks) {
-    return tasks.stream().map(this::createHistoryFrom).collect(Collectors.toList());
+    return tasks.stream().filter(task -> task.customFields().stringField(AdditionalProperty.ADHOC_EXPRESS_TASK.toString()).getOrNull() == null).map(this::createHistoryFrom).collect(Collectors.toList());
   }
 
   private List<History> createHistoriesFromNonTechnicalITasks(List<ITask> tasks) {
     return tasks.stream()
         .filter(task -> !StringUtils.equals(task.getWorkerUserName(), ISecurityConstants.SYSTEM_USER_NAME))
+        .filter(task -> task.customFields().stringField(AdditionalProperty.ADHOC_EXPRESS_TASK.toString()).getOrNull() == null)
         .map(this::createHistoryFrom).collect(Collectors.toList());
   }
 
@@ -85,7 +88,7 @@ public class HistoryService {
       history.setInvolvedFullname(note.getWritter().getFullName());
     }
     history.setInvolvedUsername(note.getWritterName());
-    history.setTimestamp(note.getCreationTimestamp());
+    history.setTimestamp(new Timestamp(note.getCreationTimestamp().getTime()));
     history.setType(HistoryType.NOTE);
     return history;
   }
