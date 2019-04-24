1cca34d8b7f5e29d169967e11fdad13495053216
lttung_wawa <tung.le@axonactive.com>
2019-04-24 17:30:46 +0700

    IVYPORTAL-6388 Announcements in Portal - Improved performance
    Former-commit-id: 3bbac05385d5859c2698c0a2defe9f7c923b95d7

--- AxonIvyPortal/PortalKit/src/ch/ivy/addon/portalkit/service/ServerService.java
+++ AxonIvyPortal/PortalKit/src/ch/ivy/addon/portalkit/service/ServerService.java
@@ -1,16 +1,16 @@
 package ch.ivy.addon.portalkit.service;
 
 
+import static ch.ivy.addon.portalkit.constant.PortalConstants.PORTAL_APPLICATION_NAME;
+
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.apache.commons.collections4.CollectionUtils;
 
-import ch.ivy.addon.portalkit.constant.PortalConstants;
 import ch.ivy.addon.portalkit.ivydata.utils.ServiceUtilities;
 import ch.ivy.addon.portalkit.persistence.domain.Application;
 import ch.ivy.addon.portalkit.util.IvyExecutor;
-import ch.ivyteam.ivy.application.ActivityState;
 import ch.ivyteam.ivy.application.IApplication;
 import ch.ivyteam.ivy.server.ServerFactory;
 
@@ -35,12 +35,10 @@ public class ServerService {
       apps = IvyExecutor.executeAsSystem(
           () -> ServerFactory.getServer().getApplicationConfigurationManager().getApplicationsSortedByName(false));
     } else {
-      apps = IvyExecutor.executeAsSystem(() -> ServiceUtilities.findApps(configuredApps));
-      IApplication defaultApplication = ServerFactory.getServer().getApplicationConfigurationManager()
-          .findApplication(PortalConstants.PORTAL_APPLICATION_NAME);
-      if (defaultApplication != null && defaultApplication.getActivityState() == ActivityState.ACTIVE) {
-        apps.add(defaultApplication);
+      if (!configuredApps.contains(PORTAL_APPLICATION_NAME)) {
+        configuredApps.add(PORTAL_APPLICATION_NAME);
       }
+      apps = IvyExecutor.executeAsSystem(() -> ServiceUtilities.findApps(configuredApps));
     }
     return apps;
   }
