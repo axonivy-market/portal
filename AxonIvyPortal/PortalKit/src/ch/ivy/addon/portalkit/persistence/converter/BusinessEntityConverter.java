3220a678541e60a92f80ca10ec3d284e5f157066
Long Do <long.do@axonactive.com>
2019-05-17 15:08:17 +0700

    IVYPORTAL-7151 Support Java 11 - Remove java boon: + Implemented

--- AxonIvyPortal/PortalKit/src/ch/ivy/addon/portalkit/persistence/converter/BusinessEntityConverter.java
+++ AxonIvyPortal/PortalKit/src/ch/ivy/addon/portalkit/persistence/converter/BusinessEntityConverter.java
@@ -1,12 +1,16 @@
 package ch.ivy.addon.portalkit.persistence.converter;
 
-import org.boon.json.ObjectMapper;
-import org.boon.json.implementation.ObjectMapperImpl;
+import java.io.IOException;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import ch.ivy.addon.portalkit.persistence.domain.BusinessEntity;
+import ch.ivy.addon.portalkit.service.exception.PortalException;
+import ch.ivyteam.ivy.environment.Ivy;
 
 /**
- * This class provides method that uses API of org.boon.json to convert Business
+ * This class provides method to convert Business
  * entity object into Json value and reverse
  */
 public final class BusinessEntityConverter {
@@ -15,12 +19,22 @@ public final class BusinessEntityConverter {
   }
 
   public static String entityToJsonValue(BusinessEntity entity) {
-    ObjectMapper objectMapper = new ObjectMapperImpl();
-    return objectMapper.writeValueAsString(entity);
+    ObjectMapper objectMapper = new ObjectMapper();
+    try {
+      return objectMapper.writeValueAsString(entity);
+    } catch (JsonProcessingException e) {
+      Ivy.log().error("Can't write json value", e);
+      throw new PortalException(e);
+    }
   }
 
   public static <T> T jsonValueToEntity(String jsonValue, Class<T> classType) {
-    ObjectMapper objectMapper = new ObjectMapperImpl();
-    return objectMapper.readValue(jsonValue, classType);
+    ObjectMapper objectMapper = new ObjectMapper();
+    try {
+      return objectMapper.readValue(jsonValue, classType);
+    } catch (IOException e) {
+      Ivy.log().error("Can't read json value", e);
+      throw new PortalException(e);
+    }
   }
 }
