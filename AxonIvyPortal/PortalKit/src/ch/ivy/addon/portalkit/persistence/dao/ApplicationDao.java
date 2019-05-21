03081383efa869e0dbf0b5077ddc9adde2db6df5
Long Do <long.do@axonactive.com>
2019-05-21 11:17:20 +0700

    IVYPORTAL-7151 Support Java 11 - Remove java boon: + Fixed wrong import

--- AxonIvyPortal/PortalKit/src/ch/ivy/addon/portalkit/persistence/dao/ApplicationDao.java
+++ AxonIvyPortal/PortalKit/src/ch/ivy/addon/portalkit/persistence/dao/ApplicationDao.java
@@ -3,7 +3,7 @@ package ch.ivy.addon.portalkit.persistence.dao;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import org.apache.commons.codec.binary.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 
 import ch.ivy.addon.portalkit.persistence.domain.Application;
 import ch.ivyteam.ivy.application.IApplication;
--- AxonIvyPortal/PortalKit/src/ch/ivy/addon/portalkit/persistence/dao/UserProcessDao.java
+++ AxonIvyPortal/PortalKit/src/ch/ivy/addon/portalkit/persistence/dao/UserProcessDao.java
@@ -3,7 +3,7 @@ package ch.ivy.addon.portalkit.persistence.dao;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import org.apache.commons.codec.binary.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 
 import ch.ivy.addon.portalkit.persistence.domain.UserProcess;
 import ch.ivyteam.ivy.application.IApplication;
