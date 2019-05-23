6b8e731cb68636faf79616d885f28b94f138821d
Minh Nguyen <minh.nguyen02@axonactive.com>
2019-05-23 09:50:08 +0700

    IVYPORTAL-6877 Email Settings - Consistency - Handle feedback: Count Ivy App

--- AxonIvyPortal/PortalKit/src/ch/ivy/addon/portalkit/bean/EmailSettingBean.java
+++ AxonIvyPortal/PortalKit/src/ch/ivy/addon/portalkit/bean/EmailSettingBean.java
@@ -5,14 +5,15 @@ import java.io.Serializable;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 
-import ch.ivy.addon.portalkit.service.ServerService;
+import ch.ivy.addon.portalkit.service.RegisteredApplicationService;
 
 @ManagedBean
 @ViewScoped
 public class EmailSettingBean implements Serializable {
   private static final long serialVersionUID = 1L;
 
-  public Integer countApplication() {
-    return ServerService.getInstance().getApplicationsRelatedToPortal().size();
+  public Long countApplication() {
+    RegisteredApplicationService service = new RegisteredApplicationService();
+    return service.countIvyApplications(service.findAllIvyApplications());
   }
 }
