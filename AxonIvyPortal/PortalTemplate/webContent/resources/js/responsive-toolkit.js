687c8ced643599302562d5d6c9afea27f4e4c8cf
Nguyen Quoc Hoan <hoan.nguyen01@axonactive.com>
2019-05-29 18:24:44 +0700

    IVYPORTAL-6535 UI components for Serenity II - updated margin of main content
    Former-commit-id: fcd1b94683674bd5a10f7512b67bb714403006c4

--- AxonIvyPortal/PortalTemplate/webContent/resources/js/responsive-toolkit.js
+++ AxonIvyPortal/PortalTemplate/webContent/resources/js/responsive-toolkit.js
@@ -744,7 +744,7 @@ function CaseListSmallScreenHandler() {
 /***************************Handle responsive for Dashboard**********************************/
 function DashboardLargeScreen() {
   var marginValWhenMainMenuOpen = 210;
-  var marginValWhenTwoMenuClose = 70;
+  var marginValWhenTwoMenuClose = 50;
 
   this.updateMainContainer = function() {
     var $mainMenu = $('.js-left-sidebar');
@@ -764,7 +764,7 @@ function DashboardLargeScreen() {
 function DashboardMediumScreen() {
   var firstCol = {
     marginValWhenMainMenuOpen : 190,
-    marginValWhenMainMenuClose : 70
+    marginValWhenMainMenuClose : 50
   };
 
   this.updateMainContainer = function() {
