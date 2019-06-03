08521502409cdaf6a191e8767424e629bd1f25f7
nam.mai <nam.mai@axonactive.com>
2019-06-03 16:58:19 +0700

    IVYPORTAL-7522: Various or urgent requests in IVYPORTAL S174 - Fixed bug: Bug of document text in Portal "Case Information"

--- AxonIvyPortal/PortalKit/webContent/resources/js/document-truncation.js
+++ AxonIvyPortal/PortalKit/webContent/resources/js/document-truncation.js
@@ -2,11 +2,11 @@ function DocumentTruncation() {
   return {
     
     truncate : function() {
+      $('.js-document-column').css('display','inline-block');
       $('.js-download-command').each(function () {
         var documentHeight = $(this).outerHeight();
         var containerHeight = $(this).parent().outerHeight();
         var chars = 20;
-        
         while (documentHeight > containerHeight) {
           var documentName = $(this).children(".js-document-name").text();
           $(this).children(".js-document-name").text(documentName.slice(0, chars) + '...' + documentName.slice(-chars));
@@ -14,6 +14,7 @@ function DocumentTruncation() {
           documentHeight = $(this).outerHeight();
         }
       });
+      $('.js-document-column').css('display','none');
     },
   }
 }
