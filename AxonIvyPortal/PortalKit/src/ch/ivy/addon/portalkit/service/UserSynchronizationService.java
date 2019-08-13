bdb468c76c92cfd5f5befcb8617768f1742e1b5e
Minh Nguyen <minh.nguyen02@axonactive.com>
2019-08-13 13:54:33 +0700

    IVYPORTAL-7893 On demand synchronisation of users - Add/Update user if login : add user to applicationproperty

--- AxonIvyPortal/PortalKit/src/ch/ivy/addon/portalkit/service/UserSynchronizationService.java
+++ AxonIvyPortal/PortalKit/src/ch/ivy/addon/portalkit/service/UserSynchronizationService.java
@@ -14,6 +14,7 @@ import ch.ivy.addon.portalkit.persistence.domain.Server;
 import ch.ivy.addon.portalkit.persistence.domain.User;
 import ch.ivy.addon.portalkit.support.DataCache;
 import ch.ivy.addon.portalkit.util.ApplicationUserCacheUtils;
+import ch.ivy.addon.portalkit.util.SecurityServiceUtils;
 import ch.ivyteam.ivy.environment.Ivy;
 
 public class UserSynchronizationService {
@@ -57,70 +58,57 @@ public class UserSynchronizationService {
     return applications.size() < 2;
   }
   
-	public static void addUserToCacheAndUserService() {
-		// check user available
-		UserService userService = new UserService();
-		List<User> usersCheck = userService.findByUserName(Ivy.session()
-				.getSessionUserName());
-		Boolean uex = false;
-		for (User userchk : usersCheck) {
-			if (userchk.getApplicationName().equals(
-					Ivy.wf().getApplication().getName())) {
-				uex = true;
-				break;
-			}
-		}
-		if (!uex) { // set user if not found
-			Ivy.log().warn(
-					"USER NOT FOUND in User Cache:"
-							+ Ivy.session().getSessionUserName());
-			// Step 1 Create Remote User
+  public static void addUserToCacheAndUserService() {
+    // check user available
+    UserService userService = new UserService();
+    List<User> usersCheck = userService.findByUserName(Ivy.session().getSessionUserName());
+    Boolean uex = false;
+    for (User userchk : usersCheck) {
+      if (userchk.getApplicationName().equals(Ivy.wf().getApplication().getName())) {
+        uex = true;
+        break;
+      }
+    }
+    if (!uex) { // set user if not found
+      // Step 1 Create Remote User
+      RemoteUser remoteUser = new RemoteUser();
+      remoteUser.setId(Ivy.session().getSessionUser().getId());
+      remoteUser.setName(Ivy.session().getSessionUser().getDisplayName()); // fullname
+      remoteUser.setUsername(Ivy.session().getSessionUserName()); // name
+      remoteUser.setAppName(Ivy.wf().getApplication().getName());
+      Long serverId = SecurityServiceUtils.getServerIdFromSession();
 
-			RemoteUser remoteUser = new RemoteUser();
-			remoteUser.setId(Ivy.session().getSessionUser().getId());
-			remoteUser.setName(Ivy.session().getSessionUser().getDisplayName()); // fullname
-			remoteUser.setUsername(Ivy.session().getSessionUserName()); // name
-			remoteUser.setAppName(Ivy.wf().getApplication().getName());
-			Long serverId = 0l;
+      try {
+        ServerService ses = new ServerService();
+        if (ses.findAll().size() > 0) {
+          serverId = ses.findAll().get(0).getId();
+          if (serverId == null) {
+            serverId = -1L;
+          }
+        }
+      } catch (Exception e) {
+        Ivy.log().error(
+            "The serverId could not be determined. Using serverId 0 for saving user:"
+                + Ivy.session().getSessionUserName());
+      }
+      remoteUser.setServerId(serverId);
 
-			try {
-				ServerService ses = new ServerService();
-				if (ses.findAll().size() > 0) {
-					serverId = ses.findAll().get(0).getId();
-					if (serverId == null) {
-						serverId = -1L;
-					}
-				}
-			} catch (Exception e) {
-				Ivy.log().error(
-						"The serverId could not be determined. Using serverId 0 for saving user:"
-								+ Ivy.session().getSessionUserName());
-			}
+      // Step 2 Convert Remote User to User and save
+      List<RemoteUser> remoteUsers = new ArrayList<RemoteUser>();
+      remoteUsers.add(remoteUser);
+      List<User> users = ApplicationUserCacheUtils.convertToEntity(remoteUsers);
+      userService.saveAll(users);
 
-			remoteUser.setServerId(serverId);
+      // Step 3 get cache and get users from it. Add them to the created
+      // user
 
-			// Step 2 Convert Remote User to User and save
-			List<RemoteUser> remoteUsers = new ArrayList<RemoteUser>();
-			remoteUsers.add(remoteUser);
-			List<User> users = ApplicationUserCacheUtils
-					.convertToEntity(remoteUsers);
-			userService.saveAll(users);
-			Ivy.log().warn(
-					"USER WAS SAVED for User Cache:"
-							+ Ivy.session().getSessionUserName());
-
-			// Step 3 get cache and get users from it. Add them to the created
-			// user
-
-			UserDao userDao = new UserDao();
-			users.addAll(DataCache.getAllUsersFromCache());
-			Repo<Long, User> repo = userDao.buildRepoIndexedByUserName(users);
-			String applicationName = Ivy.wf().getApplication().getName();
-			DataCache.cacheAllUsers(applicationName, users);
-			DataCache.cacheUsersRepo(applicationName, repo);
-			Ivy.log().warn(
-					"USER WAS SET into User Cache:"
-							+ Ivy.session().getSessionUserName());
-		}
-	}
+      UserDao userDao = new UserDao();
+      users.addAll(DataCache.getAllUsersFromCache());
+      Repo<Long, User> repo = userDao.buildRepoIndexedByUserName(users);
+      String applicationName = Ivy.wf().getApplication().getName();
+      DataCache.invalidateUsersCache(applicationName);
+      DataCache.cacheAllUsers(applicationName, users);
+      DataCache.cacheUsersRepo(applicationName, repo);
+    }
+  }
 }
