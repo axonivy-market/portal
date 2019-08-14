a5e453be7c283e4a145eefc13fe594b2d1c0abc5
Nam Chu <nam.chu@axonactive.com>
2019-08-14 18:26:07 +0700

    IVYPORTAL-7893 On demand synchronisation of users - Bug with multi apps LTS

--- AxonIvyPortal/PortalKit/src/ch/ivy/addon/portalkit/service/UserSynchronizationService.java
+++ AxonIvyPortal/PortalKit/src/ch/ivy/addon/portalkit/service/UserSynchronizationService.java
@@ -6,14 +6,12 @@ import java.util.List;
 import org.apache.commons.collections4.CollectionUtils;
 import org.boon.datarepo.Repo;
 
-import ch.ivy.addon.portalkit.bo.RemoteUser;
 import ch.ivy.addon.portalkit.persistence.dao.UserDao;
 import ch.ivy.addon.portalkit.persistence.domain.Application;
 import ch.ivy.addon.portalkit.persistence.domain.Server;
 import ch.ivy.addon.portalkit.persistence.domain.User;
 import ch.ivy.addon.portalkit.support.DataCache;
-import ch.ivy.addon.portalkit.util.ApplicationUserCacheUtils;
-import ch.ivy.addon.portalkit.util.SecurityServiceUtils;
+import ch.ivyteam.ivy.application.IApplication;
 import ch.ivyteam.ivy.environment.Ivy;
 
 public class UserSynchronizationService {
@@ -50,59 +48,45 @@ public class UserSynchronizationService {
         .orElse(new Application());
     return Ivy.request().getApplication().getName().equals(firstApplication.getName()); 
   }
-
+  
   public static void addUserToCacheAndUserService() {
-    Ivy.log().error("Call function user");
     // check user available
     UserService userService = new UserService();
     List<User> usersCheck = userService.findByUserName(Ivy.session().getSessionUserName());
 
-    boolean uex =
-        CollectionUtils
-            .emptyIfNull(usersCheck)
-            .stream()
-            .filter(
-                userchk -> userchk.getApplicationName().equals(Ivy.wf().getApplication().getName()) && userchk.getServerId()!= -1L )
-            .findAny().isPresent();
-    if (!uex) { // set user if not found
-      List<Application> apps = new ApplicationService().findAllIvyApplications();
-      for (Application app : apps) {
-        List<RemoteUser> remoteUsers = new ArrayList<RemoteUser>();
-        // Step 1 Create Remote User
-        RemoteUser remoteUser = new RemoteUser();
-        remoteUser.setId(Ivy.session().getSessionUser().getId());
-        remoteUser.setName(Ivy.session().getSessionUser().getDisplayName()); // fullname
-        remoteUser.setUsername(Ivy.session().getSessionUserName()); // name
-        remoteUser.setAppName(app.getName());
-        Long serverId = SecurityServiceUtils.getServerIdFromSession();
-        try {
-          ServerService ses = new ServerService();
-          if (ses.findAll().size() > 0) {
-            serverId = ses.findAll().get(0).getId();
-            if (serverId == null) {
-              serverId = -1L;
-            }
-          }
-        } catch (Exception e) {
-          Ivy.log().error(
-              "The serverId could not be determined. Using serverId 0 for saving user:"
-                  + Ivy.session().getSessionUserName());
-        }
-
-        remoteUser.setServerId(serverId);
-        remoteUsers.add(remoteUser);
-        List<User> users = ApplicationUserCacheUtils.convertToEntity(remoteUsers);
-        userService.saveAll(users);
-        
-        UserDao userDao = new UserDao();
-        users.addAll(DataCache.getAllUsersFromCache());
-        Repo<Long, User> repo = userDao.buildRepoIndexedByUserName(users);
-        String applicationName = app.getName();
-        DataCache.invalidateUsersCache(applicationName);
-        DataCache.cacheAllUsers(applicationName, users);
-        DataCache.cacheUsersRepo(applicationName, repo);
+//    demo AD2 
+//    demo AD3
+    
+    List<Application> apps = new ApplicationService().findAllIvyApplications();
+    List<User> users = new ArrayList<>();
+    
+    CollectionUtils.emptyIfNull(apps).forEach(app -> {
+      String applicationName = app.getName();
+      Ivy.log().error("START A CHECK");
+      usersCheck.forEach(x -> Ivy.log().error("app name {0}, server id {1}", x.getApplicationName(), x.getServerId()));
+      boolean x = CollectionUtils
+          .emptyIfNull(usersCheck)
+          .stream()
+          .filter(userchk -> userchk.getApplicationName().equals(applicationName)
+              && userchk.getServerId() != -1L )
+              .findAny().isPresent();
+      if (!x){
+        User user = new User();
+        user.setUserName(Ivy.session().getSessionUserName());
+        user.setFullUserName(Ivy.session().getSessionUser().getDisplayName());
+        user.setApplicationName(applicationName);
+        users.add(user);
       }
-
+    });
+    
+    if (CollectionUtils.isNotEmpty(users)){
+      userService.saveAll(users);
+      UserDao userDao = new UserDao();
+      users.addAll(DataCache.getAllUsersFromCache());
+      Repo<Long, User> repo = userDao.buildRepoIndexedByUserName(users);
+      String applicationName = Ivy.wf().getApplication().getName();
+      DataCache.cacheAllUsers(applicationName, users);
+      DataCache.cacheUsersRepo(applicationName, repo);
     }
   }
 }
