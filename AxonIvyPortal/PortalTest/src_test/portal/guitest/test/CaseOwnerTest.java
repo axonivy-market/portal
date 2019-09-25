09a3cecf02e9a92b8af6426882454b1ebcca582a
Long Do <long.do@axonactive.com>
2019-09-25 15:12:49 +0700

    IVYPORTAL-7532 Integrate CaseOwner Feature in Portal II: + Implemented

---
+++ AxonIvyPortal/PortalTest/src_test/portal/guitest/test/CaseOwnerTest.java
@@ -0,0 +1,43 @@
+package portal.guitest.test;
+
+import org.junit.Test;
+
+import portal.guitest.common.BaseTest;
+import portal.guitest.common.TestAccount;
+import portal.guitest.page.CaseWidgetPage;
+import portal.guitest.page.HomePage;
+import portal.guitest.page.MainMenuPage;
+
+public class CaseOwnerTest extends BaseTest {
+
+  private static final String USER_IS_OWNER_URL = "internalSupport/16A68510A341BE6E/userIsOwner.ivp";
+  private static final String ROLE_IS_OWNER_URL = "internalSupport/16A68510A341BE6E/roleIsOwner.ivp";
+  
+  private HomePage homePage;
+  private MainMenuPage mainMenuPage;
+  private CaseWidgetPage casePage;
+
+  @Test
+  public void testUserIsOwner() {
+    navigateToUrl(USER_IS_OWNER_URL);
+    navigateToUrl(HomePage.PORTAL_HOME_PAGE_URL);
+    login(TestAccount.DEMO_USER);
+    
+    homePage = new HomePage();
+    mainMenuPage = homePage.openMainMenu();
+    casePage = mainMenuPage.selectCaseMenu();
+    assertTrue(casePage.isCaseDisplayed("demo user is owner"));
+  }
+
+  @Test
+  public void testRoleIsOwner() {
+    navigateToUrl(ROLE_IS_OWNER_URL);
+    navigateToUrl(HomePage.PORTAL_HOME_PAGE_URL);
+    login(TestAccount.CASE_OWNER_USER);
+    
+    homePage = new HomePage();
+    mainMenuPage = homePage.openMainMenu();
+    casePage = mainMenuPage.selectCaseMenu();
+    assertTrue(casePage.isCaseDisplayed("CaseOwner role is owner"));
+  }
+}
