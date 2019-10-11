8685d0c1b57081a3a599a2966b237be579da3d19
Thang <thang.nguyenvan@axonactive.com>
2019-10-11 16:33:56 +0700

    IVYPORTAL-8452: Refactor Gui test on CaseDetailsTest II - Refactor AnnouncementTest

--- AxonIvyPortal/PortalTest/src_test/portal/guitest/page/AnnouncementPage.java
+++ AxonIvyPortal/PortalTest/src_test/portal/guitest/page/AnnouncementPage.java
@@ -5,19 +5,19 @@ import org.openqa.selenium.WebElement;
 
 public class AnnouncementPage extends TemplatePage {
 
-
   public static final String DE_LANGUAGE = "lies mich";
   public static final String EN_LANGUAGE = "read me";
-//  private static final String FR_LANGUAGE = "lis moi";
-//  private static final String PT_LANGUAGE = "me leia";
+  
+  @Override
+  protected String getLoadedLocator() {
+    return "id('adminui:adminTabView:announcement-tab')";
+  }
 
   public String getInfoSummary() {
     return driver.findElement(By.cssSelector("div[id$='messages'] span[class$='summary']")).getText();
   }
 
   public void closeInformConfigDialog() {
-    // waitForElementPresent(By.id("close-dialog-button"), true);
-    // waitForElementDisplayed("dialog-closing-information", true);
     WebElement closeButton = findElementById("close-dialog-button");
     closeButton.click();
   }
@@ -32,10 +32,8 @@ public class AnnouncementPage extends TemplatePage {
     waitForElementDisplayed(By.cssSelector("div[id$='messages'] span[class$='summary']"), true);
   }
 
-
   public void setAnnoucement(int Language, String content) {
-    findElementByCssSelector("input[id$='"+Language+":announcement-input']").sendKeys(content);
-    
+    findElementByCssSelector("input[id$='" + Language + ":announcement-input']").sendKeys(content);
   }
 
 }
