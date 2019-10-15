e50131cdd60afabd61c61929ac72880db719dbe1
Nam Le Dinh <nam.ledinh@axonactive.com>
2019-10-15 17:51:32 +0700

    IVYPORTAL-8502: Refactor Elapsed time chart - Implement elapsed time chart

--- AxonIvyPortal/PortalKit/webContent/resources/js/statistic-widget.js
+++ AxonIvyPortal/PortalKit/webContent/resources/js/statistic-widget.js
@@ -69,20 +69,15 @@ function barChartExtender() {
 }
 
 function elapsedTimeBarChartExtender() {
-  var currentAngle;
+ /* var currentAngle;
   var showLabel = !isNaN(this.cfg.data[0][0]);
   if (window.screen.availWidth < 1366 || this.cfg.data[0].length > 2) {
     currentAngle = -70;
   } else {
     currentAngle = 0;
-  }
-  this.cfg.grid = {
-    gridLineColor : 'transparent',
-    background : 'rgba(255,255,255, 0)',
-    drawBorder : true,
-    shadow : false
-  };
-  this.cfg.axes.yaxis.labelOptions = {
+  }*/
+
+ /* this.cfg.axes.yaxis.labelOptions = {
     textColor : 'black',
     fontSize : '11.4px'
   };
@@ -105,7 +100,7 @@ function elapsedTimeBarChartExtender() {
     tooltipAxes : 'y',
     useAxesFormatters : false,
     tooltipFormatString : "%.2f"
-  };
+  };*/
 }
 
 function chartExtender() {
@@ -119,38 +114,3 @@ function chartExtender() {
 //  this.cfg.seriesDefaults.rendererOptions.dataLabelThreshold = 0;
 }
 
-function elapsedTimeChartExtender() {
-  this.cfg.grid = {
-    background : 'rgba(255,255,255, 0)',
-    drawGridlines : false,
-    drawBorder : false,
-    shadow : false
-  };
-  this.cfg.seriesDefaults.rendererOptions.dataLabelFormatString = '%.4s%%';
-  this.cfg.seriesDefaults.rendererOptions.dataLabelThreshold = 0;
-  this.cfg.highlighter = {
-    show : true,
-    useAxesFormatters : false,
-    tooltipContentEditor : function(ev, seriesIndex, pointIndex, plot) {
-      var seconds = plot.series[seriesIndex].data[pointIndex][1];
-
-      var days = Math.floor(seconds / (3600 * 24));
-      seconds -= days * 3600 * 24;
-      var hours = Math.floor(seconds / 3600);
-      seconds -= hours * 3600;
-      var minutes = Math.floor(seconds / 60);
-      seconds -= minutes * 60;
-      var result = seconds + " seconds";
-      if (minutes > 0) {
-        result = minutes + " minutes - " + result;
-      }
-      if (hours > 0) {
-        result = hours + " hours - " + result;
-      }
-      if (days > 0) {
-        result = days + " days - " + result;
-      }
-      return result;
-    }
-  }
-}
