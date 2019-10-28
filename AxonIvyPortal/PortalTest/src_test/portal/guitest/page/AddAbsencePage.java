d29bbff213cb3f61313a8d6c3b176276274a284c
lttung_wawa <tung.le@axonactive.com>
2019-10-28 14:03:24 +0700

    Updated Tests

--- AxonIvyPortal/PortalTest/src_test/portal/guitest/page/AddAbsencePage.java
+++ AxonIvyPortal/PortalTest/src_test/portal/guitest/page/AddAbsencePage.java
@@ -1,7 +1,6 @@
 package portal.guitest.page;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
 
 public class AddAbsencePage extends TemplatePage {
 
@@ -21,8 +20,7 @@ public class AddAbsencePage extends TemplatePage {
   }
 
   private void proceed(String selector) {
-    WebElement proceedElement = findElementByCssSelector(selector);
-    proceedElement.click();
+    clickByCssSelector(selector);
     waitAjaxIndicatorDisappear();
   }
 
