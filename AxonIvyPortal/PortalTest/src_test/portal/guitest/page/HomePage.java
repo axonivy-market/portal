40d798814cda20cbf3cef89a0fb0a6a88ad5f0ab
lttung_wawa <tung.le@axonactive.com>
2019-11-01 15:09:35 +0700

    IVYPORTAL-8579 Adapt GUI Tests for Serenity - Removed obsolete code

--- AxonIvyPortal/PortalTest/src_test/portal/guitest/page/HomePage.java
+++ AxonIvyPortal/PortalTest/src_test/portal/guitest/page/HomePage.java
@@ -1,14 +1,13 @@
 package portal.guitest.page;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.support.Color;
 
 
 public class HomePage extends TemplatePage {
 
 	public final static String PORTAL_HOME_PAGE_URL = "portalTemplate/1549F58C18A6C562/PortalStart.ivp";
 	public final static String PORTAL_EXAMPLES_HOME_PAGE_URL = "portalExamples/164211E97C598DAA/PortalStart.ivp";
-	public final static String TASK_SWITCH_MODE_BTN_LOCATOR = "id('task-widget:task-list-link:task-list-link')";
+	private final static String TASK_SWITCH_MODE_BTN_LOCATOR = "id('task-widget:task-list-link:task-list-link')";
 	private static final String SHOW_ALL_PROCESSES_LINK_ID = "process-widget:process-link:process-link-label";
 	private static final String SHOW_TASK_LIST_LINK_ID = "task-widget:task-list-link:task-list-link-label";
 	private static final String SHOW_ALL_CHARTS_LINK_ID = "statistics-widget:statistic-link:statistic-link-label";
@@ -24,14 +23,6 @@ public class HomePage extends TemplatePage {
 
 	public ProcessWidgetPage getProcessWidget() {
 		return new ProcessWidgetPage();
-	}
-
-	public StatisticWidgetPage getStatisticsWidget() {
-		return new StatisticWidgetPage();
-	}
-
-	public String getMainColor() {
-		return Color.fromString(findElementById("app-menu-panel").getCssValue("background-color")).asHex().substring(1);
 	}
 
 	public boolean isShowAllProcessesLinkDisplayed() {
