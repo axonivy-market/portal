57ff34d194ed97fbb415c6804d99c0e91bcffb69
lttung_wawa <tung.le@axonactive.com>
2019-11-04 10:17:10 +0700

    IVYPORTAL-8579 Adapt GUI Tests for Serenity - Updated tests
    Former-commit-id: 506ea349f983c111db4dcb889264019e9998e5d1

--- AxonIvyPortal/PortalTest/src_test/portal/guitest/test/SearchCaseTest.java
+++ AxonIvyPortal/PortalTest/src_test/portal/guitest/test/SearchCaseTest.java
@@ -42,7 +42,7 @@ public class SearchCaseTest extends BaseTest {
     GlobalSearch globalSearch = homePage.getGlobalSearch();
     assertTrue(globalSearch.isDisplayed());
 
-    String caseName = "�sterreich Resource with ID 1212";
+    String caseName = "Österreich Resource with ID 1212";
     SearchResultPage searchResultPage = globalSearch.inputSearchKeyword(caseName);
     searchResultPage.openCaseTab();
     assertEquals(caseName, searchResultPage.getCaseResult(0));
