ba974a8d1c378d596474a026d411a43986aec9ab
Nguyen Quoc Hoan <hoan.nguyen01@axonactive.com>
2019-11-19 11:09:02 +0700

    IVYPORTAL-8866: Refactor PortalChat using Serenity II  - handle feedback
    Former-commit-id: 55ea98c747a0507f0f6467d615c4733ce0d0f1c6

--- AxonIvyPortal/PortalTemplate/webContent/resources/js/chat-transpiled.js
+++ AxonIvyPortal/PortalTemplate/webContent/resources/js/chat-transpiled.js
@@ -1055,7 +1055,7 @@ function View(uri) {
   function updateMessageListForIE11() {
     if (isIE11) {
       $(".js-message-list").css("height", "auto");
-      var margin = $('.message-list-content').get(0).offsetHeight - $(".js-message-list").get(0).scrollHeight - 235;
+      var margin = $('.message-list-content').get(0).offsetHeight - $(".js-message-list").get(0).scrollHeight - 235; // exclude height of chat-send-form
 
       if (margin < 0) {
         $(".js-message-list").css("margin-top", "");
@@ -1083,7 +1083,7 @@ function View(uri) {
   });
 
   function scrollToLastedMessage() {
-    var margin = $('.message-list-content').get(0).offsetHeight - $(".js-message-list").get(0).scrollHeight - 235;
+    var margin = $('.message-list-content').get(0).offsetHeight - $(".js-message-list").get(0).scrollHeight - 235; // exclude height of chat-send-form
 
     if (margin < 0) {
       var messagePanel = document.getElementById("chat-message-list");
