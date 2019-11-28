a98493c5a1db1dccb2a33e6a27535f3b35f56f2f
Nam Chu <nam.chu@axonactive.com>
2019-11-28 17:40:48 +0700

    - Bug: Language Setting menu display not found user Ticket https://axonivyhelp.zendesk.com/agent/tickets/26127

--- AxonIvyPortal/PortalKit/src/ch/ivy/addon/portalkit/service/UserSynchronizationService.java
+++ AxonIvyPortal/PortalKit/src/ch/ivy/addon/portalkit/service/UserSynchronizationService.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.boon.datarepo.Repo;
 
 import ch.ivy.addon.portalkit.persistence.dao.UserDao;
@@ -11,7 +12,9 @@ import ch.ivy.addon.portalkit.persistence.domain.Application;
 import ch.ivy.addon.portalkit.persistence.domain.Server;
 import ch.ivy.addon.portalkit.persistence.domain.User;
 import ch.ivy.addon.portalkit.support.DataCache;
+import ch.ivyteam.ivy.application.IApplication;
 import ch.ivyteam.ivy.environment.Ivy;
+import ch.ivyteam.ivy.server.ServerFactory;
 
 public class UserSynchronizationService {
 
@@ -57,6 +60,7 @@ public class UserSynchronizationService {
     List<User> users = new ArrayList<>();
     UserDao userDao = new UserDao();
     
+    // Prepare list user to persist to DB and cache
     CollectionUtils.emptyIfNull(apps).forEach(app -> {
       String applicationName = app.getName();
       boolean userExists = CollectionUtils
@@ -65,35 +69,58 @@ public class UserSynchronizationService {
           .filter(userchk -> userchk.getApplicationName().equals(applicationName)
               && userchk.getServerId() != -1L )
               .findAny().isPresent();
-      if (!userExists){
+      
+      if (!userExists && doesUserBelongToApp(username, applicationName)){
         User user = new User();
-        user.setUserName(Ivy.session().getSessionUserName());
+        user.setUserName(username);
         user.setFullUserName(Ivy.session().getSessionUser().getDisplayName());
         user.setApplicationName(applicationName);
-        
         users.add(user);
       }
     });
+    
     if (CollectionUtils.isNotEmpty(users)){
       List<User> cachedUsers = DataCache.getAllUsersFromCache();
       //Reload users from database to to check whether user is saved or not
       DataCache.invalidateUsersCache(Ivy.wf().getApplication().getName());
-      List<User> usersLoadedFromDB = userService.findByUserName(username); // cache & find
+      
+      // This time find user from database, not cache
+      List<User> usersLoadedFromDB = userService.findByUserName(username); 
       boolean userExists = CollectionUtils.emptyIfNull(usersLoadedFromDB).stream()
               .filter(userchk -> userchk.getApplicationName().equals(Ivy.wf().getApplication().getName())
                       && userchk.getServerId() != -1L)
               .findAny().isPresent();
       if (!userExists) {
-        userService.saveAll(users);
+        // persist to application property table only not exists user to avoid duplicate
+        for (User user : users){
+          if (!isUserAlreadySaved(usersLoadedFromDB, user)){
+            userService.save(user);
+          }
+        }
         users.addAll(cachedUsers);
         Repo<Long, User> repo = userDao.buildRepoIndexedByUserName(users);
         refreshUserAppCache(Ivy.wf().getApplication().getName(), users, repo);
       }
     }
   }
-
+  
   private static void refreshUserAppCache(String appName, List<User> users, Repo<Long, User> repo) {
     DataCache.cacheAllUsers(appName, users);
     DataCache.cacheUsersRepo(appName, repo);
+  }
+  
+  private static boolean doesUserBelongToApp(String userName, String appName){
+    IApplication findApplication = ServerFactory.getServer().getApplicationConfigurationManager().findApplication(appName);
+    return findApplication != null && findApplication.getSecurityContext().findUser(userName) != null;
+  }
+  
+  private static boolean isUserAlreadySaved(List<User> dbUsers, User saveUser){
+    return CollectionUtils
+        .emptyIfNull(dbUsers)
+        .stream()
+        .filter(userFromDb -> StringUtils.equals(userFromDb.getUserName(), saveUser.getUserName()) 
+            && StringUtils.equals(userFromDb.getApplicationName(), saveUser.getApplicationName()))
+        .findAny()
+        .isPresent();
   }
 }
