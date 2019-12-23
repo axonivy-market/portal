477b99bd6120dfce9925dc25992bb45e1b1b1f4e
ttthai_wawa <hai.tran@axonacitve.com>
2019-12-23 18:34:22 +0700

    IVYPORTAL-7074 Enhance process List - Creating and Categorization - Implement create external link process

---
+++ AxonIvyPortal/PortalKit/src/ch/ivy/addon/portalkit/bean/ExternalLinkBean.java
@@ -0,0 +1,49 @@
+package ch.ivy.addon.portalkit.bean;
+
+import java.io.IOException;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
+
+import org.primefaces.PrimeFaces;
+
+import ch.ivy.addon.portalkit.bo.ExternalLink;
+import ch.ivy.addon.portalkit.service.ExternalLinkService;
+import ch.ivyteam.ivy.environment.Ivy;
+
+@ManagedBean
+@ViewScoped
+public class ExternalLinkBean {
+  private ExternalLink externalLink;
+  private ExternalLinkService externaLinkService;
+  
+  @PostConstruct
+  public void init() {
+    externaLinkService = new ExternalLinkService();
+  }
+  
+  public void addNewExternalLink(String clientId) {
+    externalLink =  new ExternalLink();
+    PrimeFaces.current().resetInputs(clientId + ":add-external-link-form");
+  }
+  
+  public void saveNewExternalLink() {
+    externalLink.setUsername(Ivy.session().getSessionUser().getName());
+    externaLinkService.save(externalLink);
+  }
+  
+  public void startExternalLink(ExternalLink selectedExternalLink) throws IOException {
+    FacesContext.getCurrentInstance().getExternalContext().redirect(selectedExternalLink.getLink());
+  }
+
+  public ExternalLink getExternalLink() {
+    return externalLink;
+  }
+
+  public void setExternalLink(ExternalLink externalLink) {
+    this.externalLink = externalLink;
+  }
+  
+}
---
+++ AxonIvyPortal/PortalKit/src/ch/ivy/addon/portalkit/bo/ExternalLink.java
@@ -0,0 +1,78 @@
+package ch.ivy.addon.portalkit.bo;
+
+import java.util.Objects;
+
+import ch.ivy.addon.portalkit.persistence.domain.BusinessEntity;
+
+public class ExternalLink extends BusinessEntity{
+
+  private String name;
+  private String link;
+  private String username;
+  private boolean isPublic;
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getLink() {
+    return link;
+  }
+
+  public void setLink(String link) {
+    this.link = link;
+  }
+
+  public boolean isPublic() {
+    return isPublic;
+  }
+
+  public void setPublic(boolean isPublic) {
+    this.isPublic = isPublic;
+  }
+  
+  public String getUsername() {
+    return username;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+  
+  @Override
+  public String toString() {
+    return "ExternalLink {userName=" + username + ", name=" + name + ", link=" + link + ", isPublic=" + isPublic + "}";
+  }
+  
+  @Override
+  public int hashCode() {
+    return Objects.hash(name, link, username, isPublic);
+  }
+  
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj) {
+      return true;
+    }
+    if (obj == null) {
+      return false;
+    }
+    if (getClass() != obj.getClass()) {
+      return false;
+    }
+    ExternalLink other = (ExternalLink) obj;
+    if (id == null) {
+      if (other.getId() != null) {
+        return false;
+      }
+    } else if (!id.equals(other.getId())) {
+      return false;
+    }
+    return true;
+  }
+
+}
