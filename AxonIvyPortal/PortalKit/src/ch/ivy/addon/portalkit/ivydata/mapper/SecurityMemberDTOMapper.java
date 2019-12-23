6dd04e9bb99249a0caf62997a4ce26c4571b5ee1
Nam Le Dinh <nam.ledinh@axonactive.com>
2019-12-23 17:48:31 +0700

    IVYPORTAL-9347: Analyse and Improve Portal performance II - Refactor IUser, IRole, ISecurityMember to DTO to fix performance problem when search in autocomplete

---
+++ AxonIvyPortal/PortalKit/src/ch/ivy/addon/portalkit/ivydata/mapper/SecurityMemberDTOMapper.java
@@ -0,0 +1,63 @@
+package ch.ivy.addon.portalkit.ivydata.mapper;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import ch.ivy.addon.portalkit.dto.RoleDTO;
+import ch.ivy.addon.portalkit.dto.SecurityMemberDTO;
+import ch.ivy.addon.portalkit.dto.UserDTO;
+
+public class SecurityMemberDTOMapper {
+  
+  public static SecurityMemberDTO mapFromUserDTO(UserDTO userDTO) {
+    SecurityMemberDTO result = new SecurityMemberDTO();
+    
+    result.setId(userDTO.getId());
+    result.setDisplayName(userDTO.getDisplayName());
+    result.setMemberName(userDTO.getMemberName());
+    result.setName(userDTO.getName());
+    result.setUser(true);
+    
+    return result;
+  }
+  
+  public static List<SecurityMemberDTO> mapFromUserDTOs(List<UserDTO> userDTOs) {
+    List<SecurityMemberDTO> result = new ArrayList<>();
+
+    for (UserDTO m : userDTOs) {
+      SecurityMemberDTO member = mapFromUserDTO(m);
+
+      if (null != member) {
+        result.add(member);
+      }
+    }
+
+    return result;
+  }
+  
+  public static SecurityMemberDTO mapFromRoleDTO(RoleDTO roleDTO) {
+    SecurityMemberDTO result = new SecurityMemberDTO();
+    
+    result.setId(roleDTO.getId());
+    result.setDisplayName(roleDTO.getDisplayName());
+    result.setMemberName(roleDTO.getMemberName());
+    result.setName(roleDTO.getName());
+    result.setUser(false);
+    
+    return result;
+  }
+  
+  public static List<SecurityMemberDTO> mapFromRoleDTOs(List<RoleDTO> roleDTOs) {
+    List<SecurityMemberDTO> result = new ArrayList<>();
+
+    for (RoleDTO m : roleDTOs) {
+      SecurityMemberDTO member = mapFromRoleDTO(m);
+
+      if (null != member) {
+        result.add(member);
+      }
+    }
+
+    return result;
+  }
+}
