d6ada8d66d885a088645c16bc66e8d2ef9012996
Nam Le Dinh <nam.ledinh@axonactive.com>
2019-12-30 11:14:41 +0700

    IVYPORTAL-9348: Various or urgent requests in IVYPORTAL S185 - Fix TaskAnalysis tests due to functional changes - Stabilize + correct method names for announcement test
    Former-commit-id: c5125cf72c6c1996a56886636711a7c2c482f1fb

--- AxonIvyPortal/PortalTest/src_test/portal/guitest/test/AnnouncementTest.java
+++ AxonIvyPortal/PortalTest/src_test/portal/guitest/test/AnnouncementTest.java
@@ -24,7 +24,7 @@ public class AnnouncementTest extends BaseTest {
   }
 
   @Test
-  public void notificationCanBeChangeLanguage() {
+  public void testNotificationLocalizationWhenChangingLanguageSetting() {
     HomePage homePage = new HomePage();
     AdminSettingsPage adminSettingsPage = homePage.openAdminSettings();
     AnnouncementPage announcementPage = adminSettingsPage.openAnnouncementTab();
@@ -48,7 +48,7 @@ public class AnnouncementTest extends BaseTest {
   }
 
   @Test
-  public void validationDefaultLanguage() {
+  public void testValidationForDefaultLanguage() {
     HomePage homePage = new HomePage();
 
     AdminSettingsPage adminSettingsPage = homePage.openAdminSettings();
@@ -59,13 +59,12 @@ public class AnnouncementTest extends BaseTest {
   }
 
   @Test
-  public void shouldNotification() {
+  public void testShouldDisplayNotification() {
     HomePage homePage = new HomePage();
     AdminSettingsPage adminSettingsPage = homePage.openAdminSettings();
     AnnouncementPage announcementPage = adminSettingsPage.openAnnouncementTab();
     assertTrue("Admin Settings dialog is displayed", announcementPage.isDisplayed());
     
-    adminSettingsPage.openAnnouncementTab();
     announcementPage.setAnnoucement(1, "Readme1");
     announcementPage.publish();
     adminSettingsPage.closeAdminSettingDialog();
@@ -75,12 +74,11 @@ public class AnnouncementTest extends BaseTest {
   }
 
   @Test
-  public void depulishNotification() {
+  public void testDepulishNotification() {
     HomePage homePage = new HomePage();
     AdminSettingsPage adminSettingsPage = homePage.openAdminSettings();
     AnnouncementPage announcementPage = adminSettingsPage.openAnnouncementTab();
-    assertTrue("Admin Settings dialog is displayed", announcementPage.isDisplayed());
-    adminSettingsPage.openAnnouncementTab();
+    assertTrue("Admin Settings dialog is not displayed", announcementPage.isDisplayed());
 
     announcementPage.setAnnoucement(0, "Readme");
     announcementPage.setAnnoucement(1, "Readme1");
@@ -88,6 +86,6 @@ public class AnnouncementTest extends BaseTest {
     announcementPage.setAnnoucement(3, "Readme3");
     announcementPage.publish();
     announcementPage.dePublish();
-    assertTrue("Announcement is not display ", homePage.isAnnouncementMessageNotDisplayed());
+    assertTrue("Announcement is displaying ", homePage.isAnnouncementMessageNotDisplayed());
   }
 }
