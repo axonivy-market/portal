cd3be9f4c32e82ae6aeaa4e0e9fc97d0f3a9535e
nam.mai <nam.mai@axonactive.com>
2019-12-30 16:30:59 +0700

    IVYPORTAL-7219: Create Configuration File for Developers - PortalSettings and Global Variables - Handled feedback: Move loader to PortalPermissionInitBean - Handled feedback: Update name of config file - Handled feedback: Update document - Handled feedback: Portal settings in Admin setting does not take effect

--- AxonIvyPortal/PortalKit/src/ch/ivy/addon/portalkit/loader/PortalSettingLoader.java
+++ AxonIvyPortal/PortalKit/src/ch/ivy/addon/portalkit/loader/PortalSettingLoader.java
@@ -13,46 +13,41 @@ import org.apache.commons.lang3.StringUtils;
 import ch.ivy.addon.portalkit.enums.GlobalVariable;
 import ch.ivy.addon.portalkit.persistence.domain.GlobalSetting;
 import ch.ivy.addon.portalkit.service.GlobalSettingService;
-import ch.ivy.addon.portalkit.service.IvyCacheService;
 import ch.ivyteam.ivy.environment.Ivy;
-import ch.ivyteam.ivy.server.restricted.EngineMode;
 
 public class PortalSettingLoader {
-  private static final String PORTAL_CONFIG = "/portal_config.properties";
+  private static final String PORTAL_SETTINGS_CONFIG = "/portal_settings_config.properties";
 
   public void loadPortalSettings() {
-    if (EngineMode.isEmbeddedInDesigner()) {
-      Properties properties = new Properties();
+    Properties properties = new Properties();
 
-      try {
-        InputStream inputStream = PortalSettingLoader.class.getResourceAsStream(PORTAL_CONFIG);
-        if (inputStream == null) {
-          return;
-        }
-        properties.load(inputStream);
-      } catch (FileNotFoundException e) {
-        Ivy.log().error("Error when load property file", e);
-        return;
-      } catch (IOException e) {
-        Ivy.log().error("Error when load property file", e);
+    try {
+      InputStream inputStream = PortalSettingLoader.class.getResourceAsStream(PORTAL_SETTINGS_CONFIG);
+      if (inputStream == null) {
         return;
       }
-
-      GlobalSettingService globalSettingService = new GlobalSettingService();
-      List<GlobalSetting> globalSettings = globalSettingService.findAllGlobalSetting();
-
-      for (Entry<Object, Object> property : properties.entrySet()) {
-        String key = Optional.ofNullable(GlobalVariable.valueOf(property.getKey().toString()).name()).orElse("");
-        String value = Optional.ofNullable(property.getValue().toString()).orElse(null);
-
-        if (StringUtils.isNotBlank(key) && StringUtils.isNotBlank(value)) {
-          GlobalSetting setting = globalSettings.stream().filter(s -> StringUtils.equals(s.getKey(), key)).findFirst().get();
-          setting.setValue(value);
-          IvyCacheService.newInstance().cacheGlobalSetting(key, value);
-        }
-      }
-
-      globalSettingService.saveAll(globalSettings);
+      properties.load(inputStream);
+    } catch (FileNotFoundException e) {
+      Ivy.log().error("Error when load property file", e);
+      return;
+    } catch (IOException e) {
+      Ivy.log().error("Error when load property file", e);
+      return;
     }
+
+    GlobalSettingService globalSettingService = new GlobalSettingService();
+    List<GlobalSetting> globalSettings = globalSettingService.findAllGlobalSetting();
+
+    for (Entry<Object, Object> property : properties.entrySet()) {
+      String key = Optional.ofNullable(GlobalVariable.valueOf(property.getKey().toString()).name()).orElse("");
+      String value = Optional.ofNullable(property.getValue().toString()).orElse(null);
+
+      if (StringUtils.isNotBlank(key) && StringUtils.isNotBlank(value)) {
+        GlobalSetting setting = globalSettings.stream().filter(s -> StringUtils.equals(s.getKey(), key)).findFirst().get();
+        setting.setValue(value);
+      }
+    }
+
+    globalSettingService.saveAll(globalSettings);
   }
 }
