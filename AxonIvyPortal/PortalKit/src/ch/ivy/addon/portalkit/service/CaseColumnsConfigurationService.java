5e560d59b9a17948c61b54280c4e9a840b07559f
Nguyen Quoc Hoan <hoan.nguyen01@axonactive.com>
2020-01-17 11:12:16 +0700

    IVYPORTAL-7778: Error when clean up obsolete data - added try catch block for executing query

--- AxonIvyPortal/PortalKit/src/ch/ivy/addon/portalkit/service/CaseColumnsConfigurationService.java
+++ AxonIvyPortal/PortalKit/src/ch/ivy/addon/portalkit/service/CaseColumnsConfigurationService.java
@@ -1,10 +1,12 @@
 package ch.ivy.addon.portalkit.service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import ch.ivy.addon.portalkit.bo.CaseColumnsConfiguration;
 import ch.ivyteam.ivy.business.data.store.search.Filter;
 import ch.ivyteam.ivy.business.data.store.search.Result;
+import ch.ivyteam.ivy.environment.Ivy;
 
 public class CaseColumnsConfigurationService extends BusinessDataService<CaseColumnsConfiguration> {
 
@@ -23,15 +25,25 @@ public class CaseColumnsConfigurationService extends BusinessDataService<CaseCol
   }
 
   public long getTotalCount(Long applicationId) {
-    Filter<CaseColumnsConfiguration> query =
-        repo().search(getType()).numberField("applicationId").isEqualTo(applicationId);
-    return query.limit(LIMIT_100).execute().totalCount();
+    try {
+      Filter<CaseColumnsConfiguration> query =
+          repo().search(getType()).numberField("applicationId").isEqualTo(applicationId);
+      return query.execute().totalCount();
+    } catch (Exception e) {
+      Ivy.log().error(e);
+      return 0;
+    }
   }
 
   public List<CaseColumnsConfiguration> getAllConfiguration(Long applicationId, int limitFrom, int limitSize) {
-    Filter<CaseColumnsConfiguration> query =
-        repo().search(getType()).numberField("applicationId").isEqualTo(applicationId);
-    Result<CaseColumnsConfiguration> queryResult = query.limit(limitFrom, limitSize).execute();
-    return queryResult.getAll();
+    try {
+      Filter<CaseColumnsConfiguration> query =
+          repo().search(getType()).numberField("applicationId").isEqualTo(applicationId);
+      Result<CaseColumnsConfiguration> queryResult = query.limit(limitFrom, limitSize).execute();
+      return queryResult.getAll();
+    } catch (Exception e) {
+      Ivy.log().error(e);
+      return new ArrayList<>();
+    }
   }
 }
--- AxonIvyPortal/PortalKit/src/ch/ivy/addon/portalkit/service/TaskColumnsConfigurationService.java
+++ AxonIvyPortal/PortalKit/src/ch/ivy/addon/portalkit/service/TaskColumnsConfigurationService.java
@@ -1,10 +1,12 @@
 package ch.ivy.addon.portalkit.service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import ch.ivy.addon.portalkit.bo.TaskColumnsConfiguration;
 import ch.ivyteam.ivy.business.data.store.search.Filter;
 import ch.ivyteam.ivy.business.data.store.search.Result;
+import ch.ivyteam.ivy.environment.Ivy;
 
 public class TaskColumnsConfigurationService extends BusinessDataService<TaskColumnsConfiguration> {
 
@@ -21,15 +23,25 @@ public class TaskColumnsConfigurationService extends BusinessDataService<TaskCol
   }
 
   public long getTotalCount(Long applicationId) {
-    Filter<TaskColumnsConfiguration> query =
-        repo().search(getType()).numberField("applicationId").isEqualTo(applicationId);
-    return query.limit(LIMIT_10).execute().totalCount();
+    try {
+      Filter<TaskColumnsConfiguration> query =
+          repo().search(getType()).numberField("applicationId").isEqualTo(applicationId);
+      return query.execute().totalCount();
+    } catch (Exception e) {
+      Ivy.log().error(e);
+      return 0;
+    }
   }
 
   public List<TaskColumnsConfiguration> getAllConfiguration(Long applicationId, int limitFrom, int limitSize) {
-    Filter<TaskColumnsConfiguration> query =
-        repo().search(getType()).numberField("applicationId").isEqualTo(applicationId);
-    Result<TaskColumnsConfiguration> queryResult = query.limit(limitFrom, limitSize).execute();
-    return queryResult.getAll();
+    try {
+      Filter<TaskColumnsConfiguration> query =
+          repo().search(getType()).numberField("applicationId").isEqualTo(applicationId);
+      Result<TaskColumnsConfiguration> queryResult = query.limit(limitFrom, limitSize).execute();
+      return queryResult.getAll();
+    } catch (Exception e) {
+      Ivy.log().error(e);
+      return new ArrayList<>();
+    }
   }
 }
