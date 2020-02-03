d1a516b81e9112bfb480b53203da3a415fc59eca
Long Do <long.do@axonactive.com>
2020-02-03 17:13:09 +0700

    IVYPORTAL-9599 Various or urgent requests in IVYPORTAL S186: + Fixed: Wrong message for create the second upload form in express

--- AxonIvyPortal/AxonIvyExpress/src/ch/ivy/gawfs/DragAndDropController.java
+++ AxonIvyPortal/AxonIvyExpress/src/ch/ivy/gawfs/DragAndDropController.java
@@ -16,260 +16,244 @@ import ch.ivyteam.ivy.environment.Ivy;
 
 @ManagedBean(name = "dragAndDropController")
 @ViewScoped
-public class DragAndDropController implements Serializable{
+public class DragAndDropController implements Serializable {
 
-	private static final FormElementType FORM_ELEMENT_FILE_UPLOAD = FormElementType.FILE_UPLOAD;
+  private static final FormElementType FORM_ELEMENT_FILE_UPLOAD = FormElementType.FILE_UPLOAD;
 
-	private static final long serialVersionUID = 2896675125359151717L;
+  private static final long serialVersionUID = 2896675125359151717L;
 
-	private List<Formelement> availableFormelements;    
-	private List<Formelement> selectedFormelementsHeader;
-	private List<Formelement> selectedFormelementsLeftPanel;
-	private List<Formelement> selectedFormelementsRightPanel;
-	private List<Formelement> selectedFormelementsFooter;
-    
-	private List<Formelement> finalUsedFormelements;
-	
-    private Formelement selectedFormelement;
-    
-    private DynaFormController dynaFormController;
-      
-    
-    public DragAndDropController() {
-    	availableFormelements = new ArrayList<>();
-    	selectedFormelementsHeader = new ArrayList<>();
-    	selectedFormelementsLeftPanel = new ArrayList<>();
-    	selectedFormelementsRightPanel = new ArrayList<>();
-    	selectedFormelementsFooter = new ArrayList<>();
-    	finalUsedFormelements = new ArrayList<>();
-	}
-    
-    
-    public void deleteFormelement(Formelement formelement){
-    	availableFormelements.remove(formelement);
-    	updateForm();
-    }
+  private List<Formelement> availableFormelements;
+  private List<Formelement> selectedFormelementsHeader;
+  private List<Formelement> selectedFormelementsLeftPanel;
+  private List<Formelement> selectedFormelementsRightPanel;
+  private List<Formelement> selectedFormelementsFooter;
 
-     
-	public void onFieldDropHeader(DragDropEvent ddEvent) {
-		Formelement droppedFormelement = ((Formelement) ddEvent.getData());
+  private List<Formelement> finalUsedFormelements;
 
-		if (droppedFormelement.getType().equals(FORM_ELEMENT_FILE_UPLOAD)) {
-			for (Formelement element : selectedFormelementsHeader) {
-				if (element.getType().equals(FORM_ELEMENT_FILE_UPLOAD)) {
-					displayFileUploadPositionNotice();
-					return;
-				}
-				
-			}
-			for (Formelement element : selectedFormelementsFooter) {
-				if (element.getType().equals(FORM_ELEMENT_FILE_UPLOAD)) {
-					displayFileUploadPositionNotice();
-					return;
-				}
-				
-			}
-			
-		}
-		
+  private Formelement selectedFormelement;
 
-		selectedFormelementsHeader.add(droppedFormelement);
-		availableFormelements.remove(droppedFormelement);
+  private DynaFormController dynaFormController;
 
-		updateForm();
-	}
-    
-    
-    public void resetFormelementHeader(Formelement formelement){
-    	availableFormelements.add(formelement);
-    	selectedFormelementsHeader.remove(formelement);
-    	
-    	updateForm();
-    }
-    
-    
-    
-    public void onFieldDropLeftPanel(DragDropEvent ddEvent) {
-        Formelement droppedFormelement = ((Formelement) ddEvent.getData());
-        
-        if(droppedFormelement.getType().equals(FORM_ELEMENT_FILE_UPLOAD)){
-         	 displayFileUploadPositionNotice();           
-         	return;
-       }
-  
-        selectedFormelementsLeftPanel.add(droppedFormelement);
-        availableFormelements.remove(droppedFormelement);
-        
-        updateForm();        
-    }
-    
-    public void resetFormelementLeftPanel(Formelement formelement){
-    	availableFormelements.add(formelement);
-    	selectedFormelementsLeftPanel.remove(formelement);
-    	
-    	updateForm();
-    }
-    
-    public void onFieldDropRightPanel(DragDropEvent ddEvent) {
-        Formelement droppedFormelement = ((Formelement) ddEvent.getData());
-        
-        if(droppedFormelement.getType().equals(FORM_ELEMENT_FILE_UPLOAD)){
-        	 displayFileUploadPositionNotice();                    
-          	return;
+
+  public DragAndDropController() {
+    availableFormelements = new ArrayList<>();
+    selectedFormelementsHeader = new ArrayList<>();
+    selectedFormelementsLeftPanel = new ArrayList<>();
+    selectedFormelementsRightPanel = new ArrayList<>();
+    selectedFormelementsFooter = new ArrayList<>();
+    finalUsedFormelements = new ArrayList<>();
+  }
+
+
+  public void deleteFormelement(Formelement formelement) {
+    availableFormelements.remove(formelement);
+    updateForm();
+  }
+
+  public void onFieldDropHeader(DragDropEvent ddEvent) {
+    Formelement droppedFormelement = ((Formelement) ddEvent.getData());
+
+    if (droppedFormelement.getType().equals(FORM_ELEMENT_FILE_UPLOAD)) {
+      for (Formelement element : selectedFormelementsHeader) {
+        if (element.getType().equals(FORM_ELEMENT_FILE_UPLOAD)) {
+          displayMaximumFileUploadElementError();
+          return;
         }
+      }
+      for (Formelement element : selectedFormelementsFooter) {
+        if (element.getType().equals(FORM_ELEMENT_FILE_UPLOAD)) {
+          displayMaximumFileUploadElementError();
+          return;
+        }
+      }
+    }
+    selectedFormelementsHeader.add(droppedFormelement);
+    availableFormelements.remove(droppedFormelement);
+
+    updateForm();
+  }
+
+
+  public void resetFormelementHeader(Formelement formelement) {
+    availableFormelements.add(formelement);
+    selectedFormelementsHeader.remove(formelement);
+
+    updateForm();
+  }
+
+  public void onFieldDropLeftPanel(DragDropEvent ddEvent) {
+    Formelement droppedFormelement = ((Formelement) ddEvent.getData());
+
+    if (droppedFormelement.getType().equals(FORM_ELEMENT_FILE_UPLOAD)) {
+      displayFileUploadPositionNotice();
+      return;
+    }
+
+    selectedFormelementsLeftPanel.add(droppedFormelement);
+    availableFormelements.remove(droppedFormelement);
+
+    updateForm();
+  }
+
+  public void resetFormelementLeftPanel(Formelement formelement) {
+    availableFormelements.add(formelement);
+    selectedFormelementsLeftPanel.remove(formelement);
+
+    updateForm();
+  }
+
+  public void onFieldDropRightPanel(DragDropEvent ddEvent) {
+    Formelement droppedFormelement = ((Formelement) ddEvent.getData());
+
+    if (droppedFormelement.getType().equals(FORM_ELEMENT_FILE_UPLOAD)) {
+      displayFileUploadPositionNotice();
+      return;
+    }
+
+    selectedFormelementsRightPanel.add(droppedFormelement);
+    availableFormelements.remove(droppedFormelement);
+
+    updateForm();
+  }
+
+  public void resetFormelementRightPanel(Formelement formelement) {
+    availableFormelements.add(formelement);
+    selectedFormelementsRightPanel.remove(formelement);
+
+    updateForm();
+  }
+
+  public void onFieldDropFooter(DragDropEvent ddEvent) {
+    Formelement droppedFormelement = ((Formelement) ddEvent.getData());
+
+    if (droppedFormelement.getType().equals(FORM_ELEMENT_FILE_UPLOAD)) {
+      for (Formelement element : selectedFormelementsHeader) {
+        if (element.getType().equals(FORM_ELEMENT_FILE_UPLOAD)) {
+          displayMaximumFileUploadElementError();
+          return;
+        }
+
+      }
+      for (Formelement element : selectedFormelementsFooter) {
+        if (element.getType().equals(FORM_ELEMENT_FILE_UPLOAD)) {
+          displayMaximumFileUploadElementError();
+          return;
+        }
+      }
+    }
+    selectedFormelementsFooter.add(droppedFormelement);
+    availableFormelements.remove(droppedFormelement);
+    updateForm();
+  }
+
+  private void displayFileUploadPositionNotice() {
+    FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO,
+        Ivy.cms().co("/Dialogs/components/CaseDocument/fileUploadElementPosition"), "");
+    FacesContext.getCurrentInstance().addMessage("available-form-elements", message);
+  }
   
-        selectedFormelementsRightPanel.add(droppedFormelement);
-        availableFormelements.remove(droppedFormelement);
-        
-        updateForm();
-    }
-    
-    public void resetFormelementRightPanel(Formelement formelement){
-    	availableFormelements.add(formelement);
-    	selectedFormelementsRightPanel.remove(formelement);
-    	
-    	updateForm();
-    }
-    
-    
-    public void onFieldDropFooter(DragDropEvent ddEvent) {
-        Formelement droppedFormelement = ((Formelement) ddEvent.getData());
-        
-    	if (droppedFormelement.getType().equals(FORM_ELEMENT_FILE_UPLOAD)) {
-			for (Formelement element : selectedFormelementsHeader) {
-				if (element.getType().equals(FORM_ELEMENT_FILE_UPLOAD)) {
-					displayFileUploadPositionNotice();
-					return;
-				}
-				
-			}
-			for (Formelement element : selectedFormelementsFooter) {
-				if (element.getType().equals(FORM_ELEMENT_FILE_UPLOAD)) {
-					 displayFileUploadPositionNotice();          
-					return;
-				}
-				
-			}
-			
-		}
-        
-       
-  
-        selectedFormelementsFooter.add(droppedFormelement);
-        availableFormelements.remove(droppedFormelement);
-        
-        updateForm();
-        
-    }
+  public void resetFormelementFooter(Formelement formelement) {
+    availableFormelements.add(formelement);
+    selectedFormelementsFooter.remove(formelement);
 
-	private void displayFileUploadPositionNotice() {
-		FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, Ivy.cms().co("/Dialogs/components/CaseDocument/fileUploadElementPosition"), "");
-		  FacesContext.getCurrentInstance().addMessage("available-form-elements", message);
-	}
-    
-    public void resetFormelementFooter(Formelement formelement){
-    	availableFormelements.add(formelement);
-    	selectedFormelementsFooter.remove(formelement);
-    	
-    	updateForm();
-    }
+    updateForm();
+  }
+
+  private void displayMaximumFileUploadElementError() {
+    FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_ERROR,
+        Ivy.cms().co("/Dialogs/components/CaseDocument/maximumFileUploadElement"), "");
+    FacesContext.getCurrentInstance().addMessage("available-form-elements", message);
+  }
+
+  public List<Formelement> getAvailableFormelements() {
+    return availableFormelements;
+  }
 
 
-	public List<Formelement> getAvailableFormelements() {
-		return availableFormelements;
-	}
+  public void setAvailableFormelements(List<Formelement> availableFormelements) {
+    this.availableFormelements = availableFormelements;
+  }
 
 
-	public void setAvailableFormelements(List<Formelement> availableFormelements) {
-		this.availableFormelements = availableFormelements;
-	}
+  public List<Formelement> getSelectedFormelementsHeader() {
+    return selectedFormelementsHeader;
+  }
 
 
-	public List<Formelement> getSelectedFormelementsHeader() {
-		return selectedFormelementsHeader;
-	}
+  public void setSelectedFormelementsHeader(List<Formelement> selectedFormelementsHeader) {
+    this.selectedFormelementsHeader = selectedFormelementsHeader;
+  }
 
 
-	public void setSelectedFormelementsHeader(
-			List<Formelement> selectedFormelementsHeader) {
-		this.selectedFormelementsHeader = selectedFormelementsHeader;
-	}
+  public List<Formelement> getSelectedFormelementsLeftPanel() {
+    return selectedFormelementsLeftPanel;
+  }
 
 
-	public List<Formelement> getSelectedFormelementsLeftPanel() {
-		return selectedFormelementsLeftPanel;
-	}
+  public void setSelectedFormelementsLeftPanel(List<Formelement> selectedFormelementsLeftPanel) {
+    this.selectedFormelementsLeftPanel = selectedFormelementsLeftPanel;
+  }
 
 
-	public void setSelectedFormelementsLeftPanel(
-			List<Formelement> selectedFormelementsLeftPanel) {
-		this.selectedFormelementsLeftPanel = selectedFormelementsLeftPanel;
-	}
+  public List<Formelement> getSelectedFormelementsRightPanel() {
+    return selectedFormelementsRightPanel;
+  }
 
 
-	public List<Formelement> getSelectedFormelementsRightPanel() {
-		return selectedFormelementsRightPanel;
-	}
+  public void setSelectedFormelementsRightPanel(List<Formelement> selectedFormelementsRightPanel) {
+    this.selectedFormelementsRightPanel = selectedFormelementsRightPanel;
+  }
 
 
-	public void setSelectedFormelementsRightPanel(
-			List<Formelement> selectedFormelementsRightPanel) {
-		this.selectedFormelementsRightPanel = selectedFormelementsRightPanel;
-	}
+  public List<Formelement> getSelectedFormelementsFooter() {
+    return selectedFormelementsFooter;
+  }
 
 
-	public List<Formelement> getSelectedFormelementsFooter() {
-		return selectedFormelementsFooter;
-	}
+  public void setSelectedFormelementsFooter(List<Formelement> selectedFormelementsFooter) {
+    this.selectedFormelementsFooter = selectedFormelementsFooter;
+  }
 
 
-	public void setSelectedFormelementsFooter(
-			List<Formelement> selectedFormelementsFooter) {
-		this.selectedFormelementsFooter = selectedFormelementsFooter;
-	}
+  public Formelement getSelectedFormelement() {
+    return selectedFormelement;
+  }
 
 
-	public Formelement getSelectedFormelement() {
-		return selectedFormelement;
-	}
+  public void setSelectedFormelement(Formelement selectedFormelement) {
+    this.selectedFormelement = selectedFormelement;
+  }
 
 
-	public void setSelectedFormelement(Formelement selectedFormelement) {
-		this.selectedFormelement = selectedFormelement;
-	}
+  public DynaFormController getDynaFormController() {
+    return dynaFormController;
+  }
 
 
-	public DynaFormController getDynaFormController() {
-		return dynaFormController;
-	}
+  public void setDynaFormController(DynaFormController dynaFormController) {
+    this.dynaFormController = dynaFormController;
+  }
+
+  public void updateForm() {
+    dynaFormController.createForm();
+  }
 
 
-	public void setDynaFormController(DynaFormController dynaFormController) {
-		this.dynaFormController = dynaFormController;
-	}
-	
-	public void updateForm(){
-		dynaFormController.createForm();
-	}
+  public List<Formelement> getFinalUsedFormelements() {
+    return finalUsedFormelements;
+  }
 
 
-	public List<Formelement> getFinalUsedFormelements() {
-		return finalUsedFormelements;
-	}
+  public void setFinalUsedFormelements(List<Formelement> finalUsedFormelements) {
+    this.finalUsedFormelements = finalUsedFormelements;
+  }
 
+  public void addFinalUsedFormelements(Formelement formelement) {
+    this.finalUsedFormelements.add(formelement);
+  }
 
-	public void setFinalUsedFormelements(List<Formelement> finalUsedFormelements) {
-		this.finalUsedFormelements = finalUsedFormelements;
-	}
-	
-	public void addFinalUsedFormelements(Formelement formelement) {
-		this.finalUsedFormelements.add(formelement);
-	}
-
-	public boolean isNotDefined() {
-	  return selectedFormelementsHeader.isEmpty()
-	      && selectedFormelementsLeftPanel.isEmpty()
-	      && selectedFormelementsRightPanel.isEmpty()
-	      && selectedFormelementsFooter.isEmpty();
-	}
+  public boolean isNotDefined() {
+    return selectedFormelementsHeader.isEmpty() && selectedFormelementsLeftPanel.isEmpty()
+        && selectedFormelementsRightPanel.isEmpty() && selectedFormelementsFooter.isEmpty();
+  }
 }
