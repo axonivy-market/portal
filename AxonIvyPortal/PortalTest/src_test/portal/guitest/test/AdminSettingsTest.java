603740d7dea82e03170a2d175703648d78bd5952
Thang Van <thang.nguyenvan@axonactive.com>
2020-02-07 11:59:40 +0700

    IVYPORTAL-9735: Configurable Environment Info - Create GUI test

--- AxonIvyPortal/PortalTest/src_test/portal/guitest/test/AdminSettingsTest.java
+++ AxonIvyPortal/PortalTest/src_test/portal/guitest/test/AdminSettingsTest.java
@@ -6,27 +6,48 @@ import static junit.framework.Assert.assertTrue;
 import org.junit.Test;
 
 import portal.guitest.common.BaseTest;
+import portal.guitest.common.SystemProperties;
 import portal.guitest.common.TestAccount;
 import portal.guitest.page.AdminSettingsPage;
 import portal.guitest.page.HomePage;
 
 public class AdminSettingsTest extends BaseTest {
 
-  @Test
-  public void whenLoginAsAdminThenAdminMenuItemDisplayed() {
-    redirectToRelativeLink(HomePage.PORTAL_HOME_PAGE_URL);
-    login(TestAccount.ADMIN_USER);
-    HomePage homePage = new HomePage();
-    assertTrue("Admin Settings menu item is not displayed", homePage.isAdminSettingsMenuItemPresent());
-    AdminSettingsPage adminSettingsPage = homePage.openAdminSettings();
-    assertTrue("Admin Settings dialog is not displayed", adminSettingsPage.isDisplayed());
-  }
+	@Test
+	public void whenLoginAsAdminThenAdminMenuItemDisplayed() {
+		redirectToRelativeLink(HomePage.PORTAL_HOME_PAGE_URL);
+		login(TestAccount.ADMIN_USER);
+		HomePage homePage = new HomePage();
+		assertTrue("Admin Settings menu item is not displayed", homePage.isAdminSettingsMenuItemPresent());
+		AdminSettingsPage adminSettingsPage = homePage.openAdminSettings();
+		assertTrue("Admin Settings dialog is not displayed", adminSettingsPage.isDisplayed());
+	}
 
-  @Test
-  public void whenLoginAsNonAdminThenAdminMenuItemNotDisplayed() {
-    redirectToRelativeLink(HomePage.PORTAL_HOME_PAGE_URL);
-    HomePage homePage = new HomePage();
-    assertFalse("Admin Settings menu item is displayed", homePage.isAdminSettingsMenuItemPresent());
-  }
-  
+	@Test
+	public void whenLoginAsNonAdminThenAdminMenuItemNotDisplayed() {
+		redirectToRelativeLink(HomePage.PORTAL_HOME_PAGE_URL);
+		HomePage homePage = new HomePage();
+		assertFalse("Admin Settings menu item is displayed", homePage.isAdminSettingsMenuItemPresent());
+	}
+
+	@Test
+	public void testDefaultEnvironmentInfo() {
+		redirectToRelativeLink(HomePage.PORTAL_HOME_PAGE_URL);
+		login(TestAccount.ADMIN_USER);
+		HomePage homePage = new HomePage();
+		AdminSettingsPage adminSettingsPage = homePage.openAdminSettings();
+		adminSettingsPage.setEnviromentInfo();
+		assertTrue(homePage.getEnviromentInfo().contains("Host: " + SystemProperties.getServerName() + " Env:Default"));
+	}
+	
+	@Test
+	public void testCustomizedEnvironmentInfo() {
+		login(TestAccount.ADMIN_USER);
+		HomePage homePage = new HomePage();
+		AdminSettingsPage adminSettingsPage = homePage.openAdminSettings();
+		adminSettingsPage.setEnviromentInfo();
+		//Customize environment info in portal example 
+		redirectToRelativeLink(HomePage.PORTAL_EXAMPLES_PROCESS_CHAIN);
+		assertTrue(homePage.getEnviromentInfo().contains("Dev Team: Wawa, Env: Dev"));
+	}	
 }
