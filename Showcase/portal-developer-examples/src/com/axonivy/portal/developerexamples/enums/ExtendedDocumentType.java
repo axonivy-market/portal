a7d6a07186daf32836f44c39ba2485d50ea03a04
Nam Le Dinh <nam.ledinh@axonactive.com>
2020-02-10 11:37:16 +0700

    IVYPORTAL-9629: New PortalExamples Project for Business users - Rename project PortalExamples to portal-developer-examples - Refactor artifactId and java package - Change version of all project from 8.0.0.0-SNAPSHOT to 9.1.0.0-SNAPSHOT - Delete PortalMobileTest project

---
+++ Showcase/portal-developer-examples/src/com/axonivy/portal/developerexamples/enums/ExtendedDocumentType.java
@@ -0,0 +1,8 @@
+package com.axonivy.portal.developerexamples.enums;
+
+import ch.ivy.addon.portalkit.enums.DocumentType;
+
+public enum ExtendedDocumentType implements DocumentType {
+
+  DOCUMENTATION, CONTRACT, INFORMATION, EMAIL, APPRAISAL, KPI, OTHERS;
+}
