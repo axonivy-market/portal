1b1ed2f222bd357ef46a536aa0315e5f8b674155
lttung_wawa <tung.le@axonactive.com>
2020-02-13 18:24:02 +0700

    IVYPORTAL-9788 Various or urgent requests in IVYPORTAL S187 - Fixed tests on Firefox

--- AxonIvyPortal/PortalTest/src_test/portal/guitest/test/ExpressManagementTest.java
+++ AxonIvyPortal/PortalTest/src_test/portal/guitest/test/ExpressManagementTest.java
@@ -13,6 +13,7 @@ import com.jayway.awaitility.Awaitility;
 import com.jayway.awaitility.Duration;
 
 import portal.guitest.common.BaseTest;
+import portal.guitest.common.FileHelper;
 import portal.guitest.common.Sleeper;
 import portal.guitest.common.TestAccount;
 import portal.guitest.page.AdminSettingsPage;
@@ -54,7 +55,7 @@ public class ExpressManagementTest extends BaseTest {
     adminSettingsPage.openExpressManagementTab();
     ExpressManagementPage expressManagementPage = new ExpressManagementPage();
     expressManagementPage.openImportDialog();
-    expressManagementPage.selectJSONFile("express-test.json");
+    expressManagementPage.selectJSONFile(FileHelper.getAbsolutePathToTestFile("express-test.json"));
     expressManagementPage.clickOnDeployExpress();
     Sleeper.sleep(5000);
     expressManagementPage.clickOnCloseButton();
