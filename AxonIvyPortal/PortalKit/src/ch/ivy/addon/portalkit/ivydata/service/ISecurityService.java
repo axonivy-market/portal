b63888bd0112cd57994edaac3bc6e099e43672d2
ttthai_wawa <hai.tran@axonacitve.com>
2020-02-19 16:34:12 +0700

    IVYPORTAL-9477: Create a User Selection Combobox Component - Handle feedback: Update doc and description + Update size for case creator filter

--- AxonIvyPortal/PortalKit/src/ch/ivy/addon/portalkit/ivydata/service/ISecurityService.java
+++ AxonIvyPortal/PortalKit/src/ch/ivy/addon/portalkit/ivydata/service/ISecurityService.java
@@ -8,7 +8,7 @@ import ch.ivyteam.ivy.application.IApplication;
 public interface ISecurityService {
 
   /**
-   * Finds the users which has the given roles and in the given applications
+   * Finds the users which have the given roles and in the given applications
    * @param query
    * @param apps
    * @param startIndex
@@ -19,7 +19,7 @@ public interface ISecurityService {
   IvySecurityResultDTO findUsers(String query, List<String> apps, int startIndex, int count, List<String> roleNames);
   
   /**
-   * Finds the users which has the given roles and in the given application
+   * Finds the users which have the given roles and in the given application
    * @param query
    * @param app
    * @param startIndex
