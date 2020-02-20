6120df3cb108171399bb8031323a157ae33d2277
Nam Le Dinh <nam.ledinh@axonactive.com>
2020-02-20 11:39:30 +0700

    IVYPORTAL-9629: New PortalExamples Project for Business users - Add more GUITest for leave request

--- AxonIvyPortal/PortalTest/src_test/portal/guitest/userexamples/page/LeaveRequestPage.java
+++ AxonIvyPortal/PortalTest/src_test/portal/guitest/userexamples/page/LeaveRequestPage.java
@@ -17,12 +17,48 @@ public class LeaveRequestPage extends TaskTemplatePage {
   
   public String clickSubmitAndGetValidationMsg() {
     clickSubmitLeaveRequest();
-    waitForElementExisted("span.ui-messages-error-summary", true, DEFAULT_TIMEOUT);
+    waitAjaxIndicatorDisappear();
     return getValidationMsg();
   }
   
   public String getValidationMsg() {
     List<WebElement> messages = findListElementsByCssSelector(("span.ui-messages-error-summary"));
     return StringUtils.join(messages.stream().map(WebElement::getText).collect(Collectors.toList()), ","); 
+  }
+  
+  public void enterLeaveRequestInformation(String leaveType, String from, String to, String approver, String requesterComment) {
+    selectLeaveType(leaveType);
+    enterKeys(findElementById("leave-request:from_input"), from);
+    enterKeys(findElementById("leave-request:to_input"), to);
+    enterKeys(findElementById("leave-request:requester-comment"), requesterComment);
+    selectApprover(approver);
+  }
+  
+  public void enterApproverComment(String approverComment) {
+    enterKeys(findElementById("leave-request:approver-comment"), approverComment);
+  }
+  
+  public void clickApproveBtn() {
+    click(By.id("leave-request:approved-btn"));
+    waitAjaxIndicatorDisappear();
+  }
+  
+  public void clickRejectBtn() {
+    click(By.id("leave-request:rejected-btn"));
+    waitAjaxIndicatorDisappear();
+  }
+  
+  private void selectLeaveType(String leaveType) {
+    findElementById("leave-request:leave-type").click();
+    String leaveTypeSelector = "li[data-label='" + leaveType + "']";
+    waitForElementDisplayed(By.cssSelector(leaveTypeSelector), true);
+    clickByCssSelector(leaveTypeSelector);
+  }
+  
+  private void selectApprover(String approver) {
+    findElementById("leave-request:approver").click();
+    String approverSelector = "li[data-label='" + approver + "']";
+    waitForElementDisplayed(By.cssSelector(approverSelector), true);
+    clickByCssSelector(approverSelector);
   }
 }
