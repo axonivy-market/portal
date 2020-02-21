3b0675d1827b35ef5bc6391c2c809615de7aca0d
Minh Nguyen <minh.nguyen02@axonactive.com>
2020-02-21 11:54:54 +0700

    IVYPORTAL-9629 New PortalExamples Project for Business users - Handle feedback
    Former-commit-id: 5ea733679b5ecbbfb43a64cf78d8fba391173287

--- AxonIvyPortal/PortalTest/src_test/portal/guitest/userexample/test/CaseMapTest.java
+++ AxonIvyPortal/PortalTest/src_test/portal/guitest/userexample/test/CaseMapTest.java
@@ -3,129 +3,98 @@ package portal.guitest.userexample.test;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import org.openqa.selenium.By;
 
 import portal.guitest.common.BaseTest;
+import portal.guitest.common.TestAccount;
 import portal.guitest.page.HomePage;
-import portal.guitest.page.ProcessWidgetPage;
-import portal.guitest.page.TaskTemplatePage;
 import portal.guitest.page.TaskWidgetPage;
 import portal.guitest.userexamples.page.CaseMapPage;
 
 public class CaseMapTest extends BaseTest {
 
-  
   private static final String CASE_MAP_URL = "/portal-user-examples/70765b37-a3e8-418a-a8d5-c2b3a539408e.icm";
-  private ProcessWidgetPage processWidget;
-  private HomePage homePage;
+  private CaseMapPage caseMapPage;
+  private TaskWidgetPage taskWidgetPage;
 
-  
   @Override
   @Before
   public void setup() {
     super.setup();
     redirectToRelativeLink(HomePage.PORTAL_HOME_PAGE_URL);
   }
-  
+
   @Test
   public void testCaseMapApprovalWorkflow() {
     redirectToRelativeLink(CASE_MAP_URL);
-    CaseMapPage caseMapPage = new CaseMapPage();
-    caseMapPage.inputFields("John", "Jack", "1.1.2019", "VN","20000", "To buy a new car", "80000", "100000");
+    caseMapPage = new CaseMapPage();
+    caseMapPage.inputFields("John", "Jack", "1.1.2019", "VN", "20000", "To buy a new car", "80000", "100000");
     caseMapPage.clickSubmitButton();
-    
-    TaskWidgetPage taskWidgetPage = new TaskWidgetPage();
-    taskWidgetPage.filterTasksBy("Verify Personal Data");
-    taskWidgetPage.startTaskWithoutUI(0);
-    homePage = new HomePage();
-    redirectToRelativeLink(HomePage.PORTAL_HOME_PAGE_URL);
-    
-    taskWidgetPage = new TaskWidgetPage();
-    taskWidgetPage.filterTasksBy("Internal Solvency Check");
-    taskWidgetPage.startTaskWithoutUI(0);
-    homePage = new HomePage();
-    redirectToRelativeLink(HomePage.PORTAL_HOME_PAGE_URL);
-    
+    startTaskWithoutUI("Verify Personal Data");
+    startTaskWithoutUI("Internal Solvency Check");
+    login(TestAccount.DEMO_USER);
     taskWidgetPage = new TaskWidgetPage();
     taskWidgetPage.filterTasksBy("Approve Level 1");
-    TaskTemplatePage taskTemplatePage = taskWidgetPage.startTask(0);
-    
-    Assert.assertEquals("John", caseMapPage.getCustomerName());
-    Assert.assertEquals("Jack", caseMapPage.getCustomerFirstName());
-    Assert.assertEquals("VN", caseMapPage.getCountry());
-    Assert.assertEquals("20000", caseMapPage.getAmount());
-    Assert.assertEquals("To buy a new car", caseMapPage.getReason());
-    Assert.assertEquals("80000", caseMapPage.getSalary());
-    Assert.assertEquals("100000", caseMapPage.getOtherCredits());
-    taskTemplatePage.clickByCssSelector("button[id$='form:approval-button']");
-    
-    taskWidgetPage.waitForElementDisplayed(By.cssSelector(".no-task-message"), true, 10);
+    taskWidgetPage.startTask(0);
+    assertInputData();
+    caseMapPage.clickApproveButton();
+    login(TestAccount.DEMO_USER);
     taskWidgetPage = new TaskWidgetPage();
     taskWidgetPage.filterTasksBy("Approve Level 2");
-    taskTemplatePage = taskWidgetPage.startTask(0);
-    
-    Assert.assertEquals("John", caseMapPage.getCustomerName());
-    Assert.assertEquals("Jack", caseMapPage.getCustomerFirstName());
-    Assert.assertEquals("VN", caseMapPage.getCountry());
-    Assert.assertEquals("20000", caseMapPage.getAmount());
-    Assert.assertEquals("To buy a new car", caseMapPage.getReason());
-    Assert.assertEquals("80000", caseMapPage.getSalary());
-    Assert.assertEquals("100000", caseMapPage.getOtherCredits());
-    taskTemplatePage.clickByCssSelector("button[id$='form:approval-button']");
+    taskWidgetPage.startTask(0);
+    assertInputData();
+    caseMapPage.clickApproveButton();
   }
-  
+
   @Test
   public void testCaseMapRejectedWorkflow() {
     redirectToRelativeLink(CASE_MAP_URL);
-    CaseMapPage caseMapPage = new CaseMapPage();
-    caseMapPage.inputFields("John", "Jack", "1.1.2019", "VN","20000", "To buy a new car", "80000", "100000");
+    caseMapPage = new CaseMapPage();
+    caseMapPage.inputFields("John", "Jack", "1.1.2019", "VN", "20000", "To buy a new car", "80000", "100000");
     caseMapPage.clickSubmitButton();
-    
-    TaskWidgetPage taskWidgetPage = new TaskWidgetPage();
-    taskWidgetPage.filterTasksBy("Verify Personal Data");
-    taskWidgetPage.startTaskWithoutUI(0);
-    homePage = new HomePage();
-    redirectToRelativeLink(HomePage.PORTAL_HOME_PAGE_URL);
-    
-    taskWidgetPage = new TaskWidgetPage();
-    taskWidgetPage.filterTasksBy("Internal Solvency Check");
-    taskWidgetPage.startTaskWithoutUI(0);
-    homePage = new HomePage();
-    redirectToRelativeLink(HomePage.PORTAL_HOME_PAGE_URL);
-    
+    startTaskWithoutUI("Verify Personal Data");
+    startTaskWithoutUI("Internal Solvency Check");
+    login(TestAccount.DEMO_USER);
     taskWidgetPage = new TaskWidgetPage();
     taskWidgetPage.filterTasksBy("Approve Level 1");
-    TaskTemplatePage taskTemplatePage = taskWidgetPage.startTask(0);
-    
-    Assert.assertEquals("John", caseMapPage.getCustomerName());
-    Assert.assertEquals("Jack", caseMapPage.getCustomerFirstName());
-    Assert.assertEquals("VN", caseMapPage.getCountry());
-    Assert.assertEquals("20000", caseMapPage.getAmount());
-    Assert.assertEquals("To buy a new car", caseMapPage.getReason());
-    Assert.assertEquals("80000", caseMapPage.getSalary());
-    Assert.assertEquals("100000", caseMapPage.getOtherCredits());
-    
-    taskTemplatePage.clickByCssSelector("button[id$='form:rejected-button']");
-    
-    taskWidgetPage.waitForElementDisplayed(By.cssSelector(".no-task-message"), true, 10);
+    taskWidgetPage.startTask(0);
+    assertInputData();
+    caseMapPage.clickRejectButton();
     taskWidgetPage = new TaskWidgetPage();
     taskWidgetPage.filterTasksBy("Approve Level 2");
     Assert.assertEquals(0, taskWidgetPage.countTasks());
   }
+
+  private void assertInputData() {
+    Assert.assertEquals("John", caseMapPage.getCustomerName());
+    Assert.assertEquals("Jack", caseMapPage.getCustomerFirstName());
+    Assert.assertEquals("VN", caseMapPage.getCountry());
+    Assert.assertEquals("20000", caseMapPage.getAmount());
+    Assert.assertEquals("To buy a new car", caseMapPage.getReason());
+    Assert.assertEquals("80000", caseMapPage.getSalary());
+    Assert.assertEquals("100000", caseMapPage.getOtherCredits());
+  }
   
   @Test
   public void testCollectPersonalDataValidation() {
-      redirectToRelativeLink(CASE_MAP_URL);
-      CaseMapPage caseMapPage = new CaseMapPage();
-      caseMapPage.inputFields("","", "", "","", "", "", "");
-      Assert.assertEquals("Name: Value is required."
-          + ",Country: Value is required.,"
-          + "Amount (SFr.): Value is required.,"
-          + "Yearly salary: Value is required.,"
-          + "Amount of other open credits (SFr.)*: Value is required.",
-          caseMapPage.clickSubmitAndGetValidationMsg());
-      caseMapPage.inputFields("John", "Jack", "1.1.2020", "VN","20000", "To buy a new car", "80000", "100000");
-      caseMapPage.clickSubmitButton();
+    redirectToRelativeLink(CASE_MAP_URL);
+    CaseMapPage caseMapPage = new CaseMapPage();
+    caseMapPage.inputFields("", "", "", "", "", "", "", "");
+    Assert.assertEquals(
+            "Name: Value is required.,"
+            + "Country: Value is required.,"
+            + "Amount (SFr.): Value is required.,"
+            + "Yearly salary: Value is required.,"
+            + "Amount of other open credits (SFr.)*: Value is required.",
+        caseMapPage.clickSubmitAndGetValidationMsg());
+    caseMapPage.inputFields("John", "Jack", "1.1.2020", "VN", "20000", "To buy a new car", "80000", "100000");
+    caseMapPage.clickSubmitButton();
   }
-  
+
+
+  private void startTaskWithoutUI(String taskname) {
+    TaskWidgetPage taskWidgetPage = new TaskWidgetPage();
+    taskWidgetPage.filterTasksBy(taskname);
+    taskWidgetPage.startTaskWithoutUI(0);
+    redirectToRelativeLink(HomePage.PORTAL_HOME_PAGE_URL);
+  }
 }
