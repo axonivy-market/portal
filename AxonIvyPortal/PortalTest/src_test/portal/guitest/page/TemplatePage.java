673b8fe1cac59b5fc8c942499a44840a19a2ea7a
Thang Van <thang.nguyenvan@axonactive.com>
2020-03-02 14:34:02 +0700

    IVYPORTAL-9980: Various or urgent requests in IVYPORTAL S188 - Fix gui test

--- AxonIvyPortal/PortalTest/src_test/portal/guitest/page/TemplatePage.java
+++ AxonIvyPortal/PortalTest/src_test/portal/guitest/page/TemplatePage.java
@@ -11,6 +11,7 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebDriverException;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import com.jayway.awaitility.Awaitility;
@@ -19,6 +20,7 @@ import com.jayway.awaitility.Duration;
 import portal.guitest.common.Sleeper;
 import portal.guitest.common.SystemProperties;
 import portal.guitest.common.UrlHelpers;
+import vn.wawa.guitest.base.client.Browser;
 import vn.wawa.guitest.base.page.AbstractPage;
 
 public abstract class TemplatePage extends AbstractPage {
@@ -148,6 +150,7 @@ public abstract class TemplatePage extends AbstractPage {
 
   public AdminSettingsPage openAdminSettings() {
     clickUserMenuItem("adminui-menu-item");
+    waitAjaxIndicatorDisappear();
     return new AdminSettingsPage();
   }
 
@@ -309,6 +312,5 @@ public abstract class TemplatePage extends AbstractPage {
         return true;
       }
     });
-  }
- 
+  } 
 }
