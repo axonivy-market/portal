3242a602f54ed2d8fb1892019ee672c147bcf9f2
nam.mai <nam.mai@axonactive.com>
2020-03-26 10:04:22 +0700

    IVYPORTAL-7933: Handle the breadcrumb menu in the portal - Implement for Express, Task Analysis - Updated GUI Tests
    Former-commit-id: 9860a85b48627694bbca873f0c537f1d4f55268b

--- AxonIvyPortal/PortalKit/src/ch/ivy/addon/portalkit/enums/BreadCrumbKind.java
+++ AxonIvyPortal/PortalKit/src/ch/ivy/addon/portalkit/enums/BreadCrumbKind.java
@@ -1,7 +1,7 @@
 package ch.ivy.addon.portalkit.enums;
 
 public enum BreadCrumbKind {
-PROCESS, TASK, TASK_DETAIL, CASE_DETAIL, CASE, DASHBOARD;
+PROCESS, TASK, TASK_DETAIL, CASE_DETAIL, CASE, DASHBOARD, EXPRESS;
   
   @Override
   public String toString() {
