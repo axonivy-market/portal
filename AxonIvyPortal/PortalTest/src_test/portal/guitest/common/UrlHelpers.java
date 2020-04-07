8576b4fa41bc4d66a7bcf1441da279b1bccb021c
lttung_wawa <tung.le@axonactive.com>
2020-04-07 11:49:39 +0700

    IVYPORTAL-9928 Use Jenkins pipeline for Portal - Added pipeline - Updated pom - Updated GUI test

--- AxonIvyPortal/PortalTest/src_test/portal/guitest/common/UrlHelpers.java
+++ AxonIvyPortal/PortalTest/src_test/portal/guitest/common/UrlHelpers.java
@@ -10,10 +10,7 @@ import org.apache.commons.lang3.StringUtils;
 public class UrlHelpers {
 
   public static String generateAbsoluteProcessStartLink(String relativeProcessStartLink) {
-    //because we renamed PortalExamples project to portal-developer-examples, so no need to capitalize first character of this project
-    if(!relativeProcessStartLink.contains("portal-developer-examples") && !relativeProcessStartLink.contains("portal-user-examples")) {
-      relativeProcessStartLink = WordUtils.capitalize(relativeProcessStartLink);
-    }
+    relativeProcessStartLink = WordUtils.capitalize(relativeProcessStartLink);
     if (relativeProcessStartLink.endsWith(".icm")) {
       return getEngineUrl() + "/casemap/" + getApplicationName() + "/" + relativeProcessStartLink;
     }
