ae3b4f659ceb98b540dddb2127814e0b0f90b79f
Nam Chu <nam.chu@axonactive.com>
2020-04-07 12:16:05 +0700

    IVYPORTAL-10075 Various or urgent requests in IVYPORTAL S189 - Remove warning message on Portal 9.0.x

--- AxonIvyPortal/PortalKitTestHelper/src/ch/ivy/addon/portalkit/test/util/AbsenceUtils.java
+++ AxonIvyPortal/PortalKitTestHelper/src/ch/ivy/addon/portalkit/test/util/AbsenceUtils.java
@@ -1,6 +1,7 @@
 package ch.ivy.addon.portalkit.test.util;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import ch.ivyteam.ivy.application.IApplication;
 import ch.ivyteam.ivy.security.IUser;
@@ -11,7 +12,7 @@ public class AbsenceUtils {
   public static void cleanAllAbsences() {
     List<IApplication> applications = ServerFactory.getServer().getApplicationConfigurationManager().getApplications();
     for (IApplication application : applications) {
-      List<IUser> users = application.getSecurityContext().getUsers();
+      List<IUser> users = application.getSecurityContext().users().paged().stream().collect(Collectors.toList());
       for (IUser user : users) {
         List<IUserAbsence> absences = user.getAbsences();
         for (IUserAbsence absence : absences) {
