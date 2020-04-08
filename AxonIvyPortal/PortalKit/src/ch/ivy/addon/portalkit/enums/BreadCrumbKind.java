abc1239a5a4a1d6acbd9c581a6a66510f5054b95
nam.mai <nam.mai@axonactive.com>
2020-04-08 17:11:08 +0700

    IVYPORTAL-7933: Handle the breadcrumb menu in the portal - Handled feedback: breadcrumb is broken when case/task name so long - Handled feedback: remove unnecessary code
    Former-commit-id: 9720f0e6ca3568bdb024f3f4ebf59b0a73bc7908

--- AxonIvyPortal/PortalKit/src/ch/ivy/addon/portalkit/enums/BreadCrumbKind.java
+++ AxonIvyPortal/PortalKit/src/ch/ivy/addon/portalkit/enums/BreadCrumbKind.java
@@ -2,9 +2,4 @@ package ch.ivy.addon.portalkit.enums;
 
 public enum BreadCrumbKind {
 PROCESS, TASK, TASK_DETAIL, CASE_DETAIL, CASE, DASHBOARD, EXPRESS;
-  
-  @Override
-  public String toString() {
-    return super.toString().toLowerCase();
-  }
 }
