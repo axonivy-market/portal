a8d43c7927cf9e9b74d33a8eb62ed3d1748fa779
ttthai_wawa <hai.tran@axonacitve.com>
2020-04-13 10:26:29 +0700

    IVYPORTAL-10160 Various or urgent requests in IVYPORTAL S190 - Create release S189: fix compile errors + minor warnings

--- AxonIvyPortal/PortalKit/src/ch/ivy/addon/portalkit/support/UrlDetector.java
+++ AxonIvyPortal/PortalKit/src/ch/ivy/addon/portalkit/support/UrlDetector.java
@@ -1,14 +1,12 @@
 package ch.ivy.addon.portalkit.support;
 
 import java.net.MalformedURLException;
-import java.net.URISyntaxException;
 import java.net.URL;
 
 import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
 import javax.servlet.http.HttpServletRequest;
 
-import ch.ivy.addon.portalkit.service.exception.PortalException;
 import ch.ivyteam.ivy.request.RequestUriFactory;
 
 public class UrlDetector {
@@ -29,11 +27,7 @@ public class UrlDetector {
    */
   public String getBaseURL(final FacesContext facesContext) throws MalformedURLException {
     if (facesContext == null) {
-      try {
-        return RequestUriFactory.createLocalHostServerUri().toString() + "/" + RequestUriFactory.getIvyContextName();
-      } catch (URISyntaxException e) {
-        throw new PortalException(e);
-      }
+      return RequestUriFactory.createLocalHostServerUri().toString() + "/" + RequestUriFactory.getIvyContextName();
     }
     return getBaseURL(facesContext.getExternalContext());
   }
@@ -82,27 +76,5 @@ public class UrlDetector {
   public String getBaseURLWithoutPort(final HttpServletRequest request) throws MalformedURLException {
     return new URL(request.getScheme(), request.getServerName(), request.getContextPath()).toString();
   }
-  
 
-  public String getHost(String url) throws MalformedURLException {
-    URL urlObject = new URL(url);
-    int portNumber = getPortForConstructingUrl(urlObject.getProtocol(), urlObject.getPort());
-    return new URL(urlObject.getProtocol(), urlObject.getHost(), portNumber, "").toString();
-  }
-
-  /**
-   * If using default port of the protocol then return -1 to ignore port when
-   * constructing URL. E.g: http://localhost/ivy instead of
-   * http://localhost:80/ivy
-   * @param protocol 
-   * @param port 
-   * @return port
-   */
-  public int getPortForConstructingUrl(String protocol, int port) {
-    if ((RequestUriFactory.HTTPS_PROTOCOL.equalsIgnoreCase(protocol) && port == RequestUriFactory.HTTPS_PORT)
-        || (RequestUriFactory.HTTP_PROTOCOL.equalsIgnoreCase(protocol) && port == RequestUriFactory.HTTP_PORT)) {
-      return -1;
-    }
-    return port;
-  }
 }
