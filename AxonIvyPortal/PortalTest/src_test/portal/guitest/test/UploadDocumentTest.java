9928615c2eba3125d78383d0891fafe6b43f5cdc
Nguyen Quoc Hoan <hoan.nguyen01@axonactive.com>
2020-04-15 17:43:53 +0700

    IVYPORTAL-10065: Improve document symbol in case details - implement for 8.0.x

--- AxonIvyPortal/PortalTest/src_test/portal/guitest/test/UploadDocumentTest.java
+++ AxonIvyPortal/PortalTest/src_test/portal/guitest/test/UploadDocumentTest.java
@@ -3,8 +3,14 @@ package portal.guitest.test;
 import static junit.framework.Assert.assertEquals;
 import static portal.guitest.common.FileHelper.getAbsolutePathToTestFile;
 
+import java.util.List;
+
+import org.apache.commons.lang3.StringUtils;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
 import portal.guitest.common.BaseTest;
 import portal.guitest.common.Sleeper;
@@ -83,6 +89,44 @@ public class UploadDocumentTest extends BaseTest{
     int numberOfDocument = caseDetailsPage.countNumberOfDocument();
     caseDetailsPage.uploadDocumentWithoutError(getAbsolutePathToTestFile("unsupportedExtension.abc"));
     assertEquals(numberOfDocument + 1, caseDetailsPage.countNumberOfDocument());
+  }
+  
+
+  @Test
+  public void uploadDocumentAndCheckDocumentName() {
+    final String pdfFile = "test-no-files-no-js.pdf";
+    final String wordFile = "test-ms-word-extension.doc";
+    final String unsupportFile = "unsupportedExtension.abc";
+
+    initHomePage(TestAccount.ADMIN_USER);
+
+    adminSettingsPage = homePage.openAdminSettings();
+    adminSettingsPage.setFileExtensionWhiteList();
+    casePage = homePage.openCaseList();
+    caseDetailsPage = casePage.openDetailsOfCaseHasName("Leave Request");
+    
+    caseDetailsPage.uploadDocumentWithoutError(getAbsolutePathToTestFile(pdfFile));
+    Assert.assertTrue(isCorrectIconExtension(pdfFile, "fa fa-file-pdf-o"));
+    
+    caseDetailsPage.uploadDocumentWithoutError(getAbsolutePathToTestFile(wordFile));
+    Assert.assertTrue(isCorrectIconExtension(wordFile, "fa fa-file-word-o"));
+    
+    caseDetailsPage.uploadDocumentWithoutError(getAbsolutePathToTestFile(unsupportFile));
+    Assert.assertTrue(isCorrectIconExtension(unsupportFile, "fa fa-file-o"));
+  }
+  
+  private boolean isCorrectIconExtension(String fileName, String iconClass) {
+    final String caseDetailDocumentClass = "case-details-document-download-icon";
+    List<WebElement> documentItems = caseDetailsPage.findDocumentItemInCaseDetailsDocumentTable();
+    for (WebElement document:documentItems) {
+      String uploadedFileName = document.findElement(By.cssSelector(".js-document-name")).getText();
+      if (uploadedFileName.equalsIgnoreCase(fileName)) {
+        String symbol = document.findElement(By.cssSelector("." + caseDetailDocumentClass)).getAttribute("class");
+        assertEquals(symbol, iconClass.concat(StringUtils.SPACE.concat(caseDetailDocumentClass)));
+        return true;
+      }
+    }
+    return false;
   }
   
   private void initHomePage(TestAccount account) {
