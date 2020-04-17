8cdd27ec66911cb3ce895a6c3a1e3042f6275246
Nam Le Dinh <nam.ledinh@axonactive.com>
2020-04-17 16:11:36 +0700

    IVYPORTAL-10058: Enhance TaskTemplate and iframeTemplate - Add examples + GUITest

--- AxonIvyPortal/PortalTest/src_test/portal/guitest/test/TaskTemplateTest.java
+++ AxonIvyPortal/PortalTest/src_test/portal/guitest/test/TaskTemplateTest.java
@@ -2,6 +2,7 @@ package portal.guitest.test;
 
 import static junit.framework.Assert.assertEquals;
 import static junit.framework.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
 
 import java.util.concurrent.TimeUnit;
 
@@ -27,23 +28,24 @@ import portal.guitest.page.WorkingTaskDialogPage;
 public class TaskTemplateTest extends BaseTest {
 
   private String createImpersistentTaskUrl = "portalExamples/169BDE2F368D6EC4/PrimefacesElements.ivp";
+  private static final String CUSTOM_PARAMS_TEMPLATE_TASK_URL= "portalExamples/1718293B3F6E5478/start.ivp";
 
   @Override
   @Before
   public void setup() {
     super.setup();
-    redirectToRelativeLink(createTestingTasksUrl);
-    redirectToRelativeLink(HomePage.PORTAL_HOME_PAGE_URL);
   }
 
   @Test
   public void testCaseDetailsTabDisplayed() {
+    createTestDataAndRedirectToHomePage();
     TaskTemplatePage taskTemplatePage = startATaskAndOpenCaseInfo();
     assertTrue("Case details is not displayed", taskTemplatePage.containsCaseDetails());
   }
 
   @Test
   public void testAddingANote() {
+    createTestDataAndRedirectToHomePage();
     TaskTemplatePage taskTemplatePage = startATaskAndOpenCaseInfo();
     assertEquals(1, taskTemplatePage.countHistoryItems());
     taskTemplatePage.addNewNote("Sample note message");
@@ -73,6 +75,7 @@ public class TaskTemplateTest extends BaseTest {
 
   @Test
   public void testOpeningRelatedTask() {
+    createTestDataAndRedirectToHomePage();
     TaskTemplatePage taskTemplatePage = startATaskAndOpenCaseInfo();
     assertTrue(taskTemplatePage.countRelatedTasks() > 0);
     TaskDetailsPage taskDetailsPage = taskTemplatePage.openFirstRelatedTaskInHistoryArea();
@@ -81,6 +84,7 @@ public class TaskTemplateTest extends BaseTest {
 
   @Test
   public void testOpeningDocumentUploading() {
+    createTestDataAndRedirectToHomePage();
     TaskTemplatePage taskTemplatePage = startATaskAndOpenCaseInfo();
     taskTemplatePage.openDocumentUploadingDialog();
     assertTrue(taskTemplatePage.isDocumentUploadingDialogDisplayed());
@@ -88,6 +92,7 @@ public class TaskTemplateTest extends BaseTest {
   
   @Test
   public void testLeaveWorkingTaskByClickingOnLogo() {
+    createTestDataAndRedirectToHomePage();
     TaskTemplatePage taskTemplatePage = startATaskAndOpenCaseInfo();
     taskTemplatePage.clickOnLogo();
     WorkingTaskDialogPage dialogPage = new WorkingTaskDialogPage();
@@ -99,6 +104,7 @@ public class TaskTemplateTest extends BaseTest {
   
   @Test
   public void testReserveWorkingTaskByClickingOnLogo() {
+    createTestDataAndRedirectToHomePage();
     TaskTemplatePage taskTemplatePage = startATaskAndOpenCaseInfo();
     taskTemplatePage.clickOnLogo();
     WorkingTaskDialogPage dialogPage = new WorkingTaskDialogPage();
@@ -110,6 +116,7 @@ public class TaskTemplateTest extends BaseTest {
   
   @Test
   public void testResetTaskWhenStartSideStep() {
+    createTestDataAndRedirectToHomePage();
     HomePage homePage = new HomePage();
     MainMenuPage mainMenuPage = homePage.openMainMenu();
     ProcessWidgetPage processWidgetPage = mainMenuPage.selectProcessesMenu();
@@ -132,6 +139,33 @@ public class TaskTemplateTest extends BaseTest {
     assertEquals(true, taskTemplatePage.isStartAdhocBtnNotExist());
   }
 
+  @Test
+  public void testCustomParamsForTaskTemplate8() {
+    redirectToRelativeLink(CUSTOM_PARAMS_TEMPLATE_TASK_URL);
+    TaskWidgetPage taskWidgetPage = new TaskWidgetPage();
+    taskWidgetPage.filterTasksBy("Task template 8 with custom params");
+    TaskTemplatePage taskTemplatePage = taskWidgetPage.startTaskWithouWaitForTaskActionPresent(0);
+    assertFalse(taskTemplatePage.isTaskNameDisplayed());
+    assertFalse(taskTemplatePage.isTaskActionDisplayed());
+    assertFalse(taskTemplatePage.isCaseInfoButtonDisplayed());
+  }
+  
+  @Test
+  public void testCustomParamsForIFrameTaskTemplate() {
+    redirectToRelativeLink(CUSTOM_PARAMS_TEMPLATE_TASK_URL);
+    TaskWidgetPage taskWidgetPage = new TaskWidgetPage();
+    taskWidgetPage.filterTasksBy("IFrame task with custom params");
+    TaskTemplatePage taskTemplatePage = taskWidgetPage.startTaskWithouWaitForTaskActionPresent(0);
+    assertFalse(taskTemplatePage.isTaskNameDisplayed());
+    assertFalse(taskTemplatePage.isTaskActionDisplayed());
+    assertFalse(taskTemplatePage.isCaseInfoButtonDisplayed());
+  }
+  
+  private void createTestDataAndRedirectToHomePage() {
+    redirectToRelativeLink(createTestingTasksUrl);
+    redirectToRelativeLink(HomePage.PORTAL_HOME_PAGE_URL);
+  }
+  
   private TaskTemplatePage startATaskAndOpenCaseInfo() {
     TaskWidgetPage taskWidgetPage = new TaskWidgetPage();
     TaskTemplatePage taskTemplatePage = taskWidgetPage.startTask(0);
