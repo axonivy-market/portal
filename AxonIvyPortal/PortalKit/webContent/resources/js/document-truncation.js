a7e96da75665b5a3fd0087feccd44b1f51ccb8f1
nam.mai <nam.mai@axonactive.com>
2020-05-06 16:43:29 +0700

    IVYPORTAL-10236: Enhance visibility of tasks and cases depending on permissions - Clean code for task "Chrome got hang when open case detail"

--- AxonIvyPortal/PortalKit/webContent/resources/js/document-truncation.js
+++ AxonIvyPortal/PortalKit/webContent/resources/js/document-truncation.js
@@ -27,9 +27,8 @@ function DocumentTruncation() {
         } else {
           $itemColumns = $('.js-task-details-item', $selectedItem);
         }
-        $responsiveButtons = $('.js-responsive-handle-button', $selectedItem);
 
-        if ($itemColumns.length > 2 && $responsiveButtons.length > 0) {
+        if ($itemColumns.length > 2 && $('.js-responsive-handle-button', $selectedItem).length > 0) {
           var isDisplayMainMenu = $('.js-left-sidebar').hasClass('in');
           var isDisplaySecondLevelMenu = $('#second-level-menu').hasClass('on') && !$('#second-level-menu').hasClass('hide');
           var isDocResponsiveButtonClicked = $('.js-document-column-responsive-button', $selectedItem).hasClass('is-clicked');
@@ -70,7 +69,7 @@ function truncateText(needRestoreOriginText) {
       var $documentNameComponent = $(this).children('.' + documentNameClass);
       var documentName = $documentNameComponent.text();
 
-      // Document name is splitted to 2 parts: start and end part. Also include
+      // Document name is split to 2 parts: start and end part. Also include
       // 3 dots between 2 parts
       // documentChars is number of maximum chars in a part
       // documentChars includes total of document name chars and 3 dots between
