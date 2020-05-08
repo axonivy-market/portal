075b31d7d2fbf0864b7a0f11cc381aa186a86c69
Nam Le Dinh <nam.ledinh@axonactive.com>
2020-05-08 10:19:42 +0700

    IVYPORTAL-10250: Various or urgent requests in IVYPORTAL S191 - Fix compile error in PortalTest with latest designer + upgrade junit to 4.13

--- AxonIvyPortal/PortalTest/src_test/portal/guitest/test/TaskAnalysisWidgetTest.java
+++ AxonIvyPortal/PortalTest/src_test/portal/guitest/test/TaskAnalysisWidgetTest.java
@@ -1,7 +1,7 @@
 package portal.guitest.test;
 
-import static junit.framework.Assert.assertEquals;
-import static junit.framework.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import java.util.ArrayList;
 import java.util.List;
