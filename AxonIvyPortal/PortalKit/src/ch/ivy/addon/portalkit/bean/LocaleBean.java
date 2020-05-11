d025953681751e589c1d5d876d1959fb941e8d8c
ttthai_wawa <hai.tran@axonacitve.com>
2020-05-11 09:44:15 +0700

    IVYPORTAL-10252 Adapt Portal url changes II: - Fix warnings + Update migration process

--- AxonIvyPortal/PortalKit/src/ch/ivy/addon/portalkit/bean/LocaleBean.java
+++ AxonIvyPortal/PortalKit/src/ch/ivy/addon/portalkit/bean/LocaleBean.java
@@ -1,12 +1,10 @@
 package ch.ivy.addon.portalkit.bean;
 
 import java.io.Serializable;
-import java.util.Locale;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 
-import ch.ivyteam.ivy.environment.EnvironmentNotAvailableException;
 import ch.ivyteam.ivy.environment.Ivy;
 
 @ManagedBean
@@ -15,11 +13,6 @@ public class LocaleBean implements Serializable {
   private static final long serialVersionUID = 1L;
 
   public String getLocale(){
-    try {
-      return Ivy.session().getContentLocale().getLanguage();
-    } catch (EnvironmentNotAvailableException e) {
-      Ivy.log().error(e);
-      return Locale.ENGLISH.getLanguage();
-    }
+    return Ivy.session().getContentLocale().getLanguage();
   }
 }
