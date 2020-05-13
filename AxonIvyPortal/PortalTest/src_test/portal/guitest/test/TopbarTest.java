02cfe2ce3b2aab4b317944991bb1b3d43cc6a15f
Long Do <long.do@axonactive.com>
2020-05-13 09:58:13 +0700

    IVYPORTAL-10228 Improve Displaying logged in user: + Updated designer config properties and GUI tests

---
+++ AxonIvyPortal/PortalTest/src_test/portal/guitest/test/TopbarTest.java
@@ -0,0 +1,50 @@
+package portal.guitest.test;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import portal.guitest.common.BaseTest;
+import portal.guitest.common.TestAccount;
+import portal.guitest.page.HomePage;
+
+public class TopbarTest extends BaseTest {
+
+  private static final String LOGGED_IN_USER_FORMAT = "LOGGED_IN_USER_FORMAT";
+  
+  @Test
+  public void testLoggedInUserWithUsernameFormat() {
+    redirectToRelativeLink(HomePage.PORTAL_HOME_PAGE_URL);
+    updatePortalSetting(LOGGED_IN_USER_FORMAT, "USERNAME");
+    login(TestAccount.DEMO_USER);
+    HomePage homePage = new HomePage();
+    assertEquals("demo", homePage.getLoggedInUserFormat());
+  }
+  
+  @Test
+  public void testLoggedInUserWithDisplayNameFormat() {
+    redirectToRelativeLink(HomePage.PORTAL_HOME_PAGE_URL);
+    updatePortalSetting(LOGGED_IN_USER_FORMAT, "DISPLAY_NAME");
+    login(TestAccount.DEMO_USER);
+    HomePage homePage = new HomePage();
+    assertEquals("Portal Demo User", homePage.getLoggedInUserFormat());
+  }
+  
+  @Test
+  public void testLoggedInUserWithDisplayNameUsernameFormat() {
+    redirectToRelativeLink(HomePage.PORTAL_HOME_PAGE_URL);
+    updatePortalSetting(LOGGED_IN_USER_FORMAT, "DISPLAY_NAME_USERNAME");
+    login(TestAccount.DEMO_USER);
+    HomePage homePage = new HomePage();
+    assertEquals("Portal Demo User (demo)", homePage.getLoggedInUserFormat());
+  }
+
+  @Test
+  public void testLoggedInUserWithUsernameDisplayNameFormat() {
+    redirectToRelativeLink(HomePage.PORTAL_HOME_PAGE_URL);
+    updatePortalSetting(LOGGED_IN_USER_FORMAT, "USERNAME_DISPLAY_NAME");
+    login(TestAccount.DEMO_USER);
+    HomePage homePage = new HomePage();
+    assertEquals("demo (Portal Demo User)", homePage.getLoggedInUserFormat());
+  }
+}
