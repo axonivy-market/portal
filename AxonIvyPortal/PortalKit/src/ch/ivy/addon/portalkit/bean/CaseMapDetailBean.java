2a3c529c7318ec0a2bb77f1927ae0856475f2c83
Nam Chu <nam.chu@axonactive.com>
2020-05-29 19:31:49 +0700

    IVYPORTAL-10245 Portal User Example Guide - Implement Lending case detail
    Former-commit-id: efbf609896a8e8307b5d4b6e3709e47d15f1d9bc

--- AxonIvyPortal/PortalKit/src/ch/ivy/addon/portalkit/bean/CaseMapDetailBean.java
+++ AxonIvyPortal/PortalKit/src/ch/ivy/addon/portalkit/bean/CaseMapDetailBean.java
@@ -1,13 +1,16 @@
 package ch.ivy.addon.portalkit.bean;
 
+import java.io.IOException;
 import java.io.Serializable;
 import java.util.List;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
+
+import org.apache.commons.lang3.StringUtils;
 
 import ch.ivy.addon.portalkit.bo.CaseMapDetail;
-import ch.ivyteam.ivy.environment.Ivy;
 
 @ManagedBean(name = "caseMapDetailBean")
 @ViewScoped
@@ -26,10 +29,26 @@ public class CaseMapDetailBean implements Serializable {
     return detail.getStageDetail().get(stage);
   }
   
-public String getIconOfStage(CaseMapDetail detail, String stage){
-    
-    String x = detail.getStageIcon().get(stage);
-    Ivy.log().error("{0}", x);
+  public List<String> getProcessOfStage(CaseMapDetail detail, int stageIndex){
+    String stage = detail.getStages().get(stageIndex);
+    return detail.getStageDetail().get(stage);
+  }
+  
+  public String getIconOfStage(CaseMapDetail detail, String stage){
     return detail.getStageIcon().get(stage);
+  }
+  
+  public List<String> getProcessDescription(CaseMapDetail detail, String process){
+    return detail.getProcessDetail().get(process);
+  }
+  
+  public String getDisplayStage(CaseMapDetail detail, int stageIndex){
+    return String.format("Stage %d - %s", stageIndex + 1, detail.getStages().get(stageIndex));
+  }
+  
+  public void startCaseMap(String link) throws IOException {
+    if (StringUtils.isNotEmpty(link)) {
+      FacesContext.getCurrentInstance().getExternalContext().redirect(link);
+    }
   }
 }
