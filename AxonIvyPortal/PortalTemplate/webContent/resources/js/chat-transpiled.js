8d410481de696929d2007d494617ac45f5d695f5
nam.mai <nam.mai@axonactive.com>
2020-06-09 16:33:24 +0700

    IVYPORTAL-10419: Various or urgent requests in IVYPORTAL S192 - Fixed bug: Chat panel is obscured by Header and Footer component

--- AxonIvyPortal/PortalTemplate/webContent/resources/js/chat-transpiled.js
+++ AxonIvyPortal/PortalTemplate/webContent/resources/js/chat-transpiled.js
@@ -438,6 +438,12 @@ function Chat(uri, view) {
     e.stopImmediatePropagation();
     view.renderChatMessagePanelUIWhenOpen();
 
+    // Hide environment info when open chat
+    var portalEnvironment = $('.js-portal-environment');
+    if (portalEnvironment.length > 0) {
+      portalEnvironment.addClass('u-hidden');
+    }
+
     if ($(".js-top-menu").hasClass("mod-chat-active")) {
       if (isChatGroupEnabled === 'true') {
         getGroups();
@@ -981,6 +987,12 @@ function View(uri) {
   this.closeChatPanel = function () {
     closeChatMessagePanel();
     this.renderChatMessagePanelUIWhenOpen();
+
+    // Show environment info when open chat
+    var portalEnvironment = $('.js-portal-environment');
+    if (portalEnvironment.length > 0) {
+      portalEnvironment.removeClass('u-hidden');
+    }
   };
 
   function updateExistingGroups() {
--- AxonIvyPortal/PortalTemplate/webContent/resources/js/chat.js
+++ AxonIvyPortal/PortalTemplate/webContent/resources/js/chat.js
@@ -261,6 +261,13 @@ function Chat(uri, view) {
     $("#toggle-chat-panel-command").off().click(function(e) {
       e.stopImmediatePropagation();
       view.renderChatMessagePanelUIWhenOpen();
+
+      // Hide environment info when open chat
+      var portalEnvironment = $('.js-portal-environment');
+      if (portalEnvironment.length > 0) {
+        portalEnvironment.addClass('u-hidden');
+      }
+
       if ($(".js-top-menu").hasClass("mod-chat-active")) {
         if (isChatGroupEnabled === 'true'){
           getGroups();
@@ -757,6 +764,12 @@ function View(uri)
     this.closeChatPanel = function() {
         closeChatMessagePanel();
         this.renderChatMessagePanelUIWhenOpen();
+
+        // Show environment info when open chat
+        var portalEnvironment = $('.js-portal-environment');
+        if (portalEnvironment.length > 0) {
+          portalEnvironment.removeClass('u-hidden');
+        }
     }
 
     function updateExistingGroups() {
