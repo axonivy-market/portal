f8672fe23bd355a6e6eb08ae03fe3b43e78d6089
lttung_wawa <tung.le@axonactive.com>
2020-06-15 17:32:03 +0700

    IVYPORTAL-10409 Portal totally blocked due to parallel request from Chat and XHR - Kept input message if reload chat due to deactivating

--- AxonIvyPortal/PortalTemplate/webContent/resources/js/chat.js
+++ AxonIvyPortal/PortalTemplate/webContent/resources/js/chat.js
@@ -114,13 +114,13 @@ function Chat(uri, view) {
       }
 
       if ($(".js-show-chat-message.active.loaded").length == 1) {
-        this.loadChat($(".js-show-chat-message.active.loaded").get(0));
+        this.loadChat($(".js-show-chat-message.active.loaded").get(0), true);
         chat.markReadMessages();
       }
 
       if ($(".js-show-group-chat-message.active.loaded").length == 1) {
         var caseId = $(".js-show-group-chat-message.active.loaded").find("input[class='js-case-id']").get(0).value;
-        chat.loadChatGroup(caseId);
+        chat.loadChatGroup(caseId, true);
         chat.markReadGroupMessages(caseId);
       }
       this.getSendersOfUnreadMessages();
@@ -218,7 +218,7 @@ function Chat(uri, view) {
       }
     }
 
-   this.loadChat = function(recipient) {
+   this.loadChat = function(recipient, isInputMessageKept) {
       var recipientName = $(recipient).find(".js-contact-card-name", ".js-show-chat-message.active").text();
       var $notification = $(recipient).find(".js-notification");
       if (!$notification.hasClass("u-hidden")) {
@@ -232,7 +232,7 @@ function Chat(uri, view) {
         async: true,
         cache: false,
         success: function (response) {
-          view.clearMessages();
+          view.clearMessages(isInputMessageKept);            
           view.renderMessageList(response, recipientName);
           view.updateUnreadUserBadge();
         }
@@ -345,7 +345,7 @@ function Chat(uri, view) {
         }
     }
 
-    this.loadChatGroup = function(caseId) {
+    this.loadChatGroup = function(caseId, isInputMessageKept) {
       var $groupNotification = $(".js-case-id:hidden[value='" + caseId + "']").closest(".js-show-group-chat-message").find(".js-notification");;
       if (!$groupNotification.hasClass("u-hidden")) {
         $groupNotification.addClass("u-hidden");
@@ -359,7 +359,7 @@ function Chat(uri, view) {
         async: true,
         cache: false,
         success: function (response) {
-          view.clearMessages();
+          view.clearMessages(isInputMessageKept);
           view.renderMessageListForGroup(response, userName);
           view.updateUnreadUserBadge();
         }
@@ -645,10 +645,12 @@ function View(uri)
       }
     }
 
-    this.clearMessages = function() {
+    this.clearMessages = function(isInputMessageKept) {
       var messageList = document.getElementsByClassName("js-message-list")[0];
       messageList.innerText = "";
-      $(".js-input-message").val("");
+      if (!isInputMessageKept) {
+        $(".js-input-message").val("");
+      }
     }
 
     this.renderChatMessagePanelUIWhenOpen = function() {
