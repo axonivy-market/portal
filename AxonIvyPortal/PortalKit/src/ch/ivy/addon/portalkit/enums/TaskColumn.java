42533aa8dd436cee8f8387edd7c05216c0933931
nam.mai <nam.mai@axonactive.com>
2020-06-18 14:06:43 +0700

    IVYPORTAL-10005: Create a task Widget for Dashboard II - Implemented save/load column setting

---
+++ AxonIvyPortal/PortalKit/src/ch/ivy/addon/portalkit/enums/TaskColumn.java
@@ -0,0 +1,13 @@
+package ch.ivy.addon.portalkit.enums;
+
+public enum TaskColumn {
+  PRIORITY,
+  ID,
+  NAME,
+  ACTIVATOR,
+  CREATION_TIME,
+  EXPIRY_TIME,
+  STATE,
+  DESCRIPTION,
+  CATEGORY
+}
