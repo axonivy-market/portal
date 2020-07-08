30fcde9100b926dc81ca60a098cf1fbaa43fb084
Tung Le <tung.le@axonactive.com>
2020-07-08 10:32:28 +0000

    Merged in feature/IVYPORTAL-10766_Improve_GUI_test (pull request #357)
    Feature/IVYPORTAL-10766 Improve GUI test
    
    * IVYPORTAL-10766 Improve GUI test
    - Added test parameterize
    
    * IVYPORTAL-10766 Improve GUI test
    - Added test parameterize
    
    * IVYPORTAL-10766 Improve GUI test
    - Added common script for Jenkinsfile
    
    * IVYPORTAL-10766 Improve GUI test
    - Updated username of test users to fix test
    
    * IVYPORTAL-10766 Improve GUI test
    - Fix unstable TaskWidgetTest.testStartButtonStatus "The element reference of <span id="task-widget:task-list-scroller:0:task-item:task-action:task-action-component" class="ui-commandlink ui-widget ui-state-disabled task-action-item start-task-action"> is stale; either the element is no longer attached to the DOM, it is not in the current frame context, or the document has been refreshed"
    
    * IVYPORTAL-10766 Improve GUI test
    
    * no message
    
    * IVYPORTAL-10766 Improve GUI test
    
    * Merge branch 'master' into feature/IVYPORTAL-10766_Improve_GUI_test
    
    * IVYPORTAL-10766 Improve GUI test
    - Removed multi-times stuff

---
+++ build/common/utils.groovy
@@ -0,0 +1,16 @@
+
+def killUnnecessaryProcessesToRunTest() {
+  echo '====================Close unnecessary processes===================='
+  bat '''
+    taskkill -im dwm.exe -F
+    taskkill -im Taskmgr.exe -F
+    taskkill -im procexp64.exe -F
+    taskkill -im IEDriverServer.exe -F
+    taskkill -im iexplore.exe -F
+    taskkill -im GeckoFireFoxDriver.exe -F
+    taskkill -im firefox.exe -F
+    exit 0
+  '''
+}
+
+return this
