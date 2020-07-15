830dee1b9b6146602e290c47b6b856304e3eaffe
lttung_wawa <tung.le@axonactive.com>
2020-07-15 11:03:16 +0700

    IVYPORTAL-10766 Improve GUI test - Tried to stabilized and added code to analyzed ChatTest at vn.wawa.guitest.base.client.Browser.shutdown(Browser.java:67)  at portal.guitest.common.ScreenshotFailedTestRule$1.evaluate(ScreenshotFailedTestRule.java:34) Session ID is null. Using WebDriver after calling quit()? - Tried to stabilized ProcessWidgetTest.testReorderFavoriteProcess at portal.guitest.test.ProcessWidgetTest.testReorderFavoriteProcess(ProcessWidgetTest.java:177) expected:<[Case Map: Leave Request]> but was:<[AGoogle]> - Stabilized taking screenshot StatisticScreenshotTest.screenshotForStatistic  at portal.guitest.page.TemplatePage.waitForElementDisplayed(TemplatePage.java:51) at portal.guitest.page.TemplatePage.waitForElementDisplayed(TemplatePage.java:130) at portal.guitest.page.HomePage.waitForStatisticRendered(HomePage.java:43) - Archived engine log to analyze more

--- AxonIvyPortal/PortalTest/src_test/portal/guitest/common/ScreenshotFailedTestRule.java
+++ AxonIvyPortal/PortalTest/src_test/portal/guitest/common/ScreenshotFailedTestRule.java
@@ -2,15 +2,16 @@ package portal.guitest.common;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Date;
 
 import org.apache.commons.io.FileUtils;
 import org.junit.rules.MethodRule;
 import org.junit.runners.model.FrameworkMethod;
 import org.junit.runners.model.Statement;
-import org.openqa.selenium.NoSuchSessionException;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebDriverException;
 
 import vn.wawa.guitest.base.client.Browser;
 
@@ -31,13 +32,21 @@ public class ScreenshotFailedTestRule implements MethodRule {
           throw t;
         } finally {
           try {
+            System.out.println("Before shutdown " + new Date());
             Browser.getBrowser().shutdown();
-          } catch (NoSuchSessionException e) {
-            System.out.println("ERROR Browsers are killed before shutdown");
+            System.out.println("After shutdown " + new Date());
           } catch (Exception e) {
+            System.out.println("Exception " + new Date());
+            if (e instanceof WebDriverException) {
+              System.out.println("ERROR maybe browsers are killed before shutdown");
+              return;
+            }
+            e.printStackTrace();
             captureScreenshot(frameworkMethod.getName() + "-shutdown-error");
+            System.out.println("captureScreenshot "+ new Date());
             Sleeper.sleep(5000);
             Browser.getBrowser().shutdown();
+            System.out.println("shutdown" + new Date());
           }
         }
       }
