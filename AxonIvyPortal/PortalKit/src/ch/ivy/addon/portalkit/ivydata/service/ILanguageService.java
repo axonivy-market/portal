d989f13398b63200e0148ba43ea0f3ba84f5ebcf
Nam Le Dinh <nam.ledinh@axonactive.com>
2020-08-07 12:43:58 +0700

    IVYPORTAL-10800: Remove multiapp implementation - Remove multiple app for language/email setting, announcement, process

--- AxonIvyPortal/PortalKit/src/ch/ivy/addon/portalkit/ivydata/service/ILanguageService.java
+++ AxonIvyPortal/PortalKit/src/ch/ivy/addon/portalkit/ivydata/service/ILanguageService.java
@@ -1,28 +1,37 @@
 package ch.ivy.addon.portalkit.ivydata.service;
 
+import java.util.List;
+
 import ch.ivy.addon.portalkit.ivydata.bo.IvyLanguage;
 import ch.ivy.addon.portalkit.ivydata.dto.IvyLanguageResultDTO;
+import ch.ivy.addon.portalkit.ivydata.exception.PortalIvyDataException;
 
 public interface ILanguageService {
 
   /**
-   * Get Languages for the passed user
-   * @param username
+   * Get Languages for the current user
    * @return {@link IvyLanguageResultDTO}
    */
-  IvyLanguageResultDTO findUserLanguages(String username);
+  IvyLanguageResultDTO findUserLanguages();
 
   /**
-   * set Languages for the passed user
-   * @param username
+   * set Languages for the current user
    * @param language
    * @return {@link IvyLanguageResultDTO}
+   * @throws PortalIvyDataException 
    */
-  IvyLanguageResultDTO saveUserLanguage(String username, IvyLanguage language);
+  IvyLanguageResultDTO saveUserLanguage(IvyLanguage language) throws PortalIvyDataException;
 
   /**
    * get supported languages of application store in portal style
    * @return {@link IvyLanguageResultDTO}
    */
   IvyLanguageResultDTO getSupportedLanguages();
+  
+  /**
+   * get supported languages of current application store in portal style
+   * @return list of languages
+   * @throws PortalIvyDataException 
+   */
+  List<String> getSupportedLanguagesOfCurrentApp() throws PortalIvyDataException;
 }
