b0ae3b0d840bac3bba726619d95abad22f9e5acb
Nam Chu <nam.chu@axonactive.com>
2020-08-11 09:40:53 +0700

    IVYPORTAL-10800 Remove multiapp implementation - Remove multi app change password

--- AxonIvyPortal/PortalKit/src/ch/ivy/addon/portalkit/ivydata/service/IPasswordService.java
+++ AxonIvyPortal/PortalKit/src/ch/ivy/addon/portalkit/ivydata/service/IPasswordService.java
@@ -1,18 +1,12 @@
 package ch.ivy.addon.portalkit.ivydata.service;
 
-import java.util.List;
-
-import ch.ivy.addon.portalkit.ivydata.dto.IvyPasswordResultDTO;
-
 @FunctionalInterface
 public interface IPasswordService {
 
   /**
    * Change password of user
-   * @param username
    * @param newPassword
-   * @param apps
-   * @return IvyLanguageResultDTO
+   * @return boolean;
    */
-  IvyPasswordResultDTO changePassword(String username, String newPassword, List<String> apps);
+  boolean changePassword(String newPassword);
 }
