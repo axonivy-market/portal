03c4a5a400b72874c75e37b539ae22e9953a9d8e
Nguyen Quoc Hoan <hoan.nguyen01@axonactive.com>
2020-09-04 11:42:44 +0700

    IVYPORTAL-10974: Defining and executing Express: email API - handle feedback
    Former-commit-id: 5da5a121b7f282ed42dbbf33c64a88209c731ef7

--- AxonIvyPortal/AxonIvyExpress/src/ch/ivy/gawfs/mail/ExpressMailClient.java
+++ AxonIvyPortal/AxonIvyExpress/src/ch/ivy/gawfs/mail/ExpressMailClient.java
@@ -11,13 +11,13 @@ import org.apache.commons.lang3.StringUtils;
 import ch.ivy.addon.portalkit.bo.ExpressUserEmail;
 import ch.ivy.addon.portalkit.dto.ExpressAttachment;
 import ch.ivy.addon.portalkit.util.IvyExecutor;
-import ch.ivyteam.ivy.email.Attachment;
-import ch.ivyteam.ivy.email.MailClient;
-import ch.ivyteam.ivy.email.MailClientConfig;
-import ch.ivyteam.ivy.email.MailClientConfigProvider;
-import ch.ivyteam.ivy.email.MailMessage;
-import ch.ivyteam.ivy.email.MailMessage.Builder;
 import ch.ivyteam.ivy.environment.Ivy;
+import ch.ivyteam.ivy.mail.Attachment;
+import ch.ivyteam.ivy.mail.MailClient;
+import ch.ivyteam.ivy.mail.MailClientConfig;
+import ch.ivyteam.ivy.mail.MailClientConfigProvider;
+import ch.ivyteam.ivy.mail.MailMessage;
+import ch.ivyteam.ivy.mail.MailMessage.Builder;
 import ch.ivyteam.ivy.project.IIvyProject;
 import ch.ivyteam.ivy.project.IvyProjectUtil;
 import ch.ivyteam.ivy.scripting.objects.File;
@@ -28,8 +28,8 @@ public class ExpressMailClient {
 
   public static void send(ExpressUserEmail mail) {
     IvyExecutor.executeAsSystem(() -> {
-      var message = mappingMailMessage(mail);
-      var mailSetupConfig = getMailClientConfig();
+      MailMessage message = mappingMailMessage(mail);
+      MailClientConfig mailSetupConfig = getMailClientConfig();
 
       // Send Email
       var mailClient = MailClient.newMailClient(mailSetupConfig);
@@ -43,13 +43,14 @@ public class ExpressMailClient {
     for (ExpressAttachment expressAttachment : expressAttachments) {
 
       try {
-        File attachedFile = new File(expressAttachment.getPath(), false);
-        var mailAttachment = Attachment.create()
-                      .file(attachedFile.getJavaFile())
-                      .toAttachment();
+        var attachedFile = new File(expressAttachment.getPath(), false);
+        Attachment mailAttachment = Attachment.create()
+                .file(attachedFile.getJavaFile())
+                .toAttachment();
+
         emailAttachments.add(mailAttachment);
       } catch (IOException e) {
-        e.printStackTrace();
+        Ivy.log().error("Cannot read express attachment", e);
       }
     }
 
@@ -57,14 +58,13 @@ public class ExpressMailClient {
   }
 
   private static MailMessage mappingMailMessage(ExpressUserEmail mail) {
-    Builder mailMessageBuilder = MailMessage.create();
-    mailMessageBuilder.subject(mail.getSubject())
-                      .htmlContent(mail.getContent())
-                      .attachments(getAttachments(mail.getAttachments()));
-    mailMessageBuilder.to(buildToMailAddress(mail.getRecipients()));
+    Builder mailMessageBuilder = MailMessage.create()
+            .subject(mail.getSubject())
+            .htmlContent(mail.getContent())
+            .attachments(getAttachments(mail.getAttachments()))
+            .to(buildToMailAddress(mail.getRecipients()));
 
     if (StringUtils.isNotEmpty(mail.getResponseTo())) {
-      // Waiting for API update, current we lost field replyTo
       mailMessageBuilder.from(mail.getResponseTo());
     }
     return mailMessageBuilder.toMailMessage();
@@ -74,7 +74,8 @@ public class ExpressMailClient {
     List<String> recipientEmails = new ArrayList<>();
     if (recipients.contains(";")) {
       recipientEmails.addAll(Arrays.asList(recipients.split(";"))
-              .stream().map(String::trim)
+              .stream()
+              .map(String::trim)
               .collect(Collectors.toList()));
     } else {
       recipientEmails.add(recipients);
@@ -82,16 +83,13 @@ public class ExpressMailClient {
     return recipientEmails;
   }
 
-
-
   private static MailClientConfig getMailClientConfig() {
     IIvyProject ivyProject = IvyProjectUtil.getIvyProjectByName(getProjectName());
     return MailClientConfigProvider.get(ivyProject);
   }
 
   private static String getProjectName() {
-    return WorkflowNavigationUtil.getWorkflowProcessModelVersion(Ivy.request().getProcessModelVersion()).getProject()
-        .getName();
+    return WorkflowNavigationUtil.getWorkflowProcessModelVersion(Ivy.request().getProcessModelVersion()).getProject().getName();
   }
 
 }
