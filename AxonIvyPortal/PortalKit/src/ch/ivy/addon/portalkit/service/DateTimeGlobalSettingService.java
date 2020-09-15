168b87b01e4a49ebcaf56c4ea1834c90e6a0523c
Nguyen Quoc Hoan <hoan.nguyen01@axonactive.com>
2020-09-15 17:16:52 +0700

    IVYPORTAL-10960 Various or urgent requests in IVYPORTAL S197 - Reverted: Enhance Workflow Events Read All link

--- AxonIvyPortal/PortalKit/src/ch/ivy/addon/portalkit/service/DateTimeGlobalSettingService.java
+++ AxonIvyPortal/PortalKit/src/ch/ivy/addon/portalkit/service/DateTimeGlobalSettingService.java
@@ -23,10 +23,6 @@ public class DateTimeGlobalSettingService {
     return Ivy.cms().co("/patterns/datePattern");
   }
   
-  public String getDateTimestampPattern() {
-    return Ivy.cms().co("/patterns/dateTimestampPattern");
-  }
-  
   public boolean isHideTime() {
     String dateTimeGlobalSetting = globalSettingService.findGlobalSettingValue(GlobalVariable.HIDE_TIME.toString());
     return Boolean.valueOf(dateTimeGlobalSetting);
