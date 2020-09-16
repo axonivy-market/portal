631f29d57ea788572febc905400337b96f13aad9
Nguyen Quoc Hoan <hoan.nguyen01@axonactive.com>
2020-09-16 15:53:04 +0700

    IVYPORTAL-10960 Various or urgent requests in IVYPORTAL S197 - Investigate language issue for 8.0.8 with SSO
    # Conflicts:
    #	Documentation/portal-guide/source/portal-developer-guide/customization/menu.rst

--- AxonIvyPortal/PortalTemplate/src/ch/addon/portal/generic/menu/TaskSubMenuItem.java
+++ AxonIvyPortal/PortalTemplate/src/ch/addon/portal/generic/menu/TaskSubMenuItem.java
@@ -2,13 +2,13 @@ package ch.addon.portal.generic.menu;
 
 import ch.ivy.addon.portal.generic.navigation.PortalNavigator;
 import ch.ivy.addon.portalkit.enums.MenuKind;
-import ch.ivyteam.ivy.environment.Ivy;
+import ch.ivy.addon.portalkit.service.ApplicationMultiLanguage;
 
 public class TaskSubMenuItem extends SubMenuItem {
   public TaskSubMenuItem() {
     this.icon = "icon ivyicon-task-list-edit";
     this.menuKind = MenuKind.TASK;
-    this.label = Ivy.cms().co("/ch.ivy.addon.portalkit.ui.jsf/common/tasks");
+    this.label = ApplicationMultiLanguage.getCmsValueByUserLocale("/ch.ivy.addon.portalkit.ui.jsf/common/tasks");
     this.link = new PortalNavigator().getSubMenuItemUrlOfCurrentApplication(MenuKind.TASK);
   }
 }
