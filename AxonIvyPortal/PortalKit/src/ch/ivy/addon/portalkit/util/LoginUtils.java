f58c9e9785980dc98e46a899a7422b1c8e49a2e4
ttthai_wawa <hai.tran@axonacitve.com>
2020-09-18 15:31:10 +0700

    IVYPORTAL-10047 No access message instead of login form for SSO - Update document + add screenshot

--- AxonIvyPortal/PortalKit/src/ch/ivy/addon/portalkit/util/LoginUtils.java
+++ AxonIvyPortal/PortalKit/src/ch/ivy/addon/portalkit/util/LoginUtils.java
@@ -8,7 +8,7 @@ import ch.ivyteam.ivy.environment.Ivy;
 @ManagedBean
 @ViewScoped
 public class LoginUtils {
-  private static final String PORTAL_LOGIN_ERROR_MESSAGE_DISPLAY = "PortalLoginPageDisplay";
+  public static final String PORTAL_LOGIN_ERROR_MESSAGE_DISPLAY = "PortalLoginPageDisplay";
   
   public boolean isLoginPageRendered() {
     return Boolean.parseBoolean(Ivy.var().get(PORTAL_LOGIN_ERROR_MESSAGE_DISPLAY));
