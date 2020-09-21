8dfca44ed17cfcc079015427f9676ca511a0ef19
ttthai_wawa <hai.tran@axonacitve.com>
2020-09-21 15:14:22 +0700

    IVYPORTAL-10047 No access message instead of login form for SSO - Handle feedback
    Former-commit-id: 1e7ed6bcd6e16a808d8716b79af2224c0f5231fa

--- AxonIvyPortal/PortalTest/src_test/portal/guitest/document/screenshot/LoginErrorPageScreenshotTest.java
+++ AxonIvyPortal/PortalTest/src_test/portal/guitest/document/screenshot/LoginErrorPageScreenshotTest.java
@@ -6,12 +6,13 @@ import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.Dimension;
 
-import ch.ivy.addon.portalkit.util.LoginUtils;
 import ch.ivy.addon.portalkit.util.ScreenshotUtil;
 import portal.guitest.common.ScreenshotTest;
 import portal.guitest.common.Sleeper;
 
 public class LoginErrorPageScreenshotTest extends ScreenshotTest {
+  
+  private String PORTAL_LOGIN_PAGE_DISPLAY = "PortalLoginPageDisplay";
   
   @Before
   @Override
@@ -21,10 +22,10 @@ public class LoginErrorPageScreenshotTest extends ScreenshotTest {
 
   @Test
   public void testLoginErrorPage() throws IOException {
-    updateGlobalVariable(LoginUtils.PORTAL_LOGIN_ERROR_MESSAGE_DISPLAY, "false");
+    updateGlobalVariable(PORTAL_LOGIN_PAGE_DISPLAY, "false");
     ScreenshotUtil.resizeBrowser(new Dimension(1024, 768));
     Sleeper.sleep(500);//wait for focus annimation finish to capture nice screenshot
     ScreenshotUtil.capturePageScreenshot(ScreenshotUtil.LOGIN_FOLDER + "login-error-page");
-    updateGlobalVariable(LoginUtils.PORTAL_LOGIN_ERROR_MESSAGE_DISPLAY, "true");
+    updateGlobalVariable(PORTAL_LOGIN_PAGE_DISPLAY, "true");
   }
 }
