8b6e00dc023daa5a28760e3c6c122e5ec1a4790a
Long Do <long.do@axonactive.com>
2020-09-29 10:17:32 +0700

    IVYPORTAL-11106 Portal Homepage selection in user profile for each profile II: + Fixed compile errors

---
+++ AxonIvyPortal/PortalKit/src/ch/ivy/addon/portalkit/constant/UserProperty.java
@@ -0,0 +1,7 @@
+package ch.ivy.addon.portalkit.constant;
+
+public class UserProperty {
+
+  public static final String SHOW_GUIDE = "SHOW_GUIDE";
+  public static final String HOMEPAGE = "HOMEPAGE";
+}
