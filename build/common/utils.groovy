733c4b368f3b0061439d3d6d50fd5ce34bff735b
lttung_wawa <tung.le@axonactive.com>
2020-10-21 09:19:50 +0700

    IVYPORTAL-11207 Various or urgent requests in IVYPORTAL S199 - IVYPORTAL-11269 Get back latest pipeline due to lost commits

--- build/common/utils.groovy
+++ build/common/utils.groovy
@@ -1,4 +1,70 @@
 
+def stopAllElasticSearchs() {
+  echo "====================Turn off Elastics Search port from 19200 to 19210===================="
+  powershell '''
+    for (($port = 19200); $port -lt 19210; ($port++)) {
+      Get-NetTCPConnection -LocalPort $port -erroraction 'silentlycontinue'
+      if ( $LastExitCode -eq 0 ) {
+        Stop-Process -Id (Get-NetTCPConnection -LocalPort $port).OwningProcess -Force
+      }
+    }
+  '''
+  def elasticSearchServices = ['ivy-elasticsearch-master-cluster']
+  for (elasticSearchService in elasticSearchServices) {
+    stopWindowsService(elasticSearchService)
+  }
+}
+
+def stopAllEngines() {
+  echo "====================Stop engines===================="
+  def engineServices = ['Axon.ivy Engine - LTS', 'Axon.ivyEngine8.0', 'Axon.ivyEngineTrunk', 'Axon.ivy-master-cluster-gui-test-1', 'Axon.ivy-master-cluster-gui-test-2', 'Axon.ivyEngineDashboard']
+  for (engineService in engineServices) {
+    stopWindowsService(engineService)
+  }
+  stopAllElasticSearchs()
+}
+
+def startWindowsService(String serviceName) {
+  stopWindowsService(serviceName)
+  echo "====================Start service ${serviceName}===================="
+  bat """
+    @echo off
+    net start "${serviceName}"
+    if "%ERRORLEVEL%"=="1" (
+      timeout /t 30
+      sc query "${serviceName}" | find /i "RUNNING"
+    )
+  """
+}
+
+def stopWindowsService(String serviceName) {
+  echo "====================Stop service ${serviceName}===================="
+  bat """
+  @echo off
+    sc query "${serviceName}" | find /i "RUNNING"
+    if not "%ERRORLEVEL%"=="1" (net stop "${serviceName}")
+  """
+}
+
+def remoteDesktop() {
+  closeAllRemoteDesktopConnections()
+  echo '====================Setup remote desktop connections to Portal slave===================='
+  configFileProvider(
+      [configFile(fileId: 'properties-config', variable: 'PROPERTIES_CONFIG')]) {
+      props = readProperties  file: "${env.PROPERTIES_CONFIG}"
+      //e.g. remoteDesktopCmd=D:/tools/remote-desktop-plus/rdp.exe /v:10.123.1.193 /domain:wawa.vn /u:wawa /p:W4w4@CH$ /w:2560 /h:1440 /noclose
+      bat "${props.remoteDesktopCmd}"
+  }
+}
+
+def closeAllRemoteDesktopConnections() {
+  echo '====================Close all remote desktop connections===================='
+  bat '''
+    taskkill -im mstsc.exe -F
+    exit 0
+  '''
+}
+
 def killUnnecessaryProcessesToRunTest() {
   echo '====================Close unnecessary processes===================='
   bat '''
@@ -13,4 +79,10 @@ def killUnnecessaryProcessesToRunTest() {
   '''
 }
 
+def extractEngine(String engineDir, String engineDownloadURL) {
+  echo '====================Extract engine===================='
+  bat """
+    mvn clean compile -f AxonIvyPortal/PortalStyle/pom.xml -Divy.engine.directory=${engineDir} ${engineDownloadURL}
+  """
+}
 return this
