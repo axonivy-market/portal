281e84de2375c18161ee64de9a21aeb5f5bd3b05
Long Do <long.do@axonactive.com>
2020-11-02 14:09:02 +0700

    IVYPORTAL-11209 Upgrade prototyped Task Widget to official New Task Widget II: + Customized columns

---
+++ AxonIvyPortal/PortalKit/src/ch/ivy/addon/portalkit/constant/DashboardConfigurationPrefix.java
@@ -0,0 +1,10 @@
+package ch.ivy.addon.portalkit.constant;
+
+public class DashboardConfigurationPrefix {
+  
+  public static final String CMS = "cms:";
+  public static final String CUSTOM_FIELD_STRING = "string:";
+  public static final String CUSTOM_FIELD_TEXT = "text:";
+  public static final String CUSTOM_FIELD_NUMBER = "number:";
+  public static final String CUSTOM_FIELD_TIMESTAMP = "timestamp:";
+}
