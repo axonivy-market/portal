8d7eb216aaf75adc2b745e52d386b1fe840c74f8
lttung_wawa <tung.le@axonactive.com>
2020-11-16 15:10:26 +0700

    IVYPORTAL-11386 Various or urgent requests in IVYPORTAL S200 > IVYPORTAL-11343  Adapt API for IClusterNode.getId() due to ivyTeam updated Cluster API

--- AxonIvyPortal/PortalTemplate/src/ch/ivy/addon/portal/chat/ClusterChatAction.java
+++ AxonIvyPortal/PortalTemplate/src/ch/ivy/addon/portal/chat/ClusterChatAction.java
@@ -9,10 +9,10 @@ public enum ClusterChatAction implements Consumer<ClusterChatEventParameter> {
   // Action name must have prefix that defined by constant CLUSTER_CHAT_ACTION_PREFIX
   PORTAL_CHAT_SEND_PRIVATE_MESSAGE((param) -> {
     getChatService().performSendingPrivateMessage(param.getMessageText(), param.getReceiver(), param.getClientId(),
-        param.getActor(), param.getNodeId());
+        param.getActor(), param.getNodeName());
   }), PORTAL_CHAT_SEND_GROUP_MESSAGE((param) -> {
     getChatService().performSendingGroupMessage(param.getMessageText(), param.getReceiver(), param.getClientId(),
-        param.getActor(), param.getNodeId());
+        param.getActor(), param.getNodeName());
   }), PORTAL_CHAT_UPDATE_GROUP_LIST((param) -> {
     getChatService().performUpdatingGroupList(param.getGroupChat());
   }), PORTAL_CHAT_UPDATE_USER_STATUS((param) -> {
