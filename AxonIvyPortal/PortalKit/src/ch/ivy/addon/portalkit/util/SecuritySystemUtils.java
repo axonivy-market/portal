3886148def0a1979dd510480954465d41bf8cbfa
Long Vo Thanh <long.vothanh@axonactive.com>
2020-12-08 18:36:11 +0700

    IVYPORTAL-6370: Reset Password in portal - check if the ivy Security System is used

---
+++ AxonIvyPortal/PortalKit/src/ch/ivy/addon/portalkit/util/SecuritySystemUtils.java
@@ -0,0 +1,11 @@
+package ch.ivy.addon.portalkit.util;
+
+import ch.ivyteam.ivy.environment.Ivy;
+import ch.ivyteam.ivy.security.ISecurityConstants;
+
+public class SecuritySystemUtils {
+  public static boolean isIvySecuritySystem() {
+    return Ivy.session().getSecurityContext().getExternalSecuritySystemName()
+        .equals(ISecurityConstants.IVY_ENGINE_SECURITY_SYSTEM_PROVIDER_NAME);
+  }
+}
