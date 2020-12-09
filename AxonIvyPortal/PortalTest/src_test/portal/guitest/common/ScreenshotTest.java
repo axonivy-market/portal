711e8abe56e0352fcacf09d41e593caab961f2a8
Nguyen Quoc Hoan <hoan.nguyen01@axonactive.com>
2020-12-09 17:01:20 +0700

    IVYPORTAL-11437 Various or urgent requests in IVYPORTAL S201 - Unstable PortalDocumentScreenshot build

--- AxonIvyPortal/PortalTest/src_test/portal/guitest/common/ScreenshotTest.java
+++ AxonIvyPortal/PortalTest/src_test/portal/guitest/common/ScreenshotTest.java
@@ -1,5 +1,6 @@
 package portal.guitest.common;
 
+import org.junit.After;
 import org.junit.Before;
 
 public class ScreenshotTest extends BaseTest {
@@ -7,7 +8,13 @@ public class ScreenshotTest extends BaseTest {
   @Before
   @Override
   public void setup() {
+    killBrowsers();
     super.setup();
     updatePortalSetting("DISPLAY_MESSAGE_AFTER_FINISH_TASK", "false");
+  }
+  
+  @After
+  public void cleanUp() {
+    killBrowsers();
   }
 }
