2f8861e7bc9b9a205258e7daa30e70fcce81b98a
Long Vo Thanh <long.vothanh@axonactive.com>
2020-12-16 17:57:10 +0700

    IVYPORTAL-6370: Reset Password in portal - check port in UrlUtils class, update cms
    Former-commit-id: ddcc7b6ddd9f074110bd2a950df3aad0170ed375

--- AxonIvyPortal/PortalKit/src/ch/ivy/addon/portalkit/util/UrlUtils.java
+++ AxonIvyPortal/PortalKit/src/ch/ivy/addon/portalkit/util/UrlUtils.java
@@ -8,7 +8,10 @@ import ch.ivyteam.ivy.environment.Ivy;
 public class UrlUtils {
   public static String getServerUrl() throws MalformedURLException {
     URL url = new URL(Ivy.html().applicationHomeRef());
-    StringBuilder builder = new StringBuilder(url.getProtocol()).append("://").append(url.getHost()).append(":").append(url.getPort());
+    StringBuilder builder = new StringBuilder(url.getProtocol()).append("://").append(url.getHost());
+    if (url.getPort() != -1) {
+      builder.append(":").append(url.getPort());
+    }
     return builder.toString();
   }
 }
