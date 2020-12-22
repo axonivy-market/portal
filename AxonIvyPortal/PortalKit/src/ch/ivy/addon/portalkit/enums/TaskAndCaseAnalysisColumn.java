762c316bb660e987749c6f2ba31699ea58ab4059
Nam Chu <nam.chu@axonactive.com>
2020-12-22 15:13:13 +0700

    IVYPORTAL-11477 Add Category in Task and case list - Add category to export excel

--- AxonIvyPortal/PortalKit/src/ch/ivy/addon/portalkit/enums/TaskAndCaseAnalysisColumn.java
+++ AxonIvyPortal/PortalKit/src/ch/ivy/addon/portalkit/enums/TaskAndCaseAnalysisColumn.java
@@ -7,7 +7,6 @@ public enum TaskAndCaseAnalysisColumn {
   CASE_ID(false, false),
   CASE_CATEGORY(false, false),
   CASE_CREATOR(false, false),
-  CASE_OWNER(false, false),
   CASE_STATE(false, true),
   TASK_NAME(true, true),
   TASK_ID(true, false),
@@ -19,7 +18,8 @@ public enum TaskAndCaseAnalysisColumn {
   TASK_STATE(true, false),
   TASK_CREATION_TIME(true, true),
   TASK_EXPIRY_TIME(true, false),
-  TASK_FINISHED_TIME(true, true);
+  TASK_FINISHED_TIME(true, true),
+  CASE_OWNER(false, false);
 
   private boolean isTaskField;
   private boolean isDefaultColumn;
