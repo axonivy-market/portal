3b61e740484a307b7a8fd5974c72a11f71c607f0
nam.mai <nam.mai@axonactive.com>
2021-01-04 16:21:03 +0700

    IVYPORTAL-11209: Upgrade prototyped Task Widget to official New Task Widget II - Implement load statistic info on demand - Hide unset filters when open info panel

--- AxonIvyPortal/PortalKit/src/ch/ivy/addon/portalkit/dto/dashboard/TaskDashboardWidget.java
+++ AxonIvyPortal/PortalKit/src/ch/ivy/addon/portalkit/dto/dashboard/TaskDashboardWidget.java
@@ -79,6 +79,7 @@ public class TaskDashboardWidget extends DashboardWidget {
     buildTaskByStateStatistic();
     buildTaskExpiryStatistic();
     buildTaskByCategoryStatistic();
+    List<ColumnModel> aaa = getFilterableColumns();
   }
   
   private void buildTaskByStateStatistic() throws ParseException {
