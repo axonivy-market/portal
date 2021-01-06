1285c44f18e3df4a2dfa3b3e9888488fad61f08f
lttung_wawa <tung.le@axonactive.com>
2021-01-06 10:33:15 +0700

    IVYPORTAL-11538 Various or urgent requests in IVYPORTAL S202 > IVYPORTAL-11605 Fix GUI test failed due to UserGuiDialog displayed
    Former-commit-id: c820a26940e44a99138b81f5f219106ec5876f5d

--- AxonIvyPortal/PortalTest/src_test/portal/guitest/page/AutoDialogPage.java
+++ AxonIvyPortal/PortalTest/src_test/portal/guitest/page/AutoDialogPage.java
@@ -1,10 +1,12 @@
 package portal.guitest.page;
 
+import org.openqa.selenium.By;
+
 import vn.wawa.guitest.base.page.AbstractPage;
 
 public class AutoDialogPage extends AbstractPage {
 
-  private static final String TEMPLATE_PAGE_LOCATOR = "id('Caption')";
+  private static final String TEMPLATE_PAGE_LOCATOR = "id('closeButton')";
 
   public AutoDialogPage() {
     waitForLocatorDisplayed(getLoadedLocator());
@@ -14,8 +16,8 @@ public class AutoDialogPage extends AbstractPage {
     waitForElementDisplayed(locator, true, DEFAULT_TIMEOUT);
   }
 
-  public void waitContentDisplayed() {
-    waitForElementDisplayedByCssSelector("#Content");
+  public void proceed() {
+    click(By.cssSelector("#closeButton"));
   }
 
   @Override
