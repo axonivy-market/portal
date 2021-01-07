40eba620feb7fb8c23a57d0bb59fe68306454791
Nguyen Quoc Hoan <hoan.nguyen01@axonactive.com>
2021-01-07 14:25:08 +0700

    IVYPORTAL-11538 Various or urgent requests in IVYPORTAL S202 - ProcessHistory component doesn't sort by default field - fix broken UI when toggle menu

--- AxonIvyPortal/PortalTemplate/webContent/resources/js/process-history.js
+++ AxonIvyPortal/PortalTemplate/webContent/resources/js/process-history.js
@@ -1,10 +1,49 @@
-function CaseWidget() {
-  this.hideListIfEmptyCase = function(emptyRow) {
+// Define CaseWidget for sorting event callback
+var caseWidget = new CaseWidget();
+
+/* For opening ProcessHistory component in a dialog
+    - Check the case list data then hide the header if data empty
+    - Setup table header by call CaseWidget.setupHeader()
+*/
+var processHistory = {
+  setup : function(emptyRow) {
+    this.hideListIfEmptyCase(emptyRow);
+    // Setup case widget header
+    caseWidget.setupHeader();
+  },
+
+  hideListIfEmptyCase : function(emptyRow) {
     if(emptyRow) {
       $header = $('.js-case-widget-column-header');
       $list = $('.process-history-list');
       $header.hide();
       $list.hide();
     }
+  },
+
+  // For binding event of tooggle menu
+  updateLayoutWithAnimation : function() {
+    setupCaseHeader();
   }
 }
+
+Portal.updateLayoutContent();
+setupCaseHeader();
+
+var resizeTimer;
+// Update screen when window size is changed
+$(window).resize(function() {
+  Portal.updateLayoutContent();
+
+  clearTimeout(resizeTimer);
+  resizeTimer = setupCaseHeader();
+});
+
+function setupCaseHeader() {
+  setTimeout(function() {
+    caseWidget.setupHeader();
+  }, 250);
+}
+
+// Add a ScreenHandler listener to Portal Menu event
+MainMenu.init(processHistory);
