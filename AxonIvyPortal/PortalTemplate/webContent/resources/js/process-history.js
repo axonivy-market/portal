8914039efebbf0261a43fe2bc0074f28fbe630de
Nguyen Quoc Hoan <hoan.nguyen01@axonactive.com>
2021-01-07 11:14:59 +0700

    IVYPORTAL-11538 Various or urgent requests in IVYPORTAL S202 - Improved showcase Process History component, use the reality example instead of Alpha, Beta company - Fixed GUI Test

--- AxonIvyPortal/PortalTemplate/webContent/resources/js/process-history.js
+++ AxonIvyPortal/PortalTemplate/webContent/resources/js/process-history.js
@@ -1,5 +1,34 @@
-function CaseWidget() {
-  this.hideListIfEmptyCase = function(emptyRow) {
+
+// Add a ScreenHandler listener to Portal Menu event
+var responsiveToolkit = ResponsiveToolkit(new CaseListScreenHandler());
+MainMenu.init(responsiveToolkit);
+Portal.updateLayoutContent();
+// Define CaseWidget for sorting event callback
+var caseWidget = new CaseWidget();
+setupCaseHeader();
+
+
+var resizeTimer;
+// Update screen when window size is changed
+$(window).resize(function() {
+  Portal.updateLayoutContent();
+
+  clearTimeout(resizeTimer);
+  resizeTimer = setupCaseHeader();
+});
+
+/* For opening ProcessHistory component in a dialog
+    - Check the case list data then hide the header if data empty
+    - Setup table header by call CaseWidget.setupHeader()
+*/
+var processHistory = {
+  setup : function(emptyRow) {
+    this.hideListIfEmptyCase(emptyRow);
+    // Setup case widget header
+    caseWidget.setupHeader();
+  },
+
+  hideListIfEmptyCase : function(emptyRow) {
     if(emptyRow) {
       $header = $('.js-case-widget-column-header');
       $list = $('.process-history-list');
@@ -7,4 +36,10 @@ function CaseWidget() {
       $list.hide();
     }
   }
+}
+
+function setupCaseHeader() {
+  setTimeout(function() {
+    caseWidget.setupHeader();
+  }, 250);
 }
