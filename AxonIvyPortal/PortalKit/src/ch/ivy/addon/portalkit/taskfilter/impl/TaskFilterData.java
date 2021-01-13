a36a9e3179bbc4ef78443a60657f65c7604446a6
Chu Hung Nam <nam.chu@axonactive.com>
2021-01-13 10:46:53 +0000

    Merged in feature/IVYPORTAL-11518-improve-doc-list-of-support-method (pull request #433)
    Feature/IVYPORTAL-11518 improve doc list of support method
    
    * IVYPORTAL-11518 Improve documentation - List of supported methods
    - Update document
    
    * IVYPORTAL-11518 Improve documentation - List of supported methods > Clean up pom.xml of public-api
    
    * IVYPORTAL-11518 Improve documentation - List of supported methods > Updated read-the-docs version
    
    * IVYPORTAL-11518 Improve documentation - List of supported methods > Merged master to this
    
    * IVYPORTAL-11518 Improve documentation - List of supported methods > Clean up
    
    * IVYPORTAL-11518 Improve documentation - List of supported methods > Clean up
    
    * IVYPORTAL-11518 Improve documentation - List of supported methods > Clean up
    
    * Merge branch 'master' of https://bitbucket.org/axonivy-prod/axonivyportal/src/master into feature/IVYPORTAL-11518-improve-doc-list-of-support-method
    
    * Merge branch 'feature/IVYPORTAL-11518-improve-doc-list-of-support-method' of https://bitbucket.org/axonivy-prod/axonivyportal/src/master into feature/IVYPORTAL-11518-improve-doc-list-of-support-method
    
    * IVYPORTAL-11518 Improve documentation - List of supported methods
    - Handle feedback
    
    Approved-by: Hoan Nguyen

---
+++ AxonIvyPortal/PortalKit/src/ch/ivy/addon/portalkit/taskfilter/impl/TaskFilterData.java
@@ -0,0 +1,7 @@
+package ch.ivy.addon.portalkit.taskfilter.impl;
+
+import ch.ivy.addon.portalkit.filter.AbstractFilterData;
+import ch.ivy.addon.portalkit.taskfilter.TaskFilter;
+
+public class TaskFilterData extends AbstractFilterData<TaskFilter> {
+}
