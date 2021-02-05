6fc230d7f99167e7b921c3da068e1676d702c7b0
Nguyen Quoc Hoan <hoan.nguyen01@axonactive.com>
2021-02-05 08:53:22 +0700

    IVYPORTAL-11673 Various or urgent requests in IVYPORTAL S203 - Release: fix GUI test related to DATE_PATTERN

--- AxonIvyPortal/PortalTest/src_test/portal/guitest/common/DateTimePattern.java
+++ AxonIvyPortal/PortalTest/src_test/portal/guitest/common/DateTimePattern.java
@@ -2,6 +2,6 @@ package portal.guitest.common;
 
 public class DateTimePattern {
 
-  public static final String DATE_PATTERN = "dd/MM/yyyy";
-  public static final String DATE_TIME_PATTERN = "dd/MM/yyyy HH:mm";
+  public static final String DATE_PATTERN = "M/d/yy";
+  public static final String DATE_TIME_PATTERN = "M/d/yy HH:mm";
 }
