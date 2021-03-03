4ce87796c22ceb1acf5bd29c10ca63efe1a1f47b
Nam Chu <nam.chu@axonactive.com>
2021-03-03 18:19:35 +0700

    IVYPORTAL-11525 New Case Details style - Implement sort - Implement action button

--- AxonIvyPortal/PortalKit/src/ch/ivy/addon/portalkit/datamodel/internal/RelatedCaseLazyDataModel.java
+++ AxonIvyPortal/PortalKit/src/ch/ivy/addon/portalkit/datamodel/internal/RelatedCaseLazyDataModel.java
@@ -12,10 +12,10 @@ import org.primefaces.model.LazyDataModel;
 import org.primefaces.model.SortOrder;
 
 import ch.ivy.addon.portalkit.bean.IvyComponentLogicCaller;
+import ch.ivy.addon.portalkit.enums.CaseSortField;
 import ch.ivy.addon.portalkit.ivydata.searchcriteria.CaseSearchCriteria;
 import ch.ivyteam.ivy.workflow.CaseState;
 import ch.ivyteam.ivy.workflow.ICase;
-import ch.ivyteam.ivy.workflow.query.CaseQuery;
 
 public class RelatedCaseLazyDataModel extends LazyDataModel<ICase> {
 
@@ -27,28 +27,25 @@ public class RelatedCaseLazyDataModel extends LazyDataModel<ICase> {
   protected int rowIndex;
   protected CaseSearchCriteria criteria;
 
-  public RelatedCaseLazyDataModel() {
+  public RelatedCaseLazyDataModel(Long businessCaseId) {
     super();
     data = new ArrayList<>();
     caseWidgetComponentId = "related-cases-widget";
-    buildCriteria();
+    buildCriteria(businessCaseId);
   }
 
   @Override
   public List<ICase> load(int first, int pageSize, String sortField, SortOrder sortOrder, Map<String, Object> filters) {
+    criteria.setSortField(sortField);
+    criteria.setSortDescending(sortOrder == SortOrder.DESCENDING);
+    
     if (first == 0) {
       initializedDataModel();
-      criteria.setSortField(sortField);
-      criteria.setSortDescending(sortOrder == SortOrder.DESCENDING);
     }
     
     List<ICase> foundCases = findCases(criteria, first, pageSize);
     data.addAll(foundCases);
     return foundCases;
-  }
-
-  protected void buildQueryToSearchCriteria() {
-    this.criteria.setFinalCaseQuery(CaseQuery.create());
   }
 
   private List<ICase> findCases(CaseSearchCriteria criteria, int first, int pageSize) {
@@ -73,8 +70,8 @@ public class RelatedCaseLazyDataModel extends LazyDataModel<ICase> {
 
   private void initializedDataModel() {
     data.clear();
-    buildQueryToSearchCriteria();
     setRowCount(getCaseCount(criteria));
+    criteria.setFinalCaseQuery(null);
   }
 
   private int getCaseCount(CaseSearchCriteria criteria) {
@@ -86,10 +83,16 @@ public class RelatedCaseLazyDataModel extends LazyDataModel<ICase> {
     }
     return 0;
   }
-
-  private void buildCriteria() {
+  
+  private void buildCriteria(Long businessCaseId) {
     criteria = new CaseSearchCriteria();
-    criteria.setBusinessCase(true);
+    criteria.setBusinessCase(false);
+    criteria.setTechnicalCase(true);
+    criteria.setBusinessCaseId(businessCaseId);
     criteria.setIncludedStates(new ArrayList<>(Arrays.asList(CaseState.CREATED, CaseState.RUNNING, CaseState.DONE)));
+    criteria.setSortField(CaseSortField.NAME.toString());
+    criteria.setSortDescending(false);
   }
+  
+  
 }
