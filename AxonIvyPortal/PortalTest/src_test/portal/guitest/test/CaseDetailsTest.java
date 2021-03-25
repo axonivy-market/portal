c5e24c2500b284b9edf30820c6aa5d525cb0dc8d
Long Vo Thanh <long.vothanh@axonactive.com>
2021-03-25 17:43:28 +0700

    IVYPORTAL-11907: Enhance Case Details Page - GUI tests

--- AxonIvyPortal/PortalTest/src_test/portal/guitest/test/CaseDetailsTest.java
+++ AxonIvyPortal/PortalTest/src_test/portal/guitest/test/CaseDetailsTest.java
@@ -4,11 +4,16 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+import java.util.concurrent.TimeUnit;
+
 import org.apache.commons.lang.StringUtils;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+
+import com.jayway.awaitility.Awaitility;
+import com.jayway.awaitility.Duration;
 
 import ch.ivy.addon.portalkit.enums.PortalPermission;
 import ch.ivyteam.ivy.workflow.TaskState;
@@ -17,6 +22,7 @@ import portal.guitest.common.CaseState;
 import portal.guitest.common.TestAccount;
 import portal.guitest.common.TestRole;
 import portal.guitest.common.WaitHelper;
+import portal.guitest.page.AdditionalCaseDetailsPage;
 import portal.guitest.page.CaseDetailsPage;
 import portal.guitest.page.CaseWidgetPage;
 import portal.guitest.page.ExpressProcessPage;
@@ -34,6 +40,7 @@ public class CaseDetailsTest extends BaseTest {
   private HomePage homePage;
   private CaseDetailsPage detailsPage;
 
+  private static final String BUSINESS_CASE_MAP_LEAVE_REQUEST = "Business Case Map: Leave Request";
   private static final String LEAVE_REQUEST_CASE_NAME = "Leave Request";
   private static final String ORDER_PIZZA = "Order Pizza";
   private static final String GRANT_DELEGATE_OWN_TASK_PERMISSION_PROCESS_URL =
@@ -45,18 +52,29 @@ public class CaseDetailsTest extends BaseTest {
   @Before
   public void setup() {
     super.setup();
-    redirectToRelativeLink(createTestingTasksUrl);
     login(TestAccount.ADMIN_USER);
 
     homePage = new HomePage();
     grantSpecificPortalPermission(PortalPermission.TASK_CASE_ADD_NOTE);
+  }
+
+  private void createTestingTask() {
+    redirectToRelativeLink(createTestingTasksUrl);
     MainMenuPage mainMenuPage = homePage.openMainMenu();
     CaseWidgetPage casePage = mainMenuPage.selectCaseMenu();
     detailsPage = casePage.openDetailsOfCaseHasName(LEAVE_REQUEST_CASE_NAME);
   }
-  
+
+  private void createTestingCaseContainTechnicalCases() {
+    redirectToRelativeLink(createCaseWithTechnicalCaseUrl);
+    MainMenuPage mainMenuPage = homePage.openMainMenu();
+    CaseWidgetPage casePage = mainMenuPage.selectCaseMenu();
+    detailsPage = casePage.openDetailsOfCaseHasName(ORDER_PIZZA);
+  }
+
   @Test
   public void testDestroyCase() {
+    createTestingTask();
     detailsPage.onClickDestroyCase();
     detailsPage.confimDestruction();
     CaseWidgetPage casePage = new CaseWidgetPage();
@@ -66,12 +84,14 @@ public class CaseDetailsTest extends BaseTest {
 
   @Test
   public void testDisplayCaseProperties() {
+    createTestingTask();
     assertTrue(StringUtils.equalsIgnoreCase("LeaveRequest", detailsPage.getCaseCategory()));
     assertTrue(StringUtils.isNotBlank(detailsPage.getCaseDuration()));
   }
 
   @Test
   public void testAddCaseNote() {
+    createTestingTask();
     assertEquals(1, detailsPage.getNumberOfHistory());
     detailsPage.addNote("Consider the remaining annual leaves before the approval");
     assertEquals(2, detailsPage.getNumberOfHistory());
@@ -80,10 +100,7 @@ public class CaseDetailsTest extends BaseTest {
 
   @Test
   public void testShowCaseDetail() {
-    redirectToRelativeLink(createCaseWithTechnicalCaseUrl);
-    MainMenuPage mainMenuPage = homePage.openMainMenu();
-    CaseWidgetPage casePage = mainMenuPage.selectCaseMenu();
-    detailsPage = casePage.openDetailsOfCaseHasName(ORDER_PIZZA);
+    createTestingCaseContainTechnicalCases();
     assertTrue(detailsPage.isGeneralInformationComponentPresented());
     assertTrue(detailsPage.isRelatedCasesComponentPresented());
     assertTrue(detailsPage.isRelatedTasksComponentPresented());
@@ -93,24 +110,27 @@ public class CaseDetailsTest extends BaseTest {
 
   @Test
   public void testRelatedTaskStartButtonStatus() {
+    createTestingTask();
     assertFalse(detailsPage.isRelatedTaskStartEnabled(1));
     assertTrue(detailsPage.isRelatedTaskStartEnabled(2));
   }
 
   @Test
   public void testRelatedTaskStartTask() {
+    createTestingTask();
     TaskTemplatePage taskTemplate = detailsPage.startRelatedTask(2);
     assertEquals("Sick Leave Request", taskTemplate.getTaskName());
   }
 
   @Test
   public void testRelatedTaskReserveTask() {
-    detailsPage.sideStepMenuOnActionButton(2);
+    createTestingTask();
+    detailsPage.clickRelatedTaskActionButton(2);
     detailsPage.reserveTask(2);
     detailsPage.waitAjaxIndicatorDisappear();
     assertTrue(detailsPage.isTaskState(2, TaskState.PARKED));
 
-    detailsPage.sideStepMenuOnActionButton(2);
+    detailsPage.clickRelatedTaskActionButton(2);
     detailsPage.resetTask(2);
     detailsPage.waitAjaxIndicatorDisappear();
     assertTrue(detailsPage.isTaskState(2, TaskState.SUSPENDED));
@@ -118,7 +138,8 @@ public class CaseDetailsTest extends BaseTest {
 
   @Test
   public void testRelatedTaskDestroyTask() {
-    detailsPage.sideStepMenuOnActionButton(2);
+    createTestingTask();
+    detailsPage.clickRelatedTaskActionButton(2);
     Assert.assertTrue(detailsPage.isRelatedTaskDestroyEnabled(2));
     detailsPage.destroyTask(2);
     detailsPage.confimRelatedTaskDestruction();
@@ -128,6 +149,7 @@ public class CaseDetailsTest extends BaseTest {
 
   @Test
   public void testRelatedTaskDelegateTask() {
+    createTestingTask();
     assertEquals(TestRole.EVERYBODY_ROLE, detailsPage.getResponsibleOfRelatedTaskAt(2));
 
     detailsPage.openTaskDelegateDialog(2);
@@ -142,6 +164,7 @@ public class CaseDetailsTest extends BaseTest {
 
   @Test
   public void testRelatedTaskDisplayDelegateButton() {
+    createTestingTask();
     redirectToRelativeLink(GRANT_DELEGATE_OWN_TASK_PERMISSION_PROCESS_URL);
     MainMenuPage mainMenuPage = homePage.openMainMenu();
     CaseWidgetPage casePage = mainMenuPage.selectCaseMenu();
@@ -153,47 +176,85 @@ public class CaseDetailsTest extends BaseTest {
 
   @Test
   public void testRelatedTaskOpenDetails() {
-    detailsPage.sideStepMenuOnActionButton(0);
+    createTestingTask();
+    detailsPage.clickRelatedTaskActionButton(0);
     TaskDetailsPage taskDetailsPage = detailsPage.openTasksOfCasePageViaDetailsAction(0);
     assertEquals("Task Details", taskDetailsPage.getPageTitle());
   }
 
   @Test
+  public void testRelatedCaseOpenDetails() {
+    createTestingCaseContainTechnicalCases();
+    detailsPage.clickRelatedCaseActionButton(0);
+    CaseDetailsPage caseDetailsPage = detailsPage.openCasesOfCasePageViaDetailsAction(0);
+    assertEquals("Case Details", caseDetailsPage.getPageTitle());
+  }
+
+  @Test
+  public void testRelatedCaseOpenBusinessDetails() {
+    createTestingCaseContainTechnicalCases();
+    detailsPage.clickRelatedCaseActionButton(0);
+    AdditionalCaseDetailsPage caseDetailsPage = detailsPage.openRelatedCaseBusinessDetail(0);
+    Awaitility.await().atMost(new Duration(5, TimeUnit.SECONDS)).until(() -> caseDetailsPage.countBrowserTab() > 1);
+    caseDetailsPage.switchLastBrowserTab();
+    AdditionalCaseDetailsPage additionalCaseDetailsPage = new AdditionalCaseDetailsPage();
+    assertEquals("Additional Case Details", additionalCaseDetailsPage.getPageTitle());
+  }
+
+  @Test
+  public void testRelatedCaseSideSteps() {
+    redirectToRelativeLink(createTestingCaseMapUrl);
+    MainMenuPage mainMenuPage = homePage.openMainMenu();
+    CaseWidgetPage casePage = mainMenuPage.selectCaseMenu();
+    detailsPage = casePage.openDetailsOfCaseHasName(BUSINESS_CASE_MAP_LEAVE_REQUEST);
+    assertEquals(1, detailsPage.countRelatedCases());
+    detailsPage.clickRelatedCaseActionButton(0);
+    detailsPage.clickRelatedCaseSubmitLeaveReason(0);
+    mainMenuPage = homePage.openMainMenu();
+    casePage = mainMenuPage.selectCaseMenu();
+    detailsPage = casePage.openDetailsOfCaseHasName(BUSINESS_CASE_MAP_LEAVE_REQUEST);
+    assertEquals(2, detailsPage.countRelatedCases());
+    detailsPage.clickRelatedCaseActionButton(0);
+    detailsPage.clickRelatedCaseUploadAdditionalDocument(0);
+    mainMenuPage = homePage.openMainMenu();
+    casePage = mainMenuPage.selectCaseMenu();
+    detailsPage = casePage.openDetailsOfCaseHasName(BUSINESS_CASE_MAP_LEAVE_REQUEST);
+    assertEquals(3, detailsPage.countRelatedCases());
+  }
+
+  @Test
   public void testRelatedTaskOpenWorkflowEvents() {
-    detailsPage.sideStepMenuOnActionButton(0);
+    createTestingTask();
+    detailsPage.clickRelatedTaskActionButton(0);
     detailsPage.openRelatedTaskWorkflowEvents(0);
     assertTrue(detailsPage.isRelatedTaskWorkflowEventsOpened());
   }
 
   @Test
   public void testRelatedTaskAddAdHocTask() {
-    detailsPage.sideStepMenuOnActionButton(2);
+    createTestingTask();
+    detailsPage.clickRelatedTaskActionButton(2);
     ExpressProcessPage expressProcessPage = detailsPage.addAdHocTask(2);
     assertTrue(expressProcessPage.getProcessName().endsWith("Sick Leave Request"));
   }
 
   @Test
   public void testRelatedTaskExportToExcel() {
+    createTestingTask();
     detailsPage.clickExportToExcelLink("related-task-export-to-excel", "related-task-status-dialog");
     assertTrue(detailsPage.isDownloadCompleted("related-task-status-dialog"));
   }
 
   @Test
   public void testRelatedCaseExportToExcel() {
-    redirectToRelativeLink(createCaseWithTechnicalCaseUrl);
-    MainMenuPage mainMenuPage = homePage.openMainMenu();
-    CaseWidgetPage casePage = mainMenuPage.selectCaseMenu();
-    detailsPage = casePage.openDetailsOfCaseHasName(ORDER_PIZZA);
+    createTestingCaseContainTechnicalCases();
     detailsPage.clickExportToExcelLink("related-case-export-to-excel", "related-case-status-dialog");
     assertTrue(detailsPage.isDownloadCompleted("related-case-status-dialog"));
   }
 
   @Test
   public void testRelatedCaseEnableAndDisableColumns() {
-    redirectToRelativeLink(createCaseWithTechnicalCaseUrl);
-    MainMenuPage mainMenuPage = homePage.openMainMenu();
-    CaseWidgetPage casePage = mainMenuPage.selectCaseMenu();
-    detailsPage = casePage.openDetailsOfCaseHasName(ORDER_PIZZA);
+    createTestingCaseContainTechnicalCases();
     assertTrue(detailsPage.isRelatedCaseListColumnExist(RELATED_CASE_CREATED_COLUMN));
     assertTrue(detailsPage.isRelatedCaseListColumnExist(RELATED_CASE_STATE_COLUMN));
     detailsPage.clickRelatedCaseColumnsButton();
@@ -212,30 +273,33 @@ public class CaseDetailsTest extends BaseTest {
 
   @Test
   public void testRelatedTaskEnableAndDisableColumns() {
+    createTestingTask();
     assertTrue(detailsPage.isRelatedTaskListColumnExist(RELATED_TASK_EXPIRY_COLUMN));
     assertTrue(detailsPage.isRelatedTaskListColumnExist(RELATED_TASK_STATE_COLUMN));
     detailsPage.clickRelatedTaskColumnsButton();
     detailsPage.clickRelatedTaskDefaultCheckbox();
     detailsPage.clickRelatedTaskColumnCheckbox(6);
     detailsPage.clickRelatedTaskApplyButton();
-    //WaitHelper.assertTrueWithWait(() -> !detailsPage.isRelatedTaskListColumnExist(RELATED_TASK_EXPIRY_COLUMN));
+    WaitHelper.assertTrueWithWait(() -> !detailsPage.isRelatedTaskListColumnExist(RELATED_TASK_EXPIRY_COLUMN));
     WaitHelper.assertTrueWithWait(() -> detailsPage.isRelatedTaskListColumnExist(RELATED_TASK_STATE_COLUMN));
     detailsPage.clickRelatedTaskColumnsButton();
     detailsPage.clickRelatedTaskColumnCheckbox(6);
     detailsPage.clickRelatedTaskColumnCheckbox(7);
     detailsPage.clickRelatedTaskApplyButton();
     WaitHelper.assertTrueWithWait(() -> detailsPage.isRelatedTaskListColumnExist(RELATED_TASK_EXPIRY_COLUMN));
-    //WaitHelper.assertTrueWithWait(() -> !detailsPage.isRelatedTaskListColumnExist(RELATED_TASK_STATE_COLUMN));
+    WaitHelper.assertTrueWithWait(() -> !detailsPage.isRelatedTaskListColumnExist(RELATED_TASK_STATE_COLUMN));
   }
 
   @Test
   public void testHistoryAuthorIsUserFullName() {
+    createTestingTask();
     detailsPage.addNote("Sample case note");
     assertEquals(TestAccount.ADMIN_USER.getFullName(), detailsPage.getHistoryAuthor());
   }
 
   @Test
   public void testOpenViewNoteDialog() {
+    createTestingTask();
     detailsPage.addNote("Consider the remaining annual leaves before the approval");
     detailsPage.clickViewNote();
     assertTrue(detailsPage.isViewNoteDialogPresented());
@@ -243,6 +307,7 @@ public class CaseDetailsTest extends BaseTest {
   
   @Test
   public void testDragDropWidgets() {
+    createTestingTask();
     detailsPage.switchToEditMode();
     detailsPage.waitForSaveButtonDisplayed();
     detailsPage.drapAndDropWidgets("information", "document");
@@ -250,6 +315,9 @@ public class CaseDetailsTest extends BaseTest {
     detailsPage.saveAndSwitchToViewMode();
     detailsPage.switchToEditMode();
     detailsPage.waitForResetButtonDisplayed();
+    detailsPage.resetToDefault();
+    detailsPage.confirmResetToDefault();
+    detailsPage.saveAndSwitchToViewMode();
   }
 
   @After
