3719aaebc7d8145ef83d910b3a56ddd532075763
Nguyen Quoc Hoan <hoan.nguyen01@axonactive.com>
2021-04-01 13:48:22 +0700

    IVYPORTAL-10787-create-a-case-list-widget - handle feedback

--- AxonIvyPortal/PortalKit/src/ch/ivy/addon/portalkit/dto/dashboard/taskcolumn/DescriptionColumnModel.java
+++ AxonIvyPortal/PortalKit/src/ch/ivy/addon/portalkit/dto/dashboard/taskcolumn/DescriptionColumnModel.java
@@ -13,7 +13,7 @@ public class DescriptionColumnModel extends TaskColumnModel implements Serializa
   public void initDefaultValue() {
     this.header = defaultIfEmpty(this.header, cms("/ch.ivy.addon.portalkit.ui.jsf/common/description"));
     this.field = DashboardStandardTaskColumn.DESCRIPTION.getField();
-    this.styleClass = defaultIfEmpty(this.styleClass, "dashboard-tasks__description u-text-align-center");
+    this.styleClass = defaultIfEmpty(this.styleClass, "dashboard-tasks__description u-text-align-left");
     this.sortable = false;
   }
   
