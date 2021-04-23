c3a1b419a2ab008705d8584e6d677930865bf5eb
Nguyen Quoc Hoan <hoan.nguyen01@axonactive.com>
2021-04-23 11:26:10 +0700

    IVYPORTAL-12035 Various or urgent requests in IVYPORTAL - S207 - Show a tooltip to display the role members - SecurityMemberName Component
    Former-commit-id: c20d6e3cc8a21847f98ee5b6fbcc1017b9afe134

--- AxonIvyPortal/PortalKit/src/ch/ivy/addon/portalkit/util/SecurityMemberUtils.java
+++ AxonIvyPortal/PortalKit/src/ch/ivy/addon/portalkit/util/SecurityMemberUtils.java
@@ -1,5 +1,6 @@
 package ch.ivy.addon.portalkit.util;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -8,9 +9,11 @@ import ch.ivy.addon.portalkit.dto.RoleDTO;
 import ch.ivy.addon.portalkit.dto.SecurityMemberDTO;
 import ch.ivy.addon.portalkit.dto.UserDTO;
 import ch.ivyteam.ivy.environment.Ivy;
+import ch.ivyteam.ivy.persistence.query.IPagedResult;
 import ch.ivyteam.ivy.process.call.SubProcessCall;
 import ch.ivyteam.ivy.security.IRole;
 import ch.ivyteam.ivy.security.ISecurityMember;
+import ch.ivyteam.ivy.security.IUser;
 
 public class SecurityMemberUtils {
   
@@ -55,4 +58,26 @@ public class SecurityMemberUtils {
         return Ivy.wf().getSecurityContext().findRole(roleDTO.getId());
     });
   }
+
+  public static String buildTooltipFromUsers(String roleName) {
+    String linePattern = "<p class='%s'> %s </p>";
+    IRole role = Ivy.wf().getSecurityContext().findRole(roleName);
+    IPagedResult<IUser> result = role.users().assignedPaged(10);
+    List<IUser> users = result.page(1);
+    long totalCount = result.count();
+    String fullnameOfRole = SecurityMemberDisplayNameUtils.generateFullDisplayNameForRole(role.getDisplayName(), role.getName());
+    String header = Ivy.cms().co("/ch.ivy.addon.portalkit.ui.jsf/components/SecurityMemberDisplayName/userOfRoleTitle",
+        Arrays.asList(fullnameOfRole)).concat(":");
+    StringBuilder userAsHtmlText = new StringBuilder(String.format(linePattern, "tooltip-header", header));
+    for (IUser user : users) {
+      String fullnameOfUser = SecurityMemberDisplayNameUtils.generateFullDisplayNameForUser(user, user.getName());
+      userAsHtmlText.append(String.format(linePattern, "tooltip-body-item", fullnameOfUser));
+    }
+    if (totalCount > 10) {
+      String more = Ivy.cms().co("/ch.ivy.addon.portalkit.ui.jsf/common/more").concat(" ...");
+      userAsHtmlText.append(String.format(linePattern, "tooltip-body-item", more));
+    }
+    return userAsHtmlText.toString();
+  }
+
 }
