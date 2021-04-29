32a0ad13cd76a2eb25e2e1e33cd89b866b4cdca7
Long Vo Thanh <long.vothanh@axonactive.com>
2021-04-29 17:15:21 +0700

    IVYPORTAL-12095: Enhance JSON configuration for URL of custom iframe in task details - Handle feedbacK: remove TaskDetailsCustomWidgetParamPattern, use switch - case instead of if - else if for task property enums

--- AxonIvyPortal/PortalKit/src/ch/ivy/addon/portalkit/constant/TaskDetailsCustomWidgetParamPattern.java
+++
@@ -1,8 +0,0 @@
-package ch.ivy.addon.portalkit.constant;
-
-public class TaskDetailsCustomWidgetParamPattern {
-  public static final String TASK_PROPERTY_KEY_PATTERN_DELIMITER = "\\.";
-  public static final String TASK_PROPERTY_PREFIX = "task";
-  public static final String TASK_CUSTOM_FIELD_PREFIX = "custom";
-
-}
