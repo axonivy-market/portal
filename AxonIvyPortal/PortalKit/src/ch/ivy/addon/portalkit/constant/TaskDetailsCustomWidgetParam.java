d0067c88762622653a7989161b5b3ae1ceef73df
Long Vo Thanh <long.vothanh@axonactive.com>
2021-04-29 09:34:28 +0700

    IVYPORTAL-12095: Enhance JSON configuration for URL of custom iframe in task details - Handle feedback: move constant to new class TaskDetailsCustomWidgetParam

---
+++ AxonIvyPortal/PortalKit/src/ch/ivy/addon/portalkit/constant/TaskDetailsCustomWidgetParam.java
@@ -0,0 +1,10 @@
+package ch.ivy.addon.portalkit.constant;
+
+public class TaskDetailsCustomWidgetParam {
+  public static final String TASK_PROPERTY_KEY_PATTERN_DELIMITER = "\\.";
+  public static final String TASK_PROPERTY_PREFIX = "task";
+  public static final String TASK_CUSTOM_FIELD_PREFIX = "custom";
+  public static final String TASK_ID = "id";
+  public static final String TASK_CATEGORY = "category";
+
+}
