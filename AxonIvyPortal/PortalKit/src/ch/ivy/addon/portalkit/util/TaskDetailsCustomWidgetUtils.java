54b941f22007aa8bbf5cfda7447e4a1b8d379604
Long Vo Thanh <long.vothanh@axonactive.com>
2021-04-29 17:15:21 +0700

    IVYPORTAL-12095: Enhance JSON configuration for URL of custom iframe in task details - Handle feedbacK: remove TaskDetailsCustomWidgetParamPattern, use switch - case instead of if - else if for task property enums

--- AxonIvyPortal/PortalKit/src/ch/ivy/addon/portalkit/util/TaskDetailsCustomWidgetUtils.java
+++ AxonIvyPortal/PortalKit/src/ch/ivy/addon/portalkit/util/TaskDetailsCustomWidgetUtils.java
@@ -1,22 +1,26 @@
 package ch.ivy.addon.portalkit.util;
 
+import java.util.Arrays;
+
 import org.apache.commons.lang3.StringUtils;
 
-import ch.ivy.addon.portalkit.constant.TaskDetailsCustomWidgetParamPattern;
 import ch.ivy.addon.portalkit.enums.TaskDetailsCustomWidgetParam;
 import ch.ivyteam.ivy.workflow.ITask;
 import ch.ivyteam.ivy.workflow.custom.field.ICustomField;
 
 public class TaskDetailsCustomWidgetUtils {
+  public static final String TASK_PROPERTY_KEY_PATTERN_DELIMITER = "\\.";
+  public static final String TASK_PROPERTY_PREFIX = "task";
+  public static final String TASK_CUSTOM_FIELD_PREFIX = "custom";
 
   public static String getTaskPropertyByKeyPattern(ITask task, String keyPattern) {
     if (task != null) {
-      String[] keyParts = keyPattern.split(TaskDetailsCustomWidgetParamPattern.TASK_PROPERTY_KEY_PATTERN_DELIMITER);
+      String[] keyParts = keyPattern.split(TASK_PROPERTY_KEY_PATTERN_DELIMITER);
       if (keyParts.length == 2) {
         switch (keyParts[0]) {
-          case TaskDetailsCustomWidgetParamPattern.TASK_PROPERTY_PREFIX:
+          case TASK_PROPERTY_PREFIX:
             return getTaskPropertyByKey(task, keyParts[1]);
-          case TaskDetailsCustomWidgetParamPattern.TASK_CUSTOM_FIELD_PREFIX:
+          case TASK_CUSTOM_FIELD_PREFIX:
             return getTaskCustomFieldByKey(task, keyParts[1]);
           default:
             return keyPattern;
@@ -27,12 +31,14 @@ public class TaskDetailsCustomWidgetUtils {
   }
 
   private static String getTaskPropertyByKey(ITask task, String key) {
-    if (TaskDetailsCustomWidgetParam.ID.getValue().equals(key)) {
-      return String.valueOf(task.getId());
-    } else if (TaskDetailsCustomWidgetParam.CATEGORY.getValue().equals(key)) {
-      return String.valueOf(task.getCategory().getPath());
-    } else {
-      return StringUtils.EMPTY;
+    TaskDetailsCustomWidgetParam foundKey = Arrays.asList(TaskDetailsCustomWidgetParam.values()).stream().filter(k -> k.getValue().contentEquals(key)).findFirst().orElse(null);
+    switch(foundKey) {
+      case ID:
+          return String.valueOf(task.getId());
+      case CATEGORY:
+          return String.valueOf(task.getCategory().getPath());
+      default:
+          return StringUtils.EMPTY;
     }
   }
 
