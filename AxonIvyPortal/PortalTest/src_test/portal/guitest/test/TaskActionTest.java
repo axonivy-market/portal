050314f7ce4432000885c5c64ad269caf1e4cb65
PHAM HOANG HUNG <hung.pham@axonactive.com>
2021-05-04 17:23:40 +0700

    Fix GUI test for release 9.3.0-m207

--- AxonIvyPortal/PortalTest/src_test/portal/guitest/test/TaskActionTest.java
+++ AxonIvyPortal/PortalTest/src_test/portal/guitest/test/TaskActionTest.java
@@ -41,21 +41,21 @@ public class TaskActionTest extends BaseTest {
     taskWidgetPage = taskDetailsPage.goBackToTaskListFromTaskDetails();
 
     // Suspended
-    assertTaskActionsByTaskState("Suspended", Arrays.asList("Delegate", "Reserve", "Clear the expiry", "Add Ad-hoc Task"));
+    assertTaskActionsByTaskState("Suspended", Arrays.asList("Delegate", "Reserve", "Clear expiry", "Add Ad-hoc Task"));
     taskWidgetPage = taskDetailsPage.goBackToTaskListFromTaskDetails();
 
     // Reserved
     taskWidgetPage.clickOnTaskActionLink(0);
     taskWidgetPage.reserveTask(0);
     taskWidgetPage.waitAjaxIndicatorDisappear();
-    assertTaskActionsByTaskState("Reserved", Arrays.asList("Delegate", "Reset", "Clear the expiry", "Add Ad-hoc Task"));
+    assertTaskActionsByTaskState("Reserved", Arrays.asList("Delegate", "Reset", "Clear expiry", "Add Ad-hoc Task"));
 
     // In progress
     TaskTemplatePage taskTemplatePage = taskDetailsPage.clickStartTask();
     taskTemplatePage.clickCancelLink();
     taskDetailsPage = new TaskDetailsPage();
     taskWidgetPage = taskDetailsPage.goBackToTaskListFromTaskDetails();
-    assertTaskActionsByTaskState("In progress", Arrays.asList("Reserve", "Reset", "Clear the expiry", "Add Ad-hoc Task"));
+    assertTaskActionsByTaskState("In progress", Arrays.asList("Reserve", "Reset", "Clear expiry", "Add Ad-hoc Task"));
     taskWidgetPage = taskDetailsPage.goBackToTaskListFromTaskDetails();
   }
 
@@ -70,21 +70,21 @@ public class TaskActionTest extends BaseTest {
     taskWidgetPage = taskDetailsPage.goBackToTaskListFromTaskDetails();
 
     // Suspended
-    assertTaskActionsByTaskState("Suspended", Arrays.asList("Delegate", "Reserve", "Clear the expiry", "Destroy", "Add Ad-hoc Task"));
+    assertTaskActionsByTaskState("Suspended", Arrays.asList("Delegate", "Reserve", "Clear expiry", "Destroy", "Add Ad-hoc Task"));
     taskWidgetPage = taskDetailsPage.goBackToTaskListFromTaskDetails();
 
     // Reserved
     taskWidgetPage.clickOnTaskActionLink(0);
     taskWidgetPage.reserveTask(0);
     taskWidgetPage.waitAjaxIndicatorDisappear();
-    assertTaskActionsByTaskState("Reserved", Arrays.asList("Delegate", "Reset", "Clear the expiry", "Destroy", "Add Ad-hoc Task"));
+    assertTaskActionsByTaskState("Reserved", Arrays.asList("Delegate", "Reset", "Clear expiry", "Destroy", "Add Ad-hoc Task"));
 
     // In progress
     TaskTemplatePage taskTemplatePage = taskDetailsPage.clickStartTask();
     taskTemplatePage.clickCancelLink();
     taskDetailsPage = new TaskDetailsPage();
     taskWidgetPage = taskDetailsPage.goBackToTaskListFromTaskDetails();
-    assertTaskActionsByTaskState("In progress", Arrays.asList("Reserve", "Reset", "Clear the expiry", "Destroy", "Add Ad-hoc Task"));
+    assertTaskActionsByTaskState("In progress", Arrays.asList("Reserve", "Reset", "Clear expiry", "Destroy", "Add Ad-hoc Task"));
     taskWidgetPage = taskDetailsPage.goBackToTaskListFromTaskDetails();
 
     // Done
