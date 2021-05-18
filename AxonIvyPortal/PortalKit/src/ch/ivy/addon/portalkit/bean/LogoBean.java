68dbd5425688e630caf75455b783460b511c349b
nam.mai <nam.mai@axonactive.com>
2021-05-18 11:46:06 +0700

    IVYPORTAL-12151: Implement new Axon Ivy logo to all portal related products - Implemented - Updated document
    Former-commit-id: ecfef7b81112802c08a27dcc4776667a30cc2531

---
+++ AxonIvyPortal/PortalKit/src/ch/ivy/addon/portalkit/bean/LogoBean.java
@@ -0,0 +1,39 @@
+package ch.ivy.addon.portalkit.bean;
+
+import java.io.IOException;
+import java.io.Serializable;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+import ch.ivyteam.ivy.environment.Ivy;
+
+@ManagedBean
+@ViewScoped
+public class LogoBean implements Serializable {
+  private static final long serialVersionUID = 1L;
+  private static final String CORPORATE_LOGO_CMS = "/images/logo/CorporateLogo";
+  private static final String SMALL_CORPORATE_LOGO_CMS = "/images/logo/SmallCorporateLogo";
+  private static final String LOGIN_LOGO_CMS = "/images/logo/loginLogo";
+  private static final String FAVICON_LOGO_CMS = "/images/logo/faviconLogo";
+
+  public boolean hasCorporateLogoImage() throws IOException {
+    return checkDefaultContentExist(CORPORATE_LOGO_CMS);
+  }
+
+  public boolean hasSmallCorporateLogoImage() {
+    return checkDefaultContentExist(SMALL_CORPORATE_LOGO_CMS);
+  }
+
+  public boolean hasLoginLogoImage() {
+    return checkDefaultContentExist(LOGIN_LOGO_CMS);
+  }
+
+  public boolean hasFaviconLogoImage() {
+    return checkDefaultContentExist(FAVICON_LOGO_CMS);
+  }
+
+  private boolean checkDefaultContentExist(String cmsUri) {
+    return Ivy.cms().findContentObjectValue(cmsUri, Ivy.cms().getDefaultLanguage()).getContentAsBinaryStream() != null;
+  }
+}
