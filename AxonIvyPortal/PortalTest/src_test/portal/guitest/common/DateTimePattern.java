502a36875fed737ee3e34f169a1198a030736700
Pham Hoang Hung <hung.pham@axonactive.com>
2021-05-19 03:46:58 +0000

    IVYPORTAL-12148-Detect-date-pattern-by-browser-locale-with-Type-DEFAULT Approved-by: Long Vo Thanh

--- AxonIvyPortal/PortalTest/src_test/portal/guitest/common/DateTimePattern.java
+++ AxonIvyPortal/PortalTest/src_test/portal/guitest/common/DateTimePattern.java
@@ -4,4 +4,5 @@ public class DateTimePattern {
 
   public static final String DATE_PATTERN = "M/d/yy";
   public static final String DATE_TIME_PATTERN = "M/d/yy HH:mm";
+  public static final String DEFAULT_DATE_TIME_PATTERN_EN = "MMM d, y HH:mm";
 }
