de2a83b1e02e20f2ca2797565402a07087c5744e
Nguyen Quoc Hoan <hoan.nguyen01@axonactive.com>
2021-06-16 18:18:25 +0700

    IVYPORTAL-11908: Change JSON Configuration of case details to the new approach - implement
    Former-commit-id: 7a47936549a94fc0b1a6cf4b1b0f450a0598011f

---
+++ AxonIvyPortal/PortalKit/src/ch/ivy/addon/portalkit/dto/widget/HistoryWidget.java
@@ -0,0 +1,8 @@
+package ch.ivy.addon.portalkit.dto.widget;
+
+public class HistoryWidget extends AbstractWidget {
+
+  private static final long serialVersionUID = -4972389298376434497L;
+
+  public HistoryWidget() {}
+}
