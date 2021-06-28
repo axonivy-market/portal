5f46c2669798d1e2a986f104801aa02ee7d5ae37
Pham Hoang Hung <hung.pham@axonactive.com>
2021-06-28 07:48:58 +0000

    IVYPORTAL-12079 image process list mode - Implement UI, integrate more information link, edit/delete process button - Display default image if customer had defined - Allow admin to select default image - Create example of photo library

--- AxonIvyPortal/PortalTest/src_test/portal/guitest/common/Variable.java
+++ AxonIvyPortal/PortalTest/src_test/portal/guitest/common/Variable.java
@@ -34,6 +34,7 @@ public enum Variable {
   SHOW_CASE_DURATION_TIME("Portal.CaseDetails.ShowDurationTime"),
   HIDE_UPLOAD_DOCUMENT_FOR_DONE_CASE("Portal.CaseDetails.HideUploadDocumentForDoneCase"),
   DEFAULT_PROCESS_MODE("Portal.Processes.Mode"),
+  DEFAULT_PROCESS_IMAGE("Portal.Processes.DefaultImage"),
   DISPLAY_MESSAGE_AFTER_FINISH_TASK("Portal.DisplayMessageAfterFinishTask"),
   EXPRESS_END_PAGE("Portal.ExpressEndPage"),
   CLIENT_SIDE_TIMEOUT("Portal.ClientSideTimeout"),
