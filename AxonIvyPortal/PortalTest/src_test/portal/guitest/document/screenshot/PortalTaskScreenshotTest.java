1fcde0f0bdf146040ea9c93340488fccecee6d27
Nam.Chu <nam.chu@axonactive.com>
2021-06-29 10:48:33 +0700

    IVYPORTAL-12179 Display all tasks from disabled users - Add screenshot test

--- AxonIvyPortal/PortalTest/src_test/portal/guitest/document/screenshot/PortalTaskScreenshotTest.java
+++ AxonIvyPortal/PortalTest/src_test/portal/guitest/document/screenshot/PortalTaskScreenshotTest.java
@@ -252,15 +252,4 @@ public class PortalTaskScreenshotTest extends ScreenshotTest {
     taskWidgetPage.closeMainMenu();
     return taskDetails;
   }
-  
-  @Test
-  public void screenshotTaskFilterMoreOption() throws IOException {
-    ScreenshotUtil.resizeBrowser(new Dimension(SCREENSHOT_WIDTH, 1200));
-    login(TestAccount.ADMIN_USER);
-    TaskWidgetPage taskWidget = homePage.openTaskList();
-    taskWidget.openNoActivatorFilter("Missing activator");
-    taskWidget.filterByUnavailableActivator(false);
-    ScreenshotUtil.captureHalfTopRightPageScreenShot(ScreenshotUtil.TASK_WIDGET_FOLDER + "task-filter-more-option");
-  }
-
 }
