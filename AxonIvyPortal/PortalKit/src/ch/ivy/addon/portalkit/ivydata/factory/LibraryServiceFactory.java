fc7d07e5675adddaba32aca147fc7b3e15738e01
Nam.Chu <nam.chu@axonactive.com>
2021-07-07 17:39:09 +0700

    IVYPORTAL-12443 PortalGroupId variable should be detected automatically - Update default page in yaml aumatically after change group Id

--- AxonIvyPortal/PortalKit/src/ch/ivy/addon/portalkit/ivydata/factory/LibraryServiceFactory.java
+++ AxonIvyPortal/PortalKit/src/ch/ivy/addon/portalkit/ivydata/factory/LibraryServiceFactory.java
@@ -1,13 +1,13 @@
 package ch.ivy.addon.portalkit.ivydata.factory;
 
 import ch.ivy.addon.portalkit.ivydata.service.ILibraryService;
-import ch.ivy.addon.portalkit.ivydata.service.impl.NoRegisteredAppLibraryService;
+import ch.ivy.addon.portalkit.ivydata.service.impl.LibraryService;
 
 public class LibraryServiceFactory {
 
   private LibraryServiceFactory() {}
 
   public static ILibraryService getLibraryService() {
-    return new NoRegisteredAppLibraryService();
+    return new LibraryService();
   }
 }
