f9f7dc9382f5a81714d96011afab4eae7db72cf3
Loi Nguyen <loi.nguyenkimvan@axonactive.com>
2021-09-08 08:28:21 +0700

    IVYPORTAL-12469 Create GUI Tests - Change project, package name

--- AxonIvyPortal/portal-selenium-test/src_test/portal/guitest/common/DateTimePattern.java
+++
@@ -1,7 +0,0 @@
-package portal.guitest.common;
-
-public class DateTimePattern {
-
-  public static final String DATE_PATTERN = "MMM d, yyyy";
-  public static final String DATE_TIME_PATTERN = "MMM d, yyyy HH:mm";
-}
--- AxonIvyPortal/portal-selenium-test/src_test/portal/guitest/common/TestRole.java
+++
@@ -1,6 +0,0 @@
-package portal.guitest.common;
-
-public class TestRole {
-  public static final String EVERYBODY_ROLE = "Everybody";
-  public static final String HR_ROLE = "Human resources department";
-}
