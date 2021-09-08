bab0bbd7ec83142eb09d405d5eeea7edbbc6c0b3
Long Vo Thanh <long.vothanh@axonactive.com>
2021-09-08 11:43:54 +0700

    IVYPORTAL-12575 Support legacy mode and new dashboard - Implement setting for Admin with default is new dashboard - Implement logic to display dashboard for specific mode - Handle Processes page mode when Setting is legacy mode - Update GUI tests: support legacy UI tests/screenshots
    Former-commit-id: 3c39f833cf415c42c97ebac6914c71c9ae713f67

--- AxonIvyPortal/PortalTest/src_test/portal/guitest/common/ScreenshotTest.java
+++ AxonIvyPortal/PortalTest/src_test/portal/guitest/common/ScreenshotTest.java
@@ -1,6 +1,7 @@
 package portal.guitest.common;
 
 import static portal.guitest.common.Variable.DISPLAY_MESSAGE_AFTER_FINISH_TASK;
+import static portal.guitest.common.Variable.SHOW_LEGACY_UI;
 
 import org.junit.Before;
 
@@ -12,6 +13,7 @@ public class ScreenshotTest extends BaseTest {
     killBrowsers();
     super.setup();
     updatePortalSetting(DISPLAY_MESSAGE_AFTER_FINISH_TASK.getKey(), "false");
+    updatePortalSetting(SHOW_LEGACY_UI .getKey(), "true");
   }
 
 }
