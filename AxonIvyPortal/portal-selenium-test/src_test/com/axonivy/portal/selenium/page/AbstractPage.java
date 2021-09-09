8a101bbc2d19da86acfd0330aaee3534bd72b096
Loi Nguyen <loi.nguyenkimvan@axonactive.com>
2021-09-09 13:36:36 +0700

    IVYPORTAL-12469 Create GUI Tests - merged base gui test

---
+++ AxonIvyPortal/portal-selenium-test/src_test/com/axonivy/portal/selenium/page/AbstractPage.java
@@ -0,0 +1,36 @@
+package com.axonivy.portal.selenium.page;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.interactions.Actions;
+public abstract class AbstractPage {
+	
+    //protected WebDriver driver;
+    //private Actions builder;
+    protected  WebDriver driver;
+    private  Actions builder;
+    protected final Log log;
+    protected static final long DEFAULT_TIMEOUT  = 45000;
+    
+    /**
+     * This abstract method is used to determine identity of a page.
+     * @return A unique xpath for the particular page.
+     */
+    protected abstract String getLoadedLocator();
+    
+    public WebDriver getDriver() {
+    	return driver;
+    }
+    
+    protected AbstractPage() {
+        log = LogFactory.getLog(getClass());
+        log.debug("Created page abstraction for " + getClass().getName());
+    }
+    
+    public void init(WebDriver webDriver) {
+      driver = webDriver;
+      builder = new Actions(driver);
+    }
+	 
+}
