7043180dbfa8141d21035dc38f84f5d0169b53a6
Nguyen Quoc Hoan <hoan.nguyen01@axonactive.com>
2021-09-13 10:23:56 +0700

    IVYPORTAL-12037-Migrate-Portal-Configuration-stored-in-business-data-user-properties - fix conflict

--- AxonIvyPortal/PortalKit/src/ch/ivy/addon/portalkit/util/RequestUtil.java
+++ AxonIvyPortal/PortalKit/src/ch/ivy/addon/portalkit/util/RequestUtil.java
@@ -6,7 +6,6 @@ import ch.ivyteam.ivy.request.IHttpResponse;
 public class RequestUtil {
 
   public static void redirect(String uri) throws java.io.IOException {
-    Ivy.log().error("go to redirect IHttpResponse");
     IHttpResponse httpResponse = (IHttpResponse) Ivy.response();
     httpResponse.sendRedirect(uri);
   }
