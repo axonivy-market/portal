d85f382e3bc5632fd152961ef8b0ace992e5506f
lttung_wawa <tung.le@axonactive.com>
2021-09-13 09:24:46 +0700

    IVYPORTAL-12420 GUI tests for case widget of new Dashboard - Clean up

--- AxonIvyPortal/portal-selenium-test/src_test/com/axonivy/portal/selenium/common/ChromeConfiguration.java
+++
@@ -1,9 +0,0 @@
-package com.axonivy.portal.selenium.common;
-
-public class ChromeConfiguration extends AbstractBrowserConfiguration {
-
-  @Override
-  protected String getDriverPathInClasspath() {
-    return "./resources/ChromeDriver.exe";
-  }
-}
