efd3bf2a4a3475f6e9c80e589fc7b46f920846db
Pham Hoang Hung <hung.pham@axonactive.com>
2021-09-30 09:09:35 +0000

    Feature/IVYPORTAL-12084 Improve visibility of history and notes with relations between business case and technical cases
    * Implement - add note on task details that has technical case and technical case details
    
    * create & update GUI TESTs
    
    Approved-by: Loi Nguyen Kim Van
    Former-commit-id: ec7b641c4b655677616cbe92544a290c212bb574

---
+++ AxonIvyPortal/portal-selenium-test/src_test/com/axonivy/portal/selenium/page/CaseDetailsPage.java
@@ -0,0 +1,63 @@
+package com.axonivy.portal.selenium.page;
+
+import static com.codeborne.selenide.Condition.appear;
+import static com.codeborne.selenide.Condition.disappear;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
+
+import com.codeborne.selenide.ElementsCollection;
+
+public class CaseDetailsPage extends TemplatePage {
+  private static final String RELATED_CASES = "Related Cases";
+  private static final String RELATED_TASKS_OF_CASES = "Related Tasks of Case";
+  private static final String HISTORY = "History";
+
+  @Override
+  protected String getLoadedLocator() {
+    return "id('case-item-details:case-details-container:case-detail-body')";
+  }
+
+  public void waitForTechnicalCaseDisplay() {
+    $("div[id='case-details-technicalCase-panel']").waitUntil(appear, DEFAULT_TIMEOUT);
+  }
+
+  public ElementsCollection getRelatedCasesComponent() {
+    return $$("div[id='case-item-details:case-details-container:case-detail-body']").filter(text(RELATED_CASES));
+  }
+
+  public ElementsCollection getRelatedTasksOfCasesComponent() {
+    return $$("div[id='case-item-details:case-details-container:case-detail-body']")
+        .filter(text(RELATED_TASKS_OF_CASES));
+  }
+
+  public ElementsCollection getHitoriesComponent() {
+    return $$("div[id='case-item-details:case-details-container:case-detail-body']").filter(text(HISTORY));
+  }
+
+  public void addNote(String noteContent) {
+    $("a[id$=':case-histories:add-note-command']").click();
+    $("div[id$=':case-histories:add-note-dialog']").waitUntil(appear, DEFAULT_TIMEOUT);
+    $("div[id$=':case-histories:add-note-dialog']").find("textarea").sendKeys(noteContent);
+    $("button[id$=':case-histories:add-note-form:save-add-note-command']").click();
+    $("div[id$=':case-histories:add-note-dialog']").waitUntil(disappear, DEFAULT_TIMEOUT);
+  }
+
+  public ElementsCollection getNotesWithContent(String content) {
+    return $$("span[id$=':case-histories:case-histories-table'] table tbody tr td a").filter(text(content));
+  }
+
+  public void gotoTaskDetailsPageOfRelatedTask(String taskName) {
+    $$("div[id='case-details-related-task-table'] table tbody tr td span").filter(text(taskName)).first().click();
+  }
+
+  public void gotoCaseDetailsPageOfRelatedCase(String caseName) {
+    $$("div[id$=':related-cases-widget:related-cases'] table tbody tr td span").filter(text(caseName)).first().click();
+    $("div[id$=':general-information:business-case-information']").waitUntil(appear, DEFAULT_TIMEOUT);
+  }
+
+  public void gotoBusinessCase() {
+    $("a[id$=':general-information:related-business-case']").click();
+    $("div[id$=':general-information:business-case-information']").waitUntil(disappear, DEFAULT_TIMEOUT);
+  }
+}
