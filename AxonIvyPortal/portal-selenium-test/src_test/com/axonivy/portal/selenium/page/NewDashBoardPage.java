1494b2f16526d2a003e014899b23f54f6d1b26eb
Nam.Chu <nam.chu@axonactive.com>
2021-10-04 15:12:00 +0700

    IVYPORTAL-12668 Image process start widget for dashboard - Add UI test

--- AxonIvyPortal/portal-selenium-test/src_test/com/axonivy/portal/selenium/page/NewDashBoardPage.java
+++ AxonIvyPortal/portal-selenium-test/src_test/com/axonivy/portal/selenium/page/NewDashBoardPage.java
@@ -3,6 +3,9 @@ package com.axonivy.portal.selenium.page;
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.*;
 
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
+
 public class NewDashBoardPage extends TemplatePage {
 
   public CaseWidgetNewDashBoardPage selectCaseWidget(String caseWidgetName) {
@@ -17,7 +20,7 @@ public class NewDashBoardPage extends TemplatePage {
     $("button[id='switch-to-edit-mode']").waitUntil(appear, DEFAULT_TIMEOUT).shouldBe(getClickableCondition()).click();
     $("a[id='switch-to-view-mode']").waitUntil(appear, DEFAULT_TIMEOUT).should(appear);
   }
-
+  
   public void addWidget() {
     $("button[id='add-button']").waitUntil(appear, DEFAULT_TIMEOUT).shouldBe(getClickableCondition()).click();
   }
@@ -33,4 +36,21 @@ public class NewDashBoardPage extends TemplatePage {
     return new CaseEditWidgetNewDashBoardPage();
   }
 
+  public ProcessEditWidgetNewDashBoardPage editProcessWidgetConfiguration() {
+    switchToEditMode();
+    $("a[id$=':edit-widget-2']").waitUntil(Condition.appear, DEFAULT_TIMEOUT).shouldBe(getClickableCondition()).click();
+    return new ProcessEditWidgetNewDashBoardPage();
+  }
+  
+  public SelenideElement getStartProcessButton() {
+    return $("button[id$=':start-button']");
+  }
+  
+  public SelenideElement getProcessButton() {
+    return $("button[id$=':start-button']");
+  }
+  
+  public SelenideElement getProcessImage() {
+    return $("img.image-process-item-image");
+  }
 }
