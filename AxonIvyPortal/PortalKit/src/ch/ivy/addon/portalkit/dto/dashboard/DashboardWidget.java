369d16f4e64bb525ab86ccc63492be613feb8e91
nam.mai <nam.mai@axonactive.com>
2021-10-25 10:49:53 +0700

    IVYPORTAL-12543: Custom widget for Portal Dashboard - Implemented

--- AxonIvyPortal/PortalKit/src/ch/ivy/addon/portalkit/dto/dashboard/DashboardWidget.java
+++ AxonIvyPortal/PortalKit/src/ch/ivy/addon/portalkit/dto/dashboard/DashboardWidget.java
@@ -21,7 +21,8 @@ import ch.ivyteam.ivy.environment.Ivy;
 @JsonSubTypes({
   @Type(value = TaskDashboardWidget.class, name = "task"),
   @Type(value = CaseDashboardWidget.class, name = "case"),
-  @Type(value = ProcessDashboardWidget.class, name = "process")
+  @Type(value = ProcessDashboardWidget.class, name = "process"),
+  @Type(value = CustomDashboardWidget.class, name = "custom")
 })
 @JsonInclude(JsonInclude.Include.NON_EMPTY)
 public abstract class DashboardWidget implements Serializable {
--- AxonIvyPortal/PortalKit/src/ch/ivy/addon/portalkit/dto/widget/CustomWidgetData.java
+++ AxonIvyPortal/PortalKit/src/ch/ivy/addon/portalkit/dto/widget/CustomWidgetData.java
@@ -1,32 +1,39 @@
 package ch.ivy.addon.portalkit.dto.widget;
 
 import java.io.Serializable;
-import java.util.Map;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import ch.ivy.addon.portalkit.dto.dashboard.CustomDashboardWidgetParam;
+import ch.ivy.addon.portalkit.enums.DashboardCustomWidgetType;
+import ch.ivyteam.ivy.workflow.start.IWebStartable;
+import ch.ivyteam.ivy.workflow.start.StartParameter;
 
 public class CustomWidgetData implements Serializable {
 
   private static final long serialVersionUID = 8763058243562205725L;
 
   private String url;
-  private String type;
 
   private String processStart;
-  private Map<String, String> params;
 
+  @JsonIgnore
+  private List<StartParameter> params;
+
+  @JsonIgnore
+  private DashboardCustomWidgetType type;
+  @JsonIgnore
+  private IWebStartable startableProcessStart;
+  
+  private List<CustomDashboardWidgetParam> customParams;
+  
   public String getUrl() {
     return url;
   }
 
   public void setUrl(String url) {
     this.url = url;
-  }
-
-  public Map<String, String> getParams() {
-    return params;
-  }
-
-  public void setParams(Map<String, String> params) {
-    this.params = params;
   }
 
   public String getProcessStart() {
@@ -37,11 +44,35 @@ public class CustomWidgetData implements Serializable {
     this.processStart = processStart;
   }
 
-  public String getType() {
+  public DashboardCustomWidgetType getType() {
     return type;
   }
 
-  public void setType(String type) {
+  public void setType(DashboardCustomWidgetType type) {
     this.type = type;
+  }
+
+  public IWebStartable getStartableProcessStart() {
+    return startableProcessStart;
+  }
+
+  public void setStartableProcessStart(IWebStartable startableProcessStart) {
+    this.startableProcessStart = startableProcessStart;
+  }
+
+  public List<CustomDashboardWidgetParam> getCustomParams() {
+    return customParams;
+  }
+
+  public void setCustomParams(List<CustomDashboardWidgetParam> customParams) {
+    this.customParams = customParams;
+  }
+
+  public List<StartParameter> getParams() {
+    return params;
+  }
+
+  public void setParams(List<StartParameter> params) {
+    this.params = params;
   }
 }
