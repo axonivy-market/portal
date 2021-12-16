29397a4ab05a65e25161d28236be02c219f9f864
nam.mai <nam.mai@axonactive.com>
2021-12-16 11:42:41 +0700

    IVYPORTAL-12899: Various or urgent requests in IVYPORTAL - S218 > IVYPORTAL-13013: Verify PR related to legacy CMS - Reverted code for review meeting

--- AxonIvyPortal/PortalKit/src/ch/ivy/addon/portalkit/bean/LogoBean.java
+++ AxonIvyPortal/PortalKit/src/ch/ivy/addon/portalkit/bean/LogoBean.java
@@ -5,7 +5,6 @@ import java.io.Serializable;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 
-import ch.ivyteam.ivy.cm.IContentObjectValue;
 import ch.ivyteam.ivy.environment.Ivy;
 
 @ManagedBean
@@ -34,7 +33,6 @@ public class LogoBean implements Serializable {
   }
 
   private boolean checkDefaultContentExist(String cmsUri) {
-    IContentObjectValue cmsValue = Ivy.cms().findContentObjectValue(cmsUri, null);
-    return cmsValue != null && cmsValue.getContentAsBinaryStream() != null;
+    return Ivy.cms().findContentObjectValue(cmsUri, null).getContentAsBinaryStream() != null;
   }
 }
