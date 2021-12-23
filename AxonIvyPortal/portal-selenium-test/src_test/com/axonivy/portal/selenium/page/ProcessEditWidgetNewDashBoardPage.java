859bf9334600dfd1da671cf9d13d732b75d233bc
Long Vo Thanh <long.vothanh@axonactive.com>
2021-12-23 11:08:34 +0700

    IVYPORTAL-12639 GUI tests for process widget of new Dashboard - Update GUI Test for Image process - Create GUI Test for Full process - Create GUI Test for Combined process

--- AxonIvyPortal/portal-selenium-test/src_test/com/axonivy/portal/selenium/page/ProcessEditWidgetNewDashBoardPage.java
+++ AxonIvyPortal/portal-selenium-test/src_test/com/axonivy/portal/selenium/page/ProcessEditWidgetNewDashBoardPage.java
@@ -11,8 +11,8 @@ public class ProcessEditWidgetNewDashBoardPage extends TemplatePage {
   protected String getLoadedLocator() {
     return "//*[contains(@id,'new-widget-configuration-dialog')]";
   }
-  
-  public void selectImageMode(String processName) {
+
+  public void selectImageModeAndSaveWidget(String processName) {
     selectImageMode();
     selectProcess(processName);
     clickSaveProcessWidget();
@@ -22,7 +22,7 @@ public class ProcessEditWidgetNewDashBoardPage extends TemplatePage {
   private void selectProcess(String processName) {
     $("span[id$=':selected-image-process']").find("input").clear();
     $("span[id$=':selected-image-process']").find("input").sendKeys(processName);
-    $$("tr[data-item-label='" + processName + "']").get(0).click();
+    $("tr[data-item-label='" + processName + "']").click();
   }
 
   private void selectImageMode() {
@@ -30,27 +30,147 @@ public class ProcessEditWidgetNewDashBoardPage extends TemplatePage {
     $("li[data-label='Image mode']").click();
     $("span[id$=':selected-image-process']").waitUntil(Condition.appear, DEFAULT_TIMEOUT).shouldBe(getClickableCondition()).click();
   }
-  
-  public void previewImageMode(String processName) {
+
+  public void previewImageModeProcess(String processName) {
     selectImageMode();
     selectProcess(processName);
-    $("button[id$='preview-button']").click();
+    getPreviewButton().click();
   }
-  
+
+  public SelenideElement getImageModeProcessPreview() {
+    return $(".widget-preview .widget-preview--image-width");
+  }
+
+  public SelenideElement getImageModeProcessImage() {
+    return $("img.image-process-item-image");
+  }
+
   public SelenideElement getStartButton() {
     return $("button[id$=':start-button']").waitUntil(Condition.appear, DEFAULT_TIMEOUT);
   }
-  
+
   public SelenideElement getMoreInformationLink() {
     return $("span[id$=':more-information']").waitUntil(Condition.appear, DEFAULT_TIMEOUT);
   }
-  
-  public void changeImageProcess(String newProcessName) {
+
+  public void changeImageModeProcessAndSaveWidget(String newProcessName) {
     selectProcess(newProcessName);
     clickSaveProcessWidget();
   }
 
   private void clickSaveProcessWidget() {
     $("button[id='widget-configuration-save-button']").click();
+  }
+
+  // ========================================================================================
+  public void previewFullModeProcess(String processName) {
+    selectFullMode();
+    selectFullProcess(processName);
+    getPreviewButton().click();
+  }
+
+  private void selectFullMode() {
+    $("div[id$=':process-display-mode']").waitUntil(Condition.appear, DEFAULT_TIMEOUT).shouldBe(getClickableCondition())
+        .click();
+    $("li[data-label='Full mode']").click();
+    $("span[id$=':selected-full-process']").waitUntil(Condition.appear, DEFAULT_TIMEOUT)
+        .shouldBe(getClickableCondition()).click();
+  }
+
+  private void selectFullProcess(String processName) {
+    $("span[id$=':selected-full-process']").find("input").clear();
+    $("span[id$=':selected-full-process']").find("input").sendKeys(processName);
+    $("tr[data-item-label='" + processName + "']").click();
+  }
+
+  private SelenideElement getPreviewButton() {
+    return $("button[id$='preview-button']");
+  }
+
+  public SelenideElement getFullModeProcessPreview() {
+    return $(".widget-preview .widget-preview--full");
+  }
+
+  public SelenideElement getDisabledMoreInformationLink() {
+    return $("span[id$=':more-information']");
+  }
+
+  public void selectFullModeProcessAndSaveWidget(String processName) {
+    selectFullMode();
+    selectFullProcess(processName);
+    clickSaveProcessWidget();
+    $("div[id='new-widget-configuration-dialog']").waitUntil(Condition.disappear, DEFAULT_TIMEOUT);
+  }
+
+  public void changeFullModeProcessAndSaveWidget(String newProcessName) {
+    selectFullProcess(newProcessName);
+    clickSaveProcessWidget();
+    $("div[id='new-widget-configuration-dialog']").waitUntil(Condition.disappear, DEFAULT_TIMEOUT);
+  }
+
+  // ========================================================================================
+  public void previewCombinedModeProcess(String processName) {
+    selectCombinedMode();
+    selectCombinedProcess(processName);
+    getPreviewButton().click();
+  }
+
+  private void selectCombinedMode() {
+    $("div[id$=':process-display-mode']").waitUntil(Condition.appear, DEFAULT_TIMEOUT).shouldBe(getClickableCondition())
+        .click();
+    $("li[data-label='Combined mode']").click();
+    $("span[id$=':selected-combined-process']").waitUntil(Condition.appear, DEFAULT_TIMEOUT)
+        .shouldBe(getClickableCondition()).click();
+  }
+
+  private void selectCombinedProcess(String processName) {
+    $("span[id$=':selected-combined-process']").find("input").clear();
+    $("span[id$=':selected-combined-process']").find("input").sendKeys(processName);
+    $("tr[data-item-label='" + processName + "']").click();
+  }
+
+  public SelenideElement getCombinedModeProcessPreview() {
+    return $(".widget-preview .widget-preview--combined");
+  }
+
+  public SelenideElement getSelectedTasksTab() {
+    return $(".combined-process-widget__button-tabs.first-tab-button.active-button-tab");
+  }
+
+  public SelenideElement getTasksTabDataContainer() {
+    return $("div[id$=':dashboard-process-tasks-container']");
+  }
+
+  public SelenideElement getFirstTaskDisabledStartAction() {
+    return $("div[id$=':dashboard-process-tasks-container'] span.start-task-action");
+  }
+
+  public SelenideElement getCasesTab() {
+    return $(".combined-process-widget__button-tabs.last-tab-button");
+  }
+
+  public SelenideElement getSelectedCasesTab() {
+    return $(".combined-process-widget__button-tabs.last-tab-button.active-button-tab");
+  }
+
+  public SelenideElement getCasesTabDataContainer() {
+    return $("div[id$=':dashboard-process-cases-container']");
+  }
+
+  public SelenideElement getFirstCaseName() {
+    return $("div[id$=':dashboard-process-cases-container'] .dashboard-cases__column-small-screen-name");
+  }
+
+  public void selectCombinedModeProcessAndSaveWidget(String processName) {
+    selectCombinedMode();
+    selectCombinedProcess(processName);
+    clickSaveProcessWidget();
+    $("div[id='new-widget-configuration-dialog']").waitUntil(Condition.disappear, DEFAULT_TIMEOUT);
+  }
+
+  public void changeCombinedModeProcessAndSaveWidget(String newProcessName) {
+    selectCombinedProcess(newProcessName);
+    clickSaveProcessWidget();
+    $("div[id='new-widget-configuration-dialog']").waitUntil(Condition.disappear, DEFAULT_TIMEOUT);
   }
 }
--- AxonIvyPortal/portal-selenium-test/src_test/com/axonivy/portal/selenium/test/NewDashboardProcessWidgetTest.java
+++ AxonIvyPortal/portal-selenium-test/src_test/com/axonivy/portal/selenium/test/NewDashboardProcessWidgetTest.java
@@ -1,8 +1,7 @@
 package com.axonivy.portal.selenium.test;
 
 import static com.codeborne.selenide.Selenide.$;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
+import static com.codeborne.selenide.Selenide.$$;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -14,12 +13,12 @@ import com.axonivy.portal.selenium.page.NewDashboardPage;
 import com.axonivy.portal.selenium.page.ProcessEditWidgetNewDashBoardPage;
 import com.codeborne.selenide.Condition;
 
-@IvyWebTest
+@IvyWebTest(headless = false)
 public class NewDashboardProcessWidgetTest extends BaseTest {
   private NewDashboardPage newDashboardPage;
   private static final String IMAGE_URI = "PROCESSMODELING";
   private static final long DEFAULT_TIMEOUT = 45000;
-  
+
   @Override
   @BeforeEach
   public void setup() {
@@ -28,65 +27,337 @@ public class NewDashboardProcessWidgetTest extends BaseTest {
     login(TestAccount.ADMIN_USER);
     redirectToNewDashBoard();
   }
-  
+
   @Test
   public void testPreviewButtonImageProcess() {
-    ProcessEditWidgetNewDashBoardPage editProcessWidgetConfiguration = newDashboardPage.editProcessWidgetConfiguration();
-    editProcessWidgetConfiguration.previewImageMode("Categoried Leave Request");
-    editProcessWidgetConfiguration.getStartButton().is(Condition.disabled);
-    editProcessWidgetConfiguration.getMoreInformationLink().is(Condition.disabled);
+    ProcessEditWidgetNewDashBoardPage editProcessWidgetConfiguration =
+        newDashboardPage.editProcessWidgetConfiguration();
+    editProcessWidgetConfiguration.previewImageModeProcess("Categoried Leave Request");
+    editProcessWidgetConfiguration.getImageModeProcessPreview().waitUntil(Condition.appear, DEFAULT_TIMEOUT);
+    editProcessWidgetConfiguration.getStartButton().shouldBe(Condition.disabled);
+    editProcessWidgetConfiguration.getDisabledMoreInformationLink().shouldBe(Condition.appear);
+    editProcessWidgetConfiguration.getImageModeProcessImage()
+        .shouldHave(Condition.attributeMatching("src", ".*" + IMAGE_URI + ".*"));
   }
-  
+
   @Test
   public void testChangeImageProcess() {
-    ProcessEditWidgetNewDashBoardPage editProcessWidgetConfiguration = newDashboardPage.editProcessWidgetConfiguration();
-    editProcessWidgetConfiguration.selectImageMode("Categoried Leave Request");
+    ProcessEditWidgetNewDashBoardPage editProcessWidgetConfiguration =
+        newDashboardPage.editProcessWidgetConfiguration();
+    editProcessWidgetConfiguration.selectImageModeAndSaveWidget("Categoried Leave Request");
     checkStartButtonAndImageShown();
+
     editProcessWidgetConfiguration = newDashboardPage.editImageProcess();
     String newProcessName = "Appraisal";
-    editProcessWidgetConfiguration.changeImageProcess(newProcessName);
-    newDashboardPage.getProcessItemName().getText().equals(newProcessName);
+    editProcessWidgetConfiguration.changeImageModeProcessAndSaveWidget(newProcessName);
+    newDashboardPage.getProcessItemName().shouldHave(Condition.exactTextCaseSensitive(newProcessName));
   }
 
   private void checkStartButtonAndImageShown() {
-    newDashboardPage.getStartProcessButton().isDisplayed();
-    newDashboardPage.getProcessImage().attr("src").contains(IMAGE_URI);
+    newDashboardPage.getStartButton().shouldBe(Condition.disabled);
+    newDashboardPage.getDisabledMoreInformationLink().shouldBe(Condition.appear);
+    newDashboardPage.getProcessImage().shouldHave(Condition.attributeMatching("src", ".*" + IMAGE_URI + ".*"));
   }
-  
+
   @Test
   public void testDeleteImageProcess() {
-    ProcessEditWidgetNewDashBoardPage editProcessWidgetConfiguration = newDashboardPage.editProcessWidgetConfiguration();
-    editProcessWidgetConfiguration.selectImageMode("Categoried Leave Request");
+    ProcessEditWidgetNewDashBoardPage editProcessWidgetConfiguration =
+        newDashboardPage.editProcessWidgetConfiguration();
+    editProcessWidgetConfiguration.selectImageModeAndSaveWidget("Categoried Leave Request");
     checkStartButtonAndImageShown();
-    newDashboardPage.deleteImageProcess();
-    assertFalse(newDashboardPage.getImageContainer().isDisplayed());
+
+    newDashboardPage.deleteImageModeProcess();
+    newDashboardPage.getImageContainer().shouldNotBe(Condition.exist);
   }
 
   @Test
   public void testStartImageProcess() {
-    ProcessEditWidgetNewDashBoardPage editProcessWidgetConfiguration = newDashboardPage.editProcessWidgetConfiguration();
-    editProcessWidgetConfiguration.selectImageMode("Showcase Data Table");
-    checkStartButtonAndImageShown();
+    ProcessEditWidgetNewDashBoardPage editProcessWidgetConfiguration =
+        newDashboardPage.editProcessWidgetConfiguration();
+    editProcessWidgetConfiguration.selectImageModeAndSaveWidget("Showcase Data Table");
+    newDashboardPage.getStartButton().shouldBe(Condition.disabled);
+
     newDashboardPage.switchToViewMode();
-    newDashboardPage.getStartProcessButton();
+    newDashboardPage.getStartButton().shouldBe(Condition.enabled);
     newDashboardPage.startProcess();
-    
+
     // make sure process is started
-    $("span[id='title']").waitUntil(Condition.appear, DEFAULT_TIMEOUT).isDisplayed();
-    assertEquals($("span[id='title']").getAttribute("title"), "Start DataTable Showcase"); 
+    $("span[id='title']").waitUntil(Condition.appear, DEFAULT_TIMEOUT)
+        .shouldHave(Condition.attribute("title", "Start DataTable Showcase"));
   }
-  
+
   @Test
   public void testMoreInfoLinkImageProcess() {
-    ProcessEditWidgetNewDashBoardPage editProcessWidgetConfiguration = newDashboardPage.editProcessWidgetConfiguration();
-    editProcessWidgetConfiguration.selectImageMode("Showcase Data Table");
-    checkStartButtonAndImageShown();
+    ProcessEditWidgetNewDashBoardPage editProcessWidgetConfiguration =
+        newDashboardPage.editProcessWidgetConfiguration();
+    editProcessWidgetConfiguration.selectImageModeAndSaveWidget("Showcase Data Table");
+    newDashboardPage.getDisabledMoreInformationLink().shouldBe(Condition.appear);
+
     newDashboardPage.switchToViewMode();
-    newDashboardPage.getStartProcessButton();
+    newDashboardPage.getMoreInformationLink().shouldBe(Condition.appear);
     newDashboardPage.startMoreInfoLink();
-    
+
     // make sure back and start button is shown
-    $("a[id='back-link']").waitUntil(Condition.appear, DEFAULT_TIMEOUT).isDisplayed();
-    $("button[id='start-process-button']").waitUntil(Condition.appear, DEFAULT_TIMEOUT).isDisplayed();
+    $("a[id='back-link']").waitUntil(Condition.appear, DEFAULT_TIMEOUT);
+    $("button[id='start-process-button']").waitUntil(Condition.appear, DEFAULT_TIMEOUT).shouldBe(Condition.enabled);
+  }
+
+  // ===================================================================================
+  @Test
+  public void testPreviewFullModeProcess() {
+    ProcessEditWidgetNewDashBoardPage editProcessWidgetConfiguration =
+        newDashboardPage.editProcessWidgetConfiguration();
+    editProcessWidgetConfiguration.previewFullModeProcess("Categoried Leave Request");
+    editProcessWidgetConfiguration.getFullModeProcessPreview().waitUntil(Condition.appear, DEFAULT_TIMEOUT);
+    editProcessWidgetConfiguration.getStartButton().shouldBe(Condition.disabled);
+    editProcessWidgetConfiguration.getDisabledMoreInformationLink().shouldBe(Condition.appear);
+  }
+
+  @Test
+  public void testChangeFullModeProcess() {
+    ProcessEditWidgetNewDashBoardPage editProcessWidgetConfiguration =
+        newDashboardPage.editProcessWidgetConfiguration();
+    editProcessWidgetConfiguration.selectFullModeProcessAndSaveWidget("Categoried Leave Request");
+    newDashboardPage.getStartButton().shouldBe(Condition.disabled);
+    newDashboardPage.getDisabledMoreInformationLink().shouldBe(Condition.appear);
+
+    editProcessWidgetConfiguration = newDashboardPage.editFullModeProcess();
+    String newProcessName = "Appraisal";
+    editProcessWidgetConfiguration.changeFullModeProcessAndSaveWidget(newProcessName);
+    newDashboardPage.getFullModeProcessName().shouldHave(Condition.exactTextCaseSensitive(newProcessName));
+  }
+
+  @Test
+  public void testDeleteFullModeProcess() {
+    ProcessEditWidgetNewDashBoardPage editProcessWidgetConfiguration =
+        newDashboardPage.editProcessWidgetConfiguration();
+    editProcessWidgetConfiguration.selectFullModeProcessAndSaveWidget("Categoried Leave Request");
+    newDashboardPage.getStartButton().shouldBe(Condition.disabled);
+    newDashboardPage.getDisabledMoreInformationLink().shouldBe(Condition.appear);
+
+    newDashboardPage.deleteFullModeProcess();
+    newDashboardPage.getFullModeProcessContainer().shouldNotBe(Condition.exist);
+  }
+
+  @Test
+  public void testStartFullModeProcess() {
+    ProcessEditWidgetNewDashBoardPage editProcessWidgetConfiguration =
+        newDashboardPage.editProcessWidgetConfiguration();
+    editProcessWidgetConfiguration.selectFullModeProcessAndSaveWidget("Showcase Data Table");
+    newDashboardPage.getStartButton().shouldBe(Condition.disabled);
+
+    newDashboardPage.switchToViewMode();
+    newDashboardPage.getStartButton().shouldBe(Condition.enabled);
+    newDashboardPage.startProcess();
+    $("span[id='title']").waitUntil(Condition.appear, DEFAULT_TIMEOUT)
+        .shouldHave(Condition.attribute("title", "Start DataTable Showcase"));
+  }
+
+  @Test
+  public void testMoreInfoLinkFullModeProcess() {
+    ProcessEditWidgetNewDashBoardPage editProcessWidgetConfiguration =
+        newDashboardPage.editProcessWidgetConfiguration();
+    editProcessWidgetConfiguration.selectFullModeProcessAndSaveWidget("Showcase Data Table");
+    newDashboardPage.getDisabledMoreInformationLink().shouldBe(Condition.appear);
+
+    newDashboardPage.switchToViewMode();
+    newDashboardPage.getMoreInformationLink().shouldBe(Condition.appear);
+    newDashboardPage.startMoreInfoLink();
+
+    $("a[id='back-link']").waitUntil(Condition.appear, DEFAULT_TIMEOUT);
+    $("button[id='start-process-button']").waitUntil(Condition.appear, DEFAULT_TIMEOUT).shouldBe(Condition.enabled);
+  }
+
+  // ===================================================================================
+  @Test
+  public void testPreviewCombinedModeProcess() {
+    createCategoriedLeaveRequestTask();
+
+    ProcessEditWidgetNewDashBoardPage editProcessWidgetConfiguration =
+        newDashboardPage.editProcessWidgetConfiguration();
+    editProcessWidgetConfiguration.previewCombinedModeProcess("Categoried Leave Request");
+    editProcessWidgetConfiguration.getCombinedModeProcessPreview().waitUntil(Condition.appear, DEFAULT_TIMEOUT);
+    editProcessWidgetConfiguration.getStartButton().is(Condition.disabled);
+
+    editProcessWidgetConfiguration.getSelectedTasksTab().shouldBe(Condition.appear);
+    editProcessWidgetConfiguration.getSelectedCasesTab().shouldBe(Condition.disappear);
+    editProcessWidgetConfiguration.getFirstTaskDisabledStartAction().waitUntil(Condition.appear, DEFAULT_TIMEOUT)
+      .shouldHave(Condition.cssValue("pointer-events", "none"));
+
+    editProcessWidgetConfiguration.getCasesTab().shouldBe(Condition.appear).click();
+    editProcessWidgetConfiguration.getFirstCaseName().waitUntil(Condition.exist, DEFAULT_TIMEOUT)
+        .shouldHave(Condition.attribute("title", "Leave Request Test For IVYPORTAL-3369"));
+    editProcessWidgetConfiguration.getSelectedTasksTab().shouldBe(Condition.disappear);
+    editProcessWidgetConfiguration.getSelectedCasesTab().shouldBe(Condition.appear);
+  }
+
+  private void createCategoriedLeaveRequestTask() {
+    createRelatedTaskWithNoResponsible();
+    newDashboardPage.waitForAbsencesGrowlMessageDisplay();
+  }
+
+  @Test
+  public void testChangeCombinedModeProcess() {
+    ProcessEditWidgetNewDashBoardPage editProcessWidgetConfiguration =
+        newDashboardPage.editProcessWidgetConfiguration();
+    editProcessWidgetConfiguration.selectCombinedModeProcessAndSaveWidget("Categoried Leave Request");
+    newDashboardPage.getStartButton().shouldBe(Condition.disabled);
+
+    editProcessWidgetConfiguration = newDashboardPage.editCombinedModeProcess();
+    String newProcessName = "Appraisal";
+    editProcessWidgetConfiguration.changeCombinedModeProcessAndSaveWidget(newProcessName);
+    newDashboardPage.getCombinedModeProcessName().shouldHave(Condition.exactTextCaseSensitive(newProcessName));
+  }
+
+  @Test
+  public void testDeleteCombinedModeProcess() {
+    ProcessEditWidgetNewDashBoardPage editProcessWidgetConfiguration =
+        newDashboardPage.editProcessWidgetConfiguration();
+    editProcessWidgetConfiguration.selectCombinedModeProcessAndSaveWidget("Categoried Leave Request");
+    newDashboardPage.getStartButton().shouldBe(Condition.disabled);
+
+    newDashboardPage.deleteCombinedModeProcess();
+    newDashboardPage.getCombinedModeProcessContainer().shouldNotBe(Condition.exist);
+  }
+
+  @Test
+  public void testStartCombinedModeProcess() {
+    ProcessEditWidgetNewDashBoardPage editProcessWidgetConfiguration =
+        newDashboardPage.editProcessWidgetConfiguration();
+    editProcessWidgetConfiguration.selectCombinedModeProcessAndSaveWidget("Showcase Data Table");
+    newDashboardPage.getStartButton().shouldBe(Condition.disabled);
+
+    newDashboardPage.switchToViewMode();
+    newDashboardPage.getStartButton().shouldBe(Condition.enabled);
+    newDashboardPage.startProcess();
+
+    $("span[id='title']").waitUntil(Condition.appear, DEFAULT_TIMEOUT)
+        .shouldHave(Condition.attribute("title", "Start DataTable Showcase"));
+  }
+
+  @Test
+  public void testStartCombinedModeProcessWhenExpanded() {
+    ProcessEditWidgetNewDashBoardPage editProcessWidgetConfiguration =
+        newDashboardPage.editProcessWidgetConfiguration();
+    editProcessWidgetConfiguration.selectCombinedModeProcessAndSaveWidget("Showcase Data Table");
+    newDashboardPage.getStartButton().shouldBe(Condition.disabled);
+
+    newDashboardPage.switchToViewMode();
+    newDashboardPage.expandCombindedModeProcess();
+    newDashboardPage.getStartButton().shouldBe(Condition.enabled);
+    newDashboardPage.startProcess();
+
+    $("span[id='title']").waitUntil(Condition.appear, DEFAULT_TIMEOUT)
+        .shouldHave(Condition.attribute("title", "Start DataTable Showcase"));
+  }
+
+  @Test
+  public void testStartCombinedModeProcessTask() {
+    createCategoriedLeaveRequestTask();
+
+    ProcessEditWidgetNewDashBoardPage editProcessWidgetConfiguration =
+        newDashboardPage.editProcessWidgetConfiguration();
+    editProcessWidgetConfiguration.selectCombinedModeProcessAndSaveWidget("Categoried Leave Request");
+
+    newDashboardPage.switchToViewMode();
+    newDashboardPage.getCombinedModeProcessFirstTaskStartAction().shouldBe(Condition.appear).click();
+
+    $("span[id='title']").waitUntil(Condition.appear, DEFAULT_TIMEOUT)
+        .shouldHave(Condition.attribute("title", "Sick Leave Request"));
+  }
+
+  @Test
+  public void testStartCombinedModeProcessTaskWhenExpanded() {
+    createCategoriedLeaveRequestTask();
+
+    ProcessEditWidgetNewDashBoardPage editProcessWidgetConfiguration =
+        newDashboardPage.editProcessWidgetConfiguration();
+    editProcessWidgetConfiguration.selectCombinedModeProcessAndSaveWidget("Categoried Leave Request");
+
+    newDashboardPage.switchToViewMode();
+    newDashboardPage.expandCombindedModeProcess();
+    newDashboardPage.getCombinedModeProcessFirstTaskStartAction().shouldBe(Condition.appear).click();
+
+    $("span[id='title']").waitUntil(Condition.appear, DEFAULT_TIMEOUT)
+        .shouldHave(Condition.attribute("title", "Sick Leave Request"));
+  }
+
+  @Test
+  public void testOpenCombinedModeProcessTask() {
+    createCategoriedLeaveRequestTask();
+
+    ProcessEditWidgetNewDashBoardPage editProcessWidgetConfiguration =
+        newDashboardPage.editProcessWidgetConfiguration();
+    editProcessWidgetConfiguration.selectCombinedModeProcessAndSaveWidget("Categoried Leave Request");
+
+    newDashboardPage.switchToViewMode();
+    newDashboardPage.getCombinedModeProcessFirstTaskName().shouldBe(Condition.appear).click();
+
+    $$("span.ui-menuitem-text").last().waitUntil(Condition.appear, DEFAULT_TIMEOUT)
+        .shouldHave(Condition.exactTextCaseSensitive("Task: Sick Leave Request"));
+  }
+
+  @Test
+  public void testOpenCombinedModeProcessTaskWhenExpanded() {
+    createCategoriedLeaveRequestTask();
+
+    ProcessEditWidgetNewDashBoardPage editProcessWidgetConfiguration =
+        newDashboardPage.editProcessWidgetConfiguration();
+    editProcessWidgetConfiguration.selectCombinedModeProcessAndSaveWidget("Categoried Leave Request");
+
+    newDashboardPage.switchToViewMode();
+    newDashboardPage.expandCombindedModeProcess();
+    newDashboardPage.getCombinedModeProcessFirstTaskName().shouldBe(Condition.appear).click();
+
+    $$("span.ui-menuitem-text").last().waitUntil(Condition.appear, DEFAULT_TIMEOUT)
+        .shouldHave(Condition.exactTextCaseSensitive("Task: Sick Leave Request"));
+  }
+
+  @Test
+  public void testOpenCombinedModeProcessCase() {
+    createCategoriedLeaveRequestTask();
+
+    ProcessEditWidgetNewDashBoardPage editProcessWidgetConfiguration =
+        newDashboardPage.editProcessWidgetConfiguration();
+    editProcessWidgetConfiguration.selectCombinedModeProcessAndSaveWidget("Categoried Leave Request");
+
+    newDashboardPage.switchToViewMode();
+    newDashboardPage.getCasesTab().shouldBe(Condition.appear).click();
+    newDashboardPage.getCombinedModeProcessFirstCaseName().waitUntil(Condition.appear, DEFAULT_TIMEOUT).click();
+
+    $$("span.ui-menuitem-text").last().waitUntil(Condition.appear, DEFAULT_TIMEOUT)
+        .shouldHave(Condition.exactTextCaseSensitive("Case: Leave Request Test For IVYPORTAL-3369"));
+  }
+
+  @Test
+  public void testOpenCombinedModeProcessCaseWhenExpanded() {
+    createCategoriedLeaveRequestTask();
+
+    ProcessEditWidgetNewDashBoardPage editProcessWidgetConfiguration =
+        newDashboardPage.editProcessWidgetConfiguration();
+    editProcessWidgetConfiguration.selectCombinedModeProcessAndSaveWidget("Categoried Leave Request");
+
+    newDashboardPage.switchToViewMode();
+    newDashboardPage.expandCombindedModeProcess();
+    newDashboardPage.getCasesTab().shouldBe(Condition.appear).click();
+    newDashboardPage.getCombinedModeProcessFirstCaseName().waitUntil(Condition.appear, DEFAULT_TIMEOUT).click();
+
+    $$("span.ui-menuitem-text").last().waitUntil(Condition.appear, DEFAULT_TIMEOUT)
+        .shouldHave(Condition.exactTextCaseSensitive("Case: Leave Request Test For IVYPORTAL-3369"));
+  }
+
+  @Test
+  public void testExpandCollapseCombinedModeProcess() {
+    createCategoriedLeaveRequestTask();
+
+    ProcessEditWidgetNewDashBoardPage editProcessWidgetConfiguration =
+        newDashboardPage.editProcessWidgetConfiguration();
+    editProcessWidgetConfiguration.selectCombinedModeProcessAndSaveWidget("Categoried Leave Request");
+
+    newDashboardPage.switchToViewMode();
+    newDashboardPage.expandCombindedModeProcess();
+    newDashboardPage.getCombinedModeProcessCollapseLink().click();
+    newDashboardPage.getCombinedModeProcessExpandLink().waitUntil(Condition.appear, DEFAULT_TIMEOUT);
+    newDashboardPage.getCombinedModeProcessCollapseLink().shouldBe(Condition.disappear);
   }
 }
