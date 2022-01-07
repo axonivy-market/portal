3f6d0e8805b875111a4fed519610c797997151b9
Nguyen Quoc Hoan <hoan.nguyen01@axonactive.com>
2022-01-07 17:45:27 +0700

    IVYPORTAL-13105: Cannot add session user as a deputy for other user - fixed issue - added gui test

--- AxonIvyPortal/PortalTest/src_test/portal/guitest/page/AddAbsencePage.java
+++ AxonIvyPortal/PortalTest/src_test/portal/guitest/page/AddAbsencePage.java
@@ -1,20 +1,15 @@
 package portal.guitest.page;
 
-public class AddAbsencePage extends TemplatePage {
+public class AddAbsencePage extends SettingDeputyPage {
 
   @Override
   protected String getLoadedLocator() {
     return "id('absence-settings:add-new-absence-dialog_title')";
   }
   
+  @Override
   public void proceedWhenCreatingAbsence() {
     String selector = "button[id*='save-substitutes']";
     proceed(selector);
   }
-
-  private void proceed(String selector) {
-    clickByCssSelector(selector);
-    waitAjaxIndicatorDisappear();
-  }
-
 }
