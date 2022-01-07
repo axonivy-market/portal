ba257928e05b93c26a051eb41a7ea6ee5ab30051
nam.mai <nam.mai@axonactive.com>
2022-01-07 08:57:38 +0700

    IVYPORTAL-12849: Improve absence management - Fixed bug: User can set deputy for other users without UserCreateSubstitute permission - Fixed bug: User can see deputies of other users without UserReadSubstitute permission

--- AxonIvyPortal/PortalTemplate/src/ch/ivy/addon/portal/generic/bean/AbsenceManagementBean.java
+++ AxonIvyPortal/PortalTemplate/src/ch/ivy/addon/portal/generic/bean/AbsenceManagementBean.java
@@ -8,6 +8,8 @@ import static ch.ivyteam.ivy.security.IPermission.USER_DELETE_ABSENCE;
 import static ch.ivyteam.ivy.security.IPermission.USER_DELETE_OWN_ABSENCE;
 import static ch.ivyteam.ivy.security.IPermission.USER_READ_ABSENCES;
 import static ch.ivyteam.ivy.security.IPermission.USER_READ_OWN_ABSENCES;
+import static ch.ivyteam.ivy.security.IPermission.USER_READ_SUBSTITUTES;
+import static ch.ivyteam.ivy.security.IPermission.USER_READ_OWN_SUBSTITUTES;
 
 import java.io.Serializable;
 
@@ -15,6 +17,7 @@ import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 
+import ch.ivy.addon.portalkit.dto.UserDTO;
 import ch.ivy.addon.portalkit.ivydata.bo.IvyAbsence;
 import ch.ivy.addon.portalkit.util.AbsenceAndSubstituteUtils;
 import ch.ivy.addon.portalkit.util.PermissionUtils;
@@ -34,6 +37,12 @@ public class AbsenceManagementBean implements Serializable{
   private boolean absencesInThePastDeletable;
   private boolean substitutionCapable;
 
+  private boolean ownSubstitutionCreatable;
+  private boolean substitutionCreatable;
+  private boolean ownSubstitutionReadable;
+  private boolean substitutionReadable;
+  
+
   @PostConstruct
   public void init() {
     ownAbsencesReadable = PermissionUtils.hasAtLeastOnePermission(USER_READ_OWN_ABSENCES, USER_READ_ABSENCES);
@@ -44,6 +53,11 @@ public class AbsenceManagementBean implements Serializable{
     absencesInThePastDeletable = PermissionUtils.hasPermission(USER_DELETE_ABSENCE);
 
     substitutionCapable = PermissionUtils.hasAtLeastOnePermission(USER_CREATE_OWN_SUBSTITUTE, USER_CREATE_SUBSTITUTE);
+    ownSubstitutionCreatable = PermissionUtils.hasPermission(USER_CREATE_OWN_SUBSTITUTE);
+    substitutionCreatable = PermissionUtils.hasPermission(USER_CREATE_SUBSTITUTE);
+
+    substitutionReadable = PermissionUtils.hasPermission(USER_READ_SUBSTITUTES);
+    ownSubstitutionReadable = PermissionUtils.hasPermission(USER_READ_OWN_SUBSTITUTES);
   }
 
   public boolean isOwnAbsencesReadable() {
@@ -67,6 +81,17 @@ public class AbsenceManagementBean implements Serializable{
     boolean absenceInThePast = AbsenceAndSubstituteUtils.isInThePast(absence);
     return absenceInThePast ? absencesInThePastDeletable : (ownAbsencesDeletable && isOwnAbsence(absence));
   }
+
+  public boolean canCreateSubstitute(UserDTO selectedUser) {
+    boolean isLoginUser = selectedUser.getName().contentEquals(Ivy.session().getSessionUserName());
+    return substitutionCreatable || (ownSubstitutionCreatable && isLoginUser);
+  }
+
+  public boolean canReadSubstitute(UserDTO selectedUser) {
+    boolean isLoginUser = selectedUser.getName().contentEquals(Ivy.session().getSessionUserName());
+    return substitutionReadable || (ownSubstitutionReadable && isLoginUser);
+  }
+
   private boolean isOwnAbsence (IvyAbsence absence) {
     return absence.getUsername().contentEquals(Ivy.session().getSessionUserName());
   }
