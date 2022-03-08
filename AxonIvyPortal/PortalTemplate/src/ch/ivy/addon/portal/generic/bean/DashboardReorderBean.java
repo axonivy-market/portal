75f0f797bc83fda27279fcec975492d9ed1c12eb
Tung Le <tung.le@axonactive.com>
2022-03-08 08:57:23 +0000

    Merged in feature/IVYPORTAL-13071-Reorder-dashboards (pull request #556)
    IVYPORTAL-13071 Reorder dashboards
    
    * IVYPORTAL-13071 Reorder dashboards
    - Removed reordering in mange dashboard
    
    * Merged master into feature/IVYPORTAL-13071-Reorder-dashboards
    * IVYPORTAL-13071 Reorder dashboards
    - Click Save then navigate back to home page
    - Remove the growl when cancelling
    - Apply also for My Profile
    
    * IVYPORTAL-13071 Reorder dashboards
    - Click Save then navigate back to home page
    - Remove the growl when cancelling
    - Apply also for My Profile
    
    * IVYPORTAL-13071 Reorder dashboards
    - Clean up
    
    * IVYPORTAL-13071 Reorder dashboards
    - Merged with master (Edit wizard)
    
    * IVYPORTAL-13071 Reorder dashboards
    - Clean up
    
    * IVYPORTAL-13071 Reorder dashboards
    - Clean up
    
    * IVYPORTAL-13071: Reorder dashboards
    - Merge from master
    - Implement warning dialog when working in a task
    
    * Merged master into feature/IVYPORTAL-13071-Reorder-dashboards

---
+++ AxonIvyPortal/PortalTemplate/src/ch/ivy/addon/portal/generic/bean/DashboardReorderBean.java
@@ -0,0 +1,66 @@
+package ch.ivy.addon.portal.generic.bean;
+
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+import ch.ivy.addon.portal.generic.navigation.PortalNavigator;
+import ch.ivy.addon.portalkit.dto.dashboard.Dashboard;
+import ch.ivy.addon.portalkit.dto.dashboard.DashboardOrder;
+import ch.ivyteam.ivy.environment.Ivy;
+import ch.ivyteam.ivy.security.IUser;
+
+public abstract class DashboardReorderBean implements Serializable {
+
+  private static final long serialVersionUID = 1L;
+  private List<DashboardOrder> dashboardOrders;
+
+  public void initConfigration() {
+    dashboardOrders = createDashboardOrders();
+  }
+
+  public List<DashboardOrder> getDashboardOrders() {
+    return dashboardOrders;
+  }
+
+  public void setDashboardOrders(List<DashboardOrder> dashboardOrders) {
+    this.dashboardOrders = dashboardOrders;
+  }
+
+  public void save() {
+    saveArrangement();
+    backToHome();
+  }
+
+  public abstract void saveArrangement();
+
+  protected abstract List<DashboardOrder> createDashboardOrders();
+
+  protected List<DashboardOrder> convertDashboardsToDashboardOrders(Collection<Dashboard> dashboards) {
+    var orders = dashboards.stream().filter(Objects::nonNull).map(dashboard -> {
+      DashboardOrder dashboardOrder = new DashboardOrder();
+      dashboardOrder.setDashboardId(dashboard.getId());
+      dashboardOrder.setIsPublicDashboard(dashboard.getIsPublic());
+      dashboardOrder.setDashboardTitle(dashboard.getTitle());
+      dashboardOrder.setVisible(true);
+      return dashboardOrder;
+    }).collect(Collectors.toList());
+    return orders;
+  }
+
+  protected IUser currentUser() {
+    return Ivy.session().getSessionUser();
+  }
+
+  public void handleActivation(DashboardOrder dashboardOrder) {
+    dashboardOrder.setVisible(!dashboardOrder.isVisible());
+  }
+
+  public void backToHome() {
+    PortalNavigator.navigateToDashboard();
+  }
+
+}
+
