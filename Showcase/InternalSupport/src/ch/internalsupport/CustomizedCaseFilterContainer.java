a782bd7dfd71d909541c37b5aed78746b2b6e46d
Nguyen Quoc Hoan <hoan.nguyen01@axonactive.com>
2022-03-14 17:22:16 +0700

    IVYPORTAL-13250-Improve-filters-in-full-task-and-case-list-LTS - update example for customizing

--- Showcase/InternalSupport/src/ch/internalsupport/CustomizedCaseFilterContainer.java
+++ Showcase/InternalSupport/src/ch/internalsupport/CustomizedCaseFilterContainer.java
@@ -4,10 +4,14 @@ import java.util.Collections;
 
 import ch.ivy.addon.portalkit.casefilter.CaseFilterContainer;
 import ch.ivy.addon.portalkit.casefilter.impl.CaseDescriptionFilter;
+import ch.ivy.addon.portalkit.casefilter.impl.CaseIdFilter;
+import ch.ivy.addon.portalkit.casefilter.impl.CaseNameFilter;
 import ch.ivy.addon.portalkit.comparator.CaseFilterComparator;
 
 public class CustomizedCaseFilterContainer extends CaseFilterContainer{
 
+  protected CaseIdFilter idFilter = new CaseIdFilter();
+  protected CaseNameFilter nameFilter = new CaseNameFilter();
   private CaseDescriptionFilter caseDescriptionFilter = new CaseDescriptionFilter();
   
   public CustomizedCaseFilterContainer() {
@@ -22,6 +26,22 @@ public class CustomizedCaseFilterContainer extends CaseFilterContainer{
 
   public void setCaseDescriptionFilter(CaseDescriptionFilter caseDescriptionFilter) {
     this.caseDescriptionFilter = caseDescriptionFilter;
+  }
+
+  public CaseIdFilter getIdFilter() {
+    return idFilter;
+  }
+
+  public void setIdFilter(CaseIdFilter idFilter) {
+    this.idFilter = idFilter;
+  }
+
+  public CaseNameFilter getNameFilter() {
+    return nameFilter;
+  }
+
+  public void setNameFilter(CaseNameFilter nameFilter) {
+    this.nameFilter = nameFilter;
   }
 
 }
