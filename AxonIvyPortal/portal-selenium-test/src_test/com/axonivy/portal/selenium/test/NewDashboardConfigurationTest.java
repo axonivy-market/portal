a567b057bb7b00681db10994caef6d1ea7a4c64c
nam.mai <nam.mai@axonactive.com>
2022-03-14 15:41:01 +0700

    IVYPORTAL-13180: Edit Private / public dashboards - Implemented

--- AxonIvyPortal/portal-selenium-test/src_test/com/axonivy/portal/selenium/test/NewDashboardConfigurationTest.java
+++ AxonIvyPortal/portal-selenium-test/src_test/com/axonivy/portal/selenium/test/NewDashboardConfigurationTest.java
@@ -1,15 +1,20 @@
 package com.axonivy.portal.selenium.test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import com.axonivy.ivy.webtest.IvyWebTest;
 import com.axonivy.portal.selenium.common.BaseTest;
 import com.axonivy.portal.selenium.common.TestAccount;
+import com.axonivy.portal.selenium.page.NewDashboardConfigurationPage;
 import com.axonivy.portal.selenium.page.NewDashboardPage;
 import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
 
-@IvyWebTest
+@IvyWebTest(headless = false)
 public class NewDashboardConfigurationTest extends BaseTest {
 
   private NewDashboardPage newDashboardPage;
@@ -49,5 +54,33 @@ public class NewDashboardConfigurationTest extends BaseTest {
     redirectToRelativeLink("PortalKitTestHelper/14DE09882B540AD5/denyDashboardWritePublicPermission.ivp");
     newDashboardPage = new NewDashboardPage();
     newDashboardPage.getConfigureDashboardMenu().shouldBe(Condition.disappear);
+  }
+
+  @Test
+  public void testEditPublicDashboardInfo() {
+    login(TestAccount.ADMIN_USER);
+    redirectToNewDashBoard();
+    newDashboardPage.openDashboardConfigurationDialog();
+    NewDashboardConfigurationPage configPage = newDashboardPage.navigateToEditPublicDashboardPage();
+    configPage.clickEditDashboardByName("Dashboard");
+
+    List<String> permissions = new ArrayList<>();
+    permissions.add("Cost Object (CostObject)");
+    configPage.editDashboardInfo("Editted Dashboard", "dashboard description", permissions);
+
+    SelenideElement dashboard = configPage.getDashboardRowByName("Editted Dashboard");
+    dashboard.shouldBe(Condition.appear);
+    dashboard.$("td:nth-child(1)").shouldHave(Condition.exactText("Editted Dashboard"));
+    dashboard.$("td:nth-child(2)").shouldHave(Condition.exactText("dashboard description"));
+  }
+
+  @Test
+  public void testDeletePublicDashboard() {
+    login(TestAccount.ADMIN_USER);
+    redirectToNewDashBoard();
+    newDashboardPage.openDashboardConfigurationDialog();
+    NewDashboardConfigurationPage configPage = newDashboardPage.navigateToEditPublicDashboardPage();
+    configPage.clickDeleteDashboardByName("Dashboard");
+    configPage.getDashboardRows().shouldHaveSize(0);
   }
 }
