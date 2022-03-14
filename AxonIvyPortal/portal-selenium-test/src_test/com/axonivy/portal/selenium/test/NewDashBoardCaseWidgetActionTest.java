f281ebe5184258531543bde43df7ab278373dadc
Loi Nguyen <loi.nguyenkimvan@axonactive.com>
2022-03-14 08:50:40 +0700

    IVYPORTAL-13197 Add one default column to the task and case widget “Actions” - GUI tests
    Former-commit-id: e4cb7e02a312f8822141af169e4900f8d01fabf6

---
+++ AxonIvyPortal/portal-selenium-test/src_test/com/axonivy/portal/selenium/test/NewDashBoardCaseWidgetActionTest.java
@@ -0,0 +1,73 @@
+package com.axonivy.portal.selenium.test;
+
+import static com.codeborne.selenide.CollectionCondition.sizeGreaterThanOrEqual;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import com.axonivy.ivy.webtest.IvyWebTest;
+import com.axonivy.portal.selenium.common.BaseTest;
+import com.axonivy.portal.selenium.common.TestAccount;
+import com.axonivy.portal.selenium.page.CaseWidgetNewDashBoardPage;
+import com.axonivy.portal.selenium.page.NewDashboardPage;
+import com.codeborne.selenide.ElementsCollection;
+
+@IvyWebTest
+public class NewDashBoardCaseWidgetActionTest extends BaseTest {
+
+  // WIDGET NAME
+  private static final String YOUR_CASES_WIDGET = "Your Cases";
+
+  private NewDashboardPage newDashboardPage;
+  private CaseWidgetNewDashBoardPage caseWidget;
+
+  @Override
+  @BeforeEach
+  public void setup() {
+    super.setup();
+    redirectToRelativeLink(createTestingTasksUrl);
+    newDashboardPage = new NewDashboardPage();
+  }
+
+  @Test
+  public void testVisibilityCaseActionForNormalUser() {
+    login(TestAccount.DEMO_USER);
+    redirectToNewDashBoard();
+    caseWidget = newDashboardPage.selectCaseWidget(YOUR_CASES_WIDGET);
+    // In progress
+    assertTaskActionsByCaseState("In progress", new ArrayList<>());
+  }
+
+  @Test
+  public void testVisibilityCaseActionForAdminUser() {
+    login(TestAccount.ADMIN_USER);
+    redirectToRelativeLink(create12CasesWithCategoryUrl);
+    redirectToNewDashBoard();
+    caseWidget = newDashboardPage.selectCaseWidget(YOUR_CASES_WIDGET);
+    // In progress
+    assertTaskActionsByCaseState("In progress", Arrays.asList("Business details", "Destroy"));
+    // Destroyed
+    caseWidget.destroyCase(0);
+    assertTaskActionsByCaseState("Destroyed", Arrays.asList("Business details"));
+    // Done
+    assertTaskActionsByCaseState("Done", Arrays.asList("Business details"));
+  }
+
+  private void assertTaskActionsByCaseState(String state, List<String> taskActionsInCase) {
+    caseWidget.expand().shouldHave(sizeGreaterThanOrEqual(1));
+    caseWidget.openFilterWidget();
+    caseWidget.resetFilter();
+    caseWidget.openFilterWidget();
+    caseWidget.filterCaseState();
+    caseWidget.selectState(state);
+    caseWidget.applyFilter();
+    ElementsCollection actions = caseWidget.getActiveCaseActions(0);
+    actions.shouldHaveSize(taskActionsInCase.size());
+    assertTrue(actions.texts().containsAll(taskActionsInCase));
+  }
+}
