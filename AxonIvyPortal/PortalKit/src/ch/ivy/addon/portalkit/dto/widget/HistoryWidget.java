4fa87014ef15caf9424dc2535bc9cba48a403d44
Nguyen Quoc Hoan <hoan.nguyen01@axonactive.com>
2022-03-31 15:20:06 +0700

    IVYPORTAL-13198: Optimize Task Details page size - implemented

--- AxonIvyPortal/PortalKit/src/ch/ivy/addon/portalkit/dto/widget/HistoryWidget.java
+++ AxonIvyPortal/PortalKit/src/ch/ivy/addon/portalkit/dto/widget/HistoryWidget.java
@@ -3,6 +3,19 @@ package ch.ivy.addon.portalkit.dto.widget;
 public class HistoryWidget extends AbstractWidget {
 
   private static final long serialVersionUID = -4972389298376434497L;
+  private static final int DEFAULT_ROW_PER_PAGE = 5;
+  private int rowPerPage;
 
   public HistoryWidget() {}
+
+  public int getRowPerPage() {
+    if (rowPerPage == 0) {
+      rowPerPage = DEFAULT_ROW_PER_PAGE;
+    }
+    return rowPerPage;
+  }
+
+  public void setRowPerPage(int rowPerPage) {
+    this.rowPerPage = rowPerPage;
+  }
 }
