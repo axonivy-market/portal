5ce98026dbe2f92a2217d15808ba1af79d8821c4
htthanh01 <thanh.huynhthien@axonactive.com>
2022-04-29 17:43:16 +0700

    IVYPORTAL-12638: GUI tests for task widget of new Dashboard - Test case testDestroyTaskWithPermission
    Former-commit-id: 6d3a61cab6f8e7a777fef3d5a061df500295d749

--- AxonIvyPortal/portal-selenium-test/src_test/com/axonivy/portal/selenium/page/TaskEditWidgetNewDashBoardPage.java
+++ AxonIvyPortal/portal-selenium-test/src_test/com/axonivy/portal/selenium/page/TaskEditWidgetNewDashBoardPage.java
@@ -3,11 +3,15 @@ package com.axonivy.portal.selenium.page;
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.*;
 
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.SelenideElement;
 
 public class TaskEditWidgetNewDashBoardPage extends TemplatePage {
 
   private String taskEditWidgetId;
+  private static final String TASK_NAME = "Task name";
+  private static final String STATE = "State";
 
   public TaskEditWidgetNewDashBoardPage() {
     this("div[id='new-widget-configuration-dialog']");
@@ -25,6 +29,69 @@ public class TaskEditWidgetNewDashBoardPage extends TemplatePage {
   public void changeWidgetTitle(String name) {
     widgetTitle().clear();
     widgetTitle().sendKeys(name);
+  }
+  
+  private int getIndexFiltertByName(String name) {
+    ElementsCollection elementsTH =
+        $(taskEditWidgetId).waitUntil(appear, DEFAULT_TIMEOUT).$("div[id$='user-filter']").$$("div");
+    for (int i = 0; i < elementsTH.size(); i++) {
+      if (elementsTH.get(i).getText().equalsIgnoreCase(name)) {
+        return i;
+      }
+    }
+    return 0;
+  }
+  
+  private SelenideElement getAvailableFilterInput(String filterName) {
+    int index = getIndexFiltertByName(filterName);
+    return $(taskEditWidgetId).waitUntil(appear, DEFAULT_TIMEOUT).$("div[id$='user-filter']").$$("div").get(index + 1)
+        .$("input");
+  }
+  
+  public void filterTaskName(String taskName) {
+    getAvailableFilterInput(TASK_NAME).sendKeys(taskName);
+  }
+  
+  public void clickOnStateToShowDropdown() {
+    int index = getIndexFiltertByName(STATE);
+    $(taskEditWidgetId).waitUntil(appear, DEFAULT_TIMEOUT).$("div[id$='user-filter']").$$("div").get(index + 1)
+        .click();
+  }
+  
+  private SelenideElement getValueOfCheckBox(String value) {
+    return $("div[style*='display: block;'] div.ui-selectcheckboxmenu-items-wrapper")
+        .waitUntil(appear, DEFAULT_TIMEOUT).$$("li.ui-selectcheckboxmenu-item").filter(text(value)).first().$("div.ui-chkbox-box");
+  }
+  
+  private SelenideElement getCloseCheckBox() {
+    return $("div.ui-selectcheckboxmenu-panel[style*='display: block;']").waitUntil(appear, DEFAULT_TIMEOUT).$("a.ui-selectcheckboxmenu-close");
+  }
+  
+  public void selectState(String state) {
+    getValueOfCheckBox(state).shouldBe(getClickableCondition()).click();
+    getCloseCheckBox().shouldBe(getClickableCondition()).click();
+  }
+  
+  public void preview() {
+    $(taskEditWidgetId).waitUntil(appear, DEFAULT_TIMEOUT).$("button[id$='preview-button']")
+        .shouldBe(getClickableCondition()).click();
+  }
+  
+  private ElementsCollection getColumnsOfTableWidget() {
+    return $(taskEditWidgetId).waitUntil(appear, DEFAULT_TIMEOUT).$$("table tbody tr td");
+  }
+  
+  private ElementsCollection getAllTasksOfTaskWidget() {
+    return getColumnsOfTableWidget().filter(Condition.cssClass("dashboard-tasks__name"));
+  }
+  
+  public ElementsCollection countAllTasks() {
+    return getAllTasksOfTaskWidget();
+  }
+  
+  public void nextPageTable() {
+    $(taskEditWidgetId).waitUntil(appear, DEFAULT_TIMEOUT).$("div[id$='widget-preview']")
+    .waitUntil(appear, DEFAULT_TIMEOUT).$("a.ui-paginator-next").shouldBe(getClickableCondition()).click();
   }
   
   public void save() {
