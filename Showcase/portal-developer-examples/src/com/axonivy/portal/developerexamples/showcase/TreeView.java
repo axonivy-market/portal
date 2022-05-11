6c4ad136abcb2da64f205de5552c65bbc44b6678
Long Vo Thanh <long.vothanh@axonactive.com>
2022-05-11 11:37:38 +0700

    IVYPORTAL-13502 Various or urgent requests in IVYPORTAL - S225 > IVYPORTAL-13546 Fix PF 11 deprecated warning

--- Showcase/portal-developer-examples/src/com/axonivy/portal/developerexamples/showcase/TreeView.java
+++ Showcase/portal-developer-examples/src/com/axonivy/portal/developerexamples/showcase/TreeView.java
@@ -13,28 +13,28 @@ import org.primefaces.model.TreeNode;
 @ViewScoped
 public class TreeView implements Serializable {
 	private static final long serialVersionUID = 7782960801078745575L;
-	private TreeNode root;
+	private TreeNode<String> root;
      
     @PostConstruct
     public void init() {
-        root = new DefaultTreeNode("Root", null);
-        TreeNode node0 = new DefaultTreeNode("Node 0", root);
-        TreeNode node1 = new DefaultTreeNode("Node 1", root);
+        root = new DefaultTreeNode<String>("Root", null);
+        TreeNode<String> node0 = new DefaultTreeNode<String>("Node 0", root);
+        TreeNode<String> node1 = new DefaultTreeNode<String>("Node 1", root);
          
-        TreeNode node00 = new DefaultTreeNode("Node 0.0", node0);
-        TreeNode node01 = new DefaultTreeNode("Node 0.1", node0);
+        TreeNode<String> node00 = new DefaultTreeNode<String>("Node 0.0", node0);
+        TreeNode<String> node01 = new DefaultTreeNode<String>("Node 0.1", node0);
          
-        TreeNode node10 = new DefaultTreeNode("Node 1.0", node1);
+        TreeNode<String> node10 = new DefaultTreeNode<String>("Node 1.0", node1);
          
-        node1.getChildren().add(new DefaultTreeNode("Node 1.1"));
-        node00.getChildren().add(new DefaultTreeNode("Node 0.0.0"));
-        node00.getChildren().add(new DefaultTreeNode("Node 0.0.1"));
-        node01.getChildren().add(new DefaultTreeNode("Node 0.1.0"));
-        node10.getChildren().add(new DefaultTreeNode("Node 1.0.0"));
-        root.getChildren().add(new DefaultTreeNode("Node 2"));
+        node1.getChildren().add(new DefaultTreeNode<String>("Node 1.1"));
+        node00.getChildren().add(new DefaultTreeNode<String>("Node 0.0.0"));
+        node00.getChildren().add(new DefaultTreeNode<String>("Node 0.0.1"));
+        node01.getChildren().add(new DefaultTreeNode<String>("Node 0.1.0"));
+        node10.getChildren().add(new DefaultTreeNode<String>("Node 1.0.0"));
+        root.getChildren().add(new DefaultTreeNode<String>("Node 2"));
     }
  
-    public TreeNode getRoot() {
+    public TreeNode<String> getRoot() {
         return root;
     }
 }
