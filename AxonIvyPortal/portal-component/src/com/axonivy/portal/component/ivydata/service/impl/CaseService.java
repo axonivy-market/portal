0d2db7be6ed458906507282b247949107dbe0fed
nam.mai <nam.mai@axonactive.com>
2022-06-15 14:45:35 +0700

    IVYPORTAL-13491: Support portal styles for portal components in IFrame for v10 - Updated Process History component

--- AxonIvyPortal/portal-component/src/com/axonivy/portal/component/ivydata/service/impl/CaseService.java
+++ AxonIvyPortal/portal-component/src/com/axonivy/portal/component/ivydata/service/impl/CaseService.java
@@ -2,21 +2,15 @@ package com.axonivy.portal.component.ivydata.service.impl;
 
 import static com.axonivy.portal.component.util.HiddenTasksCasesConfig.isHiddenTasksCasesExcluded;
 
-import java.util.Arrays;
 import java.util.List;
 
 import com.axonivy.portal.component.enums.AdditionalProperty;
 import com.axonivy.portal.component.ivydata.dto.IvyCaseResultDTO;
-import com.axonivy.portal.component.ivydata.exception.PortalIvyDataErrorType;
-import com.axonivy.portal.component.ivydata.exception.PortalIvyDataException;
 import com.axonivy.portal.component.ivydata.searchcriteria.CaseSearchCriteria;
 import com.axonivy.portal.component.ivydata.service.ICaseService;
 import com.axonivy.portal.component.util.IvyExecutor;
 
-import ch.ivyteam.ivy.application.ActivityState;
-import ch.ivyteam.ivy.application.IApplication;
 import ch.ivyteam.ivy.environment.Ivy;
-import ch.ivyteam.ivy.server.ServerFactory;
 import ch.ivyteam.ivy.workflow.ICase;
 import ch.ivyteam.ivy.workflow.query.CaseQuery;
 
@@ -32,14 +26,8 @@ public class CaseService implements ICaseService {
   public IvyCaseResultDTO findCasesByCriteria(CaseSearchCriteria criteria, int startIndex, int count) {
     return IvyExecutor.executeAsSystem(() -> {
       IvyCaseResultDTO result = new IvyCaseResultDTO();
-      try {
-        CaseQuery finalQuery = extendQuery(criteria);
-        result.setCases(executeCaseQuery(finalQuery, startIndex, count));
-      } catch (Exception ex) {
-        Ivy.log().error("Error in getting cases", ex);
-        result
-            .setErrors(Arrays.asList(new PortalIvyDataException(PortalIvyDataErrorType.FAIL_TO_LOAD_CASE.toString())));
-      }
+      CaseQuery finalQuery = extendQuery(criteria);
+      result.setCases(executeCaseQuery(finalQuery, startIndex, count));
       return result;
     });
   }
@@ -48,67 +36,26 @@ public class CaseService implements ICaseService {
   public IvyCaseResultDTO countCasesByCriteria(CaseSearchCriteria criteria) {
     return IvyExecutor.executeAsSystem(() -> {
       IvyCaseResultDTO result = new IvyCaseResultDTO();
-      try {
-        CaseQuery finalQuery = extendQuery(criteria);
-        result.setTotalCases(countCases(finalQuery));
-      } catch (Exception ex) {
-        Ivy.log().error("Error in counting cases", ex);
-        result.setErrors(Arrays.asList(new PortalIvyDataException(PortalIvyDataErrorType.FAIL_TO_COUNT_CASE.toString())));
-      }
+      CaseQuery finalQuery = extendQuery(criteria);
+      result.setTotalCases(countCases(finalQuery));
+     
       return result;
     });
   }
 
   private CaseQuery extendQuery(CaseSearchCriteria criteria) {
     CaseQuery finalQuery = criteria.getFinalCaseQuery();
-    @SuppressWarnings("deprecation")
-    CaseQuery clonedQuery = CaseQuery.fromJson(finalQuery.asJson()); // clone to keep the final query in
-                                                                     // CaseSearchCriteria
+    CaseQuery clonedQuery = CaseQuery.fromJson(finalQuery.asJson()); // clone to keep the final query in CaseSearchCriteria
 
-    if (criteria.hasApps()) {
-      if (criteria.hasInvolvedUsername() && !criteria.isAdminQuery()) {
-        clonedQuery.where()
-            .and(queryForUsers(criteria));
-      } else {
-        clonedQuery.where().and(queryForApplications(criteria.getApps()));
-      }
-    }
-
-    if (isHiddenTasksCasesExcluded(criteria.getApps())) {
+    if (!criteria.isAdminQuery()) {
+      clonedQuery.where().and(queryForCurrentUser(criteria.isTechnicalCase(), criteria.isCaseOwnerEnabled()));
+    } 
+    
+    if (isHiddenTasksCasesExcluded()) {
       clonedQuery.where().and(queryExcludeHiddenCases());
     }
-
+    
     return clonedQuery;
-  }
-
-  private CaseQuery queryForUsers(CaseSearchCriteria criteria) {
-    List<String> apps = criteria.getApps();
-    String involvedUsername = criteria.getInvolvedUsername();
-    CaseQuery caseQuery;
-    if (criteria.isTechnicalCase()) {
-      caseQuery = CaseQuery.subCases();
-    } else {
-      caseQuery = CaseQuery.businessCases();
-    }
-
-    apps.forEach(app -> {
-      caseQuery.where().or().userIsInvolved(involvedUsername, app);
-      if (criteria.isCaseOwnerEnabled()) {
-        caseQuery.where().or().isOwner("#" + involvedUsername, app);
-      }
-    });
-    return caseQuery;
-  }
-
-  private CaseQuery queryForApplications(List<String> apps) {
-    CaseQuery caseQuery = CaseQuery.businessCases();
-    apps.forEach(app -> {
-      IApplication application = ServerFactory.getServer().getApplicationConfigurationManager().findApplication(app);
-      if (application != null && application.getActivityState() == ActivityState.ACTIVE) {
-        caseQuery.where().or().applicationId().isEqual(application.getId());
-      }
-    });
-    return caseQuery;
   }
 
   private CaseQuery queryExcludeHiddenCases() {
@@ -121,5 +68,21 @@ public class CaseService implements ICaseService {
 
   private long countCases(CaseQuery query) {
     return Ivy.wf().getGlobalContext().getCaseQueryExecutor().getCount(query);
+  }
+
+  private CaseQuery queryForCurrentUser(boolean isTechnicalCase, boolean isCaseOwnerEnabled) {
+    CaseQuery caseQuery;
+    if (isTechnicalCase) {
+      caseQuery = CaseQuery.subCases();
+    } else {
+      caseQuery = CaseQuery.businessCases();
+    }
+
+    caseQuery.where().or().currentUserIsInvolved();
+    if (isCaseOwnerEnabled) {
+      caseQuery.where().or().currentUserIsOwner();
+    }
+    
+    return caseQuery;
   }
 }
--- AxonIvyPortal/portal-component/src/com/axonivy/portal/component/util/HiddenTasksCasesConfig.java
+++ AxonIvyPortal/portal-component/src/com/axonivy/portal/component/util/HiddenTasksCasesConfig.java
@@ -1,12 +1,6 @@
 package com.axonivy.portal.component.util;
 
-import java.util.List;
-
-import org.apache.commons.collections4.CollectionUtils;
-
-import ch.ivyteam.ivy.application.restricted.IGlobalVariable;
-import ch.ivyteam.ivy.bpm.error.BpmError;
-import ch.ivyteam.ivy.server.ServerFactory;
+import ch.ivyteam.ivy.environment.Ivy;
 
 public class HiddenTasksCasesConfig {
   public static final String PORTAL_HIDDEN_TASK_CASE_EXCLUDED = "PortalHiddenTaskCaseExcluded";
@@ -15,25 +9,8 @@ public class HiddenTasksCasesConfig {
     
   }
 
-  public static boolean isHiddenTasksCasesExcluded(List<String> apps) {
-    return getGlobalVariable(apps, PORTAL_HIDDEN_TASK_CASE_EXCLUDED);
-  }
-  
-  private static boolean getGlobalVariable(List<String> apps, String variableName) {
-    if (CollectionUtils.isEmpty(apps)) {
-      return false;
-    }
-    try {
-      return ServerFactory.getServer().getSecurityManager().executeAsSystem(() -> {
-        String globalVariableValue = apps.stream().findFirst()
-                        .map(ServerFactory.getServer().getApplicationConfigurationManager()::findApplication)
-                        .map(app -> app.findGlobalVariable(variableName))
-                        .map(IGlobalVariable::getValue).orElse("true");
-        return Boolean.parseBoolean(globalVariableValue);
-      });
-    } catch (Exception exception) {
-      throw BpmError.create("ivy:portalconnector:error:applicationservice").withCause(exception).build();
-    }
+  public static boolean isHiddenTasksCasesExcluded() {
+    return Boolean.valueOf(Ivy.var().get(PORTAL_HIDDEN_TASK_CASE_EXCLUDED));
   }
   
 }
