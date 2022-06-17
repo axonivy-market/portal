215bb4d52115112e046ed02e156f7e584dd5d311
Loi Nguyen Kim Van <loi.nguyenkimvan@axonactive.com>
2022-06-17 10:19:20 +0000

    Merged in feature/IVYPORTAL-13698-sticky-dashboard-navigation (pull request #628)
    IVYPORTAL-13698 Sticky Dashboard navigation
    
    * IVYPORTAL-13698 Sticky Dashboard navigation
    - Implemented
    
    * Merge branch 'master' of https://bitbucket.org/axonivy-prod/axonivyportal/src/master into feature/IVYPORTAL-13698-sticky-dashboard-navigation
    
    * IVYPORTAL-13698 Sticky Dashboard navigation
    - Handled feedback
    
    * IVYPORTAL-13698 Sticky Dashboard navigation
    - Handled feedback
    
    * IVYPORTAL-13698 Sticky Dashboard navigation
    - Handled feedback
    
    * IVYPORTAL-13698 Sticky Dashboard navigation
    - Handled feedback
    
    Former-commit-id: 717b5d314cff1856184a7e69fd1437607ff401ac

--- AxonIvyPortal/portal-selenium-test/src_test/com/axonivy/portal/selenium/page/ProcessWidgetNewDashBoardPage.java
+++ AxonIvyPortal/portal-selenium-test/src_test/com/axonivy/portal/selenium/page/ProcessWidgetNewDashBoardPage.java
@@ -1,6 +1,8 @@
 package com.axonivy.portal.selenium.page;
 
+import static com.codeborne.selenide.Condition.appear;
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
 
 import com.codeborne.selenide.ElementsCollection;
@@ -27,5 +29,10 @@ public class ProcessWidgetNewDashBoardPage extends TemplatePage {
   
   public ElementsCollection expand() {
     return $$("div.widget__header").filter(text(processWidgetName));
+  }
+  
+  public void startProcessByName(String processName) {
+    $(processWidgetId).waitUntil(appear, DEFAULT_TIMEOUT).$$("span.process-start-list-item").filter(text(processName))
+        .first().$("a").shouldBe(getClickableCondition()).click();
   }
 }
