da54a2f275b72e73394fd694ed36dbea1dec6440
PHAM HOANG HUNG <hung.pham@axonactive.com>
2022-06-23 17:53:47 +0700

    IVYPORTAL-13658 UI improvement on devices - IVYPORTAL-13760 Widgets usually can not scroll on iPhone, Galaxy A20 - fix bug

--- AxonIvyPortal/PortalTemplate/webContent/resources/js/dashboard.js
+++ AxonIvyPortal/PortalTemplate/webContent/resources/js/dashboard.js
@@ -236,20 +236,3 @@ function loadWidgetFirstTime(loadingClass, widgetClass) {
     widget.removeClass('u-invisibility');
   }
 }
-
-function setupTableWidget(widgetClass) {
-  if (!PrimeFaces.env.ios) {
-    return;
-  }
-
-  var widget = $('.' + widgetClass);
-  if (widget.length > 0) {
-    var tableWidgetPanel = widget.find('.table-widget-panel');
-    if (tableWidgetPanel.length > 0) {
-      var widgetContent = widget.find('.widget__content');
-      if (widgetContent.length > 0) {
-        widgetContent.outerHeight(tableWidgetPanel.height() - 44);
-      }
-    }
-  }
-}
