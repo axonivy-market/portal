d52b9bc941951487bdb4b1a59534054707b1a981
nam.mai <nam.mai@axonactive.com>
2022-06-27 10:36:43 +0700

    IVYPORTAL-13491: Support portal styles for portal components in IFrame for v10 - Moved classes and sub processes related to documents to portal-component project - Updated migration notes and examples of classes and sub processes related to documents - Moved CMS of enum to proper place

--- Showcase/portal-developer-examples/src/com/axonivy/portal/developerexamples/bean/DocumentTableUsageBean.java
+++ Showcase/portal-developer-examples/src/com/axonivy/portal/developerexamples/bean/DocumentTableUsageBean.java
@@ -6,9 +6,9 @@ import java.util.List;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 
+import com.axonivy.portal.component.enums.DocumentType;
 import com.axonivy.portal.developerexamples.enums.ExtendedDocumentType;
 
-import ch.ivy.addon.portalkit.enums.DocumentType;
 import ch.ivy.addon.portalkit.service.CaseDocumentService;
 
 @ManagedBean
