b3a49864f99b13e853f89949f1e3c2049fd3252c
Nam.Chu <nam.chu@axonactive.com>
2022-07-07 17:12:32 +0700

    IVYPORTAL-13805 Remove usages of IApplication#current - Replace ivy.request.getApplication function - Implement get multi apps in version info, process start collector

--- AxonIvyPortal/PortalKit/src/ch/ivy/addon/portalkit/constant/IvyCacheIdentifier.java
+++ AxonIvyPortal/PortalKit/src/ch/ivy/addon/portalkit/constant/IvyCacheIdentifier.java
@@ -2,11 +2,13 @@ package ch.ivy.addon.portalkit.constant;
 
 public final class IvyCacheIdentifier {
 
+  @Deprecated(forRemoval = true)
   public static final String ROLES_IN_APPLICATION = "ROLES_IN_APPLICATION";
   public static final String PORTAL_ANNOUNCEMENT_CACHE_GROUP_NAME = "PORTAL_ANNOUNCEMENT";
   public static final String LOGOUT_PAGE_CACHE_GROUP_NAME = "LOGOUT_PAGE_GROUP";
   public static final String LOGOUT_PAGE_CACHE_ENTRY_NAME = "LOGOUT_PAGE_ID";
   public static final String STATISTIC_COLOR = "STATISTIC_COLOR"; 
+  public static final String ROLES_IN_SECURITY_CONTEXT = "ROLES_IN_SECURITY_CONTEXT";
 
   private IvyCacheIdentifier() {}
 }
