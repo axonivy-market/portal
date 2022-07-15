6ee7963fda941eb270900b4b2d5e16a330dae198
PHAM HOANG HUNG <hung.pham@axonactive.com>
2022-07-15 15:55:29 +0700

    IVYPORTAL-13856 Various or urgent requests in IVYPORTAL - S228 - IVYPORTAL-13952 Update description setting Portal.StatisticChartsPolling

--- AxonIvyPortal/PortalKit/src/ch/ivy/addon/portalkit/enums/GlobalVariable.java
+++ AxonIvyPortal/PortalKit/src/ch/ivy/addon/portalkit/enums/GlobalVariable.java
@@ -66,7 +66,7 @@ public enum GlobalVariable {
   HIDE_RELATED_CASE_INFO_FROM_HISTORY("Portal.Histories.HideRelatedCaseInfo", GlobalVariableType.SELECTION, Option.TRUE.toString(), "hideRelatedCaseInfoFromHistory"),
   SHOW_ERROR_LOG_TO_CONSOLE("Portal.ShowErrorLogToConsole", GlobalVariableType.SELECTION, Option.FALSE.toString(), "showErrorLogToConsole"),
   SHOW_AVATAR("Portal.ShowAvatar",GlobalVariableType.SELECTION,Option.TRUE.toString(),"showAvatar"),
-  STATISTIC_CHARTS_POLLING("Portal.StatisticChartsPolling", GlobalVariableType.NUMBER, "0", "statisticChartsPolling"),
+  STATISTIC_CHART_SCALING_INTERVAL("Portal.StatisticChartScalingInterval", GlobalVariableType.NUMBER, "0", "statisticChartScalingInterval"),
   ROLE_DIRECT_CHILDREN_LIMIT("Portal.RoleDirectChildrenLimit", GlobalVariableType.SELECTION, "50", "RoleDirectChildrenLimit", new Object[] { 10, 50, 100}),
   ROLE_PARENT_LIMIT("Portal.RoleParentLimit", GlobalVariableType.SELECTION, "10", "RoleParentLimit", new Object[] { 5, 10, 20});
   
