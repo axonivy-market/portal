12cc02e8630a975d55b946ae32131312c91ee63c
lttung_wawa <tung.le@axonactive.com>
2022-07-21 21:59:40 +0700

    IVYPORTAL-13845 Use Frame Mode for Case Map example - Fixed screenshot test
    Former-commit-id: 2c41593d16e42573cbb4e4fceb01cfa3c245ad4e

--- AxonIvyPortal/PortalTest/resources/copy-decorate-js-css.bat
+++ AxonIvyPortal/PortalTest/resources/copy-decorate-js-css.bat
@@ -1,2 +1,4 @@
 type AxonivyPortal\PortalTest\resources\css\document-screenshot.css >> AxonivyPortal\PortalKit\webContent\resources\css\portal.css
+type AxonivyPortal\PortalTest\resources\css\document-screenshot.css >> Showcase\portal-user-examples\webContent\resources\css\portal-user-examples.css
 type AxonivyPortal\PortalTest\resources\js\document-screenshot.js >> AxonivyPortal\PortalTemplate\webContent\resources\js\portal.js
+type AxonivyPortal\PortalTest\resources\js\document-screenshot.js >> Showcase\portal-user-examples\webContent\resources\js\portal-user-examples.js
