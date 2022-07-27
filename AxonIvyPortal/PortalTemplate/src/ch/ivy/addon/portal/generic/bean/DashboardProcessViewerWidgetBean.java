5fdc52e4c61d5a805103ad95490cfe456353578f
Long Vo Thanh <long.vothanh@axonactive.com>
2022-07-27 09:14:48 +0700

    IVYPORTAL-13910 Component for process and case map viewer in portalcomponents - Create new Process Viewer widget: load widget asynchronously, use DashboarhProcess for Process selection, fix style

--- AxonIvyPortal/PortalTemplate/src/ch/ivy/addon/portal/generic/bean/DashboardProcessViewerWidgetBean.java
+++ AxonIvyPortal/PortalTemplate/src/ch/ivy/addon/portal/generic/bean/DashboardProcessViewerWidgetBean.java
@@ -1,6 +1,7 @@
 package ch.ivy.addon.portal.generic.bean;
 
 import java.io.Serializable;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -10,10 +11,9 @@ import javax.faces.bean.ViewScoped;
 
 import org.apache.commons.lang3.StringUtils;
 
-import ch.ivy.addon.portalkit.dto.dashboard.ProcessViewerDashboardWidget;
+import ch.ivy.addon.portalkit.bo.Process;
+import ch.ivy.addon.portalkit.dto.dashboard.process.DashboardProcess;
 import ch.ivy.addon.portalkit.ivydata.service.impl.ProcessService;
-import ch.ivyteam.ivy.environment.Ivy;
-import ch.ivyteam.ivy.workflow.IProcessStart;
 import ch.ivyteam.ivy.workflow.start.IWebStartable;
 
 @ManagedBean
@@ -22,31 +22,18 @@ public class DashboardProcessViewerWidgetBean implements Serializable {
 
   private static final long serialVersionUID = -5617205683669511383L;
 
-  private List<IWebStartable> allPortalProcesses;
-  private List<IProcessStart> startableProcessStarts;
+  private List<DashboardProcess> defaultPortalProcesses;
 
   @PostConstruct
   public void init() {
-    allPortalProcesses = ProcessService.newInstance().findProcesses().getProcesses();
-    startableProcessStarts = Ivy.session().getStartableProcessStarts();
+    List<IWebStartable> processes = ProcessService.newInstance().findProcesses().getProcesses();
+    defaultPortalProcesses  = processes.stream().map(DashboardProcess::new)
+        .sorted(Comparator.comparing(Process::getName))
+        .collect(Collectors.toList());
   }
 
-  public List<IWebStartable> completeProcesses(String query) {
-    return allPortalProcesses.stream()
+  public List<DashboardProcess> completeProcesses(String query) {
+    return defaultPortalProcesses.stream()
         .filter(process -> StringUtils.containsIgnoreCase(process.getName(), query)).collect(Collectors.toList());
-  }
-
-  public void onSelectProcess(ProcessViewerDashboardWidget widget) {
-    widget.setProcessStart(getStartableProcessStarts().stream()
-        .filter(process -> process.getLink().getRelative().contentEquals(widget.getStartableProcessStart().getLink().getRelative()))
-        .findFirst().get().getUserFriendlyRequestPath());
-  }
-
-  public List<IProcessStart> getStartableProcessStarts() {
-    return startableProcessStarts;
-  }
-
-  public void setStartableProcessStarts(List<IProcessStart> startableProcessStarts) {
-    this.startableProcessStarts = startableProcessStarts;
   }
 }
