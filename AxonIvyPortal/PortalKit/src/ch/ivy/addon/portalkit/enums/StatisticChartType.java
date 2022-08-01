b8b90cfabc73a7153d00a9d71be91d27921ba877
Loi Nguyen <loi.nguyenkimvan@axonactive.com>
2022-08-01 11:52:18 +0700

    IVYPORTAL-6250 Statistics - New Charts - Cases by Category - Bar Chart - Handled feedback

--- AxonIvyPortal/PortalKit/src/ch/ivy/addon/portalkit/enums/StatisticChartType.java
+++ AxonIvyPortal/PortalKit/src/ch/ivy/addon/portalkit/enums/StatisticChartType.java
@@ -7,7 +7,7 @@ public enum StatisticChartType {
   CASES_BY_FINISHED_TIME("/ch.ivy.addon.portalkit.ui.jsf/statistic/chartType/caseByFinishedTime", "/ch.ivy.addon.portalkit.ui.jsf/statistic/chartType/caseByFinishedTimeDescription"),
   TASK_BY_EXPIRY("/ch.ivy.addon.portalkit.ui.jsf/statistic/chartType/taskByExpiry", null),
   ELAPSED_TIME_BY_CASE_CATEGORY("/ch.ivy.addon.portalkit.ui.jsf/statistic/chartType/elapsedTimeByCaseCategory", "/ch.ivy.addon.portalkit.ui.jsf/statistic/chartType/elapsedTimeByCaseCategoryDescription"),
-  CASES_BY_CATEGORY("/ch.ivy.addon.portalkit.ui.jsf/statistic/chartType/caseByCategory", null);
+  CASES_BY_CATEGORY("/ch.ivy.addon.portalkit.ui.jsf/statistic/chartType/casesByCategory", null);
 
   private String cmsUri;
   private String cmsDescriptionUri;
