68ff3172815952340afba7b470971f6364e5fcef
Pham Hoang Hung <hung.pham@axonactive.com>
2022-08-22 11:05:05 +0000

    Feature/IVYPORTAL-14129 Adapt securityMemberNameAndAvatar component for project portal component * adapt securityMemberNameAndAvatar component for userSelection and roleSelection * update examples * update document * fix document screenshot test * Handle feedback

--- AxonIvyPortal/portal-components/src/com/axonivy/portal/components/util/SecurityMemberDisplayNameUtils.java
+++ AxonIvyPortal/portal-components/src/com/axonivy/portal/components/util/SecurityMemberDisplayNameUtils.java
@@ -2,14 +2,18 @@ package com.axonivy.portal.components.util;
 
 import org.apache.commons.lang.StringUtils;
 
+import com.axonivy.portal.components.dto.UserDTO;
+
 import ch.ivyteam.ivy.environment.Ivy;
 import ch.ivyteam.ivy.security.ISecurityMember;
 import ch.ivyteam.ivy.security.IUser;
 
 public class SecurityMemberDisplayNameUtils {
 
-  private static final String DISABLED_USER_PREFIX_CMS = "/Labels/disabledUserPrefix";
-  private static final String NOT_AVAILABLE_CMS = "/ch.ivy.addon.portalkit.ui.jsf/common/notAvailable";
+  private static final String DISABLED_USER_PREFIX_CMS = "/Labels/DisabledUserPrefix";
+  private static final String NO_NAME_CMS = "/Labels/NoName";
+  private static final String NOT_AVAILABLE_CMS = "/Labels/NotAvailable";
+  private static final String FORMAT_DISABLED_USER = "%s %s";
 
 
   public static String generateBriefDisplayNameForSecurityMember(ISecurityMember securityMember, String securityMemberName) {
@@ -25,14 +29,14 @@ public class SecurityMemberDisplayNameUtils {
     }
     return generateBriefDisplayNameForRole(securityMember.getDisplayName(), securityMember.getName());
   }
-  
+
   public static String generateBriefDisplayNameForRole(String displayName, String name) {
     if (StringUtils.isBlank(displayName)) {
       return name;
     }
     return displayName;
   }
-  
+
   public static String generateBriefDisplayNameForUser(IUser user, String username) {
     if (StringUtils.isBlank(username)) {
       return Ivy.cms().co(NOT_AVAILABLE_CMS);
@@ -54,4 +58,18 @@ public class SecurityMemberDisplayNameUtils {
     }
     return username.startsWith("#") ? username.substring(1) : username;
   }
+
+  public static String generateDisplayNameForUserDTO(UserDTO user) {
+    if (user == null) {
+      return Ivy.cms().co(NOT_AVAILABLE_CMS);
+    }
+
+    String displayName = StringUtils.defaultIfBlank(user.getDisplayName(), Ivy.cms().co(NO_NAME_CMS));
+    return user.isEnabled() ? displayName : formatDisabledUser(displayName);
+  }
+
+  private static String formatDisabledUser(String displayName) {
+    return String.format(FORMAT_DISABLED_USER, Ivy.cms().co(DISABLED_USER_PREFIX_CMS), displayName);
+  }
+
 }
