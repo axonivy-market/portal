67d9776d3867ec7fb8b73a5c2cfff7ff8fb5f766
Loi Nguyen Kim Van <loi.nguyenkimvan@axonactive.com>
2022-08-25 08:40:50 +0000

    Merged in feature/IVYPORTAL-14161-enhance-portal-components-examples-and-documentation (pull request #707)
    Feature/IVYPORTAL-14161 enhance portal components examples and documentation
    
    * IVYPORTAL-14161 Enhance portal components examples and documentation
    - Handled feedback
    
    * Merge branch 'master' of https://bitbucket.org/axonivy-prod/axonivyportal/src/master into feature/IVYPORTAL-14161-enhance-portal-components-examples-and-documentation
    
    * IVYPORTAL-14161 Enhance portal components examples and documentation
    - Handled feedback
    
    * IVYPORTAL-14161 Enhance portal components examples and documentation
    - Fixed screenshot fail
    
    * IVYPORTAL-14161 Enhance portal components examples and documentation
    - Fixed screenshot fail
    
    * IVYPORTAL-14161 Enhance portal components examples and documentation
    - Fixed screenshot fail
    
    * IVYPORTAL-14161 Enhance portal components examples and documentation
    - Handle feedback
    
    * IVYPORTAL-14161 Enhance portal components examples and documentation
    - Handle feedback
    
    * IVYPORTAL-14161 Enhance portal components examples and documentation
    - Handle feedback
    
    * Revert "IVYPORTAL-14161 Enhance portal components examples and documentation"
    
    This reverts commit e3310d93e9c6d6e957df5f3eb16f0d15ff336988.
    
    Former-commit-id: f65b99cdad91502a2db955990dd68d76367b35c1

--- AxonIvyPortal/PortalTest/src_test/portal/guitest/page/UserSelectionComponentPage.java
+++ AxonIvyPortal/PortalTest/src_test/portal/guitest/page/UserSelectionComponentPage.java
@@ -25,31 +25,54 @@ public class UserSelectionComponentPage extends TemplatePage {
   }
   
   public void selectFirstItemForNormalUserSelectionComponent(String keyword) {
-    type(By.cssSelector("input[id$='user-by-role-autocomplete_input']"), keyword);
-    waitForElementDisplayed(By.cssSelector("span[id$='user-by-role-autocomplete_panel']"), true);
-    List<WebElement> selectionItems = findElementByCssSelector("span[id$='user-by-role-autocomplete_panel']").findElements(By.className("ui-autocomplete-item"));
-    click(selectionItems.get(0));
+    click(selectUserComponent("user-by-role-autocomplete", keyword));
     waitForElementDisplayed(By.cssSelector("span[id$='user-by-role-autocomplete_panel']"), false);
   }
   
-  public void selectFirstItemForFloatingLabelUserSelectionComponent(String keyword) {
-    type(By.cssSelector("input[id$='all-user-autocomplete_input']"), keyword);
-    waitForElementDisplayed(By.cssSelector("span[id$='all-user-autocomplete_panel']"), true);
-    List<WebElement> selectionItems = findElementByCssSelector("span[id$='all-user-autocomplete_panel']").findElements(By.className("ui-autocomplete-item"));
-    click(selectionItems.get(0));
+  public String getFirstItemForNormalUserSelectionComponent(String keyword) {
+    return selectUserComponent("user-by-role-autocomplete", keyword).getCssValue("data-item-value");
+  }
+  
+  public String getNormalUserSelection() {
+    return findElementByCssSelector("input[id$='user-by-role-autocomplete_hinput']").getCssValue("value");
+  }
+  
+  public void selectFirstItemForFloatingLabelUserSelectionComponent(String keyword) {   
+    click(selectUserComponent("all-user-autocomplete", keyword));
     waitForElementDisplayed(By.cssSelector("span[id$='all-user-autocomplete_panel']"), false);
   }
   
+  public String getFirstItemForFloatingLabelUserSelectionComponent(String keyword) {   
+    return selectUserComponent("all-user-autocomplete", keyword).getCssValue("data-item-value");
+  }
+  
+  public String getFloatingLabelUserSelection() {
+    return findElementByCssSelector("input[id$='all-user-autocomplete_hinput']").getCssValue("value");
+  }
+  
   public void selectFirstItemForAjaxEventUserSelectionComponent(String keyword) {
-    type(By.cssSelector("input[id$='item-select-event-for-user-selection_input']"), keyword);
-    waitForElementDisplayed(By.cssSelector("span[id$='item-select-event-for-user-selection_panel']"), true);
-    List<WebElement> selectionItems = findElementByCssSelector("span[id$='item-select-event-for-user-selection_panel']").findElements(By.className("ui-autocomplete-item"));
-    click(selectionItems.get(0));
+    click(selectUserComponent("item-select-event-for-user-selection", keyword));
     waitForElementDisplayed(By.id("form:item-select-event-component:item-select-event-for-user-selection-message_info-detail"), true);
+  }
+  
+  public String getFirstItemForAjaxEventUserSelectionComponent(String keyword) {
+    return selectUserComponent("item-select-event-for-user-selection", keyword).getCssValue("data-item-value");
+  }
+  
+  public String getAjaxEventUserSelection() {
+    return findElementByCssSelector("input[id$='item-select-event-for-user-selection_hinput']").getCssValue("value");
   }
   
   public void openSelectionPanelForAjaxEventUserSelectionComponent(String keyword) {
     type(By.cssSelector("input[id$='item-select-event-for-user-selection_input']"), keyword);
     waitForElementDisplayed(By.cssSelector("span[id$='item-select-event-for-user-selection_panel']"), true);
+  }
+  
+  private WebElement selectUserComponent(String componentId, String keyword) {
+    waitForElementDisplayed(By.cssSelector("input[id$='"+componentId+"_input']"), true);
+    type(By.cssSelector("input[id$='"+componentId+"_input']"), keyword);
+    waitForElementDisplayed(By.cssSelector("span[id$='"+componentId+"_panel']"), true);
+    List<WebElement> selectionItems = findElementByCssSelector("span[id$='"+componentId+"_panel']").findElements(By.className("ui-autocomplete-item"));
+    return selectionItems.get(0);
   }
 }
