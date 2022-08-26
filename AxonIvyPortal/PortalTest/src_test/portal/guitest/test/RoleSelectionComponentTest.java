9e84d9f6d7da91ac00b663f21b970300e69d6957
Long Vo Thanh <long.vothanh@axonactive.com>
2022-08-26 10:54:03 +0700

    IVYPORTAL-14161 Enhance portal components examples and documentation - Fix GUI test
    Former-commit-id: 6d9066b9629fba641c95dce23fb4ef6d68f9f832

--- AxonIvyPortal/PortalTest/src_test/portal/guitest/test/RoleSelectionComponentTest.java
+++ AxonIvyPortal/PortalTest/src_test/portal/guitest/test/RoleSelectionComponentTest.java
@@ -20,32 +20,28 @@ public class RoleSelectionComponentTest extends BaseTest {
   @Test
   public void testDefaultRoleSelectionComponent() {
     RoleSelectionComponentPage roleSelectionComponentPage = new RoleSelectionComponentPage();
-    String value = roleSelectionComponentPage.getFirstItemForDefaultRoleSelectionComponent("Backend Developer");
-    roleSelectionComponentPage.selectFirstItemForDefaultRoleSelectionComponent("Backend Developer");
+    String value = roleSelectionComponentPage.selectFirstItemForDefaultRoleSelectionComponent("Backend Developer");
     assertEquals(value, roleSelectionComponentPage.getDefaultRoleSelection());
   }
   
   @Test
   public void testRoleFromDefinedRoleSelectionComponent() {
     RoleSelectionComponentPage roleSelectionComponentPage = new RoleSelectionComponentPage();
-    String value = roleSelectionComponentPage.getFirstItemForRoleFromDefinedRoleSelectionComponent("Backend Developer");
-    roleSelectionComponentPage.selectFirstItemForRoleFromDefinedRoleSelectionComponent("Backend Developer");
+    String value = roleSelectionComponentPage.selectFirstItemForRoleFromDefinedRoleSelectionComponent("Backend Developer");
     assertEquals(value, roleSelectionComponentPage.getRoleFromDefinedRoleSelection());
   }
   
   @Test
   public void testFloatingLabelAndExcludeRoleSelection() {
     RoleSelectionComponentPage roleSelectionComponentPage = new RoleSelectionComponentPage();
-    String value = roleSelectionComponentPage.getFirstItemForFloatingLabelAndExcludeRoleSelectionComponent("Backend Developer");
-    roleSelectionComponentPage.selectFirstItemForFloatingLabelAndExcludeRoleSelectionComponent("Backend Developer");
+    String value = roleSelectionComponentPage.selectFirstItemForFloatingLabelAndExcludeRoleSelectionComponent("Backend Developer");
     assertEquals(value, roleSelectionComponentPage.getFloatingLabelAndExcludeRoleSelection());
   }
   
   @Test
   public void testAjaxEventRoleSelection() {
     RoleSelectionComponentPage roleSelectionComponentPage = new RoleSelectionComponentPage();
-    String value = roleSelectionComponentPage.getFirstItemForAjaxEventRoleSelectionComponent("Backend Developer");
-    roleSelectionComponentPage.selectFirstItemForAjaxEventRoleSelectionComponent("Backend Developer");
+    String value = roleSelectionComponentPage.selectFirstItemForAjaxEventRoleSelectionComponent("Backend Developer");
     assertEquals(value, roleSelectionComponentPage.getAjaxEventRoleSelection());
   }
 
