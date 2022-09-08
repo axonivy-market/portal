19ae8fd80061b0ee1ae5d1773152692fca7e7985
Phan Van Thai <thai.phanvan@axonactive.com>
2022-09-08 17:21:06 +0700

    IVYPORTAL-14176: support azure AD

---
+++ AxonIvyPortal/portal/src/ch/ivy/addon/portalkit/service/OAuthProviderService.java
@@ -0,0 +1,46 @@
+package ch.ivy.addon.portalkit.service;
+
+import java.io.IOException;
+import java.net.URI;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.apache.commons.io.IOUtils;
+
+import ch.ivy.addon.portalkit.bo.OAuthProvider;
+import ch.ivyteam.ivy.security.ISecurityContext;
+import ch.ivyteam.ivy.security.identity.core.auth.oauth2.OAuth2Url;
+import ch.ivyteam.ivy.security.identity.spi.IdentityProvider;
+import ch.ivyteam.ivy.security.identity.spi.auth.oauth2.OAuth2Authenticator;
+import ch.ivyteam.ivy.security.restricted.ISecurityContextInternal;
+
+public class OAuthProviderService {
+  private static OAuthProviderService instance;
+
+  public static OAuthProviderService getInstance() {
+    if (instance == null) {
+      instance = new OAuthProviderService();
+    }
+    return instance;
+  }
+
+  public List<OAuthProvider> getOAuthProviders() {
+    var securityContext = (ISecurityContextInternal) ISecurityContext.current();
+    return securityContext.identityProviders().stream().filter(p -> p.authenticator() instanceof OAuth2Authenticator)
+        .map(this::toOAuthPovider).collect(Collectors.toList());
+  }
+
+  private OAuthProvider toOAuthPovider(IdentityProvider provider) {
+    var initUri = OAuth2Url.initUri(ISecurityContext.current(), provider);
+    return new OAuthProvider(provider.displayName(), loadResource(provider.logo()), initUri);
+  }
+
+  private String loadResource(URI uri) {
+    try {
+      return IOUtils.toString(uri, StandardCharsets.UTF_8);
+    } catch (IOException ex) {
+      throw new RuntimeException(ex);
+    }
+  }
+}
