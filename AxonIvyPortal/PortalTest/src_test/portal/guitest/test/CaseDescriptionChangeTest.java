a6d53bb4af7d69418b05f6a6ce65ce3541924c2c
Nguyen Quoc Hoan <hoan.nguyen01@axonactive.com>
2022-09-08 14:30:27 +0700

    IVYPORTAL-14177: Change Edit Task and Case Name to new API - Changed to new APIs - Update examples - Write new GUi Tests

--- AxonIvyPortal/PortalTest/src_test/portal/guitest/test/CaseDescriptionChangeTest.java
+++ AxonIvyPortal/PortalTest/src_test/portal/guitest/test/CaseDescriptionChangeTest.java
@@ -3,6 +3,7 @@ package portal.guitest.test;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -12,9 +13,13 @@ import portal.guitest.page.CaseDetailsPage;
 import portal.guitest.page.CaseWidgetPage;
 import portal.guitest.page.HomePage;
 import portal.guitest.page.MainMenuPage;
+import portal.guitest.page.UserProfilePage;
 
 public class CaseDescriptionChangeTest extends BaseTest {
   
+  private HomePage homePage;
+  private CaseWidgetPage casePage;
+
   @Override
   @Before
   public void setup() {
@@ -24,24 +29,65 @@ public class CaseDescriptionChangeTest extends BaseTest {
 
   @Test
   public void testChangeCaseDescription() {
-    String newCaseDescription = "New Case Description";
+    var caseNameEn = "Leave Request";
+    var caseNameGer = "Urlaubsantrag";
+    var newCaseDescriptionEn = "New Case Description - English";
+    var newCaseDescriptionGer = "New Case Description - German";
     login(TestAccount.ADMIN_USER);
-    HomePage homePage = new HomePage();
+    homePage = new HomePage();
     MainMenuPage mainMenuPage = homePage.openMainMenu();
-    CaseWidgetPage casePage = mainMenuPage.selectCaseMenu();
-    CaseDetailsPage detailsPage = casePage.openDetailsOfCaseHasName("Leave Request");
-    detailsPage.changeCaseDescription(newCaseDescription);
-    String updatedDescription = detailsPage.getDescription();
-    assertEquals(newCaseDescription, updatedDescription);
+    casePage = mainMenuPage.selectCaseMenu();
+    CaseDetailsPage detailsPage = casePage.openDetailsOfCaseHasName(caseNameEn);
+    detailsPage.changeCaseDescription(newCaseDescriptionEn);
+    assertEquals(newCaseDescriptionEn, detailsPage.getDescription());
+
+    // Change to language to German then change description
+    changeLanguage(3);
+    redirectToRelativeLink(HomePage.PORTAL_HOME_PAGE_URL);
+    homePage = new HomePage();
+    homePage.openMainMenu();
+    casePage = mainMenuPage.selectCaseMenu();
+    detailsPage = casePage.openDetailsOfCaseHasName(caseNameGer);
+    detailsPage.changeCaseDescription(newCaseDescriptionGer);
+    assertEquals(newCaseDescriptionGer, detailsPage.getDescription());
+    
+    // Change to English then verify description
+    changeLanguage(1);
+    detailsPage = casePage.openDetailsOfCaseHasName(caseNameEn);
+    assertEquals(newCaseDescriptionEn, detailsPage.getDescription());
+    
+    // Change to German then verify description
+    changeLanguage(3);
+    redirectToRelativeLink(HomePage.PORTAL_HOME_PAGE_URL);
+    homePage = new HomePage();
+    homePage.openMainMenu();
+    casePage = mainMenuPage.selectCaseMenu();
+    detailsPage = casePage.openDetailsOfCaseHasName(caseNameGer);
+    assertEquals(newCaseDescriptionGer, detailsPage.getDescription());
   }
-  
+
   @Test
   public void testUserWithoutPermissionCannotChangeCaseDescription() {
     int caseIndex = 0;
-    HomePage homePage = new HomePage();
+    homePage = new HomePage();
     MainMenuPage mainMenuPage = homePage.openMainMenu();
-    CaseWidgetPage casePage = mainMenuPage.selectCaseMenu();
+    casePage = mainMenuPage.selectCaseMenu();
     CaseDetailsPage detailsPage = casePage.openDetailsOfCaseHasName("Leave Request");
     assertFalse(detailsPage.isCaseDescriptionChangeComponentPresented(caseIndex));
+  }
+
+  public void changeLanguage(int index) {
+    UserProfilePage userProfilePage = homePage.openMyProfilePage();
+    userProfilePage.selectLanguage(index);
+    homePage = userProfilePage.save();
+    redirectToRelativeLink(HomePage.PORTAL_HOME_PAGE_URL);
+    homePage = new HomePage();
+    var mainMenuPage = homePage.openMainMenu();
+    casePage = mainMenuPage.selectCaseMenu();
+  }
+
+  @After
+  public void resetlanguage() {
+    resetLanguageOfCurrentUser();
   }
 }
