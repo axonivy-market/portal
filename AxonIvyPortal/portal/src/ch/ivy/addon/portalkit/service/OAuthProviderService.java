97aaeadd288cb84b4021f48335dced76516890f7
Phan Van Thai <thai.phanvan@axonactive.com>
2022-09-14 12:40:16 +0700

    IVYPORTAL-14176: update originalURL

--- AxonIvyPortal/portal/src/ch/ivy/addon/portalkit/service/OAuthProviderService.java
+++ AxonIvyPortal/portal/src/ch/ivy/addon/portalkit/service/OAuthProviderService.java
@@ -9,6 +9,7 @@ import java.util.stream.Collectors;
 import org.apache.commons.io.IOUtils;
 
 import ch.ivy.addon.portalkit.bo.OAuthProvider;
+import ch.ivyteam.ivy.environment.Ivy;
 import ch.ivyteam.ivy.security.ISecurityContext;
 import ch.ivyteam.ivy.security.identity.core.auth.oauth2.OAuth2Url;
 import ch.ivyteam.ivy.security.identity.spi.IdentityProvider;
@@ -25,14 +26,15 @@ public class OAuthProviderService {
     return instance;
   }
 
-  public List<OAuthProvider> getOAuthProviders() {
+  public List<OAuthProvider> getOAuthProviders(String callbackUrl) {
     var securityContext = (ISecurityContextInternal) ISecurityContext.current();
     return securityContext.identityProviders().stream().filter(p -> p.authenticator() instanceof OAuth2Authenticator)
-        .map(this::toOAuthPovider).collect(Collectors.toList());
+        .map(p -> toOAuthPovider(p, callbackUrl)).collect(Collectors.toList());
   }
 
-  private OAuthProvider toOAuthPovider(IdentityProvider provider) {
-    var initUri = OAuth2Url.initUri(ISecurityContext.current(), provider);
+  private OAuthProvider toOAuthPovider(IdentityProvider provider, String callbackUrl) {
+    var initUri = OAuth2Url.initUri(ISecurityContext.current(), provider, callbackUrl);
+    Ivy.log().warn("initUri: {0}", initUri);
     return new OAuthProvider(provider.displayName(), loadResource(provider.logo()), initUri);
   }
 
