903e6f98a94f97cfb7e2026bb096c0c1b68f0e9b
nam.mai <nam.mai@axonactive.com>
2022-10-03 14:22:31 +0700

    IVYPORTAL-14342: New Welcome Widget - Small changes - Changed approach to store image in application CMS
    Former-commit-id: c75a537bf734819626f7e9c46b4d20ad5de75eb2

--- AxonIvyPortal/portal/src/ch/ivy/addon/portal/generic/bean/DashboardBean.java
+++ AxonIvyPortal/portal/src/ch/ivy/addon/portal/generic/bean/DashboardBean.java
@@ -29,7 +29,6 @@ import ch.ivy.addon.portalkit.dto.dashboard.DashboardTemplate;
 import ch.ivy.addon.portalkit.dto.dashboard.DashboardWidget;
 import ch.ivy.addon.portalkit.dto.dashboard.SingleProcessDashboardWidget;
 import ch.ivy.addon.portalkit.dto.dashboard.TaskDashboardWidget;
-import ch.ivy.addon.portalkit.dto.dashboard.WelcomeDashboardWidget;
 import ch.ivy.addon.portalkit.dto.dashboard.WidgetFilterModel;
 import ch.ivy.addon.portalkit.enums.BehaviourWhenClickingOnLineInTaskList;
 import ch.ivy.addon.portalkit.enums.CaseEmptyMessage;
@@ -50,7 +49,6 @@ import ch.ivy.addon.portalkit.util.DashboardUtils;
 import ch.ivy.addon.portalkit.util.DashboardWidgetUtils;
 import ch.ivy.addon.portalkit.util.TaskUtils;
 import ch.ivyteam.ivy.environment.Ivy;
-import ch.ivyteam.ivy.scripting.objects.File;
 import ch.ivyteam.ivy.security.ISecurityConstants;
 import ch.ivyteam.ivy.security.IUser;
 import ch.ivyteam.ivy.workflow.ICase;
@@ -157,9 +155,6 @@ public class DashboardBean implements Serializable {
         case CUSTOM:
           loadCustomWidget(widget);
           break;
-        case WELCOME:
-          loadWelcomeWidget(widget);
-          break;
         default:
           break;
       }
@@ -194,17 +189,6 @@ public class DashboardBean implements Serializable {
       customWidget.getData().setType(DashboardCustomWidgetType.PROCESS);
     } else {
       customWidget.getData().setType(DashboardCustomWidgetType.EXTERNAL_URL);
-    }
-  }
-
-  private void loadWelcomeWidget(DashboardWidget widget) {
-    WelcomeDashboardWidget welcomeWidget = (WelcomeDashboardWidget) widget;
-    if (StringUtils.isNotBlank(welcomeWidget.getImageLocation())) {
-      try {
-        welcomeWidget.setUploadedImageFile(new File(welcomeWidget.getImageLocation()));
-      } catch (IOException e) {
-        Ivy.log().error(e);
-      }
     }
   }
 
