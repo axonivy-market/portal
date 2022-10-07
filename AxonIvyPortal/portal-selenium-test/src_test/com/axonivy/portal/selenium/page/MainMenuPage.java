60fde4f09872be3c9eceec36fb2d70880257b31e
Hoan Nguyen <hoan.nguyen01@axonactive.com>
2022-10-07 08:20:00 +0000

    Merged in feature/IVYPORTAL-14439-fix-gui-test-related-to-waitForNavigation (pull request #745)
    Feature/IVYPORTAL-14439 fix gui test related to waitForNavigation
    - wait for old viewstate gone
    - click by js
    - wait for grow message hidden after created tasks
    - fix delete process

--- AxonIvyPortal/portal-selenium-test/src_test/com/axonivy/portal/selenium/page/MainMenuPage.java
+++ AxonIvyPortal/portal-selenium-test/src_test/com/axonivy/portal/selenium/page/MainMenuPage.java
@@ -3,6 +3,7 @@ package com.axonivy.portal.selenium.page;
 import static com.codeborne.selenide.Condition.appear;
 import static com.codeborne.selenide.Selenide.$;
 
+import com.axonivy.portal.selenium.common.WaitHelper;
 import com.codeborne.selenide.Condition;
 
 public class MainMenuPage extends TemplatePage {
@@ -14,31 +15,31 @@ public class MainMenuPage extends TemplatePage {
 
   public TaskWidgetPage openTaskList() {
     waitLeftMenuReady();
-    var taskMenu = $(".layout-menu li[role='menuitem'] a.TASK").waitUntil(appear, DEFAULT_TIMEOUT);
-    waitUntilElementToBeClickable(taskMenu);
-    taskMenu.click();
+    WaitHelper.waitForNavigation(() -> {
+      clickByJavaScript($(".layout-menu li[role='menuitem'] a.TASK").waitUntil(appear, DEFAULT_TIMEOUT).shouldBe(getClickableCondition()));
+    });
     return new TaskWidgetPage();
   }
 
   public CaseWidgetPage openCaseList() {
     waitLeftMenuReady();
-    var caseMenu = $(".layout-menu li[role='menuitem'] a.CASE").waitUntil(appear, DEFAULT_TIMEOUT);
-    waitUntilElementToBeClickable(caseMenu);
-    caseMenu.click();
+    WaitHelper.waitForNavigation(() -> {
+      clickByJavaScript($(".layout-menu li[role='menuitem'] a.CASE").waitUntil(appear, DEFAULT_TIMEOUT).shouldBe(getClickableCondition()));
+    });
     return new CaseWidgetPage();
   }
 
   public ProcessWidgetPage openProcessList() {
     waitLeftMenuReady();
-    var processMenu = $(".layout-menu li[role='menuitem'] a.PROCESS").waitUntil(appear, DEFAULT_TIMEOUT);
-    waitUntilElementToBeClickable(processMenu);
-    processMenu.click();
+    WaitHelper.waitForNavigation(() -> {
+      clickByJavaScript($(".layout-menu li[role='menuitem'] a.PROCESS").waitUntil(appear, DEFAULT_TIMEOUT).shouldBe(getClickableCondition()));
+    });
     return new ProcessWidgetPage();
   }
 
   public void openUserSettingMenu() {
     $("#top-menu").waitUntil(appear, DEFAULT_TIMEOUT);
-    $("a[id='user-settings-menu']").waitUntil(appear, DEFAULT_TIMEOUT).shouldBe(getClickableCondition()).click();
+    clickByJavaScript($("a[id='user-settings-menu']").waitUntil(appear, DEFAULT_TIMEOUT).shouldBe(getClickableCondition()));
   }
 
   private void waitLeftMenuReady() {
