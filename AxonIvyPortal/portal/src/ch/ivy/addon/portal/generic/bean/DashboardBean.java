3f787eca5bb127445e787916bf595342f0ea4a43
nam.mai <nam.mai@axonactive.com>
2022-10-11 10:39:23 +0700

    IVYPORTAL-14359: Various or urgent requests in IVYPORTAL - S232 > IVYPORTAL-14417: Widget title when configured is not respected in view mode - Fixed bug

--- AxonIvyPortal/portal/src/ch/ivy/addon/portal/generic/bean/DashboardBean.java
+++ AxonIvyPortal/portal/src/ch/ivy/addon/portal/generic/bean/DashboardBean.java
@@ -140,27 +140,11 @@ public class DashboardBean implements Serializable {
       return;
     }
     for (var widget : dashboard.getWidgets()) {
-      String cmsUri = "";
       DashboardWidgetUtils.buildWidgetColumns(widget);
-      switch (widget.getType()) {
-        case TASK:
-          cmsUri = "/ch.ivy.addon.portalkit.ui.jsf/dashboard/yourTasks";
-          break;
-        case CASE:
-          cmsUri = "/ch.ivy.addon.portalkit.ui.jsf/dashboard/yourCases";
-          break;
-        case PROCESS:
-          cmsUri = "/ch.ivy.addon.portalkit.ui.jsf/dashboard/yourProcesses";
-          break;
-        case CUSTOM:
-          loadCustomWidget(widget);
-          break;
-        default:
-          break;
+      if (DashboardWidgetType.CUSTOM.equals(widget.getType())) {
+        loadCustomWidget(widget);
       }
-      if (StringUtils.isBlank(widget.getName())) {
-        widget.setName(translate(cmsUri));
-      }
+      
       if (!(widget instanceof SingleProcessDashboardWidget)) {
         WidgetFilterService.getInstance().applyUserFilterFromSession(widget);
       }
@@ -427,5 +411,22 @@ public class DashboardBean implements Serializable {
       }
     }
     return currentDashboardIndex;
+  }
+
+  private String getCmsUriForWidgetName(DashboardWidget widget) {
+    switch (widget.getType()) {
+      case TASK:
+        return "/ch.ivy.addon.portalkit.ui.jsf/dashboard/yourTasks";
+      case CASE:
+        return "/ch.ivy.addon.portalkit.ui.jsf/dashboard/yourCases";
+      case PROCESS:
+        return "/ch.ivy.addon.portalkit.ui.jsf/dashboard/yourProcesses";
+      default:
+        return "/ch.ivy.addon.portalkit.ui.jsf/dashboard/configuration/yourWidget";
+    }
+  }
+
+  public String getDefaultWidgetName(DashboardWidget widgetToGetName) {
+    return Ivy.cms().co(getCmsUriForWidgetName(widgetToGetName));
   }
 }
