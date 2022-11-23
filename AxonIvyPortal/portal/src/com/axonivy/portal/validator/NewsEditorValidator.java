6fe90729f34f15323b62dafde9a36257da68f339
Hoãn <83745591+nqhoan-axonivy@users.noreply.github.com>
2022-11-23 18:09:47 +0700

    Feature/ivyportal 13254 new news widget for dashboard (#12)
    * IVYPORTAL-13254-new-news-widget-for-dashboard
    - Implement
    - write test
    - screenshot
    - clean up code
    - Fixed Tests
    - Handle feedback

---
+++ AxonIvyPortal/portal/src/com/axonivy/portal/validator/NewsEditorValidator.java
@@ -0,0 +1,37 @@
+package com.axonivy.portal.validator;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.FacesValidator;
+import javax.faces.validator.Validator;
+import javax.faces.validator.ValidatorException;
+
+import org.apache.commons.lang3.StringUtils;
+
+import ch.ivy.addon.portalkit.support.HtmlParser;
+import ch.ivyteam.ivy.environment.Ivy;
+
+@FacesValidator(value = "newsEditorValidator")
+public class NewsEditorValidator implements Validator {
+
+  private static final int MAX_NEWS_CONTENT_LENGTH = 1000;
+
+  @Override
+  public void validate(FacesContext context, UIComponent component, Object value) throws ValidatorException {
+    if (value == null) {
+      return;
+    }
+    String content = (String) value;
+    content = HtmlParser.extractTextFromHtml(content);
+    if (StringUtils.isNoneBlank(content)) {
+      if (content.length() > MAX_NEWS_CONTENT_LENGTH) {
+        var message = new FacesMessage(FacesMessage.SEVERITY_ERROR,
+            Ivy.cms().co("/Dialogs/com/axonivy/portal/dashboard/component/NewsWidgetConfiguration/NewsContentMaxLengthMessage"),
+            "");
+        FacesContext.getCurrentInstance().addMessage(null, message);
+        FacesContext.getCurrentInstance().validationFailed();
+      }
+    }
+  }
+}
