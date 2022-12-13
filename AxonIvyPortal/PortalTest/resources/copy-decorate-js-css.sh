fdc094c455d83d3409a9dc81f14535adeb415c9b
PHAM HOANG HUNG <hung.pham@axonactive.com>
2022-12-13 11:50:00 +0700

    test

--- AxonIvyPortal/PortalTest/resources/copy-decorate-js-css.sh
+++ AxonIvyPortal/PortalTest/resources/copy-decorate-js-css.sh
@@ -1,4 +1,4 @@
-y | cp -rf AxonivyPortal/PortalTest/resources/css/document-screenshot.css AxonivyPortal/portal/webContent/resources/css/portal.css
-y | cp -rf AxonivyPortal/PortalTest/resources/css/document-screenshot.css Showcase/portal-user-examples/webContent/resources/css/portal-user-examples.css
-y | cp -rf AxonivyPortal/PortalTest/resources/js/document-screenshot.js AxonivyPortal/portal/webContent/resources/js/portal.js
-y | cp -rf AxonivyPortal/PortalTest/resources/js/document-screenshot.js Showcase/portal-user-examples/webContent/resources/js/portal-user-examples.js
+cp AxonIvyPortal/PortalTest/resources/css/document-screenshot.css AxonIvyPortal/portal/webContent/resources/css/portal.css
+cp AxonIvyPortal/PortalTest/resources/css/document-screenshot.css Showcase/portal-user-examples/webContent/resources/css/portal-user-examples.css
+cp AxonIvyPortal/PortalTest/resources/js/document-screenshot.js AxonIvyPortal/portal/webContent/resources/js/portal.js
+cp AxonIvyPortal/PortalTest/resources/js/document-screenshot.js Showcase/portal-user-examples/webContent/resources/js/portal-user-examples.js
