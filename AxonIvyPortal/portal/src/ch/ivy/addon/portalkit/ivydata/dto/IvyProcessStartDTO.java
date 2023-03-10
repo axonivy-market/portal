4c8989c8a081db5add0ff4121c06967554d7e90d
Hoan Nguyen <83745591+nqhoan-axonivy@users.noreply.github.com>
2023-03-10 14:04:06 +0700

    IVYPORTAL-14816-Custom-widget-cannot-find-Ivy-process-if-Ivy-process-… (#51)
    * IVYPORTAL-14816-Custom-widget-cannot-find-Ivy-process-if-Ivy-process-not-shown-in-Start-list
    - Introduce new function for Custom widget
    - Adapt DTO for Custom widget
    - Deprecated obsoleted start
    - Renamed property
    
    ---------
    
    Co-authored-by: Hoan Nguyen <nqhoan@axonactive.com>

--- AxonIvyPortal/portal/src/ch/ivy/addon/portalkit/ivydata/dto/IvyProcessStartDTO.java
+++ AxonIvyPortal/portal/src/ch/ivy/addon/portalkit/ivydata/dto/IvyProcessStartDTO.java
@@ -1,23 +1,58 @@
 package ch.ivy.addon.portalkit.ivydata.dto;
 
+import ch.ivyteam.ivy.workflow.IStartElement;
 import ch.ivyteam.ivy.workflow.start.IWebStartable;
 
 public class IvyProcessStartDTO extends AbstractResultDTO {
 
+  /**
+   * Use {@link #startElement} instead
+   */
+  @Deprecated(forRemoval = true, since = "10.0.4")
+  private IWebStartable startableProcessStart;
+
+  private IStartElement startElement;
+
   public IvyProcessStartDTO() {}
 
+  /**
+   * Use {@link #IvyProcessStartDTO(IStartElement)} instead
+   * @param startableProcessStart
+   */
+  @Deprecated(forRemoval = true, since = "10.0.4")
   public IvyProcessStartDTO(IWebStartable startableProcessStart) {
     super();
     this.startableProcessStart = startableProcessStart;
   }
 
-  private IWebStartable startableProcessStart;
+  public IvyProcessStartDTO(IStartElement startElement) {
+    super();
+    this.startElement = startElement;
+  }
 
+  /**
+   * Use {@link #getStartElement()} instead
+   * @return IWebStartable
+   */
+  @Deprecated(forRemoval = true, since = "10.0.4")
   public IWebStartable getStartableProcessStart() {
     return startableProcessStart;
   }
 
+  /**
+   * Use {@link #setStartElement(IStartElement)} instead
+   * @param startableProcessStart
+   */
+  @Deprecated(forRemoval = true, since = "10.0.4")
   public void setStartableProcessStart(IWebStartable startableProcessStart) {
     this.startableProcessStart = startableProcessStart;
+  }
+
+  public IStartElement getStartElement() {
+    return startElement;
+  }
+
+  public void setStartElement(IStartElement startElement) {
+    this.startElement = startElement;
   }
 }
