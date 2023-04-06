bcdefd5c19f0851b91978237093335ee3e65b8db
Hoan Nguyen <nqhoan@axonactive.com>
2023-04-06 18:14:03 +0700

    IVYPORTAL-14699: Use-new-process-API-to-get-all-web-startables - Update parameter of custom process

--- AxonIvyPortal/portal/src/ch/ivy/addon/portalkit/ivydata/dto/IvyProcessStartDTO.java
+++ AxonIvyPortal/portal/src/ch/ivy/addon/portalkit/ivydata/dto/IvyProcessStartDTO.java
@@ -1,13 +1,10 @@
 package ch.ivy.addon.portalkit.ivydata.dto;
 
-import ch.ivyteam.ivy.workflow.IStartElement;
 import ch.ivyteam.ivy.workflow.start.IWebStartable;
 
 public class IvyProcessStartDTO extends AbstractResultDTO {
 
   private IWebStartable startableProcessStart;
-
-  private IStartElement startElement;
 
   public IvyProcessStartDTO() {}
 
@@ -16,24 +13,11 @@ public class IvyProcessStartDTO extends AbstractResultDTO {
     this.startableProcessStart = startableProcessStart;
   }
 
-  public IvyProcessStartDTO(IStartElement startElement) {
-    super();
-    this.startElement = startElement;
-  }
-
   public IWebStartable getStartableProcessStart() {
     return startableProcessStart;
   }
 
   public void setStartableProcessStart(IWebStartable startableProcessStart) {
     this.startableProcessStart = startableProcessStart;
-  }
-
-  public IStartElement getStartElement() {
-    return startElement;
-  }
-
-  public void setStartElement(IStartElement startElement) {
-    this.startElement = startElement;
   }
 }
