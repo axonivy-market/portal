05686ab83065ee8aaec98b76490b12d4dc76ea7b
Loc Nguyen <loc.nguyenthanh@axonactive.com>
2023-07-06 11:56:46 +0700

    IVYPORTAL-15004 - Adapt for LE

--- AxonIvyPortal/portal-components/src/com/axonivy/portal/components/dto/BusinessDetailsDTO.java
+++ AxonIvyPortal/portal-components/src/com/axonivy/portal/components/dto/BusinessDetailsDTO.java
@@ -1,27 +1,113 @@
 package com.axonivy.portal.components.dto;
 
+import com.axonivy.portal.components.service.exception.PortalException;
+
+import ch.ivyteam.ivy.environment.Ivy;
 import ch.ivyteam.ivy.workflow.ICase;
 
+/**
+ * Holds Business Detail Information.
+ */
 public class BusinessDetailsDTO {
   private ICase iCase;
-  private String URL;
-  private boolean isFullPath;
+  private String path;
+  private boolean isEmbedInFrame;
 
-  public BusinessDetailsDTO(ICase iCase, String uRL, boolean isFullPath) {
+  private BusinessDetailsDTO(ICase iCase, String path, boolean isEmbedInFrame) {
     this.iCase = iCase;
-    this.URL = uRL;
-    this.isFullPath = isFullPath;
+    this.path = path;
+    this.isEmbedInFrame = isEmbedInFrame;
   }
 
-  public ICase getiCase() {
+  /**
+   * Gets the ICase
+   * @return Icase
+   */
+  public ICase getCase() {
     return iCase;
   }
 
-  public String getURL() {
-    return URL;
+  /**
+   * Gets the business details path
+   * @return string path
+   */
+  public String getPath() {
+    return path;
   }
 
-  public boolean isFullPath() {
-    return isFullPath;
+  /**
+   * Gets the embed in frame boolean
+   * @return boolean isEmbedInFrame
+   */
+  public boolean isEmbedInFrame() {
+    return isEmbedInFrame;
   }
+
+  /**
+   * Create new builder for BusinessDetailsDTO
+   * @return Builder
+   */
+  public static Builder builder() {
+    return new Builder();
+  }
+
+  /**
+   * {@code BusinessDetailsDTO.Builder} is used for creating a {@code BusinessDetailsDTO} from
+   * ICase and path.
+   */
+  public static class Builder {
+    private ICase iCase;
+    private String path;
+    private boolean isEmbedInFrame = Boolean.TRUE;
+
+    /**
+     * Set {@code ICase}
+     * @param iCase
+     *
+     * @return Builder
+     */
+    public Builder iCase(ICase iCase) {
+      this.iCase = iCase;
+      return this;
+    }
+
+    /**
+     * Set path
+     * @param path
+     *
+     * @return Builder
+     */
+    public Builder path(String path) {
+      this.path = path;
+      return this;
+    }
+    
+    /**
+     * Set isEmbedInFrame
+     * @param isEmbedInFrame
+     *
+     * @return Builder
+     */
+    public Builder isEmbedInFrame(boolean isEmbedInFrame) {
+      this.isEmbedInFrame = isEmbedInFrame;
+      return this;
+    }
+    
+    /**
+     * Build BusinessDetailsDTO
+     *
+     * @return BusinessDetailsDTO
+     */
+    public BusinessDetailsDTO build() {
+      if (path == null) {
+        throw new PortalException("Path cannot be null.");
+      }
+      if (iCase == null) {
+        iCase = Ivy.wfCase();
+      }
+      return new BusinessDetailsDTO(iCase, path, isEmbedInFrame);
+    }
+
+  }
+
 }
--- AxonIvyPortal/portal-components/src/com/axonivy/portal/components/publicapi/BusinessDetailsAPI.java
+++ AxonIvyPortal/portal-components/src/com/axonivy/portal/components/publicapi/BusinessDetailsAPI.java
@@ -13,31 +13,62 @@ import com.axonivy.portal.components.service.exception.PortalException;
 public class BusinessDetailsAPI {
 
   /**
-   * Create and set URL to BUSINESS_DETAILS custom field.
+   * <p>Creates and sets path to case custom string field {@code businessDetails} for displaying business case details page.</p>
+   * <p>Use {@code com.axonivy.portal.components.dto.BusinessDetailsDTO.builder()} to
+   * build {@code BusinessDetailsDTO}</p>
+   * <p>This API supports two types of business details:
+   * process path and external link.</p> 
    *
-   * @param businessDetailsDTO Business Details Builder
+   * <p>Examples:</p>
+   * <p>For process path:</p>
+   * <pre><code>BusinessDetailsAPI.create(BusinessDetailsDTO.builder().path("Start Processes/ProcessDemo").build());</code></pre>
+   *
+   * <p>For external link:</p>
+   * <pre><code>BusinessDetailsAPI.create(BusinessDetailsDTO.builder().path("https://www.google.com").build());</code></pre>
+   *
+   * <p>To disable start process in frame, use {@code isEmbedInFrame(Boolean)} in builder. By default, {@code isEmbedInFrame} is true. {@code isEmbedInFrame} does not affect to business detail for external link.</p>
+   * 
+   * <p>To specify ICase, use {@code iCase(ICase)} in builder. By default, {@code Ivy.wfCase()} is used.
+   * @param businessDetailsDTO BusinessDetailsDTO
    */
   public static void create(BusinessDetailsDTO businessDetailsDTO) {
     String pageUrl = createPageUrl(businessDetailsDTO);
-    setToCustomfield(businessDetailsDTO, pageUrl);
+    setToCustomField(businessDetailsDTO, pageUrl);
+  }
+  
+  /**
+   * <p>Creates and sets path to case custom string field {@code businessDetails} of current case for displaying business case details page.</p>
+   *
+   * <p>Examples:</p>
+   * <pre><code>BusinessDetailsAPI.create("Start Processes/ProcessDemo");</code></pre>
+   * <pre><code>BusinessDetailsAPI.create("https://www.google.com");</code></pre>
+   *
+   * @param link String
+   */
+  public static void create(String link) {
+    BusinessDetailsDTO businessDetailsDTO = BusinessDetailsDTO.builder().path(link).build();
+    create(businessDetailsDTO);
+  }
+  
+  private static void setToCustomField(BusinessDetailsDTO businessDetailDTO, String casePageUrl) {
+    businessDetailDTO.getCase().customFields().stringField(CustomFields.BUSINESS_DETAILS).set(casePageUrl);
   }
 
-  private static void setToCustomfield(BusinessDetailsDTO businessDetailDTO, String casePageUrl) {
-    businessDetailDTO.getiCase().customFields().textField(CustomFields.BUSINESS_DETAILS).set(casePageUrl);
-  }
-
-  private static String createPageUrl(BusinessDetailsDTO businessDetailDTO) {
+  private static String createPageUrl(BusinessDetailsDTO businessDetailsDTO) {
     String casePageUrl = null;
-    if (businessDetailDTO.isFullPath()) {
-      casePageUrl = businessDetailDTO.getURL();
+    if (detectExternalLink(businessDetailsDTO.getPath())) {
+      casePageUrl = businessDetailsDTO.getPath();
     } else {
-      String processPath = ProcessStartAPI.findRelativeUrlByProcessStartFriendlyRequestPath(businessDetailDTO.getURL());
+      String processPath = ProcessStartAPI.findRelativeUrlByProcessStartFriendlyRequestPath(businessDetailsDTO.getPath());
       if (StringUtils.isEmpty(processPath)) {
-        throw new PortalException(String.format("Cannot find process path [%s].", businessDetailDTO.getURL()));
+        throw new PortalException(String.format("Cannot find process path [%s].", businessDetailsDTO.getPath()));
       }
-      casePageUrl = processPath + "?caseId=" + businessDetailDTO.getiCase().getId();
+      casePageUrl = processPath + "?caseId=" + businessDetailsDTO.getCase().getId() + (businessDetailsDTO.isEmbedInFrame() ? "&embedInFrame" : "");
     }
     return casePageUrl;
   }
 
+  private static boolean detectExternalLink(String path) {
+    return StringUtils.startsWithIgnoreCase(path, "http");
+  }
 }
