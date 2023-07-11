ce8f0f9b3db3995b3278289f9bdfbf869402cbc8
nhthinh <117440893+nhthinh-axonivy@users.noreply.github.com>
2023-07-11 15:25:25 +0700

    Feature/ivyportal 15173 remove obsolete date format from my profile (#156)
    * feature/IVYPORTAL-15173-Remove-obsolete-date-format-from-My-Profile
    _ Remove Date Format feature
    _ Adapt to use Primefaces locale PATTERN
    _ Adapt session security context
    _ Adapt test
    _ Adapt hide year, hide time feature
    _ Handle GUI Test
    _ Handle feedback
    _ fix GUI test

--- AxonIvyPortal/portal/src/ch/ivy/addon/portalkit/taskfilter/impl/TaskExpiredDateFilter.java
+++ AxonIvyPortal/portal/src/ch/ivy/addon/portalkit/taskfilter/impl/TaskExpiredDateFilter.java
@@ -31,7 +31,7 @@ public class TaskExpiredDateFilter extends TaskFilter {
 
   @Override
   public String value() {
-    DateFormat formatter = new SimpleDateFormat(DateTimeGlobalSettingService.getInstance().getDateTimePattern(), Ivy.session().getContentLocale());
+    DateFormat formatter = DateTimeGlobalSettingService.getInstance().getDefaultDateFormatter();
     if (fromExpiredDate != null && toExpiredDate != null) {
       return String.format(DASH, formatter.format(fromExpiredDate), formatter.format(toExpiredDate));
     }
