924e35dbe35dfd3244d4d7fcca8c9f444a986e42
Thinh Nguyen <thinh.nguyenhung@axonactive.com>
2023-07-14 09:12:25 +0700

    Feature/ivyportal 15173 remove obsolete date format from my profile _ fix GUI test

--- AxonIvyPortal/PortalTest/src_test/portal/guitest/test/TaskDetailsTest.java
+++ AxonIvyPortal/PortalTest/src_test/portal/guitest/test/TaskDetailsTest.java
@@ -80,7 +80,7 @@ public class TaskDetailsTest extends BaseTest {
     String tomorrowStringLiteral = prepareTomorrowAsString();
     taskDetailsPage = openDetailsPageOfFirstTask();
     taskDetailsPage.changeExpiryOfTaskAt(tomorrowStringLiteral);
-    assertTrue(StringUtils.equalsIgnoreCase(tomorrowStringLiteral, taskDetailsPage.getExpiryOfTaskAt()));
+    assertTrue(StringUtils.equalsIgnoreCase(prepareTomorrowAsLocaleDateString(), taskDetailsPage.getExpiryOfTaskAt()));
     String firstTaskNoteComment = taskDetailsPage.getFirstTaskNoteComment();
     assertTrue(StringUtils.contains(firstTaskNoteComment, "Portal Admin User (admin) has set deadline to task"));
     assertTrue(StringUtils.contains(firstTaskNoteComment, "assign Everybody as the task escalation activator"));
