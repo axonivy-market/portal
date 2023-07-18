791b3c5286a7e589288426890327e9ff2d200bbd
Thinh Nguyen <thinh.nguyenhung@axonactive.com>
2023-07-18 11:37:45 +0700

    Feature/ivyportal 15173 remove obsolete date format from my profile _ Refactor Express

--- AxonIvyPortal/portal/src/ch/ivy/addon/portalkit/taskfilter/impl/TaskExpiredDateFilter.java
+++ AxonIvyPortal/portal/src/ch/ivy/addon/portalkit/taskfilter/impl/TaskExpiredDateFilter.java
@@ -1,7 +1,6 @@
 package ch.ivy.addon.portalkit.taskfilter.impl;
 
 import java.text.DateFormat;
-import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import javax.faces.application.FacesMessage;
