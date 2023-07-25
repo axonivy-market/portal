2db2e605a2a62e4bb51180209c4c63454997cf05
Nam.Chu <nam.chu@axonactive.com>
2023-07-25 10:37:43 +0700

    IVYPORTAL-15363 Release Portal 10.0.11 - Convert project and remove unused import

--- AxonIvyPortal/portal-selenium-test/src_test/com/axonivy/portal/selenium/test/DashboardTaskWidgetTest.java
+++ AxonIvyPortal/portal-selenium-test/src_test/com/axonivy/portal/selenium/test/DashboardTaskWidgetTest.java
@@ -12,8 +12,6 @@ import org.junit.jupiter.api.Test;
 import com.axonivy.ivy.webtest.IvyWebTest;
 import com.axonivy.portal.selenium.common.BaseTest;
 import com.axonivy.portal.selenium.common.TestAccount;
-import com.axonivy.portal.selenium.page.CaseEditWidgetNewDashBoardPage;
-import com.axonivy.portal.selenium.page.CaseWidgetNewDashBoardPage;
 import com.axonivy.portal.selenium.page.DashboardModificationPage;
 import com.axonivy.portal.selenium.page.NewDashboardDetailsEditPage;
 import com.axonivy.portal.selenium.page.NewDashboardPage;
