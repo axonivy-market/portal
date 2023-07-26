a84600061bff599d24e068e7ef7fdb9d8bfbe314
nam.mai <nam.mai@axonactive.com>
2023-07-26 17:53:31 +0700

    IVYPORTAL-15211: Migrate old portal customization - Step 2 Callable Approach Implementation - Created enum for signatures - Adapt for reset password

---
+++ AxonIvyPortal/portal-components/src/com/axonivy/portal/components/enums/PortalCustomSignature.java
@@ -0,0 +1,23 @@
+package com.axonivy.portal.components.enums;
+
+public enum PortalCustomSignature {
+  LOGOUT_PAGE("portalGetLogoutPage()"),
+  RESET_PASSWORD("portalResetPassword(String,String,String,String)"),
+  SEND_PASSWORD_RESET_EMAIL("portalSendPasswordResetEmail(String)");
+
+  private String signature;
+
+  private PortalCustomSignature(String signature) {
+    this.signature = signature;
+  }
+
+  public String getSignature() {
+    return signature;
+  }
+
+  public String getDefaultSignature() {
+    String defaultSignature = signature.replaceFirst("portal", "");
+    return defaultSignature.substring(1, 0).toLowerCase() + signature.substring(1);
+  }
+
+}
