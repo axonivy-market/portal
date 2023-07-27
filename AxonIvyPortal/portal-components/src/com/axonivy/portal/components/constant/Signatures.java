57cf0331ec36f734ad7de7ae7ce2335c6378caab
nam.mai <nam.mai@axonactive.com>
2023-07-27 15:20:37 +0700

    IVYPORTAL-15211: Migrate old portal customization - Step 2 Callable Approach Implementation - Adapt for document processes - Added signature file for portal-components

--- AxonIvyPortal/portal-components/src/com/axonivy/portal/components/constant/Signatures.java
+++ AxonIvyPortal/portal-components/src/com/axonivy/portal/components/constant/Signatures.java
@@ -3,6 +3,7 @@ package com.axonivy.portal.components.constant;
 public class Signatures {
   
   public static final String logOutPage = "portalGetLogoutPage()";
+  public static final String resetPassword = "resetPassword(String,String,String,String)";
   public static final String sendResetEmail = "portalSendPasswordResetEmail(String)";
   
 }
