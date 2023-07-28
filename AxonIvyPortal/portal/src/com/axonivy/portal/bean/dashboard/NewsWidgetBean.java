b00225cd6229973282b31ec4081ee3c2bf256517
Le Ngoc Danh <127722015+lndanh-axonivy@users.noreply.github.com>
2023-07-28 10:16:45 +0700

    IVYPORTAL-15215 Support Translation of News in the News Widget with D… (#179)
    * IVYPORTAL-15215 Support Translation of News in the News Widget with DeepL: implement translation
    
    * IVYPORTAL-15215 Support Translation of News in the News Widget with DeepL: update document screenshot test
    
    * IVYPORTAL-15215 Support Translation of News in the News Widget with DeepL: handle feedback
    
    * IVYPORTAL-15215 Support Translation of News in the News Widget with DeepL: handle feedback
    
    * IVYPORTAL-15215 Support Translation of News in the News Widget with DeepL: handle feedback

--- AxonIvyPortal/portal/src/com/axonivy/portal/bean/dashboard/NewsWidgetBean.java
+++ AxonIvyPortal/portal/src/com/axonivy/portal/bean/dashboard/NewsWidgetBean.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
+import java.util.Optional;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.function.Function;
@@ -16,8 +17,10 @@ import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
 
 import org.apache.commons.collections4.CollectionUtils;
+import org.apache.logging.log4j.util.Strings;
 
 import com.axonivy.portal.dto.News;
+import com.axonivy.portal.service.DeepLTranslationService;
 import com.axonivy.portal.service.NewsService;
 
 import ch.ivy.addon.portalkit.enums.PortalPermission;
@@ -37,6 +40,8 @@ public class NewsWidgetBean implements Serializable {
   private News selectedNews;
   private String manageNewsDialogTitle;
   private boolean canManageNews;
+  private String translatedText;
+  private String warningText;
 
   @PostConstruct
   public void init() {
@@ -45,7 +50,8 @@ public class NewsWidgetBean implements Serializable {
 
   public void preAddingNews() {
     initNewsManagement();
-    manageNewsDialogTitle = Ivy.cms().co("/Dialogs/com/axonivy/portal/dashboard/component/NewsWidgetConfiguration/AddNews");
+    manageNewsDialogTitle = Ivy.cms()
+        .co("/Dialogs/com/axonivy/portal/dashboard/component/NewsWidgetConfiguration/AddNews");
     editingNewsList = new ArrayList<>();
     for (var language : supportLanguages) {
       var news = new News();
@@ -55,8 +61,7 @@ public class NewsWidgetBean implements Serializable {
   }
 
   public void publishNews() {
-    if (FacesContext.getCurrentInstance().isValidationFailed()
-        || CollectionUtils.isEmpty(editingNewsList)) {
+    if (FacesContext.getCurrentInstance().isValidationFailed() || CollectionUtils.isEmpty(editingNewsList)) {
       return;
     }
     NewsService.getInstance().saveOrUpdate(editingNewsList);
@@ -65,7 +70,8 @@ public class NewsWidgetBean implements Serializable {
 
   public void preEditingNews(News news) {
     initNewsManagement();
-    manageNewsDialogTitle = Ivy.cms().co("/Dialogs/com/axonivy/portal/dashboard/component/NewsWidgetConfiguration/EditNews");
+    manageNewsDialogTitle = Ivy.cms()
+        .co("/Dialogs/com/axonivy/portal/dashboard/component/NewsWidgetConfiguration/EditNews");
     editingNewsList = NewsService.getInstance().findNewsBySupportedLocale(news.getId(), supportLanguages);
   }
 
@@ -96,6 +102,63 @@ public class NewsWidgetBean implements Serializable {
       activeIndex = supportLanguages.indexOf(defaultLanguage);
     }
     return activeIndex < 0 ? 0 : activeIndex;
+  }
+
+  public void translateTitle(Locale language) {
+    translatedText = "";
+    Optional<News> optionalDefaultNews = getDefaultNews();
+    if (optionalDefaultNews.isPresent()) {
+      String translatedTitle = translate(optionalDefaultNews.get().getName(), language);
+      Optional<News> optionalCurrentNews = editingNewsList.stream()
+          .filter(news -> news.getLocale().getLanguage().equals(language.getLanguage())).findFirst();
+      if (optionalCurrentNews.isPresent()) {
+        optionalCurrentNews.get().setName(translatedTitle);
+      }
+      translatedText = translatedTitle;
+    }
+
+  }
+
+  public void translateDescription(Locale language) {
+    Optional<News> optionalDefaultNews = getDefaultNews();
+    if (optionalDefaultNews.isPresent()) {
+      String translatedDescription = translate(optionalDefaultNews.get().getDescription(), language);
+      Optional<News> optionalCurrentNews = editingNewsList.stream()
+          .filter(news -> news.getLocale().getLanguage().equals(language.getLanguage())).findFirst();
+      if (optionalCurrentNews.isPresent()) {
+        optionalCurrentNews.get().setDescription(translatedDescription);
+      }
+    }
+  }
+
+  public String translate(String text, Locale target) {
+    String translatedText = Strings.EMPTY;
+    warningText = Strings.EMPTY;
+    try {
+      translatedText = DeepLTranslationService.getInstance().translate(text, defaultLanguage, target);
+    } catch (Exception e) {
+      warningText = Ivy.cms().co("/ch.ivy.addon.portalkit.ui.jsf/dashboard/DashboardConfiguration/SomeThingWentWrong");
+      Ivy.log().error("DeepL Translation Service error: ", e.getMessage());
+    }
+    return translatedText;
+
+  }
+  
+  private Optional<News> getDefaultNews() {
+    return editingNewsList.stream()
+        .filter(lang -> defaultLanguage.getLanguage().equals(lang.getLocale().getLanguage())).findFirst();
+  }
+
+  public void resetTranslation() {
+    translatedText = "";
+  }
+
+  public String getTranslatedText() {
+    return translatedText;
+  }
+
+  public String getWarningText() {
+    return warningText;
   }
 
   public List<Locale> getSupportLanguages() {
