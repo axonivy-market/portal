f6fcb4eb1d36de830dfe6ced2fa48bab0451899b
Loc Nguyen <loc.nguyenthanh@axonactive.com>
2023-08-01 11:08:49 +0700

    IVYPORTAL-15211 - Fix GUI test

--- AxonIvyPortal/portal-components/src/com/axonivy/portal/components/enums/PortalCustomSignature.java
+++ AxonIvyPortal/portal-components/src/com/axonivy/portal/components/enums/PortalCustomSignature.java
@@ -8,7 +8,8 @@ public enum PortalCustomSignature {
   GET_DOCUMENT_LIST("portalGetDocumentList(ch.ivyteam.ivy.workflow.ICase)"),
   UPLOAD_DOCUMENT("portalUploadDocument(ch.ivyteam.ivy.workflow.ICase,org.primefaces.model.file.UploadedFile,Boolean,Boolean,String)"),
   DOWNLOAD_DOCUMENT("portalDownloadDocument(ch.ivyteam.ivy.workflow.ICase,com.axonivy.portal.components.ivydata.bo.IvyDocument)"),
-  DELETE_DOCUMENT("portalDeleteDocument(ch.ivyteam.ivy.workflow.ICase,com.axonivy.portal.components.ivydata.bo.IvyDocument)");
+  DELETE_DOCUMENT("portalDeleteDocument(ch.ivyteam.ivy.workflow.ICase,com.axonivy.portal.components.ivydata.bo.IvyDocument)"),
+  CONFIGURE_ROLES_FOR_GROUP_CHAT("portalConfigureRolesForGroupChat(ch.ivyteam.ivy.workflow.ITask)");
 
   private String signature;
 
