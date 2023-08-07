649f400ebd095ec89ceb32c93f81b5230bf28119
Le Ngoc Danh <127722015+lndanh-axonivy@users.noreply.github.com>
2023-08-07 11:39:17 +0700

    IVYPORTAL-15025 Improve pagination for task list on the dashboard: im… (#210) (#217)
    * IVYPORTAL-15025 Improve pagination for task list on the dashboard: implement
    
    * IVYPORTAL-15025 Improve pagination for task list on the dashboard: handle feedback
    
    * Revert "IVYPORTAL-15025 Improve pagination for task list on the dashboard: handle feedback"
    
    This reverts commit eabd8e60

--- AxonIvyPortal/portal/src/ch/ivy/addon/portalkit/datamodel/DashboardTaskLazyDataModel.java
+++ AxonIvyPortal/portal/src/ch/ivy/addon/portalkit/datamodel/DashboardTaskLazyDataModel.java
@@ -3,81 +3,64 @@ package ch.ivy.addon.portalkit.datamodel;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.CompletableFuture;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
-import org.primefaces.model.SortOrder;
+import org.apache.commons.collections4.map.HashedMap;
+import org.primefaces.model.FilterMeta;
+import org.primefaces.model.SortMeta;
 
 import ch.ivy.addon.portalkit.ivydata.searchcriteria.DashboardTaskSearchCriteria;
 import ch.ivy.addon.portalkit.ivydata.service.impl.DashboardTaskService;
-import ch.ivy.addon.portalkit.service.exception.PortalException;
-import ch.ivyteam.ivy.jsf.primefaces.legazy.LazyDataModel7;
 import ch.ivyteam.ivy.workflow.ITask;
 import ch.ivyteam.ivy.workflow.query.TaskQuery;
-import ch.ivyteam.util.threadcontext.IvyThreadContext;
 
-public class DashboardTaskLazyDataModel extends LazyDataModel7<ITask> {
+public class DashboardTaskLazyDataModel extends LiveScrollLazyModel<ITask> {
 
   private static final long serialVersionUID = -6615871274830927272L;
 
-  private static final int QUERY_PAGES_AT_FIRST_TIME = 5;
-  private static final int QUERY_PAGES = 3;
-
   private DashboardTaskSearchCriteria criteria;
-  private boolean isFirstTime = true;
   private List<ITask> tasks;
-  private CompletableFuture<Void> future;
+  private Map<Long, ITask> mapTasks;
   private TaskQuery query;
+  private int countLoad;
 
   public DashboardTaskLazyDataModel() {
     criteria = new DashboardTaskSearchCriteria();
     tasks = new ArrayList<>();
+    mapTasks = new HashedMap<>();
   }
 
   @Override
-  public List<ITask> load(int first, int pageSize, String sortField, SortOrder sortOrder, Map<String, Object> filters) {
-    if (isFirstTime) {
-      isFirstTime = false;
-      if (future != null) {
-        try {
-          future.get();
-        } catch (Exception e) {
-          throw new PortalException(e);
-        }
+  public List<ITask> load(int first, int pageSize, Map<String, SortMeta> sortBy, Map<String, FilterMeta> filterBy) {
+    if (first == 0) {
+      tasks.clear();
+      Map.Entry<String, SortMeta> sortEntry = sortBy.entrySet().iterator().next();
+      if (sortEntry != null && sortEntry.getValue() != null) {
+        SortMeta sortMeta = sortEntry.getValue();
+        criteria.setSortField(sortMeta.getField());
+        criteria.setSortDescending(sortMeta.getOrder().isDescending());
       }
-    } else {
-      if (first == 0) {
-        criteria.setSortField(sortField);
-        criteria.setSortDescending(sortOrder == SortOrder.DESCENDING);
-        query = criteria.buildQuery();
-      }
-      tasks = DashboardTaskService.getInstance().findByTaskQuery(query, first, pageSize * (first <= pageSize ? QUERY_PAGES_AT_FIRST_TIME : QUERY_PAGES));
+      query = criteria.buildQuery();
     }
-    int rowCount = tasks.size() + first;
-    List<ITask> result = new ArrayList<>();
-    for (int i = 0; i < Math.min(pageSize, tasks.size()); i++) {
-      result.add(tasks.get(i));
+    List<ITask> foundTasks = DashboardTaskService.getInstance().findByTaskQuery(query, first, pageSize);
+    tasks.addAll(foundTasks);
+    mapTasks.putAll(foundTasks.stream().collect(Collectors.toMap(o -> o.getId(), Function.identity())));
+    int rowCount = 0;
+    if (foundTasks.size() >= pageSize) {
+      rowCount = first + pageSize + 1;
+    } else {
+      rowCount = first + foundTasks.size();
     }
     setRowCount(rowCount);
-    return result;
-  }
-
-  public void loadFirstTime() {
-    query = criteria.buildQuery();
-    Object memento = IvyThreadContext.saveToMemento();
-    future = CompletableFuture.runAsync(() -> {
-      IvyThreadContext.restoreFromMemento(memento);
-      tasks = DashboardTaskService.getInstance().findByTaskQuery(query, 0, getPageSize() * QUERY_PAGES_AT_FIRST_TIME);
-      IvyThreadContext.reset();
-    });
-    isFirstTime = true;
+    return foundTasks;
   }
 
   @Override
   public ITask getRowData(String rowKey) {
-    for (ITask task : tasks) {
-      if (task.getId() == Long.valueOf(rowKey)) {
-        return task;
-      }
+    ITask task = mapTasks.get(Long.valueOf(rowKey));
+    if (task != null) {
+      return task;
     }
     return null;
   }
@@ -85,30 +68,6 @@ public class DashboardTaskLazyDataModel extends LazyDataModel7<ITask> {
   @Override
   public String getRowKey(ITask task) {
     return String.valueOf(task.getId());
-  }
-
-  /**
-   * @hidden
-   */
-  @Override
-  public void setRowIndex(int index) {
-    super.setRowIndex(index);
-  }
-
-  /**
-   * @hidden
-   */
-  @Override
-  public ITask getRowData() {
-    return super.getRowData();
-  }
-
-  /**
-   * @hidden
-   */
-  @Override
-  public boolean isRowAvailable() {
-    return super.isRowAvailable();
   }
 
   public DashboardTaskSearchCriteria getCriteria() {
@@ -127,4 +86,21 @@ public class DashboardTaskLazyDataModel extends LazyDataModel7<ITask> {
     criteria.setCanWorkOn(canWorkOn);
   }
 
+  @Override
+  public List<ITask> getResults() {
+    return this.tasks;
+  }
+
+  @Override
+  public int count(Map<String, FilterMeta> filterBy) {
+    return this.getRowCount();
+  }
+  
+  public int getCountLoad() {
+    return countLoad;
+  }
+
+  public void setCountLoad(int countLoad) {
+    this.countLoad = countLoad;
+  }
 }
