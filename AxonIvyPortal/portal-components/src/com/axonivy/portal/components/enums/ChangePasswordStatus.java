947dc285710aef43f2476eb45cd40f9fc0cefedb
Loc Nguyen <loc.nguyenthanh@axonactive.com>
2023-08-11 13:59:50 +0700

    IVYPORTAL-15211 - Fix ChangePassword sub

--- AxonIvyPortal/portal-components/src/com/axonivy/portal/components/enums/ChangePasswordStatus.java
+++ AxonIvyPortal/portal-components/src/com/axonivy/portal/components/enums/ChangePasswordStatus.java
@@ -2,4 +2,10 @@ package com.axonivy.portal.components.enums;
 
 public enum ChangePasswordStatus {
   OK, FAIL;
+  
+  public static ChangePasswordStatus toEnum(String value) {
+    for(ChangePasswordStatus v : values())
+        if(v.toString().equalsIgnoreCase(value)) return v;
+    return FAIL;
+  }
 }
