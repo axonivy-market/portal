28990952416a8efe2109d5bf42231ae780606360
tphvu <vu.tongphanhoang@axonactive.com>
2023-08-16 14:41:44 +0700

    feature/IVYPORTAL-15417-Migrate-Task-Business-State
    - implement migration TaskBusinessState for dashboard

---
+++ AxonIvyPortal/portal/src/com/axonivy/portal/migration/dashboardfilter/converter/v112/DashboardTaskWidgetFilterConverter.java
@@ -0,0 +1,109 @@
+package com.axonivy.portal.migration.dashboardfilter.converter.v112;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+import org.apache.commons.lang3.EnumUtils;
+
+import com.axonivy.portal.bo.jsonversion.AbstractJsonVersion;
+import com.axonivy.portal.bo.jsonversion.DashboardFilterJsonVersion;
+import com.axonivy.portal.migration.common.IJsonConverter;
+import com.axonivy.portal.migration.common.search.JCondition;
+import com.axonivy.portal.migration.common.search.JsonDashboardConfigurationSearch;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.TextNode;
+
+import ch.ivy.addon.portalkit.enums.DashboardStandardTaskColumn;
+import ch.ivy.addon.portalkit.enums.DashboardWidgetType;
+import ch.ivyteam.ivy.workflow.TaskState;
+import ch.ivyteam.ivy.workflow.task.TaskBusinessState;
+
+public class DashboardTaskWidgetFilterConverter implements IJsonConverter{
+  public static final String DASHBOARD_VERSION = "11.2.0";
+  public static final String USER_FILTER_LIST = "userFilterList";
+
+  @Override
+  public AbstractJsonVersion version() {
+    return new DashboardFilterJsonVersion(DASHBOARD_VERSION);
+  }
+
+  @Override
+  public void convert(JsonNode jsonNode) {
+    new JsonDashboardConfigurationSearch(jsonNode)
+      .type(DashboardWidgetType.TASK.name())
+      .findFilterableColumns()
+      .ifPresent(columns -> columns.elements().forEachRemaining(column -> {
+        if(JCondition.isField(DashboardStandardTaskColumn.STATE.getField()).test(column)) {
+          convertToTaskBusinessState(column.get(USER_FILTER_LIST));
+        }
+      }));
+  }
+
+  private void convertToTaskBusinessState(JsonNode statesNode) {
+    List<TextNode> newStates = new ArrayList<>();
+    
+    statesNode.elements().forEachRemaining(node -> {
+      TextNode newState = convertTaskBusinessState(node.asText());
+      if (newState != null) {
+        newStates.add(newState);
+      }
+    });
+    
+    ((ArrayNode) statesNode).removeAll().addAll(newStates.stream().distinct().toList());
+  }
+
+  /**
+   * Adapt task business state for dashboards
+   * IVYPORTAL-14903: Introduce TaskBusinessState
+   * 
+   */
+  private TextNode convertTaskBusinessState(String oldTaskStateString) {
+    TaskState oldTaskState = EnumUtils.getEnum(TaskState.class, oldTaskStateString);
+    TextNode result = new TextNode(oldTaskStateString);
+        
+    if (Objects.nonNull(oldTaskState)) {
+      switch (oldTaskState) {
+        case PARKED:
+        case WAITING_FOR_INTERMEDIATE_EVENT:
+        case SUSPENDED: {
+          result = new TextNode(TaskBusinessState.OPEN.name());
+          break;
+        }
+
+        case CREATED:
+        case RESUMED: {
+          result = new TextNode(TaskBusinessState.IN_PROGRESS.name());
+          break;
+        }
+
+        case DONE:
+        case READY_FOR_JOIN:
+        case JOINING: {
+          result = new TextNode(TaskBusinessState.DONE.name());
+          break;
+        }
+
+        case DESTROYED: {
+          result = new TextNode(TaskBusinessState.DESTROYED.name());
+          break;
+        }
+
+        case DELAYED: {
+          result = new TextNode(TaskBusinessState.DELAYED.name());
+          break;
+        }
+
+        case JOIN_FAILED:
+        case FAILED: {
+          result = new TextNode(TaskBusinessState.ERROR.name());
+          break;
+        }
+
+        default: result = null;
+      };
+    }
+    return result;
+  }
+}
---
+++ AxonIvyPortal/portal/src/com/axonivy/portal/migration/dashboardtemplate/converter/v112/DashboardTemplateTaskWidgetConverter.java
@@ -0,0 +1,112 @@
+package com.axonivy.portal.migration.dashboardtemplate.converter.v112;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+import org.apache.commons.lang3.EnumUtils;
+
+import com.axonivy.portal.bo.jsonversion.AbstractJsonVersion;
+import com.axonivy.portal.bo.jsonversion.DashboardJsonVersion;
+import com.axonivy.portal.migration.common.IJsonConverter;
+import com.axonivy.portal.migration.common.search.JsonWidgetSearch;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.TextNode;
+
+import ch.ivy.addon.portalkit.enums.DashboardStandardTaskColumn;
+import ch.ivy.addon.portalkit.enums.DashboardWidgetType;
+import ch.ivyteam.ivy.workflow.TaskState;
+import ch.ivyteam.ivy.workflow.task.TaskBusinessState;
+
+public class DashboardTemplateTaskWidgetConverter implements IJsonConverter {
+  public static final String DASHBOARD_VERSION = "11.2.0";
+  public static final String FILTER_LIST = "filterList";
+  public static final String FIELD = "field";
+
+  @Override
+  public AbstractJsonVersion version() {
+    return new DashboardJsonVersion(DASHBOARD_VERSION);
+  }
+
+  @Override
+  public void convert(JsonNode jsonNode) {
+      new JsonWidgetSearch(jsonNode)
+      .type(DashboardWidgetType.TASK.name())
+      .findColumns().forEach(columns -> {
+        columns.elements().forEachRemaining(col -> {
+          if (col.get("field").asText().contentEquals(DashboardStandardTaskColumn.STATE.getField())) {
+            convertTaskBusinessStates(col.get("filterList"));
+          }
+        });
+      });
+  }
+
+  private void convertTaskBusinessStates(JsonNode statesNode) {
+    if (Objects.isNull(statesNode)) {
+      return;
+    }
+    List<TextNode> newStates = new ArrayList<>();
+    statesNode.elements().forEachRemaining(node -> {
+      TextNode newState = convertTaskBusinessState(node.asText());
+      if (newState != null) {
+        newStates.add(newState);
+      }
+    });
+    ((ArrayNode) statesNode).removeAll().addAll(newStates.stream().distinct().toList());
+  }
+
+  /**
+   * Adapt task business state for dashboards
+   * IVYPORTAL-14903: Introduce TaskBusinessState
+   * 
+   */
+  private TextNode convertTaskBusinessState(String oldTaskStateString) {
+    TaskState oldTaskState = EnumUtils.getEnum(TaskState.class, oldTaskStateString);
+    TextNode result = new TextNode(oldTaskStateString);
+        
+    if (Objects.nonNull(oldTaskState)) {
+      switch (oldTaskState) {
+        case PARKED:
+        case WAITING_FOR_INTERMEDIATE_EVENT:
+        case SUSPENDED: {
+          result = new TextNode(TaskBusinessState.OPEN.name());
+          break;
+        }
+
+        case CREATED:
+        case RESUMED: {
+          result = new TextNode(TaskBusinessState.IN_PROGRESS.name());
+          break;
+        }
+
+        case DONE:
+        case READY_FOR_JOIN:
+        case JOINING: {
+          result = new TextNode(TaskBusinessState.DONE.name());
+          break;
+        }
+
+        case DESTROYED: {
+          result = new TextNode(TaskBusinessState.DESTROYED.name());
+          break;
+        }
+
+        case DELAYED: {
+          result = new TextNode(TaskBusinessState.DELAYED.name());
+          break;
+        }
+
+        case JOIN_FAILED:
+        case FAILED: {
+          result = new TextNode(TaskBusinessState.ERROR.name());
+          break;
+        }
+
+        default: result = null;
+      };
+    }
+    return result;
+  }
+
+}
