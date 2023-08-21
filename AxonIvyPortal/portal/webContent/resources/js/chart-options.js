6c2c131a22bc355b60c3dc5e1ba33a6a0b8a9a5a
Le Ngoc Danh <danh.lengoc@axonactive.com>
2023-08-21 17:19:53 +0700

    IVYPORTAL-15448  Statistics Part II - Implementation of the first "Chart-Options": implement chart js

--- AxonIvyPortal/portal/webContent/resources/js/chart-options.js
+++ AxonIvyPortal/portal/webContent/resources/js/chart-options.js
@@ -13,161 +13,93 @@ const CHART_COLORS = [
     'hsl(192, 63%, 10%)',
 ];
 
+const intervalObjects = [];
+
+function isNumeric(n) {
+    return !isNaN(parseFloat(n)) && isFinite(n);
+}
+
 $(document).ready(function () {
-    const userActionChart1 = async () => {
-        const chart = document.getElementById('chart1');
+    $('.chart-options').each(async (index, chart) => {
         let chartId = chart.getAttribute('data-chart-id');
         let response = await instance.post('/designer/api/statistic-data-service/Data', {"chartId": chartId});
+
         let data = await response.data;
         let result = data.result.aggs[0].buckets;
-        chart1Object = new Chart(chart, {
-            type: data.chartType,
-            label: data.label,
-            data: {
-                labels: result.map(bucket => bucket.key),
-                datasets: [{
-                    label: data.label,
-                    data: result.map(bucket => bucket.count),
-                    backgroundColor: CHART_COLORS
-                }
-                ]
-            },
-            options: {
-                responsive: true,
-                plugins: {
-                    legend: {
-                        position: 'top',
+        let chartType = data.chartType;
+        let chartObject;
+        if ('number' === chartType) {
+            $(chart).find('.card-number').text(result.map(bucket => bucket.count));
+        }
+        if ('bar' === chartType) {
+            let canvasObject = $(chart).find('canvas');
+            result.map(bucket => console.log((" " + bucket.key + " " + isNumeric((new Date(bucket.key)).getTime()))));
+
+            chartObject = new Chart(canvasObject, {
+                type: chartType,
+                label: data.label,
+                data: {
+                    labels: result.map(bucket => {
+                        if (isNumeric((new Date(bucket.key)).getTime())) {
+                            return bucket.key.substring(0, 10);
+                        }
+                        return bucket.key
+                    }),
+                    datasets: [{
+                        label: data.label,
+                        data: result.map(bucket => bucket.count),
+                        backgroundColor: CHART_COLORS
                     }
+                    ]
                 },
-                scales: {
-                    y: {
-                        beginAtZero: true,
-                        title: {
-                            text: data.barChartConfig.yTitle,
-                            display: true
+                options: {
+                    responsive: true,
+                    plugins: {
+                        legend: {
+                            position: 'top',
                         }
                     },
-                    x: {
-                        title: {
-                            text: data.barChartConfig.xTitle,
-                            display: true
+                    scales: {
+                        y: {
+                            beginAtZero: true,
+                            title: {
+                                text: data.barChartConfig.yTitle,
+                                display: true
+                            }
+                        },
+                        x: {
+                            title: {
+                                text: data.barChartConfig.xTitle,
+                                display: true
+                            }
                         }
                     }
                 }
-            }
-        });
-    }
+            });
+
+        }
+        if (chartObject !== undefined) {
+            intervalObjects.push({'chartObject': chartObject, 'chartType': chartType, 'chartId': chartId})
+        }
+
+    });
 
     setInterval(() => {
-        if (typeof chart1Object != "undefined") {
-            userActionChartNew(chart1Object);
+        for (let i = 0; i < intervalObjects.length; i++) {
+            let intervalObject = intervalObjects[i];
+            if (typeof intervalObject !== "undefined") {
+                userActionChartNew(intervalObject.chartObject, intervalObject.chartId);
+            }
         }
     }, 10000);
 
-    const userActionChartNew = async (chartObject) => {
-        const response = await instance.post('/designer/api/statistic-data-service/Data', {"chartId": "3"});
-        const result = await response.data.aggs[0].buckets;
+    const userActionChartNew = async (chartObject, chartId) => {
+        const response = await instance.post('/designer/api/statistic-data-service/Data', {"chartId": chartId});
+        const result = response.data.result.aggs[0].buckets;
         chartObject.data.labels = result.map(bucket => bucket.key);
         chartObject.data.datasets.forEach(dataset => {
             dataset.data = result.map(bucket => bucket.count);
-          });
+        });
         chartObject.update();
     }
-
-    const userActionChart2 = async () => {
-        const chart = document.getElementById('chart2');
-        let chartId = chart.getAttribute('data-chart-id');
-        let response = await instance.post('/designer/api/statistic-data-service/Data', {"chartId": chartId});
-        let data = await response.data;
-        let result = data.result.aggs[0].buckets;
-        new Chart(chart, {
-            type: data.chartType,
-            data: {
-                labels: result.map(bucket => bucket.key.substring(0, 10)),
-                datasets: [{
-                    label: data.label,
-                    data: result.map(bucket => bucket.count),
-                    backgroundColor: CHART_COLORS
-                }
-                ]
-            },
-            options: {
-                responsive: true,
-                plugins: {
-                    legend: {
-                        position: 'top',
-                    }
-                },
-                scales: {
-                    y: {
-                        beginAtZero: true,
-                        title: {
-                            text: data.barChartConfig.yTitle,
-                            display: true
-                        }
-                    },
-                    x: {
-                        title: {
-                            text: data.barChartConfig.xTitle,
-                            display: true
-                        }
-                    }
-                }
-            }
-        });
-    }
-
-    const userActionChart3 = async () => {
-        const chart = document.getElementById('chart3');
-        let chartId = chart.getAttribute('data-chart-id');
-        let response = await instance.post('/designer/api/statistic-data-service/Data', {"chartId": chartId});
-        let data = await response.data;
-        let result = data.result.aggs[0].buckets;
-        $('#card-number').text(result.map(bucket => bucket.count));
-    }
-
-    const userActionChart4 = async () => {
-        const chart = document.getElementById('chart4');
-        let chartId = chart.getAttribute('data-chart-id');
-        let response = await instance.post('/designer/api/statistic-data-service/Data', {"chartId": chartId});
-        let data = await response.data;
-        let result = data.result.aggs[0].buckets;
-
-        new Chart(chart, {
-            label: data.label,
-            type: data.chartType,
-            data: {
-                labels: result.map(bucket => bucket.key.substring(0, 10)),
-                datasets: [{
-                    label: 'DONE',
-                    borderWidth: 1,
-                    backgroundColor: CHART_COLORS,
-                    data: result.map(bucket => bucket.count)
-                }
-                ]
-            },
-            options: {
-                scales: {
-                    y: {
-                        beginAtZero: true,
-                        title: {
-                            text: data.barChartConfig.yTitle,
-                            display: true
-                        }
-                    },
-                    x: {
-                        title: {
-                            text: data.barChartConfig.xTitle,
-                            display: true
-                        }
-                    }
-                }
-            }
-        });
-    }
-
-    userActionChart1();
-    userActionChart2();
-    userActionChart3();
-    userActionChart4();
 });
