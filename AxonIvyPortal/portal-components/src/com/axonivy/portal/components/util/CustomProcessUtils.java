f3ae8b39e036748c4b8dd2906274e07210fc6f06
nam.mai <nam.mai@axonactive.com>
2023-08-23 15:14:04 +0700

    IVYPORTAL-15489: Custom Task Delegate & Document processes Customization Support - Handled feedback: Create new CustomProcessStatus

--- AxonIvyPortal/portal-components/src/com/axonivy/portal/components/util/CustomProcessUtils.java
+++ AxonIvyPortal/portal-components/src/com/axonivy/portal/components/util/CustomProcessUtils.java
@@ -2,18 +2,17 @@ package com.axonivy.portal.components.util;
 
 import java.util.Map;
 import java.util.Objects;
-import java.util.Optional;
+
+import com.axonivy.portal.components.enums.CustomProcessStatus;
 
 public class CustomProcessUtils {
 
   private static final String STATUS = "status";
-  public static final String SKIP_STATUS = "skip";
 
   public static boolean isSkipCustomProcess(Map<String, Object> respose) {
     if (Objects.isNull(respose)) {
       return true;
     }
-    String status = (String)respose.get(STATUS);
-    return Optional.ofNullable(status).orElse("").contentEquals(SKIP_STATUS);
+    return CustomProcessStatus.toEnum((String)respose.get(STATUS)) == CustomProcessStatus.SKIP;
   }
 }
