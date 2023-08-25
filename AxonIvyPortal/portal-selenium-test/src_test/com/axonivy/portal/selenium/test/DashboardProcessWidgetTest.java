335ded3eb90f095582159d81e8390d4d23d36b34
Thinh Nguyen <thinh.nguyenhung@axonactive.com>
2023-08-25 15:21:47 +0700

    feature/IVYPORTAL-15289-Process-Order-in-Process-start-Widget-on-Dashboard-release-10

--- AxonIvyPortal/portal-selenium-test/src_test/com/axonivy/portal/selenium/test/DashboardProcessWidgetTest.java
+++ AxonIvyPortal/portal-selenium-test/src_test/com/axonivy/portal/selenium/test/DashboardProcessWidgetTest.java
@@ -5,7 +5,6 @@ import static com.codeborne.selenide.CollectionCondition.size;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import com.axonivy.ivy.webtest.IvyWebTest;
 import com.axonivy.portal.selenium.common.BaseTest;
 import com.axonivy.portal.selenium.common.TestAccount;
 import com.axonivy.portal.selenium.common.Variable;
@@ -18,7 +17,6 @@ import com.axonivy.portal.selenium.page.TaskDetailsPage;
 import com.axonivy.portal.selenium.page.TaskTemplatePage;
 import com.codeborne.selenide.Condition;
 
-@IvyWebTest(headless = false)
 public class DashboardProcessWidgetTest extends BaseTest {
   private static final String EXPRESS_PROCESS = "EXPRESS_PROCESS";
   private static final String CASE_LEAVE_REQUEST_TEST_FOR_IVYPORTAL_3369 =
