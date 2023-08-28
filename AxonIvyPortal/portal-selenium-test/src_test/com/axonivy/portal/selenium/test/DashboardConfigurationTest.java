2120031942ba5d86d6a8066cd3a8ca3c9d2cbce9
Loc Nguyen <121596218+ntloc-axonivy@users.noreply.github.com>
2023-08-28 11:27:40 +0700

    Feature/ivyportal 15453 dashboard direct link support 10 (#249)
    * IVYPORTAL-15453 - Implement logic
    
    * IVYPORTAL-15453 - update UI
    
    * IVYPORTAL-15453 - Update logic, fix highlight menu
    
    * IVYPORTAL-15453 - Update cms
    
    * iVYPORTAL-15453 - Update cms
    
    * IVYPORTAL-15453 - Add permission and openInFrame option
    
    * IVYPORTAL-15453 - Add gui test
    
    * IVYPORTAL-15308 - update test
    
    * IVYPORTAL-15453 - Update ui
    
    * IVYPORTAL-15453 - Update screenshot + refactor share dialog
    
    * Handle feedback
    
    * IVYPORTAL-15453 - Handle feedback
    
    * IVYPORTAL-15453 - Update jenkins
    
    * IVYPORTAL-15453 - Fix guitest
    
    * IVYPORTAL-15453 - Handle feedback

--- AxonIvyPortal/portal-selenium-test/src_test/com/axonivy/portal/selenium/test/DashboardConfigurationTest.java
+++ AxonIvyPortal/portal-selenium-test/src_test/com/axonivy/portal/selenium/test/DashboardConfigurationTest.java
@@ -436,4 +436,25 @@ public class DashboardConfigurationTest extends BaseTest {
     
     configurationPage.saveImportDashboard(name, newGermanName, description, icon, permissions);
   }
+  
+  @Test
+  public void testVisibleSharingDashboardButton() {
+    redirectToRelativeLink(denyDashboardShareLinkPermissionUrl);
+    LinkNavigator.redirectToPortalDashboardConfiguration();
+    DashboardModificationPage modificationPage = navigateToConfigurationAndEditDashboards(true);
+    modificationPage.getDashboardShareLinkButton().shouldBe(Condition.disappear);
+    
+    redirectToRelativeLink(grantDashboardShareLinkPermissionUrl);
+    refreshPage();
+    DashboardModificationPage modificationPage2 = navigateToConfigurationAndEditDashboards(true);
+    modificationPage2.getDashboardShareLinkButton().shouldBe(Condition.appear);
+  }
+  
+  @Test
+  public void testSharingDashboard() {
+    redirectToRelativeLink(grantDashboardShareLinkPermissionUrl);
+    LinkNavigator.redirectToPortalDashboardConfiguration();
+    DashboardModificationPage modificationPage = navigateToConfigurationAndEditDashboards(true);
+    modificationPage.getDashboardShareLinkDialog();
+  }
 }
