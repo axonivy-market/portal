403789a27f77fd19a7f4849dfa24cb59993e3d17
Thinh Nguyen <thinh.nguyenhung@axonactive.com>
2023-08-29 09:20:53 +0700

    feature/IVYPORTAL-15289-Process-Order-in-Process-start-Widget-on-Dashboard-release-10 _ Handle Feedback

--- AxonIvyPortal/portal/src/ch/ivy/addon/portalkit/enums/ProcessSorting.java
+++ AxonIvyPortal/portal/src/ch/ivy/addon/portalkit/enums/ProcessSorting.java
@@ -5,17 +5,7 @@ import org.apache.commons.lang3.StringUtils;
 import ch.ivyteam.ivy.environment.Ivy;
 
 public enum ProcessSorting {
-  BY_ALPHABETICALLY("Alphabetically"), BY_INDEX("SortingIndex");
-
-  private final String value;
-
-  private ProcessSorting(String value) {
-    this.value = value;
-  }
-
-  public String getValue() {
-    return value;
-  }
+  BY_ALPHABETICALLY, BY_INDEX;
 
   public String getLabel() {
     String label = Ivy.cms().co("/ch.ivy.addon.portalkit.ui.jsf/Enums/ProcessSorting/" + name());
