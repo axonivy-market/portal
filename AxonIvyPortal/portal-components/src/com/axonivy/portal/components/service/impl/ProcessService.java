4187242bdece41935ee80b27c698d04e42206e94
Thinh Nguyen <thinh.nguyenhung@axonactive.com>
2023-09-07 15:54:34 +0700

    feature/IVYPORTAL-15621-Process-path-in-configuration-supports-endWiths-release10 _ fix process filter with friendly request path

--- AxonIvyPortal/portal-components/src/com/axonivy/portal/components/service/impl/ProcessService.java
+++ AxonIvyPortal/portal-components/src/com/axonivy/portal/components/service/impl/ProcessService.java
@@ -124,7 +124,7 @@ public class ProcessService implements IProcessService {
   }
   
   public IWebStartable findCustomDashboardProcessInSecurityContextByProcessId(String processId) {
-    Predicate<? super IWebStartable> predicate = startable -> StringUtils.equals(processId, startable.getId()) && 
+    Predicate<? super IWebStartable> predicate = startable -> StringUtils.endsWith(startable.getId(),processId) && 
         BooleanUtils.toBoolean(startable.customFields().value(IS_DASHBOARD_PROCESS));
     return findStartable(predicate);
   }
