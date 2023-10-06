a8adaaf1c8b1e3c51451fabd9f6f856d4abb4627
Nam.Chu <nam.chu@axonactive.com>
2023-10-06 16:16:07 +0700

    IVYPORTAL-15826 Remove legacy feature "add language" from Portal 8 - Implement for 10

--- AxonIvyPortal/portal-components/src/com/axonivy/portal/components/util/Locales.java
+++ AxonIvyPortal/portal-components/src/com/axonivy/portal/components/util/Locales.java
@@ -5,17 +5,16 @@ import java.util.Locale;
 import ch.ivyteam.ivy.environment.Ivy;
 import ch.ivyteam.ivy.language.LanguageConfigurator;
 import ch.ivyteam.ivy.security.ISecurityContext;
-import ch.ivyteam.ivy.security.IUser;
+import ch.ivyteam.ivy.security.exec.Sudo;
 
 public class Locales {
-    public Locale getCurrentLocale() {
-        return IvyExecutor.executeAsSystem(() -> {
-            IUser user = Ivy.session().getSessionUser();
-            Locale userLanguage = user.getLanguage();
-            if (userLanguage != null) {
-                return userLanguage;
-            }
-            return new LanguageConfigurator(ISecurityContext.current()).content();
-        });
-    }
+  public Locale getCurrentLocale() {
+    return Sudo.get(() -> {
+      Locale userLanguage = Ivy.session().getSessionUser().getLanguage();
+      if (userLanguage != null) {
+        return userLanguage;
+      }
+      return new LanguageConfigurator(ISecurityContext.current()).content();
+    });
+  }
 }
