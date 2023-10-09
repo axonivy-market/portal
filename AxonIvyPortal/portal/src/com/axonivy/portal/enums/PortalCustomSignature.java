985d82be582e8f8e55c24e79e751c011e43c19bc
Le Ngoc Danh <127722015+lndanh-axonivy@users.noreply.github.com>
2023-10-09 18:13:44 +0700

    IVYPORTAL-15487 Overwrite HTML Dialog Customization Support: implement (#296)
    * IVYPORTAL-15487 Overwrite HTML Dialog Customization Support: implement and remove override dialog
    
    * IVYPORTAL-15487 Overwrite HTML Dialog Customization Support: update Jenkinsfile
    
    * IVYPORTAL-15487 Overwrite HTML Dialog Customization Support: update testcase
    
    * IVYPORTAL-15487 Overwrite HTML Dialog Customization Support: handle feedback
    
    * IVYPORTAL-15487 Overwrite HTML Dialog Customization Support: handle feedback
    
    * IVYPORTAL-15487 Overwrite HTML Dialog Customization Support: handle feedback
    
    * IVYPORTAL-15487 Overwrite HTML Dialog Customization Support: handle feedback
    
    * IVYPORTAL-15487 Overwrite HTML Dialog Customization Support: handle feedback

--- AxonIvyPortal/portal/src/com/axonivy/portal/enums/PortalCustomSignature.java
+++ AxonIvyPortal/portal/src/com/axonivy/portal/enums/PortalCustomSignature.java
@@ -11,6 +11,7 @@ public enum PortalCustomSignature {
   DOWNLOAD_DOCUMENT("portalDownloadDocument(ch.ivyteam.ivy.workflow.ICase,com.axonivy.portal.components.ivydata.bo.IvyDocument)"),
   DELETE_DOCUMENT("portalDeleteDocument(ch.ivyteam.ivy.workflow.ICase,com.axonivy.portal.components.ivydata.bo.IvyDocument)"),
   DELEGATE("portalCalculateTaskDelegate(java.util.List<com.axonivy.portal.components.dto.RoleDTO>,java.util.List<com.axonivy.portal.components.dto.UserDTO>,com.axonivy.portal.components.dto.SecurityMemberDTO,ch.ivyteam.ivy.workflow.ITask)"),
+  GET_PAGE_HEADER_AND_FOOTER("portalGetPageHeaderAndFooter()"),
   LOAD_SUB_MENU_ITEMS("portalLoadSubMenuItems()");
 
   private String signature;
