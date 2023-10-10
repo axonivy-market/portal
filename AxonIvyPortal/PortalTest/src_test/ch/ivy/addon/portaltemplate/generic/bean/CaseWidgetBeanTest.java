0e21c8e10ec6b355233146727b2be0a22819282d
Loc Nguyen <loc.nguyenthanh@axonactive.com>
2023-10-10 16:25:50 +0700

    IVYPORTAL-15734 - Fix fail GUI test

--- AxonIvyPortal/PortalTest/src_test/ch/ivy/addon/portaltemplate/generic/bean/CaseWidgetBeanTest.java
+++ AxonIvyPortal/PortalTest/src_test/ch/ivy/addon/portaltemplate/generic/bean/CaseWidgetBeanTest.java
@@ -3,11 +3,9 @@ package ch.ivy.addon.portaltemplate.generic.bean;
 import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
 
-import org.apache.commons.lang3.StringUtils;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.ArgumentMatchers;
 import org.mockito.Mock;
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
@@ -15,7 +13,6 @@ import org.powermock.modules.junit4.PowerMockRunner;
 
 import ch.ivy.addon.portalkit.bean.CaseWidgetBean;
 import ch.ivy.addon.portalkit.enums.AdditionalProperty;
-import ch.ivy.addon.portalkit.util.CaseUtils;
 import ch.ivy.addon.portalkit.util.PermissionUtils;
 import ch.ivyteam.ivy.workflow.ICase;
 import ch.ivyteam.ivy.workflow.custom.field.ICustomFields;
@@ -24,7 +21,6 @@ import ch.ivyteam.ivy.workflow.custom.field.ICustomStringField;
 @RunWith(PowerMockRunner.class)
 public class CaseWidgetBeanTest {
   private static final String CUSTOMIZATION_ADDITIONAL_CASE_DETAILS_PAGE = "CustomizationAdditionalCaseDetailsPage";
-  private static final String DEFAULT_ADDITIONAL_CASE_DETAILS_PAGE = "DefaultAdditionalCaseDetailsPage";
 
   CaseWidgetBean caseWidgetBean;
   
@@ -39,23 +35,7 @@ public class CaseWidgetBeanTest {
     PowerMockito.mockStatic(PermissionUtils.class);
     caseWidgetBean = new CaseWidgetBean();
   }
-  
-  @SuppressWarnings("deprecation")
-  @Test
-  @PrepareForTest({CaseWidgetBean.class, CaseUtils.class, PermissionUtils.class, FacesContext.class})
-  public void testGetAdditionalCaseDetailsPageUri() throws Exception {
-    PowerMockito.mockStatic(CaseUtils.class);
-    mockCustomFieldForCaseDetailsPage(StringUtils.EMPTY);
-    PowerMockito.when(CaseUtils.getProcessStartUriWithCaseParameters(ArgumentMatchers.any(ICase.class), ArgumentMatchers.anyString())).thenReturn(DEFAULT_ADDITIONAL_CASE_DETAILS_PAGE);
-    
-    PowerMockito.mockStatic(FacesContext.class);
 
-    PowerMockito.when(FacesContext.getCurrentInstance()).thenReturn(facesContext);
-    PowerMockito.when(facesContext.getExternalContext())
-        .thenReturn(externalContext);
-    PowerMockito.when(externalContext.getApplicationContextPath()).thenReturn("");
-  }
-  
   @Test
   @PrepareForTest({CaseWidgetBean.class, PermissionUtils.class, FacesContext.class})
   public void testGetAdditionalCaseDetailsPageUriWithCustomization() throws Exception {
