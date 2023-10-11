2af606ed01c03a396e89c932b0f121335dcf8c48
nam.mai <nam.mai@axonactive.com>
2023-10-11 10:27:57 +0700

    IVYPORTAL-15487: Overwrite HTML Dialog Customization Support - Fixed failed gui test

--- AxonIvyPortal/PortalTest/src_test/portal/guitest/test/PortalExpressTest.java
+++ AxonIvyPortal/PortalTest/src_test/portal/guitest/test/PortalExpressTest.java
@@ -56,6 +56,7 @@ public class PortalExpressTest extends BaseTest {
 	public void setup() {
 		super.setup();
 		redirectToRelativeLink("portalKitTestHelper/14DE09882B540AD5/grantPortalPermission.ivp");
+		redirectToRelativeLink(HomePage.PORTAL_HOME_PAGE_URL);
 		homePage = new HomePage();
 	}
 
