0eb181f13e4c8082b1c64a1a14ec4ea132b3dbe9
Le Ngoc Danh <danh.lengoc@axonactive.com>
2023-10-16 14:00:16 +0700

    IVYPORTAL-15561 Refactor portal-test (gui-tests) to selenide 1/3: refactor absence test

--- AxonIvyPortal/portal-selenium-test/src_test/com/axonivy/portal/selenium/page/TemplatePage.java
+++ AxonIvyPortal/portal-selenium-test/src_test/com/axonivy/portal/selenium/page/TemplatePage.java
@@ -9,6 +9,7 @@ import static com.codeborne.selenide.Selenide.$;
 
 import java.util.ArrayList;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -17,9 +18,9 @@ import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
 import com.codeborne.selenide.WebDriverRunner;
 
-
 public abstract class TemplatePage extends AbstractPage {
   protected static final String LAYOUT_WRAPPER = ".layout-wrapper";
+  public static final String ID_PROPERTY = "id";
 
   // If page load more than 45s, mark it failed by timeout
   protected long getTimeOutForLocator() {
@@ -55,6 +56,14 @@ public abstract class TemplatePage extends AbstractPage {
     $("div[id='portal-global-growl_container']").shouldBe(appear, DEFAULT_TIMEOUT).$("div.ui-growl-message").hover();
   }
   
+  public SelenideElement waitForElementClickable(String cssSelector) {
+    return $(cssSelector).shouldBe(appear, DEFAULT_TIMEOUT).shouldBe(getClickableCondition(), DEFAULT_TIMEOUT);
+  }
+
+  public void waitForElementClickableThenClick(String cssSelector) {
+    waitForElementClickable(cssSelector).click();
+  }
+
   public void switchToIFrameOfTask() {
     switchToIframeWithId("iFrame");
   }
@@ -68,5 +77,20 @@ public abstract class TemplatePage extends AbstractPage {
     $("[id='logout-setting:logout-menu-item']").shouldBe(appear, DEFAULT_TIMEOUT);
     WaitHelper.waitForNavigationToLoginPage(() -> $("[id='logout-setting:logout-menu-item']").shouldBe(getClickableCondition(), DEFAULT_TIMEOUT).click());
     return new LoginPage();
+  }
+
+  public AbsencePage openAbsencePage() {
+    clickUserMenuItem("absence-menu-item");
+    return new AbsencePage();
+  }
+
+  private void clickUserMenuItem(String menuItemSelector) {
+    $(By.id("user-settings-menu")).shouldBe(appear, DEFAULT_TIMEOUT);
+    clickByJavaScript($(By.id("user-settings-menu")));
+    $(By.id(menuItemSelector)).shouldBe(appear, DEFAULT_TIMEOUT);
+    clickByJavaScript($(By.id(menuItemSelector)));
+    $(By.id("user-setting-container")).shouldBe(disappear, DEFAULT_TIMEOUT);
+//    WaitHelper.assertTrueWithWait(() -> !findElementById("user-setting-container").isDisplayed());
+
   }
 }
