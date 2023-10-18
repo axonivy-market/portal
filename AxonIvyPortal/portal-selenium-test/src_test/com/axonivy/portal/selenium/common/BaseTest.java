2c5027f536f127fac714f84be64269a9f9a61760
Loc Nguyen <loc.nguyenthanh@axonactive.com>
2023-10-18 16:45:10 +0700

    IVYPORTAL-15915 - Retry login

--- AxonIvyPortal/portal-selenium-test/src_test/com/axonivy/portal/selenium/common/BaseTest.java
+++ AxonIvyPortal/portal-selenium-test/src_test/com/axonivy/portal/selenium/common/BaseTest.java
@@ -12,7 +12,6 @@ import java.time.Duration;
 import org.junit.jupiter.api.AfterEach;
 
 import com.axonivy.ivy.webtest.engine.EngineUrl;
-import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.WebDriverRunner;
 
 import ch.ivy.addon.portalkit.enums.PortalPermission;
@@ -216,9 +215,11 @@ public class BaseTest {
     try {
       username = URLEncoder.encode(testAccount.getUsername(), "UTF-8");
       password = URLEncoder.encode(testAccount.getPassword(), "UTF-8");
-      open(EngineUrl.createProcessUrl(String.format(LOGIN_URL_PATTERN, username, password)));
       try {
-        $(".js-dashboard__wrapper").shouldBe(Condition.exist);
+        WaitHelper.waitForNavigation(() -> open(EngineUrl.createProcessUrl(String.format(LOGIN_URL_PATTERN, username, password))));
+        if (!$(".layout-topbar-right").exists()) {
+          throw new Exception();
+        }
       } catch (Exception e) {
         open(EngineUrl.createProcessUrl(String.format(LOGIN_URL_PATTERN, username, password)));
       }
