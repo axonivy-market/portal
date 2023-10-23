395cd1f0eee574a1034ae6a234b30d8d641be639
Le Ngoc Danh <danh.lengoc@axonactive.com>
2023-10-23 09:26:30 +0700

    IVYPORTAL-15561 Refactor portal-test (gui-tests) to selenide 1/3: refactor tests

---
+++ AxonIvyPortal/portal-selenium-test/src_test/com/axonivy/portal/selenium/page/ChangePasswordPage.java
@@ -0,0 +1,58 @@
+package com.axonivy.portal.selenium.page;
+
+import static com.codeborne.selenide.Selenide.$;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import com.axonivy.portal.selenium.common.Sleeper;
+import com.codeborne.selenide.SelenideElement;
+public class ChangePasswordPage extends TemplatePage {
+
+  
+  @Override
+  protected String getLoadedLocator() {
+    return "[id='change-password-dialog_title']";
+  }
+
+  @SuppressWarnings("deprecation")
+  public void changePassword(String currentPassword, String newPassword) {
+    inputCurrentPassword(currentPassword);
+    inputConfirmNewPassword(newPassword);
+    inputNewPassword(newPassword);
+    waitForElementClickableThenClick($(By.id(("change-password-form:password-setting:save-password-setting"))));
+    waitAjaxIndicatorDisappear();
+  }
+  
+  private void inputCurrentPassword(String currentPassword) {
+    inputField("change-password-form:password-setting:current-password", currentPassword);
+  }
+  
+  private void inputNewPassword(String newPassword) {
+    inputField("change-password-form:password-setting:new-password", newPassword);
+  }
+  
+  private void inputConfirmNewPassword(String newPassword) {
+    inputField("change-password-form:password-setting:confirm-new-password", newPassword);
+  }
+  
+  private void inputField(String inputId, String value) {
+    SelenideElement inputField = findElementById(inputId);
+    inputField.sendKeys(value);
+  }
+  
+  public boolean isWrongCurrentPasswordError() {
+    SelenideElement element = findElementByCssSelector("#change-password-form\\:password-setting\\:change-password-messages .ui-messages-error-detail");
+    return element.getText().equalsIgnoreCase("Authentication failed, your password seems to be wrong!");
+  }
+  
+  public boolean isNewPasswordNotStrongEnough() {
+    SelenideElement element = findElementByCssSelector("#change-password-form\\:password-setting\\:change-password-messages .ui-messages-error-detail");
+    return element.getText().equalsIgnoreCase("Password must be at least 4 characters long, contain at least 1 lowercase character, contain at least 1 uppercase character, contain at least 1 number, contain at least 1 special character.");
+  }
+  
+  public WebElement getChangePasswordDialog() {
+    Sleeper.sleep(200);//sleep a bit to make focus field effect before taking the screenshot
+    return findElementById("change-password-dialog");
+  }
+}
