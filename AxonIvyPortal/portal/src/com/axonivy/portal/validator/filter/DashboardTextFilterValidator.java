4a40d484ebb6406497f8f737474e9d29a1d67b90
nam.mai <nam.mai@axonactive.com>
2023-10-31 16:39:30 +0700

    IVYPORTAL-15544: Case Widget - Complex Standard Date Fields & Standard String Fields - Fixed error when edit task widget

--- AxonIvyPortal/portal/src/com/axonivy/portal/validator/filter/DashboardTextFilterValidator.java
+++ AxonIvyPortal/portal/src/com/axonivy/portal/validator/filter/DashboardTextFilterValidator.java
@@ -1,6 +1,6 @@
 package com.axonivy.portal.validator.filter;
 
-import java.util.Date;
+import java.util.ArrayList;
 import java.util.Optional;
 
 import javax.faces.application.FacesMessage;
@@ -10,6 +10,8 @@ import javax.faces.context.FacesContext;
 import javax.faces.validator.FacesValidator;
 import javax.faces.validator.Validator;
 import javax.faces.validator.ValidatorException;
+
+import org.apache.commons.collections4.CollectionUtils;
 
 import com.axonivy.portal.dto.dashboard.filter.DashboardFilter;
 
@@ -21,16 +23,17 @@ public class DashboardTextFilterValidator implements Validator {
 
   private static final String MESSAGE_PREFIX_PATTERN = "%s(%d)";
 
+  @SuppressWarnings("unchecked")
   public void validate(FacesContext context, UIComponent component, Object value) throws ValidatorException {
   
     DashboardFilter filter = (DashboardFilter) component.getAttributes().get("filter");
     Integer filterIndex = Optional.ofNullable((Integer)component.getAttributes().get("filterIndex")).orElse(0);
     String messageComponentId = Optional.ofNullable((String)component.getAttributes().get("messageId")).orElse(null);
-    validateDefaultOperator((Date)value, filter, filterIndex, component, messageComponentId);
+    validateDefaultOperator((ArrayList<String>)value, filter, filterIndex, component, messageComponentId);
   }
 
-  private void validateDefaultOperator(Date value, DashboardFilter filter, int filterIndex, UIComponent component, String messageComponentId) {
-    if (value == null) {
+  private void validateDefaultOperator(ArrayList<String> value, DashboardFilter filter, int filterIndex, UIComponent component, String messageComponentId) {
+    if (CollectionUtils.isEmpty(value)) {
       FacesContext.getCurrentInstance().addMessage(
           messageComponentId,
           new FacesMessage(FacesMessage.SEVERITY_ERROR, getRequiredMessage(filter.getField(), filterIndex), null));
