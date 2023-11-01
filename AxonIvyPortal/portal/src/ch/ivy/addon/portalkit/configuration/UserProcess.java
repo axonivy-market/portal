bde5ac1a1ae8c99b5af7ad1098aa0ccd28166122
Nam.Chu <nam.chu@axonactive.com>
2023-11-01 14:13:16 +0700

    IVYPORTAL-15611 Remove Express related code out of Portal-component - Fix compile error

--- AxonIvyPortal/portal/src/ch/ivy/addon/portalkit/configuration/UserProcess.java
+++ AxonIvyPortal/portal/src/ch/ivy/addon/portalkit/configuration/UserProcess.java
@@ -6,10 +6,11 @@ import java.util.Locale;
 import org.apache.commons.collections4.CollectionUtils;
 import org.apache.commons.lang3.StringUtils;
 
+import com.axonivy.portal.components.util.Locales;
+
 import ch.ivy.addon.portalkit.constant.DashboardConfigurationPrefix;
 import ch.ivy.addon.portalkit.dto.DisplayName;
 import ch.ivy.addon.portalkit.enums.ProcessType;
-import ch.ivy.addon.portalkit.util.Locales;
 import ch.ivyteam.ivy.environment.Ivy;
 
 public class UserProcess {
