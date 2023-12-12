0c11fa4905de042fd6a8d8fd256743922609f023
Le Ngoc Danh <127722015+lndanh-axonivy@users.noreply.github.com>
2023-12-12 14:12:32 +0700

    feature/IVYPORTAL-15491-Quick-search-on-process,-task-and-case-widget-implement (#413)
    * IVYPORTAL-15093 Prototype quick Global Search for Processes: implement new search bar for quick global search
    
    * IVYPORTAL-15093 Prototype quick Global Search for Processes: implement new search bar for quick global search
    
    * IVYPORTAL-15093 Prototype quick Global Search for Processes: implement new search bar for quick global search
    
    * IVYPORTAL-15093 Prototype quick Global Search for Processes: handle feedback
    
    * IVYPORTAL-15093 Prototype quick Global Search for Processes: handle feedback
    
    * IVYPORTAL-15491 Quick search on process, task and case widget: implement
    
    * IVYPORTAL-15491 Quick search on process, task and case widget: implement
    
    * IVYPORTAL-15491 Quick search on process, task and case widget
    - Remove comment
    
    * IVYPORTAL-15491 Quick search on process, task and case widget: handle feedback
    
    * IVYPORTAL-15491 Quick search on process, task and case widget: handle feedback
    
    * IVYPORTAL-15491 Quick search on process, task and case widget: handle feedback
    
    ---------
    
    Co-authored-by: Nam.Chu <nam.chu@axonactive.com>

---
+++ AxonIvyPortal/portal/src/com/axonivy/portal/service/GlobalSearchService.java
@@ -0,0 +1,105 @@
+package com.axonivy.portal.service;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+import org.apache.commons.lang3.StringUtils;
+
+import com.axonivy.portal.components.service.impl.ProcessService;
+import com.axonivy.portal.enums.SearchScopeCaseField;
+import com.axonivy.portal.enums.SearchScopeTaskField;
+import com.axonivy.portal.payload.SearchPayload;
+import com.axonivy.portal.response.CaseData;
+import com.axonivy.portal.response.GlobalSearchResponse;
+import com.axonivy.portal.response.ProcessData;
+import com.axonivy.portal.response.TaskData;
+
+import ch.ivy.addon.portalkit.enums.GlobalVariable;
+import ch.ivy.addon.portalkit.enums.TaskSortField;
+import ch.ivy.addon.portalkit.ivydata.dto.IvyCaseResultDTO;
+import ch.ivy.addon.portalkit.ivydata.dto.IvyTaskResultDTO;
+import ch.ivy.addon.portalkit.ivydata.searchcriteria.CaseSearchCriteria;
+import ch.ivy.addon.portalkit.ivydata.searchcriteria.TaskSearchCriteria;
+import ch.ivy.addon.portalkit.ivydata.service.impl.CaseService;
+import ch.ivy.addon.portalkit.ivydata.service.impl.TaskService;
+import ch.ivyteam.ivy.environment.Ivy;
+import ch.ivyteam.ivy.workflow.start.IWebStartable;
+
+public class GlobalSearchService {
+
+  private static GlobalSearchService instance;
+  private static final int PAGE_SIZE = 3;
+
+  public static GlobalSearchService getInstance() {
+    if (instance == null) {
+      instance = new GlobalSearchService();
+    }
+    return GlobalSearchService.instance;
+  }
+
+  public GlobalSearchResponse searchTasks(SearchPayload payload) {
+    TaskSearchCriteria query = new TaskSearchCriteria();
+    query.setKeyword(payload.getQuery());
+    query.setSortField(TaskSortField.EXPIRY_TIME.toString());
+    query.setSortDescending(true);
+    query.setQuickGlobalSearch(true);
+    query.setSearchScopeTaskFields(getSearchScopeTaskFields());
+    IvyTaskResultDTO iTasks = TaskService.newInstance().findGlobalSearchTasksByCriteria(query, 0, PAGE_SIZE);
+    List<TaskData> results = iTasks.getTasks().stream().map(TaskData::new).toList();
+    return new GlobalSearchResponse(results, iTasks.getTotalTasks());
+  }
+
+  public GlobalSearchResponse searchCases(SearchPayload payload) {
+    CaseSearchCriteria query = new CaseSearchCriteria();
+    query.setKeyword(payload.getQuery());
+    query.setSearchScopeCaseFields(getSearchScopeCaseFields());
+    IvyCaseResultDTO iCases = CaseService.newInstance().findGlobalSearchCasesByCriteria(query, 0, PAGE_SIZE);
+    List<CaseData> results = iCases.getCases().stream().map(CaseData::new).toList();
+    return new GlobalSearchResponse(results, iCases.getTotalCases());
+  }
+
+  public GlobalSearchResponse searchProcesses(SearchPayload payload) {
+    String keyword = payload.getQuery().toLowerCase();
+    List<IWebStartable> startableProcesses = ProcessService.getInstance().findProcesses();
+    List<ProcessData> processes = startableProcesses.stream()
+        .filter(process -> process.getName().toLowerCase().contains(keyword)
+            || process.getDescription().toLowerCase().contains(keyword))
+        .map(ProcessData::new).sorted(Comparator.comparing(ProcessData::getName)).toList();
+    List<ProcessData> results = processes.isEmpty() ? processes : processes.subList(0, Math.min(processes.size(), PAGE_SIZE));
+    return new GlobalSearchResponse(results, processes.size());
+  }
+
+  private List<SearchScopeTaskField> getSearchScopeTaskFields() {
+    String searchScopeTaskFieldsString = Ivy.var().get(GlobalVariable.SEARCH_SCOPE_BY_TASK_FIELDS.getKey());
+    if (StringUtils.isNotBlank(searchScopeTaskFieldsString)) {
+      List<SearchScopeTaskField> searchScopeTaskFields = new ArrayList<>();
+      String[] fieldArray = searchScopeTaskFieldsString.split(",");
+      for (String field : fieldArray) {
+        SearchScopeTaskField fieldEnum = SearchScopeTaskField.valueOf(field.toUpperCase());
+        if (fieldEnum != null) {
+          searchScopeTaskFields.add(fieldEnum);
+        }
+      }
+      return searchScopeTaskFields;
+    }
+    return List.of(SearchScopeTaskField.NAME, SearchScopeTaskField.DESCRIPTION);
+  }
+
+  private List<SearchScopeCaseField> getSearchScopeCaseFields() {
+    String searchScopeCaseFieldsString = Ivy.var().get(GlobalVariable.SEARCH_SCOPE_BY_CASE_FIELDS.getKey());
+    if (StringUtils.isNotBlank(searchScopeCaseFieldsString)) {
+      List<SearchScopeCaseField> searchScopeCaseFields = new ArrayList<>();
+      String[] fieldArray = searchScopeCaseFieldsString.split(",");
+      for (String field : fieldArray) {
+        SearchScopeCaseField fieldEnum = SearchScopeCaseField.valueOf(field.toUpperCase());
+        if (fieldEnum != null) {
+          searchScopeCaseFields.add(fieldEnum);
+        }
+      }
+      return searchScopeCaseFields;
+    }
+    return List.of(SearchScopeCaseField.NAME, SearchScopeCaseField.DESCRIPTION, SearchScopeCaseField.CUSTOM);
+  }
+
+}
---
+++ AxonIvyPortal/portal/webContent/resources/js/global-search.js
@@ -0,0 +1,370 @@
+if (document) {
+  const pathName = window.location.pathname;
+  const baseURL = pathName.substring(0, pathName.indexOf('/faces/'));
+  const searchProcessesURL = "/api/global-search/processes";
+  const searchTasksURL = "/api/global-search/tasks";
+  const searchCasesURL = "/api/global-search/cases";
+  const searchId = "quick-global-search-component:global-search-data";
+  const eleSearchId = "[id='quick-global-search-component:global-search-data']";
+  const processTabIndex = 0;
+  const taskTabIndex = 1;
+  const caseTabIndex = 2;
+  const startsWithTask = 'task: ';
+  const startsWithProcess = 'process: ';
+  const startsWithCase = 'case: ';
+  const waitTime = 500;
+  const requestInit = body => {
+    return {
+      method: "POST",
+      headers: {"X-Requested-By": "ivy", "Content-Type": "application/json", "Accept": "application/json"},
+      body
+    }
+  }
+
+  $(document).ready(function () {
+    $(eleSearchId).val('');
+
+    $(".search-bar.process-tab-title").click(() => {
+      focusSearchBox();
+      let keyword = $(eleSearchId).val().toLowerCase();
+      if (keyword.startsWith(startsWithTask)) {
+        keyword = keyword.replace(startsWithTask, startsWithProcess);
+      }
+      if (keyword.startsWith(startsWithCase)) {
+        keyword = keyword.replace(startsWithCase, startsWithProcess);
+      }
+      $(eleSearchId).val(keyword);
+
+      searchProcess(keyword);
+    });
+    $(".search-bar.task-tab-title").click(() => {
+      focusSearchBox();
+      let keyword = $(eleSearchId).val().toLowerCase();
+      if (keyword.startsWith(startsWithCase)) {
+        keyword = keyword.replace(startsWithCase, startsWithTask);
+      }
+      if (keyword.startsWith(startsWithProcess)) {
+        keyword = keyword.replace(startsWithProcess, startsWithTask);
+      }
+      $(eleSearchId).val(keyword);
+
+      searchTask(keyword);
+    });
+    $(".search-bar.case-tab-title").click(() => {
+      focusSearchBox();
+      let keyword = $(eleSearchId).val().toLowerCase();
+      if (keyword.startsWith(startsWithTask)) {
+        keyword = keyword.replace(startsWithTask, startsWithCase);
+      }
+      if (keyword.startsWith(startsWithProcess)) {
+        keyword = keyword.replace(startsWithProcess, startsWithCase);
+      }
+      $(eleSearchId).val(keyword);
+
+      searchCase(keyword);
+    });
+  });
+
+  $(eleSearchId).click((e) => {
+    if (!window.matchMedia("(max-width: 767px)").matches) {
+      $(eleSearchId).addClass('global-large-search-bar').removeClass('global-small-search-bar');
+    } else {
+      $(eleSearchId).removeClass('global-large-search-bar').addClass('global-small-search-bar');
+    }
+    let activeTab = $("li.ui-tabs-header.search-bar[aria-selected='true']");
+    let tabIndex = activeTab.attr('data-index') * 1;
+    search(e.target.value, tabIndex);
+  });
+
+  function resetSearchBar() {
+    if (!window.matchMedia("(max-width: 767px)").matches) {
+      $(eleSearchId).removeClass('global-large-search-bar').addClass('global-small-search-bar');
+    } else {
+      $(eleSearchId).addClass('global-default-search-bar').removeClass('global-large-search-bar').removeClass('global-small-search-bar');
+    }
+  }
+
+  function viewAllResults(tabIndex) {
+    rcResetAllId();
+    handlePromise(tabIndex).then(() => {
+      portalGlobalSearch([{name: 'activeTabIndex', value: getTabActiveIndex()}]);
+      return false;
+    })
+
+  }
+
+  function getTabActiveIndex() {
+    let activeTab = $("li.ui-tabs-header.search-bar[aria-selected='true']");
+    return activeTab.attr('data-index') * 1;
+  }
+
+  function handlePromise(tabIndex) {
+    return new Promise((resolve, reject) => {
+      let searchInput = $('#quick-global-search-component\\:global-search-data');
+      let searchValue = searchInput.val().toLowerCase();
+      if (tabIndex === taskTabIndex && !searchValue.startsWith(startsWithTask)) {
+        searchInput.val(startsWithTask + searchValue);
+      } else if (tabIndex === caseTabIndex && !searchValue.startsWith(startsWithCase)) {
+        searchInput.val(startsWithCase + searchValue);
+      }
+      resolve('success');
+      reject('fail');
+    })
+  }
+
+  function focusSearchBox() {
+    $(eleSearchId).focus();
+  }
+
+  let timer;
+
+  document.getElementById(searchId).addEventListener('keyup', event => {
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      doneTyping(event);
+    }, waitTime);
+  });
+
+  function doneTyping(event) {
+    if (event.key === 'Enter') {
+      portalGlobalSearch([{name: 'activeTabIndex', value: getTabActiveIndex()}]);
+      return false;
+    } else {
+      searchHandler();
+    }
+  }
+
+  function analyzeInputSearch(input) {
+    let activeTab = $("li.ui-tabs-header.search-bar[aria-selected='true']");
+    let tabIndex = activeTab.attr('data-index') * 1;
+    let keyword = input;
+    let explicit = false;
+    if (input.startsWith(startsWithProcess)) {
+      keyword = input.slice(input.indexOf(':') + 1);
+      tabIndex = processTabIndex;
+      explicit = true;
+    }
+    if (input.startsWith(startsWithTask)) {
+      keyword = input.slice(input.indexOf(':') + 1);
+      tabIndex = taskTabIndex;
+      explicit = true;
+    }
+    if (input.startsWith(startsWithCase)) {
+      keyword = input.slice(input.indexOf(':') + 1);
+      tabIndex = caseTabIndex;
+      explicit = true;
+    }
+
+    return {keyword, tabIndex, explicit}
+  }
+
+  function searchHandler() {
+    let inputSearch = $(eleSearchId).val().toLowerCase();
+    let analyzedInput = analyzeInputSearch(inputSearch);
+    let keyword = analyzedInput.keyword;
+    let tabIndex = analyzedInput.tabIndex;
+    if (analyzedInput.explicit) {
+      let activeTab = $("li.ui-tabs-header.search-bar[data-index='" + tabIndex + "']");
+      activeTab.click();
+    } else {
+      search(keyword.trim(), tabIndex);
+    }
+  }
+
+  function search(keyword, tabIndex) {
+    if (processTabIndex === tabIndex) {
+      searchProcess(keyword);
+    } else if (taskTabIndex === tabIndex) {
+      searchTask(keyword);
+    } else if (caseTabIndex === tabIndex) {
+      searchCase(keyword);
+    }
+  }
+
+  function handleError(err) {
+    console.log(err);
+  }
+
+  function handleUISearchResults($parent, data) {
+    let viewAllLink = $parent.siblings('.global-search-all-results')[0];
+    let noResults = $parent.siblings('.global-search-no-results')[0];
+    if (data && data.totalResult > 0) {
+      $(viewAllLink).removeClass('u-hidden');
+      $(viewAllLink).find('a').html(data.viewAllText + ' (' + data.totalResult + ')')
+      $(noResults).addClass('u-hidden');
+    } else {
+      $(viewAllLink).addClass('u-hidden');
+      $(noResults).removeClass('u-hidden');
+    }
+    initConfirmDialog()
+  }
+
+  function searchProcess(keyword) {
+    if (keyword.startsWith(startsWithProcess)) {
+      keyword = keyword.slice(keyword.indexOf(':') + 1);
+    }
+    let body = JSON.stringify({"query": keyword})
+    fetch(baseURL + searchProcessesURL, requestInit(body))
+      .then((response) => {
+        response.json().then(data => {
+          let $resultsContainer = $("#process-search-results");
+          $resultsContainer.html("");
+          if (data) {
+            data.results.forEach((rs, index) => {
+              document.getElementById("process-search-results").innerHTML += processComponent(rs);
+            });
+            handleUISearchResults($resultsContainer, data);
+          }
+        }, err => handleError(err));
+      }, err => handleError(err));
+
+  }
+
+  function searchTask(keyword) {
+    if (keyword.startsWith(startsWithTask)) {
+      keyword = keyword.slice(keyword.indexOf(':') + 1);
+    }
+    let body = JSON.stringify({"query": keyword})
+    fetch(baseURL + searchTasksURL, requestInit(body))
+      .then((response) => {
+        response.json().then(data => {
+          let promise = new Promise((resolve, reject) => {
+            let $resultsContainer = $("#task-search-results");
+            $resultsContainer.html("");
+            if (data) {
+              data.results.forEach((rs, index) => {
+                document.getElementById("task-search-results").innerHTML += taskComponent(rs);
+              });
+            }
+            resolve($resultsContainer);
+            reject('fail');
+          });
+          promise.then(results => {
+            handleUISearchResults(results, data)
+          });
+        }, err => handleError(err));
+      }, err => handleError(err));
+  }
+
+  function searchCase(keyword) {
+    if (keyword.startsWith(startsWithCase)) {
+      keyword = keyword.slice(keyword.indexOf(':') + 1);
+    }
+    let body = JSON.stringify({"query": keyword})
+    fetch(baseURL + searchCasesURL, requestInit(body))
+      .then((response) => {
+        response.json().then(data => {
+          let $resultsContainer = $("#case-search-results");
+          $resultsContainer.html("");
+          if (data) {
+            data.results.forEach((rs, index) => {
+              document.getElementById("case-search-results").innerHTML += caseComponent(rs);
+            });
+            handleUISearchResults($resultsContainer, data);
+          }
+        }, err => handleError(err));
+      }, err => handleError(err));
+  }
+
+  function initConfirmDialog() {
+    if (PrimeFaces.widgets['search-task-losing-confirmation-dialog']) {
+      $("a.search-task-list-item").off('click').on('click', e => {
+        PF('search-task-losing-confirmation-dialog').show();
+        let id = $(e.target).closest('.search-task-list-item').find('.search-item-result').attr("data-id");
+        rcUpdateCurrentItemId([{name: 'id', value: id}, {name: 'type', value: 'TASK'}]);
+        return false;
+      });
+      $("a.search-case-list-item").off('click').on('click', e => {
+        PF('search-task-losing-confirmation-dialog').show();
+        let id = $(e.target).closest('.search-case-list-item').find('.search-item-result').attr("data-id");
+        rcUpdateCurrentItemId([{name: 'id', value: id}, {name: 'type', value: 'CASE'}]);
+        return false;
+      });
+      $("a.search-process-list-item").off('click').on('click', e => {
+        PF('search-task-losing-confirmation-dialog').show();
+        let id = $(e.target).closest('.search-process-list-item').find('.search-item-result').attr("data-id");
+        rcUpdateCurrentItemId([{name: 'id', value: id}, {name: 'type', value: 'PROCESS'}]);
+        return false;
+      });
+
+    }
+  }
+
+  function showConfirmationDialog() {
+    if (PrimeFaces.widgets['search-task-losing-confirmation-dialog']) {
+      PF('search-task-losing-confirmation-dialog').show();
+    } else {
+      return false;
+    }
+  }
+
+
+  let processComponent = (process) => {
+    return '<a href="' + process.link + '" class="task-even-row full-mode task-start-list-item js-task-start-list-item search-process-list-item">' +
+      '   <div class="task-start-link js-task-start-link">' +
+      '     <div class="task-priority-item">' +
+      '       <span class="priority-cell search-item-result"  data-id="' + process.link + '">' +
+      '         <span class="priority-icon">' +
+      '           <i class="' + process.icon + '"></i>' +
+      '         </span>' +
+      '       </span>' +
+      '     </div>' +
+      '     <div class="task-start-info">' +
+      '       <span class="name-cell">' + process.name + '</span>' +
+      '     </div>' +
+      '    </div>' +
+      '</a>';
+  }
+
+  let taskComponent = (task) => {
+    let priority = task.priority;
+    let iconClass;
+    switch (priority) {
+      case 'LOW':
+        iconClass = "priority si si-navigation-down-circle low-priority";
+        break;
+      case 'NORMAL':
+        iconClass = "priority si si-navigation-right-circle-1 normal-priority";
+        break;
+      case 'HIGH':
+        iconClass = "priority si si-navigation-up-circle high-priority";
+        break;
+      default:
+        iconClass = "priority si si-alert-circle exception-priority";
+        break;
+    }
+    return '<a href="' + task.link + '" class="task-even-row full-mode task-start-list-item js-task-start-list-item search-task-list-item">' +
+      '  <div class="task-start-link js-task-start-link">' +
+      '     <div class="task-priority-item search-item-result" data-id="' + task.uuid + '">' +
+      '       <span class="priority-cell search-item">' +
+      '        <span class="priority-icon">' +
+      '          <i class="' + iconClass + '"></i>' +
+      '      </span>' +
+      '     </span>' +
+      '   </div>' +
+      '  <div class="task-start-info">' +
+      '    <span class="name-cell">' + task.name +
+      '    </span>' +
+      '  </div>' +
+      ' </div>' +
+      '</a>';
+  }
+
+  let caseComponent = (caze) => {
+    return '<a href="' + caze.link + '" class="task-even-row full-mode task-start-list-item js-task-start-list-item search-case-list-item">' +
+      '     <div class="task-start-link js-task-start-link">' +
+      '       <div class="task-priority-item">' +
+      '         <span class="priority-cell search-item-result" data-id="' + caze.uuid + '">' +
+      '           <span class="priority-icon">' +
+      '             <i class="si si-layout-bullets"></i>' +
+      '           </span>' +
+      '         </span>' +
+      '       </div>' +
+      '       <div class="task-start-info">' +
+      '         <span class="name-cell">' + caze.name +
+      '         </span>' +
+      '       </div>' +
+      '     </div>' +
+      '</a>';
+  }
+}
