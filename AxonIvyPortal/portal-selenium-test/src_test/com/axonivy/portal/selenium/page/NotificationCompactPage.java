ed7226dc7dd0373f0f26e10ef94ecb6b9abe0123
Nam.Chu <nam.chu@axonactive.com>
2023-12-29 15:44:30 +0700

    IVYPORTAL-16132 Notifications Settings Link & Fullscreen Notifications Page - Update screenshot

---
+++ AxonIvyPortal/portal-selenium-test/src_test/com/axonivy/portal/selenium/page/NotificationCompactPage.java
@@ -0,0 +1,17 @@
+package com.axonivy.portal.selenium.page;
+
+import static com.codeborne.selenide.Selenide.$;
+
+public class NotificationCompactPage extends TemplatePage {
+
+  @Override
+  protected String getLoadedLocator() {
+    // TODO Auto-generated method stub
+    return "[id='notifications-panel']";
+  }
+
+  public void openNotificationFullPage() {
+    $("a[id='notification-full-page']").shouldBe(getClickableCondition(), DEFAULT_TIMEOUT).click();
+    $("a[id='notification-full-form:notifications-scroller:0:notification-mark-as-read']").shouldBe(getClickableCondition(), DEFAULT_TIMEOUT);
+  }
+}
