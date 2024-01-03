4f58e2f72e6fb8f39ac9b5963bf814b5dfc7ae16
Nam.Chu <nam.chu@axonactive.com>
2024-01-03 15:47:01 +0700

    IVYPORTAL-16132 Notifications Settings Link & Fullscreen Notifications Page - Revert Jenkins file - Fix fail tests

--- AxonIvyPortal/portal-selenium-test/src_test/com/axonivy/portal/selenium/page/NewDashboardPage.java
+++ AxonIvyPortal/portal-selenium-test/src_test/com/axonivy/portal/selenium/page/NewDashboardPage.java
@@ -931,7 +931,7 @@ public class NewDashboardPage extends TemplatePage {
   }
 
   public boolean isMarkAllAsReadDisplayed(WebElement notificationsPanel) {
-    return $("[id='notificationMarkAllAsRead']").shouldBe(appear, DEFAULT_TIMEOUT).isDisplayed();
+    return $("[id='notification-mark-all-as-read']").shouldBe(appear, DEFAULT_TIMEOUT).isDisplayed();
   }
 
   public boolean isTodayGroupLineDisplayed(WebElement notificationsPanel) {
@@ -940,8 +940,7 @@ public class NewDashboardPage extends TemplatePage {
 
   public void markAsRead(WebElement notificationsPanel, int expectedBadge) {
     waitForGlobalGrowlDisappear();
-    WebElement item = $(".ui-datascroller-item");
-    item.findElement(By.id("notificationForm:notifications-scroller:0:notificationMarkAsRead")).click();
+    $("[id='notification-compact-form:notifications-scroller:0:notification-mark-as-read']").click();
     $(By.id("notifications-badge-value")).shouldBe(Condition.exactValue(String.valueOf(expectedBadge)),
         DEFAULT_TIMEOUT);
   }
@@ -952,7 +951,7 @@ public class NewDashboardPage extends TemplatePage {
   }
 
   public void markAsAllRead(WebElement notificationsPanel) {
-    $("[id='notificationMarkAllAsRead']").shouldBe(appear, DEFAULT_TIMEOUT)
+    $("[id='notification-mark-all-as-read']").shouldBe(appear, DEFAULT_TIMEOUT)
         .shouldBe(getClickableCondition(), DEFAULT_TIMEOUT).click();
     waitForElementValueChanged("#topbar-unread-notifications", "0");
   }
