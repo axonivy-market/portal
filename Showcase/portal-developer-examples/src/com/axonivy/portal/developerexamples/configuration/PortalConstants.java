560bc611e17ae7ccabd92352d2f7e57dcc91210f
Le Ngoc Danh <danh.lengoc@axonactive.com>
2024-01-08 16:07:30 +0700

    IVYPORTAL-15933 Make portal-user-examples and portal-developer-examples not depend on Portal: update code for internal support

--- Showcase/portal-developer-examples/src/com/axonivy/portal/developerexamples/configuration/PortalConstants.java
+++ Showcase/portal-developer-examples/src/com/axonivy/portal/developerexamples/configuration/PortalConstants.java
@@ -2,4 +2,5 @@ package com.axonivy.portal.developerexamples.configuration;
 
 public class PortalConstants {
   public static final String SECURITY_SERVICE_CALLABLE = "Ivy Data Processes/SecurityService";
+  public static final String BUILD_CASE_QUERY_CALLABLE = "Functional Processes/BuildCaseQuery";
 }
---
+++ Showcase/portal-developer-examples/src/com/axonivy/portal/developerexamples/dto/MainMenuNode.java
@@ -0,0 +1,23 @@
+package com.axonivy.portal.developerexamples.dto;
+
+public class MainMenuNode {
+
+  private String value;
+  private String url;
+  
+  public String getValue() {
+    return value;
+  }
+
+  public void setValue(String value) {
+    this.value = value;
+  }
+
+  public String getUrl() {
+    return url;
+  }
+
+  public void setUrl(String url) {
+    this.url = url;
+  }
+}
