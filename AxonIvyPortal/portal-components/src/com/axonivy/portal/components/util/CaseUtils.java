5bf78919b1d133e0260f43b2968f4ef2b96c73eb
Le Ngoc Danh <danh.lengoc@axonactive.com>
2024-01-08 16:15:56 +0700

    IVYPORTAL-15933 Make portal-user-examples and portal-developer-examples not depend on Portal: update code

--- AxonIvyPortal/portal-components/src/com/axonivy/portal/components/util/CaseUtils.java
+++ AxonIvyPortal/portal-components/src/com/axonivy/portal/components/util/CaseUtils.java
@@ -3,7 +3,6 @@ package com.axonivy.portal.components.util;
 import ch.ivyteam.ivy.environment.Ivy;
 import ch.ivyteam.ivy.security.exec.Sudo;
 import ch.ivyteam.ivy.workflow.ICase;
-import ch.ivyteam.ivy.workflow.query.CaseQuery;
 
 public final class CaseUtils {
 
@@ -12,13 +11,8 @@ public final class CaseUtils {
   public static ICase findCase(long caseId) {
     return Sudo.get(() -> Ivy.wf().findCase(caseId));
   }
-  
+
   public static ICase findCase(String uuid) {
     return Sudo.get(() -> Ivy.wf().findCase(uuid));
   }
-
-  public static CaseQuery createBusinessCaseQuery() {
-    return CaseQuery.businessCases();
-  }
-
 }
