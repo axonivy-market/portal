14d1163f8b2177a9fdffda268ca8052b47ed09aa
Tung Le <tung.le@axonactive.com>
2024-01-17 18:20:36 +0700

    IVYPORTAL-16428 XSS Vulnerabilty in Portal: Update library to to sanitize, updated related code

--- AxonIvyPortal/portal/src/ch/ivy/addon/portalkit/util/SecurityMemberUtils.java
+++ AxonIvyPortal/portal/src/ch/ivy/addon/portalkit/util/SecurityMemberUtils.java
@@ -79,7 +79,7 @@ public class SecurityMemberUtils {
       if (CollectionUtils.isEmpty(users)) {
         String noUsers = cms("noUser");
         usersBuilder.append(cms("roleMemberLineFormat", Arrays.asList(noUsers)));
-        return cms("roleMembersTooltipFormat", Arrays.asList(header, "no-user", usersBuilder));
+        return cms("roleMembersTooltipFormat", Arrays.asList(header, "padding-left: 0;", usersBuilder));
       }
 
       for (IUser user : users) {
@@ -90,7 +90,7 @@ public class SecurityMemberUtils {
         usersBuilder.append(cms("roleMemberLineFormat",
             Arrays.asList(Ivy.cms().co("/ch.ivy.addon.portalkit.ui.jsf/common/more").concat(" ..."))));
       }
-      return cms("roleMembersTooltipFormat", Arrays.asList(header, "", usersBuilder));
+      return cms("roleMembersTooltipFormat", Arrays.asList(header, "padding-left: 14px;", usersBuilder));
     });
   }
   
