0c65f3a72581561457d222554a24ad3c0b5e5026
Le Ngoc Danh <danh.lengoc@axonactive.com>
2024-01-22 18:19:00 +0700

    IVYPORTAL-15933 Make portal-user-examples and portal-developer-examples not depend on Portal: update testcase

--- AxonIvyPortal/portal-selenium-test/src_test/com/axonivy/portal/selenium/page/CaseEditWidgetNewDashBoardPage.java
+++ AxonIvyPortal/portal-selenium-test/src_test/com/axonivy/portal/selenium/page/CaseEditWidgetNewDashBoardPage.java
@@ -143,7 +143,8 @@ public class CaseEditWidgetNewDashBoardPage extends TemplatePage {
   public void selectFieldType(String type) {
     getColumnManagementDialog().$("div[id$='field-type-selection'] span.ui-icon-triangle-1-s")
         .shouldBe(Condition.appear, DEFAULT_TIMEOUT).shouldBe(Condition.enabled, DEFAULT_TIMEOUT).click();
-    $("div[id$='column-management-form:field-type-selection_panel'] li[data-label='" + type + "']").click();
+    waitForElementClickableThenClick(
+        $("div[id$='column-management-form:field-type-selection_panel'] li[data-label='" + type + "']"));
   }
 
   public void removeAddedField(String field) {
