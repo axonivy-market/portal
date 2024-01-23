d0c133c6da44fd57f26dbcf9563be9d2ba20b034
mnhnam-axonivy <mnhnam@axonactive.com>
2024-01-23 10:34:52 +0700

    IVYPORTAL-15933: Make portal-user-examples and portal-developer-examples not depend on Portal - Fixed some failed GUI tests - Remove deprecated example of Case details hook because we don't support it anymore

--- AxonIvyPortal/portal-selenium-test/src_test/com/axonivy/portal/selenium/test/dashboard/DashboardTaskWidgetActionTest.java
+++ AxonIvyPortal/portal-selenium-test/src_test/com/axonivy/portal/selenium/test/dashboard/DashboardTaskWidgetActionTest.java
@@ -13,6 +13,7 @@ import com.axonivy.ivy.webtest.IvyWebTest;
 import com.axonivy.portal.selenium.common.BaseTest;
 import com.axonivy.portal.selenium.common.TestAccount;
 import com.axonivy.portal.selenium.page.NewDashboardPage;
+import com.axonivy.portal.selenium.page.TaskTemplateIFramePage;
 import com.axonivy.portal.selenium.page.TaskWidgetNewDashBoardPage;
 import com.codeborne.selenide.ElementsCollection;
 
@@ -105,7 +106,9 @@ public class DashboardTaskWidgetActionTest extends BaseTest {
     filterTaskByNameAndState("Sick Leave Request", OPEN);
     TaskWidgetNewDashBoardPage taskWidget = new TaskWidgetNewDashBoardPage();
     taskWidget.startTask(0);
-    taskWidget.clickCancelTask();
+    TaskTemplateIFramePage templatePage = new TaskTemplateIFramePage();
+    templatePage.switchToIFrameOfTask();
+    newDashboardPage = templatePage.clickCancelButton();
     newDashboardPage.waitForAbsencesGrowlMessageDisplay();
 
     // In progress for admin user
@@ -117,7 +120,9 @@ public class DashboardTaskWidgetActionTest extends BaseTest {
     createTasksForTesting();
     filterTaskByNameAndState("Sick Leave Request", OPEN);
     taskWidget.startTask(0);
-    taskWidget.clickCancelTask();
+    templatePage = new TaskTemplateIFramePage();
+    templatePage.switchToIFrameOfTask();
+    newDashboardPage = templatePage.clickCancelButton();
     newDashboardPage.waitForAbsencesGrowlMessageDisplay();
     // In progress for normal user
     newDashboardPage.waitForGrowlMessageDisappear();
