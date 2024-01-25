c12fabfdf5d9e31179f12fb2ff5e6da57f9be68f
mnhnam-axonivy <mnhnam@axonactive.com>
2024-01-25 10:18:21 +0700

    IVYPORTAL-16295: SPIKE UI-PoC for Portal Chatbot - Integrate AI logic to Portal - Create suggestions

--- AxonIvyPortal/portal/src/ch/ivy/addon/portalkit/enums/PortalVariable.java
+++ AxonIvyPortal/portal/src/ch/ivy/addon/portalkit/enums/PortalVariable.java
@@ -15,7 +15,8 @@ public enum PortalVariable {
   DASHBOARD_TEMPLATES("Portal.DashboardTemplates"),
   USER_MENU("Portal.UserMenu"),
   CUSTOM_MENU_ITEMS("Portal.CustomMenuItems"),
-  STATISTIC_DATA("Portal.StatisticData");
+  STATISTIC_DATA("Portal.StatisticData"),
+  CHATBOT_ENDPOINT("Portal.Chat.ChatBotEndpoint");
 
   public String key;
 
