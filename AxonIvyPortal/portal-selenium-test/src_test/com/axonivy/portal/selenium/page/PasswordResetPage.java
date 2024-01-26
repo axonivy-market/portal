2d2c51a84d1c1d433572eff37ca5db44ee1fc7ae
Le Ngoc Danh <danh.lengoc@axonactive.com>
2024-01-26 10:49:15 +0700

    IVYPORTAL-15933 Make portal-user-examples and portal-developer-examples not depend on Portal: optimize code

--- AxonIvyPortal/portal-selenium-test/src_test/com/axonivy/portal/selenium/page/PasswordResetPage.java
+++ AxonIvyPortal/portal-selenium-test/src_test/com/axonivy/portal/selenium/page/PasswordResetPage.java
@@ -37,7 +37,7 @@ public class PasswordResetPage extends TemplatePage {
     if (strongPasswordEnough) {
       $("[id='password-reset:reset-password-form:result-message']").shouldBe(Condition.appear, DEFAULT_TIMEOUT);
     } else {
-      $("span[class='ui-messages-error-summary']").shouldBe(Condition.appear, DEFAULT_TIMEOUT);;
+      $("span[class='ui-messages-error-summary']").shouldBe(Condition.appear, DEFAULT_TIMEOUT);
     }
   }
 
@@ -52,6 +52,6 @@ public class PasswordResetPage extends TemplatePage {
 
   public void isNewPasswordNotStrongEnough() {
     findElementByCssSelector("span[class='ui-messages-error-summary']")
-        .shouldBe(Condition.text(PASSWORD_WEAK_CUSTOM_MESSAGE));
+        .shouldBe(Condition.text(PASSWORD_WEAK_CUSTOM_MESSAGE), DEFAULT_TIMEOUT);
   }
 }
