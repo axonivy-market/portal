9c1743b44007dd09916af55b1870e887f681a11a
Loc Nguyen <ntloc9@gmail.com>
2024-02-19 01:02:08 +0700

    IVYPORTAL-16025-Case-Widget-Complex-Filters-GUI-Tests-Screenshots-Documentation-LE - Update test

--- AxonIvyPortal/portal-selenium-test/src_test/com/axonivy/portal/selenium/test/dashboard/DashboardEditCaseWidgetTest.java
+++ AxonIvyPortal/portal-selenium-test/src_test/com/axonivy/portal/selenium/test/dashboard/DashboardEditCaseWidgetTest.java
@@ -25,9 +25,6 @@ public class DashboardEditCaseWidgetTest extends BaseTest{
   public void setup() {
     super.setup();
     newDashboardPage = new NewDashboardPage();
-    redirectToRelativeLink(createCaseWithTechnicalCaseUrl);
-//    redirectToRelativeLink(creat);
-    redirectToRelativeLink("internalSupport/14B2FC03D2E87141/TestCaseListPermissions.ivp");
   }
   
   @Test
@@ -42,6 +39,9 @@ public class DashboardEditCaseWidgetTest extends BaseTest{
   
   @Test
   public void filterNameTest() {
+    redirectToRelativeLink(createCaseWithTechnicalCaseUrl);
+    redirectToRelativeLink("internalSupport/14B2FC03D2E87141/TestCaseListPermissions.ivp");
+
     NewDashboardDetailsEditPage newDashboardDetailsEditPage = gotoEditPublicDashboardPage();
     newDashboardDetailsEditPage.addWidget();
     CaseEditWidgetNewDashBoardPage caseWidget = newDashboardDetailsEditPage.addNewCaseWidget();
@@ -80,14 +80,47 @@ public class DashboardEditCaseWidgetTest extends BaseTest{
     caseWidget.closeFilter();
   }
   
+  @Test
   public void filterCreatorTest() {
-    
+    login(TestAccount.DEMO_USER);
+    redirectToRelativeLink(createCaseWithTechnicalCaseUrl);
+    NewDashboardDetailsEditPage newDashboardDetailsEditPage = gotoEditPublicDashboardPage();
+    newDashboardDetailsEditPage.addWidget();
+    CaseEditWidgetNewDashBoardPage caseWidget = newDashboardDetailsEditPage.addNewCaseWidget();
+    caseWidget.waitPreviewTableLoaded();
+    caseWidget.countCases().shouldBe(CollectionCondition.size(2));
+    caseWidget.openFilter();
+    caseWidget.addFilter("Creator", FilterOperator.CURRENT_USER);
+    caseWidget.applyFilter();
+    caseWidget.countCases().shouldBe(CollectionCondition.size(1));
+    caseWidget.removeFilter(0);
+
+    caseWidget.addFilter("Creator", FilterOperator.IN);
+    caseWidget.inputValueOnLatestFilter(FilterValueType.CREATOR_TYPE, "admin");
+    caseWidget.applyFilter();
+    caseWidget.countCases().shouldBe(CollectionCondition.size(1));
+
+    caseWidget.addFilter("Creator", FilterOperator.NOT_IN);
+    caseWidget.inputValueOnLatestFilter(FilterValueType.CREATOR_TYPE, "admin");
+    caseWidget.applyFilter();
+    caseWidget.countCases().shouldBe(CollectionCondition.size(0));
   }
   
+  @Test
+  public void filterCategoryTest() {
+    login(TestAccount.ADMIN_USER);
+    NewDashboardDetailsEditPage newDashboardDetailsEditPage = gotoEditPublicDashboardPage();
+    newDashboardDetailsEditPage.addWidget();
+    CaseEditWidgetNewDashBoardPage caseWidget = newDashboardDetailsEditPage.addNewCaseWidget();
+    caseWidget.waitPreviewTableLoaded();
+    caseWidget.addFilter("Category", FilterOperator.IN);
+    caseWidget.inputValueOnLatestFilter(FilterValueType.CATEGORY_TYPE, "Leav");
+
+  }
   
   private NewDashboardDetailsEditPage gotoEditPublicDashboardPage() {
-    redirectToRelativeLink(createTestingTasksUrl);
     login(TestAccount.ADMIN_USER);
+    redirectToRelativeLink(createTestingTasksUrl);
     LinkNavigator.redirectToPortalDashboardConfiguration();
     var configurationPage = new DashboardConfigurationPage();
     var modificationPage = configurationPage.openEditPublicDashboardsPage();
