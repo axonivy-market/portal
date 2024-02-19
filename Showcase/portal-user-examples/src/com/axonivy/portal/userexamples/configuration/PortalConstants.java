31d1d3b307ce31ed8474e1b88a7dd52cef388208
mnhnam-axonivy <mnhnam@axonactive.com>
2024-02-19 16:16:51 +0700

    IVYPORTAL-15933 Make portal-user-examples and portal-developer-examples not depend on Portal - Continue remove unnecessary code of project portal-user-examples

--- Showcase/portal-user-examples/src/com/axonivy/portal/userexamples/configuration/PortalConstants.java
+++
@@ -1,5 +0,0 @@
-package com.axonivy.portal.userexamples.configuration;
-
-public class PortalConstants {
-  public static final String SECURITY_SERVICE_CALLABLE = "Ivy Data Processes/SecurityService";
-}
