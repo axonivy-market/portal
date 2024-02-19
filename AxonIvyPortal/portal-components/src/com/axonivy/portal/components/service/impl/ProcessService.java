e5ea56ae6e9d24a0df527795683a08a537282374
mnhnam-axonivy <mnhnam@axonactive.com>
2024-02-19 14:46:06 +0700

    IVYPORTAL-16560: Cannot show process in custom widget - Adapt for findProcesses() method

--- AxonIvyPortal/portal-components/src/com/axonivy/portal/components/service/impl/ProcessService.java
+++ AxonIvyPortal/portal-components/src/com/axonivy/portal/components/service/impl/ProcessService.java
@@ -1,11 +1,9 @@
 package com.axonivy.portal.components.service.impl;
 
 import static com.axonivy.portal.components.constant.CustomFields.IS_DASHBOARD_PROCESS;
-import static org.apache.commons.collections4.CollectionUtils.isNotEmpty;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.UUID;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
@@ -13,13 +11,10 @@ import org.apache.commons.lang3.BooleanUtils;
 import org.apache.commons.lang3.StringUtils;
 
 import com.axonivy.portal.components.dto.IvyProcessResultDTO;
-import com.axonivy.portal.components.enums.SessionAttribute;
 import com.axonivy.portal.components.service.IProcessService;
-import com.axonivy.portal.components.util.UserUtils;
 
 import ch.ivyteam.ivy.environment.Ivy;
 import ch.ivyteam.ivy.security.exec.Sudo;
-import ch.ivyteam.ivy.server.restricted.EngineMode;
 import ch.ivyteam.ivy.workflow.IWorkflowSession;
 import ch.ivyteam.ivy.workflow.start.IWebStartable;
 
@@ -29,8 +24,6 @@ public class ProcessService implements IProcessService {
   private static final String PORTAL_START_REQUEST_PATH = "/DefaultApplicationHomePage.ivp";
   private static ProcessService instance;
   private static IvyProcessResultDTO ivyProcessResultDTO;
-  private static String sessionUserId;
-  private static String userLanguage; 
   public ProcessService() { }
 
   /**
@@ -52,20 +45,10 @@ public class ProcessService implements IProcessService {
 
   @Override
   public IvyProcessResultDTO findProcesses() {
-    if (isInSession() && isNotEmpty(ivyProcessResultDTO.getProcesses())) {
-      return ivyProcessResultDTO;
-    }
-    updateUserSessionAtributes();
-    ivyProcessResultDTO = new IvyProcessResultDTO();
     return Sudo.get(() -> {
       ivyProcessResultDTO.setProcesses(findStartablesWithoutPortalHomeAndMSTeamsProcess(Ivy.session()));
       return ivyProcessResultDTO;
     });
-  }
-
-  private void updateUserSessionAtributes() {
-    sessionUserId = Ivy.session().getAttribute(SessionAttribute.SESSION_IDENTIFIER.toString()).toString();
-    userLanguage = UserUtils.getUserLanguage();
   }
 
   private List<IWebStartable> findStartablesWithoutPortalHomeAndMSTeamsProcess(IWorkflowSession session) {
@@ -79,16 +62,6 @@ public class ProcessService implements IProcessService {
   private boolean isNotPortalHomeAndMSTeamsProcess(IWebStartable process) {
     String relativeEncoded = process.getLink().getRelativeEncoded();
     return !StringUtils.endsWithAny(relativeEncoded, PORTAL_START_REQUEST_PATH, PORTAL_IN_TEAMS_REQUEST_PATH);
-  }
-
-  private boolean isInSession() {
-    String currentUserLanguage = UserUtils.getUserLanguage();
-    String sessionIdAttribute = SessionAttribute.SESSION_IDENTIFIER.toString();
-    if (Ivy.session().getAttribute(sessionIdAttribute) == null) {
-      Ivy.session().setAttribute(sessionIdAttribute, UUID.randomUUID().toString());
-    }
-    return EngineMode.isNot(EngineMode.DESIGNER_EMBEDDED) && currentUserLanguage.equals(userLanguage)
-        && Ivy.session().getAttribute(sessionIdAttribute).toString().equals(sessionUserId);
   }
 
   public List<IWebStartable> findCustomDashboardProcesses() {
