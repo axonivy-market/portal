326fd8d51dc4a334db35e842a95a102e917726de
Luat Luong Minh <lmluat@axonactive.com>
2024-02-20 11:47:37 +0700

    IVYPORTAL-16025-Case-Widget-Complex-Filters-GUI-Tests-Screenshots-Documentation-LE - update inputValueOnLatestFilter method

--- AxonIvyPortal/portal-selenium-test/src_test/com/axonivy/portal/selenium/common/ComplexFilterHelper.java
+++ AxonIvyPortal/portal-selenium-test/src_test/com/axonivy/portal/selenium/common/ComplexFilterHelper.java
@@ -22,20 +22,22 @@ public class ComplexFilterHelper {
     String latestFilter = String.format("div[id$=':%s:filter-component:filter-selection-panel']", filterIndex);
     return $(latestFilter).shouldBe(Condition.appear, DEFAULT_TIMEOUT);
   }
-  
+
   public static void selectFilterColumnName(String columnName, int filterIndex) {
     var filterElement = getNewFilter(filterIndex);
     filterElement.$("div[id$=':filter-component:field-selection']").shouldBe(getClickableCondition()).click();
     String columnSelection = String.format("div[id$=':%s:filter-component:field-selection_panel']", filterIndex);
     $(columnSelection).$$("ul li").filter(text(columnName)).first().click();
   }
-  
+
   public static void selectFilterOperator(FilterOperator operator, int filterIndex) {
     var filterElement = getNewFilter(filterIndex);
     filterElement.$("div[id$=':operator-selection']").shouldBe(getClickableCondition()).click();
-    WaitHelper.waitForActionComplete(".dashboard-widget-filter__main-panel", () -> $("div[id$=':operator-selection_panel'] ul[id$=':operator-selection_items']").$$("li").filter(text(operator.getValue())).first().click());
+    WaitHelper.waitForActionComplete(".dashboard-widget-filter__main-panel",
+        () -> $("div[id$=':operator-selection_panel'] ul[id$=':operator-selection_items']").$$("li")
+            .filter(text(operator.getValue())).first().click());
   }
-  
+
   public static void inputValueOnLatestFilter(FilterValueType type, Object... values) {
     int currentIndex = $$("div[id$=':filter-component:filter-selection-panel']").size();
     if (currentIndex < 1) {
@@ -45,29 +47,48 @@ public class ComplexFilterHelper {
     switch (type) {
       case STATE_TYPE:
         filterElement.$("div[id$=':states']").shouldBe(getClickableCondition()).click();
-        for (int i=0; i < values.length; i++) {
+        for (int i = 0; i < values.length; i++) {
           getValueOfCheckBox(String.valueOf(values[i])).shouldBe(getClickableCondition()).click();
         }
         getCloseCheckBox().shouldBe(getClickableCondition()).click();
         break;
-      case TEXT, NUMBER:
-        var textField = filterElement.$("div[id$=':text-list-panel']").$(".ui-chips.ui-widget").$("input").shouldBe(Condition.editable);
-        for (int i=0; i < values.length; i++) {
+      case TEXT:
+        var textField = filterElement.$("div[id$=':text-list-panel']").$(".ui-chips.ui-widget").$("input")
+            .shouldBe(Condition.editable);
+        for (int i = 0; i < values.length; i++) {
           textField.clear();
           textField.sendKeys(String.valueOf(values[i]));
           textField.pressEnter();
         }
         break;
+      case NUMBER:
+        if (filterElement.$("div[id$=':between-number-panel-and']").exists()) {
+          filterElement.$("div[id$=':between-number-panel-from']").$("input[id$=':from-number_input']")
+              .shouldBe(Condition.editable).sendKeys(String.valueOf(values[0]));
+          filterElement.$("div[id$=':between-number-panel-to']").$("input[id$=':to-number_input']")
+              .shouldBe(Condition.editable).sendKeys(String.valueOf(values[1]));
+        } else {
+          var numberField = filterElement.$("div[id$=':number-panel']").$("span[id$=':number']").$("input")
+              .shouldBe(Condition.editable);
+          for (int i = 0; i < values.length; i++) {
+            numberField.clear();
+            numberField.sendKeys(String.valueOf(values[i]));
+            numberField.pressEnter();
+          }
+        }
+
+        break;
       case DATE:
-        var dateInput = filterElement.$$(".date-picker-panel input").shouldHave(CollectionCondition.sizeGreaterThanOrEqual(values.length));
-        for (int i=0; i < dateInput.size(); i++) {
+        var dateInput = filterElement.$$(".date-picker-panel input")
+            .shouldHave(CollectionCondition.sizeGreaterThanOrEqual(values.length));
+        for (int i = 0; i < dateInput.size(); i++) {
           dateInput.get(i).clear();
           dateInput.get(i).sendKeys(String.valueOf(values[i]));
         }
         break;
       case CREATOR_TYPE:
         var creatorInput = filterElement.$("div[id$=':creators']").$("input").shouldBe(appear);
-        for (int i=0; i < values.length; i++) {
+        for (int i = 0; i < values.length; i++) {
           creatorInput.clear();
           creatorInput.sendKeys(String.valueOf(values[i]));
           var selectPanel = $("span[id$=':creators_panel'][style*='display: block']").shouldBe(appear);
@@ -79,7 +100,7 @@ public class ComplexFilterHelper {
         break;
       case CATEGORY_TYPE:
         var catogoryField = filterElement.$("div[id$=':category-list-panel']").$("input").shouldBe(appear);
-        for (int i=0; i < values.length; i++) {
+        for (int i = 0; i < values.length; i++) {
           catogoryField.clear();
           catogoryField.sendKeys(String.valueOf(values[i]));
           catogoryField.pressEnter();
@@ -89,16 +110,16 @@ public class ComplexFilterHelper {
         break;
     }
   }
-  
+
   private static SelenideElement getValueOfCheckBox(String value) {
     return $("div.ui-selectcheckboxmenu-items-wrapper").shouldBe(appear, DEFAULT_TIMEOUT)
         .$$("li.ui-selectcheckboxmenu-item").filter(text(value)).first().$("div.ui-chkbox-box");
   }
-  
+
   protected static Condition getClickableCondition() {
     return and("should be clickable", visible, exist);
   }
-  
+
   private static SelenideElement getCloseCheckBox() {
     return $("div.ui-selectcheckboxmenu-panel").shouldBe(appear, DEFAULT_TIMEOUT).$("a.ui-selectcheckboxmenu-close");
   }
