fa4c394fc2260f91ea3bae4e176da855c7304e53
Luat Luong Minh <lmluat@axonactive.com>
2024-02-21 09:24:05 +0700

    IVYPORTAL-16025-Case-Widget-Complex-Filters-GUI-Tests-Screenshots-Documentation-LE - Remove headless false and rename file

--- AxonIvyPortal/portal-selenium-test/src_test/com/axonivy/portal/selenium/test/dashboard/DashboardCaseWidgetFilterTest.java
+++ AxonIvyPortal/portal-selenium-test/src_test/com/axonivy/portal/selenium/test/dashboard/DashboardCaseWidgetFilterTest.java
@@ -20,8 +20,8 @@ import com.codeborne.selenide.CollectionCondition;
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.ElementsCollection;
 
-@IvyWebTest(headless = false)
-public class DashboardCasefWidgetFilterTest extends BaseTest {
+@IvyWebTest
+public class DashboardCaseWidgetFilterTest extends BaseTest {
 
   private NewDashboardPage newDashboardPage;
 
