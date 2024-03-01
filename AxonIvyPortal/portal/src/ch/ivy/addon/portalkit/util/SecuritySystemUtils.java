d854f3286b531e8c80663a1281b4c9296ca618ca
Phan Van Thai <thai.phanvan@axonactive.com>
2024-03-01 17:28:33 +0700

    IVYPORTAL-16347: Update

--- AxonIvyPortal/portal/src/ch/ivy/addon/portalkit/util/SecuritySystemUtils.java
+++ AxonIvyPortal/portal/src/ch/ivy/addon/portalkit/util/SecuritySystemUtils.java
@@ -1,14 +1,15 @@
 package ch.ivy.addon.portalkit.util;
 
-import ch.ivyteam.ivy.security.ISecurityConstants;
 import ch.ivyteam.ivy.security.ISecurityContext;
 import ch.ivyteam.ivy.security.exec.Sudo;
+import ch.ivyteam.ivy.security.internal.ivy.IvySecuritySystem;
 import ch.ivyteam.ivy.security.restricted.ISecurityContextInternal;
 
 public class SecuritySystemUtils {
   public static boolean isIvySecuritySystem() {
     return Sudo.get(() -> {
-      return ((ISecurityContextInternal) ISecurityContext.current()).identityProviders().stream().anyMatch(idp -> ISecurityConstants.IVY_ENGINE_SECURITY_SYSTEM_PROVIDER_NAME.equals(idp.id()));
+      var provider = ((ISecurityContextInternal) ISecurityContext.current()).identityProviders().get(0);
+      return provider instanceof IvySecuritySystem;
     });
   }
 }
