533513909a5aae5bed83ba3a7a0fddabb7990ba4
Luat Luong Minh <lmluat@axonactive.com>
2024-03-04 14:37:52 +0700

    feature/IVYPORTAL-16420-Make-Menu-Entry-Dashboard-Customizable-LE - Add new portal variable MainMenuEntry, json file, create service for editing name and icon

--- AxonIvyPortal/portal/src/ch/ivy/addon/portalkit/enums/PortalVariable.java
+++ AxonIvyPortal/portal/src/ch/ivy/addon/portalkit/enums/PortalVariable.java
@@ -15,7 +15,8 @@ public enum PortalVariable {
   DASHBOARD_TEMPLATES("Portal.DashboardTemplates"),
   USER_MENU("Portal.UserMenu"),
   CUSTOM_MENU_ITEMS("Portal.CustomMenuItems"),
-  STATISTIC_DATA("Portal.StatisticData");
+  STATISTIC_DATA("Portal.StatisticData"),
+  MAIN_MENU_ENTRY("Portal.MainMenuEntry");
 
   public String key;
 
