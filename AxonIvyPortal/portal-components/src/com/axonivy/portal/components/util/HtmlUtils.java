a38322e5d7fbbf33a864487f98c7039e3219524a
Thai Phan <117443490+pvthai-axonivy@users.noreply.github.com>
2024-03-06 17:20:53 +0700

    IVYPORTAL-16465: Unify sanitize content to prevent XSS Vulnerability (#525)
    * IVYPORTAL-16465: Unify sanitize content to prevent XSS Vulnerability
    
    * IVYPORTAL-16465: Handle feedback

--- AxonIvyPortal/portal-components/src/com/axonivy/portal/components/util/HtmlUtils.java
+++ AxonIvyPortal/portal-components/src/com/axonivy/portal/components/util/HtmlUtils.java
@@ -1,8 +1,16 @@
 package com.axonivy.portal.components.util;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 import org.primefaces.util.HtmlSanitizer;
 
-public class HtmlParser {
+public class HtmlUtils {
+  public static String parseTextFromHtml(String text) {
+    String sanitizedText = sanitize(text);
+    Document doc = Jsoup.parse(sanitizedText);
+    return doc.body().text();
+  }
+
   public static String sanitize(String text) {
     // Not use Jsoup.clean(text, Safelist.relaxed().addAttributes(":all", "style", "class")) because the result is href
     // of tag a does not allow relative path. If allowing all protocols of tag a then it could lead to XSS
