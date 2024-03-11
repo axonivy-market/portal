348b228d3932979b6bd67f32d072c67063c3fd47
Thai Phan <117443490+pvthai-axonivy@users.noreply.github.com>
2024-03-11 10:55:54 +0700

    Feature/ivyportal 16465 revert unify code for xss vulnerabilities due to special character le (#549)
    * Revert "IVYPORTAL-16465: Fix failed test (#545)"
    
    This reverts commit 66230f308c1b2c60ddf0ded296698a32e80b31db.
    
    * Revert "IVYPORTAL-16465: Unify sanitize content to prevent XSS Vulnerability (#526)"
    
    This reverts commit f8ae0f84a856fc3f71e94c02336cab0dd07991b7.

--- AxonIvyPortal/portal/src/ch/ivy/addon/portalkit/bean/VisibilityBean.java
+++ AxonIvyPortal/portal/src/ch/ivy/addon/portalkit/bean/VisibilityBean.java
@@ -10,6 +10,7 @@ import org.apache.commons.lang.StringUtils;
 
 import ch.ivy.addon.portalkit.enums.GlobalVariable;
 import ch.ivy.addon.portalkit.service.GlobalSettingService;
+import com.axonivy.portal.components.util.HtmlParser;
 
 @ManagedBean
 @ViewScoped
@@ -17,21 +18,26 @@ public class VisibilityBean implements Serializable {
 
   private static final long serialVersionUID = 6402332157509278585L;
   private GlobalSettingService globalSettingService;
-
+  
   @PostConstruct
   public void init() {
     globalSettingService = new GlobalSettingService();
   }
-
+  
   public boolean isShowButtonIcon() {
     return globalSettingService.findGlobalSettingValueAsBoolean(GlobalVariable.SHOW_BUTTON_ICON);
   }
-
+  
   public String generateButtonIcon(String iconClass) {
-    return globalSettingService.findGlobalSettingValueAsBoolean(GlobalVariable.SHOW_BUTTON_ICON) ? iconClass : StringUtils.EMPTY;
+    return globalSettingService.findGlobalSettingValueAsBoolean(GlobalVariable.SHOW_BUTTON_ICON) ? iconClass
+        : StringUtils.EMPTY;
   }
 
   public boolean isShowLoginFooter() {
     return globalSettingService.findGlobalSettingValueAsBoolean(GlobalVariable.SHOW_LOGIN_FOOTER);
+  }
+  
+  public String sanitizeContent(String content) {
+    return HtmlParser.sanitize(content);
   }
 }
