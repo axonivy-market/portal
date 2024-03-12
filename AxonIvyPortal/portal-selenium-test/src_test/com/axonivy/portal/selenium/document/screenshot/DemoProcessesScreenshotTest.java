4513f34aa0404632333bbfff07a9e5e08e054486
tphvu-axonivy <vu.tongphanhoang@axonactive.com>
2024-03-12 09:52:08 +0700

    IVYPORTAL-15715-Refactor-portal-screenshots-to-selenide-for-LTS
    - improve screenshotLeaveRequestProcess

--- AxonIvyPortal/portal-selenium-test/src_test/com/axonivy/portal/selenium/document/screenshot/DemoProcessesScreenshotTest.java
+++ AxonIvyPortal/portal-selenium-test/src_test/com/axonivy/portal/selenium/document/screenshot/DemoProcessesScreenshotTest.java
@@ -74,6 +74,7 @@ public class DemoProcessesScreenshotTest extends ScreenshotBaseTest {
     LeaveRequestPage leaveRequestPage = new LeaveRequestPage();
     leaveRequestPage.switchToIFrameOfTask();
     leaveRequestPage.waitForIFrameContentVisible();
+    leaveRequestPage.waitForLeaveRequestStepRendered();
     ScreenshotUtils.capturePageScreenshot(ScreenshotUtils.DEMO_FOLDER + "leave-request-creation");
   }
 
