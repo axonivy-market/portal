f902a799085b5f5c3e4505072bddf516abca1d33
mnhnam-axonivy <mnhnam@axonactive.com>
2024-03-20 15:05:56 +0700

    IVYPORTAL-16297 Interactive function: List tasks/cases - Improve logic to handle tools - UI/UX: added typing dots when waiting for AI - UI/UX: disable textbox and button when waiting for AI

--- AxonIvyPortal/portal/src/com/axonivy/portal/enums/ai/ToolType.java
+++ AxonIvyPortal/portal/src/com/axonivy/portal/enums/ai/ToolType.java
@@ -2,7 +2,8 @@ package com.axonivy.portal.enums.ai;
 
 public enum ToolType {
 
-  IVY("Ivy", "success"), RETRIEVAL_QA("Q&A", "info");
+  IVY("Ivy", "success"), RETRIEVAL_QA("Q&A", "info"),
+  COMPOSITE("Composite", "warning");
 
   private String label;
   private String tagSeverity;
