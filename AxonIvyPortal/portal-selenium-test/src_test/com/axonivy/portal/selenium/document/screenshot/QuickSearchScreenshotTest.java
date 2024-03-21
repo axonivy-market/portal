477d1f04ea89f211fdf2f07cfd0127f11158a3da
Luat Luong Minh <lmluat@axonactive.com>
2024-03-21 11:00:10 +0700

    feature/IVYPORTAL-16663-Quick-Search-in-Dashboard-Task-list-widget-LTS-GUI-Test-Documentation
    - Update screenshot test

--- AxonIvyPortal/portal-selenium-test/src_test/com/axonivy/portal/selenium/document/screenshot/QuickSearchScreenshotTest.java
+++ AxonIvyPortal/portal-selenium-test/src_test/com/axonivy/portal/selenium/document/screenshot/QuickSearchScreenshotTest.java
@@ -1,7 +1,5 @@
 package com.axonivy.portal.selenium.document.screenshot;
 
-import static com.codeborne.selenide.CollectionCondition.sizeGreaterThanOrEqual;
-
 import java.io.IOException;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -48,25 +46,26 @@ public class QuickSearchScreenshotTest extends ScreenshotBaseTest {
     TaskEditWidgetNewDashBoardPage taskEditWidget = taskWidget.openEditTaskWidget();
 
     ScreenshotUtils.captureElementScreenshot(taskEditWidget.getConfigurationFilter(),
-        ScreenshotUtils.SEARCH_FOLDER + "quick-search-checkbox");
+        ScreenshotUtils.NEW_DASHBOARD_FOLDER + "quick-search-checkbox");
     taskEditWidget.clickOnQuickSearchCheckBox();
     taskEditWidget.openColumnManagementDialog();
+    ScreenshotUtils.resizeBrowser(new Dimension(1366, 1000));
     ScreenshotUtils.captureElementScreenshot(taskEditWidget.getColumnManagementDialog(),
-        ScreenshotUtils.SEARCH_FOLDER + "quick-search-column-management");
+        ScreenshotUtils.NEW_DASHBOARD_FOLDER + "quick-search-column-management");
 
     taskEditWidget.saveColumnMangement();
     taskEditWidget.save();
     taskEditWidget.waitForPageLoad();
     taskWidget.isQuickSearchInputShow("0");
 
-    taskWidget.setInputForQuickSearch("Task number 10");
-    taskWidget.countAllTasks().shouldHave(sizeGreaterThanOrEqual(1));
+    taskWidget.setInputForQuickSearch(" ");
+    taskWidget.clearQuickSearchInput();
     redirectToNewDashBoard();
-    taskWidget.setInputForQuickSearch("Task number 10");
-    taskWidget.countAllTasks().shouldHave(sizeGreaterThanOrEqual(1));
+    ScreenshotUtils.resizeBrowser(new Dimension(1000, 1000));
+    taskWidget.setInputForQuickSearch(" ");
     taskWidget.clearQuickSearchInput();
     taskWidget.clickOnButtonExpandTaskWidget();
-    ScreenshotUtils.resizeBrowserAndCaptureHalfRightScreen(ScreenshotUtils.SEARCH_FOLDER + "quick-search-textbox",
-        new Dimension(1000, 200));
+    ScreenshotUtils.resizeBrowserAndCaptureHalfRightScreen(
+        ScreenshotUtils.NEW_DASHBOARD_FOLDER + "quick-search-textbox", new Dimension(800, 300));
   }
 }
