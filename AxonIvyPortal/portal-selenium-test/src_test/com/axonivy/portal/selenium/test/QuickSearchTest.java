9b9ed3bd3bc4ae14f8874d89a51bfec9c89a200d
Luat Luong Minh <lmluat@axonactive.com>
2024-03-21 13:42:01 +0700

    feature/IVYPORTAL-16663-Quick-Search-in-Dashboard-Task-list-widget-LTS-GUI-Test-Documentation
    - Delete unused imports

--- AxonIvyPortal/portal-selenium-test/src_test/com/axonivy/portal/selenium/test/QuickSearchTest.java
+++ AxonIvyPortal/portal-selenium-test/src_test/com/axonivy/portal/selenium/test/QuickSearchTest.java
@@ -1,9 +1,6 @@
 package com.axonivy.portal.selenium.test;
 
 import static com.codeborne.selenide.CollectionCondition.sizeGreaterThanOrEqual;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.List;
 
