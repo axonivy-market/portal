60facaaba608b42c16d76e08df85af77e70b2870
Loc Nguyen <loc.nguyenthanh@axonactive.com>
2024-03-26 18:34:54 +0700

    IVYPORTAL-16691-Task-widget-information-displays-technical-category-name-10 - Handle feedback

--- AxonIvyPortal/portal/src/com/axonivy/portal/dto/dashboard/WidgetInformationCategoryStatisticData.java
+++ AxonIvyPortal/portal/src/com/axonivy/portal/dto/dashboard/WidgetInformationCategoryStatisticData.java
@@ -2,13 +2,6 @@ package com.axonivy.portal.dto.dashboard;
 
 import java.io.Serializable;
 
-import org.apache.commons.lang3.StringUtils;
-
-import ch.ivyteam.ivy.cm.exec.LocalizedTextResolverFactory;
-import ch.ivyteam.ivy.environment.Ivy;
-import ch.ivyteam.ivy.workflow.category.Category;
-import ch.ivyteam.ivy.workflow.category.CategoryPath;
-
 public class WidgetInformationCategoryStatisticData implements Serializable {
 
   private static final long serialVersionUID = 8763058243562205725L;
@@ -35,12 +28,6 @@ public class WidgetInformationCategoryStatisticData implements Serializable {
 
   public void setCategoryTechnicalPath(String categoryTechnicalPath) {
     this.categoryTechnicalPath = categoryTechnicalPath;
-    if (StringUtils.isNotEmpty(categoryTechnicalPath)) {
-      Category category = Category.createFor(new CategoryPath(categoryTechnicalPath),
-          new LocalizedTextResolverFactory().createFor("Categories", Ivy.request().getProcessModelVersion()));
-      this.categoryDisplayName = category.getName();
-      this.categoryDisplayPath = category.getPath();
-    }
   }
 
   public long getSummary() {
@@ -57,12 +44,5 @@ public class WidgetInformationCategoryStatisticData implements Serializable {
 
   public void setCategoryDisplayPath(String categoryDisplayPath) {
     this.categoryDisplayPath = categoryDisplayPath;
-  }
-
-  @Override
-  public String toString() {
-    return "WidgetInformationCategoryStatisticData [categoryDisplayName=" + categoryDisplayName
-        + ", categoryTechnicalPath=" + categoryTechnicalPath + ", summary=" + summary + ", categoryDisplayPath="
-        + categoryDisplayPath + "]";
   }
 }
