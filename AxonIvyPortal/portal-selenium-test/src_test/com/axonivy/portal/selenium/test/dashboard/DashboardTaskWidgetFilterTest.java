75e689170c0f5769238f4b4bd6c868c12c91c40f
Luat Luong Minh <lmluat@axonactive.com>
2024-04-05 12:01:35 +0700

    feature/IVYPORTAL-16355-Task-Widget-Complex-Filters-GUI-Tests-&-Screenshots-11
    - Update gui test

--- AxonIvyPortal/portal-selenium-test/src_test/com/axonivy/portal/selenium/test/dashboard/DashboardTaskWidgetFilterTest.java
+++ AxonIvyPortal/portal-selenium-test/src_test/com/axonivy/portal/selenium/test/dashboard/DashboardTaskWidgetFilterTest.java
@@ -19,7 +19,7 @@ import com.codeborne.selenide.CollectionCondition;
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.ElementsCollection;
 
-@IvyWebTest(headless = false)
+@IvyWebTest
 public class DashboardTaskWidgetFilterTest extends BaseTest {
   // WIDGET NAME
   private static final String YOUR_TASK_WIDGET = "Your Tasks";
