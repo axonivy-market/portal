6cbad29f30d3f8789e5c37e574dcd24c3bfc5401
mnhnam-axonivy <mnhnam@axonactive.com>
2024-04-29 09:49:35 +0700

    IVYPORTAL-16026 Clean Code & Fix remaining bug for complex filtering - Fixed bug: Cannot find tasks by filter Responsible - Fixed bug: Task state filter show enum name instead of user friendly name - Enhance UI: Enhance layout of the filter panel - Enhance UI: Chips input smaller than text input - Enhance UI: Title too small

--- AxonIvyPortal/portal/webContent/resources/js/sidebar-widget-configuration.js
+++ AxonIvyPortal/portal/webContent/resources/js/sidebar-widget-configuration.js
@@ -1,20 +1,22 @@
 var filterPanelId = 'widget-configuration-form:new-widget-configuration-component:widget-filter-content';
 
 function openFilterPanel(event) {
-  document.getElementById(filterPanelId).style.right = calculateRightPosition();
   event.stopPropagation();
-  document.getElementById(filterPanelId).classList.add("active");
+  var filterPanel = document.getElementById(filterPanelId);
+  filterPanel.classList.add("active");
+  $('#new-widget-configuration-dialog').find('#widget-configuration-save-button').hide();
+  calculatePaneltPosition();
 }
 
 function closeFilterPanel() {
-  document.getElementById(filterPanelId).style.right = calculateRightPosition();
+  $('#new-widget-configuration-dialog').find('#widget-configuration-save-button').show();
   document.getElementById(filterPanelId).classList.remove("active");
 }
 
 $(document).ready(function () {
   registerClickOnPanelEvent();
 
-  document.getElementById(filterPanelId).style.right = calculateRightPosition();
+  calculatePaneltPosition();
   
   let validationMsg = document.getElementById("widget-configuration-form:new-widget-configuration-component:filter-messages").textContent;
   if (validationMsg !== ""){
@@ -32,9 +34,11 @@ function registerClickOnPanelEvent(){
   });
 }
 
-function calculateRightPosition(){
-  var x = document.getElementById('new-widget-configuration-dialog_content').clientWidth;
-  var y = document.getElementById('widget-configuration-form:new-widget-configuration-component:widget-preview').offsetWidth;
-  
-  document.getElementById(filterPanelId).style.right = (x > y ? 0 : x-y-20) + "px";
+function calculatePaneltPosition(){
+  var newWidgetDialog = document.getElementById('new-widget-configuration-dialog');
+  var filterPanel = document.getElementById(filterPanelId);
+
+  filterPanel.style.right = newWidgetDialog.style.left;
+  filterPanel.style.top = newWidgetDialog.style.top;
+  filterPanel.style.height = newWidgetDialog.clientHeight + 'px';
 }
