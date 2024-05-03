f06550ebc23a63d490e13a6fe65cd3eb682a431f
Luong Minh Luat <157108501+lmluat-axonivy@users.noreply.github.com>
2024-05-03 15:56:47 +0700

    Feature/ivyportal 16355 task widget complex filters g UI tests & screenshots 11 (#632)
    * feature/IVYPORTAL-16355-Task-Widget-Complex-Filters-GUI-Tests-&-Screenshots-11
    
    - Update jenkinsfile
    
    * feature/IVYPORTAL-16355-Task-Widget-Complex-Filters-GUI-Tests-&-Screenshots-11
    
    - Update gui test
    
    * feature/IVYPORTAL-16355-Task-Widget-Complex-Filters-GUI-Tests-&-Screenshots-11
    
    - Update WaitHelper class
    
    * IVYPORTAL-16355-Task-Widget-Complex-Filters-GUI-Tests-&-Screenshots-11 - Fix test
    
    * IVYPORTAL-16355-Task-Widget-Complex-Filters-GUI-Tests-&-Screenshots-11 - Fix test
    
    * IVYPORTAL-16355-Task-Widget-Complex-Filters-GUI-Tests-&-Screenshots-11 - Fix test
    
    * IVYPORTAL-16355 Task-Widget - Complex Filters - GUI-Tests & Screenshots & Documentation
    - Fixed gui test
    
    ---------
    
    Co-authored-by: Luong Minh Luat <lmluat@axonactive.com>
    Co-authored-by: Loc Nguyen <loc.nguyenthanh@axonactive.com>
    Co-authored-by: mnhnam-axonivy <mnhnam@axonactive.com>

--- AxonIvyPortal/portal-selenium-test/src_test/com/axonivy/portal/selenium/page/CaseEditWidgetNewDashBoardPage.java
+++ AxonIvyPortal/portal-selenium-test/src_test/com/axonivy/portal/selenium/page/CaseEditWidgetNewDashBoardPage.java
@@ -29,30 +29,6 @@ public class CaseEditWidgetNewDashBoardPage extends TemplatePage {
     this.caseEditWidgetId = caseWidgetId;
   }
 
-  private int getIndexFiltertByName(String name) {
-    ElementsCollection elementsTH =
-        $(caseEditWidgetId).shouldBe(appear, DEFAULT_TIMEOUT).$("div[id$='user-filter']").$$("div");
-    for (int i = 0; i < elementsTH.size(); i++) {
-      if (elementsTH.get(i).getText().equalsIgnoreCase(name)) {
-        return i;
-      }
-    }
-    return 0;
-  }
-
-  private SelenideElement getAvailableFilterInput(String filterName) {
-    int index = getIndexFiltertByName(filterName);
-    return $(caseEditWidgetId).shouldBe(appear, DEFAULT_TIMEOUT).$("div[id$='user-filter']").$$("div").get(index + 1)
-        .$("input");
-  }
-
-  private SelenideElement getAvailableFilterCheckbox(String filterName) {
-    int index = getIndexFiltertByName(filterName);
-    return $(caseEditWidgetId).shouldBe(appear, DEFAULT_TIMEOUT).$("div[id$='user-filter']").$$("div").get(index + 1)
-        .$(".ui-selectcheckboxmenu");
-  }
-
-
   private SelenideElement getValueOfCheckBox(String value) {
     return $("div.ui-selectcheckboxmenu-items-wrapper").shouldBe(appear, DEFAULT_TIMEOUT)
         .$$("li.ui-selectcheckboxmenu-item").filter(text(value)).first().$("div.ui-chkbox-box");
@@ -116,6 +92,7 @@ public class CaseEditWidgetNewDashBoardPage extends TemplatePage {
   public void waitPreviewTableLoaded() {
     $(caseEditWidgetId).$("div[id$=':dashboard-cases-container']").shouldBe(appear, DEFAULT_TIMEOUT);
     $(caseEditWidgetId).$(".case-dashboard-widget__loading-message").shouldHave(Condition.cssClass("u-display-none"), DEFAULT_TIMEOUT);
+    $(caseEditWidgetId).$("div[id$=':dashboard-cases-container']").shouldBe(appear, DEFAULT_TIMEOUT).shouldNotHave(Condition.cssClass("u-display-none"));
     $(caseEditWidgetId).shouldBe(appear, DEFAULT_TIMEOUT).$("button[id$='preview-button']").shouldNotHave(Condition.attribute("disabled", "disabled"),DEFAULT_TIMEOUT);
   }
 
@@ -338,9 +315,6 @@ public class CaseEditWidgetNewDashBoardPage extends TemplatePage {
   public void saveAfterAddingCustomField() {
     saveColumn();
     save();
-    $("span#dashboard-header-action").shouldBe(Condition.appear, DEFAULT_TIMEOUT).$("button#back-to-configuration")
-        .shouldBe(Condition.appear, DEFAULT_TIMEOUT).click();
-    $("button#back-to-home-button").shouldBe(Condition.appear, DEFAULT_TIMEOUT).click();
   }
 
   public void addCustomColumns(String... fieldNameList) {
