911101aac39497e99b3b1ad99d92fe590d4d1822
mnhnam-axonivy <mnhnam@axonactive.com>
2024-05-06 10:51:33 +0700

    IVYPORTAL-16026 Clean Code & Fix remaining bug for complex filtering - Fixed bug: Data of old filter not clear after choose "please select"

--- AxonIvyPortal/portal/src/com/axonivy/portal/dto/dashboard/filter/DashboardFilter.java
+++ AxonIvyPortal/portal/src/com/axonivy/portal/dto/dashboard/filter/DashboardFilter.java
@@ -51,6 +51,9 @@ public class DashboardFilter implements Serializable {
   @JsonIgnore
   public static final String DMY_DATE_FORMAT = "dd.MM.yyyy";
 
+  @JsonIgnore
+  private static final String DEFAULT = "default";
+
   private String field;
 
   private String from;
@@ -153,7 +156,12 @@ public class DashboardFilter implements Serializable {
   @JsonIgnore
   public boolean isTextField() {
     return (this.filterFormat == FilterFormat.TEXT || this.filterFormat == FilterFormat.STRING) && !isCategory()
-        && !isId() && !isApplication();
+        && !isId() && !isApplication() && !isDefaultField();
+  }
+
+  @JsonIgnore
+  public boolean isDefaultField() {
+    return StringUtils.isBlank(this.field);
   }
 
   @JsonIgnore
