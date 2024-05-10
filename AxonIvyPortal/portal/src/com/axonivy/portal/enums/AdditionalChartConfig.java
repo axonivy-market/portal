91a4ed8949235afd60d13119cc39e53cf1008476
mnhnam-axonivy <mnhnam@axonactive.com>
2024-05-10 17:16:39 +0700

    IVYPORTAL-16036 New Standard Dashboard for LE 11.3 with statistics - Fixed bug: Y axis not show correct metric for chart "Average runtime of cases by Category" - Fixed bug: "No category" cases don't have label - Fixed bug: date time format of KPI chart not show correctly

--- AxonIvyPortal/portal/src/com/axonivy/portal/enums/AdditionalChartConfig.java
+++ AxonIvyPortal/portal/src/com/axonivy/portal/enums/AdditionalChartConfig.java
@@ -1,12 +1,18 @@
 package com.axonivy.portal.enums;
 
+import ch.ivyteam.ivy.environment.Ivy;
+
 public enum AdditionalChartConfig {
-  EMPTY_CHART_DATA_MESSAGE("emptyChartDataMessage");
+  EMPTY_CHART_DATA_MESSAGE("emptyChartDataMessage",
+      "/ch.ivy.addon.portalkit.ui.jsf/dashboard/StatisticWidget/EmptyChartDataMessage"),
+  NO_CATEGORY("noCategory", "/ch.ivy.addon.portalkit.ui.jsf/common/noCategory");
 
   private String key;
+  private String cms;
 
-  private AdditionalChartConfig(String key) {
+  private AdditionalChartConfig(String key, String cms) {
     this.key = key;
+    this.cms = cms;
   }
 
   public String getKey() {
@@ -17,4 +23,11 @@ public enum AdditionalChartConfig {
     this.key = key;
   }
 
+  public String getCms() {
+    return Ivy.cms().co(cms);
+  }
+
+  public void setCms(String cms) {
+    this.cms = cms;
+  }
 }
