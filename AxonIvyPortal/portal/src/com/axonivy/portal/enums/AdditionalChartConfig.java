529d42c4b4dfb2aeaa0b1b46bf0a9de65827d76d
mnhnam-axonivy <mnhnam@axonactive.com>
2024-05-10 17:26:30 +0700

    Revert "IVYPORTAL-16036 New Standard Dashboard for LE 11.3 with statistics"
    This reverts commit 46d535b12cd36714fd9225744d8e835a94b53911.

--- AxonIvyPortal/portal/src/com/axonivy/portal/enums/AdditionalChartConfig.java
+++ AxonIvyPortal/portal/src/com/axonivy/portal/enums/AdditionalChartConfig.java
@@ -1,18 +1,12 @@
 package com.axonivy.portal.enums;
 
-import ch.ivyteam.ivy.environment.Ivy;
-
 public enum AdditionalChartConfig {
-  EMPTY_CHART_DATA_MESSAGE("emptyChartDataMessage",
-      "/ch.ivy.addon.portalkit.ui.jsf/dashboard/StatisticWidget/EmptyChartDataMessage"),
-  NO_CATEGORY("noCategory", "/ch.ivy.addon.portalkit.ui.jsf/common/noCategory");
+  EMPTY_CHART_DATA_MESSAGE("emptyChartDataMessage");
 
   private String key;
-  private String cms;
 
-  private AdditionalChartConfig(String key, String cms) {
+  private AdditionalChartConfig(String key) {
     this.key = key;
-    this.cms = cms;
   }
 
   public String getKey() {
@@ -23,11 +17,4 @@ public enum AdditionalChartConfig {
     this.key = key;
   }
 
-  public String getCms() {
-    return Ivy.cms().co(cms);
-  }
-
-  public void setCms(String cms) {
-    this.cms = cms;
-  }
 }
