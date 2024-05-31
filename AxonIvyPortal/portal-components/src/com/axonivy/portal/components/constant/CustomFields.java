1b6aa29be83c6f929cd2fcb1193f52c74d679616
nhthinh-axonivy <thinh.nguyenhung@axonactive.com>
2024-05-31 16:58:18 +0700

    IVYPORTAL-16222-Process-URLs-like-CaseDetails-Path-should-be-more-relative

--- AxonIvyPortal/portal-components/src/com/axonivy/portal/components/constant/CustomFields.java
+++ AxonIvyPortal/portal-components/src/com/axonivy/portal/components/constant/CustomFields.java
@@ -4,4 +4,6 @@ public class CustomFields {
   public static final String IS_EXPRESS_PROCESS = "isExpress";
   public static final String IS_DASHBOARD_PROCESS = "isDashboardProcess";
   public static final String BUSINESS_DETAILS = "businessDetails";
+  public static final String EMBED_IN_FRAME = "embedInFrame";
 }
+
--- AxonIvyPortal/portal/src/ch/ivy/addon/portalkit/bean/CaseActionBean.java
+++ AxonIvyPortal/portal/src/ch/ivy/addon/portalkit/bean/CaseActionBean.java
@@ -12,6 +12,8 @@ import javax.faces.bean.RequestScoped;
 import org.apache.commons.lang3.BooleanUtils;
 import org.apache.commons.lang3.StringUtils;
 
+import com.axonivy.portal.components.publicapi.IWebStartableAPI;
+
 import ch.ivy.addon.portal.generic.navigation.PortalNavigator;
 import ch.ivy.addon.portalkit.constant.CustomFields;
 import ch.ivy.addon.portalkit.enums.AdditionalProperty;
@@ -21,8 +23,11 @@ import ch.ivy.addon.portalkit.service.ExpressProcessService;
 import ch.ivy.addon.portalkit.service.GlobalSettingService;
 import ch.ivy.addon.portalkit.util.PermissionUtils;
 import ch.ivy.addon.portalkit.util.PortalProcessViewerUtils;
+import ch.ivyteam.ivy.application.IApplication;
+import ch.ivyteam.ivy.environment.Ivy;
 import ch.ivyteam.ivy.security.IPermission;
 import ch.ivyteam.ivy.workflow.ICase;
+import ch.ivyteam.ivy.workflow.start.IWebStartable;
 
 @ManagedBean
 @RequestScoped
@@ -55,8 +60,44 @@ public class CaseActionBean implements Serializable {
       additionalCaseDetailsPageUri = iCase.customFields()
           .textField(AdditionalProperty.CUSTOMIZATION_ADDITIONAL_CASE_DETAILS_PAGE.toString()).getOrNull();
       if (StringUtils.isNotEmpty(additionalCaseDetailsPageUri)) {
-        additionalCaseDetailsPageUri += (additionalCaseDetailsPageUri.contains("?") ? "&" : "?").concat("embedInFrame");
+        Boolean isEmbedInFrame = true;
+        if (!additionalCaseDetailsPageUri.contains("embedInFrame")) {
+          isEmbedInFrame = false;
+        }
+        String processUrl = removeQueryParameters(additionalCaseDetailsPageUri);
+        IWebStartable iWebStartable = IWebStartableAPI.findIWebStartableByProcessRelativeLink(processUrl);
+        iCase.customFields().stringField(com.axonivy.portal.components.constant.CustomFields.BUSINESS_DETAILS)
+            .set(iWebStartable.getId());
+        iCase.customFields().numberField(CustomFields.EMBED_IN_FRAME).set(isEmbedInFrame ? 1 : 0);
+        Ivy.log().error("Case 3 Finall: " + iWebStartable.getLink().getRelative() + "?uuid=" + iCase.uuid()
+            + (isEmbedInFrame ? "&embedInFrame" : ""));
+        return iWebStartable.getLink().getRelative() + "?uuid=" + iCase.uuid()
+            + (isEmbedInFrame ? "&embedInFrame" : "");
       }
+    } else if (additionalCaseDetailsPageUri.startsWith(IApplication.current().getName())) {
+      Ivy.log().error("case 1");
+      Boolean isEmbedInFrame =
+          (iCase.customFields().numberField(com.axonivy.portal.components.constant.CustomFields.EMBED_IN_FRAME).get()
+              .orElse(1).intValue() == 1 ? true : false);
+      return additionalCaseDetailsPageUri + "?uuid=" + iCase.uuid() + (isEmbedInFrame ? "&embedInFrame" : "");
+    } else if (detectExternalLink(additionalCaseDetailsPageUri)) {
+      Ivy.log().error("case 2");
+      return additionalCaseDetailsPageUri;
+    } else {
+      Ivy.log().error("case 3");
+      Ivy.log().error("URL____" + additionalCaseDetailsPageUri);
+      Boolean isEmbedInFrame = true;
+      if (!additionalCaseDetailsPageUri.contains("embedInFrame")) {
+        isEmbedInFrame = false;
+      }
+      String processUrl = removeQueryParameters(additionalCaseDetailsPageUri);
+      IWebStartable iWebStartable = IWebStartableAPI.findIWebStartableByProcessRelativeLink(processUrl);
+      iCase.customFields().stringField(com.axonivy.portal.components.constant.CustomFields.BUSINESS_DETAILS)
+          .set(iWebStartable.getId());
+      iCase.customFields().numberField(CustomFields.EMBED_IN_FRAME).set(isEmbedInFrame ? 1 : 0);
+      Ivy.log().error("Case 3 Finall: " + iWebStartable.getLink().getRelative() + "?uuid=" + iCase.uuid()
+          + (isEmbedInFrame ? "&embedInFrame" : ""));
+      return iWebStartable.getLink().getRelative() + "?uuid=" + iCase.uuid() + (isEmbedInFrame ? "&embedInFrame" : "");
     }
     if (StringUtils.isEmpty(additionalCaseDetailsPageUri)) {
       Map<String, String> params = new HashMap<>();
@@ -118,4 +159,19 @@ public class CaseActionBean implements Serializable {
   public boolean showProcessViewer(ICase caze) {
     return PortalProcessViewerUtils.isShowProcessViewer(caze);
   }
+
+  private static boolean detectExternalLink(String path) {
+    return StringUtils.startsWithIgnoreCase(path, "http") || StringUtils.startsWithIgnoreCase(path, "https");
+  }
+
+  private String removeQueryParameters(String url) {
+    int queryIndex = url.indexOf("?");
+    if (queryIndex != -1) {
+      return url.substring(0, queryIndex);
+    } else {
+      return url;
+    }
+  }
+
+
 }
