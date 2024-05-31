c741019cd3368df5369855fc45027b7b0546f69e
nhthinh-axonivy <thinh.nguyenhung@axonactive.com>
2024-05-31 18:16:34 +0700

    IVYPORTAL-16222-Process-URLs-like-CaseDetails-Path-should-be-more-relative _ Refactor BusinessDetailsAPI _ Handle old Process Path Url _ Adapt Process Path to IWebstartable _ Store new IWebstartable ID _ Handle silence migration for old Process Path Url to new IWebstartable ID

--- AxonIvyPortal/portal-components/src/com/axonivy/portal/components/publicapi/IWebStartableAPI.java
+++ AxonIvyPortal/portal-components/src/com/axonivy/portal/components/publicapi/IWebStartableAPI.java
@@ -9,47 +9,45 @@ import ch.ivyteam.ivy.environment.Ivy;
 import ch.ivyteam.ivy.workflow.IWorkflowSession;
 import ch.ivyteam.ivy.workflow.start.IWebStartable;
 
+/**
+ * <p>Finds an {@code IWebStartable} by its process relative link.</p>
+ * <p>This method searches for an {@code IWebStartable} based on the provided process path. It returns the {@code IWebStartable} if found.</p>
+ * <p>The process path can be either a relative link to a process or an external URL.</p>
+ *
+ * <p>Example:</p>
+ * <pre>{@code
+ * IWebStartable startable = IWebStartableAPI.findIWebStartableByProcessRelativeLink("designer/pro/portal/1549F58C18A6C562/DefaultApplicationHomePage.ivp");
+ * }</pre>
+ *
+ * <p>If the {@code processPath} starts with "/", the method removes the leading slash before processing.</p>
+ *
+ * @param processPath The process path or external link to search for.
+ * @return The {@code IWebStartable} if found.
+ * @throws PortalException if the {@code IWebStartable} cannot be found.
+ */
 public class IWebStartableAPI {
-
+  
   private IWebStartableAPI() {}
 
   public static IWebStartable findIWebStartableByProcessRelativeLink(String processPath) {
-    // Retrieve all IWebStartable instances from the current session
     List<IWebStartable> iWebStartables = IWorkflowSession.current().getAllStartables().toList();
     IWebStartable targetStartable = null;
-
-    // Remove leading slash if present
     if (processPath.startsWith("/")) {
       processPath = processPath.substring(1);
     }
-
-    // Extract the ending segment of the processPath
-    // For example, from
-    // "/designer/pro/portal-components-examples/176465FBFE257CF3/showInvestmentRequestCustomFields.ivp"
-    // we want to extract "showInvestmentRequestCustomFields.ivp"
     String[] processPathSegments = processPath.split("/");
     String remainingPath = String.join("/", Arrays.copyOfRange(processPathSegments, 1, processPathSegments.length));
-    // Iterate over the list of startables
     Ivy.log().error(remainingPath);
     for (IWebStartable startable : iWebStartables) {
       String startableRelativeLink = startable.getLink().getRelative();
-      Ivy.log().error(startableRelativeLink); // Log each startable relative link
-
-      // Check if the startable relative link ends with the processPathEndingSegment
       if (startableRelativeLink.endsWith(remainingPath)) {
         targetStartable = startable;
-        break; // Exit the loop once the target startable is found
+        break;
       }
     }
-
-    // If no matching startable is found, throw an exception
     if (targetStartable == null) {
       throw new PortalException(String.format("Cannot find IWebStartable by process path [%s].", processPath));
     }
-
-    // Log and return the found startable
-    Ivy.log().error(targetStartable.getLink().getRelative());
     return targetStartable;
   }
-
 }
