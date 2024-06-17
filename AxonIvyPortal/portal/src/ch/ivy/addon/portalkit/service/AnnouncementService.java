f29bbce1ffaa3ea391cea62a87acbd71e5cd2803
lmluat-axonivy <luat.luongminh@axonactive.com>
2024-06-17 15:40:28 +0700

    feature/IVYPORTAL-16878-New-Statistics-Documentation-GUI-Test
    - Update files

--- AxonIvyPortal/portal/src/ch/ivy/addon/portalkit/service/AnnouncementService.java
+++ AxonIvyPortal/portal/src/ch/ivy/addon/portalkit/service/AnnouncementService.java
@@ -39,7 +39,7 @@ public class AnnouncementService {
   public boolean isDefaultApplicationLanguage(String language) {
     return Sudo.get(
         () -> {
-          Locale content = LanguageService.newInstance().getDefaultEmailLanguage();
+          Locale content = LanguageService.getInstance().getDefaultEmailLanguage();
           return content.getLanguage().equalsIgnoreCase(language);
         });
   }
@@ -98,7 +98,7 @@ public class AnnouncementService {
       languageToAnnouncements = new HashMap<>();
     }
     
-    IvyLanguage ivyLanguage = LanguageService.newInstance().getIvyLanguageOfUser();
+    IvyLanguage ivyLanguage = LanguageService.getInstance().getIvyLanguageOfUser();
 
     List<String> supportedLanguages = ivyLanguage.getSupportedLanguages();
     
@@ -117,7 +117,7 @@ public class AnnouncementService {
 
 
   private String getDefaultEmailLanguage() {
-    return LanguageService.newInstance().getDefaultEmailLanguage().toLanguageTag();
+    return LanguageService.getInstance().getDefaultEmailLanguage().toLanguageTag();
   }
 
   private Announcement getAnnouncement() {
