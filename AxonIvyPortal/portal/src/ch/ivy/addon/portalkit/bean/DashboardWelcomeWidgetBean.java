b7245216e580496c2863da897583ab4bfc32b960
Le Ngoc Danh <127722015+lndanh-axonivy@users.noreply.github.com>
2024-06-19 11:52:26 +0700

    Bug/ivyportal 16925 welcome widget does not show image (#780)
    * IVYPORTAL-16925 Welcome widget does not show image: fix bug welcome widget
    
    * IVYPORTAL-16925 Welcome widget does not show image: fix bug welcome widget
    
    ---------
    
    Co-authored-by: lndanh-axonivy <lndanh@axonactive.com>

--- AxonIvyPortal/portal/src/ch/ivy/addon/portalkit/bean/DashboardWelcomeWidgetBean.java
+++ AxonIvyPortal/portal/src/ch/ivy/addon/portalkit/bean/DashboardWelcomeWidgetBean.java
@@ -3,6 +3,8 @@ package ch.ivy.addon.portalkit.bean;
 import static org.apache.commons.lang3.StringUtils.SPACE;
 
 import java.io.Serializable;
+import java.util.Base64;
+import java.util.List;
 import java.util.Optional;
 
 import javax.faces.bean.ManagedBean;
@@ -13,11 +15,18 @@ import org.apache.commons.lang3.StringUtils;
 
 import com.axonivy.portal.util.WelcomeWidgetUtils;
 
+import ch.addon.portal.generic.menu.MenuView;
 import ch.ivy.addon.portalkit.dto.DisplayName;
+import ch.ivy.addon.portalkit.dto.dashboard.Dashboard;
 import ch.ivy.addon.portalkit.dto.dashboard.WelcomeDashboardWidget;
+import ch.ivy.addon.portalkit.enums.DashboardWidgetType;
+import ch.ivy.addon.portalkit.enums.PortalVariable;
 import ch.ivy.addon.portalkit.enums.WelcomeTextPosition;
 import ch.ivy.addon.portalkit.jsf.Attrs;
+import ch.ivy.addon.portalkit.jsf.ManagedBeans;
+import ch.ivy.addon.portalkit.persistence.converter.BusinessEntityConverter;
 import ch.ivy.addon.portalkit.support.HtmlParser;
+import ch.ivy.addon.portalkit.util.DashboardUtils;
 import ch.ivy.addon.portalkit.util.UserUtils;
 import ch.ivyteam.ivy.cm.ContentObject;
 import ch.ivyteam.ivy.environment.Ivy;
@@ -67,9 +76,32 @@ public class DashboardWelcomeWidgetBean implements Serializable {
     if (WelcomeWidgetUtils.isObsoleteImageData(widget.getImageLocation(), widget.getId())) {
       WelcomeWidgetUtils.migrateWelcomeWidget(widget.getId(), widget.getImageType(), widget.getImageLocation());
     }
-    return WelcomeWidgetUtils.getImageContentObject(widget.getImageLocation(), widget.getImageType());
+    ContentObject imageContent = WelcomeWidgetUtils.getImageContentObject(widget.getImageLocation(), widget.getImageType());
+    removeImageContentOfWidget(imageContent);
+    return imageContent;
   }
 
+  private void removeImageContentOfWidget(ContentObject imageContent) {
+    if (StringUtils.isNotBlank(widget.getImageContent())) {
+      WelcomeWidgetUtils.readObjectValueOfDefaultLocale(imageContent).write().bytes(Base64.getDecoder().decode(widget.getImageContent()));
+      List<Dashboard> dashboards = DashboardUtils.collectDashboards();
+      for (Dashboard dashboard :  dashboards) {
+        dashboard.getWidgets().stream()
+        .filter(item -> widget.getId().equals(item.getId()) && item.getType() == DashboardWidgetType.WELCOME)
+        .findFirst()
+        .ifPresent(item -> {
+          ((WelcomeDashboardWidget) item).setImageContent(null);
+        });
+      }
+      String dashboardJson = BusinessEntityConverter.entityToJsonValue(dashboards);
+      Ivy.var().set(PortalVariable.DASHBOARD.key, dashboardJson);
+      
+      MenuView menuView = (MenuView) ManagedBeans.get("menuView");
+      menuView.updateDashboardCache(DashboardUtils.collectDashboards());
+    }
+  }
+
+  
   public void updateWelcomeText(WelcomeDashboardWidget welcomeWidget) {
     int parseClientTime = WelcomeWidgetUtils.parseClientTime();
     String greetingTextCms = WelcomeWidgetUtils.generateGreetingTextByTime(parseClientTime);
