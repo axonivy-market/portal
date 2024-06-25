a457c860bfba49907910a7d12a23b1bd4a88d3fe
Tung Le <tung.le@axonactive.com>
2024-06-25 17:22:45 +0700

    IVYPORTAL-16222 Rework - Business Case Detail > Implemented

--- AxonIvyPortal/portal-components/src/com/axonivy/portal/components/publicapi/BusinessDetailsAPI.java
+++ AxonIvyPortal/portal-components/src/com/axonivy/portal/components/publicapi/BusinessDetailsAPI.java
@@ -1,10 +1,16 @@
 package com.axonivy.portal.components.publicapi;
 
+import static com.axonivy.portal.components.constant.CustomFields.BUSINESS_DETAILS;
+
+import java.util.List;
+
 import org.apache.commons.lang.StringUtils;
 
-import com.axonivy.portal.components.constant.CustomFields;
 import com.axonivy.portal.components.dto.BusinessDetailsDTO;
 import com.axonivy.portal.components.service.exception.PortalException;
+
+import ch.ivyteam.ivy.workflow.IWorkflowSession;
+import ch.ivyteam.ivy.workflow.start.IWebStartable;
 
 /**
  * Portal API for modify business case details
@@ -17,12 +23,14 @@ public class BusinessDetailsAPI {
    * <p>Use {@code com.axonivy.portal.components.dto.BusinessDetailsDTO.builder()} to
    * build {@code BusinessDetailsDTO}</p>
    * <p>This API supports two types of business details:
-   * process path and external link.</p> 
+   * {@link IWebStartable#getId() IWebStartable ID} and external link.</p> 
    *
    * <p>Examples:</p>
-   * <p>For process path:</p>
-   * <pre><code>BusinessDetailsAPI.create(BusinessDetailsDTO.builder().path("Start Processes/ProcessDemo").build());</code></pre>
-   *
+   * <p>For IWebStartable ID:</p>
+   * <pre><code>BusinessDetailsAPI.create(BusinessDetailsDTO.builder()
+   * .path("designer/portal-components-examples/Start Processes/BusinessDetails/showInvestmentRequestCustomFields.ivp").build());</code></pre>
+   * Noted that you could use the end part of an IWebStartable ID like `Start Processes/BusinessDetails/showInvestmentRequestCustomFields.ivp` 
+   * but you need to make sure there is only one IWebStartable in the security context ends with that path.
    * <p>For external link:</p>
    * <pre><code>BusinessDetailsAPI.create(BusinessDetailsDTO.builder().path("https://www.google.com").build());</code></pre>
    *
@@ -33,49 +41,39 @@ public class BusinessDetailsAPI {
    */
   public static void create(BusinessDetailsDTO businessDetailsDTO) {
     String customField;
-    String pageUrl = createPageUrl(businessDetailsDTO);
-    if (detectExternalLink(pageUrl)) {
-      customField = pageUrl;
+    String path = businessDetailsDTO.getPath();
+    if (detectExternalLink(path)) {
+      customField = path;
     } else {
-      customField = IWebStartableAPI.findIWebStartableByProcessRelativeLinkVer10(pageUrl).getId();
+      List<IWebStartable> iWebStartables = IWorkflowSession.current().getAllStartables().toList();
+
+      IWebStartable targetStartable =
+          iWebStartables.stream().filter(startable -> startable.getId().endsWith(path)).findAny().orElseThrow(
+              () -> new PortalException(String.format("Cannot find IWebStartable by ID [%s].", path)));
+      customField = targetStartable.getId();
+      if (businessDetailsDTO.isEmbedInFrame()) {
+        customField += "?embedInFrame";
+      }
     }
-    setToCustomField(businessDetailsDTO, customField);
+    businessDetailsDTO.getCase().customFields().stringField(BUSINESS_DETAILS).set(customField);
   }
-  
+
   /**
    * <p>Creates and sets path to case custom string field {@code businessDetails} of current case for displaying business case details page.</p>
    *
    * <p>Examples:</p>
-   * <pre><code>BusinessDetailsAPI.create("Start Processes/ProcessDemo");</code></pre>
+   * <pre><code>BusinessDetailsAPI.create("designer/portal-components-examples/Start Processes/BusinessDetails/showInvestmentRequestCustomFields.ivp");</code></pre>
    * <pre><code>BusinessDetailsAPI.create("https://www.google.com");</code></pre>
    *
-   * @param link String
+   * @param link String could be an external link or {@link IWebStartable#getId() IWebStartable ID}
    */
   public static void create(String link) {
     BusinessDetailsDTO businessDetailsDTO = BusinessDetailsDTO.builder().path(link).build();
     create(businessDetailsDTO);
   }
   
-  private static void setToCustomField(BusinessDetailsDTO businessDetailDTO, String customField) {
-    businessDetailDTO.getCase().customFields().stringField(CustomFields.BUSINESS_DETAILS).set(customField);
-  }
-
-  private static String createPageUrl(BusinessDetailsDTO businessDetailsDTO) {
-    String casePageUrl = null;
-    if (detectExternalLink(businessDetailsDTO.getPath())) {
-      casePageUrl = businessDetailsDTO.getPath();
-    } else {
-      String processPath =
-          ProcessStartAPI.findRelativeUrlByProcessStartFriendlyRequestPath(businessDetailsDTO.getPath());
-      if (StringUtils.isEmpty(processPath)) {
-        throw new PortalException(String.format("Cannot find process path [%s].", businessDetailsDTO.getPath()));
-      }
-      casePageUrl = processPath;
-    }
-    return casePageUrl;
-  }
 
   private static boolean detectExternalLink(String path) {
-    return StringUtils.startsWithIgnoreCase(path, "http");
+    return StringUtils.startsWithIgnoreCase(path, "http:") || StringUtils.startsWithIgnoreCase(path, "https:");
   }
 }
