54228b626afd48c6316ab3b1ecd13644e468fcb9
Le Ngoc Danh <127722015+lndanh-axonivy@users.noreply.github.com>
2024-07-04 15:50:04 +0700

    IVYPORTAL-16411 Performance issue when filtering a list users by role… (#827)
    * IVYPORTAL-16411 Performance issue when filtering a list users by role: update userDTO and roleDTO
    
    * IVYPORTAL-16411 Performance issue when filtering a list users by role: update permission utils to check admin role
    
    * IVYPORTAL-16411 Performance issue when filtering a list users by role: update test data
    
    * IVYPORTAL-16411 Performance issue when filtering a list users by role: remove unused log
    
    ---------
    
    Co-authored-by: lndanh-axonivy <lndanh@axonactive.com>
    Co-authored-by: mnhnam-axonivy <nam.mai@axonactive.com>

--- AxonIvyPortal/portal/src/ch/ivy/addon/portalkit/ivydata/service/impl/SecurityService.java
+++ AxonIvyPortal/portal/src/ch/ivy/addon/portalkit/ivydata/service/impl/SecurityService.java
@@ -39,6 +39,17 @@ public class SecurityService {
       return result;
     });
   }
+  
+  public IvySecurityResultDTO findUsersWithRoles(String query, int startIndex,
+      int count, List<String> fromRoles, List<String> excludedUsernames) {
+    return Sudo.get(() -> {
+      IvySecurityResultDTO result = new IvySecurityResultDTO();
+      result.setUsers(
+          queryUsersWithRoles(query, startIndex, count, fromRoles,
+              excludedUsernames));
+      return result;
+    });
+  }
 
   /**
    * Query users in specific application
@@ -64,6 +75,24 @@ public class SecurityService {
         .orderBy().fullName().name()
         .executor().results(startIndex, count);
     return users.stream().map(UserDTO::new).collect(Collectors.toList());
+  }
+  
+  private List<UserDTO> queryUsersWithRoles(String query, int startIndex,
+      int count, List<String> fromRoles, List<String> excludedUsernames) {
+    UserQuery userQuery = ISecurityContext.current().users().query();
+
+    IFilterQuery filterQuery = createFilterQuery(query, userQuery);
+
+    if (CollectionUtils.isNotEmpty(fromRoles)) {
+      UserQuery hasRolesQuery = queryHasRoles(fromRoles);
+      filterQuery.andOverall(hasRolesQuery);
+    }
+    excludeUsername(excludedUsernames, filterQuery);
+
+    List<IUser> users = userQuery.orderBy().fullName().name().executor()
+        .results(startIndex, count);
+    return users.stream().map(UserDTO::newUserWithRoles)
+        .collect(Collectors.toList());
   }
 
   private void excludeUsername(List<String> excludedUsernames, IFilterQuery filterQuery) {
