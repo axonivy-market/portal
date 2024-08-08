e63bc37094709c790a44a068c9b96451863ab47f
Luong Minh Luat <157108501+lmluat-axonivy@users.noreply.github.com>
2024-08-08 09:55:37 +0700

    Feature/ivyportal 16656 quick search in processes widget lts (#923)
    * feature/IVYPORTAL-16656-Quick-Search-in-Processes-Widget-LTS
    
    - Implement feature quick search
    
    * feature/IVYPORTAL-16656-Quick-Search-in-Processes-Widget-LTS
    
    - quickSearchKeyword lowercase
    
    * feature/IVYPORTAL-16656-Quick-Search-in-Processes-Widget-LTS
    
    - Update css and add show message when widget empty
    
    * feature/IVYPORTAL-16656-Quick-Search-in-Processes-Widget-LTS
    
    - Create GUI tests for quick search in process widget
    - Update default dashboard quick search json file
    
    * feature/IVYPORTAL-16656-Quick-Search-in-Processes-Widget-LTS
    
    - Change jenkinsfile
    
    * feature/IVYPORTAL-16656-Quick-Search-in-Processes-Widget-LTS
    
    - Set quick search activated as default
    
    * feature/IVYPORTAL-16656-Quick-Search-in-Processes-Widget-LTS
    
    - Add enableQuickSearch into CompactProcessDashboardWidget class
    
    * Revert "feature/IVYPORTAL-16656-Quick-Search-in-Processes-Widget-LTS"
    
    This reverts commit aaf7b8e201dab3c074b80644ceeacca4ad6cc329.

--- AxonIvyPortal/portal/src/ch/ivy/addon/portalkit/dto/dashboard/ProcessDashboardWidget.java
+++ AxonIvyPortal/portal/src/ch/ivy/addon/portalkit/dto/dashboard/ProcessDashboardWidget.java
@@ -140,4 +140,9 @@ public class ProcessDashboardWidget extends DashboardWidget {
   public boolean isShowWidgetInfo() {
     return showWidgetInfo;
   }
+
+  @Override
+  public void setQuickSearchKeyword() {
+    this.criteria.setQuickSearchKeyword(this.getQuickSearchKeyword());
+  }
 }
