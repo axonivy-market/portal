958496826936d689a061247f2be09b16d3393885
Luong Minh Luat <157108501+lmluat-axonivy@users.noreply.github.com>
2024-08-08 09:55:37 +0700

    Feature/ivyportal 16656 quick search in processes widget lts (#923)
    * feature/IVYPORTAL-16656-Quick-Search-in-Processes-Widget-LTS
    
    - Implement feature quick search
    
    * feature/IVYPORTAL-16656-Quick-Search-in-Processes-Widget-LTS
    
    - quickSearchKeyword lowercase
    
    * feature/IVYPORTAL-16656-Quick-Search-in-Processes-Widget-LTS
    
    - Update css and add show message when widget empty
    
    * feature/IVYPORTAL-16656-Quick-Search-in-Processes-Widget-LTS
    
    - Create GUI tests for quick search in process widget
    - Update default dashboard quick search json file
    
    * feature/IVYPORTAL-16656-Quick-Search-in-Processes-Widget-LTS
    
    - Change jenkinsfile
    
    * feature/IVYPORTAL-16656-Quick-Search-in-Processes-Widget-LTS
    
    - Set quick search activated as default
    
    * feature/IVYPORTAL-16656-Quick-Search-in-Processes-Widget-LTS
    
    - Add enableQuickSearch into CompactProcessDashboardWidget class
    
    * Revert "feature/IVYPORTAL-16656-Quick-Search-in-Processes-Widget-LTS"
    
    This reverts commit aaf7b8e201dab3c074b80644ceeacca4ad6cc329.

--- AxonIvyPortal/portal-selenium-test/src_test/com/axonivy/portal/selenium/page/ProcessWidgetNewDashBoardPage.java
+++ AxonIvyPortal/portal-selenium-test/src_test/com/axonivy/portal/selenium/page/ProcessWidgetNewDashBoardPage.java
@@ -5,6 +5,8 @@ import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
 
+import org.openqa.selenium.Keys;
+
 import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.SelenideElement;
 
@@ -60,5 +62,50 @@ public class ProcessWidgetNewDashBoardPage extends TemplatePage {
 
   public boolean isWidgetInfoIconAppear() {
     return getProcessWidgetHeader().$(".widget__info-sidebar-link").isDisplayed();
+  }
+
+  public boolean isQuickSearchInputShow() {
+    waitPageLoaded();
+    return getQuickSearchForm().exists();
+  }
+
+  public void setQuickSearchKeyword(String keyword) {
+    getQuickSearchForm().$("input").setValue(keyword);
+  }
+
+  public SelenideElement getQuickSearchForm() {
+    return $("div[class*='widget-header-quick-search']").shouldBe(appear, DEFAULT_TIMEOUT).$("form");
+  }
+
+  public int getNumberOfProcessListInWidget() {
+    return getProcessWidgetHeader().$("div[id*='widget-content']").shouldBe(appear, DEFAULT_TIMEOUT)
+        .$("div[id*='process-list']").shouldBe(appear, DEFAULT_TIMEOUT).$$("span.process-start-list-item")
+        .size();
+  }
+
+  public void clearQuickSearchInput() {
+    getQuickSearchForm().$("input").clear();
+    waitForPageLoad();
+  }
+
+  public void copyAndPasteOnQuickSearchInput() {
+    SelenideElement searchInput = getQuickSearchForm().$("input");
+    searchInput.click();
+    searchInput.sendKeys(Keys.HOME);
+    searchInput.sendKeys(Keys.LEFT_SHIFT, Keys.END);
+    searchInput.sendKeys(Keys.CONTROL, "C");
+    searchInput.sendKeys(Keys.DELETE);
+    searchInput.sendKeys(Keys.CONTROL, "V");
+    searchInput.sendKeys(Keys.ENTER);
+    waitForPageLoad();
+  }
+
+  public void shiftAndArrowKeyOnQuickSearchInput() {
+    SelenideElement searchInput = getQuickSearchForm().$("input");
+    searchInput.click();
+    searchInput.sendKeys(Keys.HOME);
+    searchInput.sendKeys(Keys.LEFT_SHIFT, Keys.RIGHT, Keys.RIGHT, Keys.RIGHT, Keys.RIGHT);
+    searchInput.sendKeys(Keys.ENTER);
+    waitForPageLoad();
   }
 }
