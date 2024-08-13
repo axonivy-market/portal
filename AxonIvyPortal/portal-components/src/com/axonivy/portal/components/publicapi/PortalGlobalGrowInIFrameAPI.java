4c1009705a4d818b3151e847e1b6d7665a4b5063
Tung Le <tung.le@axonactive.com>
2024-08-13 17:00:24 +0700

    IVYPORTAL-16400 Customize Global Growl Message doesn't work > Clean up

--- AxonIvyPortal/portal-components/src/com/axonivy/portal/components/publicapi/PortalGlobalGrowInIFrameAPI.java
+++ AxonIvyPortal/portal-components/src/com/axonivy/portal/components/publicapi/PortalGlobalGrowInIFrameAPI.java
@@ -9,7 +9,7 @@ import ch.ivyteam.ivy.environment.Ivy;
 /**
  * @deprecated Customizing growl message feature is deprecated and will not be supported in the future
  */
-@Deprecated(forRemoval = true, since = "10.0.23")
+@Deprecated(forRemoval = true, since = "10.0.24")
 public class PortalGlobalGrowInIFrameAPI {
 
   public PortalGlobalGrowInIFrameAPI() {}
@@ -20,7 +20,7 @@ public class PortalGlobalGrowInIFrameAPI {
    * @param message
    * @deprecated Customizing growl message feature is deprecated and will not be supported in the future
    */
-  @Deprecated(forRemoval = true, since = "10.0.23")
+  @Deprecated(forRemoval = true, since = "10.0.24")
   public void displayCustomizedMessage(String message) {
     String statement = "parent.displayPortalGrowlMessageCommand([{name: 'taskId', value: " + Ivy.wfTask().getId()
         + "},{name: 'overridePortalGrowl', value: true},{name: 'portalGrowlMessage', value: '"
