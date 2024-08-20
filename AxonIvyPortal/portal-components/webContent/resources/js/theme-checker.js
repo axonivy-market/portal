907431a7d3d579889dcad887460c6c69d9555c17
lmluat-axonivy <luat.luongminh@axonactive.com>
2024-08-20 14:58:51 +0700

    feature/IVYPORTAL-17255-Rework-contrast-for-portal-sites-All-Portal-Sites-LTS
    - Update theme-checker.js file to change bg color of task case list page

--- AxonIvyPortal/portal-components/webContent/resources/js/theme-checker.js
+++ AxonIvyPortal/portal-components/webContent/resources/js/theme-checker.js
@@ -12,17 +12,13 @@ var ThemeChecker = {
       $('body').removeClass('serenity light dark').addClass(theme.includes('-light') ? 'freya light' : 'freya dark');
     }
 
-    baseURI = $('body').get(0).baseURI;
-    console.log(this.isCaseListOrTaskListPage(baseURI));
-    if (this.isCaseListOrTaskListPage(baseURI)) {
-      var whiteBackgroundClass = "ui-case-task-background-color";
-      $('body').addClass(whiteBackgroundClass);
-    }
-
+    // Change background color in Task Case List Page
+    $(document).ready(function () {
+      var taskListClass = document.getElementsByClassName('task-list-container js-task-list-container').length > 0;
+      var caseListClass = document.getElementsByClassName('js-case-default-widget-container case-default-widget-container').length > 0;
+      if (taskListClass || caseListClass) {
+        $('body').addClass('ui-case-task-background-color');
+      }
+    })
   },
-  isCaseListOrTaskListPage: function (baseURI) {
-    console.log(baseURI);
-    return baseURI.includes('com.axonivy.portal.developerexamples.customization.TaskWidget/TaskWidget.xhtml') || baseURI.includes('com.axonivy.portal.developerexamples.customization.CaseWidget/CaseWidget.xhtml');
-
-  }
 }
