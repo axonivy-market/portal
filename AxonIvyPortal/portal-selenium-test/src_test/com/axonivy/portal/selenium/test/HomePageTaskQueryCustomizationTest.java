f0487ef044dbca3fe291832f1abc489a30db5e34
tphvu-axonivy <vu.tongphanhoang@axonactive.com>
2024-08-28 13:49:29 +0700

    IVYPORTAL-15714-Refactor-portal-test-gui-tests-to-selenide-for-LTS
    - fix HomePageTaskQueryCustomizationTest

--- AxonIvyPortal/portal-selenium-test/src_test/com/axonivy/portal/selenium/test/HomePageTaskQueryCustomizationTest.java
+++ AxonIvyPortal/portal-selenium-test/src_test/com/axonivy/portal/selenium/test/HomePageTaskQueryCustomizationTest.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.Test;
 import com.axonivy.ivy.webtest.IvyWebTest;
 import com.axonivy.portal.selenium.common.BaseTest;
 import com.axonivy.portal.selenium.common.TestAccount;
-import com.axonivy.portal.selenium.common.Variable;
 import com.axonivy.portal.selenium.page.HomePage;
 import com.axonivy.portal.selenium.page.TaskWidgetPage;
 
@@ -18,14 +17,14 @@ public class HomePageTaskQueryCustomizationTest extends BaseTest {
   public void setup() {
     super.setup();
     createTestingTasks();
-    redirectToRelativeLink(HomePage.PORTAL_EXAMPLES_HOME_PAGE_URL);
     login(TestAccount.DEMO_USER);
   }
 
   @Test
   public void testShowHideTaskDetailOnExpandedMode() {
-    updateGlobalVariable(Variable.SHOW_LEGACY_UI.getKey(), "true");
-    updateGlobalVariable(Variable.SHOW_USER_GUIDE.getKey(), "false");
+    updateLegacyUIConfiguration();
+    HomePage homePage = new HomePage();
+    homePage.waitForGlobalGrowlDisappear();
 
     TaskWidgetPage taskWidgetPage = new TaskWidgetPage();
     assertEquals(1, taskWidgetPage.countTasks().size());
