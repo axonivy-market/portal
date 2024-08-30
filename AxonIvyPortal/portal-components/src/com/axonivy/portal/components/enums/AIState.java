b86ff61af46ba2fa9bf3c70e3e78c9cdad4c9ef2
mnhnam-axonivy <nam.mai@axonactive.com>
2024-08-30 18:20:42 +0700

    IVYPORTAL-17432 Stage 1: Create a Marketplace item for Portal Assistant - Preparation - Implemnet new step: Trigger flow step

--- AxonIvyPortal/portal-components/src/com/axonivy/portal/components/enums/AIState.java
+++ AxonIvyPortal/portal-components/src/com/axonivy/portal/components/enums/AIState.java
@@ -3,7 +3,7 @@ package com.axonivy.portal.components.enums;
 import com.fasterxml.jackson.annotation.JsonValue;
 
 public enum AIState {
-  OPEN, IN_PROGRESS, DONE, ERROR;
+  OPEN, IN_PROGRESS, DONE, ERROR, TRIGGER;
 
   @JsonValue
   public String value() {
