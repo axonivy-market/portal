81145832db48ad7d352b1c5ccdad515a9c0e076f
Luong Minh Luat <157108501+lmluat-axonivy@users.noreply.github.com>
2024-09-05 10:28:19 +0700

    feature/IVYPORTAL-17458-Quick-Search-function-keeps-working-although-… (#979)
    * feature/IVYPORTAL-17458-Quick-Search-function-keeps-working-although-it-is-deactivated-LE
    
    - fix bug related to quick search function in Task, Case, Process widget
    
    * feature/IVYPORTAL-17458-Quick-Search-function-keeps-working-although-it-is-deactivated-LE
    
    - Disable the input of quick search in edit mode

--- AxonIvyPortal/portal/src/ch/ivy/addon/portalkit/dto/dashboard/CompactProcessDashboardWidget.java
+++ AxonIvyPortal/portal/src/ch/ivy/addon/portalkit/dto/dashboard/CompactProcessDashboardWidget.java
@@ -6,6 +6,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.commons.lang.BooleanUtils;
+
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
 
@@ -176,4 +178,11 @@ public class CompactProcessDashboardWidget extends ProcessDashboardWidget {
   public void setEnableQuickSearch(boolean enableQuickSearch) {
      this.enableQuickSearch = enableQuickSearch;
     }
+  
+  @Override
+  public void setQuickSearchKeyword(String quickSearchKeyword) {
+    if (BooleanUtils.isTrue(this.enableQuickSearch)) {
+      super.setQuickSearchKeyword(quickSearchKeyword);
+    }
+  }
 }
