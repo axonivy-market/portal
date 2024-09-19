199e358de3e562d9fee427c5e8ef437598b169a7
Loc Nguyen <loc.nguyenthanh@axonactive.com>
2024-09-19 14:52:15 +0700

    feature/IVYPORTAL-17431-Remove-Express-from-Portal - Remove from case action, process infromation

--- AxonIvyPortal/portal/src/ch/ivy/addon/portalkit/constant/CustomFields.java
+++ AxonIvyPortal/portal/src/ch/ivy/addon/portalkit/constant/CustomFields.java
@@ -6,7 +6,6 @@ public class CustomFields {
   public static final String CUSTOM_TIMESTAMP_FIELD5 = "CustomTimestampField5";
 
   public static final String EMBED_IN_FRAME = "embedInFrame";
-  public static final String IS_EXPRESS_PROCESS = "isExpress";
   public static final String EXPRESS_END_PAGE_URL = "expressEndPageUrl";
   public static final String PROCESS_IMAGE = "processImage";
   public static final String PROCESS_INFORMATION = "portalProcessInfo";
