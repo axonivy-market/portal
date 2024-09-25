d212a64b52229ed406ac5a9772b91ea58477f844
tphvu-axonivy <vu.tongphanhoang@axonactive.com>
2024-09-25 22:01:01 +0700

    IVYPORTAL-17687-Improve-GUI-test-report-showing-flaky-test-on-result_LE
    - extract flaky result

--- AxonIvyPortal/portal-selenium-test/resources/extractFlakyTests.bat
+++ AxonIvyPortal/portal-selenium-test/resources/extractFlakyTests.bat
@@ -1,4 +1,4 @@
-findstr /I "\[WARNING\] Flakes:" consoleText > flaky-tests-log
-findstr /I "com.axonivy.portal.selenium" flaky-tests-log > flakyTestsReport
-sed '/Time elapsed/s/.*//' flakyTestsReport > report
+findstr /I "\[WARNING\] Flakes:" AxonIvyPortal/portal-selenium-test/target/consoleText > AxonIvyPortal/portal-selenium-test/target/flaky-tests-log
+findstr /I "com.axonivy.portal.selenium" AxonIvyPortal/portal-selenium-test/target/flaky-tests-log > AxonIvyPortal/portal-selenium-test/target/flakyTestsReport
+sed '/Time elapsed/s/.*//' AxonIvyPortal/portal-selenium-test/target/flakyTestsReport > AxonIvyPortal/portal-selenium-test/target/report
 
