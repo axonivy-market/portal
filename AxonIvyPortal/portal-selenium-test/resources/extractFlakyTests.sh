24a98ffc321d2f7fc3791c6eac7f3f235d3a9d5d
tphvu-axonivy <vu.tongphanhoang@axonactive.com>
2024-09-25 20:34:04 +0700

    IVYPORTAL-17687-Improve-GUI-test-report-showing-flaky-test-on-result_LE
    - extract flaky result

--- AxonIvyPortal/portal-selenium-test/resources/extractFlakyTests.sh
+++ AxonIvyPortal/portal-selenium-test/resources/extractFlakyTests.sh
@@ -1,8 +1,9 @@
-if grep -q "\[WARNING\] Flakes:" ../target/consoleText; then
+pwd
+if grep -q "\[WARNING\] Flakes:" AxonIvyPortal/portal-selenium-test/target/consoleText; then
 
-  awk '/^\[WARNING\] Flakes:.*$/{flag=1}/^\[ERROR\] Tests run:.*Flakes: [0-9]*$/{print;flag=0}flag' ../target/consoleText > ../target/flaky-tests-log.txt
+  awk '/^\[WARNING\] Flakes:.*$/{flag=1}/^\[ERROR\] Tests run:.*Flakes: [0-9]*$/{print;flag=0}flag' AxonIvyPortal/portal-selenium-test/target/consoleText > AxonIvyPortal/portal-selenium-test/target/flaky-tests-log.txt
 
-  grep "\[WARNING\]" ../target/flaky-tests-log.txt | awk '{print}' > ../target/flakyTests.txt
+  grep "\[WARNING\]" AxonIvyPortal/portal-selenium-test/target/flaky-tests-log.txt | awk '{print}' > AxonIvyPortal/portal-selenium-test/target/flakyTests.txt
 
   exit 1
 fi
