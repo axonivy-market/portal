867053a74b60f0745491781b7dac8861e8f2edc2
tphvu-axonivy <vu.tongphanhoang@axonactive.com>
2024-09-26 09:27:11 +0700

    IVYPORTAL-17687-Improve-GUI-test-report-showing-flaky-test-on-result_LE
    - extract flaky result

--- AxonIvyPortal/portal-selenium-test/resources/extractFlakyTests.bat
+++ AxonIvyPortal/portal-selenium-test/resources/extractFlakyTests.bat
@@ -1,6 +1,3 @@
 findstr /I "\[WARNING\] Flakes:" consoleText > flaky-tests-log
 findstr /I "com.axonivy.portal.selenium" flaky-tests-log > flakyTestsReport
 sed '/Time elapsed/s/.*//' flakyTestsReport
-
-
-this script work, you should relocate the bat file inside the target first, then run the script
