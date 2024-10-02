63f2e21c40bbb964a1f03397b4ca759a80fefed2
Vu Tong <129731357+tphvu-axonivy@users.noreply.github.com>
2024-10-02 17:23:56 +0700

    IVYPORTAL-17700-Remove-portalTest_VERSION_LE (#1104)
    - remove portalTest package
    - move jmeter to portal-selenium-test
    - move GeckoFireFoxDriver to portal-selenium-test
    - update Jenkinsfiles
    - remove in gitignore

--- AxonIvyPortal/PortalTest/resources/copy-decorate-js-css.bat
+++
@@ -1,4 +0,0 @@
-type AxonivyPortal\PortalTest\resources\css\document-screenshot.css >> AxonivyPortal\portal\webContent\resources\css\portal.css
-type AxonivyPortal\PortalTest\resources\css\document-screenshot.css >> Showcase\portal-user-examples\webContent\resources\css\portal-user-examples.css
-type AxonivyPortal\PortalTest\resources\js\document-screenshot.js >> AxonivyPortal\portal\webContent\resources\js\portal.js
-type AxonivyPortal\PortalTest\resources\js\document-screenshot.js >> Showcase\portal-user-examples\webContent\resources\js\portal-user-examples.js
--- AxonIvyPortal/PortalTest/src_test/portal/guitest/common/FileHelper.java
+++
@@ -1,9 +0,0 @@
-package portal.guitest.common;
-
-public final class FileHelper {
-  private FileHelper() {}
-
-  public static String getAbsolutePathToTestFile(String fileName) {
-    return System.getProperty("user.dir") + "\\resources\\testFile\\" + fileName;
-  }
-}
