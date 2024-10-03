212c6eeae3edca0706e1f294494da56d8bdca6bf
Luong Minh Luat <157108501+lmluat-axonivy@users.noreply.github.com>
2024-10-03 09:30:08 +0700

    bug/IVYPORTAL-16887-Case-widget-in-edit-mode-column-align-not-correctly (#834)
    * bug/IVYPORTAL-16887-Case-widget-in-edit-mode-column-align-not-correctly
    
    - Change id of edit widget when editting, if save restore the id
    
    * Revert "bug/IVYPORTAL-16887-Case-widget-in-edit-mode-column-align-not-correctly"
    
    This reverts commit b96283af1470dbbfaaea0be7a1ce575278929d60.
    
    * bug/IVYPORTAL-16887-Case-widget-in-edit-mode-column-align-not-correctly - Fix bug
    
    * bug/IVYPORTAL-16887-Case-widget-in-edit-mode-column-align-not-correctly - Remove log
    
    * bug/IVYPORTAL-16887-Case-widget-in-edit-mode-column-align-not-correctly - Update for cases
    
    ---------
    
    Co-authored-by: Loc Nguyen <loc.nguyenthanh@axonactive.com>

--- AxonIvyPortal/portal/webContent/resources/js/dashboard.js
+++ AxonIvyPortal/portal/webContent/resources/js/dashboard.js
@@ -290,6 +290,18 @@ function loadCaseAndTaskWidgetFirstTime(loadingClass, widgetClass) {
   }, 50);
 }
 
+function initTableWidget(table) {
+  if (table === undefined || table.cfg === undefined) {
+    return;
+  }
+
+  setTimeout(function(){
+    var $table = $(document.getElementById(table.id));
+    table.cfg.scrollHeight = $table.height().toString();
+    table.init(table.cfg);
+  }, 500);
+}
+
 function hideAllDashboardOverlayPanels() {
   var openingOverlayPanel = $(".js-dashboard-overlay-panel");
   if (openingOverlayPanel.length > 0) {
