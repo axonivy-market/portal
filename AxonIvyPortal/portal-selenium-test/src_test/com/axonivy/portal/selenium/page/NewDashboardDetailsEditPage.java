2d96640c1a2391676e069a61295045f5048cec22
Le Ngoc Danh <127722015+lndanh-axonivy@users.noreply.github.com>
2024-10-15 10:15:46 +0700

    IVYPORTAL-17533 create accessibility mode dashboard template for LE: fix failed test

--- AxonIvyPortal/portal-selenium-test/src_test/com/axonivy/portal/selenium/page/NewDashboardDetailsEditPage.java
+++ AxonIvyPortal/portal-selenium-test/src_test/com/axonivy/portal/selenium/page/NewDashboardDetailsEditPage.java
@@ -238,7 +238,7 @@ public class NewDashboardDetailsEditPage extends TemplatePage {
   }
   
   public SelenideElement getAccessibilityWidget() {
-    return $("[id^='id-custom-widget-iframe']");
+    return $("[id^='id-custom-widget-iframe']").shouldBe(Condition.appear, DEFAULT_TIMEOUT);
   }
   
 }
