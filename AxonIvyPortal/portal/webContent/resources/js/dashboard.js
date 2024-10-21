cb48d7c49d3637ae4a92d3b4a78c3a8c381febe0
Loc Nguyen <loc.nguyenthanh@axonactive.com>
2024-10-21 13:11:22 +0700

    bug/IVYPORTAL-17767-Task-Case-widget-in-edit-mode-column-align-not-correctly-on-LTS - Fix on LE

--- AxonIvyPortal/portal/webContent/resources/js/dashboard.js
+++ AxonIvyPortal/portal/webContent/resources/js/dashboard.js
@@ -464,3 +464,15 @@ function setLineClamp(element, number) {
 function removeStyle(element) {
   $(element).removeAttr('style');
 }
+
+function initTableWidget(table) {
+  if (table === undefined || table.cfg === undefined) {
+    return;
+  }
+
+  setTimeout(function(){
+    var $table = $(document.getElementById(table.id));
+    table.cfg.scrollHeight = $table.height().toString();
+    table.init(table.cfg);
+  }, 500);
+}
