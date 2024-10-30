930ae34cbd24c84fe84e4885a98ddfdf02b54d4a
lmluat-axonivy <luat.luongminh@axonactive.com>
2024-10-30 15:27:14 +0700

    feature/IVYPORTAL-17525-Check-portal-user-documentation-for-LTS-12.0.0-Release
    - Update screenshot test

--- AxonIvyPortal/portal-selenium-test/src_test/com/axonivy/portal/selenium/document/screenshot/DashboardScreenshotTest.java
+++ AxonIvyPortal/portal-selenium-test/src_test/com/axonivy/portal/selenium/document/screenshot/DashboardScreenshotTest.java
@@ -220,8 +220,7 @@ public class DashboardScreenshotTest extends ScreenshotBaseTest {
     taskConfigurationPage.cancelMultiLanguageDialogWhenAddWidget();
     taskConfigurationPage.openFilter();
     taskConfigurationPage.addFilter("name", FilterOperator.EMPTY);
-    ScreenshotUtils.captureElementScreenshot(taskConfigurationPage.getConfigurationFilter(),
-        ScreenshotUtils.NEW_DASHBOARD_FOLDER + "task-list-widget-configuration");
+    ScreenshotUtils.captureHalfRightPageScreenShot(ScreenshotUtils.NEW_DASHBOARD_FOLDER + "task-list-widget-configuration");
     taskConfigurationPage.closeFilter();
     WebElement columnManagementDialog = taskConfigurationPage.openColumnManagementDialog();
     ScreenshotUtils.captureElementScreenshot(columnManagementDialog,
