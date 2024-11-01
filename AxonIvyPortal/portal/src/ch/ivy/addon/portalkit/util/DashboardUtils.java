64177e5dc287e187bf7ca7b40eddf4f9b0928eee
nhthinh-axonivy <thinh.nguyenhung@axonactive.com>
2024-11-01 16:18:16 +0700

    feature/IVYPORTAL-17461-Remove-Full-Task-List-and-Replace-it-with-the-Task-Widget-ver5 _ make DefaultTaskListDashboard into static value

--- AxonIvyPortal/portal/src/ch/ivy/addon/portalkit/util/DashboardUtils.java
+++ AxonIvyPortal/portal/src/ch/ivy/addon/portalkit/util/DashboardUtils.java
@@ -49,6 +49,56 @@ public class DashboardUtils {
   public final static String DASHBOARD_MENU_JS_CLASS = "js-dashboard-group";
   public final static String HIGHLIGHT_DASHBOARD_ITEM_METHOD_PATTERN = "highlightDashboardItem('%s')";
   public final static String DASHBOARD_TASK_TEMPLATE_ID = "default-task-list-dashboard";
+  private static final String DASHBOARD_TASK_TEMPLATE_JSON = """
+      {
+        "id": "default-task-list-dashboard",
+        "version": "11.4.0",
+        "templateId": "create-from-scratch",
+        "titles": [
+          { "locale": "en", "value": "Tasks" },
+          { "locale": "fr", "value": "Tâches" },
+          { "locale": "de", "value": "Aufgaben" },
+          { "locale": "es", "value": "Tareas" }
+        ],
+        "icon": "si-task-list-edit",
+        "description": "Default Task List Dashboard",
+        "widgets": [
+          {
+            "type": "task",
+            "id": "task_e16cbee90a244f4a857d0a8c875b7b34",
+            "names": [
+              { "locale": "en", "value": "Your Tasks" },
+              { "locale": "fr", "value": "Vos Tâches" },
+              { "locale": "de", "value": "Ihre Aufgaben" },
+              { "locale": "es", "value": "Tus Tareas" }
+            ],
+            "layout": { "w": 12, "h": 8, "x": 0, "y": 0 },
+            "rowsPerPage": 5,
+            "enableQuickSearch": true,
+            "showWidgetInfo": true,
+            "showFullscreenMode": true,
+            "columns": [
+              { "field": "start", "width": "50" },
+              { "field": "priority", "width": "60" },
+              { "field": "id", "width": "80" },
+              { "field": "name", "quickSearch": true, "width": "600" },
+              { "field": "description", "quickSearch": true, "width": "250" },
+              { "field": "activator", "width": "125" },
+              { "field": "state", "width": "70" },
+              { "field": "startTimestamp", "width": "95" },
+              { "field": "expiryTimestamp", "width": "105" },
+              { "field": "category", "width": "110" },
+              { "field": "actions", "width": "85" }
+            ],
+            "canWorkOn": false,
+            "sortField": "id",
+            "sortDescending": true
+          }
+        ],
+        "permissions": [ "Everybody" ],
+        "isTopMenu": true
+      }
+      """;
 
   public static List<Dashboard> getVisibleDashboards(String dashboardJson) {
     List<Dashboard> dashboards = jsonToDashboards(dashboardJson);
@@ -278,8 +328,9 @@ public class DashboardUtils {
   }
 
   public static Dashboard getDefaultTaskListDashboard() {
-    String dashboardTaskTemplate = Ivy.var().get(PortalVariable.TASK_TEMPLATE_DASHBOARD.key);
-    return jsonToDashboard(dashboardTaskTemplate);
+    Dashboard defautTaskListDashboard = new Dashboard();
+    defautTaskListDashboard.setId(DASHBOARD_TASK_TEMPLATE_ID);
+    return jsonToDashboard(DASHBOARD_TASK_TEMPLATE_JSON);
   }
 
   public static List<Dashboard> getDashboardsWithoutMenuItem() {
