44e39d1bc03cbfc6292cc3cbe6eadb06e937b811
lmluat-axonivy <luat.luongminh@axonactive.com>
2024-12-24 09:11:15 +0700

    feature/IVYPORTAL-18117-Beautify-screenshots
    - Update screenshot

--- AxonIvyPortal/portal-selenium-test/src_test/com/axonivy/portal/selenium/document/screenshot/DemoProcessesScreenshotTest.java
+++ AxonIvyPortal/portal-selenium-test/src_test/com/axonivy/portal/selenium/document/screenshot/DemoProcessesScreenshotTest.java
@@ -98,7 +98,7 @@ public class DemoProcessesScreenshotTest extends ScreenshotBaseTest {
     TopMenuTaskWidgetPage taskWidget = new TopMenuTaskWidgetPage();
     taskWidget.clickOnTaskActionLink(0);
     ScreenshotUtils.executeDecorateJs("highlightTaskActionItem(0, 1)");
-    ScreenshotUtils.capturePageScreenshot(ScreenshotUtils.DEMO_FOLDER + "lending-casemap-external-solvency-service");
+    ScreenshotUtils.captureHalfTopPageScreenShot(ScreenshotUtils.DEMO_FOLDER + "lending-casemap-external-solvency-service");
     taskWidget.clickOnSideStepAction(0, 0);
     ScreenshotUtils.resizeBrowser(new Dimension(SCREENSHOT_WIDTH, 1150));
     ScreenshotUtils.capturePageScreenshot(ScreenshotUtils.DEMO_FOLDER + "lending-casemap-approval-task");
