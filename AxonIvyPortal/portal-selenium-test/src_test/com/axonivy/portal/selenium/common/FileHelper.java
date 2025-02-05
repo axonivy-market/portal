30cdf6b3a044eb305f1c13a8263a87db02657e52
Nam Chu <117632090+chnam-axonivy@users.noreply.github.com>
2025-02-05 17:37:17 +0700

    debug

--- AxonIvyPortal/portal-selenium-test/src_test/com/axonivy/portal/selenium/common/FileHelper.java
+++ AxonIvyPortal/portal-selenium-test/src_test/com/axonivy/portal/selenium/common/FileHelper.java
@@ -1,9 +1,11 @@
 package com.axonivy.portal.selenium.common;
 
+import java.io.File;
+
 public final class FileHelper {
   private FileHelper() {}
 
   public static String getAbsolutePathToTestFile(String fileName) {
-    return System.getProperty("user.dir") + "\\resources\\testFile\\" + fileName;
+    return System.getProperty("user.dir") + File.pathSeparator + "resources" + File.pathSeparator + "testFile" + File.pathSeparator  + fileName;
   }
 }
