fb96a01ee429e11e3af60e8b6033ec15e11505aa
nhthinh-axonivy <thinh.nguyenhung@axonactive.com>
2025-02-06 17:46:22 +0700

    feature/IVYPORTAL-18368-Improve-Lighthouse-Accessibility-Score

--- AxonIvyPortal/portal-selenium-test/lighthouse/puppeteer-script.mjs
+++ AxonIvyPortal/portal-selenium-test/lighthouse/puppeteer-script.mjs
@@ -6,7 +6,7 @@ import lighthouse from "lighthouse";
 const PORTAL_URL = "http://localhost:8080/demo-portal";
 const LOGIN_URL = `${PORTAL_URL}/login`;
 const DASHBOARD_URL = `${PORTAL_URL}/pro/portal/1549F58C18A6C562/DashboardPage.ivp?dashboardId=1`;
-const CREATE_TASK_URL = `${PORTAL_URL}/pro/portal-developer-examples/162511D2577DBA88/CategoriedLeaveRequest.ivp`;
+const CREATE_TASK_URL = `${PORTAL_URL}/pro/portal-developer-examples/162511D2577DBA88/CategoriedLeaveRequest.ivp?embedInFrame=`;
 
 const debugLog = (msg) => console.log(`[Debug] ${msg}`);
 
