3b5381e705fcffd203d1260630c9121584ff34ab
Nam Chu <117632090+chnam-axonivy@users.noreply.github.com>
2025-02-06 08:02:47 +0700

    test

--- AxonIvyPortal/portal-selenium-test/src_test/com/axonivy/portal/selenium/common/BaseTest.java
+++ AxonIvyPortal/portal-selenium-test/src_test/com/axonivy/portal/selenium/common/BaseTest.java
@@ -293,9 +293,9 @@ public class BaseTest {
       try {
         System.out.println(EngineUrl.createProcessUrl(String.format(LOGIN_URL_PATTERN, username, password)));
 //        open(EngineUrl.createProcessUrl(String.format(LOGIN_URL_PATTERN, username, password)));
-        String url = "http%3A%2F%2Flocalhost%3A8080%2Fdemo-portal%2Fpro%2FPortalKitTestHelper%2F1636734E13CEC872%2Flogin.ivp%3Fusername%3Dadmin%26password%3Dadmin";
-        open(url);
+        String url = "http://localhost:8080/demo-portal/pro/PortalKitTestHelper/1636734E13CEC872/login.ivp%3Fusername%3Dadmin%26password%3Dadmin";
         System.out.println("open url:" + url);
+        open(url);
         $(".js-dashboard__wrapper").shouldBe(Condition.exist);
       } catch (Error e) {
         open(EngineUrl.createProcessUrl(String.format(LOGIN_URL_PATTERN, username, password)));
