731fe7c03d7d18f285ba3d3fb974d7dab054d94f
lmluat-axonivy <luat.luongminh@axonactive.com>
2025-02-10 13:37:21 +0700

    feature/IVYPORTAL-18429-Process-image-show-default-image-in-dark-mode-LE
    - Update comment

--- AxonIvyPortal/portal/src/ch/ivy/addon/portalkit/bo/IvyProcess.java
+++ AxonIvyPortal/portal/src/ch/ivy/addon/portalkit/bo/IvyProcess.java
@@ -74,7 +74,7 @@ public class IvyProcess implements Process {
 
   @Override
   public String getImageUrl() {
-    // Change default value image
+    // Change default image value
     if (imageUrl.contains(DEFAULT_IMAGE_VALUE_DARK_MODE)) {
       imageUrl = getContentImageUrl(DefaultImage.PROCESSMODELING.getPath());
     }
@@ -82,7 +82,7 @@ public class IvyProcess implements Process {
   }
   
   public String getDefaultImageDarkUrl() {
-    // Change default value image
+    // Change default image value
     if (imageUrl.contains(DEFAULT_IMAGE_VALUE_LIGHT_MODE)) {
       imageUrl = getContentImageUrl(DefaultImage.PROCESSMODELINGDARK.getPath());
     }
