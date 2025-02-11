14c801f5798143f13f949e3786fe410ba74bab6d
tphvu-axonivy <vu.tongphanhoang@axonactive.com>
2025-02-11 16:12:04 +0700

    IVYPORTAL-18324-Support-bussiness-case-customfield-MASTER
    - implement feature

--- AxonIvyPortal/portal/src/ch/ivy/addon/portalkit/exporter/TaskDashboardExporter.java
+++ AxonIvyPortal/portal/src/ch/ivy/addon/portalkit/exporter/TaskDashboardExporter.java
@@ -105,6 +105,8 @@ public class TaskDashboardExporter extends DashboardWidgetExporter{
     ICustomFields customFields;
     if (type.equals(DashboardColumnFormat.CUSTOM_CASE.name())) {
       customFields = taskItem.getCase().customFields();
+    } else if (type.equals(DashboardColumnFormat.CUSTOM_BUSINESS_CASE.name())) {
+      customFields = taskItem.getCase().getBusinessCase().customFields();
     } else {
       customFields = taskItem.customFields();
     }
