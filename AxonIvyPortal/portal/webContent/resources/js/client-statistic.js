c3d3146bb3246b50a515be0589a4935d74d27db6
Vu Tong <129731357+tphvu-axonivy@users.noreply.github.com>
2025-02-12 12:05:40 +0700

    IVYPORTAL-18247 Statistic on dark mode enhancemen MASTER (#1523)
    - update colors

--- AxonIvyPortal/portal/webContent/resources/js/client-statistic.js
+++ AxonIvyPortal/portal/webContent/resources/js/client-statistic.js
@@ -6,6 +6,8 @@ const AVERAGE_BUSINESS_RUNTIME = "avg-businessRuntime";
 // Additional configs
 const EMPTY_CHART_MESSAGE =  'emptyChartDataMessage';
 const MANIPULATE_BY = 'manipulateValueBy';
+const CHART_TEXT_COLOR = '#808080';
+const CHART_GRID_COLOR = 'rgba(192, 192, 192, 0.5)';
 
 let locale;
 let datePattern;
@@ -362,7 +364,14 @@ class ClientPieChart extends ClientCanvasChart {
         },
         options: {
           responsive: true,
-          maintainAspectRatio: false
+          maintainAspectRatio: false,
+          plugins: {
+            legend: {
+              labels: {
+                color: CHART_TEXT_COLOR
+              }
+            }
+          }
         }
       });
     }
@@ -423,16 +432,28 @@ class ClientCartesianChart extends ClientCanvasChart {
               beginAtZero: true,
               title: {
                 text: chartTypeConfig.yTitle,
-                display: true
+                display: true,
+                color: CHART_TEXT_COLOR
               },
               ticks: {
-                stepSize: stepSize
+                stepSize: stepSize,
+                color: CHART_TEXT_COLOR
+              },
+              grid: {
+                color: CHART_GRID_COLOR
               }
             },
             x: {
               title: {
                 text: chartTypeConfig.xTitle,
-                display: true
+                display: true,
+                color: CHART_TEXT_COLOR
+              },
+              ticks: {
+                color: CHART_TEXT_COLOR
+              },
+              grid: {
+                color: CHART_GRID_COLOR
               }
             }
           }
