6858f568befe4f0b327c82f3c259ae412854ae9d
lmluat-axonivy <luat.luongminh@axonactive.com>
2025-02-19 13:55:44 +0700

    feature/IVYPORTAL-17382-Multi-Language-Support-for-Custom-Fields-LE
    - Update TextFilter.xhtml

--- AxonIvyPortal/portal/src/com/axonivy/portal/bean/dashboard/WidgetTextFilterBean.java
+++ AxonIvyPortal/portal/src/com/axonivy/portal/bean/dashboard/WidgetTextFilterBean.java
@@ -1,6 +1,8 @@
 package com.axonivy.portal.bean.dashboard;
 
 import java.io.Serializable;
+import java.util.Collections;
+import java.util.EnumSet;
 import java.util.List;
 
 import javax.faces.bean.ManagedBean;
@@ -9,6 +11,8 @@ import javax.faces.bean.ViewScoped;
 import com.axonivy.portal.dto.dashboard.filter.DashboardFilter;
 import com.axonivy.portal.enums.dashboard.filter.FilterOperator;
 
+import ch.ivy.addon.portalkit.util.PortalCustomFieldUtils;
+
 @ManagedBean
 @ViewScoped
 public class WidgetTextFilterBean implements Serializable {
@@ -16,9 +20,14 @@ public class WidgetTextFilterBean implements Serializable {
   private static final long serialVersionUID = 3218284115015773931L;
 
   private static List<FilterOperator> operators = FilterOperator.TEXT_OPERATORS.stream().toList();
+  private static List<FilterOperator> operatorsForCustomFieldWithCms = Collections.unmodifiableSet(EnumSet.of(FilterOperator.CONTAINS)).stream().toList();
 
   public List<FilterOperator> getOperators() {
     return operators;
+  }
+  
+  public List<FilterOperator> getOperatorsForCustomFieldWithCms() {
+    return operatorsForCustomFieldWithCms;
   }
 
   public void onChangeOperator(@SuppressWarnings("unused") DashboardFilter filter) {
@@ -39,6 +48,10 @@ public class WidgetTextFilterBean implements Serializable {
     }
 
     return false;
+  }
+  
+  public boolean isCustomFieldWithCms(DashboardFilter filter) {
+    return PortalCustomFieldUtils.isContainValidCmsPathAttribute(filter.getField(), filter.getFilterType());
   }
 
 }
