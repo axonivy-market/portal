61bd2780bea913782491f20a453e651c5142bb5d
Thai Phan <117443490+pvthai-axonivy@users.noreply.github.com>
2025-02-25 18:08:30 +0700

    IVYPORTAL-18398 Case behaviors when click on Case In list (#1549)

--- AxonIvyPortal/portal/src/ch/ivy/addon/portal/generic/bean/DashboardCaseWidgetBean.java
+++ AxonIvyPortal/portal/src/ch/ivy/addon/portal/generic/bean/DashboardCaseWidgetBean.java
@@ -1,15 +1,21 @@
 package ch.ivy.addon.portal.generic.bean;
 
+import java.io.IOException;
 import java.io.Serializable;
 import java.util.List;
 import java.util.Random;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
 
 import org.primefaces.event.SelectEvent;
+
+import com.axonivy.portal.util.BusinessDetailsUtils;
+import com.axonivy.portal.util.CaseBehaviorUtils;
 
 import ch.ivy.addon.portal.generic.navigation.PortalNavigator;
 import ch.ivy.addon.portalkit.enums.CaseEmptyMessage;
@@ -23,10 +29,20 @@ public class DashboardCaseWidgetBean implements Serializable {
   private static final long serialVersionUID = 4733478835925712413L;
 
   private CaseEmptyMessage noCasesMessage;
+  private boolean canAccessBusinessDetails;
 
-  public void navigateToSelectedCaseDetails(SelectEvent<Object> event) {
-    String uuid = ((ICase) event.getObject()).uuid();
-    PortalNavigator.navigateToPortalCaseDetails(uuid);
+  @PostConstruct
+  public void init() {
+    canAccessBusinessDetails = CaseBehaviorUtils.canAccessBusinessDetails();
+  }
+
+  public void handleRowSelectEventOnCaseWidget(SelectEvent<Object> event) throws IOException {
+    ICase icase = ((ICase) event.getObject());
+    if (canAccessBusinessDetails) {
+      FacesContext.getCurrentInstance().getExternalContext().redirect(BusinessDetailsUtils.getAdditionalCaseDetailsPageUri(icase));
+    } else {
+      PortalNavigator.navigateToPortalCaseDetails(icase.uuid());
+    }
   }
 
   public CaseEmptyMessage getNoCasesMessage() {
