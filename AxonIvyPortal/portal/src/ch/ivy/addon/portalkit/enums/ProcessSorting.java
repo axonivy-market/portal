98b14df503332a81f55a0ef9f68fca890334bf85
Thinh Nguyen <117440893+nhthinh-axonivy@users.noreply.github.com>
2025-02-26 11:50:49 +0700

    Feature/ivyportal 16167 process widget smart process order by most used (#1559)
    * feature/IVYPORTAL-16167-Process-Widget-Smart-Process-Order-by-most-used
    - implement UI
    - Add GUI Test
    - Handle feedback
    - clean up

--- AxonIvyPortal/portal/src/ch/ivy/addon/portalkit/enums/ProcessSorting.java
+++ AxonIvyPortal/portal/src/ch/ivy/addon/portalkit/enums/ProcessSorting.java
@@ -5,7 +5,7 @@ import org.apache.commons.lang3.StringUtils;
 import ch.ivyteam.ivy.environment.Ivy;
 
 public enum ProcessSorting {
-  BY_ALPHABETICALLY, BY_INDEX, BY_CUSTOM_ORDER;
+  BY_ALPHABETICALLY, BY_INDEX, BY_CUSTOM_ORDER, BY_SMART_ORDER;
 
   public String getLabel() {
     String label = Ivy.cms().co("/ch.ivy.addon.portalkit.ui.jsf/Enums/ProcessSorting/" + name());
