92d287b1c204f33e04f5ffe90a7dea20b99e2893
nhthinh-axonivy <thinh.nguyenhung@axonactive.com>
2025-03-04 14:06:00 +0700

    feature/IVYPORTAL-18551-Solve-issues-with-Cross-site-scripting-in-portal-for-pic-upload

--- AxonIvyPortal/portal/src/ch/ivy/addon/portalkit/document/DocumentExtensionConstants.java
+++ AxonIvyPortal/portal/src/ch/ivy/addon/portalkit/document/DocumentExtensionConstants.java
@@ -10,7 +10,7 @@ public class DocumentExtensionConstants {
   public static final List<String> ALLOWED_EXCEL_FORMAT = ImmutableList.of("xls", "xlsx", "xlsm", "xlsb", "xlt", "xltm");
   public static final List<String> ALLOWED_PDF_FORMAT = ImmutableList.of("pdf");
   public static final List<String> DEFAULT_WHITELIST_EXTENSION = ImmutableList.of("doc", "docx", "xls", "xlsx", "xlsm", "csv", "pdf", "ppt", "pptx", "txt" ,"zip", "jpg", "jpeg", "bmp", "png", "log");
-  public static final List<String> ARCHIVE_FORMAT = ImmutableList.of("zip", "rar", "7z");
+  public static final List<String> ALLOWED_SVG_FORMAT = ImmutableList.of("svg");  public static final List<String> ARCHIVE_FORMAT = ImmutableList.of("zip", "rar", "7z");
   public static final List<String> IMAGE_FORMAT = ImmutableList.of("jpg", "jpeg", "bmp", "png", "gif");
   public static final List<String> POWER_POINT_FORMAT =  ImmutableList.of("ppt", "pptx");
   public static final List<String> EMAIL_FORMAT =  ImmutableList.of("msg", "eml");
