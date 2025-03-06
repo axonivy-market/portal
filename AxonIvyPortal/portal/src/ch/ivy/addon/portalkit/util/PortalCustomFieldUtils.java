a91e9f3a61cbc2651c826e57afbfa17eabd268bd
lmluat-axonivy <luat.luongminh@axonactive.com>
2025-03-06 16:32:27 +0700

    feature/IVYPORTAL-17382-Multi-Language-Support-for-Custom-Fields-LE
    - Format code

--- AxonIvyPortal/portal/src/ch/ivy/addon/portalkit/util/PortalCustomFieldUtils.java
+++ AxonIvyPortal/portal/src/ch/ivy/addon/portalkit/util/PortalCustomFieldUtils.java
@@ -1,7 +1,6 @@
 package ch.ivy.addon.portalkit.util;
 
 import java.util.ArrayList;
-
 import java.util.Comparator;
 import java.util.List;
 import java.util.Set;
@@ -21,45 +20,52 @@ public class PortalCustomFieldUtils {
   public static boolean isSupportMultiLanguageCaseField(String columnField) {
     return hasCmsValues(columnField, ICustomFieldMeta.cases());
   }
-  
+
   public static boolean isSupportMultiLanguageTaskField(String columnField) {
     return hasCmsValues(columnField, ICustomFieldMeta.tasks());
   }
 
   private static boolean hasCmsValues(String columnField, Set<ICustomFieldMeta> customFieldMetaList) {
     for (ICustomFieldMeta customFieldMeta : customFieldMetaList) {
-      if (customFieldMeta.name().equals(columnField) && Boolean.valueOf(customFieldMeta.attribute(HAS_CMS_VALUES_ATTRIBUTE))) {
+      if (customFieldMeta.name().equals(columnField)
+          && Boolean.valueOf(customFieldMeta.attribute(HAS_CMS_VALUES_ATTRIBUTE))) {
         return true;
       }
     }
     return false;
   }
 
-  public static List<String> getCmsValuesMatchingWithKeywordList(String columnField, DashboardColumnType type, List<String> keywordList) {
-    Set<ICustomFieldMeta> icustomFieldMetaList = type == DashboardColumnType.CUSTOM_CASE ? ICustomFieldMeta.cases() : ICustomFieldMeta.tasks();
+  public static List<String> getCmsValuesMatchingWithKeywordList(String columnField, DashboardColumnType type,
+      List<String> keywordList) {
+    Set<ICustomFieldMeta> icustomFieldMetaList =
+        type == DashboardColumnType.CUSTOM_CASE ? ICustomFieldMeta.cases() : ICustomFieldMeta.tasks();
     List<String> matchingValueList = new ArrayList<>();
 
-    icustomFieldMetaList.stream().filter(customField -> customField.type() == CustomFieldType.STRING && customField.name().equals(columnField)).forEach(field -> {
-      for (String keyword: keywordList) {
-        Iterable<Object> list =  field.values().matching(keyword);
-        if (!Iterables.isEmpty(list)) {
-          for (Object obj : list) {
-            matchingValueList.add(obj.toString());
+    icustomFieldMetaList.stream()
+        .filter(customField -> customField.type() == CustomFieldType.STRING && customField.name().equals(columnField))
+        .forEach(field -> {
+          for (String keyword : keywordList) {
+            Iterable<Object> list = field.values().matching(keyword);
+            if (!Iterables.isEmpty(list)) {
+              for (Object obj : list) {
+                matchingValueList.add(obj.toString());
+              }
+            }
           }
-      }
-      }
-    });
-    
+        });
+
     return matchingValueList;
   }
 
   public static List<String> getAllLocalizedValueOnCaseField(String columnField) {
-    ICustomFieldMeta meta = ICustomFieldMeta.cases().stream().filter(m -> columnField.equals(m.name())).findAny().orElse(null);
+    ICustomFieldMeta meta =
+        ICustomFieldMeta.cases().stream().filter(m -> columnField.equals(m.name())).findAny().orElse(null);
     return getAllLocalizedValueByField(meta);
   }
-  
+
   public static List<String> getAllLocalizedValueOnTaskField(String columnField) {
-    ICustomFieldMeta meta = ICustomFieldMeta.tasks().stream().filter(m -> columnField.equals(m.name())).findAny().orElse(null);
+    ICustomFieldMeta meta =
+        ICustomFieldMeta.tasks().stream().filter(m -> columnField.equals(m.name())).findAny().orElse(null);
     return getAllLocalizedValueByField(meta);
   }
 
@@ -67,11 +73,9 @@ public class PortalCustomFieldUtils {
     if (meta == null) {
       return new ArrayList<String>();
     }
-    
-    return meta.values()
-        .labels().stream().sorted(Comparator.comparing(ValueLabel<Object>::label))
-        .map(vl -> vl.value().toString())
-        .toList();
+
+    return meta.values().labels().stream().sorted(Comparator.comparing(ValueLabel<Object>::label))
+        .map(vl -> vl.value().toString()).toList();
   }
 
   public static String getDisplayValueByField(ICustomFields customFields, String field) {
