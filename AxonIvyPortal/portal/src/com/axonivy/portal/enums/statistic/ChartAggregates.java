08a0ffc806c05acd741f54760daed3c171916dca
tphvu-axonivy <vu.tongphanhoang@axonactive.com>
2025-03-11 17:08:07 +0700

    IVYPORTAL-18574-Aggregation-for-custom-statistic-chart-configuration
    - add aggregates

---
+++ AxonIvyPortal/portal/src/com/axonivy/portal/enums/statistic/ChartAggregates.java
@@ -0,0 +1,25 @@
+package com.axonivy.portal.enums.statistic;
+import com.fasterxml.jackson.annotation.JsonValue;
+
+public enum ChartAggregates {
+  PRIORITY("priority"),
+  BUSINESS_STATE("businessState"),
+  CATEGORY("category"),
+  CREATOR_NAME("creator.name"),
+  START_TIMESTAMP("startTimestamp"),
+  MODIFIED_TIMESTAMP("modifiedTimestamp"),
+  END_TIMESTAMP("endTimestamp"),
+  CUSTOM_STRING_FIELD("customFields.strings"),
+  CUSTOM_TIMESTAMP_FIELD("customFields.timestamps");
+  
+  private ChartAggregates (String name) {
+    this.name = name;
+  }
+
+  private String name;
+
+  @JsonValue
+  public String getName() {
+    return name;
+  }
+}
