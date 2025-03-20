832ebb66cdeb768e5cf9b6cbd1f27250a45fdb51
Phan Van Thai <thai.phanvan@axonactive.com>
2025-03-20 17:45:19 +0700

    IVYPORTAL-18438 Refactor code

--- build/common/utils.groovy
+++ build/common/utils.groovy
@@ -165,16 +165,4 @@ def uploadBOM(def projectName, def projectVersion, def bomFile) {
   }
 }
 
-// def updateVersion(def pomFile, def groupId, def version) {
-//   def pom = new XmlSlurper().parse(new File(${pomFile}))
-
-// // Find all dependencies with groupId 'abc' and increment the version
-// pom.dependencies.dependency.findAll { it.groupId.text() == ${groupId} }.each { dep ->
-//     dep.version[0].value = ${version}  // Change this logic as per your needs
-// }
-
-// // Save the updated pom.xml
-// def writer = new FileWriter(${pomFile})
-// XmlUtil.serialize(pom, writer)
-// }
 return this
