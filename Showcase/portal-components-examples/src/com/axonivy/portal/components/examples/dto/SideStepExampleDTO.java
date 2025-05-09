41c935cd24bed46498541bf4286f51ce0c6d5c57
lndanh <danh.lengoc@axonactive.com>
2025-05-09 09:57:30 +0700

    IVYPORTAL-18785 Create concept for Side Step processes MVP - Implement logic

--- Showcase/portal-components-examples/src/com/axonivy/portal/components/examples/dto/SideStepExampleDTO.java
+++ Showcase/portal-components-examples/src/com/axonivy/portal/components/examples/dto/SideStepExampleDTO.java
@@ -3,9 +3,37 @@ package com.axonivy.portal.components.examples.dto;
 import com.axonivy.portal.components.dto.UserDTO;
 
 public class SideStepExampleDTO {
-  private UserDTO assignee;
+  private String assignee;
   private String question;
   private String answer;
+  private boolean isCompleted;
+  private String comment1;
+  private String comment2;
+  private String comment3;
+
+  public String getComment1() {
+    return comment1;
+  }
+
+  public void setComment1(String comment1) {
+    this.comment1 = comment1;
+  }
+
+  public String getComment2() {
+    return comment2;
+  }
+
+  public void setComment2(String comment2) {
+    this.comment2 = comment2;
+  }
+
+  public String getComment3() {
+    return comment3;
+  }
+
+  public void setComment3(String comment3) {
+    this.comment3 = comment3;
+  }
 
   public String getQuestion() {
     return question;
@@ -23,12 +51,20 @@ public class SideStepExampleDTO {
     this.answer = answer;
   }
 
-  public UserDTO getAssignee() {
+  public String getAssignee() {
     return assignee;
   }
 
-  public void setAssignee(UserDTO assignee) {
+  public void setAssignee(String assignee) {
     this.assignee = assignee;
+  }
+
+  public boolean isCompleted() {
+    return isCompleted;
+  }
+
+  public void setCompleted(boolean isCompleted) {
+    this.isCompleted = isCompleted;
   }
 
 }
