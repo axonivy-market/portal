1baa41e27caafa52384f58e2ff74d7fe8888fb03
lndanh <danh.lengoc@axonactive.com>
2025-05-12 15:42:17 +0700

    IVYPORTAL-18785 Create concept for Side Step processes MVP - Update code

--- AxonIvyPortal/portal-components/src/com/axonivy/portal/components/constant/CustomFields.java
+++ AxonIvyPortal/portal-components/src/com/axonivy/portal/components/constant/CustomFields.java
@@ -8,5 +8,4 @@ public final class CustomFields {
     public static final String SIDE_STEPS_TASK = "sideStepsTask";
     public static final String SIDE_STEP_CASE = "sideStepCase";
     public static final String SIDE_STEPS_PROCESS = "sideStepsProcess";
-    public static final String ORIGINAL_TASK = "originalTask";
 }
