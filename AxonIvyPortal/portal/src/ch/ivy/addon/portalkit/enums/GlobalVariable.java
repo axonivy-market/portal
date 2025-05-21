c1dacf2fbec8747c0aad18174985d079c8314b72
Thinh Nguyen <117440893+nhthinh-axonivy@users.noreply.github.com>
2025-05-21 10:52:13 +0700

    Update GlobalVariable.java

--- AxonIvyPortal/portal/src/ch/ivy/addon/portalkit/enums/GlobalVariable.java
+++ AxonIvyPortal/portal/src/ch/ivy/addon/portalkit/enums/GlobalVariable.java
@@ -90,7 +90,7 @@ public enum GlobalVariable {
   IMAGE_UPLOAD_SIZE_LIMIT("Portal.Upload.ImageUploadSizeLimit", GlobalVariableType.NUMBER, "6", "imageUploadSizeLimit"),
   DELEGATION_APPEND_OPTION("Portal.DelegationAppendOption", GlobalVariableType.EXTERNAL_SELECTION,DelegationAppendOption.NONE.name(), "delegationAppendOption",getDelegationAppendOption()),
   SESSION_CACHE_TIMEOUT("Portal.SessionCacheTimeout", GlobalVariableType.NUMBER,"86400", "SessionCacheTimeout"),
-  ENABLE_PINNED_TASK("Portal.Cases.EnablePinnedTask",GlobalVariableType.SELECTION, Option.FALSE.toString(),"enablePinTask"),
+  ENABLE_PINNED_TASK("Portal.Tasks.EnablePinnedTask",GlobalVariableType.SELECTION, Option.FALSE.toString(),"enablePinTask"),
   ENABLE_PINNED_CASE("Portal.Cases.EnablePinnedCase",GlobalVariableType.SELECTION, Option.FALSE.toString(),"enablePinCase");
 
   private String key;
