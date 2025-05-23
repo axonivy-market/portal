fd30a6d8737594cfc2c20be9089b35122c1b2891
lndanh <danh.lengoc@axonactive.com>
2025-05-23 15:56:02 +0700

    IVYPORTAL-18785 Create concept for Side Step processes MVP - Handle feedback

--- AxonIvyPortal/portal/src/ch/ivy/addon/portal/generic/bean/SideStepProcessBean.java
+++ AxonIvyPortal/portal/src/ch/ivy/addon/portal/generic/bean/SideStepProcessBean.java
@@ -6,7 +6,6 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
-import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 
