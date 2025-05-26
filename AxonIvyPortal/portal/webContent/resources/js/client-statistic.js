f8fa038ca8863caf3bef7c060298b7034db24972
Loc Nguyen <loc.nguyenthanh@axonactive.com>
2025-05-26 14:28:10 +0700

    feature/IVYPORTAL-18999-Assessibility-issue-with-Portal-LE - Update css
    # Conflicts:
    #	AxonIvyPortal/portal/webContent/resources/css/dashboard.css
    #	AxonIvyPortal/portal/webContent/resources/js/client-statistic.js

--- AxonIvyPortal/portal/webContent/resources/js/client-statistic.js
+++ AxonIvyPortal/portal/webContent/resources/js/client-statistic.js
@@ -301,7 +301,7 @@ class ClientCanvasChart extends ClientChart {
 
   // Method to init the dashboard statistic widget title
   initWidgetTitle() {
-    $(this.chart).parents('.dashboard__widget').find('.widget__header > .widget__header-title')
+    $(this.chart).parents('.dashboard__widget').find('.widget__header > .widget__header-title > h1')
       .text(this.data.chartConfig.name);
   }
 
@@ -613,7 +613,8 @@ class ClientNumberChart extends ClientChart {
   initWidgetHeaderName(chart, widgetName) {
     let widgetHeader = $(chart).parents(".card-widget-panel")
       .find(".widget__header")
-      .find(".widget__header-title").get(0);
+	  .find(".widget__header-title")
+      .find("h1").get(0);
     if (widgetHeader) {
       widgetHeader.textContent = widgetName;
     }
