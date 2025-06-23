a61480e5df7d72d1c4c7393bb7713b690a24e374
lmluat-axonivy <luat.luongminh@axonactive.com>
2025-06-23 15:40:07 +0700

    feature/IVYPORTAL-17444-Condition-based-coloring-portal-charts
    - Update css and UI

--- AxonIvyPortal/portal/src/com/axonivy/portal/enums/statistic/OperatorField.java
+++ AxonIvyPortal/portal/src/com/axonivy/portal/enums/statistic/OperatorField.java
@@ -5,17 +5,18 @@ import java.util.Collections;
 import java.util.EnumSet;
 import java.util.Set;
 
+import com.axonivy.portal.enums.HasCmsName;
 import com.fasterxml.jackson.annotation.JsonValue;
 
-public enum OperatorField {
-  Greater("greater"),
-  Less("less"),
-  GreaterOrEqual("greaterOrEqual"),
-  LessOrEqual("lessOrEqual"),
-  Equal("equal");
+public enum OperatorField implements HasCmsName {
+  GREATER("greater"),
+  LESS("less"),
+  GREATEROREQUAL("greaterOrEqual"),
+  LESSOREQUAL("lessOrEqual"),
+  EQUAL("equal");
 
   public static final Set<OperatorField> OPERATORS = Collections.unmodifiableSet(
-      EnumSet.of(Greater, Less, GreaterOrEqual, LessOrEqual, Equal));
+      EnumSet.of(GREATER, LESS, GREATEROREQUAL, LESSOREQUAL, EQUAL));
   
   private String name;
 
