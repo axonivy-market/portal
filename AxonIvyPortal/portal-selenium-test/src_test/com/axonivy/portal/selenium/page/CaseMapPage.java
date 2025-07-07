6d1875805705987404d46b50572a13ccc4b9d4f4
tphvu-axonivy <vu.tongphanhoang@axonactive.com>
2025-07-07 11:25:56 +0700

    IVYPORTAL-19220 Improve document screenshot
    - change condition to visible

--- AxonIvyPortal/portal-selenium-test/src_test/com/axonivy/portal/selenium/page/CaseMapPage.java
+++ AxonIvyPortal/portal-selenium-test/src_test/com/axonivy/portal/selenium/page/CaseMapPage.java
@@ -143,7 +143,7 @@ public class CaseMapPage extends TemplatePage {
 
   public void waitForCollectPersonalDataFormVisible() {
     $("form[id='form']").shouldBe(Condition.appear, DEFAULT_TIMEOUT);
-    $("input[id='form:first-name']").shouldBe(Condition.interactable, DEFAULT_TIMEOUT);
+    $("input[id='form:first-name']").shouldBe(Condition.visible, DEFAULT_TIMEOUT);
   }
 
 }
