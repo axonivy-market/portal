f601e99777368b8111df449c867e0354f2c63356
lmluat-axonivy <luat.luongminh@axonactive.com>
2025-07-24 10:05:53 +0700

    feature/IVYPORTAL-19222-Provide-a-mechanism-to-turn-off-the-shortcuts-12
    - Handle feedback

--- AxonIvyPortal/portal/src/ch/ivy/addon/portalkit/bean/MasterDataBean.java
+++ AxonIvyPortal/portal/src/ch/ivy/addon/portalkit/bean/MasterDataBean.java
@@ -143,12 +143,6 @@ public class MasterDataBean implements Serializable {
   }
   
   public boolean isKeyboardShortcutsEnabled() {
-    boolean isAllowedConfigShortcutsByAdmin = GlobalSettingService.getInstance().isAllowedConfigShortcutsByAdmin();
-    if (isAllowedConfigShortcutsByAdmin) {
-      return UserSettingService.getInstance().isKeyboardShortcutsEnabled();
-    }
-    UserSettingService.getInstance().updateUserProperty(UserProperty.ENABLE_KEYBOARD_SHORTCUTS,
-        Boolean.TRUE.toString());
-    return true;
+    return UserSettingService.getInstance().isKeyboardShortcutsEnabled();
   }
 }
