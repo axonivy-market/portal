5f145a92dc83de5285ccaa883117e78778e0b7c5
hhqthang-axonivy <hhqthang@axonactive.com>
2025-09-04 10:02:16 +0700

    IVYPORTAL-19324 Implement Renaming functionality in Document table - Handle feedback

--- AxonIvyPortal/portal-components/src/com/axonivy/portal/components/enums/NewFilenameValidation.java
+++ AxonIvyPortal/portal-components/src/com/axonivy/portal/components/enums/NewFilenameValidation.java
@@ -1,11 +1,15 @@
 package com.axonivy.portal.components.enums;
 
+import java.util.Optional;
+
+import org.apache.commons.lang3.StringUtils;
+
 import ch.ivyteam.ivy.environment.Ivy;
 
 public enum NewFilenameValidation {
   ALREADY_EXIST, INVALID_FORMAT, VALID;
 
   public String getLabel() {
-    return Ivy.cms().co("/Labels/Enums/NewFilenameValidation/" + name());
+    return Optional.ofNullable(Ivy.cms().co("/Labels/Enums/NewFilenameValidation/" + name())).orElse(StringUtils.EMPTY);
   }
 }
