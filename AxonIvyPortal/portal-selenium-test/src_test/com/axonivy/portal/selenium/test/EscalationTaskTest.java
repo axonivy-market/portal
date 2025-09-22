870d6b2bd90d6a74de3b80b00e2ba07d3d25fe12
Vu Tong <vu.tongphanhoang@axonactive.com>
2025-09-22 15:35:51 +0700

    Update EscalationTaskTest.java

--- AxonIvyPortal/portal-selenium-test/src_test/com/axonivy/portal/selenium/test/EscalationTaskTest.java
+++ AxonIvyPortal/portal-selenium-test/src_test/com/axonivy/portal/selenium/test/EscalationTaskTest.java
@@ -2,6 +2,8 @@ package com.axonivy.portal.selenium.test;
 
 import static com.codeborne.selenide.CollectionCondition.size;
 
+import java.io.IOException;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Dimension;
@@ -91,22 +93,25 @@ public class EscalationTaskTest extends BaseTest {
   }
 
   @Test
-  public void testTriggerEscalationTaskOnRelatedTasksOfCase() {
+  public void testTriggerEscalationTaskOnRelatedTasksOfCase() throws IOException {
     updateGlobalVariable(Variable.TASK_BEHAVIOUR_WHEN_CLICKING_ON_LINE_IN_TASK_LIST.getKey(), ACCESS_TASK_DETAILS);
     grantSpecificPortalPermission(PortalPermission.SYSTEM_TASK_READ_ALL);
     login(TestAccount.ADMIN_USER);
     redirectToNewDashBoard();
     ScreenshotUtils.resizeBrowser(new Dimension(1980, 1080));
     CaseWidgetNewDashBoardPage caseWidgetPage = NavigationHelper.navigateToCaseList();
+    ScreenshotUtils.capturePageScreenshot(ScreenshotUtils.CASE_DETAIL_FOLDER + "testTriggerEscalationTaskOnRelatedTasksOfCase-checkCaseList");
     CaseDetailsPage caseDetailsPage = caseWidgetPage.openDetailsCase(TRIGGER_ESCALATION_CASE);
+    ScreenshotUtils.capturePageScreenshot(ScreenshotUtils.CASE_DETAIL_FOLDER + "testTriggerEscalationTaskOnRelatedTasksOfCase-checkInCaseDetailsRelatedTasks");
     caseDetailsPage.getNameOfRelatedTask(0).shouldHave(Condition.text(SICK_LEAVE_REQUEST));
     caseDetailsPage.clickRelatedTaskActionButton(0);
     caseDetailsPage.triggerEscalationTask(0);
-    refreshPage();
+    ScreenshotUtils.capturePageScreenshot(ScreenshotUtils.CASE_DETAIL_FOLDER + "testTriggerEscalationTaskOnRelatedTasksOfCase-afterEscalation");
     caseDetailsPage = new CaseDetailsPage();
     caseDetailsPage.getNameOfRelatedTask(2).shouldHave(Condition.text(SICK_LEAVE_REQUEST));
     caseDetailsPage.getStateOfRelatedTask(2).shouldHave(Condition.text("Destroyed"));
     caseDetailsPage.getNameOfRelatedTask(0).shouldHave(Condition.text(SICK_LEAVE_REQUEST_ESCALATED));
+    ScreenshotUtils.capturePageScreenshot(ScreenshotUtils.CASE_DETAIL_FOLDER + "testTriggerEscalationTaskOnRelatedTasksOfCase-finalPictureOfTest");
   }
 
   @Test
