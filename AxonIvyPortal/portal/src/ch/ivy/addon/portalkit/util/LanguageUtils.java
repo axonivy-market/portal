b8d2755c61779632e5ac378098a2c1e094a31ae3
lmluat-axonivy <luat.luongminh@axonactive.com>
2025-09-29 18:24:10 +0700

    feature/IVYPORTAL-195880-Locale-Handling-Improvement-for-Widget-Headers-v12
    - Update logic in LanguageUtils

--- AxonIvyPortal/portal/src/ch/ivy/addon/portalkit/util/LanguageUtils.java
+++ AxonIvyPortal/portal/src/ch/ivy/addon/portalkit/util/LanguageUtils.java
@@ -3,7 +3,6 @@ package ch.ivy.addon.portalkit.util;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
-import java.util.Objects;
 import java.util.Optional;
 
 import org.apache.commons.collections4.CollectionUtils;
@@ -51,7 +50,7 @@ public final class LanguageUtils {
   }
 
   public static Optional<DisplayName> findNameInUserLanguage(List<DisplayName> names) {
-    return findNameByFullLocale(names, getUserLocale());
+    return findNameByLanguage(names, getUserLocale().getLanguage());
   }
 
   public static Optional<DisplayName> findNameByLanguage(List<DisplayName> names, String language) {
@@ -82,9 +81,6 @@ public final class LanguageUtils {
     Optional<DisplayName> nameInUserLanguage = LanguageUtils.findNameInUserLanguage(names);
     if (nameInUserLanguage.isPresent()) {
       nameInUserLanguage.get().setValue(name);
-    } else {
-      DisplayName newName = new DisplayName(LanguageUtils.getUserLocale(), name);
-      names.add(newName);
     }
     return new NameResult(names, name);
   }
