c40bc2b8ebc4d28cdb5feb132daca54ba700f228
lmluat-axonivy <luat.luongminh@axonactive.com>
2025-10-01 10:42:31 +0700

    feature/IVYPORTAL-195880-Locale-Handling-Improvement-for-Widget-Headers-v12
    format file

--- AxonIvyPortal/portal/src/ch/ivy/addon/portalkit/util/DisplayNameAdaptor.java
+++ AxonIvyPortal/portal/src/ch/ivy/addon/portalkit/util/DisplayNameAdaptor.java
@@ -9,50 +9,50 @@ import org.primefaces.shaded.json.JSONObject;
 
 public class DisplayNameAdaptor {
 
-	private DisplayNameConvertor convertor;
-	private Locale currentLocale;
+  private DisplayNameConvertor convertor;
+  private Locale currentLocale;
 
-	public DisplayNameAdaptor(String jsonString, Locale currentLocale) {
-		this.currentLocale = currentLocale;
-		this.convertor = parseJson(jsonString, currentLocale);
-	}
+  public DisplayNameAdaptor(String jsonString, Locale currentLocale) {
+    this.currentLocale = currentLocale;
+    this.convertor = parseJson(jsonString, currentLocale);
+  }
 
-	private DisplayNameConvertor parseJson(String jsonString, Locale locale) {
-		if (isValidJson(jsonString)) {
-			return DisplayNameConvertor.parseJson(jsonString);
-		}
-		DisplayNameConvertor displayNameConvertor = new DisplayNameConvertor();
-		displayNameConvertor.add(locale, jsonString);
-		return displayNameConvertor;
-	}
+  private DisplayNameConvertor parseJson(String jsonString, Locale locale) {
+    if (isValidJson(jsonString)) {
+      return DisplayNameConvertor.parseJson(jsonString);
+    }
+    DisplayNameConvertor displayNameConvertor = new DisplayNameConvertor();
+    displayNameConvertor.add(locale, jsonString);
+    return displayNameConvertor;
+  }
 
   @SuppressWarnings("unused")
   private boolean isValidJson(String jsonString) {
-		try {
-			new JSONObject(jsonString);
-		} catch (JSONException e) {
-			try {
-				new JSONArray(jsonString);
-			} catch(JSONException e1) {
-				return false;
-			}
-		}
-		return true;
-	}
+    try {
+      new JSONObject(jsonString);
+    } catch (JSONException e) {
+      try {
+        new JSONArray(jsonString);
+      } catch (JSONException e1) {
+        return false;
+      }
+    }
+    return true;
+  }
 
-	public String getDisplayNameAsString() {
-		return convertor.toString(currentLocale);
-	}
+  public String getDisplayNameAsString() {
+    return convertor.toString(currentLocale);
+  }
 
-	public Map<String, String> getDisplayNameAsMap() {
-		return convertor.getDisplayNameAsMap();
-	}
+  public Map<String, String> getDisplayNameAsMap() {
+    return convertor.getDisplayNameAsMap();
+  }
 
-	public void add(Locale locale, String displayName) {
-		convertor.add(locale, displayName);
-	}
+  public void add(Locale locale, String displayName) {
+    convertor.add(locale, displayName);
+  }
 
-	public String toJson(){
-		return convertor.toJson();
-	}
+  public String toJson() {
+    return convertor.toJson();
+  }
 }
