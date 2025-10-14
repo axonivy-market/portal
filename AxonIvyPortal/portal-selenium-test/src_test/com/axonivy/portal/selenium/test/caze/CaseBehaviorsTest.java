2551c09f8c161ec3a68dd5450db7e20645fe4a45
tphvu-axonivy <vu.tongphanhoang@axonactive.com>
2025-10-14 14:34:02 +0700

    IVYPORTAL-19300 Improve Github selenium build
    - make test failed to test script

--- AxonIvyPortal/portal-selenium-test/src_test/com/axonivy/portal/selenium/test/caze/CaseBehaviorsTest.java
+++ AxonIvyPortal/portal-selenium-test/src_test/com/axonivy/portal/selenium/test/caze/CaseBehaviorsTest.java
@@ -88,6 +88,7 @@ public class CaseBehaviorsTest extends BaseTest {
     CaseDetailsPage caseDetailsPage = caseWidgetPage.openCaseDetailsViaAction(0);
     caseDetailsPage.clickOnRelatedCase(TAKE_ORDER_AND_MAKE_PIZZA);
     additionalCaseDetailsPage = new AdditionalCaseDetailsPage();
-    assertTrue(additionalCaseDetailsPage.isDisplayed());
+//    assertTrue(additionalCaseDetailsPage.isDisplayed());
+    assertFalse(additionalCaseDetailsPage.isDisplayed()); // make test failed on purpose to test script showing failed tests
   }
 }
