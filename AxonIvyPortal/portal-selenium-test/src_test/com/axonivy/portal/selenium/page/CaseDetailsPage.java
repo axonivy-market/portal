acb422895ffb6ac37cd3c44bfed938754707ff93
lmluat-axonivy <luat.luongminh@axonactive.com>
2025-10-15 16:02:57 +0700

    feature/IVYPORTAL-19610-Custom-task-fields-are-not-shown-in-the-task-details-vLE
    - add gui tests and screenshot tests

--- AxonIvyPortal/portal-selenium-test/src_test/com/axonivy/portal/selenium/page/CaseDetailsPage.java
+++ AxonIvyPortal/portal-selenium-test/src_test/com/axonivy/portal/selenium/page/CaseDetailsPage.java
@@ -1105,5 +1105,24 @@ public class CaseDetailsPage extends TemplatePage {
       clickOnSystemTasksCheckbox(checkboxShouldBeChecked);
     }
   }
+  
+  public void clickCustomFieldsButtonOnActions(String taskName) {
+    clickRelatedTaskActionButton(taskName);
+    $("[id$='additional-options:task-custom-fields-command']").shouldBe(getClickableCondition()).click();
+    waitForElementDisplayed(getCustomFieldsDialog(), true);
+  }
+  
+  public SelenideElement getCustomFieldsDialog() {
+    return $("div[id$='task-custom-fields-dialog']");
+  }
+  
+  public List<String> getCustomFieldNamesOnTaskCustomFieldsDialog() {
+    return $$("span[id$='customFieldLabel']")
+        .shouldBe(CollectionCondition.sizeGreaterThanOrEqual(0), DEFAULT_TIMEOUT)
+        .asFixedIterable()
+        .stream()
+        .map(SelenideElement::getText)
+        .collect(Collectors.toList());
+  }
 }
 
