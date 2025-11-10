4693e902d6ac4a3c589da0b7e62167dbc1863e9d
lmluat-axonivy <luat.luongminh@axonactive.com>
2025-11-10 11:34:58 +0700

    bug/IVYPORTAL-19765-Shortcuts-navigation-improvement-vLE
    - enhance

--- AxonIvyPortal/portal/webContent/resources/js/global-search.js
+++ AxonIvyPortal/portal/webContent/resources/js/global-search.js
@@ -16,7 +16,7 @@ if (document) {
   const PROCESS_TAB = 'PROCESS_TAB';
   const TASK_TAB = 'TASK_TAB';
   const CASE_TAB = 'CASE_TAB';
-  const keyShortcuts = ['1', '2', '3', '4', '5', '6', '7', 'a', 'w', 'q'];
+  const keyShortcuts = ['Digit1', 'Digit2', 'Digit3', 'Digit4', 'Digit5', 'Digit6', 'Digit7', 'KeyA', 'KeyW', 'KeyQ'];
   const requestInit = body => {
     return {
       method: "POST",
@@ -143,8 +143,9 @@ if (document) {
       return false;
     }
 
-    if ($(searchFormId).is(':hidden') && event.key !== 'Tab' && !(event.altKey && keyShortcuts.includes(event.key))) {
-          $(eleSearchId).click();
+    if ($(searchFormId).is(':hidden') && event.key !== 'Tab' && !(event.altKey && keyShortcuts.includes(event.code))) {
+      $(eleSearchId).click();
+      return;
     }
 
     if ($(searchFormId).is(':visible')
