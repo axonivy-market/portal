1c19698493a597f933f921b31e454ceb981b9ac1
tphvu-axonivy <vu.tongphanhoang@axonactive.com>
2025-11-13 17:44:11 +0700

    Update NewDashboardPage.java

--- AxonIvyPortal/portal-selenium-test/src_test/com/axonivy/portal/selenium/page/NewDashboardPage.java
+++ AxonIvyPortal/portal-selenium-test/src_test/com/axonivy/portal/selenium/page/NewDashboardPage.java
@@ -891,7 +891,6 @@ public class NewDashboardPage extends TemplatePage {
   public void startTask(int index) {
     String cssSelector =
         String.format("a[id$=':task-component:dashboard-tasks:%d:dashboard-tasks-columns:0:start-task']", index);
-    $(cssSelector).shouldBe(appear, DEFAULT_TIMEOUT).scrollTo();
     $(cssSelector).shouldBe(getClickableCondition(), DEFAULT_TIMEOUT).click();
   }
 
