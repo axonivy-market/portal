0faa9b09ac64fb349c2c880785e984048534ad6c
hhqthang-axonivy <hhqthang@axonactive.com>
2025-12-08 11:15:34 +0700

    IVYPORTAL-19607 User Demos for new Statistics Features - Update doc

--- AxonIvyPortal/portal-selenium-test/src_test/com/axonivy/portal/selenium/document/screenshot/SampleDashboardScreenshotTest.java
+++ AxonIvyPortal/portal-selenium-test/src_test/com/axonivy/portal/selenium/document/screenshot/SampleDashboardScreenshotTest.java
@@ -12,6 +12,7 @@ import org.openqa.selenium.Dimension;
 import com.axonivy.ivy.webtest.IvyWebTest;
 import com.axonivy.portal.selenium.common.ScreenshotBaseTest;
 import com.axonivy.portal.selenium.common.ScreenshotUtils;
+import com.axonivy.portal.selenium.common.Sleeper;
 import com.axonivy.portal.selenium.common.TestAccount;
 import com.axonivy.portal.selenium.page.DashboardConfigurationPage;
 import com.axonivy.portal.selenium.page.MainMenuPage;
@@ -67,5 +68,7 @@ public class SampleDashboardScreenshotTest extends ScreenshotBaseTest {
 
   private void waitForDashboardLoaded() {
     $("span[id$='welcome-text']").shouldHave(Condition.text("KPI Procurement Overview"), DEFAULT_TIMEOUT);
+    // Wait for chart animation
+    Sleeper.sleep(2000);
   }
 }
