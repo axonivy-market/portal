eb9fe7cbead6058541d1b2941397b9b3e6ff7a52
Thang Huynh <hhqthang@axonactive.com>
2025-12-08 11:57:17 +0700

    IVYPORTAL-19607 User Demos for new Statistics Features (#2851)
    - Implement sample statistic demo
    - Update doc

---
+++ AxonIvyPortal/portal-selenium-test/src_test/com/axonivy/portal/selenium/document/screenshot/SampleDashboardScreenshotTest.java
@@ -0,0 +1,74 @@
+package com.axonivy.portal.selenium.document.screenshot;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
+
+import java.io.IOException;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Dimension;
+
+import com.axonivy.ivy.webtest.IvyWebTest;
+import com.axonivy.portal.selenium.common.ScreenshotBaseTest;
+import com.axonivy.portal.selenium.common.ScreenshotUtils;
+import com.axonivy.portal.selenium.common.Sleeper;
+import com.axonivy.portal.selenium.common.TestAccount;
+import com.axonivy.portal.selenium.page.DashboardConfigurationPage;
+import com.axonivy.portal.selenium.page.MainMenuPage;
+import com.axonivy.portal.selenium.page.NewDashboardDetailsEditPage;
+import com.axonivy.portal.selenium.page.NewDashboardPage;
+import com.axonivy.portal.selenium.page.ProcessWidgetPage;
+import com.codeborne.selenide.Condition;
+
+@IvyWebTest
+public class SampleDashboardScreenshotTest extends ScreenshotBaseTest {
+  
+  private final String SAMPLE_STATISTIC_DASHBOARD_MENU = "KPI Procurement Overview";
+  private final String INIT_SAMPLE_STATISTIC_CONFIG_PROCESS = "Init sample statistic dashboard config";
+  
+  @Override
+  @BeforeEach
+  public void setup() {
+    super.setup();
+    login(TestAccount.ADMIN_USER);
+  }
+
+  @Test
+  public void screenshotSampleStatisticDashboard() throws IOException {
+    showNewDashboard();
+    MainMenuPage mainMenuPage = new MainMenuPage();
+
+    ProcessWidgetPage processWidgetPage = mainMenuPage.openProcessList();
+    processWidgetPage.startProcessByName(INIT_SAMPLE_STATISTIC_CONFIG_PROCESS);
+
+    mainMenuPage.clickMainMenuItem(SAMPLE_STATISTIC_DASHBOARD_MENU);
+    waitForDashboardLoaded();
+
+    ScreenshotUtils.resizeBrowserAndCaptureWholeScreen(ScreenshotUtils.SAMPLE_DASHBOARD_FOLDER + "statistic-sample-dashboard",
+        new Dimension(1800, 2400));
+
+    NewDashboardPage dashboardPage = new NewDashboardPage();
+    DashboardConfigurationPage dashboardConfigurationPage = dashboardPage.openDashboardConfigurationPage();
+    dashboardConfigurationPage.openEditPublicDashboardsPage();
+    dashboardConfigurationPage.clickButtonOnDashboardConfigurationActionMenu("Configuration", 2);
+    NewDashboardDetailsEditPage dashboardConfigPage = new NewDashboardDetailsEditPage();
+    dashboardConfigPage.addWidget().click();
+    scrollToExampleStatisticInAddWidgetDialog();
+    ScreenshotUtils.resizeBrowserAndCaptureWholeScreen(ScreenshotUtils.SAMPLE_DASHBOARD_FOLDER + "statistic-config",
+        new Dimension(1500, 1500));
+  }
+
+  private void scrollToExampleStatisticInAddWidgetDialog() {
+    $$("span[id$='statistic-widget-name']")
+      .findBy(Condition.text("Total Procurement Spend per Department"))
+      .shouldBe(Condition.visible, DEFAULT_TIMEOUT)
+      .scrollIntoCenter();
+  }
+
+  private void waitForDashboardLoaded() {
+    $("span[id$='welcome-text']").shouldHave(Condition.text("KPI Procurement Overview"), DEFAULT_TIMEOUT);
+    // Wait for chart animation
+    Sleeper.sleep(2000);
+  }
+}
