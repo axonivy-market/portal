d46c441c10a300331f9a67ba834076792e71afae
Christian Strebel <christian.strebel@axonivy.com>
2025-12-12 10:37:45 +0100

    Remove guava (com.google.common) usages

--- AxonIvyPortal/portal/src/ch/ivy/addon/portalkit/util/PortalCustomFieldUtils.java
+++ AxonIvyPortal/portal/src/ch/ivy/addon/portalkit/util/PortalCustomFieldUtils.java
@@ -5,8 +5,6 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.Set;
 
-import com.google.common.collect.Iterables;
-
 import ch.ivy.addon.portalkit.enums.DashboardColumnType;
 import ch.ivyteam.ivy.workflow.custom.field.CustomFieldType;
 import ch.ivyteam.ivy.workflow.custom.field.ICustomFieldMeta;
@@ -46,10 +44,8 @@ public class PortalCustomFieldUtils {
         .forEach(field -> {
           for (String keyword : keywordList) {
             Iterable<Object> list = field.values().matching(keyword);
-            if (!Iterables.isEmpty(list)) {
-              for (Object obj : list) {
-                matchingValueList.add(obj.toString());
-              }
+            for (Object obj : list) {
+              matchingValueList.add(obj.toString());
             }
           }
         });
