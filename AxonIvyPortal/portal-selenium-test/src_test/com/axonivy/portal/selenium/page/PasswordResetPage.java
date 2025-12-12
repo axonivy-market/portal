1f992b3e01155967068b5bbf4b88b8ffc039a006
tphvu-axonivy <vu.tongphanhoang@axonactive.com>
2025-12-12 14:54:27 +0700

    Update PasswordResetPage.java

--- AxonIvyPortal/portal-selenium-test/src_test/com/axonivy/portal/selenium/page/PasswordResetPage.java
+++ AxonIvyPortal/portal-selenium-test/src_test/com/axonivy/portal/selenium/page/PasswordResetPage.java
@@ -37,7 +37,7 @@ public class PasswordResetPage extends TemplatePage {
     if (strongPasswordEnough) {
       $("[id='password-reset:reset-password-form:result-message']").shouldBe(Condition.appear, DEFAULT_TIMEOUT);
     } else {
-      $("span[class='ui-messages-error']").shouldBe(Condition.appear, DEFAULT_TIMEOUT);
+      $("div[class='ui-messages-error']").shouldBe(Condition.appear, DEFAULT_TIMEOUT);
     }
   }
 
