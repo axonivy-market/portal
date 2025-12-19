b90726a3e9547e1fa51a14329143ca64421dab89
tphvu-axonivy <vu.tongphanhoang@axonactive.com>
2025-12-19 18:48:24 +0700

    IVYPORTAL-19971 Fix customized iframe url showing blank
    - Change page target
    - Adapt for documentation

--- AxonIvyPortal/portal-selenium-test/src_test/com/axonivy/portal/selenium/page/TaskDetailsPage.java
+++ AxonIvyPortal/portal-selenium-test/src_test/com/axonivy/portal/selenium/page/TaskDetailsPage.java
@@ -276,7 +276,7 @@ public class TaskDetailsPage extends TemplatePage {
 
   public void waitForIFrameURLWidgetLoad() {
     SelenideElement iframe = $("iframe[name='custom-widget-iframe-url']").shouldBe(Condition.appear, DEFAULT_TIMEOUT);
-    iframe.shouldHave(Condition.attributeMatching("src", ".*example\\.com.*"));
+    iframe.shouldHave(Condition.attributeMatching("src", ".*lucide\\.dev.*"));
   }
 
   public void waitForIFrameWidgetLoad() {
