38adf869d101f55d6a7d30daad5d7ad4ff2b9a9a
lmluat-axonivy <luat.luongminh@axonactive.com>
2025-12-24 17:30:10 +0700

    feature/IVYPORTAL-19814-Improve-portal-components-to-simplify-customer-project-migration-LE
    - update doc

--- AxonIvyPortal/portal-components/src/com/axonivy/portal/components/publicapi/PortalDateTimeAPI.java
+++ AxonIvyPortal/portal-components/src/com/axonivy/portal/components/publicapi/PortalDateTimeAPI.java
@@ -23,8 +23,8 @@ public final class PortalDateTimeAPI {
   /**
    * Get the date time pattern based on the following variables:
    * - Portal.DateTimeFormat.HideYear: If true, the year part will be removed from the pattern.
-   * - Portal.DateTimeFormat.HideTime: If true and isDateFilter is false, the time part will be excluded.
-   * - Portal.DateTimeFormat.DateFilterWithTime: If true and isDateFilter is true, the time part will be included.
+   * - Portal.DateTimeFormat.HideTime: If true, the time part will be excluded.
+   * - Portal.DateTimeFormat.DateFilterWithTime: If true, the time part will be included.
    * 
    * @param isDateFilter - true if this is for date filter
    * @return date time pattern string for date picker
