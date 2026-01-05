fc590166511b149cad75ce9a1ff7e536ae325c08
lmluat-axonivy <luat.luongminh@axonactive.com>
2026-01-05 09:41:32 +0700

    feature/IVYPORTAL-17379-Extend-Leave-Task-logic-with-customize-feature-v12
    - handle feedback

--- AxonIvyPortal/portal-selenium-test/src_test/com/axonivy/portal/selenium/common/BaseTest.java
+++ AxonIvyPortal/portal-selenium-test/src_test/com/axonivy/portal/selenium/common/BaseTest.java
@@ -50,7 +50,6 @@ public class BaseTest {
       "portalKitTestHelper/153CACC26D0D4C3D/testProcessViewerPermission.ivp";
   protected String createTestingTasksUrl = "portal-developer-examples/162511D2577DBA88/CategoriedLeaveRequest.ivp";
   protected String createTestDataForCustomFieldsWithCMS = "portal-developer-examples/162511D2577DBA88/createDataForCustomFieldWithCmsValue.ivp";
-  protected String createStockInvestment = "portal-developer-examples/162511D2577DBA88/createStockInvestment.ivp";
   protected String createRelatedTaskWithNoResponsible =
       "InternalSupport/14B2FC03D2E87141/TestRelatedTaskWithNoResponsible.ivp";
   protected String createCaseWithTechnicalCaseUrl = "InternalSupport/15B1EA24CCF377E8/OrderPizza.ivp";
@@ -217,10 +216,6 @@ public class BaseTest {
 
   public void createTestingTasks() {
     redirectToRelativeLink(createTestingTasksUrl);
-  }
-  
-  public void createStockInvestment() {
-    redirectToRelativeLink(createStockInvestment);
   }
 
   public void createRelatedTaskWithNoResponsible() {
--- AxonIvyPortal/portal-selenium-test/src_test/com/axonivy/portal/selenium/test/task/ExtendingLogicOnTaskLeaveAndReserveActionsTest.java
+++ AxonIvyPortal/portal-selenium-test/src_test/com/axonivy/portal/selenium/test/task/ExtendingLogicOnTaskLeaveAndReserveActionsTest.java
@@ -7,10 +7,13 @@ import org.junit.jupiter.api.Test;
 
 import com.axonivy.ivy.webtest.IvyWebTest;
 import com.axonivy.portal.selenium.common.BaseTest;
+import com.axonivy.portal.selenium.common.FilterOperator;
+import com.axonivy.portal.selenium.common.FilterValueType;
 import com.axonivy.portal.selenium.common.NavigationHelper;
 import com.axonivy.portal.selenium.common.TestAccount;
 import com.axonivy.portal.selenium.page.MainMenuPage;
 import com.axonivy.portal.selenium.page.NewDashboardPage;
+import com.axonivy.portal.selenium.page.NotificationCompactPage;
 import com.axonivy.portal.selenium.page.TaskDetailsPage;
 import com.axonivy.portal.selenium.page.TaskTemplateIFramePage;
 import com.axonivy.portal.selenium.page.TopMenuTaskWidgetPage;
@@ -19,27 +22,55 @@ import com.codeborne.selenide.CollectionCondition;
 
 @IvyWebTest
 public class ExtendingLogicOnTaskLeaveAndReserveActionsTest extends BaseTest {
-
+  private NewDashboardPage dashboardPage;
+  
   @Override
   @BeforeEach
   public void setup() {
     super.setup();
+    dashboardPage = new NewDashboardPage();
   }
-
+  
   @Test
   public void testExecutingCustomizedLogicOnTaskLeaveAction() {
     login(TestAccount.DEMO_USER);
-    createStockInvestment();
-    openDashboard();
+    createTestingTasks();
+    redirectToNewDashBoard();
+    dashboardPage.waitForCaseWidgetLoaded();
     MainMenuPage menu = new MainMenuPage();
     menu.openTaskList();
     TopMenuTaskWidgetPage taskWidget = new TopMenuTaskWidgetPage();
-    taskWidget.clickOnTaskName("Create stock investment");
+    taskWidget.clickOnTaskName("Sick Leave Request");
     TaskTemplateIFramePage templatePage = new TaskTemplateIFramePage();
     templatePage.clickOnLogo();
     WorkingTaskDialogPage dialogPage = new WorkingTaskDialogPage();
     dialogPage.leaveTask();
-    NavigationHelper.navigateToTaskList();
+    menu.openTaskList();
+    taskWidget.setInputForQuickSearch("Sick Leave Request");
+    taskWidget.countAllTasks().shouldHave(CollectionCondition.size(1), DEFAULT_TIMEOUT);
+    TaskDetailsPage taskDetailsPage = taskWidget.openTaskDetailsPageByAction(0);
+    taskDetailsPage.getNotesWithContent("Demo has left the task recently").shouldHave(CollectionCondition.size(1));
+  }
+  
+  @Test
+  public void testExecutingCustomizedLogicOnNotificationPanel() {
+    login(TestAccount.DEMO_USER);
+    createTestingTasks();
+    redirectToNewDashBoard();
+    dashboardPage.waitForCaseWidgetLoaded();
+    MainMenuPage menu = new MainMenuPage();
+    menu.openTaskList();
+    TopMenuTaskWidgetPage taskWidget = new TopMenuTaskWidgetPage();
+    taskWidget.clickOnTaskName("Sick Leave Request");
+    TaskTemplateIFramePage templatePage = new TaskTemplateIFramePage();
+    NotificationCompactPage notificationPage = templatePage.openNotificationPanel();
+    notificationPage.openNotificationMoreActionsMenu();
+    notificationPage.clickNotificationSettingMenuItem();
+    WorkingTaskDialogPage dialogPage = new WorkingTaskDialogPage();
+    dialogPage.clickOnLeaveButton();
+    menu.openTaskList();
+    taskWidget.setInputForQuickSearch("Sick Leave Request");
+    taskWidget.countAllTasks().shouldHave(CollectionCondition.size(1), DEFAULT_TIMEOUT);
     TaskDetailsPage taskDetailsPage = taskWidget.openTaskDetailsPageByAction(0);
     taskDetailsPage.getNotesWithContent("Demo has left the task recently").shouldHave(CollectionCondition.size(1));
   }
@@ -47,49 +78,41 @@ public class ExtendingLogicOnTaskLeaveAndReserveActionsTest extends BaseTest {
   @Test
   public void testExecutingCustomizedLogicOnTaskReserveAction() {
     login(TestAccount.DEMO_USER);
-    createStockInvestment();
-    openDashboard();
+    createTestingTasks();
+    redirectToNewDashBoard();
     MainMenuPage menu = new MainMenuPage();
     menu.openTaskList();
     TopMenuTaskWidgetPage taskWidget = new TopMenuTaskWidgetPage();
-    taskWidget.clickOnTaskName("Create stock investment");
-    TaskTemplateIFramePage templatePage = new TaskTemplateIFramePage();
-    templatePage.clickOnLogo();
-    WorkingTaskDialogPage dialogPage = new WorkingTaskDialogPage();
-    dialogPage.reserveTask();
+    taskWidget.setInputForQuickSearch("Sick Leave Request");
+    taskWidget.reserveTask(0);
     login(TestAccount.ADMIN_USER);
     NavigationHelper.navigateToTaskList();
+    taskWidget.setInputForQuickSearch("Sick Leave Request");
     taskWidget.clickCustomFieldsButtonOnActions(0);
     assertTrue(taskWidget.isCustomFieldsDialogDisplayed());
     List<String> taskCustomFieldNames = taskWidget.getCustomFieldNamesOnTaskCustomFieldsDialog();
-    List<String> taskCustomFieldValues = taskWidget.getCustomFieldValuesOnTaskCustomFieldsDialog();
     assertFalse(taskCustomFieldNames.isEmpty());
-    assertFalse(taskCustomFieldValues.isEmpty());
-    assertEquals(taskCustomFieldNames.getFirst(), "reserveTask");
-    assertEquals(taskCustomFieldValues.getFirst(), "1");
+    assertTrue(taskCustomFieldNames.contains("reserveTask"));
   }
   
   @Test
   public void testExecutingCustomizedLogicOnTaskReserveInActionPanel() {
     login(TestAccount.ADMIN_USER);
-    createStockInvestment();
-    openDashboard();
+    createTestingTasks();
+    redirectToNewDashBoard();
     MainMenuPage menu = new MainMenuPage();
     menu.openTaskList();
     TopMenuTaskWidgetPage taskWidget = new TopMenuTaskWidgetPage();
+    taskWidget.openFilterWidget();
+    taskWidget.addFilter("Task name", FilterOperator.CONTAINS);
+    taskWidget.inputValueOnLatestFilter(FilterValueType.TEXT, "Sick Leave Request");
+    taskWidget.applyFilter();
     taskWidget.reserveTask(0);
     taskWidget.clickCustomFieldsButtonOnActions(0);
+    assertTrue(taskWidget.isCustomFieldsDialogDisplayed());
     List<String> taskCustomFieldNames = taskWidget.getCustomFieldNamesOnTaskCustomFieldsDialog();
-    List<String> taskCustomFieldValues = taskWidget.getCustomFieldValuesOnTaskCustomFieldsDialog();
     assertFalse(taskCustomFieldNames.isEmpty());
-    assertFalse(taskCustomFieldValues.isEmpty());
-    assertEquals(taskCustomFieldNames.getFirst(), "reserveTask");
-    assertEquals(taskCustomFieldValues.getFirst(), "1");
+    assertTrue(taskCustomFieldNames.contains("reserveTask"));
   }
 
-  private void openDashboard() {
-    redirectToNewDashBoard();
-    NewDashboardPage dashboardPage = new NewDashboardPage();
-    dashboardPage.waitForCaseWidgetLoaded();
-  }
 }
