c4c858287a89d9854c2354d3a013c491e7ae7862
hhqthang-axonivy <hhqthang@axonactive.com>
2026-01-15 14:19:46 +0700

    IVYPORTAL-20046 Remove auto re add functionality of Full Task and Case List - Update test and screenshot.

--- AxonIvyPortal/portal-selenium-test/src_test/com/axonivy/portal/selenium/document/screenshot/DashboardConfigurationScreenshotTest.java
+++ AxonIvyPortal/portal-selenium-test/src_test/com/axonivy/portal/selenium/document/screenshot/DashboardConfigurationScreenshotTest.java
@@ -66,7 +66,7 @@ public class DashboardConfigurationScreenshotTest extends ScreenshotBaseTest {
     ScreenshotUtils.captureElementScreenshot(dashboardConfigurationPage.getDashboardTemplates(),
         ScreenshotUtils.DASHBOARD_CONFIGURATION_FOLDER + "dashboard-templates");
     // screenshot highlight for accessibility template
-    // ScreenshotUtils.executeDecorateJs("createBlackMediumOutline($($('.dashboard-action-container')[4]));");
+    ScreenshotUtils.executeDecorateJs("createBlackMediumOutline($($('.dashboard-action-container')[4]));");
     ScreenshotUtils.captureElementWithMarginOptionScreenshot(dashboardConfigurationPage.getDashboardTemplates(),
             ScreenshotUtils.ACCESSIBILITY_DASHBOARD_FOLDER + "accessibility-dashboard-creation", new ScreenshotMargin(10));
     dashboardConfigurationPage.closeAddDashboardDialog();
