664d708d1a5c7cb7ebf46bebf4184ec7c114cd67
Thang Huynh <hhqthang@axonactive.com>
2026-01-27 09:19:54 +0700

    IVYPORTAL-20046 Remove auto re add functionality of Full Task and Case List (#3045)
    * IVYPORTAL-20046 Remove auto re add functionality of Full Task and Case List
    - Add cases and tasks list dashboard to dashboard templates.
    - Remove auto add again cases and tasks dashboard if not exist.
    - Add the default cases and tasks dashboard to the default-dashboard.json used when running tests.
    - Add tests that add and remove dashboard using task list and case list template.
    - Update documentation.
    - Remove ACCESS_FULL_CASE_LIST and ACCESS_FULL_TASK_LIST permissions.

--- AxonIvyPortal/portal-selenium-test/src_test/com/axonivy/portal/selenium/test/dashboard/DashboardConfigurationTest.java
+++ AxonIvyPortal/portal-selenium-test/src_test/com/axonivy/portal/selenium/test/dashboard/DashboardConfigurationTest.java
@@ -16,6 +16,7 @@ import com.axonivy.portal.selenium.common.TestAccount;
 import com.axonivy.portal.selenium.page.CaseWidgetNewDashBoardPage;
 import com.axonivy.portal.selenium.page.DashboardConfigurationPage;
 import com.axonivy.portal.selenium.page.DashboardModificationPage;
+import com.axonivy.portal.selenium.page.MainMenuPage;
 import com.axonivy.portal.selenium.page.NewDashboardDetailsEditPage;
 import com.axonivy.portal.selenium.page.NewDashboardPage;
 import com.axonivy.portal.selenium.page.ProcessWidgetNewDashBoardPage;
@@ -37,6 +38,9 @@ public class DashboardConfigurationTest extends BaseTest {
   private static final String YOUR_CASES_WIDGET = "Your Cases";
   private static final String YOUR_PROCESS_WIDGET = "Your Processes";
   private static final String NEW_PRIVATE_DASHBOARD = "New private dashboard";
+  private static final int NUMBER_OF_DEFAULT_DASHBOARDS = 3;
+  private static final int FULL_TASKS_TEMPLATE_INDEX = 3;
+  private static final int FULL_CASES_TEMPLATE_INDEX = 4;
 
   @Override
   @BeforeEach
@@ -98,6 +102,35 @@ public class DashboardConfigurationTest extends BaseTest {
     DashboardModificationPage modificationPage = navigateToConfigurationAndEditDashboards(true);
     modificationPage.clickDeleteDashboardByName("Dashboard");
     modificationPage.getDashboardRows().shouldHave(size(2));
+  }
+
+  @Test
+  public void testDeleteDefaultCaseAndTaskDashboard() {
+    DashboardModificationPage modificationPage = navigateToConfigurationAndEditDashboards(true);
+    modificationPage.clickDeleteDashboardByName("Cases");
+    modificationPage.getDashboardRows().shouldHave(size(2));
+
+    modificationPage.clickDeleteDashboardByName("Tasks");
+    modificationPage.getDashboardRows().shouldHave(size(1));
+
+    redirectToNewDashBoard();
+    MainMenuPage mainMenuPage = new MainMenuPage();
+    String expectedMenu = "Dashboard,Processes,Google";
+    assertEquals(expectedMenu, mainMenuPage.getMenuItemsAsString());
+  }
+
+  @Test
+  public void testAddDashboardUseFullCasesTemplate() {
+    testAddDashboardUsingTemplate("My Cases", FULL_CASES_TEMPLATE_INDEX, 
+        "This is my cases dashboard created using default full cases template.",
+        List.of("Everybody (Everybody)"), true, DashboardDisplayType.TOP_MENU, 1);
+  }
+
+  @Test
+  public void testAddDashboardUseFullTasksTemplate() {
+    testAddDashboardUsingTemplate("My Tasks", FULL_TASKS_TEMPLATE_INDEX, 
+        "This is my tasks dashboard created using default full tasks template.",
+        List.of("Everybody (Everybody)"), true, DashboardDisplayType.TOP_MENU, 1);
   }
 
   @Test
@@ -500,4 +533,28 @@ public class DashboardConfigurationTest extends BaseTest {
     assertEquals(element.getAttribute("title"), "Accessibility Shortcuts frame");
   }
   
+  private void testAddDashboardUsingTemplate(String name, int templateIndex, String description,
+      List<String> permissions, boolean isPublicDashboard, DashboardDisplayType type, int numberOfWidgets) {
+    String icon = "fa-coffee";
+
+    DashboardConfigurationPage configurationPage = newDashboardPage.openDashboardConfigurationPage();
+    configurationPage.openCreatePublicDashboardMenu();
+    configurationPage.createPublicDashboardFromTemplate(name, icon, description, permissions, templateIndex, isPublicDashboard, type);
+
+    NewDashboardDetailsEditPage newDashboardDetailsEditPage = new NewDashboardDetailsEditPage();
+    newDashboardDetailsEditPage.getTitleByIndex(0).shouldBe(Condition.exactText(name));
+    newDashboardDetailsEditPage.getIconByIndex(0, icon).shouldBe(Condition.appear);
+    newDashboardDetailsEditPage.getWidgets().shouldBe(CollectionCondition.size(numberOfWidgets));
+
+    goBackConfigurationAndVerifyDashboards(name, description, newDashboardDetailsEditPage, isPublicDashboard);
+
+    MainMenuPage mainMenuPage = new MainMenuPage();
+    String expectedMenu = "Dashboard,Processes,Tasks,Cases," + name + ",Google";
+    assertEquals(expectedMenu, mainMenuPage.getMenuItemsAsString());
+
+    DashboardModificationPage modificationPage = navigateToConfigurationAndEditDashboards(isPublicDashboard);
+    modificationPage.getDashboardRows().shouldHave(size(NUMBER_OF_DEFAULT_DASHBOARDS + 1));
+    modificationPage.clickDeleteDashboardByName(name);
+    modificationPage.getDashboardRows().shouldHave(size(NUMBER_OF_DEFAULT_DASHBOARDS));
+  }
 }
