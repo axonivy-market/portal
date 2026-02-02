2f9f2ffdfb9c139c37615ebf2626467cb0f25637
Thai Phan <thai.phanvan@axonactive.com>
2026-02-02 15:41:58 +0700

    IVYPORTAL-19221 Fixed "Persistent object has been deleted"

--- AxonIvyPortal/portal/src/ch/ivy/addon/portal/generic/bean/IFrameTaskTemplateBean.java
+++ AxonIvyPortal/portal/src/ch/ivy/addon/portal/generic/bean/IFrameTaskTemplateBean.java
@@ -161,9 +161,9 @@ public class IFrameTaskTemplateBean extends AbstractTaskTemplateBean implements 
   public void resetTaskAndNavigateToUrl() throws IOException {
     String sessionAttributeKey = SessionAttribute.RESET_TASK_UUID.name();
     Object value = SecurityServiceUtils.getSessionAttribute(sessionAttributeKey);
-    String taskId = value instanceof String ? (String) value : null;
-    if (taskId != null) {
-      ITask task = TaskService.newInstance().findTaskByUUID(taskId);
+    String taskUuid = value instanceof String ? (String) value : null;
+    if (taskUuid != null) {
+      ITask task = TaskService.newInstance().findTaskByUUID(taskUuid);
       if (TaskUtils.canReset(task)) {
         TaskUtils.resetTask(task);
       }
