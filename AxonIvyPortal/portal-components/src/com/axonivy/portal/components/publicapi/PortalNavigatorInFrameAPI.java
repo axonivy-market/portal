8ddc11c4bba8993da79bff788b5d279a7e08d206
Thai Phan <thai.phanvan@axonactive.com>
2026-02-02 11:15:32 +0700

    IVYPORTAL-19221 Fixed "Persistent object has been deleted"

--- AxonIvyPortal/portal-components/src/com/axonivy/portal/components/publicapi/PortalNavigatorInFrameAPI.java
+++ AxonIvyPortal/portal-components/src/com/axonivy/portal/components/publicapi/PortalNavigatorInFrameAPI.java
@@ -5,6 +5,7 @@ import java.nio.charset.StandardCharsets;
 
 import org.primefaces.PrimeFaces;
 
+import com.axonivy.portal.components.enums.SessionAttribute;
 import com.axonivy.portal.components.generic.navigation.BaseNavigator;
 
 import ch.ivyteam.ivy.environment.Ivy;
@@ -14,19 +15,24 @@ import ch.ivyteam.ivy.environment.Ivy;
  *
  */
 public final class PortalNavigatorInFrameAPI extends BaseNavigator {
-	private PortalNavigatorInFrameAPI() {
-	}
+  private PortalNavigatorInFrameAPI() {
+  }
 
-	/**
-	 * Navigate to target url
-	 * 
-	 * @param url target url
-	 */
-	public static void navigateToUrl(String url) {
-		String statement = "parent.redirectToUrlCommand([{name: 'url', value: '"
-				+ URLDecoder.decode(url, StandardCharsets.UTF_8) + "'}])";
-		PrimeFaces.current().executeScript(statement);
-	}
+  /**
+   * Navigate to target url
+   * 
+   * @param url target url
+   */
+  public static void navigateToUrl(String url) {
+    String statement = "parent.redirectToUrlCommand([{name: 'url', value: '" + URLDecoder.decode(url, StandardCharsets.UTF_8) + "'}])";
+    PrimeFaces.current().executeScript(statement);
+  }
+
+  public static void resetTaskAndRedirectToUrl(String url) {
+    Ivy.session().setAttribute(SessionAttribute.RESET_TASK_ID.toString(), Ivy.wfTask().getId());
+    String statement = "parent.resetTaskAndRedirectToUrlCommand([{name: 'url', value: '" + URLDecoder.decode(url, StandardCharsets.UTF_8) + "'}])";
+    PrimeFaces.current().executeScript(statement);
+  }
 
   /**
    * Navigate to portal home
@@ -35,13 +41,13 @@ public final class PortalNavigatorInFrameAPI extends BaseNavigator {
     navigateToUrl(Ivy.html().applicationHomeLink().getAbsoluteEncoded());
   }
 
-	/**
-	 * Navigate to PortalEndPage without finishing a task, e.g. clicking on Cancel
-	 * button then back to previous page: task list or task details or global search
-	 */
-	public static void navigateToPortalEndPage() {
-		String statement = "parent.redirectToEndPageCommand([{name: 'taskId', value: " + Ivy.wfTask().getId() + "}]);";
-		PrimeFaces.current().executeScript(statement);
-	}
+  /**
+   * Navigate to PortalEndPage without finishing a task, e.g. clicking on Cancel
+   * button then back to previous page: task list or task details or global search
+   */
+  public static void navigateToPortalEndPage() {
+    String statement = "parent.redirectToEndPageCommand([{name: 'taskId', value: " + Ivy.wfTask().getId() + "}]);";
+    PrimeFaces.current().executeScript(statement);
+  }
 
 }
