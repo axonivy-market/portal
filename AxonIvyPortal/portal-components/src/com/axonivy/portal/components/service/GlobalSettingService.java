89e1ff6498378bc35710d35bc6e4346d09559e9e
Lukas Giger <lukas.giger@axonivy.com>
2026-02-12 14:06:49 +0100

    --wip-- Rush B!

--- AxonIvyPortal/portal-components/src/com/axonivy/portal/components/service/GlobalSettingService.java
+++ AxonIvyPortal/portal-components/src/com/axonivy/portal/components/service/GlobalSettingService.java
@@ -1,9 +1,13 @@
 package com.axonivy.portal.components.service;
 
+import java.util.concurrent.ConcurrentHashMap;
+
 import org.apache.commons.lang3.StringUtils;
 
 import com.axonivy.portal.components.enums.GlobalVariable;
+import com.google.inject.servlet.SessionScoped;
 
+import ch.ivyteam.di.restricted.DiCore;
 import ch.ivyteam.ivy.environment.Ivy;
 
 public class GlobalSettingService {
@@ -25,7 +29,7 @@ public class GlobalSettingService {
 
   public String findGlobalSettingValue(GlobalVariable variable) {
     String key = variable.getKey();
-    return Ivy.var().get(key);
+    return DiCore.getGlobalInjector().getInstance(Cache.class).findValue(key);
   }
 
   public Boolean findGlobalSettingValueAsBoolean(GlobalVariable variable, boolean defaultIfEmpty) {
@@ -36,5 +40,15 @@ public class GlobalSettingService {
   public String findGlobalSettingValueAsString(GlobalVariable variable, String defaultIfEmpty) {
     String valueStr = findGlobalSettingValue(variable);
     return StringUtils.isEmpty(valueStr) ? defaultIfEmpty : valueStr;
+  }
+
+  @SessionScoped
+  public static class Cache {
+
+    private final ConcurrentHashMap<String, String> cache = new ConcurrentHashMap<>();
+
+    String findValue(String key) {
+      return cache.computeIfAbsent(key, k -> Ivy.var().get(k));
+    }
   }
 }
