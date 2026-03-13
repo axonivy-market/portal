9e9d773bbc4a665e4aed7948af8a55e7aa83b31f
Thai Phan <thai.phanvan@axonactive.com>
2026-03-13 13:43:04 +0700

    IVYPORTAL-20133 Implement Case widget works with sub cases

--- AxonIvyPortal/portal/src/com/axonivy/portal/enums/CaseQueryType.java
+++ AxonIvyPortal/portal/src/com/axonivy/portal/enums/CaseQueryType.java
@@ -1,5 +1,13 @@
 package com.axonivy.portal.enums;
 
-public enum CaseQueryType implements HasCmsName {
+import org.apache.commons.lang3.StringUtils;
+
+import ch.ivyteam.ivy.environment.Ivy;
+
+public enum CaseQueryType {
   BUSINESS_CASE, SUB_CASE, ALL;
+
+  public String getLabel() {
+    return StringUtils.defaultIfBlank(Ivy.cms().co("/Labels/Enums/CaseQueryType/" + name()), name());
+  }
 }
