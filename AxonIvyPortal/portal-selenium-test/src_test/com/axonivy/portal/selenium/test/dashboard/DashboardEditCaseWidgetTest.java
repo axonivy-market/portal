a2fa6d1e7bae1d56d06fb3047565bffac0280bcd
nhthinh-axonivy <thinh.nguyenhung@axonactive.com>
2026-03-25 10:30:26 +0700

    feature/IVYPORTAL-20004-Remove-deprecated-component-ProcessHistory - Update Example for GUI test

--- AxonIvyPortal/portal-selenium-test/src_test/com/axonivy/portal/selenium/test/dashboard/DashboardEditCaseWidgetTest.java
+++ AxonIvyPortal/portal-selenium-test/src_test/com/axonivy/portal/selenium/test/dashboard/DashboardEditCaseWidgetTest.java
@@ -1,5 +1,7 @@
 package com.axonivy.portal.selenium.test.dashboard;
 
+import static com.codeborne.selenide.Condition.text;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Dimension;
@@ -12,6 +14,7 @@ import com.axonivy.portal.selenium.common.LinkNavigator;
 import com.axonivy.portal.selenium.common.ScreenshotUtils;
 import com.axonivy.portal.selenium.common.TestAccount;
 import com.axonivy.portal.selenium.page.CaseEditWidgetNewDashBoardPage;
+import com.axonivy.portal.selenium.page.CaseWidgetNewDashBoardPage;
 import com.axonivy.portal.selenium.page.DashboardConfigurationPage;
 import com.axonivy.portal.selenium.page.NewDashboardDetailsEditPage;
 import com.codeborne.selenide.CollectionCondition;
@@ -19,6 +22,9 @@ import com.codeborne.selenide.CollectionCondition;
 @IvyWebTest
 public class DashboardEditCaseWidgetTest extends BaseTest {
   private static final String NAME_STR = "Name";
+    private static final String YOUR_CASES_WIDGET = "Your Cases";
+    private static final String ALPHA_COMPANY = "Alpha Company";
+    private static final String ORDER_PIZZA = "Order Pizza";
 
   @Override
   @BeforeEach
@@ -248,10 +254,6 @@ public class DashboardEditCaseWidgetTest extends BaseTest {
     return modificationPage.navigateToEditDashboardDetailsByName("Dashboard");
   }
 
-  // IVYPORTAL-19771 Case widget cannot sort
-  /*
-   * COMMENT OUT THIS TEST DUE TO THE BUG CASE WIDGET CANNOT SORT
-   * OPEN IT AGAIN AFTER THE TICKET IS DONE
   @Test
   public void testDefaultSortOnCaseWidget() {
     redirectToRelativeLink(createCaseWithTechnicalCaseUrl);
@@ -271,6 +273,5 @@ public class DashboardEditCaseWidgetTest extends BaseTest {
     caseEditWidgetPage.clickOnCaseNameColumn();
     caseEditWidgetPage.getFirstCaseOfCaseWidget().shouldHave(text(ORDER_PIZZA), DEFAULT_TIMEOUT);
   }
-    */
     
 }
