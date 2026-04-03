d7749964077b127167b2245706be3f2bd5aaf624
lmluat-axonivy <luat.luongminh@axonactive.com>
2026-04-03 09:24:38 +0700

    bug/IVYPORTAL-20370-Awesome-icons-for-default-dashboard-not-working
    - update buildDashboardItem

--- AxonIvyPortal/portal/src/ch/addon/portal/generic/menu/MenuView.java
+++ AxonIvyPortal/portal/src/ch/addon/portal/generic/menu/MenuView.java
@@ -160,7 +160,7 @@ public class MenuView implements Serializable {
     } else if (subItemDashboards.size() == 1) {
       Dashboard dashboard = subItemDashboards.getFirst();
       String localizedTitle = getLocalizedTitle(dashboard, currentLanguage, dashboardTitle);
-      return buildSingleDashboardMenu(localizedTitle, dashboardId, dashboardLink, dashboard.getIcon());
+      return buildSingleDashboardMenu(localizedTitle, dashboardId, dashboardLink, dashboard.getIconClass());
     }
     
     return buildSingleDashboardMenu(dashboardTitle, "", dashboardLink, "");
