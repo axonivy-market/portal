821b173fdc830ab4332273166144dabefbafbc0b
Thai Phan <thai.phanvan@axonactive.com>
2026-04-06 09:53:14 +0700

    IVYPORTAL-20317 Fix guitest

--- AxonIvyPortal/portal-selenium-test/src_test/com/axonivy/portal/selenium/common/ComplexFilterHelper.java
+++ AxonIvyPortal/portal-selenium-test/src_test/com/axonivy/portal/selenium/common/ComplexFilterHelper.java
@@ -201,11 +201,7 @@ public class ComplexFilterHelper{
       dateInput.get(i).shouldBe(Condition.empty, DEFAULT_TIMEOUT).sendKeys(String.valueOf(values[i]));
       WaitHelper.waitPageSilence();
 
-      if ($("div[id='new-widget-configuration-dialog']").isDisplayed()) {
-        filterElement.$("span button").$("span[class*='ui-icon-calendar']").click();
-      } else {
-        dateInput.get(i).pressEscape();
-      }
+      filterElement.$("span button").$("span[class*='ui-icon-calendar']").shouldBe(appear, DEFAULT_TIMEOUT).click();
     }
   }
 
@@ -226,13 +222,12 @@ public class ComplexFilterHelper{
         .shouldBe(Condition.editable);
     fromInput.clear();
     fromInput.sendKeys(String.valueOf(values[0]));
-    fromInput.pressEscape();
 
     var toInput = filterElement.$("div[id$=':between-dates-panel-to']").$("input[id$=':to-date_input']")
         .shouldBe(Condition.editable);
     toInput.clear();
     toInput.sendKeys(String.valueOf(values[1]));
-    toInput.pressEscape();
+    filterElement.$("div[id$=':between-dates-panel-to']").$("button").$("span[class*='ui-icon-calendar']").shouldBe(appear, DEFAULT_TIMEOUT).click();
   }
 
   private static SelenideElement getValueOfCheckBox(String value) {
