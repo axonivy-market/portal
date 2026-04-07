f941b7a0a9e98a37ba75ae5beb66e96c5774d14c
nhthinh-axonivy <thinh.nguyenhung@axonactive.com>
2026-04-07 15:09:56 +0700

    feature/IVYPORTAL-20217-Control-click-behavior-of-the-side-bar-with-an-admin-setting - Change logo

--- AxonIvyPortal/portal/webContent/resources/js/sidebar.js
+++ AxonIvyPortal/portal/webContent/resources/js/sidebar.js
@@ -6,6 +6,9 @@ var SidebarClickMode = (function () {
   const SEL_WRAPPER = '.js-layout-wrapper';
   const SEL_MENU_WRAPPER = '.menu-wrapper.js-left-sidebar';
   const SEL_TOGGLE_BTN = '.js-sidebar-toggle-btn';
+  const SEL_TOGGLE_ICON = '.js-sidebar-toggle-icon';
+  const CLS_ICON_EXPAND = 'si-arrow-right-1';
+  const CLS_ICON_COLLAPSE = 'si-arrow-left-1';
 
   const FREYA_TRANSITION_MS = 250;
   const COOKIE_MENU_STATIC = 'freya_menu_static';
@@ -25,9 +28,11 @@ var SidebarClickMode = (function () {
         if ($.cookie(COOKIE_MENU_STATIC)) {
           this._isExpanded = true;
           $(SEL_TOGGLE_BTN).attr('aria-expanded', 'true');
+          $(SEL_TOGGLE_ICON).removeClass(CLS_ICON_EXPAND).addClass(CLS_ICON_COLLAPSE);
         } else {
           this._isExpanded = false;
           this.ensureCollapsed();
+          $(SEL_TOGGLE_ICON).removeClass(CLS_ICON_COLLAPSE).addClass(CLS_ICON_EXPAND);
         }
         if (!this._initialized) {
           this.bindSidebarClick();
@@ -71,9 +76,11 @@ var SidebarClickMode = (function () {
       if (this._isExpanded) {
         $layoutWrapper.addClass(CLS_LAYOUT_STATIC);
         $(SEL_TOGGLE_BTN).attr('aria-expanded', 'true');
+        $(SEL_TOGGLE_ICON).removeClass(CLS_ICON_EXPAND).addClass(CLS_ICON_COLLAPSE);
       } else {
         $layoutWrapper.removeClass(CLS_LAYOUT_STATIC);
         $(SEL_TOGGLE_BTN).attr('aria-expanded', 'false');
+        $(SEL_TOGGLE_ICON).removeClass(CLS_ICON_COLLAPSE).addClass(CLS_ICON_EXPAND);
       }
       var self = this;
       setTimeout(function () {
