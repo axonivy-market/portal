06a31fd9e0fac1986351b8cde7b1cc01c294a37b
nhthinh-axonivy <thinh.nguyenhung@axonactive.com>
2026-05-18 18:44:30 +0700

    feature/IVYPORTAL-20276-change-to-Tabler-icons - fix GUI tests

--- AxonIvyPortal/portal-selenium-test/src_test/com/axonivy/portal/selenium/page/ProcessWidgetPage.java
+++ AxonIvyPortal/portal-selenium-test/src_test/com/axonivy/portal/selenium/page/ProcessWidgetPage.java
@@ -110,8 +110,9 @@ public class ProcessWidgetPage extends TemplatePage {
 
     $("input[id$=':search-icon-name-field']").sendKeys(iconClass);
 
-    $("div[id='process-widget:add-external-link-form:external-link-icon:icons-selection-form:icons']")
-        .$("a[title='" + iconClass + "']").click();
+    $("div[id='process-widget:add-external-link-form:external-link-icon:icons-selection-form:tabler-icons']")
+      .$("a.icon-selection-dialog-selecting-icon[title='" + iconClass + "']").shouldBe(appear, DEFAULT_TIMEOUT)
+      .click();
     $("div[id$='process-widget:add-external-link-form:external-link-icon:select-icon-dialog']")
         .shouldBe(Condition.disappear, DEFAULT_TIMEOUT);
 
