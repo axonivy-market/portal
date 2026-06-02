d512b7f152bc1940767b27bd9ffa40df0b653eff
Tung Le <tung.le@axonactive.com>
2026-06-02 11:54:37 +0700

    IVYPORTAL-20451 Handle more js problems

--- AxonIvyPortal/portal-components/src/com/axonivy/portal/components/document/PDFDocumentDetector.java
+++ AxonIvyPortal/portal-components/src/com/axonivy/portal/components/document/PDFDocumentDetector.java
@@ -1,13 +1,18 @@
 package com.axonivy.portal.components.document;
 
+import java.io.IOException;
 import java.io.InputStream;
 
 import org.apache.pdfbox.Loader;
-import org.apache.pdfbox.cos.COSArray;
+import org.apache.pdfbox.cos.COSBase;
 import org.apache.pdfbox.cos.COSDictionary;
 import org.apache.pdfbox.cos.COSName;
 import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDDocumentNameDictionary;
+import org.apache.pdfbox.pdmodel.PDEmbeddedFilesNameTreeNode;
+import org.apache.pdfbox.pdmodel.PDPage;
 import org.apache.pdfbox.pdmodel.encryption.InvalidPasswordException;
+import org.apache.pdfbox.pdmodel.interactive.annotation.PDAnnotation;
 
 import ch.ivyteam.ivy.bpm.error.BpmError;
 import ch.ivyteam.ivy.environment.Ivy;
@@ -20,19 +25,10 @@ public class PDFDocumentDetector implements DocumentDetector {
       return false;
     }
     try (PDDocument document = Loader.loadPDF(inputStream.readAllBytes())) {
-      COSDictionary root = document.getDocumentCatalog().getCOSObject();
-
-      if (detectJavaScript(root)) {
-        return false;
-      }
-
-      COSDictionary names = root.getCOSDictionary(COSName.NAMES);
-      COSArray embeddedFiles = getEmbeddedFiles(names);
-      boolean hasEmbeddedFiles = embeddedFiles != null && embeddedFiles.size() > 0;
-      boolean hasAcroForm = root.getCOSDictionary(COSName.ACRO_FORM) != null;
-      boolean hasOpenAction = root.getDictionaryObject(COSName.OPEN_ACTION) != null;
-
-      return !hasEmbeddedFiles && !hasAcroForm && !hasOpenAction;
+      return !containsJavaScript(document)
+          && !containsEmbeddedFiles(document)
+          && !containsAcroForm(document)
+          && !containsOpenAction(document);
     } catch (InvalidPasswordException e) {
       Ivy.log().error("This file is encrypted and cannot be scanned for security threats before uploading.");
       BpmError.create("portal:file:encrypted").throwError();
@@ -42,22 +38,82 @@ public class PDFDocumentDetector implements DocumentDetector {
     return false;
   }
 
-  private boolean detectJavaScript(COSDictionary root) {
-    COSDictionary names = root.getCOSDictionary(COSName.NAMES);
-    if (names != null && names.getCOSDictionary(COSName.JAVA_SCRIPT) != null) {
+  private boolean containsJavaScript(PDDocument document) throws IOException {
+    if (hasCatalogLevelJavaScript(document)) {
       return true;
     }
-    return root.getCOSDictionary(COSName.AA) != null;
+    return hasPageOrAnnotationJavaScript(document);
   }
 
-  private COSArray getEmbeddedFiles(COSDictionary names) {
-    if (names == null) {
-      return null;
+  private boolean hasCatalogLevelJavaScript(PDDocument document) {
+    COSDictionary catalog = document.getDocumentCatalog().getCOSObject();
+    COSDictionary names = catalog.getCOSDictionary(COSName.NAMES);
+    boolean hasJavaScriptNameTree = names != null && names.getCOSDictionary(COSName.JAVA_SCRIPT) != null;
+    boolean hasCatalogAdditionalActions = catalog.getCOSDictionary(COSName.AA) != null;
+    return hasJavaScriptNameTree || hasCatalogAdditionalActions;
+  }
+
+  private boolean hasPageOrAnnotationJavaScript(PDDocument document) throws IOException {
+    for (PDPage page : document.getPages()) {
+      if (hasJavaScriptInActions(page.getCOSObject())) {
+        return true;
+      }
+      for (PDAnnotation annotation : page.getAnnotations()) {
+        if (hasJavaScriptInActions(annotation.getCOSObject())) {
+          return true;
+        }
+      }
     }
-    COSDictionary embeddedFiles = names.getCOSDictionary(COSName.EMBEDDED_FILES);
-    if (embeddedFiles == null) {
-      return null;
+    return false;
+  }
+
+  private boolean hasJavaScriptInActions(COSDictionary cosObject) {
+    if (hasJavaScriptInAdditionalActions(cosObject)) {
+      return true;
     }
-    return (COSArray) embeddedFiles.getDictionaryObject(COSName.NAMES);
+    COSDictionary directAction = cosObject.getCOSDictionary(COSName.A);
+    return directAction != null && isJavaScriptAction(directAction);
+  }
+
+  private boolean hasJavaScriptInAdditionalActions(COSDictionary cosObject) {
+    COSDictionary additionalActions = cosObject.getCOSDictionary(COSName.AA);
+    if (additionalActions == null) {
+      return false;
+    }
+    for (COSName key : additionalActions.keySet()) {
+      COSBase actionEntry = additionalActions.getDictionaryObject(key);
+      if (actionEntry instanceof COSDictionary && isJavaScriptAction((COSDictionary) actionEntry)) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  private boolean isJavaScriptAction(COSDictionary action) {
+    COSBase actionType = action.getDictionaryObject(COSName.S);
+    return COSName.JAVA_SCRIPT.equals(actionType);
+  }
+
+  private boolean containsEmbeddedFiles(PDDocument document) {
+    PDDocumentNameDictionary namesDictionary = document.getDocumentCatalog().getNames();
+    if (namesDictionary == null) {
+      return false;
+    }
+    PDEmbeddedFilesNameTreeNode embeddedFilesTree = namesDictionary.getEmbeddedFiles();
+    if (embeddedFilesTree == null) {
+      return false;
+    }
+    COSDictionary treeNode = embeddedFilesTree.getCOSObject();
+    return treeNode.containsKey(COSName.NAMES) || treeNode.containsKey(COSName.KIDS);
+  }
+
+  private boolean containsAcroForm(PDDocument document) {
+    COSDictionary catalog = document.getDocumentCatalog().getCOSObject();
+    return catalog.getCOSDictionary(COSName.ACRO_FORM) != null;
+  }
+
+  private boolean containsOpenAction(PDDocument document) {
+    COSDictionary catalog = document.getDocumentCatalog().getCOSObject();
+    return catalog.getDictionaryObject(COSName.OPEN_ACTION) != null;
   }
 }
