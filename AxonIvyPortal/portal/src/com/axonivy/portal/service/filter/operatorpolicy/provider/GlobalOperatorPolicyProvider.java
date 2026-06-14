a2e159fb69b702fa60dee711e036e4a7cf65905a
hhqthang-axonivy <hhqthang@axonactive.com>
2026-06-14 23:01:47 +0700

    feat: add operator policy model and provider classes

---
+++ AxonIvyPortal/portal/src/com/axonivy/portal/service/filter/operatorpolicy/provider/GlobalOperatorPolicyProvider.java
@@ -0,0 +1,45 @@
+package com.axonivy.portal.service.filter.operatorpolicy.provider;
+
+import java.io.Serializable;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import org.apache.commons.lang3.StringUtils;
+
+import com.axonivy.portal.enums.dashboard.filter.FilterOperator;
+import com.axonivy.portal.service.filter.operatorpolicy.model.GlobalOperatorPolicy;
+
+import ch.ivy.addon.portalkit.enums.GlobalVariable;
+import ch.ivyteam.ivy.environment.Ivy;
+
+public class GlobalOperatorPolicyProvider implements Serializable {
+
+  private static final long serialVersionUID = 1L;
+
+  private static final String GLOBAL_OPERATOR_POLICY_KEY = GlobalVariable.FILTER_OPERATOR_POLICY.getKey();
+
+  public GlobalOperatorPolicy getPolicy() {
+    return parse(Ivy.var().get(GLOBAL_OPERATOR_POLICY_KEY));
+  }
+
+  GlobalOperatorPolicy parse(String rawPolicy) {
+    if (StringUtils.isBlank(rawPolicy)) {
+      return GlobalOperatorPolicy.empty();
+    }
+
+    Set<FilterOperator> enabledOperators = parseOperators(rawPolicy.trim().split(","));
+    List<FilterOperator> disabledOperators = Set.of(FilterOperator.values()).stream()
+        .filter(operator -> !enabledOperators.contains(operator)).collect(Collectors.toList());
+
+    return disabledOperators.isEmpty() ? GlobalOperatorPolicy.empty() : GlobalOperatorPolicy.of(disabledOperators);
+  }
+
+  private Set<FilterOperator> parseOperators(String[] operators) {
+    return Arrays.stream(operators).filter(StringUtils::isNotBlank)
+        .map(FilterOperator::fromString).filter(Optional::isPresent).map(Optional::get)
+        .collect(Collectors.toSet());
+  }
+}
