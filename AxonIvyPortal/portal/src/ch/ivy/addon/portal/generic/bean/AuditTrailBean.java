96ad9decac9b51ca3657f19c283ae7c31f49ee3c
lmluat-axonivy <luat.luongminh@axonactive.com>
2026-06-18 10:35:22 +0000

    - update css - update logic

--- AxonIvyPortal/portal/src/ch/ivy/addon/portal/generic/bean/AuditTrailBean.java
+++ AxonIvyPortal/portal/src/ch/ivy/addon/portal/generic/bean/AuditTrailBean.java
@@ -7,6 +7,8 @@ import java.util.List;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 
+import org.primefaces.event.TabChangeEvent;
+
 import ch.ivyteam.ivy.environment.Ivy;
 import ch.ivyteam.ivy.workflow.ICase;
 import ch.ivyteam.ivy.workflow.ITask;
@@ -16,9 +18,22 @@ import ch.ivyteam.ivy.workflow.ITask;
 public class AuditTrailBean implements Serializable {
     private static final long serialVersionUID = -440646080216267529L;
     private List<ITask> previousTasks = new ArrayList<>();
+    private Long taskId;
+    private ICase currentCase;
 
-    public void prepareData(Long taskId, ICase currentCase) {
-        Ivy.log().info("current case id" + currentCase.getId());
+    public void init(Long taskId, ICase currentCase) {
+        this.taskId = taskId;
+        this.currentCase = currentCase;
+    }
+
+    public void onTabChange(TabChangeEvent<?> event) {
+        if ("Tasks".equals(event.getTab().getTitle())) {
+            loadPreviousTasks();
+            Ivy.log().info(this.previousTasks.size());
+        }
+    }
+
+    private void loadPreviousTasks() {
         List<ITask> allTasks = currentCase.tasks().all();
         setPreviousTasks(getTasksBeforeCurrent(allTasks, taskId));
     }
@@ -36,5 +51,4 @@ public class AuditTrailBean implements Serializable {
     public void setPreviousTasks(List<ITask> previousTasks) {
         this.previousTasks = previousTasks;
     }
-
 }
