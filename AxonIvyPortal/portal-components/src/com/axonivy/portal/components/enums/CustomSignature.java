30ac1148917d5b28ae51854b7d4097df2c1de695
Nam Chu <117632090+chnam-axonivy@users.noreply.github.com>
2026-07-09 08:40:45 +0700

    Revert to original version

--- AxonIvyPortal/portal-components/src/com/axonivy/portal/components/enums/CustomSignature.java
+++ AxonIvyPortal/portal-components/src/com/axonivy/portal/components/enums/CustomSignature.java
@@ -3,9 +3,9 @@ package com.axonivy.portal.components.enums;
 public enum CustomSignature {
   GET_DOCUMENT_ITEMS("portalGetDocumentItems(ch.ivyteam.ivy.workflow.ICase)"),
   UPLOAD_DOCUMENT_ITEM("portalUploadDocumentItem(ch.ivyteam.ivy.workflow.ICase,org.primefaces.model.file.UploadedFile,Boolean,Boolean,String)"),
-  DOWNLOAD_DOCUMENT_ITEM("portalDownloadDocumentItem(ch.ivyteam.ivy.workflow.ICase,com.axonivy.portal.components.ivydata.bo.IvyDocument,String)"),
-  DELETE_DOCUMENT_ITEM("portalDeleteDocumentItem(ch.ivyteam.ivy.workflow.ICase,com.axonivy.portal.components.ivydata.bo.IvyDocument,String)"),
-  RENAME_DOCUMENT_ITEM("portalRenameDocumentItem(ch.ivyteam.ivy.workflow.ICase,com.axonivy.portal.components.ivydata.bo.IvyDocument,String)");
+  DOWNLOAD_DOCUMENT_ITEM("portalDownloadDocumentItem(ch.ivyteam.ivy.workflow.ICase,com.axonivy.portal.components.ivydata.bo.IvyDocument)"),
+  DELETE_DOCUMENT_ITEM("portalDeleteDocumentItem(ch.ivyteam.ivy.workflow.ICase,com.axonivy.portal.components.ivydata.bo.IvyDocument)"),
+  RENAME_DOCUMENT_ITEM("portalRenameDocumentItem(ch.ivyteam.ivy.workflow.ICase,com.axonivy.portal.components.ivydata.bo.IvyDocument)");
 
   private String signature;
 
