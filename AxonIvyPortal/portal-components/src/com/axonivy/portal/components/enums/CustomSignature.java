c4587bad38bb5ae3bb4f136fee81cdc2c997a05c
lmluat-axonivy <luat.luongminh@axonactive.com>
2026-07-14 02:41:32 +0000

    remove unuse files

--- AxonIvyPortal/portal-components/src/com/axonivy/portal/components/enums/CustomSignature.java
+++ AxonIvyPortal/portal-components/src/com/axonivy/portal/components/enums/CustomSignature.java
@@ -5,11 +5,7 @@ public enum CustomSignature {
   UPLOAD_DOCUMENT_ITEM("portalUploadDocumentItem(ch.ivyteam.ivy.workflow.ICase,org.primefaces.model.file.UploadedFile,Boolean,Boolean,String)"),
   DOWNLOAD_DOCUMENT_ITEM("portalDownloadDocumentItem(ch.ivyteam.ivy.workflow.ICase,com.axonivy.portal.components.ivydata.bo.IvyDocument)"),
   DELETE_DOCUMENT_ITEM("portalDeleteDocumentItem(ch.ivyteam.ivy.workflow.ICase,com.axonivy.portal.components.ivydata.bo.IvyDocument)"),
-  RENAME_DOCUMENT_ITEM("portalRenameDocumentItem(ch.ivyteam.ivy.workflow.ICase,com.axonivy.portal.components.ivydata.bo.IvyDocument)"),
-  GET_TASK_NOTES("portalGetTaskNotes(Long)"),
-  GET_AUDIT_TRAIL_DATA("portalGetAuditTrailData(ch.ivyteam.ivy.workflow.ICase)"),
-  SAVE_ADDITIONAL_AUDIT_TRAIL_DATA("portalSaveAdditionalAuditTrailData(ch.ivyteam.ivy.workflow.ICase,java.util.List<com.axonivy.portal.components.dto.AuditTrailDTO>)");
-
+  RENAME_DOCUMENT_ITEM("portalRenameDocumentItem(ch.ivyteam.ivy.workflow.ICase,com.axonivy.portal.components.ivydata.bo.IvyDocument)")
   private String signature;
 
   private CustomSignature(String signature) {
