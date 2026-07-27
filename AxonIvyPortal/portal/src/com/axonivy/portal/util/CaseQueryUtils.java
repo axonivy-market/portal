6d727484e562cce0f743ec3b31c5888f2cfa0478
Tung Le <tung.le@axonactive.com>
2026-07-27 16:21:00 +0700

    feat(IVYPORTAL-20780): variable to switch query CaseQuery.create() and businessCase()/subCases()

--- AxonIvyPortal/portal/src/com/axonivy/portal/util/CaseQueryUtils.java
+++ AxonIvyPortal/portal/src/com/axonivy/portal/util/CaseQueryUtils.java
@@ -1,19 +1,34 @@
 package com.axonivy.portal.util;
 
+import org.apache.commons.lang3.StringUtils;
+
+import com.axonivy.portal.enums.CaseQueryStrategy;
+
 import ch.ivy.addon.portalkit.enums.DashboardColumnType;
+import ch.ivyteam.ivy.environment.Ivy;
 import ch.ivyteam.ivy.workflow.query.CaseQuery;
 
 public final class CaseQueryUtils {
+
+  private static final String RELATED_CASE_QUERY_STRATEGY_VAR = "Portal.Tasks.RelatedCaseQueryStrategy";
 
   private CaseQueryUtils() {
   }
 
   public static CaseQuery initCaseQuery(DashboardColumnType type) {
+    if (currentStrategy() == CaseQueryStrategy.LEGACY) {
+      return CaseQuery.create();
+    }
     if (DashboardColumnType.CUSTOM_BUSINESS_CASE == type) {
       return CaseQuery.businessCases();
     } else if (DashboardColumnType.CUSTOM_CASE == type) {
       return CaseQuery.subCases();
     }
     return CaseQuery.create();
+  }
+
+  private static CaseQueryStrategy currentStrategy() {
+    String strategy = Ivy.var().get(RELATED_CASE_QUERY_STRATEGY_VAR);
+    return StringUtils.isBlank(strategy) ? CaseQueryStrategy.LEGACY : CaseQueryStrategy.valueOf(strategy);
   }
 }
