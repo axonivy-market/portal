7674b5a42d751223d98aa003b389eac12e3c1036
Nam Chu <117632090+chnam-axonivy@users.noreply.github.com>
2026-08-04 00:24:35 +0700

    fix test

--- AxonIvyPortal/portal-selenium-test/src_test/com/axonivy/portal/selenium/test/dashboard/DashboardCaseWidgetFilterTest.java
+++ AxonIvyPortal/portal-selenium-test/src_test/com/axonivy/portal/selenium/test/dashboard/DashboardCaseWidgetFilterTest.java
@@ -24,7 +24,7 @@ import com.codeborne.selenide.CollectionCondition;
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.ElementsCollection;
 
-@IvyWebTest(headless = false)
+@IvyWebTest
 public class DashboardCaseWidgetFilterTest extends BaseTest {
 
   private NewDashboardPage newDashboardPage;
