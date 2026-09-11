641d243a0f34ae51da2db132712bbcf5b82e1396
hhqthang-axonivy <hhqthang@axonactive.com>
2026-09-11 15:38:59 +0700

    fix(IVYPORTAL-21082): ignore quickSearch field to json if default value

--- AxonIvyPortal/portal/src/ch/ivy/addon/portalkit/util/DashboardWidgetUtils.java
+++ AxonIvyPortal/portal/src/ch/ivy/addon/portalkit/util/DashboardWidgetUtils.java
@@ -356,6 +356,9 @@ public class DashboardWidgetUtils {
     if (BooleanUtils.isTrue(column.getEnableFilter())) {
       column.setEnableFilter(null);
     }
+    if (BooleanUtils.isFalse(column.getQuickSearch())) {
+      column.setQuickSearch(null);
+    }
     if (BooleanUtils.isTrue(column.getSortable())) {
       column.setSortable(null);
     }
