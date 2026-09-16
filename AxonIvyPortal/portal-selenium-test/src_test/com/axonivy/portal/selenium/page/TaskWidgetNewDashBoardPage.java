aff2cc8a18c9d965b50879f43ffa13a36e229f7a
An Bui <an.buiduy@axonactive.com>
2026-09-16 01:23:33 +0000

    fix(IVYPORTAL-20935): Refactor save filter method and update filter dialog selectors for consistency

--- AxonIvyPortal/portal-selenium-test/src_test/com/axonivy/portal/selenium/page/TaskWidgetNewDashBoardPage.java
+++ AxonIvyPortal/portal-selenium-test/src_test/com/axonivy/portal/selenium/page/TaskWidgetNewDashBoardPage.java
@@ -563,12 +563,10 @@ public class TaskWidgetNewDashBoardPage extends TemplatePage {
   }
 
   public void saveFilter(String widgetFilterName) {
-    $("div.filter-overlay-panel__footer").shouldBe(appear, DEFAULT_TIMEOUT).$$("button[id$='save-filter']")
-        .filter(text("Save filter")).first().shouldBe(getClickableCondition()).click();
-    $("div#save-widget-filter-dialog").$("input[id='save-filter-form:save-filter-name']")
-        .shouldBe(appear, DEFAULT_TIMEOUT).setValue(widgetFilterName);
-    $("button[id$=':save-widget-filter-button']").click();
-    $("div[id$=':widget-saved-filters-items']").$$("div.saved-filter__items").filter(text(widgetFilterName)).first()
+    SelenideElement filterDialog = getConfigurationFilter();
+    filterDialog.$("input[id$=':inline-save-filter-name']").shouldBe(appear, DEFAULT_TIMEOUT).setValue(widgetFilterName);
+    filterDialog.$("button[id$=':inline-save-filter']").shouldBe(getClickableCondition(), DEFAULT_TIMEOUT).click();
+    $("[id$=':widget-saved-filters-items']").$$("span.saved-filter-node__text").filter(text(widgetFilterName)).first()
         .shouldBe(appear, DEFAULT_TIMEOUT);
   }
 
@@ -629,7 +627,7 @@ public class TaskWidgetNewDashBoardPage extends TemplatePage {
   }
 
   public SelenideElement getConfigurationFilter() {
-    return $("div[class*='filter-overlay-panel'][style*='display: block']").shouldBe(appear, DEFAULT_TIMEOUT);
+    return $("div.filter-dialog[style*='display: block']").shouldBe(appear, DEFAULT_TIMEOUT);
   }
 
   public void removeFocusFilterDialog() {
@@ -657,7 +655,7 @@ public class TaskWidgetNewDashBoardPage extends TemplatePage {
 
   public int getNumberOfFilter() {
     return $("div[id$='widget-filter-content']").shouldBe(appear, DEFAULT_TIMEOUT)
-        .$("div[class*='filter-overlay-panel__content']").shouldBe(appear, DEFAULT_TIMEOUT)
+        .$("div[class*='filter-dialog__content']").shouldBe(appear, DEFAULT_TIMEOUT)
         .$("div[id$='filter-container']").shouldBe(appear, DEFAULT_TIMEOUT)
         .$$("div[class*='dashboard-widget-filter__filter-wrapper']").size();
   }
